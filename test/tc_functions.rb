1aab204c5bf720be65eb87663636f0e901bfb359
Luke Kanies <luke@madstop.com>
2005-04-13 18:22:03 +0000

    functions mostly work
    git-svn-id: https://reductivelabs.com/svn/puppet/library/trunk@111 980ebf18-57e1-0310-9a29-db15c13687c0

---
+++ test/tc_functions.rb
@@ -0,0 +1,25 @@
+$:.unshift '../lib' if __FILE__ == $0 # Make this library first!
+
+require 'blink/function'
+require 'blink/fact'
+require 'test/unit'
+
+# $Id$
+
+class TestFunctions < Test::Unit::TestCase
+    def test_retrieve
+        vars = %w{operatingsystem}
+
+        vars.each { |var|
+            value = nil
+            assert_nothing_raised() {
+                value = Blink::Function["retrieve"].call(var)
+            }
+
+            assert_equal(
+                Blink::Fact[var],
+                value
+            )
+        }
+    end
+end
