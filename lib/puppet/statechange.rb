9c7c71ccd8631c0dce65e91380704338a9dab219
Luke Kanies <luke@madstop.com>
2005-08-04 18:42:17 +0000

    passing state, so more info can be extracted
    git-svn-id: https://reductivelabs.com/svn/puppet/library/trunk@498 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/statechange.rb
+++ lib/puppet/statechange.rb
@@ -60,7 +60,7 @@ module Puppet
                 # should basically point to that, right?
                 return Puppet::Event.new(
                     :event => event,
-                    :state => @state.name,
+                    :state => @state,
                     :object => @state.parent,
                     :transaction => @transaction,
                     :message => self.to_s
@@ -76,7 +76,7 @@ module Puppet
                 #end
                 return Puppet::Event.new(
                     :event => pname + "_failed",
-                    :state => @state.name,
+                    :state => @state,
                     :object => @state.parent,
                     :transaction => @transaction,
                     :message => "Failed: " + self.to_s
