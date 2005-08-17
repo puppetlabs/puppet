4f0750bb5e57fa625205574ddccb9fe3eef76b12
Luke Kanies <luke@madstop.com>
2005-08-17 15:35:39 +0000

    adding
    git-svn-id: https://reductivelabs.com/svn/puppet/library/trunk@554 980ebf18-57e1-0310-9a29-db15c13687c0

---
+++ test/other/tc_transportable.rb
@@ -0,0 +1,14 @@
+if __FILE__ == $0
+    $:.unshift '..'
+    $:.unshift '../../lib'
+    $puppetbase = "../../../../language/trunk"
+end
+
+require 'puppet'
+require 'puppettest'
+
+# $Id$
+
+class TestPortable < TestPuppet
+    
+end
