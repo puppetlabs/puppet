b7974b59ec046837d0fdf4d732ccbedddb8de146
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-01-09 17:18:18 +0000

    Updated to version 0.10.0
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@792 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -12,7 +12,7 @@ require 'puppet/util'
 #
 # it's also a place to find top-level commands like 'debug'
 module Puppet
-PUPPETVERSION = '0.9.4'
+PUPPETVERSION = '0.10.0'
 
     def Puppet.version
         return PUPPETVERSION
