a5d24049f7c53edfcbd88c940633c34514dad827
lutter <lutter@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-02-07 23:53:05 +0000

    Simple emacs mode for editing manifests; only does pretty colors right now
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@874 980ebf18-57e1-0310-9a29-db15c13687c0

---
+++ ext/emacs/puppet-mode-init.el
@@ -0,0 +1,6 @@
+;;
+;; Setup puppet-mode for autoloading
+;;
+(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
+
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
