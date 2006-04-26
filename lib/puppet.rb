77bf69c5527e66bb8f06aaeac7ad10847866d7f5
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-04-26 16:38:10 +0000

    Adding release tag REL_0_16_1
    git-svn-id: https://reductivelabs.com/svn/puppet/tags/REL_0_16_1@1138 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -14,7 +14,7 @@ require 'puppet/util'
 #
 # it's also a place to find top-level commands like 'debug'
 module Puppet
-    PUPPETVERSION = '0.16.0'
+    PUPPETVERSION = '0.16.1'
 
     def Puppet.version
         return PUPPETVERSION
