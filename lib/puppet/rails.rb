89ce72ff802b5dcfbbc01aa9818e3eb4a869bfa8
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-05-17 23:42:22 +0000

    fixing stupid debian rails mistake
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@1205 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/rails.rb
+++ lib/puppet/rails.rb
@@ -6,19 +6,20 @@ require 'puppet'
 begin
     require 'active_record'
 rescue LoadError => detail
-    if Facter["operatingsystem"].value == "Debian"
-        count = 0
-        Dir.entries("/usr/share/rails").each do |dir|
-            libdir = File.join("/usr/share/rails", dir, "lib")
-            if FileTest.exists?(libdir) and ! $:.include?(libdir)
-                count += 1
-                $: << libdir
+    if Facter["operatingsystem"].value == "Debian" and
+        FileTest.exists?("/usr/share/rails")
+            count = 0
+            Dir.entries("/usr/share/rails").each do |dir|
+                libdir = File.join("/usr/share/rails", dir, "lib")
+                if FileTest.exists?(libdir) and ! $:.include?(libdir)
+                    count += 1
+                    $: << libdir
+                end
             end
-        end
 
-        if count > 0
-            retry
-        end
+            if count > 0
+                retry
+            end
     end
 end
 
