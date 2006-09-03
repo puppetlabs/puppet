2cb2e03071f3411d1418ec19b14b0d13035e691a
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-09-03 01:56:45 +0000

    Applying patch from #251, and switching "confine" to "commands", so we can document the command requirements.
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@1535 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/provider/service/redhat.rb
+++ lib/puppet/provider/service/redhat.rb
@@ -4,15 +4,15 @@ Puppet::Type.type(:service).provide :redhat, :parent => :init do
     desc "Red Hat's (and probably many others) form of ``init``-style service
         management; uses ``chkconfig`` for service enabling and disabling."
 
-    confine :exists => "/sbin/chkconfig"
+    commands :chkconfig => "/sbin/chkconfig"
 
-    defaultfor :operatingsystem => [:redhat, :fedora]
+    defaultfor :operatingsystem => [:redhat, :fedora, :suse]
 
     # Remove the symlinks
     def disable
         begin
-            output = util_execute("/sbin/chkconfig #{@model[:name]} off 2>&1")
-            output += util_execute("/sbin/chkconfig --del #{@model[:name]} 2>&1")
+            output = util_execute("#{command(:chkconfig)} #{@model[:name]} off 2>&1")
+            output += util_execute("#{command(:chkconfig)} --del #{@model[:name]} 2>&1")
         rescue Puppet::ExecutionFailure
             raise Puppet::Error, "Could not disable %s: %s" %
                 [self.name, output]
@@ -21,11 +21,17 @@ Puppet::Type.type(:service).provide :redhat, :parent => :init do
 
     def enabled?
         begin
-            output = util_execute("/sbin/chkconfig #{@model[:name]} 2>&1").chomp
+            output = util_execute("#{command(:chkconfig)} #{@model[:name]} 2>&1").chomp
         rescue Puppet::ExecutionFailure
             return :false
         end
 
+        # If it's disabled on SuSE, then it will print output showing "off"
+        # at the end
+        if output =~ /.* off$/
+            return :false
+        end
+	
         return :true
     end
 
@@ -33,8 +39,8 @@ Puppet::Type.type(:service).provide :redhat, :parent => :init do
     # in the init scripts.
     def enable
         begin
-            output = util_execute("/sbin/chkconfig --add #{@model[:name]} 2>&1")
-            output += util_execute("/sbin/chkconfig #{@model[:name]} on 2>&1")
+            output = util_execute("#{command(:chkconfig)} --add #{@model[:name]} 2>&1")
+            output += util_execute("#{command(:chkconfig)} #{@model[:name]} on 2>&1")
         rescue Puppet::ExecutionFailure
             raise Puppet::Error, "Could not enable %s: %s" %
                 [self.name, output]
