5b6ee8c823a78f26cd1a1ad957da3d234910b9c2
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-10-07 00:19:10 +0000

    Adding a "genmodule" equivalent to classgen, which we will use for reporting
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@1745 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/util/classgen.rb
+++ lib/puppet/util/classgen.rb
@@ -25,85 +25,26 @@ module Puppet::Util::ClassGen
     # * <tt>:prefix</tt>: The constant prefix.  Default to nothing; if specified,
     #   the capitalized name is appended and the result is set as the constant.
     def genclass(name, options = {}, &block)
-        options = symbolize_options(options)
-        parent = options[:parent] || self
+        genthing(name, Class, options, block)
+    end
 
-        name = symbolize(name.to_s.downcase)
-
-        # Create the class, with the correct name.
-        klass = Class.new(parent) do
-            @name = name
-        end
-
-        unless const = options[:constant]
-            prefix = options[:prefix] || ""
-            const = prefix + name2const(name)
-        end
-
-        if const_defined? const
-            if options[:overwrite]
-                Puppet.info "Redefining %s subclass %s" % [parent, name]
-                remove_const(const)
-            else
-                raise Puppet::ConstantAlreadyDefined,
-                    "Class %s is already defined in %s" % [const, parent]
-            end
-        end
-        const_set(const, klass)
-
-        # Initialize any necessary variables.
-        if klass.respond_to? :initvars
-            klass.initvars
-        end
-
-        if attrs = options[:attributes]
-            attrs.each do |param, value|
-                method = param.to_s + "="
-                if klass.respond_to? method
-                    klass.send(method, value)
-                end
-            end
-        end
-
-        if klass.respond_to? :preinit
-            klass.preinit
-        end
-
-        block ||= options[:block]
-
-        # Evaluate the passed block if there is one.  This should usually
-        # define all of the work.
-        if block
-            klass.class_eval(&block)
-        end
-
-        if klass.respond_to? :postinit
-            klass.postinit
-        end
-
-        # If we were told to stick it in a hash, then do so
-        if hash = options[:hash]
-            if hash.include? name and ! options[:overwrite]
-                raise SubclassAlreadyDefined,
-                    "Already a generated class named %s" % name
-            end
-
-            hash[name] = klass
-        end
-
-        # If we were told to stick it in a hash, then do so
-        if array = options[:array]
-            if (klass.respond_to? :name and
-                            array.find { |c| c.name == name } and
-                            ! options[:overwrite])
-                raise SubclassAlreadyDefined,
-                    "Already a generated class named %s" % name
-            end
-
-            array << klass
-        end
-
-        return klass
+    # Create a new module.  Valid options are:
+    # * <tt>:array</tt>: An array of existing classes.  If specified, the new
+    #   class is added to this array.
+    # * <tt>:attributes</tt>: A hash of attributes to set before the block is
+    #   evaluated.
+    # * <tt>:block</tt>: The block to evaluate in the context of the class.
+    #   You can also just pass the block normally, but it will still be evaluated
+    #   with <tt>class_eval</tt>.
+    # * <tt>:constant</tt>: What to set the constant as.  Defaults to the
+    #   capitalized name.
+    # * <tt>:hash</tt>: A hash of existing classes.  If specified, the new
+    #   class is added to this hash, and it is also used for overwrite tests.
+    # * <tt>:overwrite</tt>: Whether to overwrite an existing class.
+    # * <tt>:prefix</tt>: The constant prefix.  Default to nothing; if specified,
+    #   the capitalized name is appended and the result is set as the constant.
+    def genmodule(name, options = {}, &block)
+        genthing(name, Module, options, block)
     end
 
     # Remove an existing class
@@ -127,8 +68,124 @@ module Puppet::Util::ClassGen
 
     private
 
+    # This does the actual work of creating our class or module.  It's just a
+    # slightly abstract version of genclass.
+    def genthing(name, type, options, block)
+        options = symbolize_options(options)
+
+        name = symbolize(name.to_s.downcase)
+
+        if type == Module
+            #evalmethod = :module_eval
+            evalmethod = :class_eval
+            # Create the class, with the correct name.
+            klass = Module.new do
+                class << self
+                    attr_reader :name
+                end
+                @name = name
+            end
+        else 
+            options[:parent] ||= self
+            evalmethod = :class_eval
+            # Create the class, with the correct name.
+            klass = Class.new(options[:parent]) do
+                @name = name
+            end
+        end
+
+        # Create the constant as appropriation.
+        handleclassconst(klass, name, options)
+
+        # Initialize any necessary variables.
+        initclass(klass, options)
+
+        block ||= options[:block]
+
+        # Evaluate the passed block if there is one.  This should usually
+        # define all of the work.
+        if block
+            klass.send(evalmethod, &block)
+        end
+
+        if klass.respond_to? :postinit
+            klass.postinit
+        end
+
+        # Store the class in hashes or arrays or whatever.
+        storeclass(klass, name, options)
+
+        return klass
+    end
+
+    # Handle the setting and/or removing of the associated constant.
+    def handleclassconst(klass, name, options)
+        unless const = options[:constant]
+            prefix = options[:prefix] || ""
+            const = prefix + name2const(name)
+        end
+
+        if const_defined? const
+            if options[:overwrite]
+                Puppet.info "Redefining %s in %s" % [name, self]
+                remove_const(const)
+            else
+                raise Puppet::ConstantAlreadyDefined,
+                    "Class %s is already defined in %s" % [const, self]
+            end
+        end
+        const_set(const, klass)
+
+        return const
+    end
+
+    # Perform the initializations on the class.
+    def initclass(klass, options)
+        if klass.respond_to? :initvars
+            klass.initvars
+        end
+
+        if attrs = options[:attributes]
+            attrs.each do |param, value|
+                method = param.to_s + "="
+                if klass.respond_to? method
+                    klass.send(method, value)
+                end
+            end
+        end
+
+        if klass.respond_to? :preinit
+            klass.preinit
+        end
+    end
+
+    # Convert our name to a constant.
     def name2const(name)
         name.to_s.capitalize
+    end
+
+    # Store the class in the appropriate places.
+    def storeclass(klass, klassname, options)
+        if hash = options[:hash]
+            if hash.include? klassname and ! options[:overwrite]
+                raise Puppet::SubclassAlreadyDefined,
+                    "Already a generated class named %s" % klassname
+            end
+
+            hash[klassname] = klass
+        end
+
+        # If we were told to stick it in a hash, then do so
+        if array = options[:array]
+            if (klass.respond_to? :name and
+                            array.find { |c| c.name == klassname } and
+                            ! options[:overwrite])
+                raise Puppet::SubclassAlreadyDefined,
+                    "Already a generated class named %s" % klassname
+            end
+
+            array << klass
+        end
     end
 end
 
--- test/util/classgen.rb
+++ test/util/classgen.rb
@@ -16,23 +16,127 @@ class TestPuppetUtilClassGen < Test::Unit::TestCase
         end
     end
 
+    def testclasses(name)
+        sub = Class.new(GenTest) do @name = "base#{name.to_s}" end
+        self.class.const_set("Base#{name.to_s}", sub)
+
+        klass = Class.new(FakeBase) do @name = "gen#{name.to_s}"end
+
+        return sub, klass
+    end
+
+    def test_handleclassconst
+        sub, klass = testclasses("const")
+        const = nil
+        assert_nothing_raised do
+            const = sub.send(:handleclassconst, klass, klass.name, {})
+        end
+
+        # make sure the constant is set
+        assert(defined?(Baseconst::Genconst), "const was not defined")
+        assert_equal(Baseconst::Genconst.object_id, klass.object_id)
+
+        # Now make sure don't replace by default
+        newklass = Class.new(FakeBase) do @name = klass.name end
+        assert_raise(Puppet::ConstantAlreadyDefined) do
+            const = sub.send(:handleclassconst, newklass, klass.name, {})
+        end
+        assert_equal(Baseconst::Genconst.object_id, klass.object_id)
+
+        # Now make sure we can replace it
+        assert_nothing_raised do
+            const = sub.send(:handleclassconst, newklass, klass.name, :overwrite => true)
+        end
+        assert_equal(Baseconst::Genconst.object_id, newklass.object_id)
+
+        # Now make sure we can choose our own constant
+        assert_nothing_raised do
+            const = sub.send(:handleclassconst, newklass, klass.name,
+                :constant => "Fooness")
+        end
+        assert(defined?(Baseconst::Fooness), "Specified constant was not defined")
+
+        # And make sure prefixes work
+        assert_nothing_raised do
+            const = sub.send(:handleclassconst, newklass, klass.name,
+                :prefix => "Test")
+        end
+        assert(defined?(Baseconst::TestGenconst), "prefix was not used")
+    end
+
+    def test_initclass_preinit
+        sub, klass = testclasses("preinit")
+
+        class << klass
+            attr_accessor :set
+            def preinit
+                @set = true
+            end
+        end
+
+        assert(!klass.set, "Class was already initialized")
+
+        assert_nothing_raised do sub.send(:initclass, klass, {}) end
+
+        assert(klass.set, "Class was not initialized")
+    end
+
+    def test_initclass_initvars
+        sub, klass = testclasses("initvars")
+
+        class << klass
+            attr_accessor :set
+            def initvars
+                @set = true
+            end
+        end
+
+        assert(!klass.set, "Class was already initialized")
+
+        assert_nothing_raised do sub.send(:initclass, klass, {}) end
+
+        assert(klass.set, "Class was not initialized")
+    end
+
+    def test_initclass_attributes
+        sub, klass = testclasses("attributes")
+
+        class << klass
+            attr_accessor :one, :two, :three
+        end
+
+        assert(!klass.one, "'one' was already set")
+
+        assert_nothing_raised do sub.send(:initclass, klass,
+            :attributes => {:one => :a, :two => :b}) end
+
+        assert_equal(:a, klass.one, "Class was initialized incorrectly")
+        assert_equal(:b, klass.two, "Class was initialized incorrectly")
+        assert_nil(klass.three, "Class was initialized incorrectly")
+    end
+
     def test_genclass
         hash = {}
+        array = []
 
         name = "yayness"
         klass = nil
         assert_nothing_raised {
-            klass = GenTest.genclass(name, :hash => hash, :parent => FakeBase) do
-                class << self
-                    attr_accessor :name
-                end
+            klass = GenTest.genclass(name, :array => array,
+                :hash => hash, :parent => FakeBase) do
+                    class << self
+                        attr_accessor :name
+                    end
             end
         }
 
-        assert(klass.respond_to?(:name), "Class did not execute block")
+        assert(klass.respond_to?(:name=), "Class did not execute block")
 
         assert(hash.include?(klass.name),
             "Class did not get added to hash")
+        assert(array.include?(klass),
+            "Class did not get added to array")
+        assert_equal(klass.superclass, FakeBase, "Parent class was wrong")
     end
 
     # Make sure we call a preinithook, if there is one.
@@ -56,6 +160,31 @@ class TestPuppetUtilClassGen < Test::Unit::TestCase
 
         assert(klass.preinited, "prehook did not get called")
         assert(klass.postinited, "posthook did not get called")
+    end
+
+    def test_modulegen
+        hash = {}
+        array = []
+
+        name = "modness"
+        mod = nil
+        assert_nothing_raised {
+            mod = GenTest.genmodule(name, :array => array, :hash => hash) do
+                class << self
+                    attr_accessor :yaytest
+                end
+
+                @yaytest = true
+            end
+        }
+
+        assert(mod.respond_to?(:yaytest), "Class did not execute block")
+
+        assert_instance_of(Module, mod)
+        assert(hash.include?(mod.name),
+            "Class did not get added to hash")
+        assert(array.include?(mod),
+            "Class did not get added to array")
     end
 end
 
