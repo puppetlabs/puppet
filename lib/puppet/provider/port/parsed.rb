f8b9e866082794e5d7fb0082ebf4df24823b2053
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2006-11-13 07:55:05 +0000

    Fixing a small syntax error in the port provider
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@1874 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/provider/port/parsed.rb
+++ lib/puppet/provider/port/parsed.rb
@@ -25,8 +25,7 @@ Puppet::Type.type(:port).provide(:parsed,
 
     # We have to manually parse the line, since it's so darn complicated.
     record_line :parsed, :fields => %w{name port protocols alias description},
-        :optional => %w{alias description}
-    do |line|
+        :optional => %w{alias description} do |line|
         if line =~ /\/ddp/
             raise "missed ddp in %s" % line
         end
