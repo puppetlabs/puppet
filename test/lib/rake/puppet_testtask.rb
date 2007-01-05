954a285a25c990c48d68aeb46bf866cede0af007
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-01-05 21:55:18 +0000

    Fixing puppet test task for older ruby versions
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2057 980ebf18-57e1-0310-9a29-db15c13687c0

--- test/lib/rake/puppet_testtask.rb
+++ test/lib/rake/puppet_testtask.rb
@@ -3,9 +3,12 @@
 module Rake
     class PuppetTestTask < Rake::TestTask
         def rake_loader
-            file = find_file('rake/puppet_test_loader') or
-                fail "unable to find rake test loader"
-            puts file
+            if Integer(RUBY_VERSION.split(/\./)[2]) < 4
+                file = super
+            else
+                file = find_file('rake/puppet_test_loader') or
+                    fail "unable to find rake test loader"
+            end
             return file
         end
     end
