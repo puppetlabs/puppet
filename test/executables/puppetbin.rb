8d90e5672e1f516310cd98077e8932b79c4970cf
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-01-30 19:18:32 +0000

    Puppet can now read files on stdin.  This means you can put "#!/usr/bin/env puppet" in the first line of a puppet manifest and then execute the manifest normally.  Yay!
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2130 980ebf18-57e1-0310-9a29-db15c13687c0

--- test/executables/puppetbin.rb
+++ test/executables/puppetbin.rb
@@ -64,6 +64,28 @@ class TestPuppetBin < Test::Unit::TestCase
 
         assert(FileTest.exists?(path), "Failed to create config'ed file")
     end
+
+    def test_stdin_execution
+        path = tempfile()
+        manifest = tempfile()
+        env = %x{which env}.chomp
+        if env == ""
+            Puppet.info "cannot find env; cannot test stdin_execution"
+            return
+        end
+        File.open(manifest, "w") do |f|
+            f.puts "#!#{env} puppet
+            file { '#{path}': ensure => file }"
+        end
+        File.chmod(0755, manifest)
+
+        assert_nothing_raised {
+            out = %x{#{manifest} 2>&1}
+        }
+        assert($? == 0, "manifest exited with code %s" % $?.to_i)
+
+        assert(FileTest.exists?(path), "Failed to create config'ed file")
+    end
 end
 
 # $Id$
