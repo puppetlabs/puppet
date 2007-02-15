0b5600a6b49400551b01cec7aa3286d42e45d36c
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-02-15 19:08:12 +0000

    Fixing features to use the new feature location
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2190 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/feature/ldap.rb
+++ lib/puppet/feature/ldap.rb
@@ -1,7 +1,7 @@
 #  Created by Luke Kanies on 2006-12-27.
 #  Copyright (c) 2006. All rights reserved.
 
-require 'puppet/feature'
+require 'puppet/util/feature'
 
 Puppet.features.add(:ldap, :libs => ["ldap"])
 
--- lib/puppet/feature/rails.rb
+++ lib/puppet/feature/rails.rb
@@ -1,7 +1,7 @@
 #  Created by Luke Kanies on 2006-11-07.
 #  Copyright (c) 2006. All rights reserved.
 
-require 'puppet/feature'
+require 'puppet/util/feature'
 
 Puppet.features.add(:rails) do
     begin
