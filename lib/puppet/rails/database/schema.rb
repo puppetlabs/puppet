9fe8905abf99c7683d4ac0ce111f567d83f22b54
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-02-16 23:26:24 +0000

    Changing date to datetime in the database
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2205 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/rails/database/schema.rb
+++ lib/puppet/rails/database/schema.rb
@@ -13,30 +13,30 @@ class Puppet::Rails::Schema
                     t.column :source_file_id, :integer
                     t.column :exported, :boolean
                     t.column :line, :integer
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end
 
                 create_table :source_files do |t| 
                     t.column :filename, :string
                     t.column :path, :string
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end
 
                 create_table :puppet_classes do |t| 
                     t.column :name, :string
                     t.column :host_id, :integer
                     t.column :source_file_id, :integer
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end
 
                 create_table :hosts do |t|
                     t.column :name, :string, :null => false
                     t.column :ip, :string
-                    t.column :last_compile, :date
-                    t.column :last_freshcheck, :date
-                    t.column :last_report, :date
+                    t.column :last_compile, :datetime
+                    t.column :last_freshcheck, :datetime
+                    t.column :last_report, :datetime
                     #Use updated_at to automatically add timestamp on save.
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                     t.column :source_file_id, :integer
                 end
 
@@ -44,27 +44,27 @@ class Puppet::Rails::Schema
                     t.column :name, :string, :null => false
                     t.column :value, :text, :null => false
                     t.column :host_id, :integer, :null => false
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end
 
                 create_table :params do |t|
                     t.column :name, :string, :null => false
                     t.column :value,  :text, :null => false
                     t.column :line, :integer
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                     t.column :resource_id, :integer
                 end
          
                 create_table :tags do |t| 
                     t.column :name, :string
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end 
 
                 create_table :taggings do |t| 
                     t.column :tag_id, :integer
                     t.column :taggable_id, :integer
                     t.column :taggable_type, :string
-                    t.column :updated_at, :date
+                    t.column :updated_at, :datetime
                 end
             end
             $stdout.close
