ca255b932127c59158d42f8af1c0ae5f3d42bc3b
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-05-25 04:49:01 +0000

    fixing the method to check for hostdir writability in the rrdgraph report
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2534 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet/reports/rrdgraph.rb
+++ lib/puppet/reports/rrdgraph.rb
@@ -99,7 +99,7 @@ Puppet::Network::Handler.report.newreport(:rrdgraph) do
     def process(time = nil)
         time ||= Time.now.to_i
 
-        unless File.directory?(hostdir) and File.writeable?(hostdir)
+        unless File.directory?(hostdir) and FileTest.writable?(hostdir)
             # Some hackishness to create the dir with all of the right modes and ownership
             config = Puppet::Util::Config.new
             config.setdefaults(:reports, :hostdir => {:default => hostdir, :owner => Puppet[:user], :mode => 0755, :group => Puppet[:group], :desc => "eh"})
