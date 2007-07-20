d3988cc37ad63258b7c166d5bc333a69938cfde8
luke <luke@980ebf18-57e1-0310-9a29-db15c13687c0>
2007-07-20 19:23:33 +0000

    Updated to version 0.23.1
    git-svn-id: https://reductivelabs.com/svn/puppet/trunk@2724 980ebf18-57e1-0310-9a29-db15c13687c0

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -25,7 +25,7 @@ require 'puppet/util/suidmanager'
 # it's also a place to find top-level commands like 'debug'
 
 module Puppet
-    PUPPETVERSION = '0.23.0'
+    PUPPETVERSION = '0.23.1'
 
     def Puppet.version
         return PUPPETVERSION
