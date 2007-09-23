c40da335123ee839294b37134d1e6361000bf216
Luke Kanies <luke@madstop.com>
2007-09-23 15:39:07 -0500

    Adding a "memory" node terminus, which will be used by 'puppet' and the Cfengine 'module_puppet', since they need to set up the node specially with classes and other weird things.

---
+++ spec/integration/node.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+#
+#  Created by Luke Kanies on 2007-9-23.
+#  Copyright (c) 2007. All rights reserved.
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+require 'puppet/node'
+
+describe Puppet::Node, " when using the memory terminus" do
+    before do
+        @name = "me"
+        @node = Puppet::Node.new(@name)
+        Puppet[:node_terminus] = "memory"
+    end
+
+    it "should find no nodes by default" do
+        Puppet::Node.find(@name).should be_nil
+    end
+
+    it "should be able to find nodes that were previously saved" do
+        @node.save
+        Puppet::Node.find(@name).should equal(@node)
+    end
+
+    it "should replace existing saved nodes when a new node with the same name is saved" do
+        @node.save
+        two = Puppet::Node.new(@name)
+        two.save
+        Puppet::Node.find(@name).should equal(two)
+    end
+
+    it "should be able to remove previously saved nodes" do
+        @node.save
+        Puppet::Node.destroy(@node)
+        Puppet::Node.find(@name).should be_nil
+    end
+
+    it "should fail when asked to destroy a node that does not exist" do
+        proc { Puppet::Node.destroy(@node) }.should raise_error(ArgumentError)
+    end
+
+    after do
+        Puppet.settings.clear
+    end
+end
---
+++ spec/unit/indirector/memory.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+require 'puppet/indirector/memory'
+
+describe "A Memory Terminus", :shared => true do
+    it "should find no instances by default" do
+        @searcher.find(@name).should be_nil
+    end
+
+    it "should be able to find instances that were previously saved" do
+        @searcher.save(@instance)
+        @searcher.find(@name).should equal(@instance)
+    end
+
+    it "should replace existing saved instances when a new instance with the same name is saved" do
+        @searcher.save(@instance)
+        two = stub 'second', :name => @name
+        @searcher.save(two)
+        @searcher.find(@name).should equal(two)
+    end
+
+    it "should be able to remove previously saved instances" do
+        @searcher.save(@instance)
+        @searcher.destroy(@instance)
+        @searcher.find(@name).should be_nil
+    end
+
+    it "should fail when asked to destroy an instance that does not exist" do
+        proc { @searcher.destroy(@instance) }.should raise_error(ArgumentError)
+    end
+end
+
+describe Puppet::Indirector::Memory do
+    it_should_behave_like "A Memory Terminus"
+
+    before do
+        Puppet::Indirector::Terminus.stubs(:register_terminus_class)
+        @model = mock 'model'
+        @indirection = stub 'indirection', :name => :mystuff, :register_terminus_type => nil, :model => @model
+        Puppet::Indirector::Indirection.stubs(:instance).returns(@indirection)
+
+        @memory_class = Class.new(Puppet::Indirector::Memory) do
+            def self.to_s
+                "Testing"
+            end
+        end
+
+        @searcher = @memory_class.new
+        @name = "me"
+        @instance = stub 'instance', :name => @name
+    end
+end
