8722e439b71de005a56820b3e3cf704e3a5b5c8c
David Lutterkort <dlutter@redhat.com>
2007-10-02 15:18:03 -0700

    Use external helper script to talk to yum; should avoid any more trouble with "yum list". Fixes trac #836
    Signed-off-by: David Lutterkort <dlutter@redhat.com>

---
+++ lib/puppet/provider/package/yumhelper.py
@@ -0,0 +1,37 @@
+# Python helper script to query for the packages that have
+# pending updates. Called by the yum package provider
+#
+# (C) 2007 Red Hat Inc.
+# David Lutterkort <dlutter @redhat.com>
+
+import yum
+import sys
+
+OVERRIDE_OPTS = {
+    'debuglevel': 0,
+    'errorlevel': 0,
+    'logfile': '/dev/null'
+}
+
+def pkg_lists():
+    my = yum.YumBase()
+    my.doConfigSetup()
+
+    for k in OVERRIDE_OPTS.keys():
+        if hasattr(my.conf, k):
+            setattr(my.conf, k, OVERRIDE_OPTS[k])
+        else:
+            my.conf.setConfigOption(k, OVERRIDE_OPTS[k])
+
+    my.doTsSetup()
+    my.doRpmDBSetup()
+    return my.doPackageLists('updates')
+
+try:
+    ypl = pkg_lists()
+except IOError, e:
+    print "_err IOError %d %s" % (e.errno, e)
+    sys.exit(1)
+    
+for pkg in ypl.updates:
+    print "_pkg %s %s %s %s %s" % (pkg.name, pkg.epoch, pkg.version, pkg.release, pkg.arch)
