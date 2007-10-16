ce349683b76ab9d21f4d89e2ec818c0755848a1d
Rick Bradley <rick@rickbradley.com>
2007-10-16 15:56:03 -0500

    Make the actual runtime be more robust when mongrel is not installed.

--- lib/puppet/network/http.rb
+++ lib/puppet/network/http.rb
@@ -1,7 +1,10 @@
 class Puppet::Network::HTTP
     def self.server_class_by_type(kind)
         return Puppet::Network::HTTP::WEBrick if kind.to_sym == :webrick
-        return Puppet::Network::HTTP::Mongrel if kind.to_sym == :mongrel
+        if kind.to_sym == :mongrel
+            raise ArgumentError, "Mongrel is not installed on this platform" unless Puppet.features.mongrel?
+            return Puppet::Network::HTTP::Mongrel 
+        end
         raise ArgumentError, "Unknown HTTP server name [#{kind}]"
     end
 end
--- spec/unit/network/http.rb
+++ spec/unit/network/http.rb
@@ -15,7 +15,12 @@ describe Puppet::Network::HTTP do
     it "should return the mongrel HTTP server class when asked for a mongrel server" do
         Puppet::Network::HTTP.server_class_by_type(:mongrel).should be(Puppet::Network::HTTP::Mongrel)
     end
-      
+    
+    it "should fail to return the mongrel HTTP server class if mongrel is not available " do
+        Puppet.features.expects(:mongrel?).returns(false)
+        Proc.new { Puppet::Network::HTTP.server_class_by_type(:mongrel) }.should raise_error(ArgumentError)
+    end
+    
     it "should return an error when asked for an unknown server" do
         Proc.new { Puppet::Network::HTTP.server_class_by_type :foo }.should raise_error(ArgumentError)
     end
