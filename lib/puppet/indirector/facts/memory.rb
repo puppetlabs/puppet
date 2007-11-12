a4e8f1ca2cc6c1257e4dae0f32e24ff71aa94eea
Luke Kanies <luke@madstop.com>
2007-11-12 10:08:17 -0600

    Adding a memory terminus for facts, which is really only used for testing

---
+++ lib/puppet/indirector/facts/memory.rb
@@ -0,0 +1,9 @@
+require 'puppet/node/facts'
+require 'puppet/indirector/memory'
+
+class Puppet::Node::Facts::Memory < Puppet::Indirector::Memory
+    desc "Keep track of facts in memory but nowhere else.  This is used for
+        one-time compiles, such as what the stand-alone ``puppet`` does.
+        To use this terminus, you must load it with the data you want it
+        to contain."
+end
