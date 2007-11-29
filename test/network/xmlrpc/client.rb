a012849e9ca496ccf72cbaf307f220f3891b802e
Jeffrey J McCune <jeff@northstarlabs.net>
2007-11-29 17:15:15 -0500

    Updated tests for http_enable_post_connection_check configuration setting.

--- test/network/xmlrpc/client.rb
+++ test/network/xmlrpc/client.rb
@@ -72,11 +72,6 @@ class TestXMLRPCClient < Test::Unit::TestCase
         http.expects(:cert=).with(:ccert)
         http.expects(:key=).with(:ckey)
         http.expects(:verify_mode=).with(OpenSSL::SSL::VERIFY_PEER)
-        if Puppet[:http_enable_post_connection_check]
-            http.expects(:enable_post_connection_check=).with(true)
-        else
-            http.expects(:enable_post_connection_check=).with(false)
-        end
         http.expects(:cert_store=)
 
         assert_nothing_raised do
