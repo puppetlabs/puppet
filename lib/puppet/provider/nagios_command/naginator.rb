68cde4f1babe3ebf61ffe215c6328faf7818d40f
Luke Kanies <luke@madstop.com>
2007-12-31 14:02:11 -0600

    Removing the one-off naginator provider for nagios_command.

--- lib/puppet/provider/nagios_command/naginator.rb
+++
@@ -1,4 +0,0 @@
-require 'puppet/provider/naginator'
-
-Puppet::Type.type(:nagios_command).provide(:naginator, :parent => Puppet::Provider::Naginator, :default_target => '/tmp/nagios/nagios_command.cfg') do
-end
