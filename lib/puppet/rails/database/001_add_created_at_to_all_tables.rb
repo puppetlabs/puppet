0139889be92add151e624710261ef6f8c0048586
Blake Barnett <bdb@bdb-debvm1.stanford.edu>
2008-02-28 22:51:20 -0800

    * Add migration for "created_at" (hobo expects it) * Tweaks for puppetshow interface cleanup * Delete unused tagging lib and puppet_class model

---
+++ lib/puppet/rails/database/001_add_created_at_to_all_tables.rb
@@ -0,0 +1,17 @@
+class AddCreatedAtToAllTables < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.connection.tables.each do |t|
+      unless ActiveRecord::Base.connection.columns(t).collect {|c| c.name}.include?("created_at")
+        add_column t.to_s, :created_at, :datetime 
+      end  
+    end 
+  end
+
+  def self.down
+    ActiveRecord::Base.connection.tables.each do |t|
+      unless ActiveRecord::Base.connection.columns(t).collect {|c| c.name}.include?("created_at")
+        remove_column t.to_s, :created_at
+      end  
+    end  
+  end
+end
