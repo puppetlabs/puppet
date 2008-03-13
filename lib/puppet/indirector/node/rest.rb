7d51146e98e4135d933be5b2b227a0ca92f06ef2
Rick Bradley <rick@rickbradley.com>
2008-03-12 23:08:13 -0500

    fixing Puppet::Node::REST class name to work with autoloader inflection (Puppet::Node::Rest), so we can do Puppet::Node.terminus_class = :rest

--- lib/puppet/indirector/node/rest.rb
+++ lib/puppet/indirector/node/rest.rb
@@ -1,7 +1,7 @@
 require 'puppet/node'
 require 'puppet/indirector/rest'
 
-class Puppet::Node::REST < Puppet::Indirector::REST
+class Puppet::Node::Rest < Puppet::Indirector::REST
     desc "This will eventually be a REST-based mechanism for finding nodes.  It is currently non-functional."
     # TODO/FIXME
 end
