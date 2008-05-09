da4cdd27573a319e8fa5f89642c98377b8911fa4
James Turnbull <james@lovedthanlost.net>
2008-05-10 09:54:49 +1000

    Fix for ticket #1218 - changed to appropriate variable name

--- lib/puppet/util/settings.rb
+++ lib/puppet/util/settings.rb
@@ -122,7 +122,7 @@ class Puppet::Util::Settings
             if pval = self.value(varname)
                 pval
             else
-                raise Puppet::DevError, "Could not find value for %s" % parent
+                raise Puppet::DevError, "Could not find value for %s" % value
             end
         end
 
