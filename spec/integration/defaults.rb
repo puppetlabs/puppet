5f600ddc04efd1c4faac9f63b3a07af5282be707
Luke Kanies <luke@madstop.com>
2008-06-15 16:33:48 -0500

    Fixing #1168 (for 0.24.x) -- automatically downcasing the fqdn. Also requiring that passed in certnames be downcased; the setting system isn't currently flexible enough to automatically downcase it for the user.

--- spec/integration/defaults.rb
+++ spec/integration/defaults.rb
@@ -5,13 +5,19 @@ require File.dirname(__FILE__) + '/../spec_helper'
 require 'puppet/defaults'
 
 describe "Puppet defaults" do
+    after { Puppet.settings.clear }
+
     describe "when setting the :factpath" do
-        after { Puppet.settings.clear }
 
         it "should add the :factpath to Facter's search paths" do
             Facter.expects(:search).with("/my/fact/path")
 
             Puppet.settings[:factpath] = "/my/fact/path"
+        end
+    end
+    describe "when setting the :certname" do
+        it "should fail if the certname is not downcased" do
+            lambda { Puppet.settings[:certname] = "Host.Domain.Com" }.should raise_error(ArgumentError)
         end
     end
 end
