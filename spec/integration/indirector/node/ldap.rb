38540d56f28487ab55fec1ffd662e593be2997dd
Luke Kanies <luke@madstop.com>
2008-07-02 19:47:12 -0500

    Fixing the ldap node integration test so it cleans up

--- spec/integration/indirector/node/ldap.rb
+++ spec/integration/indirector/node/ldap.rb
@@ -5,17 +5,10 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require 'puppet/indirector/node/ldap'
 
 describe Puppet::Node::Ldap do
-    before do
-        Puppet[:node_terminus] = :ldap
-        Puppet::Node.stubs(:terminus_class).returns :ldap
-    end
-
-    after do
-        Puppet.settings.clear
-    end
-
     it "should use a restrictive filter when searching for nodes in a class" do
-        Puppet::Node.indirection.terminus(:ldap).expects(:ldapsearch).with("(&(objectclass=puppetClient)(puppetclass=foo))")
+        ldap = Puppet::Node.indirection.terminus(:ldap)
+        Puppet::Node.indirection.stubs(:terminus).returns ldap
+        ldap.expects(:ldapsearch).with("(&(objectclass=puppetClient)(puppetclass=foo))")
 
         Puppet::Node.search "eh", :class => "foo"
     end
