686ba4d4c21f6f1e073bd845492f2fe3cb4837a2
James Turnbull <james@lovedthanlost.net>
2008-07-20 16:11:56 +1000

    Revert "Merging fsweetser's selinux patch against 0.24.4"
    This reverts commit f16da4250c16aeab932a81a349df059c69d7ee23.

--- lib/puppet/type/file.rb
+++ lib/puppet/type/file.rb
@@ -1148,5 +1148,4 @@ module Puppet
     require 'puppet/type/file/group'
     require 'puppet/type/file/mode'
     require 'puppet/type/file/type'
-    require 'puppet/type/file/selcontext'  # SELinux file context
 end
