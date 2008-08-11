7accb893d8b4a530aac7ccd402756afb0ce599bd
Brice Figureau <brice-puppet@daysofwonder.com>
2008-08-11 15:58:40 +0200

    id column is autogenerated by rails as a primary key, there is no need to create an additional index on this column. This changeset contains the new schema and a migration.
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

---
+++ lib/puppet/rails/database/002_remove_duplicated_index_on_all_tables.rb
@@ -0,0 +1,17 @@
+class RemoveDuplicatedIndexOnAllTables < ActiveRecord::Migration
+    def self.up
+        ActiveRecord::Base.connection.tables.each do |t|
+            if ActiveRecord::Base.connection.indexes(t).collect {|c| c.columns}.include?("id")
+                remove_index t.to_s, :id
+            end
+        end 
+    end
+
+    def self.down
+        ActiveRecord::Base.connection.tables.each do |t|
+            unless ActiveRecord::Base.connection.indexes(t).collect {|c| c.columns}.include?("id")
+                add_index t.to_s, :id, :integer => true
+            end
+        end    
+    end
+end
