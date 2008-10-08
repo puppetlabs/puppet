307260f5493b54c4d9435dd8e71b30d5c08edb05
James Turnbull <james@lovedthanlost.net>
2008-10-08 13:57:54 +1100

    Remove old selboolean unit tests and fix permissions on new tests

--- spec/unit/other/selinux.rb
+++ spec/unit/other/selinux.rb
@@ -2,37 +2,7 @@
 
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-require 'puppet/type/selboolean'
 require 'puppet/type/selmodule'
-
-describe Puppet.type(:selboolean), " when manipulating booleans" do
-	before :each do
-		@bool = Puppet::Type::Selboolean.create(
-			:name => "foo",
-			:value => "on",
-			:persistent => true )
-	end
-	it "should be able to access :name" do
-		@bool[:name].should == "foo"
-	end
-	it "should be able to access :value" do
-		@bool.property(:value).should == :on
-	end
-	it "should set :value to off" do
-		@bool[:value] = :off
-		@bool.property(:value).should == :off
-	end
-	it "should be able to access :persistent" do
-		@bool[:persistent].should == :true
-	end
-	it "should set :persistent to false" do
-		@bool[:persistent] = false
-		@bool[:persistent].should == :false
-	end
-	after :each do
-		Puppet::Type::Selboolean.clear()
-	end
-end
 
 describe Puppet.type(:selmodule), " when checking policy modules" do
 	before :each do
