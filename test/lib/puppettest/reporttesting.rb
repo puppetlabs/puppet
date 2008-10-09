b08002e45aa051b84e2feaab75dce8fd5016ce39
Luke Kanies <luke@madstop.com>
2008-10-08 22:13:52 -0500

    Fixing some tests that were broken in the fix for #1633
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- test/lib/puppettest/reporttesting.rb
+++ test/lib/puppettest/reporttesting.rb
@@ -6,8 +6,6 @@ module PuppetTest::Reporttesting
         3.times { |i|
             # We have to use warning so that the logs always happen
             log = Puppet.warning("Report test message %s" % i)
-            log.tags = %w{a list of tags}
-            log.tags << "tag%s" % i
 
             report.newlog(log)
         }
