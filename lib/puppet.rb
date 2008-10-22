5ba54d216cbb7d06c19bd343fb546d170e9895a2
Luke Kanies <luke@madstop.com>
2008-10-23 01:19:26 +0200

    Updated to version 0.24.6

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -25,7 +25,7 @@ require 'puppet/util/suidmanager'
 # it's also a place to find top-level commands like 'debug'
 
 module Puppet
-    PUPPETVERSION = '0.24.5'
+    PUPPETVERSION = '0.24.6'
 
     def Puppet.version
         return PUPPETVERSION
