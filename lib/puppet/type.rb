27a750d4884f2f48c05f5ab6a5c70c7cecf5ff04
James Turnbull <james@lovedthanlost.net>
2008-11-22 10:00:55 +1100

    Revert "Fixing #1755 - File modes (and other strange properties) will now display correctly"
    This reverts commit ed4c4050a8ac46c509b20031814646a49ba86e54.

--- lib/puppet/type.rb
+++ lib/puppet/type.rb
@@ -2487,8 +2487,8 @@ class Type
         trans = TransObject.new(self.title, self.class.name)
 
         values = retrieve()
-        values.each do |param, value|
-            trans[param.name] = param.is_to_s(value)
+        values.each do |name, value|
+            trans[name.name] = value
         end
 
         @parameters.each do |name, param|
