68ffd46c9fd87e07d0171e7daa54b951f621c7b7
Andrew Shafer <andrew@reductivelabs.com>
2008-12-08 00:09:50 -0700

    Bug #1803 Zfs should auto require the ancestor file systems

--- spec/unit/type/zfs.rb
+++ spec/unit/type/zfs.rb
@@ -25,4 +25,21 @@ describe zpool do
             zpool.attrclass(parameter).ancestors.should be_include(Puppet::Parameter)
         end
     end
+
+    it "should autorequire the containing zfss and the zpool" do
+            #this is a little funky because the autorequire depends on a property with a feature
+            foo_pool = Puppet.type(:zpool).create(:name => "foo")
+
+            foo_bar_zfs = Puppet.type(:zfs).create(:name => "foo/bar")
+            foo_bar_baz_zfs = Puppet.type(:zfs).create(:name => "foo/bar/baz")
+            foo_bar_baz_buz_zfs = Puppet.type(:zfs).create(:name => "foo/bar/baz/buz")
+
+            config = Puppet::Node::Catalog.new :testing do |conf|
+                [foo_pool, foo_bar_zfs, foo_bar_baz_zfs, foo_bar_baz_buz_zfs].each { |resource| conf.add_resource resource }
+            end
+
+            req = foo_bar_baz_buz_zfs.autorequire.collect { |edge| edge.source.ref }
+
+            [foo_pool.ref, foo_bar_zfs.ref, foo_bar_baz_zfs.ref].each { |ref| req.include?(ref).should == true }
+    end
 end
