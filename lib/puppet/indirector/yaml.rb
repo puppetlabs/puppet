45144a1b9da2839fd9f8a182a8f82ecb06e17292
Luke Kanies <luke@madstop.com>
2008-12-10 23:29:39 -0600

    Fixing #1812 (hopefully) - adding read and write locks to yaml.
    It's obviously not really possible to test that this fixes it,
    but I'm confident that the locks work, and now we're using them,
    so it *should*.
    
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/indirector/yaml.rb
+++ lib/puppet/indirector/yaml.rb
@@ -1,16 +1,25 @@
 require 'puppet/indirector/terminus'
+require 'puppet/util/file_locking'
 
 # The base class for YAML indirection termini.
 class Puppet::Indirector::Yaml < Puppet::Indirector::Terminus
+    include Puppet::Util::FileLocking
+
     # Read a given name's file in and convert it from YAML.
     def find(request)
         file = path(request.key)
         return nil unless FileTest.exist?(file)
 
+        yaml = nil
         begin
-            return from_yaml(File.read(file))
+            readlock(file) { |fh| yaml = fh.read }
         rescue => detail
             raise Puppet::Error, "Could not read YAML data for %s %s: %s" % [indirection.name, request.key, detail]
+        end
+        begin
+            return from_yaml(yaml)
+        rescue => detail
+            raise Puppet::Error, "Could not parse YAML data for %s %s: %s" % [indirection.name, request.key, detail]
         end
     end
 
@@ -28,7 +37,7 @@ class Puppet::Indirector::Yaml < Puppet::Indirector::Terminus
         end
 
         begin
-            File.open(file, "w", 0660) { |f| f.print to_yaml(request.instance) }
+            writelock(file, 0660) { |f| f.print to_yaml(request.instance) }
         rescue TypeError => detail
             Puppet.err "Could not save %s %s: %s" % [self.name, request.key, detail]
         end
@@ -36,7 +45,7 @@ class Puppet::Indirector::Yaml < Puppet::Indirector::Terminus
 
     # Get the yaml directory
     def base
-      (Puppet[:name] == "puppetmasterd") ? Puppet[:yamldir] : Puppet[:clientyamldir]
+        (Puppet[:name] == "puppetmasterd") ? Puppet[:yamldir] : Puppet[:clientyamldir]
     end
 
     # Return the path to a given node's file.
