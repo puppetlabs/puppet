6b4e5f49a8d1d062aefae31a923cff9e3f0d31ba
Luke Kanies <luke@madstop.com>
2009-01-22 16:29:33 -0600

    Reformatting tests for facts
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- spec/unit/node/facts.rb
+++ spec/unit/node/facts.rb
@@ -4,37 +4,39 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 require 'puppet/node/facts'
 
-describe Puppet::Node::Facts, " when indirecting" do
-    before do
-        @indirection = stub 'indirection', :request => mock('request'), :name => :facts
+describe Puppet::Node::Facts, "when indirecting" do
+    describe "when indirecting" do
+        before do
+            @indirection = stub 'indirection', :request => mock('request'), :name => :facts
 
-        # We have to clear the cache so that the facts ask for our indirection stub,
-        # instead of anything that might be cached.
-        Puppet::Util::Cacher.expire
+            # We have to clear the cache so that the facts ask for our indirection stub,
+            # instead of anything that might be cached.
+            Puppet::Util::Cacher.expire
 
-        @facts = Puppet::Node::Facts.new("me", "one" => "two")
+            @facts = Puppet::Node::Facts.new("me", "one" => "two")
+        end
+
+        it "should redirect to the specified fact store for retrieval" do
+            Puppet::Node::Facts.stubs(:indirection).returns(@indirection)
+            @indirection.expects(:find)
+            Puppet::Node::Facts.find(:my_facts)
+        end
+
+        it "should redirect to the specified fact store for storage" do
+            Puppet::Node::Facts.stubs(:indirection).returns(@indirection)
+            @indirection.expects(:save)
+            @facts.save
+        end
+
+        it "should default to the 'facter' terminus" do
+            Puppet::Node::Facts.indirection.terminus_class.should == :facter
+        end
     end
 
-    it "should redirect to the specified fact store for retrieval" do
-        Puppet::Node::Facts.stubs(:indirection).returns(@indirection)
-        @indirection.expects(:find)
-        Puppet::Node::Facts.find(:my_facts)
-    end
-
-    it "should redirect to the specified fact store for storage" do
-        Puppet::Node::Facts.stubs(:indirection).returns(@indirection)
-        @indirection.expects(:save)
-        @facts.save
-    end
-
-    it "should default to the 'facter' terminus" do
-        Puppet::Node::Facts.indirection.terminus_class.should == :facter
-    end
-end
-
-describe Puppet::Node::Facts, " when storing and retrieving" do
-    it "should add metadata to the facts" do
-        facts = Puppet::Node::Facts.new("me", "one" => "two", "three" => "four")
-        facts.values[:_timestamp].should be_instance_of(Time)
+    describe "when storing and retrieving" do
+        it "should add metadata to the facts" do
+            facts = Puppet::Node::Facts.new("me", "one" => "two", "three" => "four")
+            facts.values[:_timestamp].should be_instance_of(Time)
+        end
     end
 end
