4b5ec82d32babdf3ccc935748eb80bca30b379ff
Luke Kanies <luke@madstop.com>
2009-02-17 17:24:25 -0600

    reformatting the environment tests
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- spec/unit/node/environment.rb
+++ spec/unit/node/environment.rb
@@ -17,21 +17,21 @@ describe Puppet::Node::Environment do
     it "should treat an environment specified as names or strings as equivalent" do
         Puppet::Node::Environment.new(:one).should equal(Puppet::Node::Environment.new("one"))
     end
-end
 
-describe Puppet::Node::Environment, " when modeling a specific environment" do
-    it "should have a method for returning the environment name" do
-        Puppet::Node::Environment.new("testing").name.should == :testing
-    end
+    describe "when modeling a specific environment" do
+        it "should have a method for returning the environment name" do
+            Puppet::Node::Environment.new("testing").name.should == :testing
+        end
 
-    it "should provide an array-like accessor method for returning any environment-specific setting" do
-        env = Puppet::Node::Environment.new("testing")
-        env.should respond_to(:[])
-    end
+        it "should provide an array-like accessor method for returning any environment-specific setting" do
+            env = Puppet::Node::Environment.new("testing")
+            env.should respond_to(:[])
+        end
 
-    it "should ask the Puppet settings instance for the setting qualified with the environment name" do
-        Puppet.settings.expects(:value).with("myvar", :testing).returns("myval")
-        env = Puppet::Node::Environment.new("testing")
-        env["myvar"].should == "myval"
+        it "should ask the Puppet settings instance for the setting qualified with the environment name" do
+            Puppet.settings.expects(:value).with("myvar", :testing).returns("myval")
+            env = Puppet::Node::Environment.new("testing")
+            env["myvar"].should == "myval"
+        end
     end
 end
