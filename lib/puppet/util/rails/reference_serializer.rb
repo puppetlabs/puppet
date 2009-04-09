7a91e1f5c03ed02eed4c45fddd740b899650bf36
Luke Kanies <luke@madstop.com>
2009-04-09 17:43:17 -0500

    Changing rails value serialization to deal with booleans
    The database was automatically converting booleans
    to strings, and value comparison was not working correctly
    as a result.
    
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/util/rails/reference_serializer.rb
+++ lib/puppet/util/rails/reference_serializer.rb
@@ -1,16 +1,30 @@
 
 module Puppet::Util::ReferenceSerializer
     def unserialize_value(val)
-        if val =~ /^--- [!:]/
+        case val
+        when /^--- /
             YAML.load(val)
+        when "true"
+            true
+        when "false"
+            false
         else
             val
         end
     end
 
     def serialize_value(val)
-        if val.is_a?(Puppet::Parser::Resource::Reference)
+        case val
+        when Puppet::Parser::Resource::Reference
             YAML.dump(val)
+        when true, false
+            # The database does this for us, but I prefer the
+            # methods be their exact inverses.
+            # Note that this means quoted booleans get returned
+            # as actual booleans, but there doesn't appear to be
+            # a way to fix that while keeping the ability to
+            # search for parameters set to true.
+            val.to_s
         else
             val
         end
