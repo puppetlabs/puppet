22ae661263b9b42c3ad4d4d6dee3d3b1d1d2e25b
Luke Kanies <luke@madstop.com>
2009-04-10 15:23:31 -0500

    Adding "rubygems" and "stomp" features
    Signed-off-by: Luke Kanies <luke@madstop.com>

---
+++ lib/puppet/feature/rubygems.rb
@@ -0,0 +1,6 @@
+#  Created by Luke Kanies on 2006-11-07.
+#  Copyright (c) 2006. All rights reserved.
+
+require 'puppet/util/feature'
+
+Puppet.features.add(:rubygems, :libs => "rubygems")
---
+++ lib/puppet/feature/stomp.rb
@@ -0,0 +1,6 @@
+require 'puppet/util/feature'
+
+# We want this to load if possible, but it's not automatically
+# required.
+Puppet.features.rubygems?
+Puppet.features.add(:stomp, :libs => %{stomp})
