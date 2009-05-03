a6af5bf8aa45af05186088b02e68acff41f404a6
James Turnbull <james@lovedthanlost.net>
2009-05-03 19:25:08 +1000

    Added split function

---
+++ lib/puppet/parser/functions/split.rb
@@ -0,0 +1,11 @@
+module Puppet::Parser::Functions
+  newfunction(:split, :type => :rvalue, 
+      :doc => "Split a string variable into an array using the specified split character.
+Usage::
+    $string    = 'value1,value2'
+    $array_var = split($string, ',')
+
+$array_var holds the result ['value1', 'value2']") do |args|
+    return args[0].split(args[1])
+  end
+end
