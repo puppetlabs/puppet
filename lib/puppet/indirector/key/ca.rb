42247f0f5d08bce4d3fd2b020b770a12cc2f91db
Luke Kanies <luke@madstop.com>
2009-06-01 21:31:49 -0500

    Fixing #2315 - ca --generate works again
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/indirector/key/ca.rb
+++ lib/puppet/indirector/key/ca.rb
@@ -6,15 +6,7 @@ class Puppet::SSL::Key::Ca < Puppet::Indirector::SslFile
         with the CA key, because that's the only key that the CA ever interacts
         with."
 
-    # This is just to pass the validation in the base class.  Eh.
-    store_at :cakey
+    store_in :privatekeydir
 
     store_ca_at :cakey
-
-    def path(name)
-        unless ca?(name)
-            raise ArgumentError, "The :ca terminus can only handle the CA private key"
-        end
-        super
-    end
 end
--- spec/unit/indirector/key/ca.rb
+++ spec/unit/indirector/key/ca.rb
@@ -12,6 +12,11 @@ describe Puppet::SSL::Key::Ca do
         Puppet::SSL::Key::Ca.doc.should be_instance_of(String)
     end
 
+    it "should use the :privatekeydir as the collection directory" do
+        Puppet.settings.expects(:value).with(:privatekeydir).returns "/key/dir"
+        Puppet::SSL::Key::Ca.collection_directory.should == "/key/dir"
+    end
+
     it "should store the ca key at the :cakey location" do
         Puppet.settings.stubs(:use)
         Puppet.settings.stubs(:value).returns "whatever"
@@ -19,16 +24,5 @@ describe Puppet::SSL::Key::Ca do
         file = Puppet::SSL::Key::Ca.new
         file.stubs(:ca?).returns true
         file.path("whatever").should == "/ca/key"
-    end
-
-    describe "when choosing the path for the public key" do
-        it "should fail if the key is not for the CA" do
-            Puppet.settings.stubs(:use)
-            Puppet.settings.stubs(:value).returns "whatever"
-            Puppet.settings.stubs(:value).with(:cakey).returns "/ca/key"
-            file = Puppet::SSL::Key::Ca.new
-            file.stubs(:ca?).returns false
-            lambda { file.path("whatever") }.should raise_error(ArgumentError)
-        end
     end
 end
