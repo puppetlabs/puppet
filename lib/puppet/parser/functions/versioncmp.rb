48d5e8c61cdd0e5e777bbac3adb71d06c109f0da
Brice Figureau <brice-puppet@daysofwonder.com>
2009-06-04 20:48:13 +0200

    Enhance versioncmp documentation
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

--- lib/puppet/parser/functions/versioncmp.rb
+++ lib/puppet/parser/functions/versioncmp.rb
@@ -1,6 +1,27 @@
 require 'puppet/util/package'
 
-Puppet::Parser::Functions::newfunction(:versioncmp, :type => :rvalue, :doc => "Compares two versions.") do |args|
+Puppet::Parser::Functions::newfunction(:versioncmp, :type => :rvalue, 
+    :doc => "Compares two versions
+
+Prototype::
+
+    \$result = versioncmp(a, b)
+
+    where a and b are arbitrary version strings
+
+This functions returns a number::
+
+    * > 0 if version a is greater than version b
+    * == 0 if both version are equals
+    * < 0 if version a is less than version b
+
+Example::
+
+    if versioncmp('2.6-1', '2.4.5') > 0 {
+        notify('2.6-1 is > than 2.4.5')
+    }
+
+") do |args|
 
     unless args.length == 2
         raise Puppet::ParseError, "versioncmp should have 2 arguments"
