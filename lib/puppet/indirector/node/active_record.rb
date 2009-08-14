7e0924737d817644402b1c3d2d5abb2cf006e76b
Luke Kanies <luke@madstop.com>
2009-08-14 13:52:03 -0700

    Fixing fact-missing problem when puppet.conf is reparsed
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/indirector/node/active_record.rb
+++ lib/puppet/indirector/node/active_record.rb
@@ -4,4 +4,10 @@ require 'puppet/node'
 
 class Puppet::Node::ActiveRecord < Puppet::Indirector::ActiveRecord
     use_ar_model Puppet::Rails::Host
+
+    def find(request)
+        node = super
+        node.fact_merge
+        node
+    end
 end
--- spec/unit/indirector/node/active_record.rb
+++ spec/unit/indirector/node/active_record.rb
@@ -17,4 +17,18 @@ describe "Puppet::Node::ActiveRecord" do
     it "should use Puppet::Rails::Host as its ActiveRecord model" do
         Puppet::Node::ActiveRecord.ar_model.should equal(Puppet::Rails::Host)
     end
+
+    it "should call fact_merge when a node is found" do
+        db_instance = stub 'db_instance'
+        Puppet::Node::ActiveRecord.ar_model.expects(:find_by_name).returns db_instance
+
+        node = Puppet::Node.new("foo")
+        db_instance.expects(:to_puppet).returns node
+        ar = Puppet::Node::ActiveRecord.new
+
+        node.expects(:fact_merge)
+
+        request = Puppet::Indirector::Request.new(:node, :find, "what.ever")
+        ar.find(request)
+    end
 end
