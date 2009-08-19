b0f219ad177c8e9c261578586f0324cef0c71cd2
Luke Kanies <luke@madstop.com>
2009-08-19 11:20:14 -0700

    Removing chuser on darwin restriction
    For too long we have refused to change users
    on Darwin because a long time ago the ruby they
    shipped with was really, really broken.
    
    It's been fixed for a while, so this just removes
    the restriction.
    
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/util.rb
+++ lib/puppet/util.rb
@@ -26,10 +26,6 @@ module Util
 
     # Change the process to a different user
     def self.chuser
-        if Facter["operatingsystem"].value == "Darwin"
-            $stderr.puts "Ruby on darwin is broken; puppetmaster will not set its UID to 'puppet' and must run as root"
-            return
-        end
         if group = Puppet[:group]
             group = self.gid(group)
             unless group
