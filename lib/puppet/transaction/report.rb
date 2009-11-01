5a8b460001337ac2971c9b8b9b61f600a86890bf
Luke Kanies <luke@madstop.com>
2009-11-01 14:57:18 -0500

    Removing unused code and adding a couple of tests
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/transaction/report.rb
+++ lib/puppet/transaction/report.rb
@@ -26,11 +26,6 @@ class Puppet::Transaction::Report
     def initialize
         @metrics = {}
         @logs = []
-
-        @records = Hash.new do |hash, key|
-            hash[key] = []
-        end
-
         @host = Puppet[:certname]
     end
 
@@ -52,10 +47,6 @@ class Puppet::Transaction::Report
     # Add a new log message.
     def newlog(msg)
         @logs << msg
-    end
-
-    def record(metric, object)
-        @records[metric] << object
     end
 
     # Provide a summary of this report.
--- spec/unit/transaction/report.rb
+++ spec/unit/transaction/report.rb
@@ -1,41 +1,61 @@
 #!/usr/bin/env ruby
-#
-#  Created by Luke Kanies on 2007-10-12.
-#  Copyright (c) 2007. All rights reserved.
 
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 require 'puppet/transaction/report'
 
-describe Puppet::Transaction::Report, " when being indirect" do
-    it "should redirect :find to the indirection" do
-        @indirection = stub 'indirection', :name => :report
-        Puppet::Transaction::Report.stubs(:indirection).returns(@indirection)
-        @indirection.expects(:find)
-        Puppet::Transaction::Report.find(:report)
+describe Puppet::Transaction::Report do
+    it "should set its host name to the certname" do
+        Puppet.settings.expects(:value).with(:certname).returns "myhost"
+        Puppet::Transaction::Report.new.host.should == "myhost"
     end
 
-    it "should redirect :save to the indirection" do
-        Facter.stubs(:value).returns("eh")
-        @indirection = stub 'indirection', :name => :report
-        Puppet::Transaction::Report.stubs(:indirection).returns(@indirection)
-        report = Puppet::Transaction::Report.new
-        @indirection.expects(:save)
-        report.save
+    describe "when accepting logs" do
+        before do
+            @report = Puppet::Transaction::Report.new
+        end
+
+        it "should add new logs to the log list" do
+            @report << "log"
+            @report.logs[-1].should == "log"
+        end
+
+        it "should return self" do
+            r = @report << "log"
+            r.should equal(@report)
+        end
     end
 
-    it "should default to the 'processor' terminus" do
-        Puppet::Transaction::Report.indirection.terminus_class.should == :processor
-    end
+    describe "when using the indirector" do
+        it "should redirect :find to the indirection" do
+            @indirection = stub 'indirection', :name => :report
+            Puppet::Transaction::Report.stubs(:indirection).returns(@indirection)
+            @indirection.expects(:find)
+            Puppet::Transaction::Report.find(:report)
+        end
 
-    it "should delegate its name attribute to its host method" do
-        report = Puppet::Transaction::Report.new
-        report.expects(:host).returns "me"
-        report.name.should == "me"
-    end
+        it "should redirect :save to the indirection" do
+            Facter.stubs(:value).returns("eh")
+            @indirection = stub 'indirection', :name => :report
+            Puppet::Transaction::Report.stubs(:indirection).returns(@indirection)
+            report = Puppet::Transaction::Report.new
+            @indirection.expects(:save)
+            report.save
+        end
 
-    after do
-        Puppet::Util::Cacher.expire
+        it "should default to the 'processor' terminus" do
+            Puppet::Transaction::Report.indirection.terminus_class.should == :processor
+        end
+
+        it "should delegate its name attribute to its host method" do
+            report = Puppet::Transaction::Report.new
+            report.expects(:host).returns "me"
+            report.name.should == "me"
+        end
+
+        after do
+            Puppet::Util::Cacher.expire
+        end
     end
 end
 
