106c9a91929f00a852f7027088cee6684f83ab63
Jesse Wolfe <jes5199@gmail.com>
2009-11-17 10:17:21 -0800

    Fixing 904 RDoc::usage incompatible with rubygems
    Monkey-patch workaround for RDoc so it doesn't get confused by rubygems
    stub executables.
    
    Signed-off-by: Jesse Wolfe <jes5199@gmail.com>

---
+++ lib/puppet/util/monkey_patches.rb
@@ -0,0 +1,8 @@
+module RDoc 
+    def self.caller(skip=nil)
+        in_gem_wrapper = false
+        Kernel.caller.reject { |call|
+            in_gem_wrapper ||= call =~ /#{Regexp.escape $0}:\d+:in `load'/
+        }
+    end
+end
