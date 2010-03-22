88d6cd5368668b0cf203f6363032adc8d166e876
Jesse Wolfe <jes5199@gmail.com>
2010-03-22 13:53:50 -0700

    Bug #3401 Spec failed due to missing manditory setting in mock
    Puppet::Util::Settings#use now requires the :noop setting to exist, and
    this test was not providing one in its mocked default structure.

--- spec/integration/util/settings.rb
+++ spec/integration/util/settings.rb
@@ -7,9 +7,13 @@ require 'puppet_spec/files'
 describe Puppet::Util::Settings do
     include PuppetSpec::Files
 
+    def minimal_default_settings
+        { :noop => {:default => false, :desc => "noop"} }
+    end
+
     it "should be able to make needed directories" do
         settings = Puppet::Util::Settings.new
-        settings.setdefaults :main, :maindir => [tmpfile("main"), "a"]
+        settings.setdefaults :main, minimal_default_settings.update( :maindir => [tmpfile("main"), "a"] )
 
         settings.use(:main)
 
@@ -18,7 +22,7 @@ describe Puppet::Util::Settings do
 
     it "should make its directories with the corret modes" do
         settings = Puppet::Util::Settings.new
-        settings.setdefaults :main, :maindir => {:default => tmpfile("main"), :desc => "a", :mode => 0750}
+        settings.setdefaults :main,  minimal_default_settings.update( :maindir => {:default => tmpfile("main"), :desc => "a", :mode => 0750} )
 
         settings.use(:main)
 
