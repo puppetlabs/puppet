0eea2f5d67efc6eebe89c4a97043150dbee0dcc6
James Turnbull <james@lovedthanlost.net>
2010-04-01 16:40:55 +1100

    Updated version and CHANGELOG to 0.25.5rc1

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -24,7 +24,7 @@ require 'puppet/util/suidmanager'
 # it's also a place to find top-level commands like 'debug'
 
 module Puppet
-    PUPPETVERSION = '0.25.4'
+    PUPPETVERSION = '0.25.5'
 
     def Puppet.version
         return PUPPETVERSION
