069bf1b4a9cd9de6cea0415754e078d66641ae44
James Turnbull <james@lovedthanlost.net>
2010-06-22 15:51:08 +1000

    Fixed require warning documentation

--- lib/puppet/parser/functions/require.rb
+++ lib/puppet/parser/functions/require.rb
@@ -3,13 +3,12 @@ Puppet::Parser::Functions::newfunction(:require,
         :doc =>"Evaluate one or more classes,  adding the required class as a dependency.
 
 The relationship metaparameters work well for specifying relationships
-between individual resources, but they can be clumsy for specifying 
+between individual resources, but they can be clumsy for specifying
 relationships between classes.  This function is a superset of the
 'include' function, adding a class relationship so that the requiring
 class depends on the required class.
 
-.. Warning::
-  using require in place of include can lead to unwanted dependency cycles.
+Warning: using require in place of include can lead to unwanted dependency cycles.
   For instance the following manifest, with 'require' instead of 'include'
   would produce a nasty dependence cycle, because notify imposes a before
   between File[/foo] and Service[foo]::
