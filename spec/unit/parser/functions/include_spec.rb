9778f2a47922a66e59d571c1c98552223a817ce1
Nick Lewis <nick@puppetlabs.com>
2010-07-23 17:04:22 -0700

    [#4242] Fixed recursion due to parents including their children
    Resources mark themselves as evaluated to prevent being evaluated
    again. Unfortunately, they were not marking themselves until after
    they had finished being completely evaluated. Thus, there was
    nothing actually stopping recursive evaluations. This patch just
    makes resources mark themselves as evaluated when they start
    evaluating, and adds tests.
    
    The original setting of evaluated was done in an ensure block, so
    this doesn't change the behavior of a resource which fails to
    evaluate. The only places evaluated? is checked aren't affected
    by this change, as they wouldn't want to evaluate it when it's
    already being evaluated anyway.

---
+++ spec/unit/parser/functions/include_spec.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "the 'include' function" do
+
+  before :each do
+    Puppet::Node::Environment.stubs(:current).returns(nil)
+    @compiler = Puppet::Parser::Compiler.new(Puppet::Node.new("foo"))
+    @scope = Puppet::Parser::Scope.new(:compiler => @compiler)
+  end
+
+  it "should exist" do
+    Puppet::Parser::Functions.function("include").should == "function_include"
+  end
+
+  it "should include a single class" do
+    inc = "foo"
+    @compiler.expects(:evaluate_classes).with {|klasses,parser,lazy| klasses == [inc]}.returns([inc])
+    @scope.function_include("foo")
+  end
+
+  it "should include multiple classes" do
+    inc = ["foo","bar"]
+    @compiler.expects(:evaluate_classes).with {|klasses,parser,lazy| klasses == inc}.returns(inc)
+    @scope.function_include(["foo","bar"])
+  end
+
+  it "should not lazily evaluate the included class" do
+    @compiler.expects(:evaluate_classes).with {|klasses,parser,lazy| lazy == false}.returns("foo")
+    @scope.function_include("foo")
+  end
+
+  it "should allow a parent to include its child" do
+    @parent_type = Puppet::Resource::Type.new(:hostclass, "parent")
+    @parent_resource = Puppet::Parser::Resource.new(:hostclass, "parent", :scope => @scope)
+    @subscope = @parent_type.subscope(@scope,@parent_resource)
+    @scope.environment.known_resource_types.stubs(:find_hostclass).with{|nses,name| name.downcase == "parent"}.returns(@parent_type)
+
+    @type = Puppet::Resource::Type.new(:hostclass, "foo")
+    @type.stubs(:parent_scope).returns(@subscope)
+    @type.parent = "parent"
+    @resource = Puppet::Parser::Resource.new(:hostclass, "foo", :scope => @subscope)
+    @resource.stubs(:resource_type).returns(@type)
+    @scope.environment.known_resource_types.stubs(:find_hostclass).with{|nses,name| name.downcase == "foo"}.returns(@parent_type)
+    Puppet::Resource.stubs(:new).returns(@resource)
+    Puppet::Parser::Resource.stubs(:new).returns(@resource)
+    lambda { @subscope.function_include("foo") }.should_not raise_error
+  end
+end
