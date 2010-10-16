f57425da6aeefd4ff019068c5933add0d2a02f87
Brice Figureau <brice-puppet@daysofwonder.com>
2010-10-16 20:04:57 +0200

    Fix #4921 - race condition in Parser Functions creation
    The autoloading is not thread safe, which means two threads could both
    autoload the same function at the same time.
    
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

---
+++ spec/integration/parser/functions_spec.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Puppet::Parser::Functions do
+  before :each do
+    Puppet::Parser::Functions.rmfunction("template") if Puppet::Parser::Functions.function("template")
+  end
+
+  it "should support multiple threads autoloading the same function" do
+    threads = []
+    lambda {
+      10.times { |a|
+        threads << Thread.new {
+          Puppet::Parser::Functions.function("template")
+        }
+      }
+    }.should_not raise_error
+    threads.each { |t| t.join }
+  end
+end
