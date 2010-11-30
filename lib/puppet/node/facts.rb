7de6af87109062a4c0b038f2f2d93191e6a93e4f
Paul Berry <paul@puppetlabs.com>
2010-11-30 11:59:45 -0800

    Maint: Add a default value for key in Facts::NodeExpirer#save
    Changed to match the signature of the method being overridden.  This
    will allow code to call Facts.indirection.save() without a key.

--- lib/puppet/node/facts.rb
+++ lib/puppet/node/facts.rb
@@ -15,7 +15,7 @@ class Puppet::Node::Facts
 
   # We want to expire any cached nodes if the facts are saved.
   module NodeExpirer
-    def save(instance, key)
+    def save(instance, key = nil)
       Puppet::Node.indirection.expire(instance.name)
       super
     end
