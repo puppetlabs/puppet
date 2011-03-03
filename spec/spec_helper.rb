f490526726a5f35437cda11652ae9977d89279e5
Daniel Pittman <daniel@rimspace.net>
2011-03-03 10:35:27 -0800

    (#6582) move more helper code into methods, out of RSpec#configure
    We move the tempfile cleanup support off into the module that uses it, which
    removes some of the dependency on magic globals from configure.  It still
    exists, but is hidden in the same module that uses it, which helps.
    
    Reviewed-By: Nick Lewis <nick@puppetlabs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,26 +55,7 @@ RSpec.configure do |config|
     Puppet::Node::Environment.clear
     Puppet::Util::Storage.clear
 
-    if defined?($tmpfiles)
-      $tmpfiles.each do |file|
-        file = File.expand_path(file)
-        if Puppet.features.posix? and file !~ /^\/tmp/ and file !~ /^\/var\/folders/
-          puts "Not deleting tmpfile #{file} outside of /tmp or /var/folders"
-          next
-        elsif Puppet.features.microsoft_windows?
-          tempdir = File.expand_path(File.join(Dir::LOCAL_APPDATA, "Temp"))
-          if file !~ /^#{tempdir}/
-            puts "Not deleting tmpfile #{file} outside of #{tempdir}"
-            next
-          end
-        end
-        if FileTest.exist?(file)
-          system("chmod -R 755 '#{file}'")
-          system("rm -rf '#{file}'")
-        end
-      end
-      $tmpfiles.clear
-    end
+    PuppetSpec::Files.cleanup
 
     @logs.clear
     Puppet::Util::Log.close_all
