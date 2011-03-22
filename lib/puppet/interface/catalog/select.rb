50ba62d68ef3a6a81e00581dd7d74f6387bd73fc
Nick Lewis <nick@puppetlabs.com>
2011-03-22 14:29:52 -0700

    maint: Make args to Catalog.select explicit
    No other args are used, so passing *args and shifting is unnecessary.

--- lib/puppet/interface/catalog/select.rb
+++ lib/puppet/interface/catalog/select.rb
@@ -1,9 +1,7 @@
 # Select and show a list of resources of a given type.
 Puppet::Interface.interface(:catalog) do
   action :select do
-    invoke do |*args|
-      host = args.shift
-      type = args.shift
+    invoke do |host,type|
       catalog = Puppet::Resource::Catalog.indirection.find(host)
 
       catalog.resources.reject { |res| res.type != type }.each { |res| puts res }
