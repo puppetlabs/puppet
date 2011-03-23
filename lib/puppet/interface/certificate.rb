a1ce253d5896c6923165b5c00edef87003b16d7b
Luke Kanies <luke@puppetlabs.com>
2011-03-22 19:17:57 -0700

    Adding Certficate#generate
    Signed-off-by: Luke Kanies <luke@puppetlabs.com>

--- lib/puppet/interface/certificate.rb
+++ lib/puppet/interface/certificate.rb
@@ -1,6 +1,15 @@
 require 'puppet/interface/indirector'
 
 Puppet::Interface::Indirector.interface(:certificate) do
+  action :generate do
+    invoke do |name|
+      require 'puppet/ssl/host'
+
+      host = Puppet::SSL::Host.new(name)
+      host.generate
+    end
+  end
+
   action :sign do |name|
     invoke do |name|
       unless Puppet::SSL::Host.ca_location
@@ -9,8 +18,9 @@ Puppet::Interface::Indirector.interface(:certificate) do
 
       location = Puppet::SSL::Host.ca_location
       if location == :local && !Puppet::SSL::CertificateAuthority.ca?
-        Puppet::Application[:certificate].class.run_mode("master")
-        set_run_mode Puppet::Application[:certificate].class.run_mode
+        app = Puppet::Application[:certificate]
+        app.class.run_mode("master")
+        app.set_run_mode Puppet::Application[:certificate].class.run_mode
       end
 
       Puppet::SSL::Host.indirection.save(Puppet::SSL::Host.new(name))
