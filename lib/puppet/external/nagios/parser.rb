8c32db76f68148b2e58ba744b965a3c8cd8f1663
Matt Robinson <matt@puppetlabs.com>
2011-03-22 22:56:46 -0700

    (#6820) Fix nagios parser to use proper hash syntax for Ruby 1.9
    Reviewed-by: Jacob Helwig <jacob@puppetlabs.com>

--- lib/puppet/external/nagios/parser.rb
+++ lib/puppet/external/nagios/parser.rb
@@ -753,7 +753,7 @@ module_eval <<'.,.,', 'grammar.ry', 40
 
 module_eval <<'.,.,', 'grammar.ry', 42
   def _reduce_10( val, _values, result )
-result = {val[0],val[1]}
+result = {val[0] => val[1]}
   result
 end
 .,.,
