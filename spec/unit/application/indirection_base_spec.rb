56ba0a25753c7d6a736efa0c58fa0bb664e219c2
Daniel Pittman <daniel@puppetlabs.com>
2011-03-23 17:52:52 -0700

    MAINT: implement a pending test for code we wrote...
    We had an outstanding pending test for code we wrote, and which we were not
    actually testing stand-alone.  This implements the test for that.
    
    Reviewed-By: Pieter van de Bruggen <pieter@puppetlabs.com>

--- spec/unit/application/indirection_base_spec.rb
+++ spec/unit/application/indirection_base_spec.rb
@@ -4,7 +4,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
 require 'puppet/application/indirection_base'
 
 describe Puppet::Application::IndirectionBase do
-  it "should support a 'from' terminus"
+  it "should support a 'terminus' accessor" do
+    test = subject
+    expect { test.terminus = :foo }.should_not raise_error
+    test.terminus.should == :foo
+  end
+
+  it "should have a 'terminus' CLI option" do
+    subject.class.option_parser_commands.select do |options, function|
+      options.index { |o| o =~ /terminus/ }
+    end.should_not be_empty
+  end
 
   describe "setup" do
     it "should fail if its string does not support an indirection"
