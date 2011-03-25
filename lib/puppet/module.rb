29268f3fda3c6e29f60d05e5aa387ff0424e7b1e
Luke Kanies <luke@puppetlabs.com>
2011-03-25 12:39:13 -0700

    Fixing Module#path detection
    For some reason FileTest.exist? was returning false,
    and FileTest.directory? returns true.
    
    I've also added much better tests for this behavior.
    
    Signed-off-by: Luke Kanies <luke@puppetlabs.com>
    Reviewed-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/module.rb
+++ lib/puppet/module.rb
@@ -138,7 +138,7 @@ class Puppet::Module
 
   # Find this module in the modulepath.
   def path
-    environment.modulepath.collect { |path| File.join(path, name) }.find { |d| FileTest.exist?(d) }
+    environment.modulepath.collect { |path| File.join(path, name) }.find { |d| FileTest.directory?(d) }
   end
 
   # Find all plugin directories.  This is used by the Plugins fileserving mount.
