a1a09b086dc19ad5257599f24ef8d92bfcabc11e
Daniel Pittman <daniel@puppetlabs.com>
2011-04-06 14:37:07 -0700

    maint: whitespace cleanup for spec_helper.
    We had some incorrect indentation in the file; we fix that up now before we
    make any substantive changes.
    
    Paired-With: Dan Bode <dan@puppetlabs.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,31 +12,33 @@ Pathname.glob("#{dir}/shared_behaviours/**/*.rb") do |behaviour|
 end
 
 RSpec.configure do |config|
-    config.mock_with :mocha
+  config.mock_with :mocha
 
-    config.before :each do
-      # Set the confdir and vardir to gibberish so that tests
-      # have to be correctly mocked.
-      Puppet[:confdir] = "/dev/null"
-      Puppet[:vardir] = "/dev/null"
+  config.before :each do
+    # Set the confdir and vardir to gibberish so that tests
+    # have to be correctly mocked.
+    Puppet[:confdir] = "/dev/null"
+    Puppet[:vardir] = "/dev/null"
 
-      # Avoid opening ports to the outside world
-      Puppet.settings[:bindaddress] = "127.0.0.1"
+    # Avoid opening ports to the outside world
+    Puppet.settings[:bindaddress] = "127.0.0.1"
 
-      @logs = []
-      Puppet::Util::Log.newdestination(@logs)
-    end
+    @logs = []
+    Puppet::Util::Log.newdestination(@logs)
 
-    config.after :each do
-      Puppet.settings.clear
+    
+  end
 
-      @logs.clear
-      Puppet::Util::Log.close_all
-    end
+  config.after :each do
+    Puppet.settings.clear
+
+    @logs.clear
+    Puppet::Util::Log.close_all
+  end
 end
 
 # We need this because the RAL uses 'should' as a method.  This
 # allows us the same behaviour but with a different method name.
 class Object
-    alias :must :should
+  alias :must :should
 end
