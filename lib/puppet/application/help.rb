2a87f410eae84a0a7c4f39d9c1ef742c3e7ba8fc
Daniel Pittman <daniel@puppetlabs.com>
2011-04-11 16:43:56 -0700

    (#6962) Override 'render' in help to just return the string.
    The default behaviour is to serialize the result somehow, defaulting to
    displayed, render should be a no-op.  This means overriding the parent method.
    
    Paired-With: Matt Robinson <matt@puppetlabs.com>

--- lib/puppet/application/help.rb
+++ lib/puppet/application/help.rb
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 require 'puppet/application/faces_base'
 
 class Puppet::Application::Help < Puppet::Application::FacesBase
-  def render(result)
-    result.join("\n")
-  end
+  # Meh.  Disable the default behaviour, which is to inspect the
+  # string and return that – not so helpful. --daniel 2011-04-11
+  def render(result) result end
 end
