349bd96c0fd59f8c7c1a83b7d40000d703136a87
Dominic Maraglia <dmaraglia@gmail.com>
2011-04-25 11:14:04 -0700

    Fix test ticket_6928_puppet_master_parse_fails
    Output for error conditions changed causing a regex to fail

--- acceptance/tests/ticket_6928_puppet_master_parse_fails.rb
+++ acceptance/tests/ticket_6928_puppet_master_parse_fails.rb
@@ -7,8 +7,7 @@ create_remote_file(master, '/tmp/bad.pp', 'notify{bad:')
 
 step "Master: use --parseonly on an invalid manifest, should return 1 and issue deprecation warning"
 on master, puppet_master( %w{--parseonly /tmp/bad.pp} ), :acceptable_exit_codes => [ 1 ]
-  fail_test "Deprecation warning not issued for --parseonly" unless
-    stdout.include? '--parseonly has been removed. Please use \'puppet parser validate <manifest>\''
+  assert_match(/--parseonly has been removed. Please use \'puppet parser validate <manifest>\'/, stdout, "Deprecation warning not issued for --parseonly on #{master}" )
 
 step "Agents: create valid, invalid formatted manifests"
 agents.each do |host|
@@ -19,8 +18,7 @@ end
 step "Agents: use --parseonly on an invalid manifest, should return 1 and issue deprecation warning"
 agents.each do |host|
   on(host, "puppet --parseonly /tmp/bad.pp}", :acceptable_exit_codes => [ 1 ]) do
-    fail_test "Deprecation warning not issued for --parseonly" unless
-      stdout.include? '--parseonly has been removed. Please use \'puppet parser validate <manifest>\''
+    assert_match(/--parseonly has been removed. Please use \'puppet parser validate <manifest>\'/, stdout, "Deprecation warning not issued for --parseonly on #{host}" )
   end
 end
 
@@ -29,10 +27,9 @@ agents.each do |host|
   on(host, "puppet parser validate /tmp/good.pp", :acceptable_exit_codes => [ 0 ])
 end
 
-step "Test Face for ‘parser validate’ with bad manifest -- should fail"
+step "Test Faces for ‘parser validate’ with bad manifest -- should fail"
 agents.each do |host|
   on(host, "puppet parser validate /tmp/bad.pp", :acceptable_exit_codes => [ 1 ]) do
-    fail_test "Bad manifest detection failed" unless
-      stderr.include? 'Could not run: Could not parse for environment production'
+    assert_match(/err: Could not parse for environment production/, stdout, "Bad manifest detection failed on #{host}" )
   end
 end
