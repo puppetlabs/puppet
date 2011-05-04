8a8e1981f2189ba0ae3942625e93f495671c20ee
Dominic Maraglia <dmaraglia@gmail.com>
2011-05-04 14:19:57 -0700

    adding test for ticket 7117

--- acceptance/tests/ticket_7117_broke_env_criteria_authconf.rb
+++ acceptance/tests/ticket_7117_broke_env_criteria_authconf.rb
@@ -1,7 +1,5 @@
 test_name "#7117 Broke the environment criteria in auth.conf"
 
-# curl -k -H "Accept: yaml" https://cent-55-64-1:8140/override/facts/cent-55-64-1.local
-
 # add to auth.conf
 add_2_authconf = %q{
 path /
@@ -14,7 +12,11 @@ step "Save original auth.conf file and create a temp auth.conf"
 on master, "cp #{config['puppetpath']}/auth.conf /tmp/auth.conf-7117; echo '#{add_2_authconf}' > #{config['puppetpath']}/auth.conf"
 
 step "Fetch agent facts from Puppet Master"
-on agents, "curl -k -H \"Accept: yaml\" https://#{master}:8140/override/facts/cent-55-64-1.local"
+agents.each do |host|
+  on(host, "curl -k -H \"Accept: yaml\" https://#{master}:8140/override/facts/\`hostname -f\`") do
+    assert_match(/--- !ruby\/object:Puppet::Node::Facts/, stdout, "Agent Facts not returned for #{host}") 
+  end
+end
 
 step "Restore original auth.conf file"
 on master, "cp -f /tmp/auth.conf-7117 #{config['puppetpath']}/auth.conf"
