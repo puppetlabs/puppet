615946619b7121ab724fac1a3d78b8f4072b867d
Matt Robinson <matt@puppetlabs.com>
2011-05-13 16:29:47 -0700

    (#7291) The 'script' version of actions needs options argument
    Ruby 1.9 is stricter about arity for for arguments passed as a block.  A
    test case just hadn't been updated to take this into account for Face
    scripts (a simpler form of action), which isn't surprising since script
    isn't used anywhere in the code, which makes it a prime candidate for
    removal as far as I'm concerned, but apparently Luke wants it in there.
    
    Reviewed-by: Pieter van de Bruggen <pieter@puppetlabs.com>

--- spec/lib/puppet/face/huzzah.rb
+++ spec/lib/puppet/face/huzzah.rb
@@ -3,5 +3,5 @@ Puppet::Face.define(:huzzah, '2.0.1') do
   copyright "Puppet Labs", 2011
   license   "Apache 2 license; see COPYING"
   summary "life is a thing for celebration"
-  script :bar do "is where beer comes from" end
+  script :bar do |options| "is where beer comes from" end
 end
