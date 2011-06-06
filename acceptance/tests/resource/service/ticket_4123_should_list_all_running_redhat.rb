c8df02703ac29b00140648640f85ec044a9cfd6e
Matt Robinson <matt@puppetlabs.com>
2011-06-06 14:40:39 -0700

    (#7193) Fix path issues with acceptance tests that call old shell tests
    Some rearranging of the acceptance test directories caused some ruby
    tests that referenced old shell tests to lose their relative paths.
    
    Paired-with: Dominic Maraglia <dominic@puppetlabs.com>

--- acceptance/tests/resource/service/ticket_4123_should_list_all_running_redhat.rb
+++ acceptance/tests/resource/service/ticket_4123_should_list_all_running_redhat.rb
@@ -7,6 +7,6 @@ hosts.each do |host|
   unless host['platform'].include? 'centos' or host['platform'].include? 'redhat'
     skip_test "Test not supported on this plaform" 
   else
-    run_script_on(host,'tests/acceptance/resource/service/ticket_4123_should_list_all_running_redhat.sh')
+    run_script_on(host,'acceptance-tests/tests/resource/service/ticket_4123_should_list_all_running_redhat.sh')
   end
 end
