9017fea1904270f1c1a5365fe23fc52817867334
Daniel Pittman <daniel@puppetlabs.com>
2011-06-07 14:24:42 -0700

    (#7728) Acceptance test for whit notifications.
    This adds an acceptance test to verify that we emit the expected logs, but
    none of the unexpected logs, when we process a manifest showing the whit
    notification problem.
    
    Reviewed-By: Dominic Maraglia <dominic@puppetlabs.com>
    Reviewed-By: Jacob Helwig <jacob@puppetlabs.com>

---
+++ acceptance/tests/ticket_7728_don't_log_whits_on_failure.rb
@@ -0,0 +1,20 @@
+test_name "#7728: Don't log whits on resource failure"
+
+manifest = %Q{
+  class foo {
+    exec { "test": command => "/usr/bin/false" }
+
+    notify { "before": before  => Exec["test"] }
+    notify { "after":  require => Exec["test"] }
+  }
+
+  include foo
+}
+
+apply_manifest_on(agents, manifest) do
+  # Note: using strings in the match, because I want them regexp-escaped,
+  # and the assertion code will do that automatically. --daniel 2011-06-07
+  assert_match('notice: /Stage[main]/Foo/Notify[after]: Dependency Exec[test] has failures: true', stdout, "the after dependency must be reported")
+  assert_no_match('Class[Foo]', stdout, 'the class should not be mentioned')
+  assert_no_match('Stage[Main]', stdout, 'the class should not be mentioned')
+end
