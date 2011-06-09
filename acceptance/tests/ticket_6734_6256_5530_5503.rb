b502423356f0cf9c42f06fe5822c8840069620ab
Nick Lewis <nick@puppetlabs.com>
2011-06-09 15:41:44 -0700

    Update acceptance tests to use with_master_running_on
    A few of these tests weren't yet using this helper, and were instead
    implementing similar behavior on their own. Now they all use the standard
    method.
    
    Reviewed-By: Jacob Helwig <jacob@puppetlabs.com>
    Reviewed-By: Dominic Maraglia <dominic@puppetlabs.com>

--- acceptance/tests/ticket_6734_6256_5530_5503.rb
+++ acceptance/tests/ticket_6734_6256_5530_5503.rb
@@ -4,9 +4,7 @@
  
 test_name "Tickets 6734 6256 5530 5503i Puppet Master fails to start"
 
-# Kill running Puppet Master
 with_master_running_on(master) do
-
   step "Check permissions on puppet/rrd/"
   on master, "ls -l /var/lib/puppet | grep rrd | awk '{print $3\" \"$4}'" do
     fail_test "puppet/rrd does not exist/wrong permission" unless stdout.include? 'puppet puppet'
