3d082e4d936c62723f93a424f2ccf30852e934ad
R.I.Pienaar <rip@devco.net>
2011-06-11 13:02:43 +0100

    Fix spec failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ $:.insert(0, File.join([File.dirname(__FILE__), "..", "..", "lib"]))
 require 'rubygems'
 require 'rspec'
 require 'hiera/backend/puppet_backend'
-require 'hiera/puppet_logger'
 require 'hiera/scope'
 require 'rspec/mocks'
 require 'mocha'
