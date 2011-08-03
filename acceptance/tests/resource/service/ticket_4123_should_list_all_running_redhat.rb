8d86e5a421eeaf0972ad520b4a732c33754671bb
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-03 16:12:34 -0700

    (9547) Minor mods to acceptance tests
    Consolidation of all RHEL based systems to now be identified as
    "el-" vs. rhel-, centos-, scientific-, etc.  Platform regex
    changed to only use "el-".

--- acceptance/tests/resource/service/ticket_4123_should_list_all_running_redhat.rb
+++ acceptance/tests/resource/service/ticket_4123_should_list_all_running_redhat.rb
@@ -4,7 +4,7 @@ step "Validate services running agreement ralsh vs. OS service count"
 # ticket_4123_should_list_all_running_redhat.sh
 
 hosts.each do |host|
-  if host['platform'].include?('centos') or host['platform'].include?('rhel')
+  if host['platform'].include?('el-')
     run_script_on(host,'acceptance-tests/tests/resource/service/ticket_4123_should_list_all_running_redhat.sh')
   else
     skip_test "Test not supported on this plaform" 
