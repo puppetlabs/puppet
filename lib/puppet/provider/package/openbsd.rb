3d1c68709cf7fd6a739598c5f11375796dd3d402
Peter Meier <peter.meier@immerda.ch>
2011-08-04 00:05:11 +0200

    Possible fix for #8435 - remote package installation broken
    This is the current fix I'm using to be able to use remote package
    installations on openbsd. This is just a proposal as it's a quick
    and dirty workaround and lacks tests.
    
    Brought to 2.7.x and conflicts resolved in:
     * lib/puppet/provider/package/openbsd.rb
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/provider/package/openbsd.rb
+++ lib/puppet/provider/package/openbsd.rb
@@ -57,18 +57,18 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
 
     unless @resource[:source]
       raise Puppet::Error,
-        "You must specify a package source for BSD packages"
+      "You must specify a package source for BSD packages"
     end
 
-    if @resource[:source][-1,1] == ::File::PATH_SEPARATOR
-      e_vars = { :PKG_PATH => @resource[:source] }
-      full_name = [ @resource[:name], get_version || @resource[:ensure], @resource[:flavor] ].join('-').chomp('-')
+    if @resource[:source][-1,1] == ::File::SEPARATOR
+      e_vars = { 'PKG_PATH' => @resource[:source] }
+      full_name = [ @resource[:name], get_version || @resource[:ensure], @resource[:flavor] ].join('-').chomp('-').chomp('-')
     else
       e_vars = {}
       full_name = @resource[:source]
     end
 
-     Puppet::Util::Execution::withenv(e_vars) { pkgadd full_name }
+    Puppet::Util::Execution::withenv(e_vars) { pkgadd full_name }
   end
 
   def get_version
@@ -77,6 +77,7 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
       regex = /^(.*)-(\d[^-]*)[-]?(\D*)(.*)$/
       fields = [ :name, :version, :flavor ]
       master_version = 0
+      version = -1
 
       process.each_line do |line|
         if match = regex.match(line.split[0])
@@ -89,6 +90,7 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
       end
 
       return master_version unless master_version == 0
+      return '' if version == -1
       raise Puppet::Error, "#{version} is not available for this package"
     end
   rescue Puppet::ExecutionFailure
