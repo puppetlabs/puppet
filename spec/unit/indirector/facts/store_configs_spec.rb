4b55e72aba4d572199b4c0d11474e813c77be5fa
Daniel Pittman <daniel@puppetlabs.com>
2011-08-18 16:13:17 -0700

    (#9051) Implement the StoreConfigs indirection itself.
    We now have a StoreConfigs terminus base class, and concrete uses in a number
    of indirections, along with testing to ensure they behave as expected.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

---
+++ spec/unit/indirector/facts/store_configs_spec.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env rspec
+require 'spec_helper'
+require 'puppet/node'
+require 'puppet/indirector/memory'
+require 'puppet/indirector/facts/store_configs'
+
+class Puppet::Node::Facts::StoreConfigsTesting < Puppet::Indirector::Memory
+end
+
+describe Puppet::Node::Facts::StoreConfigs do
+  after :all do
+    Puppet::Node::Facts.indirection.reset_terminus_class
+    Puppet::Node::Facts.indirection.cache_class = nil
+  end
+
+  it_should_behave_like "a StoreConfigs terminus"
+end
