d8516d947c4fae19e5a478e9401fdeff2ff4725f
Daniel Pittman <daniel@puppetlabs.com>
2011-10-19 11:50:52 -0700

    (#2848) Consistently use `subject_alt_names` as accessor name.
    We had several different names for the same operation on different SSL
    objects; this made little sense.  Now they are unified to use the same,
    standard-based, name for the accessor.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- spec/unit/ssl/certificate_spec.rb
+++ spec/unit/ssl/certificate_spec.rb
@@ -89,7 +89,7 @@ describe Puppet::SSL::Certificate do
       @certificate.should respond_to(:content)
     end
 
-    describe "#alternate_names" do
+    describe "#subject_alt_names" do
       it "should list all alternate names when the extension is present" do
         key = Puppet::SSL::Key.new('quux')
         key.generate
@@ -116,7 +116,7 @@ describe Puppet::SSL::Certificate do
 
         cert = Puppet::SSL::CertificateFactory.build('client', csr, raw_csr, 14)
         certificate = @class.from_s(cert.to_pem)
-        certificate.alternate_names.should == []
+        certificate.subject_alt_names.should == []
       end
     end
 
