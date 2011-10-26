5a2952c5adb7c900af7f133d16a41b20a7df1d03
Nick Lewis <nick@puppetlabs.com>
2011-10-26 10:24:08 -0700

    (maint) Fix CA-related specs failing on Windows
    These specs were all failing on Windows because they were trying to
    perform actions related to signing certificates (either to test that
    behavior, or as setup), which is not supported on Windows. So we
    disable these tests on Windows.

--- spec/unit/ssl/host_spec.rb
+++ spec/unit/ssl/host_spec.rb
@@ -79,7 +79,7 @@ describe Puppet::SSL::Host do
     Puppet::SSL::Host.localhost.should equal(host)
   end
 
-  it "should create a localhost cert if no cert is available and it is a CA with autosign and it is using DNS alt names" do
+  it "should create a localhost cert if no cert is available and it is a CA with autosign and it is using DNS alt names", :unless => Puppet.features.microsoft_windows? do
     Puppet[:autosign] = true
     Puppet[:confdir] = tmpdir('conf')
     Puppet[:dns_alt_names] = "foo,bar,baz"
