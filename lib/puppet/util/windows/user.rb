1e541900c5eba981be1008e73aaedf3bc2493381
Josh Cooper <josh@puppetlabs.com>
2011-11-11 10:11:24 -0800

    (#10299) Use CheckTokenMembership to see if user has admin rights
    Previously, on Windows 2003 and earlier, Puppet.features.root? was
    implemented by checking if the current user is a member of the local
    Administrators group. However, many accounts, e.g. LocalSystem, are
    implicit members of this group, so Puppet.features.root? would
    incorrectly return false. This led to puppet not being able to find
    its default configuration directory, among other things.
    
    Conversely, a process can be executing using a restricted token, so
    while the user may be a member of the Administrators group, the
    process will be running with less privileges, and
    Puppet.features.root?  would incorrectly return true.
    
    This commit uses CheckTokenMembership to determine if the local
    Administrators group SID is both present and enabled in the calling
    thread's access token.
    
    The behavior on Vista/2008 is unchanged. The calling thread's token
    must be currently elevated.

--- lib/puppet/util/windows/user.rb
+++ lib/puppet/util/windows/user.rb
@@ -1,9 +1,12 @@
 require 'puppet/util/windows'
 
+require 'win32/security'
+
 module Puppet::Util::Windows::User
+  include Windows::Security
+  extend Windows::Security
+
   def admin?
-    require 'sys/admin'
-    require 'win32/security'
     require 'facter'
 
     majversion = Facter.value(:kernelmajversion)
@@ -12,8 +15,30 @@ module Puppet::Util::Windows::User
     # if Vista or later, check for unrestricted process token
     return Win32::Security.elevated_security? unless majversion.to_f < 6.0
 
-    group = Sys::Admin.get_group("Administrators", :sid => Win32::Security::SID::BuiltinAdministrators)
-    group and group.members.index(Sys::Admin.get_login) != nil
+    # otherwise 2003 or less
+    check_token_membership
   end
   module_function :admin?
+
+  def check_token_membership
+    sid = 0.chr * 80
+    size = [80].pack('L')
+    member = 0.chr * 4
+
+    unless CreateWellKnownSid(WinBuiltinAdministratorsSid, nil, sid, size)
+      raise Puppet::Util::Windows::Error.new("Failed to create administrators SID")
+    end
+
+    unless IsValidSid(sid)
+      raise Puppet::Util::Windows::Error.new("Invalid SID")
+    end
+
+    unless CheckTokenMembership(nil, sid, member)
+      raise Puppet::Util::Windows::Error.new("Failed to check membership")
+    end
+
+    # Is administrators SID enabled in calling thread's access token?
+    member.unpack('L')[0] == 1
+  end
+  module_function :check_token_membership
 end
