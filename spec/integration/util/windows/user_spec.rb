e094195901aa77e5183845eb781435d722054d92
Josh Cooper <josh@puppetlabs.com>
2011-11-11 10:11:24 -0800

    (#10299) Use CheckTokenMembership to see if user has admin rights
    Previously, on Windows 2003 and earlier, Puppet.features.root? was
    implemented by checking if the current user is a member of the local
    Administrators group. However, many accounts, e.g. LocalSystem, are
    implicit members of this group, so Puppet.features.root? would
    incorrectly return false. This led to puppet not being able to find
    its default configuration directory, among other things.
    
    Conversely, a process can be executing using a restricted token, so
    while the user may be a member of the Administrators group, the
    process will be running with less privileges, and
    Puppet.features.root?  would incorrectly return true.
    
    This commit uses CheckTokenMembership to determine if the local
    Administrators group SID is both present and enabled in the calling
    thread's access token.
    
    The behavior on Vista/2008 is unchanged. The calling thread's token
    must be currently elevated.

--- spec/integration/util/windows/user_spec.rb
+++ spec/integration/util/windows/user_spec.rb
@@ -8,26 +8,23 @@ describe "Puppet::Util::Windows::User", :if => Puppet.features.microsoft_windows
       Facter.stubs(:value).with(:kernelmajversion).returns("5.2")
     end
 
-    it "should be root if user is a member of the Administrators group" do
-      Sys::Admin.stubs(:get_login).returns("Administrator")
-      Sys::Group.stubs(:members).returns(%w[Administrator])
-
+    it "should be an admin if user's token contains the Administrators SID" do
+      Puppet::Util::Windows::User.expects(:check_token_membership).returns(true)
       Win32::Security.expects(:elevated_security?).never
+
       Puppet::Util::Windows::User.should be_admin
     end
 
-    it "should not be root if the process is running as Guest" do
-      Sys::Admin.stubs(:get_login).returns("Guest")
-      Sys::Group.stubs(:members).returns([])
-
+    it "should not be an admin if user's token doesn't contain the Administrators SID" do
+      Puppet::Util::Windows::User.expects(:check_token_membership).returns(false)
       Win32::Security.expects(:elevated_security?).never
+
       Puppet::Util::Windows::User.should_not be_admin
     end
 
-    it "should raise an exception if the process fails to open the process token" do
-      Win32::Security.stubs(:elevated_security?).raises(Win32::Security::Error, "Access denied.")
-      Sys::Admin.stubs(:get_login).returns("Administrator")
-      Sys::Group.expects(:members).never
+    it "should raise an exception if we can't check token membership" do
+      Puppet::Util::Windows::User.expects(:check_token_membership).raises(Win32::Security::Error, "Access denied.")
+      Win32::Security.expects(:elevated_security?).never
 
       lambda { Puppet::Util::Windows::User.should raise_error(Win32::Security::Error, /Access denied./) }
     end
@@ -38,23 +35,23 @@ describe "Puppet::Util::Windows::User", :if => Puppet.features.microsoft_windows
       Facter.stubs(:value).with(:kernelmajversion).returns("6.0")
     end
 
-    it "should be root if user is running with elevated privileges" do
+    it "should be an admin if user is running with elevated privileges" do
       Win32::Security.stubs(:elevated_security?).returns(true)
-      Sys::Admin.expects(:get_login).never
+      Puppet::Util::Windows::User.expects(:check_token_membership).never
 
       Puppet::Util::Windows::User.should be_admin
     end
 
-    it "should not be root if user is not running with elevated privileges" do
+    it "should not be an admin if user is not running with elevated privileges" do
       Win32::Security.stubs(:elevated_security?).returns(false)
-      Sys::Admin.expects(:get_login).never
+      Puppet::Util::Windows::User.expects(:check_token_membership).never
 
       Puppet::Util::Windows::User.should_not be_admin
     end
 
     it "should raise an exception if the process fails to open the process token" do
       Win32::Security.stubs(:elevated_security?).raises(Win32::Security::Error, "Access denied.")
-      Sys::Admin.expects(:get_login).never
+      Puppet::Util::Windows::User.expects(:check_token_membership).never
 
       lambda { Puppet::Util::Windows::User.should raise_error(Win32::Security::Error, /Access denied./) }
     end
