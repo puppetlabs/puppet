884381f7d6bd7ed3fb3ad9028932f45861aff99a
Tim Bishop <tim@bishnet.net>
2011-11-18 00:45:53 +0000

    (#11318) Add password management on FreeBSD
    This adds the manages_passwords feature to the pw user provider. It is based
    on the patch by Andrew Hust that was integrated into FreeBSD puppet port. It
    adds tests covering the create, delete and modify processes of the provider.
    
    This integrates a fix for #7500 that was introduced by the original patch.
    The existing code takes the first character of each property and uses it as a
    flag. However, with pw, the -p flag is for setting the password expiration.
    The result is that the password isn't set at create time and that the password
    is set to expire. The next run of puppet correctly sets the password but the
    expiry is still set. The new code avoids using -p for passwords, and also sets
    the password correctly when an account is created.
    
    Reviewed-by: Patrick Carlisle <patrick@puppetlabs.com>

--- lib/puppet/provider/user/pw.rb
+++ lib/puppet/provider/user/pw.rb
@@ -1,10 +1,11 @@
 require 'puppet/provider/nameservice/pw'
+require 'open3'
 
 Puppet::Type.type(:user).provide :pw, :parent => Puppet::Provider::NameService::PW do
   desc "User management via `pw` on FreeBSD."
 
   commands :pw => "pw"
-  has_features :manages_homedir, :allows_duplicates
+  has_features :manages_homedir, :allows_duplicates, :manages_passwords
 
   defaultfor :operatingsystem => :freebsd
 
@@ -23,7 +24,7 @@ Puppet::Type.type(:user).provide :pw, :parent => Puppet::Provider::NameService::
   def addcmd
     cmd = [command(:pw), "useradd", @resource[:name]]
     @resource.class.validproperties.each do |property|
-      next if property == :ensure
+      next if property == :ensure or property == :password
       # the value needs to be quoted, mostly because -c might
       # have spaces in it
       if value = @resource.should(property) and value != ""
@@ -36,6 +37,31 @@ Puppet::Type.type(:user).provide :pw, :parent => Puppet::Provider::NameService::
     cmd << "-m" if @resource.managehome?
 
     cmd
+  end
+
+  def create
+    super
+
+    # Set the password after create if given
+    self.password = @resource[:password] if @resource[:password]
+  end
+
+  # use pw to update password hash
+  def password=(cryptopw)
+    Puppet.debug "change password for user '#{@resource[:name]}' method called with hash '#{cryptopw}'"
+    stdin, stdout, stderr = Open3.popen3("pw user mod #{@resource[:name]} -H 0")
+    stdin.puts(cryptopw)
+    stdin.close
+    Puppet.debug "finished password for user '#{@resource[:name]}' method called with hash '#{cryptopw}'"
+  end
+
+  # get password from /etc/master.passwd
+  def password
+    Puppet.debug "checking password for user '#{@resource[:name]}' method called"
+    current_passline = `getent passwd #{@resource[:name]}`
+    current_password = current_passline.chomp.split(':')[1] if current_passline
+    Puppet.debug "finished password for user '#{@resource[:name]}' method called : '#{current_password}'"
+    current_password
   end
 end
 
