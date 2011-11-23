632457e437f3a022273ab747831b1e30cd32743b
Hunter Haugen <h.haugen@gmail.com>
2011-11-22 18:07:35 -0800

    Rubygems is not required to use hiera
    Hiera may be used if the libs are sync'd from a puppet module via
    pluginsync, even without rubygems

--- lib/puppet/parser/functions/hiera.rb
+++ lib/puppet/parser/functions/hiera.rb
@@ -20,7 +20,6 @@ module Puppet::Parser::Functions
         configfile = File.join([File.dirname(Puppet.settings[:config]), "hiera.yaml"])
 
         raise(Puppet::ParseError, "Hiera config file #{configfile} not readable") unless File.exist?(configfile)
-        raise(Puppet::ParseError, "You need rubygems to use Hiera") unless Puppet.features.rubygems?
 
         require 'hiera'
         require 'hiera/scope'
