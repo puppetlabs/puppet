ee4dbf4d15207f84107ae5b0fb287b5425b3ebd9
Kelsey Hightower <kelsey@puppetlabs.com>
2011-12-05 10:14:38 -0500

    (#7656) Port PMT test fixtures into Puppet core
    Before this patch, the Puppet Module Tool (PMT) test fixtures lived in
    the PMT codebase -- in a separate project.
    
    This patch ports the PMT test fixtures into core Puppet. There are no
    major changes or refactorings. This patch places the PMT test fixtures
    under the `spec/fixtures` directory; the same relative location of the
    previous codebase.

---
+++ spec/fixtures/releases/jamtur01-apache/lib/puppet/provider/a2mod/debian.rb
@@ -0,0 +1,21 @@
+Puppet::Type.type(:a2mod).provide(:debian) do
+    desc "Manage Apache 2 modules on Debian-like OSes (e.g. Ubuntu)"
+
+    commands :encmd => "a2enmod"
+    commands :discmd => "a2dismod"
+
+    defaultfor :operatingsystem => [:debian, :ubuntu]
+
+    def create
+        encmd resource[:name]
+    end
+
+    def destroy
+        discmd resource[:name]
+    end
+
+    def exists?
+        mod= "/etc/apache2/mods-enabled/" + resource[:name] + ".load"
+        File.exists?(mod)
+    end
+end
