bdeb3b77525bb3932ada6b5fa0c09289df7d8f48
Kelsey Hightower <kelsey@puppetlabs.com>
2011-12-05 12:07:02 -0500

    (#7656) Add search action to module_tool face
    Before this patch, the module_tool face did not have the ability to
    search a repository for a module, and there was no test coverage for the
    module_tool search action.
    
    This patch adds support for searching a repository for a module to the
    module_tool face.
    
    Example usage:
    
        $ puppet module_tool search modulename
    
    This patch includes unit tests for the module_tool search action.

---
+++ lib/puppet/face/module_tool/search.rb
@@ -0,0 +1,38 @@
+Puppet::Face.define(:module_tool, '0.0.1') do
+  action(:search) do
+    summary "Search a repository for a module."
+    description <<-EOT
+      Search a repository for modules whose names match a specific substring.
+    EOT
+
+    returns "Array of module metadata hashes"
+
+    examples <<-EOT
+      Search the default repository for a module:
+
+      $ puppet module_tool search modulename
+    EOT
+
+    arguments "<term>"
+
+    option "--module-repository=", "-r=" do
+      default_to { Puppet.settings[:module_repository] }
+      summary "Module repository to use."
+      description <<-EOT
+        Module repository to use.
+      EOT
+    end
+
+    when_invoked do |term, options|
+      Puppet.notice "Searching #{options[:module_repository]}"
+      Puppet::Module::Tool::Applications::Searcher.run(term, options)
+    end
+
+    when_rendering :console do |return_value|
+      Puppet.notice "#{return_value.size} found."
+      return_value.map do |match|
+        "#{match['full_name']} (#{match['version']})"
+      end.join("\n")
+    end
+  end
+end
---
+++ spec/unit/face/module_tool/search_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'puppet/face'
+
+describe "puppet module_tool search" do
+  subject { Puppet::Face[:module_tool, :current] }
+
+  let(:options) do
+    {}
+  end
+
+  describe "option validation" do
+    context "without any options" do
+      it "should require a search term" do
+        pattern = /wrong number of arguments/
+        expect { subject.search }.to raise_error ArgumentError, pattern
+      end
+    end
+
+    it "should accept the --module-repository option" do
+      options[:module_repository] = "http://forge.example.com"
+      Puppet::Module::Tool::Applications::Searcher.expects(:run).with("puppetlabs-apache", options).once
+      subject.search("puppetlabs-apache", options)
+    end
+  end
+
+  describe "inline documentation" do
+    subject { Puppet::Face[:module_tool, :current].get_action :search }
+
+    its(:summary)     { should =~ /search.*module/im }
+    its(:description) { should =~ /search.*module/im }
+    its(:returns)     { should =~ /array/i }
+    its(:examples)    { should_not be_empty }
+
+    %w{ license copyright summary description returns examples }.each do |doc|
+      context "of the" do
+        its(doc.to_sym) { should_not =~ /(FIXME|REVISIT|TODO)/ }
+      end
+    end
+  end
+end
