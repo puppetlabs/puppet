6d2fc99f0f8f30a3cfe59dfbbeb431d446eefca2
Kelsey Hightower <kelsey@puppetlabs.com>
2011-12-05 11:52:06 -0500

    (#7656) Add generate action to module_tool face
    Before this patch, the module_tool face did not have the ability to
    generate boilerplate for a new module, and there was no test coverage
    for the module_tool generate action.
    
    This patch adds support for generating boilerplate for a new module to
    the module_tool face.
    
    Example usage:
    
        $ puppet module_tool generate username-modulename
    
    This patch includes unit tests for the module_tool generate action.

---
+++ lib/puppet/face/module_tool/generate.rb
@@ -0,0 +1,30 @@
+Puppet::Face.define(:module_tool, '0.0.1') do
+  action(:generate) do
+    summary "Generate boilerplate for a new module."
+    description <<-EOT
+      Generate boilerplate for a new module by creating a directory
+      pre-populated with a directory structure and files recommended for
+      Puppet best practices.
+    EOT
+
+    returns "Array of Pathname objects representing paths of generated files."
+
+    examples <<-EOT
+      Generate a new module in the current directory:
+
+      $ puppet module_tool generate username-modulename
+    EOT
+
+    arguments "<name>"
+
+    when_invoked do |name, options|
+      Puppet::Module::Tool::Applications::Generator.run(name, options)
+    end
+
+    when_rendering :console do |return_value|
+      return_value.map do |generated_file|
+        "#{generated_file}"
+      end.join("\n")
+    end
+  end
+end
