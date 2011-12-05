da4a2361c65f5b48558384927d8e65a50914bda0
Kelsey Hightower <kelsey@puppetlabs.com>
2011-12-05 11:45:29 -0500

    (#7656) Add build action to module_tool face
    Before this patch, the module_tool face did not have the ability to
    build module release archives, and there was no test coverage for the
    module_tool build action.
    
    This patch adds support for building module release archives to the
    module_tool face.
    
    Example usage:
    
        $ puppet module_tool build /path/to/module
    
    This patch includes unit tests for the module_tool build action.

---
+++ lib/puppet/face/module_tool/build.rb
@@ -0,0 +1,35 @@
+Puppet::Face.define(:module_tool, '0.0.1') do
+  action(:build) do
+    summary "Build a module release package."
+    description <<-EOT
+      Build a module release archive file by processing the Modulefile in the
+      module directory.  The release archive file will be stored in the pkg
+      directory of the module directory.
+    EOT
+
+    returns "Pathname object representing the path to the release archive."
+
+    examples <<-EOT
+      Build a module release from within the module directory:
+
+      $ puppet module_tool build
+
+      Build a module release from outside the module directory:
+
+      $ puppet module_tool build /path/to/module
+    EOT
+
+    arguments "<path>"
+
+    when_invoked do |path, options|
+      root_path = Puppet::Module::Tool.find_module_root(path)
+      Puppet::Module::Tool::Applications::Builder.run(root_path, options)
+    end
+
+    when_rendering :console do |return_value|
+      # Get the string representation of the Pathname object and print it to
+      # the console.
+      return_value.to_s
+    end
+  end
+end
