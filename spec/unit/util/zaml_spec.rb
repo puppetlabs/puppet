a01aab2ce758f6b38f3a52c36842834cd08e3644
Jeff McCune <jeff@puppetlabs.com>
2011-12-07 10:45:24 -0800

    (#11246) Add UTF-8 String#to_yaml spec tests
    This patch adds a specification test to validate that Puppet can
    properly serialize a UTF-8 encoded string to YAML.  Without this patch
    we don't have a test that covers this desired behavior.
    
    This patch provides an automated way to reproduce the bug reported in
    ticket #11246.
    
    The specific test for #11246 that will fail with Ruby 1.9 but not with 1.8 are
    these two.  The tests encode and catch our expectation that UTF-8 encoded
    strings properly serialize and de-serialize to and from YAML.
    
      1) UTF-8 String YAML Handling (Bug #11246) UTF-8 String Literal should serialize to YAML
         Failure/Error: subject.to_yaml
         Encoding::CompatibilityError:
           incompatible encoding regexp match (ASCII-8BIT regexp with UTF-8 string)
         # ./lib/puppet/util/zaml.rb:241:in `block in to_zaml'
         # ./lib/puppet/util/zaml.rb:90:in `first_time_only'
         # ./lib/puppet/util/zaml.rb:227:in `to_zaml'
         # ./lib/puppet/util/zaml.rb:22:in `dump'
         # ./lib/puppet/util/monkey_patches.rb:32:in `to_yaml'
         # ./spec/unit/util/zaml_spec.rb:83:in `block (3 levels) in <top (required)>'
    
      2) UTF-8 String YAML Handling (Bug #11246) UTF-8 String Literal should serialize and deserialize to the same thing
         Failure/Error: YAML.load(subject.to_yaml).should == subject
         Encoding::CompatibilityError:
           incompatible encoding regexp match (ASCII-8BIT regexp with UTF-8 string)
         # ./lib/puppet/util/zaml.rb:241:in `block in to_zaml'
         # ./lib/puppet/util/zaml.rb:90:in `first_time_only'
         # ./lib/puppet/util/zaml.rb:227:in `to_zaml'
         # ./lib/puppet/util/zaml.rb:22:in `dump'
         # ./lib/puppet/util/monkey_patches.rb:32:in `to_yaml'
         # ./spec/unit/util/zaml_spec.rb:86:in `block (3 levels) in <top (required)>'

--- spec/unit/util/zaml_spec.rb
+++ spec/unit/util/zaml_spec.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env rspec
+# encoding: UTF-8
+# The above encoding line is a magic comment to set the default encoding of
+# this file for the Ruby interpreter.  The source encoding affects String and
+# Regexp objects created from this source file.
+
 require 'spec_helper'
 
 require 'puppet/util/monkey_patches'
@@ -58,5 +63,43 @@ describe "Pure ruby yaml implementation" do
     x2[0].should == 1
     x2[1].should == 2
     x2[2].should equal(x2)
+  end
+end
+
+# Note, many of these tests will pass on Ruby 1.8 but fail on 1.9.  This is
+# intentional since the string encoding behavior changed significantly in 1.9.
+describe "UTF-8 String YAML Handling (Bug #11246)" do
+  # JJM All of these snowmen are different representations of the same
+  # UTF-8 encoded string.
+  let(:snowman)         { 'Snowman: [☃]' }
+  let(:snowman_escaped) { "Snowman: [\xE2\x98\x83]" }
+  let(:snowman_yaml)    { "--- \"Snowman: [\\xe2\\x98\\x83]\"" }
+  let(:snowman_re)      { /^Snowman: \[☃\]$/ }
+
+  describe "UTF-8 String Literal" do
+    subject { snowman }
+
+    it "should serialize to YAML" do
+      subject.to_yaml
+    end
+    it "should serialize and deserialize to the same thing" do
+      YAML.load(subject.to_yaml).should == subject
+    end
+  end
+  describe "YAML containing a UTF-8 String" do
+    subject { snowman_yaml }
+
+    it "should load from YAML" do
+      YAML.load(subject)
+    end
+    it "should equal the orginal string literal" do
+      YAML.load(subject).should == snowman
+    end
+    it "should equal the orginal string escape" do
+      YAML.load(subject).should == snowman_escaped
+    end
+    it "should work with Regexp's" do
+      YAML.load(subject).should =~ snowman_re
+    end
   end
 end
