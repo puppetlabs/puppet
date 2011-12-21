masterzen/puppet-old

Add the example 'log' listener
This listener stores the last 20 event durations for a given label.
It is not terribly useful except giving an idea of the api.

Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

spec/unit/util/instrumentation/listeners/log_spec.rb
@@ -1,0 +1,35 @@
+Dir.chdir(File.dirname(__FILE__)) { (s = lambda { |f| File.exist?(f) ? require(f) : Dir.chdir("..") { s.call(f) } }).call("spec/spec_helper.rb") }
+
+require 'puppet/util/instrumentation'
+
+Puppet::Util::Instrumentation.init
+log = Puppet::Util::Instrumentation.listener(:log)
+
+describe log do
+  before(:each) do
+    @log = log.new
+  end
+
+  it "should have a notify method" do
+    @log.should respond_to(:notify)
+  end
+
+  it "should have a data method" do
+    @log.should respond_to(:data)
+  end
+
+  it "should keep data for stop event" do
+    @log.notify(:test, :stop, { :started => Time.at(123456789), :finished => Time.at(123456790)})
+    @log.data.should == {:test=>["test took 1.0"]}
+  end
+
+  it "should not keep data for start event" do
+    @log.notify(:test, :start, { :started => Time.at(123456789)})
+    @log.data.should be_nil
+  end
+
+  it "should not keep more than 20 events per label" do
+    25.times { @log.notify(:test, :stop, { :started => Time.at(123456789), :finished => Time.at(123456790)}) }
+    @log.data[:test].size.should == 20
+  end
+end

puppetlabs/puppet

spec/unit/util/instrumentation/listeners/log_spec.rb
@@ -1,0 +1,34 @@
+require 'spec_helper'
+require 'puppet/util/instrumentation'
+
+Puppet::Util::Instrumentation.init
+log = Puppet::Util::Instrumentation.listener(:log)
+
+describe log do
+  before(:each) do
+    @log = log.new
+  end
+
+  it "should have a notify method" do
+    @log.should respond_to(:notify)
+  end
+
+  it "should have a data method" do
+    @log.should respond_to(:data)
+  end
+
+  it "should keep data for stop event" do
+    @log.notify(:test, :stop, { :started => Time.at(123456789), :finished => Time.at(123456790)})
+    @log.data.should == {:test=>["test took 1.0"]}
+  end
+
+  it "should not keep data for start event" do
+    @log.notify(:test, :start, { :started => Time.at(123456789)})
+    @log.data.should be_empty
+  end
+
+  it "should not keep more than 20 events per label" do
+    25.times { @log.notify(:test, :stop, { :started => Time.at(123456789), :finished => Time.at(123456790)}) }
+    @log.data[:test].size.should == 20
+  end
+end