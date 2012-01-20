bf267c0de7fb79004b3e9677eb6dcbe482c9dfe1
Dominic Maraglia <dominic@puppetlabs.com>
2012-01-19 17:35:46 -0800

    (#11727) Fix support stdin test
    Stdin test had a few typos that required fixing.
    ticket_3360 uses the only global config hash -- changed this
    to support the per host based configuration data.

--- acceptance/tests/ticket_11727_support_stdin_parsing_in_puppet_parser_validate.rb
+++ acceptance/tests/ticket_11727_support_stdin_parsing_in_puppet_parser_validate.rb
@@ -1,6 +1,6 @@
 test_name "#11727: support stdin parsing in puppet parser validate"
 
-pp = "#{scratch}/11727.pp"
+pp = "/tmp/11727.pp"
 
 step "validate with a tty parses the default manifest"
 on agents, puppet(%w{parser validate}) do
@@ -9,10 +9,10 @@ on agents, puppet(%w{parser validate}) do
 end
 
 step "create the remote manifest file for redirection"
-create_remote_file agents, pp 'notice("hello")'
+create_remote_file(agents, pp, 'notice("hello")')
 
 step "validate with redirection parses STDIN"
-on agents, puppet(%w{parser validate <}, pp), do
+on agents, puppet(%w{parser validate <}, pp) do
   assert_no_match(/Validating the default manifest/, stdout,
                   "there was message about validating default manifest despite redirect")
 end
