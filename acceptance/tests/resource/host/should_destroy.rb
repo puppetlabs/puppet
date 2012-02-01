1d1f68791a6eec931a55fd778a1c638ccb085902
Josh Cooper <josh@puppetlabs.com>
2012-02-01 15:57:16 -0800

    (#12412) Refactor puppet_resource('host')
    Previously, the host tests were creating files in '/tmp' and failing
    on Windows.
    
    This commit changes them to use `agent.tmpfile` instead. Most of the
    changes are a result of not being able to call `TestCase#on` with an
    array of agents, and instead iterating over each one in an outer loop
    (so that context-dependent tmpfiles can be created).

--- acceptance/tests/resource/host/should_destroy.rb
+++ acceptance/tests/resource/host/should_destroy.rb
@@ -1,19 +1,21 @@
 test_name "should be able to remove a host record"
 
-file = "/tmp/hosts-#{Time.new.to_i}"
-line = "127.0.0.7 test1"
+agents.each do |agent|
+  file = agent.tmpfile('host-destroy')
+  line = "127.0.0.7 test1"
 
-step "set up files for the test"
-on agents, "printf '#{line}\n' > #{file}"
+  step "set up files for the test"
+  on agent, "printf '#{line}\n' > #{file}"
 
-step "delete the resource from the file"
-on(agents, puppet_resource('host', 'test1', "target=#{file}",
+  step "delete the resource from the file"
+  on(agent, puppet_resource('host', 'test1', "target=#{file}",
               'ensure=absent', 'ip=127.0.0.7'))
 
-step "verify that the content was removed"
-on(agents, "cat #{file}; rm -f #{file}") do
+  step "verify that the content was removed"
+  on(agent, "cat #{file}; rm -f #{file}") do
     fail_test "the content was still present" if stdout.include? line
-end
+  end
 
-step "clean up after the test"
-on agents, "rm -f #{file}"
+  step "clean up after the test"
+  on agent, "rm -f #{file}"
+end
