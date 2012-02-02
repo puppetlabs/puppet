982b53ac614fc6d3abd0d63dd4c2f8b127554f62
Jeff Weiss <jeff.a.weiss@gmail.com>
2012-02-02 15:56:02 -0800

    (#12396) DRY up self.timeout
    Move duplicate definitions of self.timeout into newly created module.

--- lib/puppet/indirector/facts/facter.rb
+++ lib/puppet/indirector/facts/facter.rb
@@ -2,6 +2,10 @@ require 'puppet/node/facts'
 require 'puppet/indirector/code'
 
 class Puppet::Node::Facts::Facter < Puppet::Indirector::Code
+  require 'puppet/util/config_timeout'
+  class <<self
+    include Puppet::Util::ConfigTimeout
+  end
   desc "Retrieve facts from Facter.  This provides a somewhat abstract interface
     between Puppet and Facter.  It's only `somewhat` abstract because it always
     returns the local host's facts, regardless of what you attempt to find."
@@ -44,7 +48,7 @@ class Puppet::Node::Facts::Facter < Puppet::Indirector::Code
         fqfile = ::File.join(dir, file)
         begin
           Puppet.info "Loading facts in #{fqfile}"
-          Timeout::timeout(self.timeout) do
+          ::Timeout::timeout(self.timeout) do
             load file
           end
         rescue SystemExit,NoMemoryError
@@ -54,23 +58,6 @@ class Puppet::Node::Facts::Facter < Puppet::Indirector::Code
         end
       end
     end
-  end
-
-  def self.timeout
-    timeout = Puppet[:configtimeout]
-    case timeout
-    when String
-      if timeout =~ /^\d+$/
-        timeout = Integer(timeout)
-      else
-        raise ArgumentError, "Configuration timeout must be an integer"
-      end
-    when Integer # nothing
-    else
-      raise ArgumentError, "Configuration timeout must be an integer"
-    end
-
-    timeout
   end
 
   def destroy(facts)
