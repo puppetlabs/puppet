f73a741e0dc6e0f15956966fb24dac97a3bd6b4a
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-02-03 10:06:45 -0500

    (#7485) Support zypper 0.6 with zypper package provider
    Zypper version 0.6.104 which is the default version on SLED 10 does not
    support the --quiet flag. It only supports the --terse flag from
    rug.
    Zypper version 0.6.13 on OpenSuSE 10.2 does not support the auto agree
    with licenses and this flag is not used.
    Tested on:
    SLE 10.2, 11.0, 11.1
    OpenSuSE 10.2, 10.3, 11.1, 11.2, 11.3, 11.4

--- spec/unit/provider/package/zypper_spec.rb
+++ spec/unit/provider/package/zypper_spec.rb
@@ -24,7 +24,7 @@ describe provider_class do
     @provider.should respond_to(:install)
   end
 
-  it "should have a latest method" do
+  it "should have an uninstall method" do
     @provider = provider_class.new
     @provider.should respond_to(:uninstall)
   end
@@ -39,17 +39,64 @@ describe provider_class do
     @provider.should respond_to(:latest)
   end
 
-  describe "when installing" do
+  describe "when installing with zypper version >= 1.0" do
     it "should use a command-line with versioned package'" do
       @resource.stubs(:should).with(:ensure).returns "1.2.3-4.5.6"
-      @provider.expects(:zypper).with('--quiet', :install, '-l', '-y', 'mypackage-1.2.3-4.5.6')
+      @provider.stubs(:zypper_version).returns "1.2.8"
+
+      @provider.expects(:zypper).with('--quiet', :install,
+        '--auto-agree-with-licenses', '--no-confirm', 'mypackage-1.2.3-4.5.6')
       @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
       @provider.install
     end
 
     it "should use a command-line without versioned package" do
       @resource.stubs(:should).with(:ensure).returns :latest
-      @provider.expects(:zypper).with('--quiet', :install, '-l', '-y', 'mypackage')
+      @provider.stubs(:zypper_version).returns "1.2.8"
+      @provider.expects(:zypper).with('--quiet', :install,
+        '--auto-agree-with-licenses', '--no-confirm', 'mypackage')
+      @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
+      @provider.install
+    end
+  end
+
+  describe "when installing with zypper version = 0.6.104" do
+    it "should use a command-line with versioned package'" do
+      @resource.stubs(:should).with(:ensure).returns "1.2.3-4.5.6"
+      @provider.stubs(:zypper_version).returns "0.6.104"
+
+      @provider.expects(:zypper).with('--terse', :install,
+        '--auto-agree-with-licenses', '--no-confirm', 'mypackage-1.2.3-4.5.6')
+      @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
+      @provider.install
+    end
+
+    it "should use a command-line without versioned package" do
+      @resource.stubs(:should).with(:ensure).returns :latest
+      @provider.stubs(:zypper_version).returns "0.6.104"
+      @provider.expects(:zypper).with('--terse', :install,
+        '--auto-agree-with-licenses', '--no-confirm', 'mypackage')
+      @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
+      @provider.install
+    end
+  end
+
+  describe "when installing with zypper version = 0.6.13" do
+    it "should use a command-line with versioned package'" do
+      @resource.stubs(:should).with(:ensure).returns "1.2.3-4.5.6"
+      @provider.stubs(:zypper_version).returns "0.6.13"
+
+      @provider.expects(:zypper).with('--terse', :install,
+        '--no-confirm', 'mypackage-1.2.3-4.5.6')
+      @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
+      @provider.install
+    end
+
+    it "should use a command-line without versioned package" do
+      @resource.stubs(:should).with(:ensure).returns :latest
+      @provider.stubs(:zypper_version).returns "0.6.13"
+      @provider.expects(:zypper).with('--terse', :install,
+        '--no-confirm', 'mypackage')
       @provider.expects(:query).returns "mypackage 0 1.2.3 4.5.6 x86_64"
       @provider.install
     end
