8ae92477c2d6b6e94ec2d06784fa5554f6656d45
Josh Cooper <josh@puppetlabs.com>
2012-02-03 10:16:17 -0800

    (#12412) Skip ca acceptance test on Window agents
    Previously, this test would fail on Windows agents, because the ca
    code makes assumptions about posix style paths. Since this
    functionality is not supported on Windows anyways, this commit just
    skips the test when run on Windows agents. In order to make that
    possible, it refactors the test to use the `agents.each do |agent|`
    style test.

--- acceptance/tests/ticket_3961_puppet_ca_should_produce_certs.rb
+++ acceptance/tests/ticket_3961_puppet_ca_should_produce_certs.rb
@@ -1,9 +1,6 @@
 test_name "#3961: puppet ca should produce certs spec"
 
-scratch = "/tmp/puppet-ssl-3961"
 target  = "working3961.example.org"
-
-options = { :confdir => scratch, :vardir => scratch }
 
 expect = ['notice: Signed certificate request for ca',
           'notice: Rebuilding inventory file',
@@ -11,19 +8,28 @@ expect = ['notice: Signed certificate request for ca',
           'notice: Signed certificate request for working3961.example.org',
           'notice: Removing file Puppet::SSL::CertificateRequest working3961.example.org']
 
-
-step "removing the SSL scratch directory..."
-on agents, "rm -vrf #{scratch}"
-
-step "generate a certificate in #{scratch}"
-on(agents,puppet_cert('--trace', '--generate', target, options)) do
-  expect.each do |line|
-    stdout.index(line) or fail_test("missing line in output: #{line}")
+agents.each do |agent|
+  if agent['platform'].include?('windows')
+    skip_test "Test not supported on this platform"
+    next
   end
+
+  scratch = agent.tmpdir('puppet-ssl-3961')
+  options = { :confdir => scratch, :vardir => scratch }
+
+  step "removing the SSL scratch directory..."
+  on(agent, "rm -vrf #{scratch}")
+
+  step "generate a certificate in #{scratch}"
+  on(agent,puppet_cert('--trace', '--generate', target, options)) do
+    expect.each do |line|
+      stdout.index(line) or fail_test("missing line in output: #{line}")
+    end
+  end
+
+  step "verify the certificate for #{target} exists"
+  on agent, "test -f #{scratch}/ssl/certs/#{target}.pem"
+
+  step "verify the private key for #{target} exists"
+  on agent, "grep -q 'BEGIN RSA PRIVATE KEY' #{scratch}/ssl/private_keys/#{target}.pem"
 end
-
-step "verify the certificate for #{target} exists"
-on agents, "test -f #{scratch}/ssl/certs/#{target}.pem"
-
-step "verify the private key for #{target} exists"
-on agents, "grep -q 'BEGIN RSA PRIVATE KEY' #{scratch}/ssl/private_keys/#{target}.pem"
