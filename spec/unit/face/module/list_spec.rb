a7f063bf3574f13b1fba0f249ec06ed8ba171442
Matt Robinson <matt@puppetlabs.com>
2012-02-11 10:35:46 -0800

    (#12243) puppet module list explicit when empty modulepath

--- spec/unit/face/module/list_spec.rb
+++ spec/unit/face/module/list_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'puppet/face'
 require 'puppet/module_tool'
+require 'puppet_spec/modules'
 
 describe "puppet module list" do
   include PuppetSpec::Files
@@ -10,19 +11,15 @@ describe "puppet module list" do
 
     @modpath1 = File.join(dir, "modpath1")
     @modpath2 = File.join(dir, "modpath2")
+    @modulepath = "#{@modpath1}#{File::PATH_SEPARATOR}#{@modpath2}"
+    Puppet.settings[:modulepath] = @modulepath
 
     FileUtils.mkdir_p(@modpath1)
     FileUtils.mkdir_p(@modpath2)
   end
 
-  def mkmodule(name, path)
-    mod_path = File.join(path, name)
-    FileUtils.mkdir_p(mod_path)
-    mod_path
-  end
-
   it "should return an empty list per dir in path if there are no modules" do
-    Puppet.settings[:modulepath] = "#{@modpath1}#{File::PATH_SEPARATOR}#{@modpath2}"
+    Puppet.settings[:modulepath] = @modulepath
     Puppet::Face[:module, :current].list.should == {
       @modpath1 => [],
       @modpath2 => []
@@ -30,12 +27,11 @@ describe "puppet module list" do
   end
 
   it "should include modules separated by the environment's modulepath" do
-    foomod1 = mkmodule('foo', @modpath1)
-    barmod1 = mkmodule('bar', @modpath1)
-    foomod2 = mkmodule('foo', @modpath2)
+    foomod1 = PuppetSpec::Modules.create('foo', @modpath1)
+    barmod1 = PuppetSpec::Modules.create('bar', @modpath1)
+    foomod2 = PuppetSpec::Modules.create('foo', @modpath2)
 
     env = Puppet::Node::Environment.new
-    env.modulepath = [@modpath1, @modpath2]
 
     Puppet::Face[:module, :current].list.should == {
       @modpath1 => [
@@ -47,8 +43,8 @@ describe "puppet module list" do
   end
 
   it "should use the specified environment" do
-    foomod1 = mkmodule('foo', @modpath1)
-    barmod1 = mkmodule('bar', @modpath1)
+    foomod1 = PuppetSpec::Modules.create('foo', @modpath1)
+    barmod1 = PuppetSpec::Modules.create('bar', @modpath1)
 
     usedenv = Puppet::Node::Environment.new('useme')
     usedenv.modulepath = [@modpath1, @modpath2]
@@ -63,8 +59,8 @@ describe "puppet module list" do
   end
 
   it "should use the specified modulepath" do
-    foomod1 = mkmodule('foo', @modpath1)
-    barmod2 = mkmodule('bar', @modpath2)
+    foomod1 = PuppetSpec::Modules.create('foo', @modpath1)
+    barmod2 = PuppetSpec::Modules.create('bar', @modpath2)
 
     Puppet::Face[:module, :current].list(:modulepath => "#{@modpath1}#{File::PATH_SEPARATOR}#{@modpath2}").should == {
       @modpath1 => [ Puppet::Module.new('foo') ],
@@ -73,8 +69,8 @@ describe "puppet module list" do
   end
 
   it "should use the specified modulepath over the specified environment in place of the environment's default path" do
-    foomod1 = mkmodule('foo', @modpath1)
-    barmod2 = mkmodule('bar', @modpath2)
+    foomod1 = PuppetSpec::Modules.create('foo', @modpath1)
+    barmod2 = PuppetSpec::Modules.create('bar', @modpath2)
     env = Puppet::Node::Environment.new('myenv')
     env.modulepath = ['/tmp/notused']
 
@@ -99,5 +95,41 @@ describe "puppet module list" do
   end
 
   describe "when rendering" do
+    it "should explicitly state when a modulepath is empty" do
+      empty_modpath = tmpdir('empty')
+      Puppet::Face[:module, :current].list_when_rendering_console(
+        { empty_modpath => [] },
+        {:modulepath => empty_modpath}
+      ).should == <<-HEREDOC.gsub('        ', '')
+        #{empty_modpath} (No modules installed)
+      HEREDOC
+    end
+
+    it "should print dependencies as a tree" do
+      PuppetSpec::Modules.create('dependable', @modpath1, :version => '0.0.5')
+      PuppetSpec::Modules.create(
+        'other_mod',
+        @modpath1,
+        :version => '1.0.0',
+        :dependencies => [{
+          "version_requirement" => ">= 0.0.5",
+          "name"                => "matt/dependable"
+        }]
+      )
+
+      dependency_tree = Puppet::Face[:module, :current].list
+
+      output = Puppet::Face[:module, :current].list_when_rendering_console(
+        dependency_tree,
+        {:tree => true}
+      )
+
+      output.should == <<-HEREDOC.gsub('        ', '')
+        #{@modpath1}
+        other_mod (1.0.0)
+          dependable (0.0.5)
+        #{@modpath2} (No modules installed)
+      HEREDOC
+    end
   end
 end
