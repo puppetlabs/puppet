b2b922ec824d29e415e498ed3aee4d1213f1ec83
Pieter van de Bruggen <pvande@gmail.com>
2012-02-10 17:48:58 -0800

    Revert "Revert "(#12339) Improving documentation around Puppet::Util::Terminal.width.""
    This reverts commit c01571a1afb92e5e011bc396777562cdda9d0698.

--- lib/puppet/util/terminal.rb
+++ lib/puppet/util/terminal.rb
@@ -1,15 +1,17 @@
 module Puppet::Util::Terminal
-  class << self
-    # Borrowed shamelessly from Thor, who borrowed this from Rake.
-    def width
-      if Puppet.features.posix?
-        result = %x{stty size 2>/dev/null}.split[1] ||
-                 %x{tput cols 2>/dev/null}.split[0] ||
-                '80'
-      end
-      return result.to_i
-    rescue
-      return 80
+  # Attempts to determine the width of the terminal.  This is currently only
+  # supported on POSIX systems, and relies on the claims of `stty` (or `tput`).
+  #
+  # Borrowed shamelessly from Thor, who borrowed this from Rake.
+  # @return [Number] The column width of the terminal.  Defaults to 80 columns.
+  def self.width
+    if Puppet.features.posix?
+      result = %x{stty size 2>/dev/null}.split[1] ||
+               %x{tput cols 2>/dev/null}.split[0] ||
+              '80'
     end
+    return result.to_i
+  rescue
+    return 80
   end
 end
