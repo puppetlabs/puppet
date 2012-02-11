43af91e8ab28b9c368f9f5f6d06b2a115d43661a
Pieter van de Bruggen <pvande@gmail.com>
2012-02-11 13:01:59 -0800

    Basic module search tests now assert data.

--- acceptance/tests/modules/search/by_description.rb
+++ acceptance/tests/modules/search/by_description.rb
@@ -6,8 +6,20 @@ apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 
 step 'Search for a module by description'
 on master, puppet("module search dummy") do
-  # Expected result: Module with a (full) description matching /dummy/ returned
-  # TODO: Assert results.
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+NAME                  DESCRIPTION                  AUTHOR          KEYWORDS
+pmtacceptance-nginx   This is a dummy nginx mo...  @pmtacceptance  nginx
+pmtacceptance-thin    This is a dummy thin mod...  @pmtacceptance  ruby thin
+pmtacceptance-apollo  This is a dummy apollo m...  @pmtacceptance  stomp apollo
+pmtacceptance-java    This is a dummy java mod...  @pmtacceptance  java
+pmtacceptance-stdlib  This is a dummy stdlib m...  @pmtacceptance  stdlib libs
+pmtacceptance-git     This is a dummy git modu...  @pmtacceptance  git dvcs
+pmtacceptance-apache  This is a dummy apache m...  @pmtacceptance  apache php
+pmtacceptance-php     This is a dummy php modu...  @pmtacceptance  apache php
+pmtacceptance-geordi  This is a module that do...  @pmtacceptance  star trek
+STDOUT
   assert !stdout.empty?
 end
 
--- acceptance/tests/modules/search/by_keyword.rb
+++ acceptance/tests/modules/search/by_keyword.rb
@@ -4,18 +4,23 @@ step 'Stub http://forge.puppetlabs.com'
 require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 
-step 'Search for a module by partial keyword'
-on master, puppet("module search hub") do
-  # Expected result: No results
-  # TODO: Assert results.
-  assert !stdout.empty?
-end
-
 step 'Search for a module by exact keyword'
 on master, puppet("module search github") do
-  # Expected result: pmtacceptance-git
-  # TODO: Assert results.
-  assert !stdout.empty?
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+NAME               DESCRIPTION                    AUTHOR          KEYWORDS
+pmtacceptance-git  This is a dummy git module...  @pmtacceptance  git github
+STDOUT
+end
+
+step 'Search for a module by partial keyword'
+on master, puppet("module search hub") do
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+No results found for 'hub'.
+STDOUT
 end
 
 step 'Unstub http://forge.puppetlabs.com'
