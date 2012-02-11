6b77c175c63ddb4ea3612cdb2eb5f2dc32c0b8fa
Pieter van de Bruggen <pvande@gmail.com>
2012-02-11 15:58:51 -0800

    Fancy colors.  Easily accessible.

--- lib/puppet/application/module.rb
+++ lib/puppet/application/module.rb
@@ -1,3 +1,11 @@
 require 'puppet/application/face_base'
 
-class Puppet::Application::Module < Puppet::Application::FaceBase; end
+class Puppet::Application::Module < Puppet::Application::FaceBase
+  def setup
+    super
+    if self.render_as.name == :console
+      Puppet::Util::Log.close(:console)
+      Puppet::Util::Log.newdestination(:new_console)
+    end
+  end
+end
--- lib/puppet/face/module.rb
+++ lib/puppet/face/module.rb
@@ -2,6 +2,8 @@ require 'puppet/face'
 require 'puppet/module_tool'
 
 Puppet::Face.define(:module, '1.0.0') do
+  extend ColoredOutput
+
   copyright "Puppet Labs", 2011
   license   "Apache 2 license; see COPYING"
 
