a00867dae5a516dfc12e954afeb7c762e833debe
Pieter van de Bruggen <pvande@gmail.com>
2012-02-11 13:01:59 -0800

    Basic module search tests now assert data.

--- acceptance/tests/modules/search/by_module_name.rb
+++ acceptance/tests/modules/search/by_module_name.rb
@@ -4,11 +4,15 @@ step 'Stub http://forge.puppetlabs.com'
 require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 
-step 'Search for a module by partial name'
-on master, puppet("module search name") do
-  # Expected result: Module named module-names returned
-  # TODO: Assert results.
-  assert !stdout.empty?
+step 'Search for modules by partial name'
+on master, puppet("module search acceptance-a") do
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+NAME                  DESCRIPTION                  AUTHOR          KEYWORDS
+pmtacceptance-apollo  This is a dummy apollo m...  @pmtacceptance  stomp apollo
+pmtacceptance-apache  This is a dummy apache m...  @pmtacceptance  apache php
+STDOUT
 end
 
 step 'Unstub http://forge.puppetlabs.com'
