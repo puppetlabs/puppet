b662863580281ea280f20a2a1425ddee3b444f81
Pieter van de Bruggen <pvande@gmail.com>
2012-02-12 14:23:10 -0800

    Cleaning up the search acceptance tests; Forge-related behaviors are commented out.

--- acceptance/tests/modules/search/communication_error.rb
+++ acceptance/tests/modules/search/communication_error.rb
@@ -1,8 +1,20 @@
-test_name 'puppet module search should print a reasonable message on communication errors'
+begin test_name 'puppet module search should print a reasonable message on communication errors'
 
 step 'Stub http://forge.puppetlabs.com'
-require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
-apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '127.0.0.2' }"
 
-step 'Unstub http://forge.puppetlabs.com'
+step "Search against a non-existent Forge"
+on master, puppet("module search yup"), :acceptable_exit_codes => [1] do
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+STDOUT
+  assert_equal <<-STDERR, stderr
+Error: Could not connect to http://forge.puppetlabs.com
+  There was a network communications problem
+    Check your network connection and try again
+STDERR
+end
+
+ensure step 'Unstub http://forge.puppetlabs.com'
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ensure => absent }"
+end
--- acceptance/tests/modules/search/no_results.rb
+++ acceptance/tests/modules/search/no_results.rb
@@ -1,8 +1,18 @@
-test_name 'puppet module search should print a reasonable message for no results'
+begin test_name 'puppet module search should print a reasonable message for no results'
 
 step 'Stub http://forge.puppetlabs.com'
 require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 
-step 'Unstub http://forge.puppetlabs.com'
+step "Search for a module that doesn't exist"
+on master, puppet("module search module_not_appearing_in_this_forge") do
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Searching http://forge.puppetlabs.com ...
+No results found for 'module_not_appearing_in_this_forge'.
+STDOUT
+end
+
+ensure step 'Unstub http://forge.puppetlabs.com'
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ensure => absent }"
+end
