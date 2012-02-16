5a1bd0008c45b8fec041871c978426cf4960f606
Pieter van de Bruggen <pvande@gmail.com>
2012-02-16 15:39:59 -0800

    Adding to and tightening install acceptance tests.

---
+++ acceptance/tests/modules/install/ignoring_dependencies.rb
@@ -0,0 +1,22 @@
+begin test_name "puppet module install (ignoring dependencies)"
+
+step 'Setup'
+require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
+apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
+
+step "Install a module, but ignore dependencies"
+on master, "puppet module install pmtacceptance-java --ignore-dependencies" do
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+/etc/puppet/modules
+└── java (v1.7.0)
+STDOUT
+end
+on master, '[ -d /etc/puppet/modules/java ]'
+on master, '[ ! -d /etc/puppet/modules/stdlib ]'
+
+ensure step "Teardown"
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ensure => absent }"
+apply_manifest_on master, "file { '/etc/puppet/modules': recurse => true, purge => true, force => true }"
+end
