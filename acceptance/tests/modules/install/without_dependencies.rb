e9ae20968664517aa9c8b6863c802896e5e19966
Pieter van de Bruggen <pvande@gmail.com>
2012-02-16 15:39:59 -0800

    Adding to and tightening install acceptance tests.

---
+++ acceptance/tests/modules/install/without_dependencies.rb
@@ -0,0 +1,21 @@
+begin test_name "puppet module install (without dependencies)"
+
+step 'Setup'
+require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
+apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
+
+step "Install a module without dependencies"
+on master, "puppet module install pmtacceptance-nginx" do
+  assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+/etc/puppet/modules
+└── nginx (v0.0.1)
+STDOUT
+end
+on master, '[ -d /etc/puppet/modules/nginx ]'
+
+ensure step "Teardown"
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ensure => absent }"
+apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
+end
