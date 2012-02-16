dd2b89509047e1afed1c9600087a28f4b9b8bd32
Matt Robinson <matt@puppetlabs.com>
2012-02-16 11:20:37 -0800

    Fix spec ordering failure on environment
    The module_requirements method was sorting the requirements, but only as
    it went along, not for the entire list of requirements that got put in
    the data structure.  This caused intermittent test failures since
    sometimes the results would come out in a different order than the
    specs asserted.
    
    This moves the sort to happen *after* the data is all in.

--- spec/unit/node/environment_spec.rb
+++ spec/unit/node/environment_spec.rb
@@ -131,7 +131,7 @@ describe Puppet::Node::Environment do
       Puppet[:modulepath] = "#{@path_one}#{sep}#{@path_two}"
     end
 
-    it "should not return non-directories and warn" do
+    it "should not return non-directories" do
       FileTest.expects(:directory?).with(@path_one).returns true
       FileTest.expects(:directory?).with(@path_two).returns false
 
@@ -235,8 +235,17 @@ describe Puppet::Node::Environment do
               :dependencies => [{ 'name' => 'puppetlabs/bar', "version_requirement" => "3.0.0" }]
             }
           )
+          PuppetSpec::Modules.create(
+            'alpha',
+            @first,
+            :metadata => {
+              :author       => 'puppetlabs',
+              :dependencies => [{ 'name' => 'puppetlabs/bar', "version_requirement" => "~3.0.0" }]
+            }
+          )
 
           env.module_requirements.should == {
+            'puppetlabs/alpha' => [],
             'puppetlabs/foo' => [
               {
                 "name"    => "puppetlabs/bar",
@@ -245,6 +254,11 @@ describe Puppet::Node::Environment do
               }
             ],
             'puppetlabs/bar' => [
+              {
+                "name"    => "puppetlabs/alpha",
+                "version" => "9.9.9",
+                "version_requirement" => "~3.0.0"
+              },
               {
                 "name"    => "puppetlabs/baz",
                 "version" => "9.9.9",
