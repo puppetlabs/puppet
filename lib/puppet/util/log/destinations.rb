601f09168793204f7d788737f0f949dccea87d38
Matt Robinson <matt@puppetlabs.com>
2012-02-15 21:27:32 -0800

    fixing log desination specs

--- lib/puppet/util/log/destinations.rb
+++ lib/puppet/util/log/destinations.rb
@@ -85,14 +85,106 @@ Puppet::Util::Log.newdesttype :file do
   end
 end
 
-Puppet::Util::Log.newdesttype :console do
-  require 'puppet/util/colors'
-  include Puppet::Util::Colors
+module ColoredOutput
+  RED        = {:console => "\e[0;31m", :html => "color: #FFA0A0"     }
+  GREEN      = {:console => "\e[0;32m", :html => "color: #00CD00"     }
+  YELLOW     = {:console => "\e[0;33m", :html => "color: #FFFF60"     }
+  BLUE       = {:console => "\e[0;34m", :html => "color: #80A0FF"     }
+  PURPLE     = {:console => "\e[0;35m", :html => "color: #FFA500"     }
+  CYAN       = {:console => "\e[0;36m", :html => "color: #40FFFF"     }
+  WHITE      = {:console => "\e[0;37m", :html => "color: #FFFFFF"     }
+  HRED       = {:console => "\e[1;31m", :html => "color: #FFA0A0"     }
+  HGREEN     = {:console => "\e[1;32m", :html => "color: #00CD00"     }
+  HYELLOW    = {:console => "\e[1;33m", :html => "color: #FFFF60"     }
+  HBLUE      = {:console => "\e[1;34m", :html => "color: #80A0FF"     }
+  HPURPLE    = {:console => "\e[1;35m", :html => "color: #FFA500"     }
+  HCYAN      = {:console => "\e[1;36m", :html => "color: #40FFFF"     }
+  HWHITE     = {:console => "\e[1;37m", :html => "color: #FFFFFF"     }
+  BG_RED     = {:console => "\e[0;41m", :html => "background: #FFA0A0"}
+  BG_GREEN   = {:console => "\e[0;42m", :html => "background: #00CD00"}
+  BG_YELLOW  = {:console => "\e[0;43m", :html => "background: #FFFF60"}
+  BG_BLUE    = {:console => "\e[0;44m", :html => "background: #80A0FF"}
+  BG_PURPLE  = {:console => "\e[0;45m", :html => "background: #FFA500"}
+  BG_CYAN    = {:console => "\e[0;46m", :html => "background: #40FFFF"}
+  BG_WHITE   = {:console => "\e[0;47m", :html => "background: #FFFFFF"}
+  BG_HRED    = {:console => "\e[1;41m", :html => "background: #FFA0A0"}
+  BG_HGREEN  = {:console => "\e[1;42m", :html => "background: #00CD00"}
+  BG_HYELLOW = {:console => "\e[1;43m", :html => "background: #FFFF60"}
+  BG_HBLUE   = {:console => "\e[1;44m", :html => "background: #80A0FF"}
+  BG_HPURPLE = {:console => "\e[1;45m", :html => "background: #FFA500"}
+  BG_HCYAN   = {:console => "\e[1;46m", :html => "background: #40FFFF"}
+  BG_HWHITE  = {:console => "\e[1;47m", :html => "background: #FFFFFF"}
+  RESET      = {:console => "\e[0m",    :html => ""                   }
+
+  Colormap = {
+    :debug => WHITE,
+    :info => GREEN,
+    :notice => CYAN,
+    :warning => YELLOW,
+    :err => HPURPLE,
+    :alert => RED,
+    :emerg => HRED,
+    :crit => HRED,
+
+    :red        => RED,
+    :green      => GREEN,
+    :yellow     => YELLOW,
+    :blue       => BLUE,
+    :purple     => PURPLE,
+    :cyan       => CYAN,
+    :white      => WHITE,
+    :hred       => HRED,
+    :hgreen     => HGREEN,
+    :hyellow    => HYELLOW,
+    :hblue      => HBLUE,
+    :hpurple    => HPURPLE,
+    :hcyan      => HCYAN,
+    :hwhite     => HWHITE,
+    :bg_red     => BG_RED,
+    :bg_green   => BG_GREEN,
+    :bg_yellow  => BG_YELLOW,
+    :bg_blue    => BG_BLUE,
+    :bg_purple  => BG_PURPLE,
+    :bg_cyan    => BG_CYAN,
+    :bg_white   => BG_WHITE,
+    :bg_hred    => BG_HRED,
+    :bg_hgreen  => BG_HGREEN,
+    :bg_hyellow => BG_HYELLOW,
+    :bg_hblue   => BG_HBLUE,
+    :bg_hpurple => BG_HPURPLE,
+    :bg_hcyan   => BG_HCYAN,
+    :bg_hwhite  => BG_HWHITE,
+    :reset      => { :console => "\e[m", :html => "" }
+  }
+
+  def colorize(level, str)
+    case Puppet[:color]
+    when true, :ansi, "ansi", "yes"; console_color(level, str)
+    when :html, "html"; html_color(level, str)
+    else
+      str
+    end
+  end
+
+  def console_color(level, str)
+    Colormap[level][:console] +
+    str.gsub(RESET[:console], Colormap[level][:console]) +
+    RESET[:console]
+  end
+
+  def html_color(level, str)
+    %{<span style="%s">%s</span>} % [Colormap[level][:html], str]
+  end
 
   def initialize
     # Flush output immediately.
+    $stderr.sync = true
     $stdout.sync = true
   end
+end
+
+Puppet::Util::Log.newdesttype :console do
+  include ColoredOutput
 
   def handle(msg)
     if msg.source == "Puppet"
@@ -103,36 +195,17 @@ Puppet::Util::Log.newdesttype :console do
   end
 end
 
-Puppet::Util::Log.newdesttype :telly_prototype_console do
-  require 'puppet/util/colors'
-  include Puppet::Util::Colors
-
-  def initialize
-    # Flush output immediately.
-    $stderr.sync = true
-    $stdout.sync = true
-  end
+Puppet::Util::Log.newdesttype :new_console do
+  include ColoredOutput
 
   def handle(msg)
-    error_levels = {
-      :warning => 'Warning',
-      :err     => 'Error',
-      :alert   => 'Alert',
-      :emerg   => 'Emergency',
-      :crit    => 'Critical'
-    }
-
-    str = msg.respond_to?(:multiline) ? msg.multiline : msg.to_s
-
     case msg.level
-    when *error_levels.keys
-      $stderr.puts colorize(:hred, "#{error_levels[msg.level]}: #{str}")
-    when :info
-      $stdout.puts "#{colorize(:green, 'Info')}: #{str}"
-    when :debug
-      $stdout.puts "#{colorize(:cyan, 'Debug')}: #{str}"
+    when :err
+        $stderr.puts colorize(:hred, "Error: #{msg}")
+    when :warning
+      $stderr.puts colorize(:hred, "Warning: #{msg}")
     else
-      $stdout.puts str
+      $stdout.puts msg
     end
   end
 end
