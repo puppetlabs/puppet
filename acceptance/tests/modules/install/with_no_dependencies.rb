00d72915aaea6d5295e975cb6d3607a0cbbf19ed
Pieter van de Bruggen <pvande@gmail.com>
2012-02-17 15:27:52 -0800

    Renaming test for clarity.

--- acceptance/tests/modules/install/with_no_dependencies.rb
+++ acceptance/tests/modules/install/with_no_dependencies.rb
@@ -1,11 +1,11 @@
-begin test_name "puppet module install (without dependencies)"
+begin test_name "puppet module install (with no dependencies)"
 
 step 'Setup'
 require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
 apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
 
-step "Install a module without dependencies"
+step "Install a module with no dependencies"
 on master, puppet("module install pmtacceptance-nginx") do
   assert_equal '', stderr
 #  assert_equal <<-STDOUT, stdout
