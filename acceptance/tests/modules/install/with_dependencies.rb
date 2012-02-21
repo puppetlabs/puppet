cbd3491aafca7c4e48584bf3f15846454d2bd9f1
Matt Robinson <matt@puppetlabs.com>
2012-02-21 15:36:44 -0800

    A start to the install acceptance tests
    These should all be passing, but don't assert STDOUT for the install
    command.  Also, it's using the `puppet module list` command to assert
    what got installed, so the output format on that will have to change
    once Kelsey's code merges in.

---
+++ acceptance/tests/modules/install/with_dependencies.rb
@@ -0,0 +1,26 @@
+begin test_name "puppet module install (without dependencies)"
+
+step 'Setup'
+require 'resolv'; ip = Resolv.getaddress('forge-dev.puppetlabs.com')
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
+apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
+
+step "Install a module with dependencies"
+on master, "puppet module install pmtacceptance-apollo" do
+  assert_equal '', stderr
+end
+
+on master, 'puppet module list' do
+  assert_equal <<-STDOUT, stdout
+/etc/puppet/modules
+pmtacceptance-apollo (0.0.1)
+pmtacceptance-java (1.7.1)
+pmtacceptance-stdlib (1.0.0)
+/usr/share/puppet/modules (No modules installed)
+STDOUT
+end
+
+ensure step "Teardown"
+apply_manifest_on master, "host { 'forge.puppetlabs.com': ensure => absent }"
+apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
+end
