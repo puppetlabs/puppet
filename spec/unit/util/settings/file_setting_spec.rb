puppetlabs/puppet

commit 97c043f440115fcfd9d3ddaad59af5c8da875329

Fix path handling
*) Use File.expand_path as indicator for being an absolute path
*) Use basename instead of parsing the path manually

lib/puppet/util/settings/file_setting.rb
@@ -49,9 +49,9 @@ class Puppet::Util::Settings::FileSetting < Puppet::Util::Settings::Setting
     # the variable 'dir', or adding a slash at the end.
     def munge(value)
         # If it's not a fully qualified path...
-        if value.is_a?(String) and value !~ /^\$/ and value !~ /^\// and value != 'false'
+        if value.is_a?(String) and value !~ /^\$/ and value != 'false'
             # Make it one
-            value = File.join(Dir.getwd, value)
+            value = File.expand_path(value)
         end
         if value.to_s =~ /\/$/
             @type = :directory
@@ -83,7 +83,7 @@ class Puppet::Util::Settings::FileSetting < Puppet::Util::Settings::Setting
         return nil unless path.is_a?(String)
 
         # Make sure the paths are fully qualified.
-        path = File.join(Dir.getwd, path) unless path =~ /^\//
+        path = File.expand_path(path)
 
         return nil unless type == :directory or create_files? or File.exist?(path)
         return nil if path =~ /^\/dev/

lib/puppet/type/file.rb
@@ -29,7 +29,8 @@ Puppet::Type.newtype(:file) do
         isnamevar
 
         validate do |value|
-            unless value =~ /^#{File::SEPARATOR}/
+            # use underlying platform's convention to check for relative paths
+            unless File.expand_path(value) == value
                 fail Puppet::Error,"File paths must be fully qualified, not '#{value}'"
             end
         end

lib/puppet/util/command_line.rb
@@ -62,7 +62,7 @@ module Puppet
             private
 
             def subcommand_and_args( zero, argv, stdin )
-                zero = zero.gsub(/.*#{File::SEPARATOR}/,'').sub(/\.rb$/, '')
+                zero = File.basename(zero, '.rb')
 
                 if zero == 'puppet'
                     case argv.first

commit c4b3c8470698bf4dbaf0e8ec1ff8da63407dc0e6

(#9491) Preserve trailing slashes in FileSettings
We use the trailing slash in a FileSetting to determine whether it's a
directory. The change in 97c043f4 caused trailing slashes to be stripped.
This caused errors like:

change from directory to file failed: Could not set 'file on ensure: Is a directory - /var/lib/puppet/facts

lib/puppet/util/settings/file_setting.rb
@@ -50,12 +50,10 @@ class Puppet::Util::Settings::FileSetting < Puppet::Util::Settings::Setting
   def munge(value)
     # If it's not a fully qualified path...
     if value.is_a?(String) and value !~ /^\$/ and value != 'false'
-      # Make it one
+      # Preserve trailing slash (stripped by expand_path)
+      isdir = value =~ /\/$/
       value = File.expand_path(value)
-    end
-    if value.to_s =~ /\/$/
-      @type = :directory
-      return value.sub(/\/$/, '')
+      value += '/' if isdir
     end
     value
   end

spec/unit/util/settings/file_setting_spec.rb
@@ -286,5 +286,14 @@ describe Puppet::Util::Settings::FileSetting do
       @file.to_resource[:links].should == :follow
     end
   end
+
+  describe "when munging a filename" do
+    it "should preserve trailing slashes" do
+      settings = mock 'settings'
+      file = Puppet::Util::Settings::FileSetting.new(:settings => settings, :desc => "eh", :name => :mydir, :section => "mysect")
+      path = @basepath + '/'
+      file.munge(path).should == path
+    end
+  end
 end
 