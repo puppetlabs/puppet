3d9b1cf772d2f6ad26c8c124ea636bccf346c2c3
Pieter van de Bruggen <pvande@gmail.com>
2012-03-02 13:55:29 -0800

    Module Install acceptance test fixes.

--- acceptance/tests/modules/install/with_dependencies.rb
+++ acceptance/tests/modules/install/with_dependencies.rb
@@ -6,17 +6,17 @@ apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
 
 step "Install a module with dependencies"
-on master, "puppet module install pmtacceptance-apollo" do
+on master, puppet("module install pmtacceptance-apollo") do
   assert_equal '', stderr
 end
 
-on master, 'puppet module list' do
+on master, puppet('module list') do
   assert_equal <<-STDOUT, stdout
 /etc/puppet/modules
-pmtacceptance-apollo (0.0.1)
-pmtacceptance-java (1.7.1)
-pmtacceptance-stdlib (1.0.0)
-/usr/share/puppet/modules (No modules installed)
+├── pmtacceptance-apollo (v0.0.1)
+├── pmtacceptance-java (v1.7.1)
+└── pmtacceptance-stdlib (v1.0.0)
+/usr/share/puppet/modules (no modules installed)
 STDOUT
 end
 
--- acceptance/tests/modules/install/with_unnecessary_upgrade.rb
+++ acceptance/tests/modules/install/with_unnecessary_upgrade.rb
@@ -6,32 +6,32 @@ apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
 
 step "Install an older module version"
-on master, "puppet module install pmtacceptance-java --version 1.7.0" do
+on master, puppet("module install pmtacceptance-java --version 1.7.0") do
   assert_equal '', stderr
 end
 
-on master, 'puppet module list' do
+on master, puppet('module list') do
   assert_equal <<-STDOUT, stdout
 /etc/puppet/modules
-pmtacceptance-java (1.7.0)
-pmtacceptance-stdlib (1.0.0)
-/usr/share/puppet/modules (No modules installed)
+├── pmtacceptance-java (v1.7.0)
+└── pmtacceptance-stdlib (v1.0.0)
+/usr/share/puppet/modules (no modules installed)
 STDOUT
 end
 
 
 step "Install a module that depends on a dependency that could be upgraded, but already satisfies constraints"
-on master, "puppet module install pmtacceptance-apollo" do
+on master, puppet("module install pmtacceptance-apollo") do
   assert_equal '', stderr
 end
 
-on master, 'puppet module list' do
+on master, puppet('module list') do
   assert_equal <<-STDOUT, stdout
 /etc/puppet/modules
-pmtacceptance-apollo (0.0.1)
-pmtacceptance-java (1.7.0)
-pmtacceptance-stdlib (1.0.0)
-/usr/share/puppet/modules (No modules installed)
+├── pmtacceptance-apollo (v0.0.1)
+├── pmtacceptance-java (v1.7.0)
+└── pmtacceptance-stdlib (v1.0.0)
+/usr/share/puppet/modules (no modules installed)
 STDOUT
 end
 
