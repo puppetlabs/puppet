127220465c15b845ac5f51d2e6a1c545bfdca2b5
Pieter van de Bruggen <pvande@gmail.com>
2012-03-02 15:59:27 -0800

    Misc. acceptance test fixes.

--- acceptance/tests/modules/install/with_cycles.rb
+++ acceptance/tests/modules/install/with_cycles.rb
@@ -6,16 +6,23 @@ apply_manifest_on master, "host { 'forge.puppetlabs.com': ip => '#{ip}' }"
 apply_manifest_on master, "file { ['/etc/puppet/modules', '/usr/share/puppet/modules']: recurse => true, purge => true, force => true }"
 
 step "Install a module with cycles"
-on master, puppet("module install pmtacceptance-php") do
+on master, puppet("module install pmtacceptance-php --version 0.0.1") do
   assert_equal '', stderr
+  assert_equal <<-STDOUT, stdout
+Preparing to install into /etc/puppet/modules ...
+Downloading from http://forge.puppetlabs.com ...
+Installing -- do not interrupt ...
+/etc/puppet/modules
+└─┬ pmtacceptance-php (v0.0.1)
+  └── pmtacceptance-apache (v0.0.1)
+STDOUT
 end
 
 on master, puppet('module list') do
   assert_equal <<-STDOUT, stdout
 /etc/puppet/modules
 ├── pmtacceptance-apache (v0.0.1)
-├── pmtacceptance-php (v0.0.2)
-└── pmtacceptance-stdlib (v0.0.1)
+└── pmtacceptance-php (v0.0.1)
 /usr/share/puppet/modules (no modules installed)
 STDOUT
 end
