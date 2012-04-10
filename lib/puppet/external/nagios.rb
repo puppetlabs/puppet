89a485bbabf5b65bc58b358a6d31945f53e6bb61
Daniel Pittman <daniel@puppetlabs.com>
2012-04-10 09:30:20 -0700

    Bring Nagios module back into the core.
    This reverts commit ec1bcdd537d7d8d180c36d16c46e8daa257b7bcc.
    
    We didn't actually have internal agreement that this should be removed to a
    module, despite my belief, so this merge shouldn't have gone ahead.

---
+++ lib/puppet/external/nagios.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby -w
+
+#--------------------
+# A script to retrieve hosts from ldap and create an importable
+# cfservd file from them
+
+require 'digest/md5'
+#require 'ldap'
+require 'puppet/external/nagios/parser.rb'
+require 'puppet/external/nagios/base.rb'
+
+module Nagios
+  NAGIOSVERSION = '1.1'
+  # yay colors
+  PINK = "[0;31m"
+  GREEN = "[0;32m"
+  YELLOW = "[0;33m"
+  SLATE = "[0;34m"
+  ORANGE = "[0;35m"
+  BLUE = "[0;36m"
+  NOCOLOR = "[0m"
+  RESET = "[0m"
+
+  def self.version
+    NAGIOSVERSION
+  end
+
+  class Config
+    def Config.import(config)
+
+      text = String.new
+
+      File.open(config) { |file|
+        file.each { |line|
+          text += line
+        }
+      }
+      parser = Nagios::Parser.new
+      parser.parse(text)
+    end
+
+    def Config.each
+      Nagios::Object.objects.each { |object|
+        yield object
+      }
+    end
+  end
+end
---
+++ lib/puppet/type/nagios_host.rb
@@ -0,0 +1,3 @@
+require 'puppet/util/nagios_maker'
+
+Puppet::Util::NagiosMaker.create_nagios_type :host
