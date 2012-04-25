puppetlabs/puppet

commit 8b8179470f7a0a9a60c42fa253ee52ef511793e7

(#13966) Remove Puppet[:name] option
Replace Puppet[:name] with run_mode.
Set application default pidfile for puppet queue.
Make application defaults trump defaults.rb for default comment inside
--genconfig.

lib/puppet/defaults.rb
@@ -678,7 +678,7 @@ EOT
       },
       :pidfile => {
           :type => :file,
-          :default  => "$rundir/$name.pid",
+          :default  => "$rundir/#{Puppet.run_mode.name}.pid",
           :desc     => "The pid file",
       },
       :bindaddress => {

lib/puppet/application.rb
@@ -267,7 +267,6 @@ class Application
 
   def app_defaults()
     {
-        :name     => name,
         :run_mode => self.class.run_mode.name,
         :confdir  => self.class.run_mode.conf_dir,
         :vardir   => self.class.run_mode.var_dir,

lib/puppet/application/queue.rb
@@ -4,6 +4,10 @@ require 'puppet/util'
 class Puppet::Application::Queue < Puppet::Application
 
   attr_accessor :daemon
+  
+  def app_defaults()
+    super.merge( :pidfile => "$rundir/queue.pid" )
+  end
 
   def preinit
     require 'puppet/daemon'

lib/puppet/daemon.rb
@@ -8,7 +8,7 @@ class Puppet::Daemon
   attr_accessor :agent, :server, :argv
 
   def daemonname
-    Puppet[:name]
+    Puppet.run_mode.name
   end
 
   # Put the daemon into the background.
@@ -38,9 +38,9 @@ class Puppet::Daemon
       Puppet::Util::Log.reopen
       Puppet.debug("Finished closing streams for daemon mode")
     rescue => detail
-      Puppet.err "Could not start #{Puppet[:name]}: #{detail}"
+      Puppet.err "Could not start #{Puppet.run_mode.name}: #{detail}"
       Puppet::Util::replace_file("/tmp/daemonout", 0644) do |f|
-        f.puts "Could not start #{Puppet[:name]}: #{detail}"
+        f.puts "Could not start #{Puppet.run_mode.name}: #{detail}"
       end
       exit(12)
     end
@@ -54,7 +54,7 @@ class Puppet::Daemon
   # Create a pidfile for our daemon, so we can be stopped and others
   # don't try to start.
   def create_pidfile
-    Puppet::Util.synchronize_on(Puppet[:name],Sync::EX) do
+    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
       raise "Could not create PID file: #{pidfile}" unless Puppet::Util::Pidlock.new(pidfile).lock
     end
   end
@@ -84,7 +84,7 @@ class Puppet::Daemon
 
   # Remove the pid file for our daemon.
   def remove_pidfile
-    Puppet::Util.synchronize_on(Puppet[:name],Sync::EX) do
+    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
       Puppet::Util::Pidlock.new(pidfile).unlock
     end
   end

lib/puppet/network/server.rb
@@ -29,14 +29,14 @@ class Puppet::Network::Server
   # Create a pidfile for our daemon, so we can be stopped and others
   # don't try to start.
   def create_pidfile
-    Puppet::Util.synchronize_on(Puppet[:name],Sync::EX) do
+    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
       raise "Could not create PID file: #{pidfile}" unless Puppet::Util::Pidlock.new(pidfile).lock
     end
   end
 
   # Remove the pid file for our daemon.
   def remove_pidfile
-    Puppet::Util.synchronize_on(Puppet[:name],Sync::EX) do
+    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
       Puppet::Util::Pidlock.new(pidfile).unlock
     end
   end

lib/puppet/transaction.rb
@@ -225,7 +225,7 @@ class Puppet::Transaction
 
   # Should we ignore tags?
   def ignore_tags?
-    ! (@catalog.host_config? or Puppet[:name] == "puppet")
+    ! @catalog.host_config? 
   end
 
   # this should only be called by a Puppet::Type::Component resource now

lib/puppet/util/log/destinations.rb
@@ -9,8 +9,7 @@ Puppet::Util::Log.newdesttype :syslog do
 
   def initialize
     Syslog.close if Syslog.opened?
-    name = Puppet[:name]
-    name = "puppet-#{name}" unless name =~ /puppet/
+    name = "puppet-#{Puppet.run_mode.name}"
 
     options = Syslog::LOG_PID | Syslog::LOG_NDELAY
 

lib/puppet/util/settings.rb
@@ -18,10 +18,10 @@ class Puppet::Util::Settings
   attr_accessor :files
   attr_reader :timer
 
-  ReadOnly = [:run_mode, :name]
+  ReadOnly = [:run_mode]
 
   # These are the settings that every app is required to specify; there are reasonable defaults defined in application.rb.
-  REQUIRED_APP_SETTINGS = [:name, :run_mode, :logdir, :confdir, :vardir]
+  REQUIRED_APP_SETTINGS = [:run_mode, :logdir, :confdir, :vardir]
 
   def self.default_global_config_dir()
     Puppet.features.microsoft_windows? ? File.join(Dir::COMMON_APPDATA, "PuppetLabs", "puppet", "etc") : "/etc/puppet"
@@ -102,6 +102,8 @@ class Puppet::Util::Settings
     #  and we want to retain this cli values.
     @used = [] if clear_cli
 
+    @app_defaults_initialized = false if clear_application_defaults
+
     @cache.clear
   end
   private :unsafe_clear
@@ -127,7 +129,6 @@ class Puppet::Util::Settings
     app_defaults.each do |key, value|
       set_value(key, value, :application_defaults)
     end
-
 
     @app_defaults_initialized = true
   end
@@ -771,7 +772,7 @@ class Puppet::Util::Settings
 
   # Convert our list of config settings into a configuration file.
   def to_config
-    str = %{The configuration file for #{Puppet[:name]}.  Note that this file
+    str = %{The configuration file for #{Puppet.run_mode.name}.  Note that this file
 is likely to have unused configuration parameters in it; any parameter that's
 valid anywhere in Puppet can be in any config file, even if it's not used.
 
@@ -866,7 +867,7 @@ if @config.include?(:run_mode)
 
   # Find the correct value using our search path.  Optionally accept an environment
   # in which to search before the other configuration sections.
-  def value(param, environment = nil)
+  def value(param, environment = nil, bypass_interpolation = false)
     param = param.to_sym
     environment &&= environment.to_sym
 
@@ -885,6 +886,7 @@ if @config.include?(:run_mode)
 
     val = uninterpolated_value(param, environment)
 
+    return val if bypass_interpolation
     if param == :code
       # if we interpolate code, all hell breaks loose.
       return val

commit bbda9d21ce193efad2fc0c59d71b482bac0aeede

(#13966) Fix default pidfile race condition
Commit 8b81794 introduced a race condition for the pidfile that
incorrectly defaulted all pidfiles to user.pid because of when in load
time defaults.rb was read.  Run_mode bootstraps as user and then
switches to [master|agent] if necessary. This change modifies the
default pidfile value to interpolate run_mode at time of pidfile use
rather than bootstrapped run_mode.  This commit restores proper
functionality for the pidfile names.

lib/puppet/defaults.rb
@@ -678,7 +678,7 @@ EOT
       },
       :pidfile => {
           :type => :file,
-          :default  => "$rundir/#{Puppet.run_mode.name}.pid",
+          :default  => "$rundir/${run_mode}.pid",
           :desc     => "The pid file",
       },
       :bindaddress => {