61e7d3cb3ad4f72c28b02cc3e71702f9224e2b89
Stefan Schulte <stefan.schulte@taunusstein.net>
2012-04-25 23:11:04 +0200

    maint: Fix pkg_spec test
    The pkg_spec test meant to compare different hashes that were created
    like
    
        Hash.new(:key1 => val1, :key2 => val2)
    
    At least on ruby 1.9 this will always return an empty hash so the spec
    tests didn't test correctness at all.
    
    The fix uses just {:key1 => val1, :key2 => val2} to create the different
    hashes so the comparsion does make sense again.

--- spec/unit/provider/package/pkg_spec.rb
+++ spec/unit/provider/package/pkg_spec.rb
@@ -29,20 +29,20 @@ describe Puppet::Type.type(:package).provider(:pkg) do
   describe "when calling instances" do
     it "should correctly parse lines with preferred publisher" do
       described_class.expects(:pkg).with(:list,'-H').returns File.read(my_fixture('simple'))
-      @instances = described_class.instances.map { |p| Hash.new(:name => p.get(:name), :ensure => p.get(:ensure)) }
+      @instances = described_class.instances.map { |p| {:name => p.get(:name), :ensure => p.get(:ensure)} }
       @instances.size.should == 4
-      @instances[0].should == Hash.new(:name => 'SUNPython', :ensure => :present)
-      @instances[1].should == Hash.new(:name => 'SUNWbind', :ensure => :present)
-      @instances[2].should == Hash.new(:name => 'SUNWdistro-license-copyright', :ensure => :present)
-      @instances[3].should == Hash.new(:name => 'SUNWfppd', :ensure => :present)
+      @instances[0].should == {:name => 'SUNPython', :ensure => :present}
+      @instances[1].should == {:name => 'SUNWbind', :ensure => :present}
+      @instances[2].should == {:name => 'SUNWdistro-license-copyright', :ensure => :present}
+      @instances[3].should == {:name => 'SUNWfppd', :ensure => :present}
     end
 
     it "should correctly parse lines with non preferred publisher" do
       described_class.expects(:pkg).with(:list,'-H').returns File.read(my_fixture('publisher'))
-      @instances = described_class.instances.map { |p| Hash.new(:name => p.get(:name), :ensure => p.get(:ensure)) }
+      @instances = described_class.instances.map { |p| {:name => p.get(:name), :ensure => p.get(:ensure)} }
       @instances.size.should == 2
-      @instances[0].should == Hash.new(:name => 'SUNWpcre', :ensure => :present)
-      @instances[1].should == Hash.new(:name => 'service/network/ssh', :ensure => :present)
+      @instances[0].should == {:name => 'SUNWpcre', :ensure => :present}
+      @instances[1].should == {:name => 'service/network/ssh', :ensure => :present}
     end
 
     it "should warn about incorrect lines" do
