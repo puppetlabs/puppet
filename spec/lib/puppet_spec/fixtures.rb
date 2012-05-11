3b144bf7a093510436e215f92a47ab35c2236645
Patrick Carlisle <patrick@puppetlabs.com>
2012-05-11 13:47:16 -0700

    Revert "maint: use relative paths to fixtures."
    This reverts commit 7ce9e17cf1077604accc1a5cfe90ad7b1bb99a55.
    The change to using Pathname in fixtures is not compatible with Windows.

--- spec/lib/puppet_spec/fixtures.rb
+++ spec/lib/puppet_spec/fixtures.rb
@@ -1,5 +1,3 @@
-require 'pathname'
-
 module PuppetSpec::Fixtures
   def fixtures(*rest)
     File.join(PuppetSpec::FIXTURE_DIR, *rest)
@@ -13,23 +11,18 @@ module PuppetSpec::Fixtures
     fail "sorry, I couldn't work out your path from the caller stack!"
   end
   def my_fixture(name)
-    file = (Pathname(my_fixture_dir) + name).relative_path_from(Pathname.getwd).to_s
-
+    file = File.join(my_fixture_dir, name)
     unless File.readable? file then
       fail Puppet::DevError, "fixture '#{name}' for #{my_fixture_dir} is not readable"
     end
     return file
   end
   def my_fixtures(glob = '*', flags = 0)
-    pwd   = Pathname.getwd
-    files = Pathname.glob(Pathname(my_fixture_dir) + glob, flags).
-      map {|f| f.relative_path_from(pwd).to_s }
-
+    files = Dir.glob(File.join(my_fixture_dir, glob), flags)
     unless files.length > 0 then
       fail Puppet::DevError, "fixture '#{glob}' for #{my_fixture_dir} had no files!"
     end
-
-    block_given? and files.each {|file| yield file }
+    block_given? and files.each do |file| yield file end
     files
   end
 end
