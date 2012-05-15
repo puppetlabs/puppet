2ec9b74df24d6764fa8bd3c8287efd6c7a4eaae6
Jeff Weiss <jeff.weiss@puppetlabs.com>
2012-05-15 15:26:30 -0700

    (maint) Make Puppet have hard dependency on Facter 2.0
    Puppet 3.0 requires Facter 2.0. Make the dependency hard and have Puppet
    fail with a useful error message if Facter doesn't exist or is the wrong
    version.

--- lib/puppet.rb
+++ lib/puppet.rb
@@ -6,7 +6,6 @@ end
 
 # see the bottom of the file for further inclusions
 require 'singleton'
-require 'facter'
 require 'puppet/error'
 require 'puppet/util'
 require 'puppet/util/autoload'
@@ -149,6 +148,10 @@ module Puppet
   def self.newtype(name, options = {}, &block)
     Puppet::Type.newtype(name, options, &block)
   end
+
+  # We don't want to continue if Facter is not around, or isn't feature
+  # compliant
+  exit(1) unless Puppet.features.facter?
 end
 
 # This feels weird to me; I would really like for us to get to a state where there is never a "require" statement
