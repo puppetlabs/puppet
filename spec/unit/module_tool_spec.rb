e9e1a21d0f9a6efdf0b283371e40b018fd1380c4
Chris Price <chris@puppetlabs.com>
2012-05-15 10:39:43 -0700

    Fix failing spec tests for module tool

--- spec/unit/module_tool_spec.rb
+++ spec/unit/module_tool_spec.rb
@@ -118,28 +118,36 @@ TREE
     [:environment, :modulepath].each do |value|
       describe "if #{value} is part of options" do
         let (:options) { {} }
+
         before(:each) do
           options[value] = setting[value]
           Puppet[value] = "bar"
         end
+
         it "should set Puppet[#{value}] to the options[#{value}]" do
           subject.set_option_defaults options
           Puppet[value].should == options[value]
         end
+
         it "should not override options[#{value}]" do
           subject.set_option_defaults options
           options[value].should == setting[value]
         end
+
       end
+
       describe "if #{value} is not part of options" do
         let (:options) { {} }
+
         before(:each) do
           Puppet[value] = setting[value]
         end
+
         it "should populate options[#{value}] with the value of Puppet[#{value}]" do
           subject.set_option_defaults options
           Puppet[value].should == options[value]
         end
+
         it "should not override Puppet[#{value}]" do
           subject.set_option_defaults options
           Puppet[value].should == setting[value]
@@ -149,36 +157,47 @@ TREE
 
     describe ':target_dir' do
       let (:sep) { File::PATH_SEPARATOR }
-      let (:my_fake_path) { "/my/fake/dir#{sep}/my/other/dir"}
+
+      let (:my_fake_path) {
+          ["/my/fake/dir", "/my/other/dir"].collect { |dir| make_absolute(dir) } .join(sep)
+      }
       let (:options) { {:modulepath => my_fake_path}}
+
       describe "when not specified" do
+
         it "should set options[:target_dir]" do
           subject.set_option_defaults options
           options[:target_dir].should_not be_nil
         end
+
         it "should be the first path of options[:modulepath]" do
           subject.set_option_defaults options
           options[:target_dir].should == my_fake_path.split(sep).first
         end
       end
+
       describe "when specified" do
-        let (:my_target_dir) { "/foo/bar" }
+        let (:my_target_dir) { make_absolute("/foo/bar") }
         before(:each) do
           options[:target_dir] = my_target_dir
         end
+
         it "should not be overridden" do
           subject.set_option_defaults options
           options[:target_dir].should == my_target_dir
         end
+
         it "should be prepended to options[:modulepath]" do
           subject.set_option_defaults options
           options[:modulepath].split(sep).first.should == my_target_dir
         end
+
         it "should leave the remainder of options[:modulepath] untouched" do
           subject.set_option_defaults options
           options[:modulepath].split(sep).drop(1).join(sep).should == my_fake_path
         end
       end
+
     end
   end
 end
