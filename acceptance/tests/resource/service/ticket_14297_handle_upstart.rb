466ab492841d1c6a451ad9ce1ef3128bca8267d5
Matthaus Litteken <matthaus@puppetlabs.com>
2012-05-25 10:30:41 -0700

    (#14297) Define agent in upstart acceptance test
    agent wasn't being passed into the helper function in the upstart acceptance
    test, so this adds the agent variable to the method and passes it in when
    called.

--- acceptance/tests/resource/service/ticket_14297_handle_upstart.rb
+++ acceptance/tests/resource/service/ticket_14297_handle_upstart.rb
@@ -6,7 +6,7 @@ confine :to, :platform => 'ubuntu'
 # pick any ubuntu agent
 agent = agents.first
 
-def check_service_for(pkg, type)
+def check_service_for(pkg, type, agent)
   if pkg == "apache2"
     if type == "stop"
       on agent, "service #{pkg} status", :acceptable_exit_codes => [1,2,3]
@@ -28,19 +28,19 @@ end
   on agent, "service #{pkg} start", :acceptable_exit_codes => [0,1]
 
   step "Check that status for running #{pkg}"
-  check_service_for(pkg, "start")
+  check_service_for(pkg, "start", agent)
 
   step "Stop #{pkg} with `puppet resource'"
   on agent, puppet_resource("service #{pkg} ensure=stopped")
 
   step "Check that status for stopped #{pkg}"
-  check_service_for(pkg, "stop")
+  check_service_for(pkg, "stop", agent)
 
   step "Start #{pkg} with `puppet resource'"
   on agent, puppet_resource("service #{pkg} ensure=running")
 
   step "Check that status for started #{pkg}"
-  check_service_for(pkg, "start")
+  check_service_for(pkg, "start", agent)
 
   on agent, puppet_resource("package #{pkg} ensure=absent")
 end
