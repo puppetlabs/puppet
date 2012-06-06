d76a9d87547a12a41c4ac4fefe234f996e85dab6
Patrick Carlisle <patrick@puppetlabs.com>
2012-06-06 11:55:54 -0700

    (#14841) Add test for hiera_hash parser function
    This required pulling in the puppetlabs_spec_helper, since the parser
    functions depend on puppet being somewhat configured.

--- spec/unit/parser/functions/hiera_hash_spec.rb
+++ spec/unit/parser/functions/hiera_hash_spec.rb
@@ -3,9 +3,20 @@ require 'hiera'
 require 'spec_helper'
 
 describe 'Puppet::Parser::Functions#hiera_hash' do
-  it 'should require a key argument' do
+  before do
     Puppet::Parser::Functions.function(:hiera_hash)
     @scope = Puppet::Parser::Scope.new
+    configfile = File.join(File.dirname(Puppet.settings[:config]), "hiera.yaml")
+    File.stubs(:exist?).with(configfile).returns true
+    YAML.stubs(:load_file).with(configfile).returns(Hash.new)
+  end
+
+  it 'should require a key argument' do
     expect { @scope.function_hiera_hash([]) }.should raise_error(Puppet::ParseError)
+  end
+
+  it 'should raise a useful error when nil is returned' do
+    Hiera.any_instance.expects(:lookup).returns(nil)
+    expect { @scope.function_hiera_hash("badkey") }.should raise_error(Puppet::ParseError, /Could not find data item badkey/ )
   end
 end
