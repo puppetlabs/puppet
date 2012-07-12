ab521c639d0c0d6ce225ad7e1e7ab6cf0e62e63e
Daniel Pittman <daniel@puppetlabs.com>
2012-07-12 16:00:07 -0700

    zaml: string output time reduced by fifty percent
    This changes the implementation of string output in the ZAML encoder.  On my
    test that delivers around a fifty percent reduction in output time, from 4.11
    to 1.89 seconds on my test of ~ 35,000 output strings.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/util/zaml.rb
+++ lib/puppet/util/zaml.rb
@@ -227,51 +227,44 @@ class Exception
 end
 
 class String
-  ZAML_ESCAPES = %w{\x00 \x01 \x02 \x03 \x04 \x05 \x06 \a \x08 \t \n \v \f \r \x0e \x0f \x10 \x11 \x12 \x13 \x14 \x15 \x16 \x17 \x18 \x19 \x1a \e \x1c \x1d \x1e \x1f }
-  def escaped_for_zaml
-    # JJM (Note the trailing dots to construct a multi-line method chain.) This
-    # code is meant to escape all bytes which are not ASCII-8BIT printable
-    # characters.  Multi-byte unicode characters are handled just fine because
-    # each byte of the character results in an escaped string emitted to the
-    # YAML stream.  When the YAML is de-serialized back into a String the bytes
-    # will be reconstructed properly into the unicode character.
-    self.to_ascii8bit.gsub( /\x5C/n, "\\\\\\" ).  # Demi-kludge for Maglev/rubinius; the regexp should be /\\/ but parsetree chokes on that.
-    gsub( /"/n, "\\\"" ).
-    gsub( /([\x00-\x1F])/n ) { |x| ZAML_ESCAPES[ x.unpack("C")[0] ] }.
-    gsub( /([\x80-\xFF])/n ) { |x| "\\x#{x.unpack("C")[0].to_s(16)}" }
-  end
+  ZAML_ESCAPED_CONTROL_CODES = %w{
+    \x00 \x01 \x02 \x03 \x04 \x05 \x06 \a   \x08 \t
+    \n   \v   \f   \r   \x0e \x0f \x10 \x11 \x12 \x13
+    \x14 \x15 \x16 \x17 \x18 \x19 \x1a \e   \x1c \x1d
+    \x1e \x1f
+  }
+
   def to_zaml(z)
-    z.first_time_only(self) {
-      hex_num = '0x[a-f\d]+'
-      float = '\d+\.?\d*'
-      num = "[-+]?(?:#{float}|#{hex_num})"
+    z.first_time_only(self) do
       case
-        when self == ''
-          z.emit('""')
-        # when self =~ /[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\xFF]/
-        #   z.emit("!binary |\n")
-        #   z.emit([self].pack("m*"))
-        when (
-          (self =~ /\A(true|false|yes|no|on|null|off|#{num}(:#{num})*|!|=|~)$/i) or
-          (self =~ /\A\n* /) or
-          (self =~ /[\s:]$/) or
-          (self =~ /^[>|][-+\d]*\s/i) or
-          (self[-1..-1] =~ /\s/) or
-          # This regular expression assumes the string is a byte sequence.
-          # It does not concern itself with characters so we convert the string
-          # to ASCII-8BIT for Ruby 1.9 to match up encodings.
-          (self.to_ascii8bit=~ /[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\xFF]/n) or
-          (self =~ /[,\[\]\{\}\r\t]|:\s|\s#/) or
-          (self =~ /\A([-:?!#&*'"]|<<|%.+:.)/)
-          )
-          z.emit("\"#{escaped_for_zaml}\"")
-        when self =~ /\n/
-          if self[-1..-1] == "\n" then z.emit('|+') else z.emit('|-') end
-          z.nested { split("\n",-1).each { |line| z.nl; z.emit(line.chomp("\n")) } }
-        else
-          z.emit(self)
+      when self == ''
+        z.emit('""')
+      when self =~ /\n/
+        # embedded newline, more complex output.
+        if self[-1..-1] == "\n" then z.emit('|+') else z.emit('|-') end
+        z.nested { split("\n",-1).each { |line| z.nl; z.emit(line.chomp("\n")) } }
+      when ((self =~ /^[a-zA-Z\/][-\[\]_\/.:a-zA-Z0-9]*$/) and
+          (self !~ /^(?:true|false|yes|no|on|null|off)$/i))
+        # simple string, just emit.
+        z.emit(self)
+      else
+        # JJM (Note the trailing dots to construct a multi-line method chain.) This
+        # code is meant to escape all bytes which are not ASCII-8BIT printable
+        # characters.  Multi-byte unicode characters are handled just fine because
+        # each byte of the character results in an escaped string emitted to the
+        # YAML stream.  When the YAML is de-serialized back into a String the bytes
+        # will be reconstructed properly into the unicode character.
+        #
+        # Using gsub is as efficient as any other approach. --daniel 2012-07-12
+        escaped = to_ascii8bit.
+          gsub("\\", "\\\\\\" ).
+          gsub('"', '\"').
+          gsub(/([\x00-\x1F])/n) {|x| ZAML_ESCAPES[ x[0].ord ] }.
+          gsub(/([\x80-\xFF])/n) {|x| "\\x#{x[0].ord.to_s(16)}" }
+
+        z.emit("\"#{escaped}\"")
       end
-    }
+    end
   end
 
   # Return a guranteed ASCII-8BIT encoding for Ruby 1.9 This is a helper
@@ -279,12 +272,12 @@ class String
   # sequences deliberately rather than dealing with characters.
   # The method may or may not return a new instance.
   if String.method_defined?(:encoding)
-    ASCII = Encoding.find("ASCII-8BIT")
+    ASCII_ENCODING = Encoding.find("ASCII-8BIT")
     def to_ascii8bit
-      if self.encoding.name == "ASCII-8BIT"
+      if self.encoding == ASCII_ENCODING
         self
       else
-        self.dup.force_encoding("ASCII-8BIT")
+        self.dup.force_encoding(ASCII_ENCODING)
       end
     end
   else
