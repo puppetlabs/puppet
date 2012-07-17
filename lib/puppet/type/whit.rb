741e20f2eefda43c5cd1fc5df692213bece19b71
Daniel Pittman <daniel@puppetlabs.com>
2012-07-17 15:31:46 -0700

    whit: cache the `to_s` result.
    This saves about 1.6MB of generated memory, something on the order of 10,000
    object allocations and garbage collections, and a whole lot of waste in my
    test system.
    
    This runs at, like, O(50n), so while it is just linear it is still a huge
    constant factor through the system.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/type/whit.rb
+++ lib/puppet/type/whit.rb
@@ -10,18 +10,25 @@ Puppet::Type.newtype(:whit) do
   end
 
 
-  # Hide the fact that we're a whit from logs
+  # Hide the fact that we're a whit from logs.
+  #
+  # I hate you, milkman whit.  You are so painful, so often.
+  #
+  # In this case the memoized version means we generate a new string about 1.9
+  # percent of the time, and we allocate about 1.6MB less memory, and generate
+  # a whole lot less GC churn.
+  #
+  # That number probably goes up at least O(n) with the complexity of your
+  # catalog, and I suspect beyond that, because that is, like, 10,000 calls
+  # for 200 distinct objects.  Even with just linear, that is a constant
+  # factor of, like, 50n. --daniel 2012-07-17
   def to_s
-    name.sub(/^completed_|^admissible_/, "")
+    @to_s ||= name.sub(/^completed_|^admissible_/, "")
   end
-
-  def path
-    to_s
-  end
+  alias path to_s
 
   def refresh
-    # We don't do anything with them, but we need this to
-    #   show that we are "refresh aware" and not break the
-    #   chain of propagation.
+    # We don't do anything with them, but we need this to show that we are
+    # "refresh aware" and not break the chain of propagation.
   end
 end
