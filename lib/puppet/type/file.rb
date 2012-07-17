301cdbdeaa8d2072139a3442eccd41a2c8c27a9e
Daniel Pittman <daniel@puppetlabs.com>
2012-07-17 12:46:45 -0700

    type: support implicit "identity" transformation
    Almost every implementation of `title_patterns` on a type returned with a
    lambda that performed an "identity" transformation - `lambda {|x| x }`.
    
    That actually cost a notable amount of memory allocation, because it created a
    new instance of the lambda on every invocation. That represented about three
    percent of total memory allocation, or 8MB total.
    
    This allows the `title_patterns` data to simply omit the lambda - if you don't
    supply one, we don't invoke it to transform the result, just assign directly.
    
    This allows everyone to keep using this the way they currently do, including
    supplying an identity lambda if they want, but allows internal types and types
    that don't override the default to be more memory efficient.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/type/file.rb
+++ lib/puppet/type/file.rb
@@ -34,7 +34,7 @@ Puppet::Type.newtype(:file) do
     parent directories of a file, the file resource will autorequire them."
 
   def self.title_patterns
-    [ [ /^(.*?)\/*\Z/m, [ [ :path, lambda{|x| x} ] ] ] ]
+    [ [ /^(.*?)\/*\Z/m, [ [ :path ] ] ] ]
   end
 
   newparam(:path) do
@@ -63,9 +63,7 @@ Puppet::Type.newtype(:file) do
 
     # and the reverse
     unmunge do |value|
-      basedir = Puppet::FileCollection.collection.path(value[:index])
-
-      ::File.join( basedir, value[:name] )
+      ::File.join(Puppet::FileCollection.collection.path(value[:index]), value[:name])
     end
   end
 
