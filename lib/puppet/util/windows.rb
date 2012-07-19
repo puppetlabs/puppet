f684d6aa3dbbd599d3e6769c75c5514c20f2308b
Josh Cooper <josh@puppetlabs.com>
2012-07-18 17:06:47 -0700

    (#11870) Add registry module
    Ruby includes a built-in set of modules and classes for interacting with
    the registry, however, it is only present on Windows.  This makes it
    difficult to write spec tests that can run on non-Windows platforms due to
    the `Win32` namespace and related constants.
    
    This commit adds a utility module to wrap the Windows specific code.  This
    way if you write a module/class that uses the registry, you can just add
    expects/stub expectations to the utility module or the class that
    includes/extends it.

--- lib/puppet/util/windows.rb
+++ lib/puppet/util/windows.rb
@@ -1,7 +1,10 @@
 module Puppet::Util::Windows
-  require 'puppet/util/windows/error'
-  require 'puppet/util/windows/security'
-  require 'puppet/util/windows/user'
-  require 'puppet/util/windows/process'
-  require 'puppet/util/windows/file'
+  if Puppet::Util::Platform.windows?
+    require 'puppet/util/windows/error'
+    require 'puppet/util/windows/security'
+    require 'puppet/util/windows/user'
+    require 'puppet/util/windows/process'
+    require 'puppet/util/windows/file'
+  end
+  require 'puppet/util/windows/registry'
 end
