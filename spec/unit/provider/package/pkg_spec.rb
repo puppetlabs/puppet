5b385800684bbdd63cffdb4558443555cf98bb17
rahul <rahul@pe-solaris11-vm.(none)>
2012-07-19 13:20:37 -0700

    (#15644) make sure parse_line is in scope
    parse_line was not in scope earlier.

--- spec/unit/provider/package/pkg_spec.rb
+++ spec/unit/provider/package/pkg_spec.rb
@@ -64,6 +64,16 @@ describe Puppet::Type.type(:package).provider(:pkg) do
       @instances[11].should == {:name => 'security/sudo', :ensure => :present}
     end
 
+    it "should work correctly for ensure latest on solaris 11" do
+      @provider.stubs(:pkg).with(:list,'-Ha','dummy').returns File.read(my_fixture('dummy_solaris11'))
+      @provider.latest.should == 'installed'
+    end
+
+    it "should work correctly for ensure latest on solaris 11(2)" do
+      @provider.stubs(:pkg).with(:list,'-Ha','dummy').returns File.read(my_fixture('dummy_solaris11.known'))
+      @provider.latest.should == 'known'
+    end
+
     it "should warn about incorrect lines" do
       fake_output = File.read(my_fixture('incomplete'))
       error_line = fake_output.split($/).first
