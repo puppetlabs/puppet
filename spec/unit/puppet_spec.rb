e30d6efbe975d79458a6aac0c52a5854a50cb7ad
Matthaus Litteken <matthaus@puppetlabs.com>
2012-08-15 16:44:44 -0700

    (#15852) Remove version check from puppet_spec.rb
    This commit removes the version test from puppet, as it doesn't ensure any
    functionality of puppet, but only that the version string matches certain
    criteria.

--- spec/unit/puppet_spec.rb
+++ spec/unit/puppet_spec.rb
@@ -2,16 +2,9 @@
 require 'spec_helper'
 require 'puppet'
 require 'puppet_spec/files'
-require 'semver'
 
 describe Puppet do
   include PuppetSpec::Files
-
-  context "#version" do
-    it "should be valid semver" do
-      SemVer.should be_valid Puppet.version
-    end
-  end
 
   Puppet::Util::Log.eachlevel do |level|
     it "should have a method for sending '#{level}' logs" do
