puppetlabs/puppet

commit b33d517ca2aca6c113f136a753be9b8d49f74ec5

Move Puppet version into lib/puppet/version.rb
Without this patch the entire Puppet library must be loaded in order to
get the version.  This is problematic for executed metadata files like
the rubygems gemspec or bundler Gemfile.

This patch fixes the problem by moving the Puppet.version method and
Puppet::VERSION constant into an isolated small file.  Gemfile and
gemspec files may determine the version using `require 'puppet/version'`

lib/puppet/version.rb
@@ -1,0 +1,10 @@
+# The version method and constant are isolated in puppet/version.rb so that a
+# simple `require 'puppet/version'` allows a rubygems gemspec or bundler
+# Gemfile to get the Puppet version of the gem install.
+module Puppet
+  PUPPETVERSION = '2.7.19'
+
+  def Puppet.version
+    PUPPETVERSION
+  end
+end

lib/puppet.rb
@@ -1,3 +1,5 @@
+require 'puppet/version'
+
 # Try to load rubygems.  Hey rubygems, I hate you.
 begin
   require 'rubygems'
@@ -24,12 +26,6 @@ require 'puppet/util/run_mode'
 # it's also a place to find top-level commands like 'debug'
 
 module Puppet
-  PUPPETVERSION = '2.7.19'
-
-  def Puppet.version
-    PUPPETVERSION
-  end
-
   class << self
     include Puppet::Util
     attr_reader :features

commit bb8c396f1d775a9608afed645db69f202cf11f82

(Maint) Fix Puppet.version in 3.x
Without this patch applied the Puppet 3.x branch is mis-reporting its
version as 2.7.19.  This is a problem because the spec helper uses the
`Puppet.version` method to determine how it should construct a
Puppet::Parser::Scope instance for parser function testing.  The
incorrect version string is causing the scope instance to be constructed
incorrectly ultimately causing the test failures with stdlib 3.x and
master.

This patch restores the Puppet.version to 3.0.0-rc3 which was the
version string prior to the problem being introduced in b33d517.

lib/puppet/version.rb
@@ -6,7 +6,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 #
 module Puppet
-  PUPPETVERSION = '2.7.19'
+  PUPPETVERSION = '3.0.0-rc3'
 
   def self.version
     @puppet_version || PUPPETVERSION