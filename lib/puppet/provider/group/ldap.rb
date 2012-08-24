1a2e8fc508d2a7837b541e54be11cca6dcb6affc
Dominic Cleal <dcleal@redhat.com>
2012-08-24 22:22:34 +0100

    (#14822) Use feature confine for feature tests during run
    Fixes two providers to use the "confine :feature" statement, allowing for the
    features to be re-evaluated during a run instead of only once.

--- lib/puppet/provider/group/ldap.rb
+++ lib/puppet/provider/group/ldap.rb
@@ -12,7 +12,7 @@ Puppet::Type.type(:group).provide :ldap, :parent => Puppet::Provider::Ldap do
     not specify one, but it is a potentially expensive operation, as it
     iterates across all existing groups to pick the appropriate next one."
 
-  confine :true => Puppet.features.ldap?, :false => (Puppet[:ldapuser] == "")
+  confine :feature => :ldap, :false => (Puppet[:ldapuser] == "")
 
   # We're mapping 'members' here because we want to make it
   # easy for the ldap user provider to manage groups.  This
