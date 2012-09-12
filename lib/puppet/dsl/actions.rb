a053234b501a3d3f184fa29023119d815e18f71e
Mateusz Lenik <mt.lenik@gmail.com>
2012-09-13 00:31:28 +0200

    Updated error message when calling functions when importing
    * The error message it reflects the error better.
    * Removing the TOCTOU condition from call_function
      method.

--- lib/puppet/dsl/actions.rb
+++ lib/puppet/dsl/actions.rb
@@ -203,8 +203,7 @@ module Puppet
       ##
       def call_function(name, args)
         # when performing type import the scope is nil
-        raise NoMethodError, "resources can't be created in top level scope when importing a manifest" if Parser.current_scope.nil?
-        raise Puppet::DSL::InvalidFunctionError, "calling undefined function #{name}(#{args.join ', '})" unless is_function? name
+        raise NoMethodError, "functions can't be called in top level scope when importing a manifest" if Parser.current_scope.nil?
         Parser.current_scope.send name, args
       end
 
