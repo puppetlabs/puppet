f0ef301bc3b22511e38de4e7eead198af0c05724
Daniel Pittman <daniel@puppetlabs.com>
2012-09-13 11:26:28 -0700

    (#16330) Update ZAML to latest upstream.
    This rebases our internal ZAML on the upstream 0.1.3 release, including the
    update to dual-license the code.  It then merges back all our changes and
    squashes them into place - which, in essence, means bumping the version
    number; we already had all the other relevant changes in place.
    
    Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

--- lib/puppet/util/zaml.rb
+++ lib/puppet/util/zaml.rb
@@ -18,13 +18,35 @@
 #
 # http://github.com/hallettj/zaml
 #
-# Authors: Markus Roberts, Jesse Hallett, Ian McIntosh, Igal Koshevoy, Simon Chiang
+# ## License (from upstream)
 #
+# Copyright (c) 2008-2009 ZAML contributers
+#
+# This program is dual-licensed under the GNU General Public License
+# version 3 or later and under the Apache License, version 2.0.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version; or under the terms of the Apache License,
+# Version 2.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License and the Apache License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# You may obtain a copy of the Apache License at
+# <https://www.apache.org/licenses/LICENSE-2.0.html>.
 
 require 'yaml'
 
 class ZAML
-  VERSION = "0.1.1"
+  VERSION = "0.1.3"
   #
   # Class Methods
   #
