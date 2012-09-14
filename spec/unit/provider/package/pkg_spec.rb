f96262acdad1f5d4c241e1fe8be829dd6ad76b28
rahul <rahul@puppetlabs.com>
2012-09-14 16:15:11 -0700

    (#16120) ips package provider gets feature holdable.
    Add the missing test case to specs.

--- spec/unit/provider/package/pkg_spec.rb
+++ spec/unit/provider/package/pkg_spec.rb
@@ -214,6 +214,7 @@ describe Puppet::Type.type(:package).provider(:pkg) do
       it "should install if no version was previously installed, and a specific version was requested" do
         resource[:ensure] = '0.0.7'
         provider.expects(:query).with().returns({:ensure => :absent})
+        Puppet::Util::Execution.expects(:execute).with(['/bin/pkg', 'unfreeze', 'dummy'], {:failonfail => false, :combine => true})
         Puppet::Util::Execution.expects(:execute).with(['/bin/pkg', 'install', '--accept', 'dummy@0.0.7'], {:failonfail => false, :combine => true}).returns ''
         $CHILD_STATUS.stubs(:exitstatus).returns 0
         provider.install
