0fde9c174b73832dde98a715b63054af41979577
rahul <rahul@puppetlabs.com>
2012-09-15 00:07:57 -0700

    (#16430) refactor smf tests
    This patch refactors smf tests and adds a couple of new tests.

--- acceptance/tests/resource/service/smf_should_stop.rb
+++ acceptance/tests/resource/service/smf_should_stop.rb
@@ -1,4 +1,4 @@
-test_name "SMF: basic tests"
+test_name "SMF: should stop a given service"
 confine :to, :platform => 'solaris'
 
 require 'puppet/acceptance/solaris_util'
@@ -13,28 +13,19 @@ end
 
 
 agents.each do |agent|
-  clean agent, :service => 'tstapp'
   manifest, method = setup agent, :service => 'tstapp'
-  step "SMF: clean slate"
-  apply_manifest_on(agent, 'service {tstapp : ensure=>stopped}') do
-    assert_match( /.*/, result.stdout, "err: #{agent}")
-  end
-
   step "SMF: ensre it is created with a manifest"
   apply_manifest_on(agent, 'service {tstapp : ensure=>running, manifest=>"%s"}' % manifest) do
     assert_match( / ensure changed 'stopped' to 'running'/, result.stdout, "err: #{agent}")
   end
-  step "SMF: verify it with puppet"
-  on agent, "puppet resource service tstapp" do
-    assert_match( /ensure => 'running'/, result.stdout, "err: #{agent}")
-  end
 
-  step "SMF: verify with svcs that the service is online"
-  on agent, "svcs -l application/tstapp" do
-    assert_match( /state\s+online/, result.stdout, "err: #{agent}")
-  end
   step "SMF: stop the service"
   apply_manifest_on(agent, 'service {tstapp : ensure=>stopped}') do
     assert_match( /changed 'running' to 'stopped'/, result.stdout, "err: #{agent}")
+  end
+
+  step "SMF: verify with svcs that the service is not online"
+  on agent, "svcs -l application/tstapp", :acceptable_exit_codes => [0,1] do
+    assert_no_match( /state\s+online/, result.stdout, "err: #{agent}")
   end
 end
