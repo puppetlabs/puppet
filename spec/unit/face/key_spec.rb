edb535e25cf3faa6607ee8fc5d33d74d7ec00892
Jeff McCune <jeff@puppetlabs.com>
2012-09-21 00:28:51 -0700

    Revert "Merge branch 'fix/3.x/fix_puppet_face_failures' into 3.x"
    This reverts commit 8dff14b9c5a3cdd99ede63dbcbb2dbe00b457926, reversing
    changes made to 8b059a9a9e7c765d3050163ca59abeffe3e297b5.
    
    Revert the modulepath face loader change set for the following reason:
    
    This change set is intended to load faces from the module path.   It
    works great...  _if_ you explicitly set --confdir on the command line
    like I do with my zsh puppet() function.  If you don't, then it blows up
    because the global for the confdir is set by defaults at a different
    enough point in the code for us to overlook the issue.
    
    If we invest in removing global state, specifically Puppet.settings, I
    think we'll see a very quick return.
    
    The confdir setting should be "available" for use at the same point,
    regardless of where it comes from; defaults, puppet.conf, CLI, or
    (hopefully) the environment.

--- spec/unit/face/key_spec.rb
+++ spec/unit/face/key_spec.rb
@@ -2,8 +2,6 @@
 require 'spec_helper'
 require 'puppet/face'
 
-describe "Puppet::Face[:key, '0.0.1']" do
-  subject { Puppet::Face[:key, '0.0.1'] }
-
+describe Puppet::Face[:key, '0.0.1'] do
   it "should actually have some tests..."
 end
