df7a72ffec395adb638b12067029503711291a88
Jeff McCune <jeff@puppetlabs.com>
2012-09-20 19:07:50 -0700

    (Maint) Fix spec failures with Puppet::Face[]
    Without this patch the describe blocks are being executed before the
    spec helper before hooks.  This is problem because the
    Puppet::Face.find method now searches the modulepath.  The modulepath
    setting is not yet initialized when the subject of the describe block is
    evaluated.
    
    This patch fixes the problem by explicitly setting the subject inside
    the example groups.  Doing so delays evaluation of the find method until
    after settings have initialized.
    
    I suppose all of this could be summed up as, "Load faces after settings
    have been initialized by the spec helper, not before."
    
    Paired-with: Andrew Parker <andy@puppetlabs.com>

--- spec/unit/face/instrumentation_data_spec.rb
+++ spec/unit/face/instrumentation_data_spec.rb
@@ -2,6 +2,8 @@
 require 'spec_helper'
 require 'puppet/face'
 
-describe Puppet::Face[:instrumentation_data, '0.0.1'] do
+describe "Puppet::Face[:instrumentation_data, '0.0.1']" do
+  subject { Puppet::Face[:instrumentation_data, '0.0.1'] }
+
   it_should_behave_like "an indirector face"
 end
