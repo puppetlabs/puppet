e45fbc15472328db3dc9273bb7da0db5abd73509
nfagerlund <nick.fagerlund@gmail.com>
2012-10-10 14:45:50 -0700

    Maint: Document every terminus, especially static_compiler
    Many termini were added to the codebase without a desc string. This commit
    adds one to every extant terminus, although the instrumentation ones simply
    say "undocumented."

--- lib/puppet/indirector/resource/active_record.rb
+++ lib/puppet/indirector/resource/active_record.rb
@@ -1,6 +1,10 @@
 require 'puppet/indirector/active_record'
 
 class Puppet::Resource::ActiveRecord < Puppet::Indirector::ActiveRecord
+
+  desc "A component of ActiveRecord storeconfigs. ActiveRecord-based storeconfigs
+    and inventory are deprecated. See http://links.puppetlabs.com/activerecord-deprecation"
+
   def initialize
     Puppet.deprecation_warning "ActiveRecord-based storeconfigs and inventory are deprecated. See http://links.puppetlabs.com/activerecord-deprecation"
     super
