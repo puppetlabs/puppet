3cda4f411793b61eb2efeba212fd70fbe56c140f
Andrew Parker <andy@puppetlabs.com>
2012-10-25 13:59:20 -0700

    (#16791) Revert "Merge branch 'feature/2.7.x/json-terminus-backport' of https://github.com/daniel-pittman/puppet into 2.7.x"
    This reverts commit ba969173ffad096401bc5d0df44b17324318ca03, reversing
    changes made to 5e3fbcae67cfe4e3eb0564735742354bf85d79e7.
    
    This change had introduced a new terminus for the catalog indirection
    that wrote json files instead of yaml. Unfortunately the changes didn't
    include actually being able to configure puppet to use this. This left
    the decision about whether to backport another functionality change, or
    to undo this change.
    
    Since 2.7 is supposed to be a stable branch and should only be receiving
    bug fixes, this change does not really belong in the 2.7 series. All of
    the other backported performance fixes are small tweaks to existing
    behavior, whereas this one provides a new set of functionality.
    Therefore this change will not be made in the 2.7 series.

--- lib/puppet/indirector/catalog/json.rb
+++
@@ -1,6 +0,0 @@
-require 'puppet/resource/catalog'
-require 'puppet/indirector/json'
-
-class Puppet::Resource::Catalog::Json < Puppet::Indirector::JSON
-  desc "Store catalogs as flat files, serialized using JSON."
-end
