9e13374acf01546b097308b18cc13c0e36a7feb9
Moses Mendoza <moses@puppetlabs.com>
2012-11-05 15:23:44 -0800

    Revert "(#15560) Delete user's profile after deleting account"
    This reverts commit 5e84c971c4f6142d3e4ed46595e528ea35bac9fb.
    
    This commit has been released in 3.0.0, but should probably not be released in
    2.7.20. (#15560) addresses managing of homedirs in Windows - the issue is
    deleting a user does not result in the removal of the profile even if
    `managehome => true` is set (a feature we claim to support in
    http://docs.puppetlabs.com/references/2.7.19/type.html). However, users of
    puppet may have come to inadvertently rely on this bug, and expect user
    homedirs to remain, even with managehome => true. Suddenly deleting them in
    Puppet 2.7.20 may cause serious problems for folks, particularly since deletion
    of user data is already a particularly sensitive issue with regulatory/
    compliance implications for many organizations.
    
    Conflicts:
    	spec/unit/provider/user/windows_adsi_spec.rb

--- lib/puppet/provider/user/windows_adsi.rb
+++ lib/puppet/provider/user/windows_adsi.rb
@@ -39,14 +39,7 @@ Puppet::Type.type(:user).provide :windows_adsi do
   end
 
   def delete
-    # lookup sid before we delete account
-    sid = uid if @resource.managehome?
-
     Puppet::Util::ADSI::User.delete(@resource[:name])
-
-    if sid
-      Puppet::Util::ADSI::UserProfile.delete(sid)
-    end
   end
 
   # Only flush if we created or modified a user, not deleted
