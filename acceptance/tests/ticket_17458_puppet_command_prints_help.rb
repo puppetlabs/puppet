puppetlabs/puppet

commit 20efe942d2cfe295f406735a57145079523279a5

util: don't rebuild regexp every time we invoke absolute_file?
Previously, the `absolute_path?` tool rebuilt both regular expressions, POSIX
and Windows, every time it was invoked.  Since they are constant, promoting
them to a constant saves some time and trouble for the VM.

We can also tell it that you can interpolate the values once on Windows,
rather than every time through, which should improve performance and object
load further on that platform.

Signed-off-by: Daniel Pittman <daniel@puppetlabs.com>

lib/puppet/util.rb
@@ -195,18 +195,16 @@ module Util
 
   # Determine in a platform-specific way whether a path is absolute. This
   # defaults to the local platform if none is specified.
+  #
+  # Escape once for the string literal, and once for the regex.
+  slash = '[\\\\/]'
+  label = '[^\\\\/]+'
+  AbsolutePathWindows = %r!^(?:(?:[A-Z]:#{slash})|(?:#{slash}#{slash}#{label}#{slash}#{label})|(?:#{slash}#{slash}\?#{slash}#{label}))!io
+  AbsolutePathPosix   = %r!^/!
   def absolute_path?(path, platform=nil)
-    # Escape once for the string literal, and once for the regex.
-    slash = '[\\\\/]'
-    name = '[^\\\\/]+'
-    regexes = {
-      :windows => %r!^(([A-Z]:#{slash})|(#{slash}#{slash}#{name}#{slash}#{name})|(#{slash}#{slash}\?#{slash}#{name}))!i,
-      :posix   => %r!^/!,
-    }
-
     # Due to weird load order issues, I was unable to remove this require.
     # This is fixed in Telly so it can be removed there.
-    require 'puppet'
+    require 'puppet' unless defined?(Puppet)
 
     # Ruby only sets File::ALT_SEPARATOR on Windows and the Ruby standard
     # library uses that to test what platform it's on.  Normally in Puppet we
@@ -214,8 +212,16 @@ module Util
     # be called during the initialization of features so it can't depend on
     # that.
     platform ||= Puppet::Util::Platform.windows? ? :windows : :posix
+    regex = case platform
+            when :windows
+              AbsolutePathWindows
+            when :posix
+              AbsolutePathPosix
+            else
+              raise Puppet::DevError, "unknown platform #{platform} in absolute_path"
+            end
 
-    !! (path =~ regexes[platform])
+    !! (path =~ regex)
   end
   module_function :absolute_path?
 

commit d16d357c278bf40222a75f41d6a1f21107acbe9e

(#17458) Add acceptance test for executing external subcommands
Commit 20efe94 broke running `puppet` and `puppet-*` external
subcommands. This adds an acceptance test to make sure puppet doesn't
blow up when given an unknown external subcommand.

Paired-with: Josh Cooper <josh@puppetlabs.com>

acceptance/tests/ticket_17458_puppet_command_prints_help.rb
@@ -1,0 +1,5 @@
+test_name "puppet command with an unknown external command prints help"
+
+on agents, puppet('unknown') do
+  assert_match(/See 'puppet help' for help on available puppet subcommands/, stdout)
+end