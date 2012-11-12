d47d09c6aa63e27ad25d342672d8d359739a39f6
Jeff McCune <jeff@puppetlabs.com>
2012-11-12 14:57:59 -0500

    (#17488) Handle SELinux errors gracefully
    Without this patch applied the SELinux support in Puppet does not
    gracefully handle errors when running unprivileged.  This is a problem
    because `puppet --noop` is often run as a normal user.
    
    This patch addresses the problem by catching exceptions raised for no
    such file or directory and permission denied errors.  Puppet issues a
    warning when these exceptions are handled.
    
    The behavior without this patch applied is:
    
        [jeff@pe-centos6 ~]$ puppet apply --noop --execute "file { '/root/chuj': ensure => absent; }"
        Permission denied - /root/chuj
    
    Which causes Puppet to prematurely abort.  With this patch applied,
    Puppet finishes the catalog run and issues a warning to the user:
    
        [jeff@pe-centos6 ~]$ puppet apply --noop --execute "file { '/root/chuj': ensure => present; }"
        Warning: /File[/root/chuj]/seluser: Could not stat; Permission denied - /root/chuj
        Warning: /File[/root/chuj]/selrole: Could not stat; Permission denied - /root/chuj
        Warning: /File[/root/chuj]/seltype: Could not stat; Permission denied - /root/chuj
        Warning: /File[/root/chuj]/selrange: Could not stat; Permission denied - /root/chuj
        Warning: /File[/root/chuj]: Could not stat; permission denied
        /File[/root/chuj]/ensure: current_value absent, should be present (noop)
        Class[Main]: Would have triggered 'refresh' from 1 events
        Stage[main]: Would have triggered 'refresh' from 1 events
        Finished catalog run in 0.04 seconds

--- lib/puppet/util/selinux.rb
+++ lib/puppet/util/selinux.rb
@@ -42,13 +42,17 @@ module Puppet::Util::SELinux
     # If the file exists we should pass the mode to matchpathcon for the most specific
     # matching.  If not, we can pass a mode of 0.
     begin
-      filestat = File.lstat(file)
-      mode = filestat.mode
-    rescue Errno::ENOENT
-      mode = 0
-    rescue Errno::EACCES
-      return nil
+      filestat = file_lstat(file)
+    rescue Errno::EACCES, Errno::ENOENT => detail
+      warning "Could not stat; #{detail}"
     end
+
+    if filestat
+      mode = filestat.mode
+    else
+      mode = 0
+    end
+
     retval = Selinux.matchpathcon(file, mode)
     if retval == -1
       return nil
@@ -210,4 +214,14 @@ module Puppet::Util::SELinux
     # Should never be reached...
     return mounts['/']
   end
+
+  ##
+  # file_lstat is an internal, private method to allow precise stubbing and
+  # mocking without affecting the rest of the system.
+  #
+  # @return [File::Stat] File.lstat result
+  def file_lstat(path)
+    File.lstat(path)
+  end
+  private :file_lstat
 end
