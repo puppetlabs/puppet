d07eb39edb8827e0b9e9ebbd57c0a0000ef3982b
Daniel Lobato García <elobatocs@gmail.com>
2012-11-19 15:44:23 +0100

    Modified provider so it doesnt use external dependencies. Added support for reposdir and fixed bugs

--- lib/puppet/provider/yumrepo/yumrepo.rb
+++ lib/puppet/provider/yumrepo/yumrepo.rb
@@ -1,29 +1,56 @@
 require 'fileutils'
-
-begin
-  require 'inifile'
-rescue
-  Puppet.warning "The inifile gem is required for Yumrepo"
-end
+require 'ruby-debug'
 
 Puppet::Type.type(:yumrepo).provide(:yumrepo) do
   desc "Manage yum repositories"
+  mk_resource_methods
 
   def create
-    desired_yumconfig.write unless self.exists?
+    desired_yumconfig.store
   end
 
   def destroy
-    File.rm resource[:path] if self.exists? 
+    File.delete resource.section.file
   end
 
   def exists?
-    desired_yumconfig == current_yumconfig
+    repo_in_default_location? || repo_in_other_location?(get_reposdir)
+  end
+
+  def get_reposdir
+    file = Puppet::Util::IniConfig::File.new 
+    file.read("/etc/yum.conf")
+    file.each_section { |section| return section["reposdir"] unless section["reposdir"].nil? }
+    "/etc/yum.repos.d/"
+  end
+
+  # Returns true if repo is in repos directory/name_repo.repo (most likely case)
+  def repo_in_default_location?
+    file = Puppet::Util::IniConfig::File.new
+    file.read(resource.section.file)
+    file.include?(resource.name)
+  end
+
+  def repo_in_other_location?(reposdir)
+    file_names = Dir.entries(reposdir)
+    file_names.delete '.'
+    file_names.delete '..'
+    file = Puppet::Util::IniConfig::File.new 
+
+    file_names.each do |repo|
+      file.read(repo)
+      file.each_section { |section| return true if section.name == resource.name } #repo found!
+    end
+    
+    false
   end
 
   def desired_yumconfig
-    yumconfig = IniFile.new(:filename => "/etc/yum.repos.d/#{resource.name}.repo", :comment => '#', :parameter => '=')
+    yumconfig = Puppet::Util::IniConfig::File.new
+    yumconfig.read("#{resource.section.file}")
+
     section = resource[:name]
+    yumconfig.each_file { |f| yumconfig.add_section(section,f) }
 
     options = [
       :baseurl,
@@ -51,14 +78,15 @@ Puppet::Type.type(:yumrepo).provide(:yumrepo) do
       if resource[option].nil?
         next
       else
-        yumconfig[section][option.to_s] = resource[option].to_s
+        if option == :gpgkey
+          yumconfig[section][option.to_s] = resource[option].to_s if resource[:gpgcheck] == 1
+        else
+          yumconfig[section][option.to_s] = resource[option].to_s unless resource[option].to_s == 'absent'
+        end
       end
     end
 
     return yumconfig
   end
 
-  def current_yumconfig
-    yumconfig = IniFile.load("/etc/yum.repos.d/#{resource.name}.repo", :comment => '#', :parameter => '=')
-  end
-end  
+end 
