568b3c9b70bece9c850fc3b42044d71ad1814559
Toomas Pelberg <toomas.pelberg@playtech.com>
2012-11-19 13:32:53 +0200

    installing old versions was still broken, the correct way to do this is:
     package { "package-${::package_version}": ensure => installed }

--- lib/puppet/provider/package/yum.rb
+++ lib/puppet/provider/package/yum.rb
@@ -9,17 +9,19 @@ Puppet::Type.type(:package).provide :yum, :parent => :rpm, :source => :rpm do
 
   YUMHELPER = File::join(File::dirname(__FILE__), "yumhelper.py")
   YUM_INSTALLONLY = File::join(File::dirname(__FILE__), "yum_installonly.py")
+  NEVRAFORMAT = "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}"
+  NEVRA_FIELDS = [:name, :epoch, :version, :release, :arch]
 
   attr_accessor :latest_info
 
   if command('rpm')
     confine :true => begin
       rpm('--version')
-      rescue Puppet::ExecutionFailure
-        false
-      else
-        true
-      end
+    rescue Puppet::ExecutionFailure
+      false
+    else
+      true
+    end
   end
 
   defaultfor :operatingsystem => [:fedora, :centos, :redhat]
@@ -46,11 +48,11 @@ Puppet::Type.type(:package).provide :yum, :parent => :rpm, :source => :rpm do
     # Add our 'latest' info to the providers.
     packages.each do |name, package|
       if info = updates[package[:name]]
-        package.provider.latest_info = info[0]
+        package.provider.latest_info = info
       end
     end
   end
-  
+
   def installonly_pkg?
     return false if @resource.should(:ensure) == :latest
     python("#{YUM_INSTALLONLY}","#{@resource[:name]}").each_line do |line|
@@ -61,7 +63,41 @@ Puppet::Type.type(:package).provide :yum, :parent => :rpm, :source => :rpm do
         return true if((line.split[2] == @resource.should(:ensure) || "#{line.split[2]}-#{line.split[3]}" == @resource.should(:ensure)) && line.split.last)
       end
     end
-    false  
+    false
+  end
+
+  def current_versions
+    # Yes, we could have many due to installonly_pkg's and multilibs
+    cmd = ["-q", @resource[:name], "--nosignature", "--nodigest", "--qf", "#{NEVRAFORMAT}\n"]
+    begin
+      output = rpm(*cmd)
+    rescue Puppet::ExecutionFailure
+      return nil
+    end
+    return nil if output == nil
+    versions=[]
+    output.each_line do |line|
+      line.chomp!
+      next if line.empty?
+      hash={}
+      NEVRA_FIELDS.zip(line.split) { |f, v| hash[f] = v }
+      hash[:provider] = self.name
+      hash[:ensure] = "#{hash[:version]}-#{hash[:release]}"
+      versions << {:name => self.name,:version => hash[:ensure]} unless versions.member?(hash)
+    end
+    versions
+  end
+
+  def latest_installed_version(installed_versions=self.current_versions)
+    return nil if self.query == nil
+    latest_version=self.query[:ensure]
+    installed_versions.each do |v|
+      next unless v[:version]
+      if ((Puppet::Util::Package.versioncmp(latest_version, v[:version]) < 0))
+        latest_version=v[:version]
+      end
+    end
+    latest_version
   end
 
   def install
@@ -70,7 +106,8 @@ Puppet::Type.type(:package).provide :yum, :parent => :rpm, :source => :rpm do
     wanted = @resource[:name]
     operation = :install
     installonly_pkg = self.installonly_pkg?
-    self.debug "Want to install #{wanted} version: #{should} installonly?: #{installonly_pkg}"
+    installed_versions = self.current_versions
+    latest_version = self.latest_installed_version(installed_versions)
 
     case should
     when true, false, Symbol
@@ -79,30 +116,38 @@ Puppet::Type.type(:package).provide :yum, :parent => :rpm, :source => :rpm do
     else
       # Add the package version
       wanted += "-#{should}"
-      is = self.query
-      if (is && Puppet::Util::Package.versioncmp(should, is[:ensure]) < 0) && !(installonly_pkg)
-        self.debug "Downgrading package #{@resource[:name]} from version #{is[:ensure]} to #{should}"
-        operation = :downgrade
+      if (latest_version != nil && installed_versions != nil)
+        if installed_versions.member?({:name => @resource[:name],:version => should})
+          return
+        else
+          if ((Puppet::Util::Package.versioncmp(should, latest_version) < 0) && !(installonly_pkg))
+            self.debug "Downgrading package #{@resource[:name]} from version #{latest_version} to #{should}"
+            operation = :downgrade
+          end
+        end
       end
     end
 
     output = yum "-d", "0", "-e", "0", "-y", operation, wanted
 
-    is = self.query
-    raise Puppet::Error, "Could not find package #{self.name}" unless is
-
-    # FIXME: Should we raise an exception even if should == :latest
-    # and yum updated us to a version other than @param_hash[:ensure] ?
-    raise Puppet::Error, "Failed to update to version #{should}, got version #{is[:ensure]} instead" if (should && should != is[:ensure]) && !(installonly_pkg)
+    installed_versions = self.current_versions
+    latest_version = self.latest_installed_version(installed_versions)
+    raise Puppet::Error, "Could not find package #{self.name}" unless installed_versions
+    if (should && installed_versions.member?({:name => @resource[:name],:version => should}))
+      # We have what we came for, good to go
+      return
+    else
+      # We don't have it, bummer.
+      raise Puppet::Error, "Failed to update to version #{should}, got version #{latest_version} instead"
+    end
   end
 
   # What's the latest package version available?
   def latest
+    self.current_versions
     upd = latest_info
     unless upd.nil?
-      # FIXME: there could be more than one update for a package
-      # because of multiarch
-      return "#{upd[:epoch]}:#{upd[:version]}-#{upd[:release]}"
+      return upd.collect {|u| "#{u[:epoch]} #{u[:version]}-#{u[:release]}"}
     else
       # Yum didn't find updates, pretend the current
       # version is the latest
