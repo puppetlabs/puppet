cff8ef1605d710a398931c57de4c03d81a9e5798
Andrew Parker <andy@puppetlabs.com>
2012-12-06 15:56:00 -0800

    (#16753) Verify by using puppet node search
    Previously, the test checked that the yaml file was written by looking
    at a specific file on the filesystem. This is, however, not the
    guarantee that was asked for in the bug. This changes the test to check
    that the data is available by using a puppet subcommand to read the
    available data and so allows us to modify in the future how to actually
    store the data.

--- acceptance/tests/node/check_woy_cache_works.rb
+++ acceptance/tests/node/check_woy_cache_works.rb
@@ -2,6 +2,7 @@ test_name "ticket #16753 node data should be cached in yaml to allow it to be qu
 
 require 'securerandom'
 require 'puppet/acceptance/temp_file_utils'
+require 'yaml'
 extend Puppet::Acceptance::TempFileUtils
 
 node_name = "woy_node_#{SecureRandom.hex}"
@@ -15,10 +16,22 @@ auth yes
 allow *
 AUTHCONF
 
+routes_contents = YAML.dump({
+  "node" => {
+    "node" => {
+      "terminus" => "yaml",
+      "cache" => "yaml"
+    }
+  }
+})
+
 initialize_temp_dirs
+
 create_test_file master, "auth.conf", auth_contents, {}
+create_test_file master, "routes.conf", routes_contents, {}
 
 authfile = get_test_file_path master, "auth.conf"
+routes = get_test_file_path master, "routes.conf"
 
 on master, "chmod 644 #{authfile}"
 with_master_running_on(master, "--rest_authconfig #{authfile} --daemonize --dns_alt_names=\"puppet, $(facter hostname), $(facter fqdn)\" --autosign true") do
@@ -26,6 +39,10 @@ with_master_running_on(master, "--rest_authconfig #{authfile} --daemonize --dns_
   # only one agent is needed because we only care about the file written on the master
   run_agent_on(agents[0], "--no-daemonize --verbose --onetime --node_name_value #{node_name} --server #{master}")
 
-  assert_match(/name:\s*#{node_name}/, file_contents(master, "#{master["puppetvardir"]}/yaml/node/#{node_name}.yaml"),
-               "Expect node name '#{node_name}' to be present in node yaml file content written by the WriteOnlyYaml terminus")
+
+  yamldir = on(master, puppet("master", "--configprint", "yamldir")).stdout.chomp
+  on master, puppet('node', 'search', '"*"', "--route_file", routes, '--clientyamldir', yamldir) do
+    assert_match(/"name":["\s]*#{node_name}/, stdout,
+                 "Expect node name '#{node_name}' to be present in node yaml content written by the WriteOnlyYaml terminus")
+  end
 end
