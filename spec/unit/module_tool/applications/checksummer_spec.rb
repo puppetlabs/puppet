aaa96a54e53c9be6294e499f3ebc5a918b04b8b4
mruzicka <michal.ruzicka@cloudsmith.com>
2012-12-13 14:18:23 +0100

    (#14728) Use mock objects rather than stubbing real ones.
    This patch simplifies the mocking in the PMT change command unit tests
    by using mock objects rather than trying to stub the functionality of
    real ones.

--- spec/unit/module_tool/applications/checksummer_spec.rb
+++ spec/unit/module_tool/applications/checksummer_spec.rb
@@ -9,40 +9,31 @@ describe Puppet::ModuleTool::Applications::Checksummer, :fails_on_windows => tru
   let(:module_install_path) { 'foo' }
   let(:module_metadata_file) { 'metadata.json' }
 
-  before(:all) do
-    Pathname.class_eval do
-      class << self
-        alias_method :real_new, :new
-      end
-    end
-  end
-
-  after(:all) do
-    Pathname.class_eval do
-      class << self
-        remove_method :real_new
-      end
-    end
-  end
-
-  def stub_module_pathname(relative_path, content, count = 1)
-    Pathname.expects(:new).with(relative_path).returns(Pathname.real_new(relative_path)).times(count)
-    path = "#{module_install_path}/#{relative_path}"
-    pathname = Pathname.real_new(path)
-    if content.nil?
-      pathname.expects(:exist?).returns(false)
-    else
-      pathname.expects(:exist?).returns(true)
-      pathname.expects(:read).returns(content)
-    end
-    Pathname.expects(:new).with(path).returns(pathname).times(count)
+  def stub_module_file_pathname(relative_path, content, count = 1)
+    module_install_pathname.expects(:+).\
+      with(relative_path).\
+      returns(
+        mock() do
+          if content.nil?
+            expects(:exist?).returns(false)
+          else
+            expects(:exist?).returns(true)
+            expects(:read).returns(content)
+          end
+        end
+      ).\
+      times(count)
   end
 
   context %q{when metadata.json doesn't exist in the specified path} do
-    it 'throws an exception' do
-      module_install_pathname = Pathname.real_new(module_install_path)
+    let(:module_install_pathname) {
+      module_install_pathname = mock()
       Pathname.expects(:new).with(module_install_path).returns(module_install_pathname)
-      stub_module_pathname(module_metadata_file, nil)
+      module_install_pathname
+    }
+
+    it 'throws an exception' do
+      stub_module_file_pathname(module_metadata_file, nil)
 
       lambda { subject.run }.should raise_error(ArgumentError, 'No metadata.json found.')
     end
@@ -62,17 +53,21 @@ describe Puppet::ModuleTool::Applications::Checksummer, :fails_on_windows => tru
       }
     }
 
-    before(:each) do
-      module_install_pathname = Pathname.real_new(module_install_path)
+    let(:module_install_pathname) {
+      module_install_pathname = mock()
       Pathname.expects(:new).with(module_install_path).returns(module_install_pathname)
-      Pathname.expects(:new).with(module_install_pathname).returns(Pathname.real_new(module_install_pathname))
-      stub_module_pathname(module_metadata_file, PSON.dump(module_metadata), 2)
+      Pathname.expects(:new).with(module_install_pathname).returns(module_install_pathname)
+      module_install_pathname
+    }
+
+    before(:each) do
+      stub_module_file_pathname(module_metadata_file, PSON.dump(module_metadata), 2)
     end
 
     it 'reports removed files' do
       removed_file = module_files.keys[0]
 
-      stub_module_pathname(removed_file, nil)
+      stub_module_file_pathname(removed_file, nil)
 
       subject.run.should == [removed_file]
     end
@@ -80,7 +75,7 @@ describe Puppet::ModuleTool::Applications::Checksummer, :fails_on_windows => tru
     it 'reports changed files' do
       changed_file = module_files.keys[0]
 
-      stub_module_pathname(changed_file, ' ' << module_files[changed_file])
+      stub_module_file_pathname(changed_file, ' ' << module_files[changed_file])
 
       subject.run.should == [changed_file]
     end
@@ -88,7 +83,7 @@ describe Puppet::ModuleTool::Applications::Checksummer, :fails_on_windows => tru
     it 'does not report unchanged files' do
       unchanged_file = module_files.keys[0]
 
-      stub_module_pathname(unchanged_file, module_files[unchanged_file])
+      stub_module_file_pathname(unchanged_file, module_files[unchanged_file])
 
       subject.run.should == []
     end
