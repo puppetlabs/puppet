911db9f2ae5b243dc0b20c8d81a8d84169637589
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2012-12-18 19:21:24 +0100

    (yardoc) Add yardoc to the Report related classes.
    This commit adds yardoc to Reports and Puppet::Transaction::Report.

--- lib/puppet/reports.rb
+++ lib/puppet/reports.rb
@@ -1,6 +1,32 @@
 require 'puppet/util/instance_loader'
 
-# A simple mechanism for loading and returning reports.
+# This class is an implementation of a simple mechanism for loading and returning reports.
+# The intent is that a user registers a report by calling {register_report} and providing a code
+# block that performs setup, and defines a method called `process`. The setup, and the `process` method
+# are called in the context where `self` is an instance of {Puppet::Transaction::Report} which provides the actual
+# data for the report via its methods.
+#
+# @example Minimal scaffolding for a report...
+#   Puppet::Reports::.register_report(:myreport) do
+#     # do setup here
+#     def process
+#       if self.status == 'failed'
+#         msg = "failed puppet run for #{self.host} #{self.status}"
+#         . . . 
+#       else
+#         . . .
+#       end
+#     end 
+#   end
+#
+# Required configuration:
+# --
+# * A .rb file that defines a new report should be placed in the master's directory `lib/puppet/reports`
+# * The `puppet.conf` file must have `report = true` in the `[agent]` section
+# 
+# @see Puppet::Transaction::Report
+# @api public
+#
 class Puppet::Reports
   extend Puppet::Util::ClassGen
   extend Puppet::Util::InstanceLoader
@@ -9,10 +35,22 @@ class Puppet::Reports
   instance_load :report, 'puppet/reports'
 
   class << self
+    # @api private
     attr_reader :hooks
   end
 
-  # Add a new report type.
+  # Adds a new report type.
+  # The block should contain setup, and define a method with the name `process`. The `process` method is
+  # called when the report is executed; the `process` method has access to report data via methods available
+  # in its context where `self` is an instance of {Puppet::Transaction::Report}.
+  # 
+  # For an example, see the overview of this class.
+  #
+  # @param name [Symbol] the name of the report (do not use whitespace in the name).
+  # @param options [Hash] a hash of options
+  # @option options [Boolean] :useyaml whether yaml should be used or not
+  # @todo Uncertain what the options :useyaml really does; "whether yaml should be used or not", used where/how?
+  # 
   def self.register_report(name, options = {}, &block)
     name = name.intern
 
@@ -25,7 +63,8 @@ class Puppet::Reports
     end
   end
 
-  # Collect the docs for all of our reports.
+  # Collects the docs for all reports.
+  # @api private
   def self.reportdocs
     docs = ""
 
@@ -41,7 +80,8 @@ class Puppet::Reports
     docs
   end
 
-  # List each of the reports.
+  # Lists each of the reports.
+  # @api private
   def self.reports
     instance_loader(:report).loadall
     loaded_instances(:report)
