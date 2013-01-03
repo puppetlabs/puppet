7555053a1d499b1f691754a3871fb5054214cb30
Garrett Honeycutt <garrett@puppetlabs.com>
2013-01-03 11:22:24 -0500

    (#18335) urpmi use osfamily for mandrake
    This commit will allow the urpmi package provider to use osfamily
    instead of operatingsystem. It is dependent on ticket #18336 which will
    provide the mandrake osfamily entry in facter and is slated for 2.x.

--- lib/puppet/provider/package/urpmi.rb
+++ lib/puppet/provider/package/urpmi.rb
@@ -12,7 +12,7 @@ Puppet::Type.type(:package).provide :urpmi, :parent => :rpm, :source => :rpm do
       end
   end
 
-  defaultfor :operatingsystem => [:mandriva, :mandrake]
+  defaultfor :osfamily => :mandrake
 
   has_feature :versionable
 
