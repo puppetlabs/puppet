04404e2f6a37fc904ef0b3b5f2ecc5ccfddec851
James Perry <jjperry69@gmail.com>
2013-01-07 13:04:05 -0500

    (#17716) HPUX_user_provider Updated to allow user provider to manage passwords on hpux in either normal or trusted computing modes. Updated spec file to make sure changes are in place. Specifically manages_passwords, user exists and password function returns a password.

--- spec/unit/provider/user/hpux_spec.rb
+++ spec/unit/provider/user/hpux_spec.rb
@@ -1,4 +1,4 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 require 'spec_helper'
 
 provider_class = Puppet::Type.type(:user).provider(:hpuxuseradd)
@@ -20,5 +20,26 @@ describe provider_class do
     @provider.stubs(:exists?).returns(true)
     @provider.expects(:execute).with { |args| args.include?("-F") }
     @provider.delete
+  end
+
+end
+
+describe provider_class do
+
+  it "should have feature manages_passwords" do
+      provider_class.should be_manages_passwords
+  end
+
+  it "should return nil if user does not exist" do
+      @resource = stub("resource", :name => "no_user")
+      @provider = provider_class.new(@resource)
+      @provider.password.must be_nil
+  end
+
+
+  it "should return password entry if exists" do
+      @resource = stub("resource", :name => "root")
+      @provider = provider_class.new(@resource)
+      @provider.password.should be_true
   end
 end
