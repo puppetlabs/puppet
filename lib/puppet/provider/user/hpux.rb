32427887851cecdb83ed0fcf8e025d4f6d1c201f
Jeff McCune <jeff@puppetlabs.com>
2013-01-09 16:50:59 -0800

    (maint) Re-indent the HPUX user provider file
    Without this patch the hpux user provider uses 4 space soft tabs.  This
    patch re-formats the file to our 2 space soft tab standard.

--- lib/puppet/provider/user/hpux.rb
+++ lib/puppet/provider/user/hpux.rb
@@ -30,37 +30,37 @@ Puppet::Type.type(:user).provide :hpuxuseradd, :parent => :useradd do
   end
 
   def password
-      # Password management routine for trusted and non-trusted systems
-      #temp=""
-      while ent = Etc.getpwent() do
-          if ent.name == resource.name
-             temp=ent.name
-             break
-          end
+    # Password management routine for trusted and non-trusted systems
+    #temp=""
+    while ent = Etc.getpwent() do
+      if ent.name == resource.name
+        temp=ent.name
+        break
       end
-      Etc.endpwent()
-      if !temp
-          return nil
-      end
+    end
+    Etc.endpwent()
+    if !temp
+      return nil
+    end
 
-      ent = Etc.getpwnam(resource.name)
-      if ent.passwd == "*"
-          # Either no password or trusted password, check trusted
-          file_name="/tcb/files/auth/#{resource.name.chars.first}/#{resource.name}"
-          if File.file?(file_name)
-              # Found the tcb user for the specific user, now get passwd
-              File.open(file_name).each do |line|
-                  if ( line =~ /u_pwd/ )
-                      temp_passwd=line.split(":")[1].split("=")[1]
-                      ent.passwd = temp_passwd
-                      return ent.passwd
-                  end
-              end
-          else
-              debug "No trusted computing user file #{file_name} found."
+    ent = Etc.getpwnam(resource.name)
+    if ent.passwd == "*"
+      # Either no password or trusted password, check trusted
+      file_name="/tcb/files/auth/#{resource.name.chars.first}/#{resource.name}"
+      if File.file?(file_name)
+        # Found the tcb user for the specific user, now get passwd
+        File.open(file_name).each do |line|
+          if ( line =~ /u_pwd/ )
+            temp_passwd=line.split(":")[1].split("=")[1]
+            ent.passwd = temp_passwd
+            return ent.passwd
           end
+        end
       else
-          return ent.passwd
+        debug "No trusted computing user file #{file_name} found."
       end
+    else
+      return ent.passwd
+    end
   end
 end
