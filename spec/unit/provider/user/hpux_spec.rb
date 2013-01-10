083719b1f596c16ee9f060130215148355f68b39
Jeff McCune <jeff@puppetlabs.com>
2013-01-09 16:48:24 -0800

    (#17716) Improve the provider spec tests for hpux password management
    Without this patch the behavior tests for the HPUX user provider are
    failing with this error:
    
        Failures:
          1) Puppet::Type::User::ProviderHpuxuseradd should return password entry if exists
             Failure/Error: @provider.password.should be_true
               expected: true value
                    got: nil
             # ./spec/unit/provider/user/hpux_spec.rb:43:in `block (2 levels) in <top (required)>'
    
    This patch fixes the problem by updating the spec tests to stub only when
    necessary.

--- spec/unit/provider/user/hpux_spec.rb
+++ spec/unit/provider/user/hpux_spec.rb
@@ -1,45 +1,52 @@
 #!/usr/bin/env ruby
 require 'spec_helper'
+require 'etc'
 
 provider_class = Puppet::Type.type(:user).provider(:hpuxuseradd)
 
 describe provider_class do
-  # left from the useradd test... I have no clue what I'm doing.
-  before do
-    @resource = stub("resource", :name => "myuser", :managehome? => nil, :should => "fakeval", :[] => "fakeval")
-    @provider = provider_class.new(@resource)
+  let :resource do
+    Puppet::Type.type(:user).new(
+      :title => 'testuser',
+      :comment => 'Test J. User',
+      :provider => :hpuxuseradd
+    )
   end
+  let(:provider) { resource.provider }
 
   it "should add -F when modifying a user" do
-    @resource.expects(:allowdupe?).returns true
-    @provider.expects(:execute).with { |args| args.include?("-F") }
-    @provider.uid = 1000
+    resource.stubs(:allowdupe?).returns true
+    provider.expects(:execute).with { |args| args.include?("-F") }
+    provider.uid = 1000
   end
 
   it "should add -F when deleting a user" do
-    @provider.stubs(:exists?).returns(true)
-    @provider.expects(:execute).with { |args| args.include?("-F") }
-    @provider.delete
+    provider.stubs(:exists?).returns(true)
+    provider.expects(:execute).with { |args| args.include?("-F") }
+    provider.delete
   end
 
-end
+  context "managing passwords" do
+    let :pwent do
+      Struct::Passwd.new("testuser", "foopassword")
+    end
 
-describe provider_class do
+    before :each do
+      Etc.stubs(:getpwent).returns(pwent)
+      Etc.stubs(:getpwnam).returns(pwent)
+    end
 
-  it "should have feature manages_passwords" do
+    it "should have feature manages_passwords" do
       provider_class.should be_manages_passwords
-  end
+    end
 
-  it "should return nil if user does not exist" do
-      @resource = stub("resource", :name => "no_user")
-      @provider = provider_class.new(@resource)
-      @provider.password.must be_nil
-  end
+    it "should return nil if user does not exist" do
+      Etc.stubs(:getpwent).returns(nil)
+      provider.password.must be_nil
+    end
 
-
-  it "should return password entry if exists" do
-      @resource = stub("resource", :name => "root")
-      @provider = provider_class.new(@resource)
-      @provider.password.should be_true
+    it "should return password entry if exists" do
+      provider.password.must == "foopassword"
+    end
   end
 end
