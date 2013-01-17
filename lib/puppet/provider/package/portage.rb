3e0a63b142b51f7e988c8fd08137cf990962ce82
l <technik@omc.net>
2013-01-17 15:29:25 +0100

    Fix portage-provider so that packages with keywords can be installed
    This patch removes the --stable restriction from the portage provider,
    which made the installation of packages with keyword-restrictions impossible.
    By superimposing this restriction the provider broke in many installations
    and thus rendered puppet unusable for systems that relied on packages with
    keyword-restrictions. This applies for example to all packages provided by
    overlays.
    This fix puts the administrator back into the responsibility to decide which
    packages he/she wants to install.

--- lib/puppet/provider/package/portage.rb
+++ lib/puppet/provider/package/portage.rb
@@ -13,19 +13,17 @@ Puppet::Type.type(:package).provide :portage, :parent => Puppet::Provider::Packa
   defaultfor :operatingsystem => :gentoo
 
   def self.instances
-    result_format = /^(\S+)\s+(\S+)\s+\[(\S+)\]\s+\[(\S+)\]\s+(\S+)\s+(.*)$/
-    result_fields = [:category, :name, :ensure, :version_available, :vendor, :description]
+    result_format = self.eix_result_format
+    result_fields = self.eix_result_fields
 
-    version_format = "{last}<version>{}"
-    search_format = "<category> <name> [<installedversions:LASTVERSION>] [<bestversion:LASTVERSION>] <homepage> <description>\n"
-
+    version_format = self.eix_version_format
     begin
       eix_file = File.directory?("/var/cache/eix") ? "/var/cache/eix/portage.eix" : "/var/cache/eix"
       update_eix if !FileUtils.uptodate?(eix_file, %w{/usr/bin/eix /usr/portage/metadata/timestamp})
 
       search_output = nil
       Puppet::Util.withenv :LASTVERSION => version_format do
-        search_output = eix "--nocolor", "--pure-packages", "--stable", "--installed", "--format", search_format
+        search_output = eix *(self.eix_search_arguments + ["--installed"])
       end
 
       packages = []
@@ -72,12 +70,10 @@ Puppet::Type.type(:package).provide :portage, :parent => Puppet::Provider::Packa
   end
 
   def query
-    result_format = /^(\S+)\s+(\S+)\s+\[(\S*)\]\s+\[(\S+)\]\s+(\S+)\s+(.*)$/
-    result_fields = [:category, :name, :ensure, :version_available, :vendor, :description]
+    result_format = self.class.eix_result_format
+    result_fields = self.class.eix_result_fields
 
-    version_format = "{last}<version>{}"
-    search_format = "<category> <name> [<installedversions:LASTVERSION>] [<bestversion:LASTVERSION>] <homepage> <description>\n"
-
+    version_format = self.class.eix_version_format
     search_field = package_name.count('/') > 0 ? "--category-name" : "--name"
     search_value = package_name
 
@@ -87,7 +83,7 @@ Puppet::Type.type(:package).provide :portage, :parent => Puppet::Provider::Packa
 
       search_output = nil
       Puppet::Util.withenv :LASTVERSION => version_format do
-        search_output = eix "--nocolor", "--pure-packages", "--stable", "--format", search_format, "--exact", search_field, search_value
+        search_output = eix *(self.class.eix_search_arguments + ["--exact",search_field,search_value])
       end
 
       packages = []
@@ -120,5 +116,26 @@ Puppet::Type.type(:package).provide :portage, :parent => Puppet::Provider::Packa
 
   def latest
     self.query[:version_available]
+  end
+
+  private
+  def self.eix_search_format
+    "'<category> <name> [<installedversions:LASTVERSION>] [<bestversion:LASTVERSION>] <homepage> <description>'"
+  end
+
+  def self.eix_result_format
+    /^(\S+)\s+(\S+)\s+\[(\S*)\]\s+\[(\S*)\]\s+(\S+)\s+(.*)$/
+  end
+
+  def self.eix_result_fields
+    [:category, :name, :ensure, :version_available, :vendor, :description]
+  end
+
+  def self.eix_version_format
+    "{last}<version>{}"
+  end
+
+  def self.eix_search_arguments
+    ["--nocolor", "--pure-packages", "--format",self.eix_search_format]
   end
 end
