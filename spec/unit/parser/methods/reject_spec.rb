1bfa52db0192224f8c2f204c3a50806ff1e98605
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-01-22 01:55:28 +0100

    (#18764) Add methods for enumerable functions.
    This adds support for the functions/methods collect, reject, select, and
    reduce. This implementation depends on support for lambdas being added
    in puppet as described in issue #11331). Which probably will be released
    in puppet 3.2.

---
+++ spec/unit/parser/methods/reject_spec.rb
@@ -0,0 +1,40 @@
+require 'puppet'
+require 'spec_helper'
+require 'puppet_spec/compiler'
+
+describe 'the reject method' do
+  include PuppetSpec::Compiler
+
+  before :each do
+    node      = Puppet::Node.new("floppy", :environment => 'production')
+    @compiler = Puppet::Parser::Compiler.new(node)
+    @scope    = Puppet::Parser::Scope.new(@compiler)
+    @topscope = @scope.compiler.topscope
+    @scope.parent = @topscope
+  end
+
+  context "should be callable as" do
+    it 'reject on an array (no berries)' do
+      catalog = compile_to_catalog(<<-MANIFEST)
+        $a = ['strawberry','blueberry','orange']
+        $a.reject {|$x| = $x  =~ /berry$/}.foreach {|$v| 
+          file { "/file_$v": ensure => present }
+        }
+      MANIFEST
+
+      catalog.resource(:file, "/file_orange")['ensure'].should == 'present'
+      catalog.resource(:file, "/file_strawberry").should == nil
+    end    
+    it 'reject on an array (no berries)' do
+      catalog = compile_to_catalog(<<-MANIFEST)
+        $a = ['strawberry','blueberry','orange']
+        $a.reject {|$x| $foo = $x  =~ /berry$/}.foreach {|$v| 
+          file { "/file_$v": ensure => present }
+        }
+      MANIFEST
+
+      catalog.resource(:file, "/file_orange")['ensure'].should == 'present'
+      catalog.resource(:file, "/file_strawberry").should == nil
+    end    
+  end
+end
