ae47b68937150b1cfc3ee5e1161f4be179179fcd
Johan Huysmans <johan.huysmans@inuits.be>
2013-01-25 16:32:40 +0100

    18030 - erb parse hiera results
    The results provided by a hiera lookup are erb-parsed,
    just like the inline_template or template functions.
    This means that ruby can be used in your hiera yaml, but also
    calls to puppet variables an even calls to puppet functions.
    This results in the possibility to perform another hiera call.
    
    The main code is located in hiera_puppet.rb.
    The data_binding and indirector/hiera files have to be changed
    due to the fact that the indirector didn't call HieraPuppet
    but performed calls to hiera.lookup directly.
    
    The change in the indirector/hiera file is written by ripienaar,
    see: https://github.com/puppetlabs/puppet/pull/1217/files

--- lib/puppet/indirector/hiera.rb
+++ lib/puppet/indirector/hiera.rb
@@ -1,4 +1,5 @@
 require 'puppet/indirector/terminus'
+require 'hiera_puppet'
 
 class Puppet::Indirector::Hiera < Puppet::Indirector::Terminus
   def initialize(*args)
@@ -9,31 +10,11 @@ class Puppet::Indirector::Hiera < Puppet::Indirector::Terminus
   end
 
   def find(request)
-    hiera.lookup(request.key, nil, request.options[:variables], nil, nil)
+    HieraPuppet.lookup(request.key, nil, request.options[:variables], nil, nil)
+  rescue Puppet::ParseError
+    Puppet.debug("Hiera did not find any data, returning nil from the hiera data backend")
+    nil
   end
 
-  private
-
-  def self.hiera_config
-    hiera_config = Puppet.settings[:hiera_config]
-    config = {}
-
-    if File.exist?(hiera_config)
-      config = Hiera::Config.load(hiera_config)
-    else
-      Puppet.warning "Config file #{hiera_config} not found, using Hiera defaults"
-    end
-
-    config[:logger] = 'puppet'
-    config
-  end
-
-  def self.hiera
-    @hiera ||= Hiera.new(:config => hiera_config)
-  end
-
-  def hiera
-    self.class.hiera
-  end
 end
 
