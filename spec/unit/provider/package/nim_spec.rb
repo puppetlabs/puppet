d07e0067440526f23a6e818f7b4bca7740061570
Chris Price <chris@puppetlabs.com>
2013-01-31 17:22:48 -0800

    Support RPM version numbers in NIM package provider
    This commit adds a call to `nimclient -o showres` prior to installing
    a package; this is necessary so that we can determine whether the
    package is an installp package or an RPM package.  This is
    unfortunately necessary because the installation command for specific
    versions of an RPM differs very slightly from the command for installing
    a specific installp/BFF package.
    
    We also have to do an unfortunate amount of regex-based parsing of
    the output from that command, because I have been unable to find
    a command that provides concise, easily parseable output *and* shows
    information about both RPMs and BFFs available on a NIM lpp source.

--- spec/unit/provider/package/nim_spec.rb
+++ spec/unit/provider/package/nim_spec.rb
@@ -4,6 +4,46 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:package).provider(:nim)
 
 describe provider_class do
+  context "when parsing nimclient showres output" do
+    describe "#parse_showres_output" do
+      it "should be able to parse installp/BFF package listings" do
+        nimclient_showres_output = <<END
+mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
+ @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
+ + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
+ + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
+
+END
+
+        packages = subject.send(:parse_showres_output, nimclient_showres_output)
+        Set.new(packages.keys).should == Set.new(['mypackage.foo'])
+        versions = packages['mypackage.foo']
+        ['1.2.3.1', '1.2.3.4', '1.2.3.8'].each do |version|
+          versions.has_key?(version).should == true
+          versions[version].should == :installp
+        end
+      end
+
+      it "should be able to parse RPM package listings" do
+        nimclient_showres_output = <<END
+mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
+ @@R:mypackage.foo-1.2.3-1 1.2.3-1
+ @@R:mypackage.foo-1.2.3-4 1.2.3-4
+ @@R:mypackage.foo-1.2.3-8 1.2.3-8
+
+END
+
+        packages = subject.send(:parse_showres_output, nimclient_showres_output)
+        Set.new(packages.keys).should == Set.new(['mypackage.foo'])
+        versions = packages['mypackage.foo']
+        ['1.2.3-1', '1.2.3-4', '1.2.3-8'].each do |version|
+          versions.has_key?(version).should == true
+          versions[version].should == :rpm
+        end
+      end
+    end
+  end
+
   before(:each) do
     # Create a mock resource
     @resource = stub 'resource'
@@ -12,7 +52,7 @@ describe provider_class do
     @resource.stubs(:[]).returns(nil)
 
     # But set name and source
-    @resource.stubs(:[]).with(:name).returns "mypackage"
+    @resource.stubs(:[]).with(:name).returns "mypackage.foo"
     @resource.stubs(:[]).with(:source).returns "mysource"
     @resource.stubs(:[]).with(:ensure).returns :installed
 
@@ -28,14 +68,56 @@ describe provider_class do
   describe "when installing" do
     it "should install a package" do
       @resource.stubs(:should).with(:ensure).returns(:installed)
-      @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage")
+      @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo")
       @provider.install
     end
 
-    it "should install a versioned package" do
-      @resource.stubs(:should).with(:ensure).returns("1.2.3.4")
-      @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage 1.2.3.4")
-      @provider.install
+    context "when installing versioned packages" do
+
+      it "should fail if the package is not available on the lpp source" do
+        nimclient_showres_output = ""
+
+        @resource.stubs(:should).with(:ensure).returns("1.2.3.4")
+        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.4'").returns(nimclient_showres_output)
+        expect {
+          @provider.install
+        }.to raise_error(Puppet::Error, "Unable to find package 'mypackage.foo' with version '1.2.3.4' on lpp_source 'mysource'")
+      end
+
+      it "should succeed if a BFF/installp package is available on the lpp source" do
+          nimclient_sequence = sequence('nimclient')
+
+          nimclient_showres_output = <<END
+mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
+ @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
+ + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
+ + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
+
+END
+
+          @resource.stubs(:should).with(:ensure).returns("1.2.3.4")
+          Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.4'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+          @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo 1.2.3.4").in_sequence(nimclient_sequence)
+          @provider.install
+        end
+
+      it "should succeed if an RPM package is available on the lpp source" do
+        nimclient_sequence = sequence('nimclient')
+
+        nimclient_showres_output = <<END
+mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
+ @@R:mypackage.foo-1.2.3-1 1.2.3-1
+ @@R:mypackage.foo-1.2.3-4 1.2.3-4
+ @@R:mypackage.foo-1.2.3-8 1.2.3-8
+
+END
+
+        @resource.stubs(:should).with(:ensure).returns("1.2.3-4")
+        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\-4'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+        @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo-1.2.3-4").in_sequence(nimclient_sequence)
+        @provider.install
+      end
+
     end
   end
 end
