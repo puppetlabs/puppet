fd9edcae96e108cc43f57d1ada0908a18c1c4f5c
Chris Price <chris@puppetlabs.com>
2013-02-05 12:24:28 -0800

    DRY up spec tests for nim package provider

--- spec/unit/provider/package/nim_spec.rb
+++ spec/unit/provider/package/nim_spec.rb
@@ -26,18 +26,31 @@ describe provider_class do
     @provider.should respond_to(:install)
   end
 
-  context "when installing" do
-    it "should install a package" do
-      nimclient_showres_output = <<END
+  let(:bff_showres_output) {
+    <<END
 mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
  @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
  + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
  + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
 
 END
+  }
+
+  let(:rpm_showres_output) {
+    <<END
+mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
+ @@R:mypackage.foo-1.2.3-1 1.2.3-1
+ @@R:mypackage.foo-1.2.3-4 1.2.3-4
+ @@R:mypackage.foo-1.2.3-8 1.2.3-8
+
+END
+  }
+
+  context "when installing" do
+    it "should install a package" do
 
       @resource.stubs(:should).with(:ensure).returns(:installed)
-      Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo'").returns(nimclient_showres_output)
+      Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo'").returns(bff_showres_output)
       @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo 1.2.3.8")
       @provider.install
     end
@@ -57,30 +70,14 @@ END
       it "should succeed if a BFF/installp package is available on the lpp source" do
         nimclient_sequence = sequence('nimclient')
 
-        nimclient_showres_output = <<END
-mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
- @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
- + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
- + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
-
-END
-
         @resource.stubs(:should).with(:ensure).returns("1.2.3.4")
-        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.4'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.4'").returns(bff_showres_output).in_sequence(nimclient_sequence)
         @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo 1.2.3.4").in_sequence(nimclient_sequence)
         @provider.install
       end
 
       it "should fail if the specified version of a BFF package is superseded" do
         nimclient_sequence = sequence('nimclient')
-
-        nimclient_showres_output = <<END
-mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
- @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
- + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
- + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
-
-END
 
         install_output = <<OUTPUT
 +-----------------------------------------------------------------------------+
@@ -128,7 +125,7 @@ mypackage.foo              1.2.3.1         Already superseded by 1.2.3.4
 OUTPUT
 
         @resource.stubs(:should).with(:ensure).returns("1.2.3.1")
-        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.1'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\.1'").returns(bff_showres_output).in_sequence(nimclient_sequence)
         @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo 1.2.3.1").in_sequence(nimclient_sequence).returns(install_output)
 
         expect { @provider.install }.to raise_error(Puppet::Error, "NIM package provider is unable to downgrade packages")
@@ -138,16 +135,8 @@ OUTPUT
     it "should succeed if an RPM package is available on the lpp source" do
         nimclient_sequence = sequence('nimclient')
 
-        nimclient_showres_output = <<END
-mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
- @@R:mypackage.foo-1.2.3-1 1.2.3-1
- @@R:mypackage.foo-1.2.3-4 1.2.3-4
- @@R:mypackage.foo-1.2.3-8 1.2.3-8
-
-END
-
         @resource.stubs(:should).with(:ensure).returns("1.2.3-4")
-        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\-4'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+        Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\-4'").returns(rpm_showres_output).in_sequence(nimclient_sequence)
         @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo-1.2.3-4").in_sequence(nimclient_sequence)
         @provider.install
       end
@@ -155,14 +144,6 @@ END
 
     it "should fail if the specified version of a RPM package is superseded" do
       nimclient_sequence = sequence('nimclient')
-
-      nimclient_showres_output = <<END
-mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
- @@R:mypackage.foo-1.2.3-1 1.2.3-1
- @@R:mypackage.foo-1.2.3-4 1.2.3-4
- @@R:mypackage.foo-1.2.3-8 1.2.3-8
-
-END
 
       install_output = <<OUTPUT
 
@@ -182,7 +163,7 @@ mypackage.foo-1.2.3-1 is superseded by mypackage.foo-1.2.3-4
 OUTPUT
 
       @resource.stubs(:should).with(:ensure).returns("1.2.3-1")
-      Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\-1'").returns(nimclient_showres_output).in_sequence(nimclient_sequence)
+      Puppet::Util.expects(:execute).with("nimclient -o showres -a resource=mysource |grep -p -E 'mypackage\\.foo( |-)1\\.2\\.3\\-1'").returns(rpm_showres_output).in_sequence(nimclient_sequence)
       @provider.expects(:nimclient).with("-o", "cust", "-a", "installp_flags=acgwXY", "-a", "lpp_source=mysource", "-a", "filesets=mypackage.foo-1.2.3-1").in_sequence(nimclient_sequence).returns(install_output)
 
       expect { @provider.install }.to raise_error(Puppet::Error, "NIM package provider is unable to downgrade packages")
@@ -211,15 +192,7 @@ OUTPUT
   context "when parsing nimclient showres output" do
     describe "#parse_showres_output" do
       it "should be able to parse installp/BFF package listings" do
-        nimclient_showres_output = <<END
-mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
- @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
- + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
- + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
-
-END
-
-        packages = subject.send(:parse_showres_output, nimclient_showres_output)
+        packages = subject.send(:parse_showres_output, bff_showres_output)
         Set.new(packages.keys).should == Set.new(['mypackage.foo'])
         versions = packages['mypackage.foo']
         ['1.2.3.1', '1.2.3.4', '1.2.3.8'].each do |version|
@@ -229,15 +202,7 @@ END
       end
 
       it "should be able to parse RPM package listings" do
-        nimclient_showres_output = <<END
-mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
- @@R:mypackage.foo-1.2.3-1 1.2.3-1
- @@R:mypackage.foo-1.2.3-4 1.2.3-4
- @@R:mypackage.foo-1.2.3-8 1.2.3-8
-
-END
-
-        packages = subject.send(:parse_showres_output, nimclient_showres_output)
+        packages = subject.send(:parse_showres_output, rpm_showres_output)
         Set.new(packages.keys).should == Set.new(['mypackage.foo'])
         versions = packages['mypackage.foo']
         ['1.2.3-1', '1.2.3-4', '1.2.3-8'].each do |version|
@@ -250,14 +215,7 @@ END
     describe "#determine_latest_version" do
       context "when there are multiple versions" do
         it "should return the latest version" do
-          nimclient_showres_output = <<END
-mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
- @@R:mypackage.foo-1.2.3-1 1.2.3-1
- @@R:mypackage.foo-1.2.3-8 1.2.3-8
- @@R:mypackage.foo-1.2.3-4 1.2.3-4
-
-END
-          subject.send(:determine_latest_version, nimclient_showres_output, 'mypackage.foo').should == [:rpm, '1.2.3-8']
+          subject.send(:determine_latest_version, rpm_showres_output, 'mypackage.foo').should == [:rpm, '1.2.3-8']
         end
       end
 
@@ -276,25 +234,11 @@ END
 
     describe "#determine_package_type" do
       it "should return :rpm for rpm packages" do
-        nimclient_showres_output = <<END
-mypackage.foo                                                                ALL  @@R:mypackage.foo _all_filesets
- @@R:mypackage.foo-1.2.3-1 1.2.3-1
- @@R:mypackage.foo-1.2.3-4 1.2.3-4
- @@R:mypackage.foo-1.2.3-8 1.2.3-8
-
-END
-        subject.send(:determine_package_type, nimclient_showres_output, 'mypackage.foo', '1.2.3-4').should == :rpm
+        subject.send(:determine_package_type, rpm_showres_output, 'mypackage.foo', '1.2.3-4').should == :rpm
       end
 
       it "should return :installp for installp/bff packages" do
-        nimclient_showres_output = <<END
-mypackage.foo                                                           ALL  @@I:mypackage.foo _all_filesets
- @ 1.2.3.1  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.1
- + 1.2.3.4  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.4
- + 1.2.3.8  MyPackage Runtime Environment                       @@I:mypackage.foo 1.2.3.8
-
-END
-        subject.send(:determine_package_type, nimclient_showres_output, 'mypackage.foo', '1.2.3.4').should == :installp
+        subject.send(:determine_package_type, bff_showres_output, 'mypackage.foo', '1.2.3.4').should == :installp
       end
     end
   end
