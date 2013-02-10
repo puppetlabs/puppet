01290fb15b81f18546455f45ed25b40d67428eb9
Stefan Schulte <stefan.schulte@taunusstein.net>
2013-02-10 23:26:38 +0100

    (maint) Simplify systemd spec tests
    It seems to be easier to deal with real object instances instead of fake
    objects.

--- spec/unit/provider/service/systemd_spec.rb
+++ spec/unit/provider/service/systemd_spec.rb
@@ -1,38 +1,34 @@
 #! /usr/bin/env ruby
 #
-# Unit testing for the RedHat service Provider
+# Unit testing for the systemd service Provider
 #
 require 'spec_helper'
 
-provider_class = Puppet::Type.type(:service).provider(:systemd)
-
-describe provider_class do
+describe Puppet::Type.type(:service).provider(:systemd) do
   before :each do
-    @class = Puppet::Type.type(:service).provider(:redhat)
-    @resource = stub 'resource'
-    @resource.stubs(:[]).returns(nil)
-    @resource.stubs(:[]).with(:name).returns "myservice.service"
-    @provider = provider_class.new
-    @resource.stubs(:provider).returns @provider
-    @provider.resource = @resource
+    Puppet::Type.type(:service).stubs(:defaultprovider).returns described_class
+  end
+
+  let :provider do
+    described_class.new(:name => 'myservice.service')
   end
 
   [:enabled?, :enable, :disable, :start, :stop, :status, :restart].each do |method|
     it "should have a #{method} method" do
-      @provider.should respond_to(method)
+      provider.should respond_to(method)
     end
   end
 
 
   it 'should return resources from self.instances' do
-    provider_class.expects(:systemctl).with('list-units', '--full', '--all',  '--no-pager').returns(
+    described_class.expects(:systemctl).with('list-units', '--full', '--all',  '--no-pager').returns(
       "my_service loaded active running\nmy_other_service loaded active running"
     )
-    provider_class.instances.map {|provider| provider.name}.should =~ ["my_service","my_other_service"]
+    described_class.instances.map {|provider| provider.name}.should =~ ["my_service","my_other_service"]
   end
 
   it "(#16451) has command systemctl without being fully qualified" do
-    provider_class.instance_variable_get(:@commands).
+    described_class.instance_variable_get(:@commands).
       should include(:systemctl => 'systemctl')
   end
 
