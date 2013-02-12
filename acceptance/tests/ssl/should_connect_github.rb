33e822a2e09237d38506692c967b54e19d991501
Josh Cooper <josh@puppetlabs.com>
2013-02-12 11:14:29 -0800

    (#18978) Redo acceptance test
    The previous test did not work when executed through the acceptance
    framework due to quoting issues. I fixed it on *nix, but it fails on
    Windows due to the RUBYLIB not being set correctly.
    
    This commit just changes the test to ensure we can authenticate the
    forge's SSL cert, which is the main motivation behind #18978 anyways.
    But since the module tool is not yet supported (#11276), this test is
    skipped on windows.

--- acceptance/tests/ssl/should_connect_github.rb
+++ acceptance/tests/ssl/should_connect_github.rb
@@ -1,26 +1,8 @@
-test_name "puppet should be able to authenticate to a well-known SSL server"
+test_name "puppet should be able to authenticate the forge"
 
-script = <<EOM
-#! /usr/bin/env ruby
-require 'rubygems'
-require 'openssl'
-require 'net/https'
-require 'puppet'
+# See #11276
+confine :except, :platform => 'windows'
 
-cert_store = OpenSSL::X509::Store.new
-cert_store.set_default_paths
-
-conn = Net::HTTP.new('github.com', 443)
-conn.use_ssl     = true
-conn.verify_mode = OpenSSL::SSL::VERIFY_PEER
-conn.cert_store  = cert_store
-
-conn.start {|c| puts "connected" }
-EOM
-
-step "connect to github.com"
 agents.each do |agent|
-  on(agents, "ruby -e #{script}") do
-    assert_match(/\Aconnected\Z/, stdout)
-  end
+  on(agent, puppet("module search stdlib"))
 end
