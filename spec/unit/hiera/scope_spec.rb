45c707dac560c11eea8711c5388b5d42705b474a
Andrew Parker <andy@puppetlabs.com>
2013-02-22 02:09:53 -0800

    (Maint) Use real scope in testing
    Using the real scope is completely possible in these tests since it has
    no dependencies on the outside world and is essentially a value object.
    Therefore it should be used in the tests to make them more
    comprehensive.

--- spec/unit/hiera/scope_spec.rb
+++ spec/unit/hiera/scope_spec.rb
@@ -2,48 +2,39 @@ require 'spec_helper'
 require 'hiera/scope'
 
 describe Hiera::Scope do
+  let(:real) { Puppet::Parser::Scope.new_for_test_harness("test_node") }
+  let(:scope) { Hiera::Scope.new(real) }
+
   describe "#initialize" do
     it "should store the supplied puppet scope" do
-      real = {}
-      scope = Hiera::Scope.new(real)
       scope.real.should == real
     end
   end
 
   describe "#[]" do
     it "should return nil when no value is found" do
-      real = mock
-      real.expects(:lookupvar).with("foo").returns(nil)
-
-      scope = Hiera::Scope.new(real)
       scope["foo"].should == nil
     end
 
     it "should treat '' as nil" do
-      real = mock
-      real.expects(:lookupvar).with("foo").returns("")
+      real["foo"] = ""
 
-      scope = Hiera::Scope.new(real)
       scope["foo"].should == nil
     end
 
     it "sould return found data" do
-      real = mock
-      real.expects(:lookupvar).with("foo").returns("bar")
+      real["foo"] = "bar"
 
-      scope = Hiera::Scope.new(real)
       scope["foo"].should == "bar"
     end
 
     it "should get calling_class and calling_module from puppet scope" do
-      real = Puppet::Parser::Scope.new_for_test_harness("test_node")
       source = mock
       source.expects(:type).returns(:hostclass).once
       source.expects(:name).returns("Foo::Bar").once
       source.expects(:module_name).returns("foo").once
       real.expects(:source).returns(source).at_least_once
 
-      scope = Hiera::Scope.new(real)
       scope["calling_class"].should == "foo::bar"
       scope["calling_module"].should == "foo"
     end
@@ -51,19 +42,12 @@ describe Hiera::Scope do
 
   describe "#include?" do
     it "should correctly report missing data" do
-      real = mock
-      real.expects(:lookupvar).with("foo").returns("")
+      real["foo"] = ""
 
-      scope = Hiera::Scope.new(real)
       scope.include?("foo").should == false
     end
 
     it "should always return true for calling_class and calling_module" do
-      real = mock
-      real.expects(:lookupvar).with("calling_class").never
-      real.expects(:lookupvar).with("calling_module").never
-
-      scope = Hiera::Scope.new(real)
       scope.include?("calling_class").should == true
       scope.include?("calling_module").should == true
     end
