8c769aa2c25926bcf244cf127ce5bbc41c06f645
Andrew Parker <andy@puppetlabs.com>
2013-03-11 14:25:47 -0700

    (Maint) Do not try to cound objects on ruby 1.8
    ObjectSpace.count_objects only exists on 1.9 or greater (in fact 1.9.2
    or greater, but be only support 1.9.3). This makes the test for this
    functionality pending on ruby 1.8 so that we can have the functionality,
    but not test it where it can't work.

--- spec/unit/util/profiler/object_counts_spec.rb
+++ spec/unit/util/profiler/object_counts_spec.rb
@@ -3,10 +3,12 @@ require 'puppet/util/profiler'
 
 describe Puppet::Util::Profiler::ObjectCounts do
   it "reports the changes in the system object counts" do
-    profiler = Puppet::Util::Profiler::ObjectCounts.new(nil, nil)
+    pending("Can only count objects on ruby 1.9 or greater", :if => RUBY_VERSION < '1.9') do
+      profiler = Puppet::Util::Profiler::ObjectCounts.new(nil, nil)
 
-    message = profiler.finish(profiler.start)
+      message = profiler.finish(profiler.start)
 
-    message.should =~ / T_STRING: \d+, /
+      message.should =~ / T_STRING: \d+, /
+    end
   end
 end
