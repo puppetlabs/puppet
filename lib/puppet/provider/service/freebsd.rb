49757640876fd454f82467ea2eec2d185f09c50b
robertomulabi <robertomulabi@gmail.com>
2013-03-14 12:45:59 -0300

    Fix service resource on FreeBSD
    The command:
    puppet resource service puppet enable=true
    will wrongly append to rc.conf:
    puppet_enable_enable="YES"
    What should be appended is:
    puppet_enable="YES"
    The regexp /(.*)(_enable)?=(.*)/ is wrong because the first (.*) consumes the _enable, which is then used as the rcvar_name.

--- lib/puppet/provider/service/freebsd.rb
+++ lib/puppet/provider/service/freebsd.rb
@@ -41,7 +41,7 @@ Puppet::Type.type(:service).provide :freebsd, :parent => :init do
   def rcvar_name
     name = self.rcvar[1]
     self.error("No rcvar name found in rcvar") if name.nil?
-    name = name.gsub!(/(.*)(_enable)?=(.*)/, '\1')
+    name = name.gsub!(/(.*?)(_enable)?=(.*)/, '\1')
     self.error("rcvar name is empty") if name.nil?
     self.debug("rcvar name is #{name}")
     name
