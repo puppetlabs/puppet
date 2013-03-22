puppetlabs/puppet

commit f64e0a94c60506eec44645b07c36ff996ee2b5cd

(#17190) Provide more benchmarking info during compile
Many users find that they have very little insight into what the
compiler is doing and spending time on during compilation. This provides
extra logging for getting much finer grained information.

lib/puppet/indirector/catalog/compiler.rb
@@ -16,20 +16,22 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
       raise ArgumentError, "Facts but no fact format provided for #{request.key}"
     end
 
-    # If the facts were encoded as yaml, then the param reconstitution system
-    # in Network::HTTP::Handler will automagically deserialize the value.
-    if text_facts.is_a?(Puppet::Node::Facts)
-      facts = text_facts
-    else
-      facts = Puppet::Node::Facts.convert_from(format, text_facts)
-    end
+    benchmark(:notice, "Found facts") do
+      # If the facts were encoded as yaml, then the param reconstitution system
+      # in Network::HTTP::Handler will automagically deserialize the value.
+      if text_facts.is_a?(Puppet::Node::Facts)
+        facts = text_facts
+      else
+        facts = Puppet::Node::Facts.convert_from(format, text_facts)
+      end
 
-    unless facts.name == request.key
-      raise Puppet::Error, "Catalog for #{request.key.inspect} was requested with fact definition for the wrong node (#{facts.name.inspect})."
-    end
+      unless facts.name == request.key
+        raise Puppet::Error, "Catalog for #{request.key.inspect} was requested with fact definition for the wrong node (#{facts.name.inspect})."
+      end
 
-    facts.add_timestamp
-    Puppet::Node::Facts.indirection.save(facts)
+      facts.add_timestamp
+      Puppet::Node::Facts.indirection.save(facts)
+    end
   end
 
   # Compile a node's catalog.
@@ -54,7 +56,9 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
   end
 
   def initialize
-    set_server_facts
+    benchmark(:notice, "Setup server facts for compiling") do
+      set_server_facts
+    end
   end
 
   # Is our compiler part of a network, or are we just local?
@@ -76,9 +80,7 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
     str += " in environment #{node.environment}" if node.environment
     config = nil
 
-    loglevel = networked? ? :notice : :none
-
-    benchmark(loglevel, str) do
+    benchmark(:notice, str) do
       begin
         config = Puppet::Parser::Compiler.compile(node)
       rescue Puppet::Error => detail
@@ -92,17 +94,22 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
 
   # Turn our host name into a node object.
   def find_node(name, *args)
-    begin
-      return nil unless node = Puppet::Node.indirection.find(name, *args)
-    rescue => detail
-      message = "Failed when searching for node #{name}: #{detail}"
-      Puppet.log_exception(detail, message)
-      raise Puppet::Error, message
+    node = nil
+    benchmark(:notice, "Found node information") do
+      begin
+        node = Puppet::Node.indirection.find(name)
+      rescue => detail
+        message = "Failed when searching for node #{name}: #{detail}"
+        Puppet.log_exception(detail, message)
+        raise Puppet::Error, message
+      end
+
+
+      # Add any external data to the node.
+      if node
+        add_node_data(node)
+      end
     end
-
-
-    # Add any external data to the node.
-    add_node_data(node)
 
     node
   end

spec/unit/indirector/catalog/compiler_spec.rb
@@ -34,11 +34,6 @@ describe Puppet::Resource::Catalog::Compiler do
       compiler.find(Puppet::Indirector::Request.new(:catalog, :find, 'node1', nil, :node => 'node1'))
       compiler.find(Puppet::Indirector::Request.new(:catalog, :find, 'node2', nil, :node => 'node2'))
     end
-
-    it "should provide a method for determining if the catalog is networked" do
-      compiler = Puppet::Resource::Catalog::Compiler.new
-      compiler.should respond_to(:networked?)
-    end
   end
 
   describe "when finding catalogs" do
@@ -134,26 +129,6 @@ describe Puppet::Resource::Catalog::Compiler do
       Puppet::Parser::Compiler.expects(:compile).returns result
       @compiler.find(@request).should equal(result)
     end
-
-    it "should benchmark the compile process" do
-      Puppet::Node.indirection.stubs(:find).returns(@node)
-      @compiler.stubs(:networked?).returns(true)
-      @compiler.expects(:benchmark).with do |level, message|
-        level == :notice and message =~ /^Compiled catalog/
-      end
-      Puppet::Parser::Compiler.stubs(:compile)
-      @compiler.find(@request)
-    end
-
-    it "should log the benchmark result" do
-      Puppet::Node.indirection.stubs(:find).returns(@node)
-      @compiler.stubs(:networked?).returns(true)
-      Puppet::Parser::Compiler.stubs(:compile)
-
-      Puppet.expects(:notice).with { |msg| msg =~ /Compiled catalog/ }
-
-      @compiler.find(@request)
-    end
   end
 
   describe "when extracting facts from the request" do
@@ -180,7 +155,7 @@ describe Puppet::Resource::Catalog::Compiler do
 
       @facts.timestamp = Time.parse('2010-11-01')
       @now = Time.parse('2010-11-02')
-      Time.expects(:now).returns(@now)
+      Time.stubs(:now).returns(@now)
 
       @compiler.extract_facts_from_request(@request)
       @facts.timestamp.should == @now

lib/puppet/parser/compiler.rb
@@ -90,18 +90,19 @@ class Puppet::Parser::Compiler
   # This is the main entry into our catalog.
   def compile
     # Set the client's parameters into the top scope.
-    set_node_parameters
-    create_settings_scope
+    Puppet::Util.benchmark(:notice, "Compile: Set node parameters") { set_node_parameters }
 
-    evaluate_main
+    Puppet::Util.benchmark(:notice, "Compile: Created settings scope") { create_settings_scope }
 
-    evaluate_ast_node
+    Puppet::Util.benchmark(:notice, "Compile: Evaluated main") { evaluate_main }
 
-    evaluate_node_classes
+    Puppet::Util.benchmark(:notice, "Compile: Evaluated AST node") { evaluate_ast_node }
 
-    evaluate_generators
+    Puppet::Util.benchmark(:notice, "Compile: Evaluated node classes") { evaluate_node_classes }
 
-    finish
+    Puppet::Util.benchmark(:notice, "Compile: Evaluated generators") { evaluate_generators }
+
+    Puppet::Util.benchmark(:notice, "Compile: Finished catalog") { finish }
 
     fail_on_unevaluated
 
@@ -231,8 +232,10 @@ class Puppet::Parser::Compiler
       # We have to iterate over a dup of the array because
       # collections can delete themselves from the list, which
       # changes its length and causes some collections to get missed.
-      @collections.dup.each do |collection|
-        found_something = true if collection.evaluate
+      Puppet::Util.benchmark(:notice, "Evaluated collections") do
+        @collections.dup.each do |collection|
+          found_something = true if collection.evaluate
+        end
       end
     end
 
@@ -244,7 +247,15 @@ class Puppet::Parser::Compiler
   # evaluate_generators loop.
   def evaluate_definitions
     exceptwrap do
-      !unevaluated_resources.each { |resource| resource.evaluate }.empty?
+      more = nil
+      Puppet::Util.benchmark(:notice, "Evaluated definitions") do
+        more = !unevaluated_resources.each { |resource|
+          Puppet::Util.benchmark(:notice, "Evaluated resource #{resource}") do
+            resource.evaluate
+          end
+        }.empty?
+      end
+      more
     end
   end
 
@@ -257,9 +268,12 @@ class Puppet::Parser::Compiler
     loop do
       done = true
 
-      # Call collections first, then definitions.
-      done = false if evaluate_collections
-      done = false if evaluate_definitions
+      Puppet::Util.benchmark(:notice, "Iterated (#{count + 1}) on generators") do
+        # Call collections first, then definitions.
+        done = false if evaluate_collections
+        done = false if evaluate_definitions
+      end
+
       break if done
 
       count += 1

lib/puppet/indirector/indirection.rb
@@ -199,11 +199,14 @@ class Puppet::Indirector::Indirection
           cache.save request(:save, key, result, options)
         end
 
+        filtered = result
         if terminus.respond_to?(:filter)
-          terminus.filter(result)
-        else
-          result
+          Puppet::Util.benchmark(:notice, "Filtered result for #{self.name} #{request.key}") do
+            filtered = terminus.filter(result)
+          end
         end
+
+        filtered
       end
     end
   end

lib/puppet/network/http/handler.rb
@@ -63,12 +63,14 @@ module Puppet::Network::HTTP::Handler
 
   # handle an HTTP request
   def process(request, response)
-    indirection, method, key, params = uri2indirection(http_method(request), path(request), params(request))
+    Puppet::Util.benchmark(:notice, "Processed request #{http_method(request)} #{path(request)}") do
+      indirection, method, key, params = uri2indirection(http_method(request), path(request), params(request))
 
-    check_authorization(indirection, method, key, params)
-    warn_if_near_expiration(client_cert(request))
+      check_authorization(indirection, method, key, params)
+      warn_if_near_expiration(client_cert(request))
 
-    send("do_#{method}", indirection, key, params, request, response)
+      send("do_#{method}", indirection, key, params, request, response)
+    end
   rescue SystemExit,NoMemoryError
     raise
   rescue Exception => e
@@ -118,10 +120,15 @@ module Puppet::Network::HTTP::Handler
     format = format_to_use(request)
     set_content_type(response, format)
 
+    rendered_result = result
     if result.respond_to?(:render)
-      set_response(response, result.render(format))
-    else
-      set_response(response, result)
+      Puppet::Util.benchmark(:notice, "Rendered result in #{format}") do
+       rendered_result = result.render(format)
+      end
+    end
+
+    Puppet::Util.benchmark(:notice, "Sent response") do
+      set_response(response, rendered_result)
     end
   end
 

lib/puppet/util.rb
@@ -178,7 +178,7 @@ module Util
       seconds = Benchmark.realtime {
         yield
       }
-      object.send(level, msg + (" in %0.2f seconds" % seconds))
+      object.send(level, "BENCHMARK: " + msg + (" in %0.2f seconds" % seconds))
       return seconds
     else
       yield

spec/unit/network/http/handler_spec.rb
@@ -81,9 +81,9 @@ describe Puppet::Network::HTTP::Handler do
     end
 
     it "should create an indirection request from the path, parameters, and http method" do
-      @handler.expects(:path).with(@request).returns "mypath"
-      @handler.expects(:http_method).with(@request).returns "mymethod"
-      @handler.expects(:params).with(@request).returns "myparams"
+      @handler.stubs(:path).with(@request).returns "mypath"
+      @handler.stubs(:http_method).with(@request).returns "mymethod"
+      @handler.stubs(:params).with(@request).returns "myparams"
 
       @handler.expects(:uri2indirection).with("mymethod", "mypath", "myparams").returns stub("request", :method => :find)
 

commit 95681a1e0d1ec9c7ca10fc85f4e2ebab6a7e2d6a

(#17190) Re-introduce environment for catalog compile
Commit f64e0a94c60506eec44645b07c36ff996ee2b5cd accidentally removed the
pass-through of `*args`, which removed the passing of `:environment` to
the Puppet::Node indirection. This adds a test that would have failed
and restructures the code so that the mistake would have been more
obvious.

lib/puppet/indirector/catalog/compiler.rb
@@ -94,11 +94,11 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
   end
 
   # Turn our host name into a node object.
-  def find_node(name, *args)
+  def find_node(name, environment)
     Puppet::Util::Profiler.profile("Found node information") do
       node = nil
       begin
-        node = Puppet::Node.indirection.find(name)
+        node = Puppet::Node.indirection.find(name, :environment => environment)
       rescue => detail
         message = "Failed when searching for node #{name}: #{detail}"
         Puppet.log_exception(detail, message)
@@ -133,7 +133,7 @@ class Puppet::Resource::Catalog::Compiler < Puppet::Indirector::Code
     # node's catalog with only one certificate and a modification to auth.conf 
     # If no key is provided we can only compile the currently connected node.
     name = request.key || request.node
-    if node = find_node(name, :environment => request.environment)
+    if node = find_node(name, request.environment)
       return node
     end
 

spec/unit/indirector/catalog/compiler_spec.rb
@@ -28,8 +28,8 @@ describe Puppet::Resource::Catalog::Compiler do
       node1 = stub 'node1', :merge => nil
       node2 = stub 'node2', :merge => nil
       compiler.stubs(:compile)
-      Puppet::Node.indirection.stubs(:find).with('node1', anything).returns(node1)
-      Puppet::Node.indirection.stubs(:find).with('node2', anything).returns(node2)
+      Puppet::Node.indirection.stubs(:find).with('node1', has_entry(:environment => anything)).returns(node1)
+      Puppet::Node.indirection.stubs(:find).with('node2', has_entry(:environment => anything)).returns(node2)
 
       compiler.find(Puppet::Indirector::Request.new(:catalog, :find, 'node1', nil, :node => 'node1'))
       compiler.find(Puppet::Indirector::Request.new(:catalog, :find, 'node2', nil, :node => 'node2'))