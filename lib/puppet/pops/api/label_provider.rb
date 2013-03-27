caa2d7c8b053b9cee0a03b400a69136e2b62cc17
Andrew Parker <andy@puppetlabs.com>
2013-03-27 15:34:42 -0700

    (Maint) Fix label provider on 1.8.7
    String subscripting works differently on 1.8.7 from 1.9.3.

--- lib/puppet/pops/api/label_provider.rb
+++ lib/puppet/pops/api/label_provider.rb
@@ -5,6 +5,12 @@ module Puppet::Pops::API
   # This simple implementation calls #to_s on the given object, and handles articles 'a/an/the'.
   #
   class LabelProvider
+    VOWELS = %w{a e i o u y}
+    CONSONANTS = %w{b c d f g h j k l m n p q r s t v w x z}
+    SKIPPED_CHARACTERS = %w{" '}
+    A = "a"
+    AN = "an"
+
     # Provides a label for the given object by calling `to_s` on the object.
     # The intent is for this method to be overridden in concrete label providers.
     def label o
@@ -20,7 +26,7 @@ module Puppet::Pops::API
     # Produces a label for the given objects type/operator with indefinite article (A/An)
     def a_an_uc o
       text = label(o)
-      "#{article(text,true)} #{text}"
+      "#{article(text).capitalize} #{text}"
     end
 
     # Produces a label for the given object with *definitie article* (the).
@@ -33,23 +39,39 @@ module Puppet::Pops::API
       "The #{label(o)}"
     end
 
+  private
+
     # Produces an *indefinite article* (a/an) for the given text ('a' if it starts with a vowel)
     # This is obviously flawed in the general sense as may labels have punctuation at the start and
     # this method does not translate punctuation to English words. Also, if a vowel is pronounced
     # as a consonant, the article should not be "an".
     #
-    def article s, capitalize = false
-      char = s[0,1]
-      # skip an initial quote to pick first real char
-      char = s[1] if char == '\'' || char == '"'
-      char = char.downcase if char
-      if %w{a e i o u y}.include? char
-        result = "an"
-      else
-        result = "a"
+    def article s
+      article_for_letter(first_letter_of(s))
+    end
+
+    def first_letter_of(string)
+      char = string[0,1]
+      if SKIPPED_CHARACTERS.include? char
+        char = string[1,1]
       end
-      result = result.capitalize if capitalize
-      result
+
+      if char == ""
+        raise Puppet::DevError, "<#{string}> does not appear to contain a word"
+      end
+
+      char
+    end
+
+    def article_for_letter(letter)
+      downcased = letter.downcase
+      if VOWELS.include? downcased
+        AN
+      elsif CONSONANTS.include? downcased
+        A
+      else
+        raise Puppet::DevError, "Cannot determine article for word starting with <#{downcased}>"
+      end
     end
   end
 end
