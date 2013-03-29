8b7d3feef9721c9db15d961843c3b2c916813908
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-03-28 18:20:31 -0700

    Docs: Note in config reference that puppet master needs a restart to notice hiera.yaml edits

--- lib/puppet/defaults.rb
+++ lib/puppet/defaults.rb
@@ -214,7 +214,7 @@ module Puppet
     :node_cache_terminus => {
       :type       => :terminus,
       :default    => nil,
-      :desc       => "How to store cached nodes. 
+      :desc       => "How to store cached nodes.
       Valid values are (none), 'json', 'yaml' or write only yaml ('write_only_yaml').
       The master application defaults to 'write_only_yaml', all others to none.",
     },
@@ -225,7 +225,7 @@ module Puppet
     },
     :hiera_config => {
       :default => "$confdir/hiera.yaml",
-      :desc    => "The hiera configuration file",
+      :desc    => "The hiera configuration file. Puppet only reads this file on startup, so you must restart the puppet master every time you edit it.",
       :type    => :file,
     },
     :catalog_terminus => {
