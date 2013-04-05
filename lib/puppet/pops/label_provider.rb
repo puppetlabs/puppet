e0f9c758ef1df3581e873e99fa2dbe5755e093f4
Patrick Carlisle <patrick@puppetlabs.com>
2013-04-05 13:58:22 -0700

    (Maint) Label non-word characters with 'a' in label provider
    This restores the previous behavior of the LabelProvider to accept strings
    beginning with non-word characters and prepend them with 'a'.

--- lib/puppet/pops/label_provider.rb
+++ lib/puppet/pops/label_provider.rb
@@ -3,7 +3,6 @@
 #
 class Puppet::Pops::LabelProvider
   VOWELS = %w{a e i o u y}
-  CONSONANTS = %w{b c d f g h j k l m n p q r s t v w x z}
   SKIPPED_CHARACTERS = %w{" '}
   A = "a"
   AN = "an"
@@ -14,34 +13,35 @@ class Puppet::Pops::LabelProvider
     o.to_s
   end
 
-  # Produces a label for the given objects type/operator with indefinite article (a/an)
+  # Produces a label for the given text with indefinite article (a/an)
   def a_an o
     text = label(o)
     "#{article(text)} #{text}"
   end
 
-  # Produces a label for the given objects type/operator with indefinite article (A/An)
+  # Produces a label for the given text with indefinite article (A/An)
   def a_an_uc o
     text = label(o)
     "#{article(text).capitalize} #{text}"
   end
 
-  # Produces a label for the given object with *definitie article* (the).
+  # Produces a label for the given text with *definitie article* (the).
   def the o
     "the #{label(o)}"
   end
 
-  # Produces a label for the given object with *definitie article* (The).
+  # Produces a label for the given text with *definitie article* (The).
   def the_uc o
     "The #{label(o)}"
   end
 
-private
+  private
 
-  # Produces an *indefinite article* (a/an) for the given text ('a' if it starts with a vowel)
-  # This is obviously flawed in the general sense as may labels have punctuation at the start and
-  # this method does not translate punctuation to English words. Also, if a vowel is pronounced
-  # as a consonant, the article should not be "an".
+  # Produces an *indefinite article* (a/an) for the given text ('a' if
+  # it starts with a vowel) This is obviously flawed in the general
+  # sense as may labels have punctuation at the start and this method
+  # does not translate punctuation to English words. Also, if a vowel is
+  # pronounced as a consonant, the article should not be "an".
   #
   def article s
     article_for_letter(first_letter_of(s))
@@ -64,10 +64,8 @@ private
     downcased = letter.downcase
     if VOWELS.include? downcased
       AN
-    elsif CONSONANTS.include? downcased
-      A
     else
-      raise Puppet::DevError, "Cannot determine article for word starting with <#{downcased}>"
+      A
     end
   end
 end
--- spec/unit/pops/label_provider_spec.rb
+++ spec/unit/pops/label_provider_spec.rb
@@ -12,6 +12,10 @@ describe Puppet::Pops::LabelProvider do
     labeler.a_an('bear').should == 'a bear'
   end
 
+  it "prefixes non-word characters with an 'a'" do
+    labeler.a_an('[] expression').should == 'a [] expression'
+  end
+
   it "ignores a single quote leading the word" do
     labeler.a_an("'owl'").should == "an 'owl'"
   end
@@ -22,12 +26,6 @@ describe Puppet::Pops::LabelProvider do
 
   it "capitalizes the indefinite article for a word when requested" do
     labeler.a_an_uc('owl').should == 'An owl'
-  end
-
-  it "raises an error on a leading space" do
-    expect {
-      labeler.a_an(' owl')
-    }.to raise_error(Puppet::DevError, /Cannot determine article for word starting with < >/)
   end
 
   it "raises an error when missing a character to work with" do
