d4ced3678160ab36322c667658138bd41893f1ef
Nick Lewis <nick@puppetlabs.com>
2013-04-26 14:52:10 -0700

    (maint) Handle different ssh service names in `puppet resource service` test
    This test was using a regex to match the output for the ssh/sshd service
    from puppet resource, but was explicitly invoking the command with the
    name ssh. Since we can check the state of that service from the list of
    all instances (in which case the regex is actually beneficial), there's
    no need to look at the specific service separately. This combines the
    two steps, and makes the test more robust to different names for the ssh
    service.

--- acceptance/tests/resource/service/should_not_change_the_system.rb
+++ acceptance/tests/resource/service/should_not_change_the_system.rb
@@ -3,27 +3,20 @@ test_name "`puppet resource service` should list running services without changi
 confine :except, :platform => 'windows'
 
 hosts.each do |host|
-  step "make sure ssh reports running"
-
-  # We want to validate later that ssh is "still" running, which means it has
-  # to be running to start with
-  on host, 'puppet resource service ssh'
-  assert_match /service { 'ssh[^']*':\n\s*ensure\s*=>\s*'(?:true|running)'/, stdout, "ssh is not running"
-
-  step "list running services"
+  step "list running services and make sure ssh reports running"
 
   on host, 'puppet resource service'
+  assert_match /service { 'ssh[^']*':\n\s*ensure\s*=>\s*'(?:true|running)'/, stdout, "ssh is not running"
   expected_output = stdout
 
-  step "make sure nothing on the system was changed"
+  step "make sure nothing on the system was changed and ssh is still running"
 
   on host, 'puppet resource service'
+
+  # It's possible that `puppet resource service` changed the system before
+  # printing output the *first* time, so in addition to comparing the output,
+  # we also want to check that a known service is in a good state. We use ssh
+  # because our tests run over ssh, so it must be present.
+  assert_match /service { 'ssh[^']*':\n\s*ensure\s*=>\s*'(?:true|running)'/, stdout, "ssh is no longer running"
   assert_equal expected_output, stdout, "`puppet resource service` changed the state of the system"
-
-  step "make sure ssh is still running"
-
-  # We know ssh must be running, since we're using it to test. It should still
-  # be running after `puppet resource service`.
-  on host, 'puppet resource service ssh'
-  assert_match /service { 'ssh[^']*':\n\s*ensure\s*=>\s*'(?:true|running)'/, stdout, "`puppet resource service` changed the state of the system"
 end
