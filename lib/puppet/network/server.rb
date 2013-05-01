fe4681e1e9a3a8211c51e505ade41f3699e30088
Andrew Parker <andy@puppetlabs.com>
2013-04-30 20:20:25 -0700

    (Maint) Remove duplication btwn Daemon and Server
    The Puppet::Network::Server had a lot of the same code as the
    Puppet::Daemon (closing filehandles, managing a pidfile for locking).
    There was a comment in the Server class that indicated that it was not
    even clear if the duplicate code was being used.
    
    It turns out that the code was being used, but was incorrect. By turning
    on debug and watching the log output of the master, you were able to see
    that the close_streams was happening twice, as you would expect by
    following the code paths. There is no need for this to happen. In
    addition the pidfile is managed twice. This was discovered by
    instrumenting the code with a debug statement and noticing that the
    pidfile was locked 3 times (one from the server, one from the daemon,
    and one more from an unclear location).
    
    Removing all of this turns Puppet::Network::Server into a simple wrapper
    around the WeBrick code, reduces the overhead for daemonizing, and
    clarifes the pid as lockfile ownership.

--- lib/puppet/network/server.rb
+++ lib/puppet/network/server.rb
@@ -1,52 +1,10 @@
 require 'puppet/network/http'
-require 'puppet/util/pidlock'
 require 'puppet/network/http/webrick'
 
 #
 # @api private
 class Puppet::Network::Server
   attr_reader :address, :port
-
-  # TODO: does anything actually call this?  It seems like it's a duplicate of
-  # the code in Puppet::Daemon, but that it's not actually called anywhere.
-
-  # Put the daemon into the background.
-  def daemonize
-    if pid = fork
-      Process.detach(pid)
-      exit(0)
-    end
-
-    # Get rid of console logging
-    Puppet::Util::Log.close(:console)
-
-    Process.setsid
-    Dir.chdir("/")
-  end
-
-  def close_streams()
-    Puppet::Daemon.close_streams()
-  end
-
-  # Create a pidfile for our daemon, so we can be stopped and others
-  # don't try to start.
-  def create_pidfile
-    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
-      raise "Could not create PID file: #{pidfile}" unless Puppet::Util::Pidlock.new(pidfile).lock
-    end
-  end
-
-  # Remove the pid file for our daemon.
-  def remove_pidfile
-    Puppet::Util.synchronize_on(Puppet.run_mode.name,Sync::EX) do
-      Puppet::Util::Pidlock.new(pidfile).unlock
-    end
-  end
-
-  # Provide the path to our pidfile.
-  def pidfile
-    Puppet[:pidfile]
-  end
 
   def initialize(address, port)
     @port = port
@@ -63,27 +21,16 @@ class Puppet::Network::Server
     @listening
   end
 
-  def listen
+  def start
     raise "Cannot listen -- already listening." if listening?
     @listening = true
     @http_server.listen(address, port)
   end
 
-  def unlisten
+  def stop
     raise "Cannot unlisten -- not currently listening." unless listening?
     @http_server.unlisten
     @listening = false
-  end
-
-  def start
-    create_pidfile
-    close_streams if Puppet[:daemonize]
-    listen
-  end
-
-  def stop
-    unlisten
-    remove_pidfile
   end
 
   def wait_for_shutdown
