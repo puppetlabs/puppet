ec5048ac0a45d5e76de2b74dba2374ef0b355f5f
John Duarte <john.duarte@puppetlabs.com>
2013-06-25 16:21:14 -0700

    Remove prescribed modulepath from basic test
    Remove flag for defining installation path for modules. This is
    covered by another test.
    
    Refactor out hardcoded module name from the body of the test and
    place it in a variable.
    
    Expand test to verify that the modules manifest init.pp file is
    present on the system.

--- acceptance/tests/modules/install/basic_install.rb
+++ acceptance/tests/modules/install/basic_install.rb
@@ -1,16 +1,45 @@
 test_name "puppet module install (agent)"
 
+module_user = "pmtacceptance"
+module_name = "nginx"
+
+teardown do
+  agents.each do |agent|
+    result = on agent, puppet("config print modulepath")
+    result.stdout.split(':').each do |module_path|
+      if ! module_path.include? "/opt"
+        on agent, "[ -d #{module_path} ] && rm -fr #{module_path}/* || true"
+      end
+    end
+  end
+end
+
 agents.each do |agent|
   step 'setup'
   stub_forge_on(agent)
 
-  modulesdir = agent.tmpdir('puppet_module_build')
-  teardown do
-    on agent, "rm -rf #{modulesdir}"
+  step "install module '#{module_user}-#{module_name}'"
+  on(agent, puppet("module install #{module_user}-#{module_name}")) do
+    assert_match(/#{module_user}-#{module_name}/, stdout,
+          "Module name not displayed during install")
+    assert_match(/Notice: Installing -- do not interrupt/, stdout,
+          "No installing notice displayed!")
   end
 
-  step "install module to '#{modulesdir}'"
-  on(agent, puppet("module install pmtacceptance-nginx  --target-dir='#{modulesdir}'")) do
-    assert_match(/#{modulesdir}\n└── pmtacceptance-nginx \(.*\)/, stdout)
+  step "check for a '#{module_name}' manifest"
+  manifest_found = false
+  result = on agent, puppet("config print modulepath")
+  result.stdout.split(':').each do |module_path|
+    module_path = module_path.strip
+    if ! module_path.include? "/opt"
+      r = on(agent, "[ -f #{module_path}/#{module_name}/manifests/init.pp ]", :acceptable_error_codes => [0,1])
+      if r.exit_code == 0:
+        manifest_found = true
+        break
+      end
+    end
   end
+  assert_equal( true, manifest_found, "Manifest file not found for '#{module_name}' module")
+
+
 end
