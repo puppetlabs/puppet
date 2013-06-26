7a04e627d5c63d8e5330334f70f03845dc051750
John Duarte <john.duarte@puppetlabs.com>
2013-06-26 09:48:21 -0700

    DRY up expected output in with_modulepath

--- acceptance/tests/modules/install/with_modulepath.rb
+++ acceptance/tests/modules/install/with_modulepath.rb
@@ -2,6 +2,14 @@
 
 test_name "puppet module install (with modulepath)"
 
+expected_output = <<-OUTPUT
+    \e[mNotice: Preparing to install into #{master['puppetpath']}/modules2 ...\e[0m
+    \e[mNotice: Downloading from https://forge.puppetlabs.com ...\e[0m
+    \e[mNotice: Installing -- do not interrupt ...\e[0m
+    #{master['puppetpath']}/modules2
+    └── pmtacceptance-nginx (\e[0;36mv0.0.1\e[0m)
+OUTPUT
+
 step 'Setup'
 
 stub_forge_on(master)
@@ -14,25 +22,13 @@ end
 
 step "Install a module with relative modulepath"
 on master, "cd #{master['puppetpath']}/modules2 && puppet module install pmtacceptance-nginx --modulepath=." do
-  assert_output <<-OUTPUT
-    \e[mNotice: Preparing to install into #{master['puppetpath']}/modules2 ...\e[0m
-    \e[mNotice: Downloading from https://forge.puppetlabs.com ...\e[0m
-    \e[mNotice: Installing -- do not interrupt ...\e[0m
-    #{master['puppetpath']}/modules2
-    └── pmtacceptance-nginx (\e[0;36mv0.0.1\e[0m)
-  OUTPUT
+  assert_output expected_output
 end
 on master, "[ -d #{master['puppetpath']}/modules2/nginx ]"
 
 step "Install a module with absolute modulepath"
 on master, "test -d #{master['puppetpath']}/modules2/nginx && rm -rf #{master['puppetpath']}/modules2/nginx"
 on master, puppet("module install pmtacceptance-nginx --modulepath=#{master['puppetpath']}/modules2") do
-  assert_output <<-OUTPUT
-    \e[mNotice: Preparing to install into #{master['puppetpath']}/modules2 ...\e[0m
-    \e[mNotice: Downloading from https://forge.puppetlabs.com ...\e[0m
-    \e[mNotice: Installing -- do not interrupt ...\e[0m
-    #{master['puppetpath']}/modules2
-    └── pmtacceptance-nginx (\e[0;36mv0.0.1\e[0m)
-  OUTPUT
+  assert_output expected_output
 end
 on master, "[ -d #{master['puppetpath']}/modules2/nginx ]"
