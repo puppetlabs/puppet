9c841501dcfca952ee284a8bb7a8b33c4822ac1d
John Duarte <john.duarte@puppetlabs.com>
2013-06-26 16:18:43 -0700

    Use expected_output template for all test steps in by_module_name

--- acceptance/tests/modules/search/by_module_name.rb
+++ acceptance/tests/modules/search/by_module_name.rb
@@ -20,21 +20,18 @@ on master, puppet("module search #{search_string}") do
 end
 
 step 'Search for modules by partial full name (dashed)'
-on master, puppet("module search tance-ge") do
+search_string = "tance-ge"
+on master, puppet("module search #{search_string}") do
+  em_module_name = module_name.gsub(/#{search_string}/, "\e[0;32m#{search_string}\e[0m")
   assert_equal '', stderr
-  assert_equal <<-STDOUT, stdout
-\e[mNotice: Searching https://forge.puppetlabs.com ...\e[0m
-NAME                  DESCRIPTION                  AUTHOR          KEYWORDS     
-pmtaccep\e[0;32mtance-ge\e[0mordi  UNKNOWN                      @pmtacceptance  star trek    
-STDOUT
+  assert_equal expected_output % [em_module_name], stdout
 end
 
 step 'Search for modules by partial full name (slashed)'
-on master, puppet("module search tance/ge") do
+search_string = "tance/ge"
+on master, puppet("module search #{search_string}") do
+  search_string = search_string.gsub(/\//, "-")
+  em_module_name = module_name.gsub(/#{search_string}/, "\e[0;32m#{search_string}\e[0m")
   assert_equal '', stderr
-  assert_equal <<-STDOUT, stdout
-\e[mNotice: Searching https://forge.puppetlabs.com ...\e[0m
-NAME                  DESCRIPTION                  AUTHOR          KEYWORDS     
-pmtaccep\e[0;32mtance-ge\e[0mordi  UNKNOWN                      @pmtacceptance  star trek    
-STDOUT
+  assert_equal expected_output % [em_module_name], stdout
 end
