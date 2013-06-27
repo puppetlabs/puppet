85c0505a6d19732e37e2cfbbbf42b5e9d9c39236
John Duarte <john.duarte@puppetlabs.com>
2013-06-27 08:17:14 -0700

    Add gt and lt steps to puppet module uninstall with version test

--- acceptance/tests/modules/uninstall/using_version_filter.rb
+++ acceptance/tests/modules/uninstall/using_version_filter.rb
@@ -6,6 +6,7 @@ file {
   [
     '#{master['distmoduledir']}/crakorn',
     '#{master['sitemoduledir']}/crakorn',
+    '#{master['sitemoduledir']}/appleseed',
   ]: ensure => directory;
   '#{master['distmoduledir']}/crakorn/metadata.json':
     content => '{
@@ -25,12 +26,22 @@ file {
       "license": "MIT",
       "dependencies": []
     }';
+  '#{master['sitemoduledir']}/appleseed/metadata.json':
+    content => '{
+      "name": "jimmy/appleseed",
+      "version": "0.4.0",
+      "source": "",
+      "author": "jimmy",
+      "license": "MIT",
+      "dependencies": []
+    }';
 }
 PP
 
 teardown do
   on master, "rm -rf #{master['distmoduledir']}/crakorn"
   on master, "rm -rf #{master['sitemoduledir']}/crakorn"
+  on master, "rm -rf #{master['sitemoduledir']}/appleseed"
 end
 
 on master, "[ -d #{master['distmoduledir']}/crakorn ]"
@@ -56,3 +67,23 @@ on master, puppet('module uninstall jimmy-crakorn --version 0.5.x'), :acceptable
   OUTPUT
 end
 on master, "[ -d #{master['distmoduledir']}/crakorn ]"
+
+step "Try to uninstall jimmy-appleseed v0.4.0 with `--version >9.9.9`"
+on master, puppet('module uninstall jimmy-appleseed --version ">9.9.9"'), :acceptable_exit_codes => [1] do
+  assert_output <<-OUTPUT
+    STDOUT> \e[mNotice: Preparing to uninstall 'jimmy-appleseed' (\e[0;36m>9.9.9\e[m) ...\e[0m
+    STDERR> \e[1;31mError: Could not uninstall module 'jimmy-appleseed' (>9.9.9)
+    STDERR>   No installed version of 'jimmy-appleseed' matches (>9.9.9)
+    STDERR>     'jimmy-appleseed' (v0.4.0) is installed in #{master['sitemoduledir']}\e[0m
+  OUTPUT
+end
+on master, "[ -d #{master['sitemoduledir']}/appleseed ]"
+
+step "Uninstall jimmy-appleseed v0.4.0 with `--version >0.0.0`"
+on master, puppet('module uninstall jimmy-appleseed --version ">0.0.0"') do
+  assert_output <<-OUTPUT
+    \e[mNotice: Preparing to uninstall 'jimmy-appleseed' (\e[0;36m>0.0.0\e[m) ...\e[0m
+    Removed 'jimmy-appleseed' (\e[0;36mv0.4.0\e[0m) from #{master['sitemoduledir']}
+  OUTPUT
+end
+on master, "[ ! -d #{master['sitemoduledir']}/appleseed ]"
