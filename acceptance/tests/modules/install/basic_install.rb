83df85fcf1bf70bd45385001ba6d5b167407a157
John Duarte <john.duarte@puppetlabs.com>
2013-06-27 15:55:37 -0700

    Use master/agent['distmodule'] parameter to manage  module(s)

--- acceptance/tests/modules/install/basic_install.rb
+++ acceptance/tests/modules/install/basic_install.rb
@@ -4,13 +4,9 @@ module_user = "pmtacceptance"
 module_name = "nginx"
 
 teardown do
+  on master, "rm -rf #{master['distmoduledir']}/*"
   agents.each do |agent|
-    result = on agent, puppet("config print modulepath")
-    result.stdout.split(':').each do |module_path|
-      if ! module_path.include? "/opt"
-        on agent, "[ -d #{module_path} ] && rm -fr #{module_path}/* || true"
-      end
-    end
+    on agent, "rm -rf #{agent['distmoduledir']}/*"
   end
 end
 
@@ -27,19 +23,7 @@ agents.each do |agent|
   end
 
   step "check for a '#{module_name}' manifest"
-  manifest_found = false
-  result = on agent, puppet("config print modulepath")
-  result.stdout.split(':').each do |module_path|
-    module_path = module_path.strip
-    if ! module_path.include? "/opt"
-      r = on(agent, "[ -f #{module_path}/#{module_name}/manifests/init.pp ]", :acceptable_error_codes => [0,1])
-      if r.exit_code == 0:
-        manifest_found = true
-        break
-      end
-    end
-  end
-  assert_equal( true, manifest_found, "Manifest file not found for '#{module_name}' module")
+    on(agent, "[ -f #{agent['distmoduledir']}/#{module_name}/manifests/init.pp ]")
 
 
 end
