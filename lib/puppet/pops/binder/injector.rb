f6784f033ae62c3b1cc815767c5f3c4666fb8fd8
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-07-01 08:28:46 -0700

    (#16856) Refactor producer to use a class instead of just a lambda.
    In order to support custom producers it was not enough to just
    return a lambda as the producer. This is now instead an instance of
    Puppet::Pops::Binder::Producer.

--- lib/puppet/pops/binder/injector.rb
+++ lib/puppet/pops/binder/injector.rb
@@ -2,7 +2,7 @@
 #
 # Initialization
 # --------------
-# The injector is initialized with a configured Binder. The Binder instance contains a resolved set of key to "binding information"
+# The injector is initialized with a configured Binder. The Binder instance contains a resolved set of key => "binding information"
 # that is used to setup the injector.
 #
 # Lookup
@@ -123,23 +123,64 @@ class Puppet::Pops::Binder::Injector
     produce(scope, entries[key])
   end
 
-  # TODO
+  # Lookup (a.k.a "inject") producer of a value given a key.
+  # The producer lookup may be called with different parameters. This method is a convenience method that
+  # dispatches to one of #lookup_producer_key or #lookup_producer_type depending on the arguments
+  #
+  # @overload lookup_producer(scope, key)
+  #   (see #lookup_proudcer_key)
+  #   @param scope [Puppet::Parser::Scope] the scope to use for evaluation
+  #   @param key [Object] an opaque object being the full key
+  #
+  # @overload lookup_producer(scope, type, name = '')
+  #  (see #lookup_type)
+  #   @param scope [Puppet::Parser::Scope] the scope to use for evaluation
+  #   @param type [Puppet::Pops::Types::PObjectType], the type of what to lookup
+  #   @param name [String], the name to use, defaults to empty string (for unnamed)
+  #
+  # @overload lookup_producer(scope, name)
+  #  Lookup of Data type with given name.
+  #   @see #lookup_type
+  #   @param scope [Puppet::Parser::Scope] the scope to use for evaluation
+  #   @param name [String], the Data/name to lookup
+  #
+  # @return [Puppet::Pops::Binder::Producer] a producer
+  #
+  # @api public
+  #
   def lookup_producer(scope, *args)
     # TODO: should return an object that may have additional ways of creating an instance
     # These are obviously not of value in the Puppet DSL (since methods cannot be invoked)
     # To support getting a producer that can behave as the Proc producers, the Proc should
     # be wrapped in an instance when user has not supplied a Producer class.
-    raise NotImplementedError, "lookup_producer is not implemented yet"
+    raise ArgumentError, "lookup_producer should be called with two or three arguments, got: #{args.size()+1}" unless args.size <= 2
+    case args[0]
+    when Puppet::Pops::Types::PObjectType
+      lookup_producer_type(scope, *args)
+    when String
+      raise ArgumentError, "lookup_producer of name should only pass the name" unless args.size == 1
+      lookup_key(scope, key_factory.data_key(args[0]))
+    else
+      raise ArgumentError, "lookup_producer using a key should only pass a single key" unless args.size == 1
+      lookup_producer_key(scope, args[0])
+    end
   end
 
-  # TODO
+  # Looks up a Producer given an opaque binder key.
+  # @returns [Puppet::Pops::Binder::Producer, nil] the bound producer, or nil if no such producer was found.
+  # @api public
+  #
   def lookup_producer_key(scope, key)
-    raise NotImplementedError, "lookup_producer_key is not implemented yet"
+    producer(scope, entries[key])
   end
 
-  # TODO
+  # Looks up a Producer given a type/name key.
+  # @note The result is not type checked (it cannot be until the producer has produced an instance).
+  # @returns [Puppet::Pops::Binder::Producer, nil] the bound producer, or nil if no such producer was found
+  # @api public
+  #
   def lookup_producer_type(scope, type, name='')
-    raise NotImplementedError, "lookup_producer_type is not implemented yet"
+    lookup_producer_key(named_key(type, name))
   end
 
   # TODO: Optional Producers; they should have a list of other producers (to be tested in turn for production) ??
@@ -147,13 +188,37 @@ class Puppet::Pops::Binder::Injector
   # @return [nil] if the entry is nil (i.e. when not found)
   # @return [Object] the produced instance / value (non type-safe except for multibind contributions)
   #
-  def produce(scope, entry)
+
+  # Returns the producer for the entry
+  # @return [Puppet::Pops::Binder::Producer] the entry's producer.
+  #
+  # @api private
+  #
+  def producer(scope, entry)
     return nil unless entry # not found
     unless entry.cached_producer
       entry.cached_producer = @@producer_visitor.visit_this(self, entry.binding.producer, scope, entry)
     end
     raise ArgumentError, "Injector entry without a producer TODO: detail" unless entry.cached_producer
-    entry.cached_producer.call(scope)
+    entry.cached_producer
+  end
+
+  # Creates a producer if given argument is a lambda, else returns the give producer
+  # @return [Puppet::Pops::Binder::Producer] the given or producer wrapped lambda producer
+  # @api private
+  #
+  def create_producer(lambda_or_producer)
+    return lambda_or_producer if lambda_or_producer.is_a?(Puppet::Pops::Binder::Producer)
+    return Puppet::Pops::Binder::LambdaProducer.new(lambda_or_producer)
+  end
+
+  # Returns the produced instance
+  # @return [Object] the produced instance
+  # @api private
+  #
+  def produce(scope, entry)
+    return nil unless entry # not found
+    producer(scope, entry).produce(scope)
   end
 
   # Called when producer is missing (e.g. a Multibinding)
@@ -180,44 +245,47 @@ class Puppet::Pops::Binder::Injector
 
   def produce_ArrayMultibindProducerDescriptor(descriptor, entry)
     p = array_multibind_producer(entry.binding)
-    caching?(descriptor) ? singleton_producer(p.call(scope)) : p
+    caching?(descriptor) ? singleton_producer(p.produce(scope)) : p
   end
 
   def produce_HashMultibindProducerDescriptor(descriptor, entry)
     p = hash_multibind_producer(entry.binding)
-    caching?(descriptor) ? singleton_producer(p.call(scope)) : p
+    caching?(descriptor) ? singleton_producer(p.produce(scope)) : p
   end
 
   # Produces a constant value
   # If not a singleton the value is deep-cloned (if not immutable) before returned.
   #
   def produce_ConstantProducerDescriptor(descriptor, scope, entry)
-    if caching?(descriptor)
+    x = if caching?(descriptor)
       deep_cloning_producer(descriptor.value)
     else
       singleton_producer(descriptor.value)
     end
+    create_producer(x)
   end
 
   # Produces a new instance of the given class with given initialization arguments
   # If a singleton, the producer is asked to produce a single value and this is then considered a singleton.
   #
   def produce_InstanceProducer(descriptor, scope, entry)
-    if caching?(descriptor)
+    x = if caching?(descriptor)
       instantiating_producer.new(descriptor.class_name, *(descriptor.arguments))
     else
-      singleton_producer(instantiating_producer(descriptor.class_name, *(descriptor.arguments)).call(scope))
+      singleton_producer(instantiating_producer(descriptor.class_name, *(descriptor.arguments)).produce(scope))
     end
+    create_producer(x)
   end
 
   # Evaluates a contained expression. If this is a singleton, the evaluation is performed once.
   #
   def produce_EvaluatingProducerDescriptor(descriptor, scope, entry)
-    if caching?(descriptor)
+    x = if caching?(descriptor)
       evaluating_producer(descriptor.expr)
     else
-      singleton_producer(evaluating_producer(descriptor.expr).call(scope))
+      singleton_producer(evaluating_producer(descriptor.expr).produce(scope))
     end
+    create_producer(x)
   end
 
   private
@@ -242,13 +310,11 @@ class Puppet::Pops::Binder::Injector
   #
 
   def singleton_producer(value)
-    lambda do |scope|
-      return value
-    end
+    create_producer(lambda {|scope| value })
   end
 
   def deep_cloning_producer(value)
-    lambda do |scope|
+    x = lambda do |scope|
       case value
       # These are immutable
       when Integer, Float, TrueClass, FalseClass, Symbol
@@ -261,32 +327,27 @@ class Puppet::Pops::Binder::Injector
       # The default serialize/deserialize to get a deep copy
       Marshal.load(Marshal.dump(value))
     end
+    create_producer(x)
   end
 
   def instantiating_producer(class_name, *init_args)
-    lambda do |scope|
-      Object.const_get(class_name).new(*init_args)
-    end
+    create_producer(lambda {|scope| Object.const_get(class_name).new(*init_args) } )
   end
 
   def evaluating_producer(expr)
     puppet3_ast = Puppet::Pops::Model::AstTransformer.new().transform(expr)
-    lambda do |scope|
-      puppet_3_ast.evaluate(scope)
-    end
+    create_producer(lambda { |scope| puppet_3_ast.evaluate(scope) })
   end
 
   def lookup_producer(type, name)
-    lambda do |scope|
-      lookup_type(scope, type, name)
-    end
+    create_producer( lambda { |scope| lookup_type(scope, type, name) })
   end
 
   # TODO: Support combinator lambda combinator => |$memo, $x| { $memo + $x }
   # @api private
   def array_multibind_producer(binding)
     contributions_key = key_factory.multibind_contributions_key(bindings.id)
-    lambda do |scope|
+    x = lambda do |scope|
       result = []
       lookup_key(scope, contributions_key).each do |k|
         val = lookup_key(scope, k)
@@ -300,13 +361,14 @@ class Puppet::Pops::Binder::Injector
       end
       val
     end
+    create_producer(x)
   end
 
   # TODO: Support combinator lambda combinator => |$key, $current, $value| { . . .}
   # @api private
   def hash_multibind_producer(binding)
     contributions_key = key_factory.multibind_contributions_key(bindings.id)
-    lambda do |scope|
+    x = lambda do |scope|
       result = {}
       lookup_key(scope, contributions_key).each do |k|
         # get the entry (its name is needed)
@@ -323,5 +385,6 @@ class Puppet::Pops::Binder::Injector
       end
       val
     end
+    create_producer(x)
   end
 end
