85a5d8d6d252f5653b22106cdb37c2993d22c848
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-07-01 19:55:54 -0700

    (#16856) Add missing model objects to LabelProvider.
    Also adds category.value to the Category label on the format
    "Category 'categorization/value' e.g. Category 'environment/dev'.

--- lib/puppet/pops/binder/bindings_label_provider.rb
+++ lib/puppet/pops/binder/bindings_label_provider.rb
@@ -1,36 +1,38 @@
-# A provider of labels for model object, producing a human name for the model object.
-# As an example, if object is an ArithmeticExpression with operator +, `#a_an(o)` produces "a '+' Expression",
-# #the(o) produces "the + Expression", and #label produces "+ Expression".
+# A provider of labels for bindings model object, producing a human name for the model object.
 #
 class Puppet::Pops::Binder::BindingsLabelProvider < Puppet::Pops::LabelProvider
   def initialize
     @@label_visitor ||= Puppet::Pops::Visitor.new(self,"label",0,0)
   end
 
-  # Produces a label for the given objects type/operator without article.
+  # Produces a label for the given object without article.
+  # @return [String] a human readable label
+  #
   def label o
    @@label_visitor.visit(o)
   end
 
   def label_PObjectType o                       ; "#{Puppet::Pops::Types::TypeFactory.label(o)}" end
-  def label_ProducerDescriptor o                ; "Producer"                            end
-  def label_NonCachingProducerDescriptor o      ; "Non Caching Producer"                end
-  def label_ConstantProducerDescriptor o        ; "Producer['#{o.value}']"              end
-  def label_EvaluatingProducerDescriptor o      ; "Evaluating Producer"                 end
-  def label_InstanceProducerDescriptor o        ; "Producer[#{o.class_name}]"           end
-  def label_LookupProducerDescriptor o          ; "Lookup Producer[#{o.name}]"          end
-  def label_MultibindProducerDescriptor o       ; "Multibind Producer"                  end
-  def label_ArrayMultibindProducerDescriptor o  ; "Array Multibind Producer"            end
-  def label_HashMultibindProducerDescriptor o   ; "Hash Multibind Producer"             end
-  def label_Binding o                           ; "Binding"                             end
-  def label_Multibinding o                      ; "Multibinding"                        end
-  def label_MultibindContribution o             ; "Multibind Contribution"              end
-  def label_Bindings o                          ; "Bindings"                            end
-  def label_NamedBindings o                     ; "Named Bindings"                      end
-  def label_Category o                          ; "Category '#{o.categorization}'"      end
-  def label_CategorizedBindings o               ; "Categorized Bindings"                end
-  def label_LayeredBindings o                   ; "Layered Bindings"                    end
-  def label_NamedLayer o                        ; "Layer '#{o.name}'"                   end
-  def label_EffectiveCategories o               ; "Effective Categories"                end
+  def label_ProducerDescriptor o                ; "Producer"                                     end
+  def label_NonCachingProducerDescriptor o      ; "Non Caching Producer"                         end
+  def label_ConstantProducerDescriptor o        ; "Producer['#{o.value}']"                       end
+  def label_EvaluatingProducerDescriptor o      ; "Evaluating Producer"                          end
+  def label_InstanceProducerDescriptor o        ; "Producer[#{o.class_name}]"                    end
+  def label_LookupProducerDescriptor o          ; "Lookup Producer[#{o.name}]"                   end
+  def label_FirstFoundProducerDescriptor o      ; "First Found Producer"                         end
+  def label_ProducerProducerDescriptor o        ; "Producer[Producer]"                           end
+  def label_MultibindProducerDescriptor o       ; "Multibind Producer"                           end
+  def label_ArrayMultibindProducerDescriptor o  ; "Array Multibind Producer"                     end
+  def label_HashMultibindProducerDescriptor o   ; "Hash Multibind Producer"                      end
+  def label_Binding o                           ; "Binding"                                      end
+  def label_Multibinding o                      ; "Multibinding"                                 end
+  def label_MultibindContribution o             ; "Multibind Contribution"                       end
+  def label_Bindings o                          ; "Bindings"                                     end
+  def label_NamedBindings o                     ; "Named Bindings"                               end
+  def label_Category o                          ; "Category '#{o.categorization}/#{o.value}'"    end
+  def label_CategorizedBindings o               ; "Categorized Bindings"                         end
+  def label_LayeredBindings o                   ; "Layered Bindings"                             end
+  def label_NamedLayer o                        ; "Layer '#{o.name}'"                            end
+  def label_EffectiveCategories o               ; "Effective Categories"                         end
 
 end
