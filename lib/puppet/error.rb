cde4d6e2a86ed4754c12edf997cf02651f7ebf48
Andrew Parker <andy@puppetlabs.com>
2013-07-13 15:46:59 -0700

    (#21409) Preserve original error when propogating
    Previously the original error was lost when some of the error handling
    routines dealt with it (such as Puppet::Util.exceptwrap). This allows
    the original error to be propogated out along with it.

--- lib/puppet/error.rb
+++ lib/puppet/error.rb
@@ -1,7 +1,7 @@
 module Puppet
   # The base class for all Puppet errors. It can wrap another exception
   class Error < RuntimeError
-    attr_reader :original
+    attr_accessor :original
     def initialize(message, original=nil)
       super(message)
       @original = original
