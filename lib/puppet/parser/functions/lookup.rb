382a77c2e63faff1b2f597526a67a727be2154ee
Andrew Parker <andy@puppetlabs.com>
2013-07-22 14:20:24 -0700

    (#16856) Check looked up type against provided type in lookup()
    The lookup() function can now enforce the type that the caller of the
    function is expecting to receive by being provided a type specification
    as a second argument. If no argument is provided it will default to
    needing "Data", which is the supertype of all of the data types (Object
    is a supertype, but doesn't not result in getting a looked up value
    because the type is also part of a namespace for the injection system
    and it holds object separate from data).

--- lib/puppet/parser/functions/lookup.rb
+++ lib/puppet/parser/functions/lookup.rb
@@ -1,3 +1,5 @@
-Puppet::Parser::Functions.newfunction(:lookup, :type => :rvalue) do |args|
-  compiler.injector.lookup(self, args[0])
+Puppet::Parser::Functions.newfunction(:lookup, :type => :rvalue, :arity => -2) do |args|
+  type_parser = Puppet::Pops::Types::TypeParser.new
+  type = type_parser.parse(args[1] || "Data")
+  compiler.injector.lookup(self, type, args[0])
 end
--- spec/unit/parser/functions/lookup_spec.rb
+++ spec/unit/parser/functions/lookup_spec.rb
@@ -1,17 +1,42 @@
 require 'spec_helper'
 require 'puppet/pops'
+require 'stringio'
 
 describe "lookup function" do
+  it "must be called with at least a name to lookup" do
+    scope = scope_with_injections_from(bound(bindings))
+
+    expect do
+      scope.function_lookup([])
+    end.to raise_error(ArgumentError, /Wrong number of arguments/)
+  end
+
   it "looks up a value that exists" do
-    scope = scope_with_injections_from(bound("a_value", "something"))
+    scope = scope_with_injections_from(bound(bind_single("a_value", "something")))
 
     expect(scope.function_lookup(['a_value'])).to eq('something')
   end
 
   it "returns nil when the requested value is not bound" do
-    scope = scope_with_injections_from(bound("a_value", "something"))
+    scope = scope_with_injections_from(bound(bind_single("a_value", "something")))
 
     expect(scope.function_lookup(['not_bound_value'])).to be_nil
+  end
+
+  it "raises an error when the bound type is not assignable to the requested type" do
+    scope = scope_with_injections_from(bound(bind_single("a_value", "something")))
+
+    expect do
+      scope.function_lookup(['a_value', 'Integer'])
+    end.to raise_error(ArgumentError, /incompatible type, expected: Integer, got: String/)
+  end
+
+  it "returns the value if the bound type is assignable to the requested type" do
+    typed_bindings = bindings
+    typed_bindings.bind().string().name("a_value").to("something")
+    scope = scope_with_injections_from(bound(typed_bindings))
+
+    expect(scope.function_lookup(['a_value', 'Data'])).to eq("something")
   end
 
   def scope_with_injections_from(binder)
@@ -22,13 +47,20 @@ describe "lookup function" do
     scope
   end
 
-  def bound(name, value)
-    bindings = Puppet::Pops::Binder::BindingsFactory.named_bindings("testing")
-    bindings.bind().name(name).to(value)
+  def bindings
+    Puppet::Pops::Binder::BindingsFactory.named_bindings("testing")
+  end
 
+  def bind_single(name, value)
+    local_bindings = Puppet::Pops::Binder::BindingsFactory.named_bindings("testing")
+    local_bindings.bind().name(name).to(value)
+    local_bindings
+  end
+
+  def bound(local_bindings)
     binder = Puppet::Pops::Binder::Binder.new
     binder.define_categories(Puppet::Pops::Binder::BindingsFactory.categories([]))
-    binder.define_layers(Puppet::Pops::Binder::BindingsFactory.layered_bindings(Puppet::Pops::Binder::BindingsFactory.named_layer('test layer', bindings.model)))
+    binder.define_layers(Puppet::Pops::Binder::BindingsFactory.layered_bindings(Puppet::Pops::Binder::BindingsFactory.named_layer('test layer', local_bindings.model)))
 
     binder
   end
