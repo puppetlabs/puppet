33043de32ae0555d1e019255a7ddbef93524f304
Andrew Parker <andy@puppetlabs.com>
2013-07-22 17:33:08 -0700

    (#16856) Support parameterized collection types in type specifications
    The previous support for type specifications in the language did not
    allow for parameterized collection types. This adds support for those
    types.

--- lib/puppet/pops/types/type_parser.rb
+++ lib/puppet/pops/types/type_parser.rb
@@ -6,25 +6,68 @@
 #
 # @api private
 class Puppet::Pops::Types::TypeParser
+  TYPES = Puppet::Pops::Types::TypeFactory
+
+  def initialize
+    @parser = Puppet::Pops::Parser::Parser.new()
+    @type_transformer = Puppet::Pops::Visitor.new(nil, "interpret", 0, 0)
+  end
+
   def parse(string)
-    types = Puppet::Pops::Types::TypeFactory
-    case string
-    when "Integer"
-      types.integer
-    when "Float"
-      types.float
-    when "String"
-      types.string
-    when "Boolean"
-      types.boolean
-    when "Pattern"
-      types.pattern
-    when "Data"
-      types.data
-    when "Object"
-      types.object
+    @string = string
+    interpret(@parser.parse_string(@string).current)
+  end
+
+  def interpret(ast)
+    @type_transformer.visit_this(self, ast)
+  end
+
+  def interpret_QualifiedReference(name_ast)
+    case name_ast.value
+    when "integer"
+      TYPES.integer
+    when "float"
+      TYPES.float
+    when "string"
+      TYPES.string
+    when "boolean"
+      TYPES.boolean
+    when "pattern"
+      TYPES.pattern
+    when "data"
+      TYPES.data
     else
-      raise Puppet::ParseError, "Unknown type <#{string}>"
+      raise_unknown_type_error(name_ast)
     end
+  end
+
+  def interpret_AccessExpression(parameterized_ast)
+    parameters = parameterized_ast.keys.collect { |param| interpret(param) }
+    case parameterized_ast.left_expr.value
+    when "array"
+      if parameters.size != 1
+        raise_invalid_parameters_error("Array", 1, parameters.size)
+      end
+      TYPES.array_of(parameters[0])
+    when "hash"
+      if parameters.size != 2
+        raise_invalid_parameters_error("Hash", 2, parameters.size)
+      end
+      TYPES.hash_of(parameters[1], parameters[0])
+    else
+      raise_unknown_type_error(parameterized_ast.left_expr)
+    end
+  end
+
+  private
+
+  def raise_invalid_parameters_error(type, required, given)
+    raise Puppet::ParseError, "Invalid number of type parameters specified: #{type} requires #{required}, #{given} provided"
+  end
+
+  def raise_unknown_type_error(ast)
+    position = Puppet::Pops::Adapters::SourcePosAdapter.adapt(ast)
+    original = position.extract_text_from_string(@string)
+    raise Puppet::ParseError, "Unknown type <#{original}>"
   end
 end
--- spec/unit/pops/types/type_parser_spec.rb
+++ spec/unit/pops/types/type_parser_spec.rb
@@ -7,25 +7,63 @@ describe Puppet::Pops::Types::TypeParser do
   let(:parser) { Puppet::Pops::Types::TypeParser.new }
   let(:types) { Puppet::Pops::Types::TypeFactory }
 
-  it "rejects an invalide type" do
-    expect { parser.parse("NotAType") }.to raise_error(Puppet::ParseError, /Unknown type <NotAType>/)
+  it "rejects an invalid type simple type" do
+    expect { parser.parse("NotAType") }.to raise_type_error_for("NotAType")
+  end
+
+  it "rejects an unknown parameterized type" do
+    expect { parser.parse("NotAType[Integer]") }.to raise_type_error_for("NotAType")
+  end
+
+  it "does not support types that do not make sense in the puppet language" do
+    expect { parser.parse("Object") }.to raise_type_error_for("Object")
+    expect { parser.parse("Collection[Integer]") }.to raise_type_error_for("Collection")
   end
 
   it "parses a simple, unparameterized type into the type object" do
-    expect(parser.parse(the_type_spec_for(types.integer))).to be_the_type(types.integer)
-    expect(parser.parse(the_type_spec_for(types.float))).to be_the_type(types.float)
-    expect(parser.parse(the_type_spec_for(types.string))).to be_the_type(types.string)
-    expect(parser.parse(the_type_spec_for(types.boolean))).to be_the_type(types.boolean)
-    expect(parser.parse(the_type_spec_for(types.pattern))).to be_the_type(types.pattern)
-    expect(parser.parse(the_type_spec_for(types.data))).to be_the_type(types.data)
-    expect(parser.parse(the_type_spec_for(types.object))).to be_the_type(types.object)
+    expect(the_type_parsed_from(types.integer)).to be_the_type(types.integer)
+    expect(the_type_parsed_from(types.float)).to be_the_type(types.float)
+    expect(the_type_parsed_from(types.string)).to be_the_type(types.string)
+    expect(the_type_parsed_from(types.boolean)).to be_the_type(types.boolean)
+    expect(the_type_parsed_from(types.pattern)).to be_the_type(types.pattern)
+    expect(the_type_parsed_from(types.data)).to be_the_type(types.data)
+  end
+
+  it "parses a parameterized type into the type object" do
+    parameterized_array = types.array_of(types.integer)
+    parameterized_hash = types.hash_of(types.integer, types.boolean)
+
+    expect(the_type_parsed_from(parameterized_array)).to be_the_type(parameterized_array)
+    expect(the_type_parsed_from(parameterized_hash)).to be_the_type(parameterized_hash)
+  end
+
+  it "rejects an array spec with the wrong number of parameters" do
+    expect { parser.parse("Array[Integer, Integer]") }.to raise_the_parameter_error("Array", 1, 2)
+    expect { parser.parse("Hash[Integer]") }.to raise_the_parameter_error("Hash", 2, 1)
   end
 
   matcher :be_the_type do |type|
+    calc = Puppet::Pops::Types::TypeCalculator.new
+
     match do |actual|
-      calc = Puppet::Pops::Types::TypeCalculator.new
       calc.assignable?(actual, type) && calc.assignable?(type, actual)
     end
+
+    failure_message_for_should do |actual|
+      "expected #{calc.string(type)}, but was #{calc.string(actual)}"
+    end
+  end
+
+  def raise_the_parameter_error(type, required, given)
+    raise_error(Puppet::ParseError, /#{type} requires #{required}, #{given} provided/)
+  end
+
+  def raise_type_error_for(type_name)
+    raise_error(Puppet::ParseError, /Unknown type <#{type_name}>/)
+  end
+
+  def the_type_parsed_from(type)
+    parser.parse(the_type_spec_for(type))
   end
 
   def the_type_spec_for(type)
