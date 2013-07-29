73b421ccba0788aaca881ef822623816ad57e8f8
Kylo Ginsberg <kylo@puppetlabs.com>
2013-07-29 15:42:44 -0700

    (#21970) Comment fixup
    The REST certificate endpoint supports only find, not save.

--- lib/puppet/indirector/certificate/rest.rb
+++ lib/puppet/indirector/certificate/rest.rb
@@ -2,7 +2,7 @@ require 'puppet/ssl/certificate'
 require 'puppet/indirector/rest'
 
 class Puppet::SSL::Certificate::Rest < Puppet::Indirector::REST
-  desc "Find and save certificates over HTTP via REST."
+  desc "Find certificates over HTTP via REST."
 
   use_server_setting(:ca_server)
   use_port_setting(:ca_port)
