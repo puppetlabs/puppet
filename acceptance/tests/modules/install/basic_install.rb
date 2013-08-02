f02f186919f73e063f287530b9565431ee479239
Pieter van de Bruggen <pvande@gmail.com>
2013-08-02 11:40:20 -0700

    Backporting acceptance tests.

---
+++ acceptance/tests/modules/install/basic_install.rb
@@ -0,0 +1,19 @@
+test_name "puppet module install (agent)"
+require 'puppet/acceptance/module_utils'
+extend Puppet::Acceptance::ModuleUtils
+
+agents.each do |agent|
+  step 'setup'
+  stub_forge_on(agent)
+
+  modulesdir = agent.tmpdir('puppet_module_build')
+  teardown do
+    on agent, "rm -rf #{modulesdir}"
+  end
+
+  step "install module to '#{modulesdir}'"
+  on(agent, puppet("module install pmtacceptance-nginx  --target-dir='#{modulesdir}'")) do
+    assert_match(/#{modulesdir}\n└── pmtacceptance-nginx \(.*\)/, stdout)
+    assert_module_installed_on_disk(agent, modulesdir, 'nginx')
+  end
+end
