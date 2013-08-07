1fcec065901e5b604ed31407e49a356d9452a846
Kylo Ginsberg <kylo@puppetlabs.com>
2013-08-06 21:15:40 -0700

    (#16856) fix hiera2 yaml backend for empty/invalid yaml files

--- lib/puppet/pops/binder/hiera2/yaml_backend.rb
+++ lib/puppet/pops/binder/hiera2/yaml_backend.rb
@@ -3,7 +3,9 @@ class Puppet::Pops::Binder::Hiera2::YamlBackend < Puppetx::Puppet::Hiera2Backend
   def read_data(module_dir, source)
     begin
       source_file = File.join(module_dir, "#{source}.yaml")
-      YAML.load_file(source_file)
+      # if file is present but empty or has only "---", YAML.load_file returns false,
+      # in which case fall back to returning an empty hash
+      YAML.load_file(source_file) || {}
     rescue Errno::ENOTDIR
       # This is OK, the file doesn't need to be present. Return an empty hash
       {}
