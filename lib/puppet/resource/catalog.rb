83c59593a3ee769bf676439a8f9347e83d8445f1
Kylo Ginsberg <kylo@puppetlabs.com>
2013-08-07 20:59:48 -0700

    (#22110) set version=0 in catalog constructor so it's always an integer (not nil)

--- lib/puppet/resource/catalog.rb
+++ lib/puppet/resource/catalog.rb
@@ -239,6 +239,7 @@ class Puppet::Resource::Catalog < Puppet::Graph::SimpleGraph
     @resource_table = {}
     @resources = []
     @relationship_graph = nil
+    @version = 0
 
     @host_config = true
 
