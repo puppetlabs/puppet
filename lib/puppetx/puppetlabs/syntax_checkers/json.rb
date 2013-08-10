04dc6a487c23fe758df61dbb9c5bfcb3c55bb33c
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-08-10 23:10:28 +0200

    (#19886) Correct issues from review (yardoc, use of '::')
    - This corrects the calling a class method using CLASS::method instead
    of CLASS.method.
    - Yardoc corrected (check method does not return a value)
    - Error message formatting improved.

--- lib/puppetx/puppetlabs/syntax_checkers/json.rb
+++ lib/puppetx/puppetlabs/syntax_checkers/json.rb
@@ -1,3 +1,5 @@
+require 'puppetx/puppet/syntax_checker'
+
 # A syntax checker for JSON.
 # @api public
 class Puppetx::Puppetlabs::SyntaxCheckers::Json < Puppetx::Puppet::SyntaxChecker
@@ -7,29 +9,29 @@ class Puppetx::Puppetlabs::SyntaxCheckers::Json < Puppetx::Puppet::SyntaxChecker
   # method.
   #
   # @param text [String] The text to check
-  # @param syntax [String] The syntax identifier in mime style (e.g. 'json', 'json-patch+json', 'xml', 'myapp+xml'
+  # @param syntax [String] The syntax identifier in mime style (e.g. 'json', 'json-patch+json', 'myapp+xml')
   # @option location_info [String] :file The filename where the string originates
   # @option location_info [Integer] :line The line number identifying the location where the string is being used/checked
   # @option location_info [Integer] :position The position on the line identifying the location where the string is being used/checked
-  # @return [Boolean] Whether the checked string had issues (warnings and/or errors) or not.
+  # @return [Void]
   # @api public
   #
   def check(text, syntax, acceptor, location_info={})
     raise ArgumentError.new("Json syntax checker: the text to check must be a String.") unless text.is_a?(String)
     raise ArgumentError.new("Json syntax checker: the syntax identifier must be a String, e.g. json, data+json") unless syntax.is_a?(String)
     raise ArgumentError.new("Json syntax checker: invalid Acceptor, got: '#{acceptor.class.name}'.") unless acceptor.is_a?(Puppet::Pops::Validation::Acceptor)
-    raise ArgumentError.new("Json syntax checker: location_info must be a Hash") unless info.is_a?(Hash)
+    raise ArgumentError.new("Json syntax checker: location_info must be a Hash") unless location_info.is_a?(Hash)
 
     begin
       JSON.parse(text)
     rescue => e
       # Cap the message to 100 chars and replace newlines
-      msg = "Json syntax checker:: Cannot parse invalid JSON string. \"#{e.message().slice(0,100).gsub(/\r?\n/, "\\n")}\""
+      msg = "Json syntax checker: Cannot parse invalid JSON string. \"#{e.message().slice(0,100).gsub(/\r?\n/, "\\n")}\""
 
       # TODO: improve the pops API to allow simpler diagnostic creation while still maintaining capabilities
       # and the issue code. (In this case especially, where there is only a single error message being issued).
       #
-      issue = Puppet::Pops::Issues::issue(:ILLEGAL_JSON) { msg }
+      issue = Puppet::Pops::Issues.issue(:ILLEGAL_JSON) { msg }
       source_pos = Puppet::Pops::Adapters::SourcePosAdapter.new()
       source_pos.line = location_info[:line]
       source_pos.pos = location_info[:pos]
