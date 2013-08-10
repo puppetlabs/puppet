6c2745d8a740a6860b51c410a0268c686ffc839f
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-08-10 05:30:25 +0200

    (maint) Remove unused Hiera2::Backend class
    This class is no longer used. It contained a very simple Hiera2 backend
    loader that was replaced with an extension using the same loader as
    for other extensions.

--- lib/puppet/pops/binder/hiera2.rb
+++ lib/puppet/pops/binder/hiera2.rb
@@ -2,12 +2,9 @@
 # to read module specific data. The configuration is expected to be found in
 # a hiera.yaml file in the root of each module
 module Puppet::Pops::Binder::Hiera2
-  require 'puppet/pops/binder/hiera2/backend'
   require 'puppet/pops/binder/hiera2/config_checker'
   require 'puppet/pops/binder/hiera2/config'
   require 'puppet/pops/binder/hiera2/diagnostic_producer'
   require 'puppet/pops/binder/hiera2/bindings_provider'
   require 'puppet/pops/binder/hiera2/issues'
-#  require 'puppet/pops/binder/hiera2/json_backend'
-#  require 'puppet/pops/binder/hiera2/yaml_backend'
 end
