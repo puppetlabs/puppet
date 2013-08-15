1d7a6dbe87695b0fd9ed64adb170e1a75de21b1d
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-08-14 17:11:10 -0700

    (maint) local puppet user group setup uses puppet

--- acceptance/config/el6/setup/local/pre-suite/02_PuppetUserAndGroup.rb
+++ acceptance/config/el6/setup/local/pre-suite/02_PuppetUserAndGroup.rb
@@ -1,17 +1,9 @@
 test_name 'Puppet User and Group' do
   hosts.each do |host|
-    next if !host['roles'].include?('master')
 
-    step "ensure puppet user and group added to master nodes" do
-      on host, "getent group puppet || groupadd puppet"
-
-      if host['platform'].include? 'solaris'
-        useradd_opts = '-d /puppet -m -s /bin/sh -g puppet puppet'
-      else
-        useradd_opts = 'puppet -g puppet -G puppet'
-      end
-
-      on host, "getent passwd puppet || useradd #{useradd_opts}"
+    step "ensure puppet user and group added to all nodes because this is what the packages do" do
+      on host, puppet("resource user puppet ensure=present")
+      on host, puppet("resource group puppet ensure=present")
     end
 
   end
