8bdf1b90bd61147d2a80284a59c24333471a12a0
Andrew Parker <andy@puppetlabs.com>
2013-08-15 16:26:51 -0700

    (#22205) Re-introduce old ordering as a Prioritizer
    This creates an old prioritizer for resources as it existed in commit
    f51e967b73ff0154801f514aff40ca524bdd8ab1.

--- lib/puppet/graph.rb
+++ lib/puppet/graph.rb
@@ -1,6 +1,7 @@
 module Puppet::Graph
   require 'puppet/graph/simple_graph'
   require 'puppet/graph/sequential_prioritizer'
+  require 'puppet/graph/title_hash_prioritizer'
   require 'puppet/graph/rb_tree_map'
   require 'puppet/graph/key'
   require 'puppet/graph/relationship_graph'
