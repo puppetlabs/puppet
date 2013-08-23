ae113fc7dfa6b10c7ec890d406fde0c29683fd18
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-08-23 14:30:41 -0700

    (#22324) Run agent_disable_lockfile.rb in the context of a master
    Prior to PE port this test was run against a running master.  That
    change was lost when the test was fixed up to be pe/foss neutral.  This
    commit adds back a running master.
    
    It also replaces run_agent_on which is deprecated in current
    puppet_acceptance.

--- acceptance/tests/agent/agent_disable_lockfile.rb
+++ acceptance/tests/agent/agent_disable_lockfile.rb
@@ -7,7 +7,6 @@ test_name "the agent --disable/--enable functionality should manage the agent lo
 #  in terms of actually restricting or allowing new agent runs to begin.
 #
 
-
 require 'puppet/acceptance/temp_file_utils'
 
 extend Puppet::Acceptance::TempFileUtils
@@ -33,58 +32,63 @@ tuples = [
 
 tuples.each do |expected_message, explicitly_specify_message|
 
-  step "disable the agent; specify message? '#{explicitly_specify_message}', message: '#{expected_message}'" do
-    agents.each do |agent|
-      if (explicitly_specify_message)
-        run_agent_on(agent, "--disable \"#{expected_message}\"")
-      else
-        run_agent_on(agent, "--disable")
-      end
+  with_puppet_running_on(master) do
 
-      agent_disabled_lockfile = "#{agent['puppetvardir']}/state/agent_disabled.lock"
-      unless file_exists?(agent, agent_disabled_lockfile) then
-        fail_test("Failed to create disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
-      end
-      lock_file_content = file_contents(agent, agent_disabled_lockfile)
-      # This is a hack; we should parse the JSON into a hash, but I don't think I have a library available
-      #  from the acceptance test framework that I can use to do that.  So I'm falling back to <gasp> regex.
-      lock_file_content_regex = /"disabled_message"\s*:\s*"#{expected_message}"/
-      unless lock_file_content =~ lock_file_content_regex
-        fail_test("Disabled lock file contents invalid; expected to match '#{lock_file_content_regex}', got '#{lock_file_content}' on agent '#{agent}'")
-      end
-    end
-  end
+    step "disable the agent; specify message? '#{explicitly_specify_message}', message: '#{expected_message}'" do
+      agents.each do |agent|
+        if (explicitly_specify_message)
+          on(agent, puppet('agent', "--disable \"#{expected_message}\""))
+        else
+          on(agent, puppet('agent', "--disable"))
+        end
 
-  step "attempt to run the agent (message: '#{expected_message}')" do
-    agents.each do |agent|
-      run_agent_on(agent, "--no-daemonize --verbose --onetime --test --server #{master}",
-                   :acceptable_exit_codes => [1]) do
-        disabled_regex = /administratively disabled.*'#{expected_message}'/
-        unless result.stdout =~ disabled_regex
-          fail_test("Unexpected output from attempt to run agent disabled; expecting to match '#{disabled_regex}', got '#{result.stdout}' on agent '#{agent}'")
+        agent_disabled_lockfile = "#{agent['puppetvardir']}/state/agent_disabled.lock"
+        unless file_exists?(agent, agent_disabled_lockfile) then
+          fail_test("Failed to create disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
+        end
+        lock_file_content = file_contents(agent, agent_disabled_lockfile)
+
+        # This is a hack; we should parse the JSON into a hash, but I don't
+        # think I have a library available from the acceptance test framework
+        # that I can use to do that.  So I'm falling back to <gasp> regex.
+        lock_file_content_regex = /"disabled_message"\s*:\s*"#{expected_message}"/
+        unless lock_file_content =~ lock_file_content_regex
+          fail_test("Disabled lock file contents invalid; expected to match '#{lock_file_content_regex}', got '#{lock_file_content}' on agent '#{agent}'")
         end
       end
     end
-  end
 
-  step "enable the agent (message: '#{expected_message}')" do
-    agents.each do |agent|
-
-      agent_disabled_lockfile = "#{agent['puppetvardir']}/state/agent_disabled.lock"
-      run_agent_on(agent, "--enable")
-      if file_exists?(agent, agent_disabled_lockfile) then
-        fail_test("Failed to remove disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
+    step "attempt to run the agent (message: '#{expected_message}')" do
+      agents.each do |agent|
+        on(agent, puppet('agent', "--test --server #{master}"),
+                     :acceptable_exit_codes => [1]) do
+          disabled_regex = /administratively disabled.*'#{expected_message}'/
+          unless result.stdout =~ disabled_regex
+            fail_test("Unexpected output from attempt to run agent disabled; expecting to match '#{disabled_regex}', got '#{result.stdout}' on agent '#{agent}'")
+          end
+        end
       end
     end
 
-  step "verify that we can run the agent (message: '#{expected_message}')" do
-    agents.each do |agent|
-      run_agent_on(agent)
+    step "enable the agent (message: '#{expected_message}')" do
+      agents.each do |agent|
+
+        agent_disabled_lockfile = "#{agent['puppetvardir']}/state/agent_disabled.lock"
+        on(agent, puppet('agent', "--enable"))
+        if file_exists?(agent, agent_disabled_lockfile) then
+          fail_test("Failed to remove disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
+        end
       end
     end
-  end
+
+    step "verify that we can run the agent (message: '#{expected_message}')" do
+      agents.each do |agent|
+        on(agent, puppet('agent', "--test --server #{master}"))
+      end
+    end
+
+  end # with_puppet_running_on block
 
 end # tuples block
 
 @all_tests_passed = true
-
