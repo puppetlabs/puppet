36fc4e17e466905b3cae8f84c02bd65057cbbf17
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-09-11 16:27:29 -0700

    (maint) Stops firewall on redhat/debian platforms
    We do need the firewalls disabled for puppet kick tests to work.
    Firewall service on Fedora is firewalld (F18 anyway).

--- acceptance/config/el6/setup/jenkins/pre-suite/02_StopFirewall.rb
+++ acceptance/config/el6/setup/jenkins/pre-suite/02_StopFirewall.rb
@@ -1,3 +1,12 @@
-#test_name "Stop firewall" do
-#  on hosts, puppet_resource('service', 'iptables', 'ensure=stopped')
-#end
+test_name "Stop firewall" do
+  hosts.each do |host|
+    case host['platform']
+    when /fedora/
+      on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
+    when /el|centos|debian|ubuntu/
+      on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
+    else
+      logger.notify("Not sure how to clear firewall on #{host['platform']}")
+    end
+  end
+end
