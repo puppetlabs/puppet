c777fcc67298bcc8270d5faafd47161a16a89126
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-09-11 12:27:26 -0700

    (maint) Refactor Puppet::Util::Docs.scrub
    It was hard to understand what this method was doing, and once I got it, I
    noticed it wasn't quite doing what it claimed. (It was cheating -- grabbing the
    indent of the second line with text and assuming that was the least common
    indent. This meant you could break it by starting a doc fragment with a line of
    text, two newlines, and an indented block of example code.)
    
    This commit replaces it with something more clear and correct, as well as some
    comments explaining what we expect it to do. We also trim trailing whitespace,
    while we're at it; this costs some speed, but should make it even more
    reliable/useful.
    
    We also add tests for scrub in this commit, at spec/unit/util/docs_spec.rb.

---
+++ spec/unit/util/docs_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe Puppet::Util::Docs do
+
+  describe '.scrub' do
+    let(:my_cleaned_output) do
+      %q{This resource type uses the prescribed native tools for creating
+groups and generally uses POSIX APIs for retrieving information
+about them.  It does not directly modify `/etc/passwd` or anything.
+
+* Just for fun, we'll add a list.
+* list item two,
+  which has some add'l lines included in it.
+
+And here's a code block:
+
+    this is the piece of code
+    it does something cool
+
+**Autorequires:** I would be listing autorequired resources here.}
+    end
+
+    it "strips the least common indent from multi-line strings, without mangling indentation beyond the least common indent" do
+      input = <<EOT
+        This resource type uses the prescribed native tools for creating
+        groups and generally uses POSIX APIs for retrieving information
+        about them.  It does not directly modify `/etc/passwd` or anything.
+
+        * Just for fun, we'll add a list.
+        * list item two,
+          which has some add'l lines included in it.
+
+        And here's a code block:
+
+            this is the piece of code
+            it does something cool
+
+        **Autorequires:** I would be listing autorequired resources here.
+EOT
+      output = Puppet::Util::Docs.scrub(input)
+      expect(output).to eq my_cleaned_output
+    end
+
+    it "ignores the first line when calculating least common indent" do
+      input = "This resource type uses the prescribed native tools for creating
+        groups and generally uses POSIX APIs for retrieving information
+        about them.  It does not directly modify `/etc/passwd` or anything.
+
+        * Just for fun, we'll add a list.
+        * list item two,
+          which has some add'l lines included in it.
+
+        And here's a code block:
+
+            this is the piece of code
+            it does something cool
+
+        **Autorequires:** I would be listing autorequired resources here."
+      output = Puppet::Util::Docs.scrub(input)
+      expect(output).to eq my_cleaned_output
+    end
+
+    it "strips trailing whitespace from each line, and strips trailing newlines at end" do
+      input = "This resource type uses the prescribed native tools for creating  \n        groups and generally uses POSIX APIs for retrieving information \n        about them.  It does not directly modify `/etc/passwd` or anything.  \n\n        * Just for fun, we'll add a list. \n        * list item two,\n          which has some add'l lines included in it.    \n\n        And here's a code block:\n\n            this is the piece of code \n            it does something cool \n\n        **Autorequires:** I would be listing autorequired resources here. \n\n"
+      output = Puppet::Util::Docs.scrub(input)
+      expect(output).to eq my_cleaned_output
+    end
+
+    it "has no side effects on original input string" do
+      input       = "First line \n        second line \n        \n            indented line \n        \n        last line\n\n"
+      clean_input = "First line \n        second line \n        \n            indented line \n        \n        last line\n\n"
+      not_used = Puppet::Util::Docs.scrub(input)
+      expect(input).to eq clean_input
+    end
+
+    it "does not include whitespace-only lines when calculating least common indent" do
+      input           = "First line\n        second line\n  \n            indented line\n\n        last line"
+      expected_output = "First line\nsecond line\n\n    indented line\n\nlast line"
+      #bogus_output   = "First line\nsecond line\n\n  indented line\n\nlast line"
+      output = Puppet::Util::Docs.scrub(input)
+      expect(output).to eq expected_output
+    end
+
+    it "accepts a least common indent of zero, thus not adding errors when input string is already scrubbed" do
+      expect(Puppet::Util::Docs.scrub(my_cleaned_output)).to eq my_cleaned_output
+    end
+
+
+  end
+end
+
