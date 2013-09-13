941dc243fd18f694f38bdc30e7f9de60d25a20f2
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-09-13 10:30:54 -0700

    (maint) Ubuntu firewall service is ufw

--- acceptance/config/el6/setup/packages/pre-suite/02_StopFirewall.rb
+++ acceptance/config/el6/setup/packages/pre-suite/02_StopFirewall.rb
@@ -3,8 +3,10 @@ test_name "Stop firewall" do
     case host['platform']
     when /fedora/
       on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
-    when /el|centos|debian|ubuntu/
+    when /el|centos|debian/
       on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
+    when /ubuntu/
+      on host, puppet('resource', 'service', 'ufw', 'ensure=stopped')
     else
       logger.notify("Not sure how to clear firewall on #{host['platform']}")
     end
