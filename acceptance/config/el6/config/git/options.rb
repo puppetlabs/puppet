2296b3b345a7624f347d5b11c86fe5138c467532
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-09-19 11:47:20 -0700

    (maint) Check and require both sha and config for git and package tests
    Allows git checkout tests to checkout specific SHA as well.

--- acceptance/config/el6/config/git/options.rb
+++ acceptance/config/el6/config/git/options.rb
@@ -2,7 +2,6 @@
   :install => [
     'git://github.com/puppetlabs/facter.git#stable',
     'git://github.com/puppetlabs/hiera.git#stable',
-    'git://github.com/puppetlabs/puppet.git#master',
   ],
   :pre_suite => ['setup/git/pre-suite'],
   :ntp => true,
