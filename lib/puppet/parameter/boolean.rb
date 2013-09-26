eff8fb085f6cc48616a97b20d501a9256e42c763
Dustin J. Mitchell <dustin@mozilla.com>
2013-09-26 15:27:24 -0400

    (#22699) set value_collection for boolean params

--- lib/puppet/parameter/boolean.rb
+++ lib/puppet/parameter/boolean.rb
@@ -7,4 +7,9 @@ class Puppet::Parameter::Boolean < Puppet::Parameter
   def unsafe_munge(value)
     Puppet::Coercion.boolean(value)
   end
+
+  def self.initvars
+    super
+    @value_collection.newvalues(*Puppet::Coercion.boolean_values)
+  end
 end
