2f61d80d549a93141381e61bbbd2f84513d91dfc
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-10-04 16:25:22 -0700

    (#22324) Fix modules/install/already_installed tests for pe
    These two tests check the behavior of the module install tool when the
    module we request to install is already installed.  The tests were
    simulating an installed module by manually creating some files, but this
    misses the step of chowning files based on the owner/group settings of
    the moduledir, which is something the puppet module tool does, and
    something module_utils is explicitly testing.
    
    Rather than trying to simulate the owner/group setting that puppet
    module tool would perform, I start off by installing the module with the
    module tool, and then the test continues as before by attempting to
    reinstall.

--- acceptance/tests/modules/install/already_installed_with_local_changes.rb
+++ acceptance/tests/modules/install/already_installed_with_local_changes.rb
@@ -4,6 +4,8 @@ extend Puppet::Acceptance::ModuleUtils
 
 module_author = "pmtacceptance"
 module_name   = "nginx"
+module_reference = "#{module_author}-#{module_name}"
+module_path = "#{master['distmoduledir']}/#{module_name}"
 module_dependencies = []
 
 orig_installed_modules = get_installed_modules_for_hosts hosts
@@ -11,56 +13,49 @@ teardown do
   rm_installed_modules_from_hosts orig_installed_modules, (get_installed_modules_for_hosts hosts)
 end
 
-step 'Setup'
-
-stub_forge_on(master)
-
-apply_manifest_on master, <<-PP
-file {
-  [
-    '#{master['distmoduledir']}/#{module_name}',
-  ]: ensure => directory;
-  '#{master['distmoduledir']}/#{module_name}/metadata.json':
-    content => '{
-      "name": "#{module_author}/#{module_name}",
-      "version": "0.0.1",
-      "source": "",
-      "author": "#{module_author}",
-      "license": "MIT",
-      "checksums": {
-        "README": "2a3adc3b053ef1004df0a02cefbae31f"
-      },
-      "dependencies": []
-    }';
-  '#{master['distmoduledir']}/#{module_name}/README':
-    content => '#{module_name} module';
-}
-PP
-
-
-step "Try to install a module that is already installed"
-on master, puppet("module install #{module_author}-#{module_name}"), :acceptable_exit_codes => [1] do
-  assert_match(/#{module_author}-#{module_name}.*is already installed/, stderr,
-        "Error that module was already installed was not displayed")
-  assert_match(/changes made locally/, stderr,
-        "Error that module has local changes was not displayed")
+step 'Setup' do
+  stub_forge_on(master)
 end
-assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
 
-step "Try to install a specific version of a module that is already installed"
-on master, puppet("module install #{module_author}-#{module_name} --version 1.x"), :acceptable_exit_codes => [1] do
-  assert_match(/Could not install module '#{module_author}-#{module_name}' \(v1.x\)/, stderr,
-        "Error that specified module version could not be installed was not displayed")
-  assert_match(/#{module_author}-#{module_name}.*is already installed/, stderr,
-        "Error that module was already installed was not displayed")
-  assert_match(/changes made locally/, stderr,
-        "Error that module has local changes was not displayed")
+step "Check that module is not installed" do
+  on master, %Q{[ ! -d "#{module_path}" ]}
 end
-assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
 
-step "Install a module that is already installed (with --force)"
-on master, puppet("module install #{module_author}-#{module_name} --force") do
-  assert_module_installed_ui(stdout, module_author, module_name)
+step "Install module" do
+  on master, puppet("module install #{module_reference}")
+  assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
 end
-assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
-#validate checksum
+
+step "Make local changes in installed module" do
+  on master, "echo 'changed' >> #{module_path}/README"
+end
+
+step "Try to install a module that is already installed" do
+  on master, puppet("module install #{module_reference}"), :acceptable_exit_codes => [1] do
+    assert_match(/#{module_reference}.*is already installed/, stderr,
+          "Error that module was already installed was not displayed")
+    assert_match(/changes made locally/, stderr,
+          "Error that module has local changes was not displayed")
+  end
+  assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
+end
+
+step "Try to install a specific version of a module that is already installed" do
+  on master, puppet("module install #{module_reference} --version 1.x"), :acceptable_exit_codes => [1] do
+    assert_match(/Could not install module '#{module_reference}' \(v1.x\)/, stderr,
+          "Error that specified module version could not be installed was not displayed")
+    assert_match(/#{module_reference}.*is already installed/, stderr,
+          "Error that module was already installed was not displayed")
+    assert_match(/changes made locally/, stderr,
+          "Error that module has local changes was not displayed")
+  end
+  assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
+end
+
+step "Install a module that is already installed (with --force)" do
+  on master, puppet("module install #{module_reference} --force") do
+    assert_module_installed_ui(stdout, module_author, module_name)
+  end
+  assert_module_installed_on_disk(master, master['distmoduledir'], module_name)
+  #validate checksum
+end
