puppetlabs/puppet

commit e96ac6b6abb9f387e9b8cd097b024aaa4bc656ee

(#19875) Package descriptions obtained by rpm/dpkg providers
Previously Puppet::Provider::Package::Rpm and Dpkg implementations
obtained package information from the system without capturing package
description information.

This change modifies the rpm and dpkg-query format strings to include
descriptions.  This can be used for querying package state on a node.

The rpm implementation is a simple change relying on a SUMMARY field
which is a single line and which does not change the one line per
package parsing structure in the Rpm class.

The dpkg implementation is a more complicated change because we are
using a DESCRIPTION field in dpkg-query's format string, which is a
multi-line value.  The first line is a summary, and is all we want, but
dpkg-query then print's x additional lines of long description.  A new
parse_multi_line method has been added to treat these multi-line results
as a single entry, grab the first line with summary and ignore the
excess lines without triggering warnings.  Completely invalid package
entry results should still trigger warnings.

Current dpkg-query versions (as of 1.16.2 from 2012, I believe) have a
binary:Summary field which would return this to a single line parse and
make it simpler and less error prone again.  But earlier Debian/Ubuntu
installations don't have this (Ubuntu 12.04 for instance).

Dpkg.instance, Dpkg#query both used a Dpkg.dpkgquery_piped to expose an
IO pipe for parsing.  Regex match used in Rpm to ensure we only consume
good query output lines.

lib/puppet/provider/package/rpm.rb
@@ -7,9 +7,13 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
 
   has_feature :versionable
 
+  # Note: self:: is required here to keep these constants in the context of what will
+  # eventually become this Puppet:Type::Package::ProviderRpm class.
+  self::RPM_DESCRIPTION_DELIMITER = ':DESC:'
   # The query format by which we identify installed packages
-  NEVRAFORMAT = "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}"
-  NEVRA_FIELDS = [:name, :epoch, :version, :release, :arch]
+  self::NEVRA_FORMAT = %Q{'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} #{self::RPM_DESCRIPTION_DELIMITER} %{SUMMARY}\\n'}
+  self::NEVRA_REGEX  = %r{^(\S+) (\S+) (\S+) (\S+) (\S+) #{self::RPM_DESCRIPTION_DELIMITER} ?(.*)$}
+  self::NEVRA_FIELDS = [:name, :epoch, :version, :release, :arch, :description]
 
   commands :rpm => "rpm"
 
@@ -44,11 +48,11 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
 
     # list out all of the packages
     begin
-      execpipe("#{command(:rpm)} -qa #{nosignature} #{nodigest} --qf '#{NEVRAFORMAT}\n'") { |process|
+      execpipe("#{command(:rpm)} -qa #{nosignature} #{nodigest} --qf #{self::NEVRA_FORMAT}") { |process|
         # now turn each returned line into a package object
         process.each_line { |line|
           hash = nevra_to_hash(line)
-          packages << new(hash)
+          packages << new(hash) unless hash.empty?
         }
       }
     rescue Puppet::ExecutionFailure
@@ -65,7 +69,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
     #NOTE: Prior to a fix for issue 1243, this method potentially returned a cached value
     #IF YOU CALL THIS METHOD, IT WILL CALL RPM
     #Use get(:property) to check if cached values are available
-    cmd = ["-q", @resource[:name], "#{self.class.nosignature}", "#{self.class.nodigest}", "--qf", "#{NEVRAFORMAT}\n"]
+    cmd = ["-q", @resource[:name], "#{self.class.nosignature}", "#{self.class.nodigest}", "--qf", self.class::NEVRA_FORMAT]
 
     begin
       output = rpm(*cmd)
@@ -74,7 +78,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
     end
 
     # FIXME: We could actually be getting back multiple packages
-    # for multilib
+    # for multilib and this will only return the first such package
     @property_hash.update(self.class.nevra_to_hash(output))
 
     @property_hash.dup
@@ -86,7 +90,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
       @resource.fail "RPMs must specify a package source"
     end
 
-    cmd = [command(:rpm), "-q", "--qf", "#{NEVRAFORMAT}\n", "-p", "#{@resource[:source]}"]
+    cmd = [command(:rpm), "-q", "--qf", self.class::NEVRA_FORMAT, "-p", source]
     h = self.class.nevra_to_hash(execfail(cmd, Puppet::Error))
     h[:ensure]
   end
@@ -135,12 +139,25 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
     self.install
   end
 
+  private
+
+  # @param line [String] one line of rpm package query information
+  # @return [Hash] of NEVRA_FIELDS strings parsed from package info
+  # if we failed to parse
+  # @note warns if failed to match a line, and returns an empty Hash.
+  # @api private
   def self.nevra_to_hash(line)
-    line.chomp!
+    line.strip!
     hash = {}
-    NEVRA_FIELDS.zip(line.split) { |f, v| hash[f] = v }
-    hash[:provider] = self.name
-    hash[:ensure] = "#{hash[:version]}-#{hash[:release]}"
-    hash
+
+    if match = self::NEVRA_REGEX.match(line)
+      self::NEVRA_FIELDS.zip(match.captures) { |f, v| hash[f] = v }
+      hash[:provider] = self.name
+      hash[:ensure] = "#{hash[:version]}-#{hash[:release]}"
+    else
+      Puppet.warning "Failed to match rpm line #{line}"
+    end
+
+    return hash
   end
 end

spec/unit/provider/package/aptrpm_spec.rb
@@ -19,7 +19,7 @@ describe Puppet::Type.type(:package).provider(:aptrpm) do
     def rpm
       pkg.provider.expects(:rpm).
         with('-q', 'faff', '--nosignature', '--nodigest', '--qf',
-             "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n")
+             "'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n'")
     end
 
     it "should report absent packages" do
@@ -28,7 +28,7 @@ describe Puppet::Type.type(:package).provider(:aptrpm) do
     end
 
     it "should report present packages correctly" do
-      rpm.returns("faff-1.2.3-1 0 1.2.3-1 5 i686\n")
+      rpm.returns("faff-1.2.3-1 0 1.2.3-1 5 i686 :DESC: faff desc\n")
       pkg.property(:ensure).retrieve.should == "1.2.3-1-5"
     end
   end

spec/unit/provider/package/rpm_spec.rb
@@ -8,16 +8,18 @@ describe provider_class do
 
   let (:packages) do
     <<-RPM_OUTPUT
-    cracklib-dicts 0 2.8.9 3.3 x86_64
-    basesystem 0 8.0 5.1.1.el5.centos noarch
-    chkconfig 0 1.3.30.2 2.el5 x86_64
-    myresource 0 1.2.3.4 5.el4 noarch
+    cracklib-dicts 0 2.8.9 3.3 x86_64 :DESC: The standard CrackLib dictionaries
+    basesystem 0 8.0 5.1.1.el5.centos noarch :DESC: The skeleton package which defines a simple Red Hat Enterprise Linux system
+    chkconfig 0 1.3.30.2 2.el5 x86_64 :DESC: A system tool for maintaining the /etc/rc*.d hierarchy
+    myresource 0 1.2.3.4 5.el4 noarch :DESC: Now with summary
+    mysummaryless 0 1.2.3.4 5.el4 noarch :DESC:
     RPM_OUTPUT
   end
 
+  let(:resource_name) { 'myresource' }
   let(:resource) do
     Puppet::Type.type(:package).new(
-      :name     => 'myresource',
+      :name     => resource_name,
       :ensure   => :installed
     )
   end
@@ -28,6 +30,10 @@ describe provider_class do
     provider
   end
 
+  let(:nevra_format) { %Q{'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n'} }
+  let(:execute_options) do
+    {:failonfail => true, :combine => true, :custom_environment => {}}
+  end
   let(:rpm_version) { "RPM version 5.0.0\n" }
 
   before(:each) do
@@ -35,13 +41,13 @@ describe provider_class do
     subject.stubs(:which).with("rpm").returns("/bin/rpm")
     subject.instance_variable_set("@current_version", nil)
     Puppet::Type::Package::ProviderRpm.expects(:execute).with(["/bin/rpm", "--version"]).returns(rpm_version).at_most_once
-    Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "--version"], {:failonfail => true, :combine => true, :custom_environment => {}}).returns(rpm_version).at_most_once
+    Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "--version"], execute_options).returns(rpm_version).at_most_once
   end
 
   describe "self.instances" do
     describe "with a modern version of RPM" do
       it "should include all the modern flags" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf '%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n'").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf #{nevra_format}").yields(packages)
 
         installed_packages = subject.instances
       end
@@ -50,7 +56,7 @@ describe provider_class do
     describe "with a version of RPM < 4.1" do
       let(:rpm_version) { "RPM version 4.0.2\n" }
       it "should exclude the --nosignature flag" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa  --nodigest --qf '%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n'").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa  --nodigest --qf #{nevra_format}").yields(packages)
 
         installed_packages = subject.instances
       end
@@ -59,14 +65,14 @@ describe provider_class do
     describe "with a version of RPM < 4.0.2" do
       let(:rpm_version) { "RPM version 3.0.5\n" }
       it "should exclude the --nodigest flag" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa   --qf '%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n'").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa   --qf #{nevra_format}").yields(packages)
 
         installed_packages = subject.instances
       end
     end
 
     it "returns an array of packages" do
-      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf '%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n'").yields(packages)
+      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf #{nevra_format}").yields(packages)
 
       installed_packages = subject.instances
 
@@ -78,7 +84,8 @@ describe provider_class do
           :version => "2.8.9",
           :release => "3.3",
           :arch => "x86_64",
-          :ensure => "2.8.9-3.3"
+          :ensure => "2.8.9-3.3",
+          :description => "The standard CrackLib dictionaries",
         }
       installed_packages[1].properties.should ==
         {
@@ -88,7 +95,8 @@ describe provider_class do
           :version => "8.0",
           :release => "5.1.1.el5.centos",
           :arch => "noarch",
-          :ensure => "8.0-5.1.1.el5.centos"
+          :ensure => "8.0-5.1.1.el5.centos",
+          :description => "The skeleton package which defines a simple Red Hat Enterprise Linux system",
         }
       installed_packages[2].properties.should ==
         {
@@ -98,7 +106,19 @@ describe provider_class do
           :version => "1.3.30.2",
           :release => "2.el5",
           :arch => "x86_64",
-          :ensure => "1.3.30.2-2.el5"
+          :ensure => "1.3.30.2-2.el5",
+          :description => "A system tool for maintaining the /etc/rc*.d hierarchy",
+        }
+      installed_packages.last.properties.should ==
+        {
+          :provider    => :rpm,
+          :name        => "mysummaryless",
+          :epoch       => "0",
+          :version     => "1.2.3.4",
+          :release     => "5.el4",
+          :arch        => "noarch",
+          :ensure      => "1.2.3.4-5.el4",
+          :description => "",
         }
     end
   end
@@ -114,7 +134,7 @@ describe provider_class do
 
     describe "when not already installed" do
       it "should only include the '-i' flag" do
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-i", '/path/to/package'], {:failonfail => true, :combine => true, :custom_environment => {}})
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-i", '/path/to/package'], execute_options)
         provider.install
       end
    end
@@ -127,10 +147,18 @@ describe provider_class do
      end
 
      it "should include the '-U --oldpackage' flags" do
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", ["-U", "--oldpackage"], '/path/to/package'], {:failonfail => true, :combine => true, :custom_environment => {}})
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", ["-U", "--oldpackage"], '/path/to/package'], execute_options)
         provider.install
      end
    end
+  end
+
+  describe "#latest" do
+    it "should retrieve version string after querying rpm for version from source file" do
+      resource.expects(:[]).with(:source).returns('source-string')
+      Puppet::Util::Execution.expects(:execfail).with(["/bin/rpm", "-q", "--qf", nevra_format, "-p", "source-string"], Puppet::Error).returns("myresource 0 1.2.3.4 5.el4 noarch :DESC:\n")
+      provider.latest.should == "1.2.3.4-5.el4"
+    end
   end
 
   describe "#uninstall" do
@@ -142,31 +170,87 @@ describe provider_class do
     end
 
     describe "on a modern RPM" do
-      before(:each) do 
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-q",  "myresource", '--nosignature', '--nodigest', "--qf", "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n"], {:failonfail => true, :combine => true, :custom_environment => {}}).returns("myresource 0 1.2.3.4 5.el4 noarch\n")
+      before(:each) do
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-q",  "myresource", '--nosignature', '--nodigest', "--qf", nevra_format], execute_options).returns("myresource 0 1.2.3.4 5.el4 noarch :DESC:\n")
       end
 
       let(:rpm_version) { "RPM version 4.10.0\n" }
 
       it "should include the architecture in the package name" do
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-e", 'myresource-1.2.3.4-5.el4.noarch'], {:failonfail => true, :combine => true, :custom_environment => {}}).returns('').at_most_once
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-e", 'myresource-1.2.3.4-5.el4.noarch'], execute_options).returns('').at_most_once
         provider.uninstall
       end
     end
 
     describe "on an ancient RPM" do
-      before(:each) do 
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-q",  "myresource", '', '', "--qf", "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH}\n"], {:failonfail => true, :combine => true, :custom_environment => {}}).returns("myresource 0 1.2.3.4 5.el4 noarch\n")
+      before(:each) do
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-q",  "myresource", '', '', '--qf', nevra_format], execute_options).returns("myresource 0 1.2.3.4 5.el4 noarch :DESC:\n")
       end
 
       let(:rpm_version) { "RPM version 3.0.6\n" }
 
       it "should exclude the architecture from the package name" do
-        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-e", 'myresource-1.2.3.4-5.el4'], {:failonfail => true, :combine => true, :custom_environment => {}}).returns('').at_most_once
+        Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-e", 'myresource-1.2.3.4-5.el4'], execute_options).returns('').at_most_once
         provider.uninstall
       end
     end
 
+  end
+
+  describe "parsing" do
+    let(:resource_name) { 'name' }
+
+    def parser_test(rpm_output_string, gold_hash, number_of_warnings = 0)
+      Puppet.expects(:warning).times(number_of_warnings)
+      Puppet::Util::Execution.expects(:execute).with(["/bin/rpm", "-q", resource_name, "--nosignature", "--nodigest", "--qf", nevra_format], execute_options).returns(rpm_output_string)
+      provider.query.should == gold_hash
+    end
+
+    ['name', 'epoch', 'version', 'release', 'arch'].each do |field|
+      let('delimiter') { ':DESC:' }
+      let(:package_hash) do
+        {
+          :name => 'name',
+          :epoch => 'epoch',
+          :version => 'version',
+          :release => 'release',
+          :arch => 'arch',
+          :description => 'a description',
+          :provider => :rpm,
+          :ensure => 'version-release',
+        }
+      end
+      let(:line) { 'name epoch version release arch :DESC: a description' }
+
+      it "should still parse if #{field} is replaced by delimiter" do
+        parser_test(
+          line.gsub(field, delimiter),
+          package_hash.merge(
+            field.to_sym => delimiter,
+            :ensure => 'version-release'.gsub(field, delimiter)
+          )
+        )
+      end
+
+    end
+
+    it "should still parse if missing description" do
+      parser_test(
+        line.gsub(/#{delimiter} .+$/, delimiter),
+        package_hash.merge(:description => '')
+      )
+    end
+
+    it "should still parse if description contains a new line" do
+      parser_test(
+        line.gsub(/#{delimiter} .+$/, "#{delimiter} whoops\nnewline"),
+        package_hash.merge(:description => 'whoops')
+      )
+    end
+
+    it "should warn but not fail if line is unparseable" do
+      parser_test('bad data', {}, 1)
+    end
   end
 
   describe ".nodigest" do

lib/puppet/provider/package/dpkg.rb
@@ -11,39 +11,75 @@ Puppet::Type.type(:package).provide :dpkg, :parent => Puppet::Provider::Package 
   commands :dpkg_deb => "/usr/bin/dpkg-deb"
   commands :dpkgquery => "/usr/bin/dpkg-query"
 
+  # Performs a dpkgquery call with a pipe so that output can be processed
+  # inline in a passed block.
+  # @param args [Array<String>] any command line arguments to be appended to the command
+  # @param block expected to be passed on to execpipe
+  # @return whatever the block returns
+  # @see Puppet::Util::Execution.execpipe
+  # @api private
+  def self.dpkgquery_piped(*args, &block)
+    cmd = args.unshift(command(:dpkgquery))
+    Puppet::Util::Execution.execpipe(cmd, &block)
+  end
+
   def self.instances
     packages = []
 
     # list out all of the packages
-    cmd = "#{command(:dpkgquery)} -W --showformat '${Status} ${Package} ${Version}\\n'"
-    Puppet.debug "Executing '#{cmd}'"
-    Puppet::Util::Execution.execpipe(cmd) do |process|
-      # our regex for matching dpkg output
-      regex = %r{^(\S+) +(\S+) +(\S+) (\S+) (\S*)$}
-      fields = [:desired, :error, :status, :name, :ensure]
-      hash = {}
-
-      # now turn each returned line into a package object
-      process.each_line { |line|
-        if hash = parse_line(line)
-          packages << new(hash)
-        end
-      }
+    dpkgquery_piped('-W', '--showformat', self::DPKG_QUERY_FORMAT_STRING) do |pipe|
+      until pipe.eof?
+        hash = parse_multi_line(pipe)
+        packages << new(hash) if hash
+      end
     end
 
     packages
   end
 
-  self::REGEX = %r{^(\S+) +(\S+) +(\S+) (\S+) (\S*)$}
-  self::FIELDS = [:desired, :error, :status, :name, :ensure]
+  private
 
+  # Note: self:: is required here to keep these constants in the context of what will
+  # eventually become this Puppet:Type::Package::ProviderDpkg class.
+  self::DPKG_DESCRIPTION_DELIMITER = ':DESC:'
+  self::DPKG_QUERY_FORMAT_STRING = %Q{'${Status} ${Package} ${Version} #{self::DPKG_DESCRIPTION_DELIMITER} ${Description}\\n#{self::DPKG_DESCRIPTION_DELIMITER}\\n'}
+  self::FIELDS_REGEX = %r{^(\S+) +(\S+) +(\S+) (\S+) (\S*) #{self::DPKG_DESCRIPTION_DELIMITER} (.*)$}
+  self::FIELDS= [:desired, :error, :status, :name, :ensure, :description]
+  self::END_REGEX = %r{^#{self::DPKG_DESCRIPTION_DELIMITER}$}
+
+  # Handles parsing one package's worth of multi-line dpkg-query output.  Will
+  # emit warnings if it encounters an initial line that does not match
+  # DPKG_QUERY_FORMAT_STRING.  Swallows extra description lines silently.
+  #
+  # @param pipe [IO] the pipe yielded while processing dpkg output
+  # @return [Hash,nil] parsed dpkg-query entry as a hash of FIELDS strings or
+  # nil if we failed to parse
+  # @api private
+  def self.parse_multi_line(pipe)
+
+    line = pipe.gets
+    unless hash = parse_line(line)
+      Puppet.warning "Failed to match dpkg-query line #{line.inspect}"
+      return nil
+    end
+
+    consume_excess_description(pipe)
+
+    return hash
+  end
+
+  # @param line [String] one line of dpkg-query output
+  # @return [Hash,nil] a hash of FIELDS or nil if we failed to match
+  # @api private
   def self.parse_line(line)
-    if match = self::REGEX.match(line)
+    hash = nil
+
+    if match = self::FIELDS_REGEX.match(line)
       hash = {}
 
-      self::FIELDS.zip(match.captures) { |field,value|
+      self::FIELDS.zip(match.captures) do |field,value|
         hash[field] = value
-      }
+      end
 
       hash[:provider] = self.name
 
@@ -53,13 +89,32 @@ Puppet::Type.type(:package).provide :dpkg, :parent => Puppet::Provider::Package 
         hash[:ensure] = :absent
       end
       hash[:ensure] = :held if hash[:desired] == 'hold'
-    else
-      Puppet.warning "Failed to match dpkg-query line #{line.inspect}"
-      return nil
     end
 
-    hash
+    return hash
   end
+
+  # Silently consumes the extra description lines from dpkg-query and brings
+  # us to the next package entry start.
+  #
+  # @note dpkg-query Description field has a one line summary and a multi-line
+  # description.  dpkg-query binary:Summary is what we want to use but was
+  # introduced in 2012 dpkg 1.16.2
+  # (https://launchpad.net/debian/+source/dpkg/1.16.2) and is not not available
+  # in older Debian versions.  So we're placing a delimiter marker at the end
+  # of the description so we can consume and ignore the multiline description
+  # without issuing warnings
+  #
+  # @param pipe [IO] the pipe yielded while processing dpkg output
+  # @return nil
+  def self.consume_excess_description(pipe)
+    until pipe.eof?
+      break if self::END_REGEX.match(pipe.gets)
+    end
+    return nil
+  end
+
+  public
 
   def install
     unless file = @resource[:source]
@@ -94,26 +149,24 @@ Puppet::Type.type(:package).provide :dpkg, :parent => Puppet::Provider::Package 
   end
 
   def query
-    packages = []
-
-    fields = [:desired, :error, :status, :name, :ensure]
-
-    hash = {}
+    hash = nil
 
     # list out our specific package
     begin
-      output = dpkgquery(
+      self.class.dpkgquery_piped(
         "-W",
         "--showformat",
-        '${Status} ${Package} ${Version}\\n',
+        self.class::DPKG_QUERY_FORMAT_STRING,
         @resource[:name]
-      )
+      ) do |pipe|
+        hash = self.class.parse_multi_line(pipe)
+      end
     rescue Puppet::ExecutionFailure
       # dpkg-query exits 1 if the package is not found.
       return {:ensure => :purged, :status => 'missing', :name => @resource[:name], :error => 'ok'}
     end
 
-    hash = self.class.parse_line(output) || {:ensure => :absent, :status => 'missing', :name => @resource[:name], :error => 'ok'}
+    hash ||= {:ensure => :absent, :status => 'missing', :name => @resource[:name], :error => 'ok'}
 
     if hash[:error] != "ok"
       raise Puppet::Error.new(
@@ -148,4 +201,5 @@ Puppet::Type.type(:package).provide :dpkg, :parent => Puppet::Provider::Package 
       execute([:dpkg, "--set-selections"], :failonfail => false, :combine => false, :stdinfile => tmpfile.path.to_s)
     end
   end
+
 end

spec/unit/provider/package/apt_spec.rb
@@ -8,7 +8,11 @@ describe provider do
     @resource = stub 'resource', :[] => "asdf"
     @provider = provider.new(@resource)
 
-    @fakeresult = "install ok installed asdf 1.0\n"
+    @fakeresult = <<-EOF
+install ok installed asdf 1.0 "asdf summary
+ asdf multiline description
+ with multiple lines
+EOF
   end
 
   it "should be versionable" do

spec/unit/provider/package/aptitude_spec.rb
@@ -10,13 +10,13 @@ describe Puppet::Type.type(:package).provider(:aptitude) do
   it { should be_versionable }
 
   context "when retrieving ensure" do
-    { :absent   => "deinstall ok config-files faff 1.2.3-1\n",
-      "1.2.3-1" => "install ok installed faff 1.2.3-1\n",
+    { :absent   => "deinstall ok config-files faff 1.2.3-1 :DESC: faff summary\n:DESC:\n",
+      "1.2.3-1" => "install ok installed faff 1.2.3-1 :DESC: faff summary\n:DESC:\n",
     }.each do |expect, output|
       it "should detect #{expect} packages" do
-        pkg.provider.expects(:dpkgquery).
-          with('-W', '--showformat', '${Status} ${Package} ${Version}\n', 'faff').
-          returns(output)
+        Puppet::Util::Execution.expects(:execpipe).
+          with([pkg.provider.command(:dpkgquery), '-W', '--showformat', "'${Status} ${Package} ${Version} :DESC: ${Description}\\n:DESC:\\n'", 'faff']).
+          yields(StringIO.new(output))
 
         pkg.property(:ensure).retrieve.should == expect
       end

commit 921eccb17d027ca2f02714b7449ad2dd689ffffe

(#22810) Fix rpm provider query method
Background: commit e96ac6b6abb9f387e9b8cd097b024aaa4bc656ee inadvertently
broke the rpm provider query method by wrapping the format in both single
quotes and surrounding double quotes.

This patch restores the quoting behavior prior to the above commit
while preserving the additional field introduced in that commit.

lib/puppet/provider/package/rpm.rb
@@ -11,7 +11,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
   # eventually become this Puppet:Type::Package::ProviderRpm class.
   self::RPM_DESCRIPTION_DELIMITER = ':DESC:'
   # The query format by which we identify installed packages
-  self::NEVRA_FORMAT = %Q{'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} #{self::RPM_DESCRIPTION_DELIMITER} %{SUMMARY}\\n'}
+  self::NEVRA_FORMAT = %Q{%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} #{self::RPM_DESCRIPTION_DELIMITER} %{SUMMARY}\\n}
   self::NEVRA_REGEX  = %r{^(\S+) (\S+) (\S+) (\S+) (\S+)(?: #{self::RPM_DESCRIPTION_DELIMITER} ?(.*))?$}
   self::RPM_PACKAGE_NOT_FOUND_REGEX = /package .+ is not installed/
   self::NEVRA_FIELDS = [:name, :epoch, :version, :release, :arch, :description]
@@ -49,7 +49,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
 
     # list out all of the packages
     begin
-      execpipe("#{command(:rpm)} -qa #{nosignature} #{nodigest} --qf #{self::NEVRA_FORMAT}") { |process|
+      execpipe("#{command(:rpm)} -qa #{nosignature} #{nodigest} --qf '#{self::NEVRA_FORMAT}'") { |process|
         # now turn each returned line into a package object
         process.each_line { |line|
           hash = nevra_to_hash(line)

spec/unit/provider/package/aptrpm_spec.rb
@@ -19,7 +19,7 @@ describe Puppet::Type.type(:package).provider(:aptrpm) do
     def rpm
       pkg.provider.expects(:rpm).
         with('-q', 'faff', '--nosignature', '--nodigest', '--qf',
-             "'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n'")
+             "%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n")
     end
 
     it "should report absent packages" do

spec/unit/provider/package/rpm_spec.rb
@@ -30,7 +30,7 @@ describe provider_class do
     provider
   end
 
-  let(:nevra_format) { %Q{'%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n'} }
+  let(:nevra_format) { %Q{%{NAME} %|EPOCH?{%{EPOCH}}:{0}| %{VERSION} %{RELEASE} %{ARCH} :DESC: %{SUMMARY}\\n} }
   let(:execute_options) do
     {:failonfail => true, :combine => true, :custom_environment => {}}
   end
@@ -47,7 +47,7 @@ describe provider_class do
   describe "self.instances" do
     describe "with a modern version of RPM" do
       it "should include all the modern flags" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf #{nevra_format}").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf '#{nevra_format}'").yields(packages)
 
         installed_packages = subject.instances
       end
@@ -56,7 +56,7 @@ describe provider_class do
     describe "with a version of RPM < 4.1" do
       let(:rpm_version) { "RPM version 4.0.2\n" }
       it "should exclude the --nosignature flag" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa  --nodigest --qf #{nevra_format}").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa  --nodigest --qf '#{nevra_format}'").yields(packages)
 
         installed_packages = subject.instances
       end
@@ -65,14 +65,14 @@ describe provider_class do
     describe "with a version of RPM < 4.0.2" do
       let(:rpm_version) { "RPM version 3.0.5\n" }
       it "should exclude the --nodigest flag" do
-        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa   --qf #{nevra_format}").yields(packages)
+        Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa   --qf '#{nevra_format}'").yields(packages)
 
         installed_packages = subject.instances
       end
     end
 
     it "returns an array of packages" do
-      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf #{nevra_format}").yields(packages)
+      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf '#{nevra_format}'").yields(packages)
 
       installed_packages = subject.instances
 

spec/unit/provider/package/yum_spec.rb
@@ -153,7 +153,7 @@ _pkg mysummaryless 0 1.2.3.4 5.el4 noarch
     end
 
     def expect_execpipe_to_provide_package_info_for_an_rpm_query
-      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf #{nevra_format}").yields(packages)
+      Puppet::Util::Execution.expects(:execpipe).with("/bin/rpm -qa --nosignature --nodigest --qf '#{nevra_format}'").yields(packages)
     end
 
     def expect_python_yumhelper_call_to_return_latest_info