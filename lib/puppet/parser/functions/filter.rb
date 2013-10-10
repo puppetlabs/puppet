ed6916b94d9c0e794f0ca6f94b45ecce0aad4db0
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-10-10 02:32:37 +0200

    (#22792) Rename the 'select' function to 'filter'

--- lib/puppet/parser/functions/filter.rb
+++ lib/puppet/parser/functions/filter.rb
@@ -1,20 +1,21 @@
 require 'puppet/parser/ast/lambda'
 
 Puppet::Parser::Functions::newfunction(
-:select,
+:filter,
 :type => :rvalue,
 :arity => 2,
 :doc => <<-'ENDHEREDOC') do |args|
   Applies a parameterized block to each element in a sequence of entries from the first
-  argument and returns an array with the entires for which the block evaluates to true.
+  argument and returns an array or hash (same type as left operand)
+  with the entires for which the block evaluates to true.
 
   This function takes two mandatory arguments: the first should be an Array or a Hash, and the second
   a parameterized block as produced by the puppet syntax:
 
-        $a.select |$x| { ... }
+        $a.filter |$x| { ... }
 
   When the first argument is an Array, the block is called with each entry in turn. When the first argument
-  is a hash the entry is an array with `[key, value]`.
+  is a Hash the entry is an array with `[key, value]`.
 
   The returned filtered object is of the same type as the receiver.
 
@@ -22,16 +23,16 @@ Puppet::Parser::Functions::newfunction(
 
         # selects all that end with berry
         $a = ["raspberry", "blueberry", "orange"]
-        $a.select |$x| { $x =~ /berry$/ }
+        $a.filter |$x| { $x =~ /berry$/ }
 
-  - Since 3.2
+  - Since 3.4
   - requires `parser = future`.
   ENDHEREDOC
 
   receiver = args[0]
   pblock = args[1]
 
-  raise ArgumentError, ("select(): wrong argument type (#{pblock.class}; must be a parameterized block.") unless pblock.is_a? Puppet::Parser::AST::Lambda
+  raise ArgumentError, ("filter(): wrong argument type (#{pblock.class}; must be a parameterized block.") unless pblock.is_a? Puppet::Parser::AST::Lambda
 
   case receiver
   when Array
@@ -42,6 +43,6 @@ Puppet::Parser::Functions::newfunction(
     result = Hash[result] unless result.is_a? Hash
     result
   else
-    raise ArgumentError, ("select(): wrong argument type (#{receiver.class}; must be an Array or a Hash.")
+    raise ArgumentError, ("filter(): wrong argument type (#{receiver.class}; must be an Array or a Hash.")
   end
 end
