03399c53d8adc61aa3847927ac5e29f3fdc71fd7
John Julien <john@julienfamily.com>
2013-10-12 19:39:14 -0500

    (#9546) Added external fact plugin sync support to plugin face

--- lib/puppet/face/plugin.rb
+++ lib/puppet/face/plugin.rb
@@ -42,6 +42,10 @@ Puppet::Face.define(:plugin, '0.0.1') do
                                          Puppet[:plugindest],
                                          Puppet[:pluginsource],
                                          Puppet[:pluginsignore]).evaluate
+      Puppet::Configurer::Downloader.new("pluginfacts",
+                                         Puppet[:pluginfactdest],
+                                         Puppet[:pluginfactsource],
+                                         Puppet[:pluginsignore]).evaluate
     end
 
     when_rendering :console do |value|
