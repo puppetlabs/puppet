4daef1d373da1bbbd2118881e2ce59c7c1649682
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-10-18 13:53:16 -0700

    Docs: Mention that fqdn_rand accepts strings as seed additions

--- lib/puppet/parser/functions/fqdn_rand.rb
+++ lib/puppet/parser/functions/fqdn_rand.rb
@@ -3,10 +3,13 @@ require 'digest/md5'
 Puppet::Parser::Functions::newfunction(:fqdn_rand, :arity => -2, :type => :rvalue, :doc =>
   "Generates random numbers based on the node's fqdn. Generated random values
   will be a range from 0 up to and excluding n, where n is the first parameter.
-  The second argument specifies a number to add to the seed and is optional, for example:
+  The second argument specifies a number or string to add to the seed and is optional.
+
+  Examples:
 
       $random_number = fqdn_rand(30)
-      $random_number_seed = fqdn_rand(30,30)") do |args|
+      $random_number_seeded = fqdn_rand(30,30)
+      $random_number_seeded2 = fqdn_rand(30,'foo')") do |args|
     max = args.shift.to_i
     seed = Digest::MD5.hexdigest([self['::fqdn'],args].join(':')).hex
     Puppet::Util.deterministic_rand(seed,max)
