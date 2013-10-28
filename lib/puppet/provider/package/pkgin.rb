ab618aafa654f49c21f28b4277c30b19e5d21cee
Javier Palacios <javiplx@gmail.com>
2013-10-28 20:27:41 +0000

    Signal to update method in case no upgrade is posible

--- lib/puppet/provider/package/pkgin.rb
+++ lib/puppet/provider/package/pkgin.rb
@@ -78,14 +78,18 @@ Puppet::Type.type(:package).provide :pkgin, :parent => Puppet::Provider::Package
 
   def latest
     package = parse_pkgsearch_line.detect{ |package| package[:status] == '<' }
-    return nil if not package
+    if not package
+      set( { :abort => true } )
+      return nil
+    end
     notice  "Upgrading #{package[:name]} to #{package[:version]}"
-    pkgin("-y", :install, package[:name]) unless resource.noop
-    return :latest
+    return package[:version]
   end
 
   def update
-    install
+    unless @property_hash[:abort]
+      install
+    end
   end
 
 end
