4cd31eeb0bbd43d120a7e588111d00e2b3b4d717
Kylo Ginsberg <kylo@puppetlabs.com>
2013-10-28 15:39:10 -0700

    (#9546) Update configurer_spec for external facts pluginsync.
    Prior to the #9546 work, there was only one download of plugins,
    and now there may be two.  Update the configurer_spec to reflect
    that change.

--- spec/integration/configurer_spec.rb
+++ spec/integration/configurer_spec.rb
@@ -9,7 +9,9 @@ describe Puppet::Configurer do
   describe "when downloading plugins" do
     it "should use the :pluginsignore setting, split on whitespace, for ignoring remote files" do
       resource = Puppet::Type.type(:notify).new :name => "yay"
-      Puppet::Type.type(:file).expects(:new).with { |args| args[:ignore] == Puppet[:pluginsignore].split(/\s+/) }.returns resource
+      Puppet::Type.type(:file).expects(:new).at_most(2).with do |args|
+        args[:ignore] == Puppet[:pluginsignore].split(/\s+/)
+      end.returns resource
 
       configurer = Puppet::Configurer.new
       configurer.stubs(:download_plugins?).returns true
