17a6d5aaec6e964d86251954e26335c4f6748bbd
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-11-08 11:40:12 -0800

    (maint) Extract common ssl setup for acceptance tests
    Extracts cert initialization from git/package certificate provisioning,
    and common ssl reset steps from the autosign_command and
    puppet_cert_generate_and_autosign tests into the
    puppet/acceptance/common_utils helper lib.

--- acceptance/tests/ssl/autosign_command.rb
+++ acceptance/tests/ssl/autosign_command.rb
@@ -1,48 +1,10 @@
+require 'puppet/acceptance/common_utils'
+extend Puppet::Acceptance::CAUtils
+
 test_name "autosign_command behavior (#7244)" do
 
   def assert_key_generated(name)
     assert_match(/Creating a new SSL key for #{name}/, stdout, "Expected agent to create a new SSL key for autosigning")
-  end
-
-  def reset_agent_ssl
-    return if master.is_pe?
-    clear_agent_ssl
-
-    hostname = master.execute('facter hostname')
-    fqdn = master.execute('facter fqdn')
-
-    step "Master: Ensure the master bootstraps CA"
-    with_puppet_running_on(master,
-                            :master => {
-                              :dns_alt_names => "puppet,#{hostname},#{fqdn}",
-                              :autosign => true,
-                            }
-                          ) do
-
-      agents.each do |agent|
-        next if agent == master
-        step "Clear old agent certificate from master" do
-          agent_cn = on(agent, puppet('agent --configprint certname')).stdout.chomp
-          puts "agent cn: #{agent_cn}"
-          clean_cert(master, agent_cn) if agent_cn
-        end
-      end
-    end
-  end
-
-
-  def clean_cert(host, cn)
-    on(host, puppet('cert', 'clean', cn), :acceptable_exit_codes => [0, 24])
-  end
-
-  def clear_agent_ssl
-    return if master.is_pe?
-    step "All: Clear agent only ssl settings (do not clear master)"
-    hosts.each do |host|
-      next if host == master
-      ssldir = on(host, puppet('agent --configprint ssldir')).stdout.chomp
-      on( host, host_command("rm -rf '#{ssldir}'") )
-    end
   end
 
   testdir = master.tmpdir('autosign_command')
@@ -55,7 +17,7 @@ test_name "autosign_command behavior (#7244)" do
 
   step "Step 1: ensure autosign_command can approve CSRs"
 
-  reset_agent_ssl
+  reset_agent_ssl(false)
 
   master_opts = {'master' => {'autosign' => 'false', 'autosign_command' => '/bin/true'}}
   with_puppet_running_on(master, master_opts) do
@@ -68,7 +30,7 @@ test_name "autosign_command behavior (#7244)" do
     end
   end
 
-  reset_agent_ssl
+  reset_agent_ssl(false)
 
   step "Step 2: ensure autosign_command can reject CSRs"
 
