d28b7d45d1aaac48b6074f5365e80dfe7ad8729d
Dominic Cleal <dcleal@redhat.com>
2013-11-09 11:06:35 +0000

    (maint) Add specs for parser resource parameters
    Classes Puppet::Parser::AST::ResourceParam and Puppet::Parser::Resource::Param

---
+++ spec/unit/parser/resource/param_spec.rb
@@ -0,0 +1,38 @@
+#! /usr/bin/env ruby
+require 'spec_helper'
+
+describe Puppet::Parser::Resource::Param do
+  it "can be instantiated" do
+    Puppet::Parser::Resource::Param.new(:name => 'myparam', :value => 'foo')
+  end
+
+  it "stores the source file" do
+    param = Puppet::Parser::Resource::Param.new(:name => 'myparam', :value => 'foo', :file => 'foo.pp')
+    param.file.should == 'foo.pp'
+  end
+
+  it "stores the line number" do
+    param = Puppet::Parser::Resource::Param.new(:name => 'myparam', :value => 'foo', :line => 42)
+    param.line.should == 42
+  end
+
+  context "parameter validation" do
+    it "throws an error when instantiated without a name" do
+      expect {
+        Puppet::Parser::Resource::Param.new(:value => 'foo')
+      }.to raise_error(Puppet::Error, /name is a required option/)
+    end
+
+    it "throws an error when instantiated without a value" do
+      expect {
+        Puppet::Parser::Resource::Param.new(:name => 'myparam')
+      }.to raise_error(Puppet::Error, /value is a required option/)
+    end
+
+    it "throws an error when instantiated with a nil value" do
+      expect {
+        Puppet::Parser::Resource::Param.new(:name => 'myparam', :value => nil)
+      }.to raise_error(Puppet::Error, /value is a required option/)
+    end
+  end
+end
