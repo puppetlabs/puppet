ee910a4e50f50472b7898f81926f803e49223eb4
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-11-14 11:07:43 -0800

    Maint: Clarify doc string for contain function
    The second sentence used "any given" in an ambiguous way. This edit clarifies
    that only the classes provided as arguments will be declared.

--- lib/puppet/parser/functions/contain.rb
+++ lib/puppet/parser/functions/contain.rb
@@ -4,8 +4,10 @@
 Puppet::Parser::Functions::newfunction(
   :contain,
   :arity => -2,
-  :doc => "Contain one or more classes inside the current class. Any
-given undeclared classes will be declared as if called with 'include'.
+  :doc => "Contain one or more classes inside the current class. If any of
+these classes are undeclared, they will be declared as if called with the
+`include` function. Accepts a class name, an array of class names, or a
+comma-separated list of class names.
 
 A contained class will not be applied before the containing class is
 begun, and will be finished before the containing class is finished.
