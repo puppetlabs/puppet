4d7a26e1bd4dc20687015f0771da72374d10f128
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-12-16 16:52:24 +0100

    (PUP-939) Add support for enumerable type in reduce function.
    This adds support to perform reduce on an enumerable type (Integer
    range).
    
    This also corrects a typo in an error message (said 'collect' instead
    of 'reduce').

--- lib/puppet/parser/functions/reduce.rb
+++ lib/puppet/parser/functions/reduce.rb
@@ -6,14 +6,15 @@ Puppet::Parser::Functions::newfunction(
   Applies a parameterized block to each element in a sequence of entries from the first
   argument (_the collection_) and returns the last result of the invocation of the parameterized block.
 
-  This function takes two mandatory arguments: the first should be an Array or a Hash, and the last
-  a parameterized block as produced by the puppet syntax:
+  This function takes two mandatory arguments: the first should be an Array, Hash, or enumerable type, and
+  the last a parameterized block as produced by the puppet syntax:
 
         $a.reduce |$memo, $x| { ... }
 
-  When the first argument is an Array, the block is called with each entry in turn. When the first argument
-  is a hash each entry is converted to an array with `[key, value]` before being fed to the block. An optional
-  'start memo' value may be supplied as an argument between the array/hash and mandatory block.
+  When the first argument is an Array or an enumerable type, the block is called with each entry in turn.
+  When the first argument is a hash each entry is converted to an array with `[key, value]` before being
+  fed to the block. An optional 'start memo' value may be supplied as an argument between the array/hash
+  and mandatory block.
 
   If no 'start memo' is given, the first invocation of the parameterized block will be given the first and second
   elements of the collection, and if the collection has fewer than 2 elements, the first
@@ -48,11 +49,26 @@ Puppet::Parser::Functions::newfunction(
         $a.reduce([na, 4]) |$memo, $entry| { [sum, $memo[1]+$entry[1]] }
         #=> [sum, 10]
 
+  *Examples*
+
+        Integer[1,4].reduce |$memo, $x| { $memo + $x }
+        #=> 10
+
   - Since 3.2
   - requires `parser = future`.
   ENDHEREDOC
 
   require 'puppet/parser/ast/lambda'
+
+  def enumerable_Type(o)
+    tc = Puppet::Pops::Types::TypeCalculator.new()
+    enumerable = tc.enumerable(o)
+    if enumerable.nil?
+      raise ArgumentError, ("reduce(): given type '#{tc.string(o)}' is not enumerable")
+    end
+    enumerable.each
+  end
+
   case args.length
   when 2
     pblock = args[1]
@@ -65,8 +81,12 @@ Puppet::Parser::Functions::newfunction(
     raise ArgumentError, ("reduce(): wrong argument type (#{args[1].class}; must be a parameterized block.")
   end
   receiver = args[0]
-  unless [Array, Hash].include?(receiver.class)
-    raise ArgumentError, ("collect(): wrong argument type (#{args[0].class}; must be an Array or a Hash.")
+  case receiver
+  when Array, Hash
+  when Puppet::Pops::Types::PAbstractType
+    receiver = enumerable_Type(receiver)
+  else
+    raise ArgumentError, ("reduce(): wrong argument type (#{args[0].class}; must be an Array, Hash, or enumerable type.")
   end
   if args.length == 3
     receiver.reduce(args[1]) {|memo, x| pblock.call(self, memo, x) }
