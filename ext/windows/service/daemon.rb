0ce0c641dc6ddab50b837c917170747b1139d5a0
glennsarti <spektor@techie.com>
2013-12-16 23:28:29 +0800

    (PUP-1048) Windows puppet service should log to the eventlog
    Cleaned up some whitespace issues
    Changed the wording on some of the log entries to avoid confusion between 'Service resuming' and a Service Resume control message
    Added logging for the Pause, Resume and Shutdown service control messages.  Previously there was none
    Noted Bug id 11 in the win32-service ruby code
    Added a commented out handler for the Interrogation event for debuggin purposes
    Changed the name of the event log function from raise_windows_event to report_windows_event to avoid connotations with raise
    Changed the event source to Puppet so that the correct event source DLL is used.  However it may be a good idea to split the event source between the service and the actual agent so it's easier to read in the Eventlog.  This would require a change to the puppet_for_the_win repo as well.
    The eventlog.close command is within an ensure block to stop leaked handles if an error occurs
    The --logtofile argument was working for the service but was causing the puppet agent run to fail as it didn't know what to do with it.  Used a simple text substitution to remove it from the args string.
    Spelling mistake
    Rebasing broke detecting of the --logtofile argument.  Changed the variable used
    Changed wording of the log message when putting the service into a paused state.  Now references a Puppet Issue ticket for tracking
    The ensure block will make sure that the eventlog handle is not nil before trying to close the handle

--- ext/windows/service/daemon.rb
+++ ext/windows/service/daemon.rb
@@ -31,15 +31,16 @@ class WindowsDaemon < Win32::Daemon
     args = argsv.join(' ')
     @loglevel = LEVELS.index(argsv.index('--debug') ? :debug : :notice)
 
-    @LOG_TO_FILE = (argv.index('--logtofile') ? true : false)
+    @LOG_TO_FILE = (argsv.index('--logtofile') ? true : false)
 
     if (@LOG_TO_FILE)
       FileUtils.mkdir_p(File.dirname(LOG_FILE))
+      args = args.gsub("--logtofile","")
     end
     basedir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
     # The puppet installer registers a 'Puppet' event source.  For the moment events will be logged with this key, but
-    # it may be a good idea to split the Service and Puppet evnts later so it's easier to read in the windows Event Log.
+    # it may be a good idea to split the Service and Puppet events later so it's easier to read in the windows Event Log.
     #
     # Example code to register an event source;
     # eventlogdll =  File.expand_path(File.join(basedir, 'puppet', 'ext', 'windows', 'eventlog', 'puppetres.dll'))
@@ -53,7 +54,7 @@ class WindowsDaemon < Win32::Daemon
     #   )
     # end
 
-    log_notice("Starting service: #{args}")
+    log_notice("Starting service with arguments: #{args}")
 
     while running? do
       return if state != RUNNING
@@ -83,7 +84,7 @@ class WindowsDaemon < Win32::Daemon
 
       log_debug("Service waiting for #{runinterval} seconds")
       sleep(runinterval)
-      log_debug('Service resuming')
+      log_debug('Service woken up')
     end
 
     log_notice('Service stopped')
@@ -95,6 +96,26 @@ class WindowsDaemon < Win32::Daemon
     log_notice('Service stopping')
     Thread.main.wakeup
   end
+
+  def service_pause
+    # The service will not stay in a paused stated, instead it will go back into a running state after a short period of time.  This is an issue in the Win32-Service ruby code
+    # Raised bug https://github.com/djberg96/win32-service/issues/11 and is fixed in version 0.8.3.
+    # Because the Pause feature is so rarely used, there is no point in creating a workaround until puppet uses 0.8.3.
+    log_notice('Service pausing. The service will not stay paused. See Puppet Issue PUP-1471 for more information')
+  end
+
+  def service_resume
+    log_notice('Service resuming')
+  end
+
+  def service_shutdown
+    log_notice('Host shutting down')
+  end
+
+  # Interrogation handler is just for debug.  Can be commented out or removed entirely.
+  # def service_interrogate
+  #   log_debug('Service is being interrogated')
+  # end
 
   def log_exception(e)
     log_err(e.message)
@@ -109,30 +130,35 @@ class WindowsDaemon < Win32::Daemon
 
       case level
         when :debug
-          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+          report_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
         when :info
-          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+          report_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
         when :notice
-          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+          report_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
         when :err
-          raise_windows_event(Win32::EventLog::ERR,0x03,msg.to_s)
+          report_windows_event(Win32::EventLog::ERR,0x03,msg.to_s)
         else
-          raise_windows_event(Win32::EventLog::WARN,0x02,msg.to_s)
+          report_windows_event(Win32::EventLog::WARN,0x02,msg.to_s)
       end
     end
   end
 
-  def raise_windows_event(type,id,message)
+  def report_windows_event(type,id,message)
     begin
+      eventlog = nil
       eventlog = Win32::EventLog.open("Application")
       eventlog.report_event(
-        :source      => "Puppet Agent",
+        :source      => "Puppet",
         :event_type  => type,   # Win32::EventLog::INFO or WARN, ERROR
         :event_id    => id,     # 0x01 or 0x02, 0x03 etc.
         :data        => message # "the message"
       )
     rescue Exception => e
       # Ignore all errors
+    ensure
+      if (!eventlog.nil?)
+        eventlog.close
+      end
     end
   end
 end
