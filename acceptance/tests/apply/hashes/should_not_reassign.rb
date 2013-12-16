09a44c6ecfcc665baa496d9f1117d94f3bd94044
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2013-12-16 19:59:32 +0100

    (maint) Fix "should_not_reassign" acceptance test for future parser
    The future evaluator emits a different error message.

--- acceptance/tests/apply/hashes/should_not_reassign.rb
+++ acceptance/tests/apply/hashes/should_not_reassign.rb
@@ -5,6 +5,12 @@ $my_hash['one']='1.5'
 }
 
 apply_manifest_on(agents, manifest, :acceptable_exit_codes => [1]) do
-    fail_test "didn't find the failure" unless
-        stderr.include? "Assigning to the hash 'my_hash' with an existing key 'one'"
+  expected_error_message =
+  case Puppet[:parser]
+  when 'future'
+    "Illegal attempt to assign via [index/key]. Not an assignable reference"
+  else
+    "Assigning to the hash 'my_hash' with an existing key 'one'"
+  end
+  fail_test("didn't find the failure") unless stderr.include?(expected_error_message)
 end
