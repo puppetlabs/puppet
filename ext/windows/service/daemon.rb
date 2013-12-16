f6055775f31d65ee4f485a9c1ea5ca6c5fee3caa
glennsarti <spektor@techie.com>
2013-12-16 23:15:14 +0800

    (PUP-1048) Windows puppet service should log to the eventlog
    Initial attempt to add in eventlogging for all log messages
    New service command line argument (--logtofile). By default the service will log all messages to the Eventlog.  This argument will log messages both to the Eventlog and the Puppet log file in Common Application Data.  This can be useful if the EventLog logging is failing or another application depends on the the logfile
    Basic tests show the new code works however more testing should be done

--- ext/windows/service/daemon.rb
+++ ext/windows/service/daemon.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 require 'win32/daemon'
 require 'win32/dir'
 require 'win32/process'
+require 'win32/eventlog'
 
 require 'windows/synchronize'
 require 'windows/handle'
@@ -13,6 +14,7 @@ class WindowsDaemon < Win32::Daemon
   include Windows::Handle
   include Windows::Process
 
+  @LOG_TO_FILE = false
   LOG_FILE =  File.expand_path(File.join(Dir::COMMON_APPDATA, 'PuppetLabs', 'puppet', 'var', 'log', 'windows.log'))
   LEVELS = [:debug, :info, :notice, :err]
   LEVELS.each do |level|
@@ -22,13 +24,34 @@ class WindowsDaemon < Win32::Daemon
   end
 
   def service_init
-    FileUtils.mkdir_p(File.dirname(LOG_FILE))
   end
 
   def service_main(*argsv)
     argsv = (argsv << ARGV).flatten.compact
     args = argsv.join(' ')
     @loglevel = LEVELS.index(argsv.index('--debug') ? :debug : :notice)
+
+    @LOG_TO_FILE = (argv.index('--logtofile') ? true : false)
+
+    if (@LOG_TO_FILE)
+      FileUtils.mkdir_p(File.dirname(LOG_FILE))
+    end
+    basedir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+    # The puppet installer registers a 'Puppet' event source.  For the moment events will be logged with this key, but
+    # it may be a good idea to split the Service and Puppet evnts later so it's easier to read in the windows Event Log.
+    #
+    # Example code to register an event source;
+    # eventlogdll =  File.expand_path(File.join(basedir, 'puppet', 'ext', 'windows', 'eventlog', 'puppetres.dll'))
+    # if (File.exists?(eventlogdll))
+    #   Win32::EventLog.add_event_source(
+    #      'source' => "Application",
+    #      'key_name' => "Puppet Agent",
+    #      'category_count' => 3,
+    #      'event_message_file' => eventlogdll,
+    #      'category_message_file' => eventlogdll
+    #   )
+    # end
 
     log_notice("Starting service: #{args}")
 
@@ -37,7 +60,6 @@ class WindowsDaemon < Win32::Daemon
 
       log_notice('Service running')
 
-      basedir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
       puppet = File.join(basedir, 'bin', 'puppet.bat')
       unless File.exists?(puppet)
         log_err("File not found: '#{puppet}'")
@@ -81,7 +103,36 @@ class WindowsDaemon < Win32::Daemon
 
   def log(msg, level)
     if LEVELS.index(level) >= @loglevel
-      File.open(LOG_FILE, 'a') { |f| f.puts("#{Time.now} Puppet (#{level}): #{msg}") }
+      if (@LOG_TO_FILE)
+        File.open(LOG_FILE, 'a') { |f| f.puts("#{Time.now} Puppet (#{level}): #{msg}") }
+      end
+
+      case level
+        when :debug
+          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+        when :info
+          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+        when :notice
+          raise_windows_event(Win32::EventLog::INFO,0x01,msg.to_s)
+        when :err
+          raise_windows_event(Win32::EventLog::ERR,0x03,msg.to_s)
+        else
+          raise_windows_event(Win32::EventLog::WARN,0x02,msg.to_s)
+      end
+    end
+  end
+
+  def raise_windows_event(type,id,message)
+    begin
+      eventlog = Win32::EventLog.open("Application")
+      eventlog.report_event(
+        :source      => "Puppet Agent",
+        :event_type  => type,   # Win32::EventLog::INFO or WARN, ERROR
+        :event_id    => id,     # 0x01 or 0x02, 0x03 etc.
+        :data        => message # "the message"
+      )
+    rescue Exception => e
+      # Ignore all errors
     end
   end
 end
