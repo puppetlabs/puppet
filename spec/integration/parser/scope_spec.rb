29cff9d91866c45e2c5cd1ab78395ae7783c9987
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2013-12-25 19:53:28 +0100

    (PUP-1220) make the dynamic scoping fix for #22800 flexible
    As suggested by Henrik Lindberg, make the parser only behave correctly
    when parser=future is configured. If working manifests rely on the
    regression, the next point release won't break them. Instead, users can
    test for this using the configuration setting.
    
    The next major release should adopt the fix as standard behavior and
    the legacy support should be removed. Since the variable lookups in
    templates relies on the accessing of local attributes, there is no
    way to issue deprecation warnings to users.

--- spec/integration/parser/scope_spec.rb
+++ spec/integration/parser/scope_spec.rb
@@ -58,6 +58,23 @@ describe "Two step scoping for variables" do
         end.to raise_error(/The value 'top_msg' cannot be converted to Numeric/)
       end
     end
+
+    it "when using a template ignores the dynamic value of the var when using the @varname syntax" do
+      expect_the_message_to_be('node_msg') do <<-MANIFEST
+          node default {
+            $var = "node_msg"
+            include foo
+          }
+          class foo {
+            $var = "foo_msg"
+            include bar
+          }
+          class bar {
+            notify { 'something': message => inline_template("<%= @var %>"), }
+          }
+        MANIFEST
+      end
+    end
   end
 
   shared_examples_for "the scope" do
@@ -575,41 +592,22 @@ describe "Two step scoping for variables" do
         end
       end
 
-      context "when using a template" do
-        it "ignores the dynamic value of the var when using scope.lookupvar" do
-          expect_the_message_to_be('node_msg') do <<-MANIFEST
-              node default {
-                $var = "node_msg"
-                include foo
-              }
-              class foo {
-                $var = "foo_msg"
-                include bar
-              }
-              class bar {
-                notify { 'something': message => inline_template("<%= scope.lookupvar('var') %>"), }
-              }
-            MANIFEST
-          end
-        end
-        it "ignores the dynamic value of the var when using the @varname syntax" do
-          expect_the_message_to_be('node_msg') do <<-MANIFEST
-              node default {
-                $var = "node_msg"
-                include foo
-              }
-              class foo {
-                $var = "foo_msg"
-                include bar
-              }
-              class bar {
-                notify { 'something': message => inline_template("<%= @var %>"), }
-              }
-            MANIFEST
-          end
+      it "when using a template ignores the dynamic value of the var when using scope.lookupvar" do
+        expect_the_message_to_be('node_msg') do <<-MANIFEST
+            node default {
+              $var = "node_msg"
+              include foo
+            }
+            class foo {
+              $var = "foo_msg"
+              include bar
+            }
+            class bar {
+              notify { 'something': message => inline_template("<%= scope.lookupvar('var') %>"), }
+            }
+          MANIFEST
         end
       end
-
     end
 
     describe "using plussignment to change in a new scope" do
