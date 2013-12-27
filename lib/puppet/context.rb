46f330073399cb4887acd739244399be6806c160
Andrew Parker <andy@puppetlabs.com>
2013-12-27 10:33:57 -0800

    (PUP-672) Add nested context capabilities
    A major problem in puppet is keeping track of contextual information.
    The Puppet::Indirector::Request attempts to do this, but it isn't able
    to pass that information between indirector requests. This adds a
    global context system, where once a value is set it cannot be changed,
    but a subcontext can be created.

---
+++ lib/puppet/context.rb
@@ -0,0 +1,60 @@
+module Puppet::Context
+  class ValueAlreadyBoundError < Puppet::Error; end
+  class UndefinedBindingError < Puppet::Error; end
+
+  class Bindings
+    attr_reader :parent
+
+    def initialize(parent)
+      @parent = parent
+      @table = {}
+    end
+
+    def bind(name, value)
+      if @table.include?(name)
+        raise ValueAlreadyBoundError, name
+      else
+        @table[name] = value
+      end
+    end
+
+    def lookup(name)
+      if @table.include?(name)
+        @table[name]
+      elsif @parent
+        @parent.lookup(name)
+      else
+        raise UndefinedBindingError, name
+      end
+    end
+  end
+
+  @bindings = Bindings.new(nil)
+
+  def self.push
+    @bindings = Bindings.new(@bindings)
+  end
+
+  def self.pop
+    @bindings = @bindings.parent
+  end
+
+  def self.bind(name, value)
+    @bindings.bind(name, value)
+  end
+
+  def self.lookup(name)
+    @bindings.lookup(name)
+  end
+
+  def self.override(bindings)
+    push
+    bindings.each do |name, value|
+      bind(name, value)
+    end
+
+    yield
+  ensure
+    pop
+  end
+end
---
+++ spec/unit/context_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe Puppet::Context do
+  before :each do
+    Puppet::Context.push
+  end
+
+  after :each do
+    Puppet::Context.pop
+  end
+
+  it "holds values for later lookup" do
+    Puppet::Context.bind("a", 1)
+
+    expect(Puppet::Context.lookup("a")).to eq(1)
+  end
+
+  it "does not allow a value to be re-set" do
+    Puppet::Context.bind("a", 1)
+
+    expect { Puppet::Context.bind("a", 1) }.to raise_error(Puppet::Context::ValueAlreadyBoundError)
+  end
+
+  it "fails to lookup a value that does not exist" do
+    expect { Puppet::Context.lookup("a") }.to raise_error(Puppet::Context::UndefinedBindingError)
+  end
+
+  it "allows rebinding values in a nested context" do
+    Puppet::Context.bind("a", 1)
+
+    inner = nil
+    Puppet::Context.override("a" => 2) do
+      inner = Puppet::Context.lookup("a")
+    end
+
+    expect(inner).to eq(2)
+  end
+
+  it "outer bindings are available in an overridden context" do
+    Puppet::Context.bind("a", 1)
+
+    inner_a = nil
+    inner_b = nil
+    Puppet::Context.override("b" => 2) do
+      inner_a = Puppet::Context.lookup("a")
+      inner_b = Puppet::Context.lookup("b")
+    end
+
+    expect(inner_a).to eq(1)
+    expect(inner_b).to eq(2)
+  end
+
+  it "overridden bindings do not exist outside of the override" do
+    Puppet::Context.bind("a", 1)
+
+    Puppet::Context.override("a" => 2) do
+    end
+
+    expect(Puppet::Context.lookup("a")).to eq(1)
+  end
+
+  it "overridden bindings do not exist outside of the override even when leaving via an error" do
+    Puppet::Context.bind("a", 1)
+
+    begin
+      Puppet::Context.override("a" => 2) do
+        raise "this should still cause the bindings to leave"
+      end
+    rescue
+    end
+
+    expect(Puppet::Context.lookup("a")).to eq(1)
+  end
+end
