513f5fff4b3a28272bde74cd528a17958fbd9397
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-01-02 15:48:41 -0800

    Revert "Merge pull request #2207 from jpartlow/maint/master/simplify-confine-for-6857-test"
    This reverts commit 6ad16a47f878df3eb3a31c063bff3e0f5c63745b, reversing
    changes made to 2f1bee412b41cd2b244da39883f3a08a37aef1a3.

--- acceptance/tests/ticket_6857_password-disclosure-when-changing-a-users-password.rb
+++ acceptance/tests/ticket_6857_password-disclosure-when-changing-a-users-password.rb
@@ -1,10 +1,14 @@
 test_name "#6857: redact password hashes when applying in noop mode"
 
 hosts_to_test = agents.reject do |agent|
-  result = on(agent, %Q{#{agent['puppetbindir']}/ruby -e 'require "shadow" or raise'}, :silent => true)
-  result.exit_code != 0
+  if agent['platform'].match /(?:ubuntu|centos|debian|el-|fc-)/
+    result = on(agent, %Q{#{agent['puppetbindir']}/ruby -e 'require "shadow" or raise'}, :silent => true)
+    result.exit_code != 0
+  else
+    false
+  end
 end
-skip_test "No suitable hosts found.  Without the Ruby shadow library, passwords cannot be set." if hosts_to_test.empty?
+skip_test "No suitable hosts found" if hosts_to_test.empty?
 
 adduser_manifest = <<MANIFEST
 user { 'passwordtestuser':
