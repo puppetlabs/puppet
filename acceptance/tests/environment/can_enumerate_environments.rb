1fa50a475cab553a84d9bda6c14abf68db23833a
Kylo Ginsberg <kylo@puppetlabs.com>
2014-01-09 23:08:02 -0800

    (PUP-1151) Fix the master port # (thus addressing a failure on windows)

--- acceptance/tests/environment/can_enumerate_environments.rb
+++ acceptance/tests/environment/can_enumerate_environments.rb
@@ -1,12 +1,8 @@
 test_name "Can enumerate environments via an HTTP endpoint"
 
-def master_port(agent)
-  on(agent, "puppet config print masterport --section agent").stdout.chomp
-end
-
 with_puppet_running_on(master, {}) do
   agents.each do |agent|
-    on agent, "curl -ksv https://#{master}:#{master_port(agent)}/v2/environments", :acceptable_exit_codes => [0,7] do
+    on agent, "curl -ksv https://#{master}:8140/v2/environments", :acceptable_exit_codes => [0,7] do
       assert_match(/< HTTP\/1\.\d 403/, stderr)
     end
   end
