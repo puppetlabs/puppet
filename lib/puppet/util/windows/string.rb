7ef76fd8ead2ea1cdcfd8b7a5485b7026e3bc059
Josh Cooper <josh@puppetlabs.com>
2014-01-14 17:18:07 -0800

    (PUP-1389) Simplify logic for appending extra NULL
    This commit simplifies the process for appending a NULL terminator to
    the wide string.

--- lib/puppet/util/windows/string.rb
+++ lib/puppet/util/windows/string.rb
@@ -1,20 +1,11 @@
 require 'puppet/util/windows'
 
 module Puppet::Util::Windows::String
-  require 'ffi'
-
   def wide_string(str)
+    # bug in win32-api, see https://tickets.puppetlabs.com/browse/PUP-1389
     wstr = str.encode('UTF-16LE')
-
-    ptr = FFI::MemoryPointer.new(:uint16, wstr.length + 1)
-    ptr.put_string(0, wstr)
-    ptr.put_uint8(ptr.size - 1, 0)
-    ptr.put_uint8(ptr.size - 2, 0)
-
-    ffi_str = ptr.get_bytes(0, ptr.size)
-    ffi_str.force_encoding('UTF-16LE')
-
-    ffi_str.strip
+    wstr << 0
+    wstr.strip
   end
   module_function :wide_string
 end
