76c064f2f3f6f7ebb660a03861e4156288af828f
Andrew Parker <andy@puppetlabs.com>
2014-01-17 13:41:19 -0800

    (PUP-1118) Add check for getting multiple environments
    The acceptance test now checks for getting multiple environments listed
    from the environmentsdir

--- acceptance/tests/environment/can_enumerate_environments.rb
+++ acceptance/tests/environment/can_enumerate_environments.rb
@@ -25,7 +25,15 @@ def curl_master_from(agent, path, headers = '', &block)
   on agent, "#{curl_base} '#{url}'", &block
 end
 
-with_puppet_running_on(master, {}) do
+environments_dir = master.tmpdir("environments")
+on master, "mkdir -p #{environments_dir}/env1"
+on master, "mkdir -p #{environments_dir}/env2"
+
+with_puppet_running_on(master, {
+  :master => {
+    "environmentsdir" => environments_dir
+  }
+}) do
   agents.each do |agent|
     step "Ensure that an unauthenticated client cannot access the environments list" do
       on agent, "curl -ksv https://#{master}:#{master_port(agent)}/v2.0/environments", :acceptable_exit_codes => [0,7] do
@@ -36,7 +44,7 @@ with_puppet_running_on(master, {}) do
     step "Ensure that an authenticated client can retrieve the list of environments" do
       curl_master_from(agent, '/v2.0/environments') do
         data = JSON.parse(stdout)
-        assert_equal(["production"], data["environments"].keys)
+        assert_equal(["env1", "env2"], data["environments"].keys.sort)
       end
     end
   end
