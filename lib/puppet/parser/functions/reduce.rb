0ef996bd0115041eefa4195abe2723d2b78897f4
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-02-06 18:00:50 +0100

    (PUP-1029) Make reduce functon use common Enumeration support.
    This makes the reduce function use the common Enumeration support.
    Documentation is updated.

--- lib/puppet/parser/functions/reduce.rb
+++ lib/puppet/parser/functions/reduce.rb
@@ -4,23 +4,27 @@ Puppet::Parser::Functions::newfunction(
 :arity => -2,
 :doc => <<-'ENDHEREDOC') do |args|
   Applies a parameterized block to each element in a sequence of entries from the first
-  argument (_the collection_) and returns the last result of the invocation of the parameterized block.
+  argument (_the enumerable_) and returns the last result of the invocation of the parameterized block.
 
-  This function takes two mandatory arguments: the first should be an Array, Hash, or enumerable type, and
-  the last a parameterized block as produced by the puppet syntax:
+  This function takes two mandatory arguments: the first should be an Array, Hash, or something of
+  enumerable type, and the last a parameterized block as produced by the puppet syntax:
 
         $a.reduce |$memo, $x| { ... }
+        reduce($a) |$memo, $x| { ... }
 
-  When the first argument is an Array or an enumerable type, the block is called with each entry in turn.
+  When the first argument is an Array or someting of an enumerable type, the block is called with each entry in turn.
   When the first argument is a hash each entry is converted to an array with `[key, value]` before being
   fed to the block. An optional 'start memo' value may be supplied as an argument between the array/hash
   and mandatory block.
 
+        $a.reduce(start) |$memo, $x| { ... }
+        reduce($a, start) |$memo, $x| { ... }
+
   If no 'start memo' is given, the first invocation of the parameterized block will be given the first and second
-  elements of the collection, and if the collection has fewer than 2 elements, the first
+  elements of the enumeration, and if the enumerable has fewer than 2 elements, the first
   element is produced as the result of the reduction without invocation of the block.
 
-  On each subsequent invocations, the produced value of the invoked parameterized block is given as the memo in the
+  On each subsequent invocation, the produced value of the invoked parameterized block is given as the memo in the
   next invocation.
 
   *Examples*
@@ -34,6 +38,10 @@ Puppet::Parser::Functions::newfunction(
         $a = {a => 1, b => 2, c => 3}
         $a.reduce |$memo, $entry| { [sum, $memo[1]+$entry[1]] }
         #=> [sum, 6]
+
+        # reverse a string
+        "abc".reduce |$memo, $char| { "$char$memo" }
+        #=>"cbe"
 
   It is possible to provide a starting 'memo' as an argument.
 
@@ -54,20 +62,12 @@ Puppet::Parser::Functions::newfunction(
         Integer[1,4].reduce |$memo, $x| { $memo + $x }
         #=> 10
 
-  - Since 3.2
+  - Since 3.2 for Array and Hash
+  - Since 3.5 for additional enumerable types
   - requires `parser = future`.
   ENDHEREDOC
 
   require 'puppet/parser/ast/lambda'
-
-  def enumerable_Type(o)
-    tc = Puppet::Pops::Types::TypeCalculator.new()
-    enumerable = tc.enumerable(o)
-    if enumerable.nil?
-      raise ArgumentError, ("reduce(): given type '#{tc.string(o)}' is not enumerable")
-    end
-    enumerable.each
-  end
 
   case args.length
   when 2
@@ -81,16 +81,14 @@ Puppet::Parser::Functions::newfunction(
     raise ArgumentError, ("reduce(): wrong argument type (#{pblock.class}; must be a parameterized block.")
   end
   receiver = args[0]
-  case receiver
-  when Array, Hash
-  when Puppet::Pops::Types::PAbstractType
-    receiver = enumerable_Type(receiver)
-  else
-    raise ArgumentError, ("reduce(): wrong argument type (#{args[0].class}; must be an Array, Hash, or enumerable type.")
+  enum = Puppet::Pops::Types::Enumeration.enumerator(receiver)
+  unless enum
+    raise ArgumentError, ("reduce(): wrong argument type (#{receiver.class}; must be something enumerable.")
   end
+
   if args.length == 3
-    receiver.reduce(args[1]) {|memo, x| pblock.call(self, memo, x) }
+    enum.reduce(args[1]) {|memo, x| pblock.call(self, memo, x) }
   else
-    receiver.reduce {|memo, x| pblock.call(self, memo, x) }
+    enum.reduce {|memo, x| pblock.call(self, memo, x) }
   end
 end
