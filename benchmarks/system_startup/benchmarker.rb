ba5a68ff329a64d370f6a8ba2aa27bed4157279e
Andrew Parker <andy@puppetlabs.com>
2014-02-07 11:05:03 -0800

    (PUP-753) Benchmark system startup
    This adds a simple benchmark to check how long it takes to get puppet to
    respond to commands on the CLI. This is an important aspect of percieved
    performance for many users. And for anyone trying to script puppet
    commands, this becomes a *very* important aspect of actual system
    performance.

---
+++ benchmarks/system_startup/benchmarker.rb
@@ -0,0 +1,17 @@
+class Benchmarker
+  def initialize(target, size)
+  end
+
+  def setup
+  end
+
+  def generate
+  end
+
+  def run
+    # Just running help is probably a good proxy of a full startup.
+    # Simply asking for the version might also be good, but it would miss all
+    # of the app searching and loading parts
+    `puppet help`
+  end
+end
