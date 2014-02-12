6a5600f8c0906331f64a93ea407938ab2498e2ca
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-02-12 10:33:36 -0800

    (PUP-1584) Module tool acceptance with_environment tests directory envs
    Update the acceptance/tests/modules/*/with_environment.rb tests to check
    both legacy and new directory environments.  Also extracts common
    environment setup to module_utils.

--- acceptance/tests/modules/upgrade/with_environment.rb
+++ acceptance/tests/modules/upgrade/with_environment.rb
@@ -9,37 +9,34 @@ module_dependencies = ["stdlib"]
 orig_installed_modules = get_installed_modules_for_hosts hosts
 teardown do
   rm_installed_modules_from_hosts orig_installed_modules, (get_installed_modules_for_hosts hosts)
-  # TODO make helper take environments into account
-  on master, "rm -rf #{master['puppetpath']}/testenv #{master['puppetpath']}/puppet2.conf"
 end
 
 step 'Setup'
 
 stub_forge_on(master)
 
-# Configure a non-default environment
-apply_manifest_on master, %Q{
-  file {
-    [
-      '#{master['puppetpath']}/testenv',
-      '#{master['puppetpath']}/testenv/modules',
-    ]:
-      ensure => directory,
-  }
-  file {
-    '#{master['puppetpath']}/puppet2.conf':
-      source => $settings::config,
-  }
-}
-on master, "{ echo '[testenv]'; echo 'modulepath=#{master['puppetpath']}/testenv/modules'; } >> #{master['puppetpath']}/puppet2.conf"
+puppet_conf = generate_base_legacy_and_directory_environments(master['puppetpath'])
 
-on master, puppet("module install #{module_author}-#{module_name} --config=#{master['puppetpath']}/puppet2.conf --version 1.6.0 --environment=testenv") do
-  assert_module_installed_ui(stdout, module_author, module_name)
+install_test_module_in = lambda do |environment|
+  on master, puppet("module install #{module_author}-#{module_name} --config=#{puppet_conf} --version 1.6.0 --environment=#{environment}") do
+    assert_module_installed_ui(stdout, module_author, module_name)
+  end
 end
 
-step "Upgrade a module that has a more recent version published"
-on master, puppet("module upgrade #{module_author}-#{module_name} --config=#{master['puppetpath']}/puppet2.conf --environment=testenv") do
-  assert_module_installed_ui(stdout, module_author, module_name)
-  on master, "[ -f #{master['puppetpath']}/testenv/modules/#{module_name}/Modulefile ]"
-  on master, "grep 1.7.1 #{master['puppetpath']}/testenv/modules/#{module_name}/Modulefile"
+check_module_upgrade_in = lambda do |environment, environment_path|
+  on master, puppet("module upgrade #{module_author}-#{module_name} --config=#{puppet_conf} --environment=#{environment}") do
+    assert_module_installed_ui(stdout, module_author, module_name)
+    on master, "[ -f #{environment_path}/#{module_name}/Modulefile ]"
+    on master, "grep 1.7.1 #{environment_path}/#{module_name}/Modulefile"
+  end
+end
+
+step "Upgrade a module that has a more recent version published in a legacy environment" do
+  install_test_module_in.call('legacyenv')
+  check_module_upgrade_in.call('legacyenv', "#{master['puppetpath']}/legacyenv/modules")
+end
+
+step "Upgrade a module that has a more recent version published in a directory environment" do
+  install_test_module_in.call('direnv')
+  check_module_upgrade_in.call('direnv', "#{master['puppetpath']}/environments/direnv/modules")
 end
