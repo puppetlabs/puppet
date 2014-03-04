674bee94bd1b08342067094276c51ad025c10e53
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-03-04 14:06:15 -0800

    (maint) Match current spec style and usage

--- spec/unit/parser/functions/defined_spec.rb
+++ spec/unit/parser/functions/defined_spec.rb
@@ -11,41 +11,40 @@ describe "the 'defined' function" do
     @scope = Puppet::Parser::Scope.new(@compiler)
   end
 
-  it "should exist" do
-    Puppet::Parser::Functions.function("defined").should == "function_defined"
+  it "exists" do
+    expect(Puppet::Parser::Functions.function("defined")).to be_eql("function_defined")
   end
 
-  it "should be true when the name is defined as a class" do
+  it "is true when the name is defined as a class" do
     @scope.known_resource_types.add Puppet::Resource::Type.new(:hostclass, "yayness")
-    @scope.function_defined(["yayness"]).should be_true
+    expect(@scope.function_defined(["yayness"])).to be_true
   end
 
-  it "should be true when the name is defined as a definition" do
+  it "is true when the name is defined as a definition" do
     @scope.known_resource_types.add Puppet::Resource::Type.new(:definition, "yayness")
-    @scope.function_defined(["yayness"]).should be_true
+    expect(@scope.function_defined(["yayness"])).to be_true
   end
 
-  it "should be true when the name is defined as a builtin type" do
-    @scope.function_defined(["file"]).should be_true
+  it "is true when the name is defined as a builtin type" do
+    expect(@scope.function_defined(["file"])).to be_true
   end
 
-
-  it "should be true when any of the provided names are defined" do
+  it "is true when any of the provided names are defined" do
     @scope.known_resource_types.add Puppet::Resource::Type.new(:definition, "yayness")
-    @scope.function_defined(["meh", "yayness", "booness"]).should be_true
+    expect(@scope.function_defined(["meh", "yayness", "booness"])).to be_true
   end
 
-  it "should be false when a single given name is not defined" do
-    @scope.function_defined(["meh"]).should be_false
+  it "is false when a single given name is not defined" do
+    expect(@scope.function_defined(["meh"])).to be_false
   end
 
-  it "should be false when none of the names are defined" do
-    @scope.function_defined(["meh", "yayness", "booness"]).should be_false
+  it "is false when none of the names are defined" do
+    expect(@scope.function_defined(["meh", "yayness", "booness"])).to be_false
   end
 
-  it "should be true when a resource reference is provided and the resource is in the catalog" do
+  it "is true when a resource reference is provided and the resource is in the catalog" do
     resource = Puppet::Resource.new("file", "/my/file")
     @compiler.add_resource(@scope, resource)
-    @scope.function_defined([resource]).should be_true
+    expect(@scope.function_defined([resource])).to be_true
   end
 end
