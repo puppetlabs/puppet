4500f9fafeca5221a0d627e4547b45dc9a6920f9
Reid Vandewiele <reid@puppetlabs.com>
2014-03-07 12:47:40 -0800

    Remove now-redundant Tar::Solaris from module_tool
    Changes to the way Puppet::ModuleTool::Tar::Gnu works means that the
    *::Tar::Solaris variant should now be redundant. It existed to require
    gtar when the underlying platform did not support GNU-tar options, but
    the changes to the core Tar::Gnu class mean that we no longer use any
    flags or options that don't work just fine with oldschool unix tar
    executables. Remove Puppet::ModuleTool::Tar::Solaris.
    
    On a side note, Tar::Gnu should probably be renamed to something more
    generic.

--- lib/puppet/module_tool/tar.rb
+++ lib/puppet/module_tool/tar.rb
@@ -3,14 +3,10 @@ require 'puppet/util'
 
 module Puppet::ModuleTool::Tar
   require 'puppet/module_tool/tar/gnu'
-  require 'puppet/module_tool/tar/solaris'
   require 'puppet/module_tool/tar/mini'
 
   def self.instance(module_name)
-    gtar_platforms = ['Solaris', 'OpenBSD']
-    if gtar_platforms.include?(Facter.value('osfamily')) && Puppet::Util.which('gtar')
-      Solaris.new
-    elsif Puppet::Util.which('tar') && ! Puppet::Util::Platform.windows?
+    if Puppet::Util.which('tar') && ! Puppet::Util::Platform.windows?
       Gnu.new
     elsif Puppet.features.minitar? && Puppet.features.zlib?
       Mini.new(module_name)
--- lib/puppet/module_tool/tar/solaris.rb
+++
@@ -1,5 +0,0 @@
-class Puppet::ModuleTool::Tar::Solaris < Puppet::ModuleTool::Tar::Gnu
-  def initialize
-    super("gtar")
-  end
-end
