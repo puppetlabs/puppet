275ac959db060f80e4b8635e1213cc0c2255c732
Peter Huene <peter.huene@puppetlabs.com>
2014-03-12 15:46:22 -0700

    (maint) Fix face specs to not pollute global state.
    The face_collection_base spec attempts to restore global state stored in
    Puppet::Interface::FaceCollection so that it doesn't interfere with
    other specs that may use faces.
    
    It fails to clear any files required by the autoloader, but clears the
    @faces member.  The result is that if a face is referenced again from
    another spec, it doesn't get defined and added to @faces and cannot be
    found.
    
    The fix is to clear the state before and after each example.
    
    The problem can be observed with the following:
    rspec spec/unit/interface/face_collection_spec.rb spec/unit/application/face_base_spec.rb
    
    Additionally, the indirection_base spec registers a face but does not remove the face
    after the examples have fun.  The fix is to remove the face from the FaceCollection after
    all examples.

--- spec/unit/application/indirection_base_spec.rb
+++ spec/unit/application/indirection_base_spec.rb
@@ -8,19 +8,27 @@ require 'puppet/indirector/face'
 # Stub for testing; the names are critical, sadly. --daniel 2011-03-30
 class Puppet::Application::TestIndirection < Puppet::Application::IndirectionBase
 end
-
-face = Puppet::Indirector::Face.define(:test_indirection, '0.0.1') do
-  summary "fake summary"
-  copyright "Puppet Labs", 2011
-  license   "Apache 2 license; see COPYING"
-end
-# REVISIT: This horror is required because we don't allow anything to be
-# :current except for if it lives on, and is loaded from, disk. --daniel 2011-03-29
-face.instance_variable_set('@version', :current)
-Puppet::Face.register(face)
 ########################################################################
 
 describe Puppet::Application::IndirectionBase do
+  before :all do
+    face = Puppet::Indirector::Face.define(:test_indirection, '0.0.1') do
+      summary "fake summary"
+      copyright "Puppet Labs", 2011
+      license   "Apache 2 license; see COPYING"
+    end
+    # REVISIT: This horror is required because we don't allow anything to be
+    # :current except for if it lives on, and is loaded from, disk. --daniel 2011-03-29
+    face.instance_variable_set('@version', :current)
+
+    Puppet::Face.register(face)
+  end
+
+  after :all do
+    # Delete the face so that it doesn't interfere with other specs
+    Puppet::Interface::FaceCollection.instance_variable_get(:@faces).delete Puppet::Interface::FaceCollection.underscorize(face.name)
+  end
+
   it "should accept a terminus command line option" do
     # It would be nice not to have to stub this, but whatever... writing an
     # entire indirection stack would cause us more grief. --daniel 2011-03-31
