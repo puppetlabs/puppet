cdf70e242f0bf81423419aae97060acab185e802
Colin <colin@puppetlabs.com>
2014-03-13 13:29:39 -0700

    (PUP-1937) Calling 'ruby' on Windows Fails for PE
    The 'ruby' executable does not exist on a PE installation. Instead, the
    real name is 'ruby.exe' and it is located within the PE installation
    directory. This commit includes a new method (ruby_cmd) in WindowsUtil
    for finding the correct executable name and fixes three tests that call
    'ruby' directly on Windows.

--- acceptance/lib/puppet/acceptance/windows_utils.rb
+++ acceptance/lib/puppet/acceptance/windows_utils.rb
@@ -6,8 +6,20 @@ module Puppet
 require 'win32/dir'
 puts Dir::PROFILE.match(/(.*)\\\\[^\\\\]*/)[1]
 END
-        on(agent, "ruby -rubygems -e \"#{getbasedir}\"").stdout.chomp
+        on(agent, "#{ruby_cmd(agent)} -rubygems -e \"#{getbasedir}\"").stdout.chomp
       end
+
+      # ruby for Windows on a PE install lives in <path to Puppet Enterprise>/sys/ruby/bin/ruby.exe
+      # However, FOSS can just use "ruby".
+      def ruby_cmd(agent)
+        if options[:type] =~ /pe/
+          pre_env = agent['puppetbindir'] ? "env PATH=\"#{agent['puppetbindir']}:#{agent['puppetbindir']}/../sys/ruby/bin/:${PATH}\"" : ''
+          "#{pre_env} ruby.exe"
+        else
+          'ruby'
+        end
+      end
+
     end
   end
 end
