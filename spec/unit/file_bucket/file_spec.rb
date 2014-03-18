63537032267349717f50bdfaddc780a439c9e0c4
Jared Jennings <jared.jennings.ctr@us.af.mil>
2014-03-18 14:56:56 -0500

    (PUP-1840) Use configured digest_algorithm for file bucketing and fetching
    Without this patch the filebucket works using MD5, which may fail on
    FIPS 140-2 compliant hosts. This patch makes the filebucket use the
    digest algorithm indicated by the digest_algorithm setting in
    puppet.conf.

--- spec/unit/file_bucket/file_spec.rb
+++ spec/unit/file_bucket/file_spec.rb
@@ -1,19 +1,13 @@
 #! /usr/bin/env ruby
 require 'spec_helper'
-
 require 'puppet/file_bucket/file'
-require 'digest/md5'
-require 'digest/sha1'
 
-describe Puppet::FileBucket::File do
+
+describe Puppet::FileBucket::File, :uses_checksums => true do
   include PuppetSpec::Files
 
-  let(:contents) { "file\r\n contents" }
-  let(:digest) { "8b3702ad1aed1ace7e32bde76ffffb2d" }
-  let(:checksum) { "{md5}#{digest}" }
   # this is the default from spec_helper, but it keeps getting reset at odd times
   let(:bucketdir) { Puppet[:bucketdir] = tmpdir('bucket') }
-  let(:destdir) { File.join(bucketdir, "8/b/3/7/0/2/a/d/#{digest}") }
 
   it "defaults to serializing to `:s`" do
     expect(Puppet::FileBucket::File.default_format).to eq(:s)
@@ -23,20 +17,20 @@ describe Puppet::FileBucket::File do
    expect(Puppet::FileBucket::File.supported_formats).to include(:s, :pson)
   end
 
-  it "can make a round trip through `s`" do
-    file = Puppet::FileBucket::File.new(contents)
+  using_checksums_it "can make a round trip through `s`" do
+    file = Puppet::FileBucket::File.new(plaintext)
 
     tripped = Puppet::FileBucket::File.convert_from(:s, file.render)
 
-    expect(tripped.contents).to eq(contents)
+    expect(tripped.contents).to eq(plaintext)
   end
 
-  it "can make a round trip through `pson`" do
-    file = Puppet::FileBucket::File.new(contents)
+  using_checksums_it "can make a round trip through `pson`" do
+    file = Puppet::FileBucket::File.new(plaintext)
 
     tripped = Puppet::FileBucket::File.convert_from(:pson, file.render(:pson))
 
-    expect(tripped.contents).to eq(contents)
+    expect(tripped.contents).to eq(plaintext)
   end
 
   it "should raise an error if changing content" do
@@ -54,16 +48,35 @@ describe Puppet::FileBucket::File do
     }.to raise_error(ArgumentError, /Unknown option\(s\): crazy_option/)
   end
 
-  it "should set the contents appropriately" do
-    Puppet::FileBucket::File.new(contents).contents.should == contents
+  using_checksums_it "should set the contents appropriately" do
+    Puppet::FileBucket::File.new(plaintext).contents.should == plaintext
   end
 
-  it "should default to 'md5' as the checksum algorithm if the algorithm is not in the name" do
-    Puppet::FileBucket::File.new(contents).checksum_type.should == "md5"
+  using_checksums_describe "with various checksum algorithms" do
+    it "should default to #{metadata[:digest_algorithm]} as the checksum algorithm if the algorithm is not in the name" do
+      Puppet::FileBucket::File.new(plaintext).checksum_type.should == algo
+    end
   end
 
-  it "should calculate the checksum" do
-    Puppet::FileBucket::File.new(contents).checksum.should == checksum
+  using_checksums_it "should support multiple checksum algorithms" do
+    p = Puppet::FileBucket::File.new(plaintext)
+  end
+
+  using_checksums_it "should reject unknown checksum algorithms" do
+    expect {
+      oda = Puppet[:digest_algorithm]
+      begin
+        Puppet[:digest_algorithm] = 'wefoijwefoij23f02j'
+        Puppet::FileBucket::File.new(plaintext)
+      ensure
+        Puppet[:digest_algorithm] = oda
+      end
+    }.to raise_error(Puppet::Error)
+  end
+
+  using_checksums_it "should calculate checksums with various algorithms" do
+    plaintext.should == "my\r\ncontents"
+    Puppet::FileBucket::File.new(plaintext).checksum.should == "{#{algo}}#{checksum}"
   end
 
   describe "when using back-ends" do
@@ -76,12 +89,12 @@ describe Puppet::FileBucket::File do
     end
   end
 
-  it "should return a url-ish name" do
-    Puppet::FileBucket::File.new(contents).name.should == "md5/#{digest}"
+  using_checksums_it "should return a url-ish name" do
+    Puppet::FileBucket::File.new(plaintext).name.should == "#{algo}/#{checksum}"
   end
 
-  it "should reject a url-ish name with an invalid checksum" do
-    bucket = Puppet::FileBucket::File.new(contents)
+  using_checksums_it "should reject a url-ish name with an invalid checksum" do
+    bucket = Puppet::FileBucket::File.new(plaintext)
     expect { bucket.name = "sha1/ae548c0cd614fb7885aaa0b6cb191c34/new/path" }.to raise_error(NoMethodError, /undefined method .name=/)
   end
 
@@ -95,33 +108,33 @@ describe Puppet::FileBucket::File do
     Puppet::FileBucket::File.from_pson({"contents"=>"file contents"}).contents.should == "file contents"
   end
 
-  def make_bucketed_file
-    FileUtils.mkdir_p(destdir)
-    File.open("#{destdir}/contents", 'wb') { |f| f.write contents }
-  end
+  using_checksums_describe "using the indirector's find method" do
+    def make_bucketed_file
+      FileUtils.mkdir_p("#{bucketdir}/#{bucket_dir}")
+      File.open("#{bucketdir}/#{bucket_dir}/contents", 'w') { |f| f.write plaintext }
+    end
 
-  describe "using the indirector's find method" do
     it "should return nil if a file doesn't exist" do
-      bucketfile = Puppet::FileBucket::File.indirection.find("md5/#{digest}")
+      bucketfile = Puppet::FileBucket::File.indirection.find("#{algo}/#{checksum}")
       bucketfile.should == nil
     end
 
     it "should find a filebucket if the file exists" do
       make_bucketed_file
-      bucketfile = Puppet::FileBucket::File.indirection.find("md5/#{digest}")
-      bucketfile.checksum.should == checksum
+      bucketfile = Puppet::FileBucket::File.indirection.find("#{algo}/#{checksum}")
+      bucketfile.should_not == nil
     end
 
     describe "using RESTish digest notation" do
       it "should return nil if a file doesn't exist" do
-        bucketfile = Puppet::FileBucket::File.indirection.find("md5/#{digest}")
+        bucketfile = Puppet::FileBucket::File.indirection.find("#{algo}/#{checksum}")
         bucketfile.should == nil
       end
 
       it "should find a filebucket if the file exists" do
         make_bucketed_file
-        bucketfile = Puppet::FileBucket::File.indirection.find("md5/#{digest}")
-        bucketfile.checksum.should == checksum
+        bucketfile = Puppet::FileBucket::File.indirection.find("#{algo}/#{checksum}")
+        bucketfile.should_not == nil
       end
     end
   end
