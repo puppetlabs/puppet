9d58b3201c2e4498da16180df4920b2a32975285
Chris Portman <chris.portman@optusnet.com.au>
2014-03-28 10:03:11 +1100

    (PUP-2086) Allow purging of groups with filters
    Implementing unless_gid and unless_system_group as parameters of the resources type to allow filtering when purging groups
    The behaviour when a combination of only_gid, unless_gid and unless_system_group is as follows:
    
       only_gid && unless_gid raises error:  Theses to parameters conflict and it is an error to use them both
       only_gid && unless_system_group:  If the only_gid option contains a GID that is lower than the GID specified by unless_system_group (default 500) then an error is raised because it is a direct conflict. If there is no overlap, then no error is raised nor is the unless_system_group evaluated as the system GIDs are outside the only_gid selection anyway ant thus would not be purged.
       unless_gid && unless_system_group: These are complimentary as they both exclude GIDs from being purged.  Unless_gid is first evaluated and then if that doesnt disqualify the GID from purging, unless_system_group is evaluated, if that still does not disqualify the GID from purging, the GID is purged.

--- spec/unit/type/resources_spec.rb
+++ spec/unit/type/resources_spec.rb
@@ -206,79 +206,317 @@ describe resources do
     end
   end
 
+  describe "group_check purge behaviour" do
+    describe "with unless_system_group => true" do
+      before do
+        @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_system_group => true
+        @res.catalog = Puppet::Resource::Catalog.new
+      end
+
+      it "should never purge hardcoded system groups" do
+        %w{root nobody bin noaccess daemon sys adm lp mail wheel}.each do |sys_group|
+          @res.group_check(Puppet::Type.type(:group).new(:name => sys_group)).should be_false
+        end
+      end
+
+      it "should not purge system groups if unless_system_group => true" do
+        group_hash = {:name => 'system_group', :gid => 125, :system => true}
+        group = Puppet::Type.type(:group).new(group_hash)
+        group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+        @res.group_check(group).should be_false
+      end
+
+      it "should purge manual groups if unless_system_group => true" do
+        group_hash = {:name => 'system_group', :gid => 525, :system => true}
+        group = Puppet::Type.type(:group).new(group_hash)
+        group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+        @res.group_check(group).should be_true
+      end
+
+      it "should purge system groups over 500 if unless_system_group => 600" do
+        res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_system_group => 600
+        res.catalog = Puppet::Resource::Catalog.new
+        group_hash = {:name => 'system_group', :gid => 525, :system => true}
+        group = Puppet::Type.type(:group).new(group_hash)
+        group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+        res.group_check(group).should be_false
+      end
+    end
+
+    describe "with unless_gid" do
+      describe "with a comma separated string of gids" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_gid => '15000, 15001, 15002'
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should purge gids that are not in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+
+        it "should not purge gids that are in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 15000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+      end
+
+      describe "with a gid array" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_gid => [15_000, 15_001, 15_002]
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should purge gids that are not in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+
+        it "should not purge gids that are in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 15000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+      end
+
+      describe "with a single gid integer" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_gid => 15_000
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should purge gids that are not specified" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+
+        it "should not purge gids that are specified" do
+          group_hash = {:name => 'special_group', :gid => 15_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+      end
+
+      describe "with a single gid string" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_gid => '15000'
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should purge gids that are not specified" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+
+        it "should not purge gids that are specified" do
+          group_hash = {:name => 'special_group', :gid => 15_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+      end
+    end
+
+    describe "with only_gid" do
+      describe "with a comma separated string of gids" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :only_gid => '15000, 15001, 15002'
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should not purge gids that are not in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+
+        it "should purge gids that are in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 15000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+      end
+
+      describe "with a gid array" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :only_gid => [15_000, 15_001, 15_002]
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should not purge gids that are not in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+
+        it "should purge gids that are in a specified array" do
+          group_hash = {:name => 'special_group', :gid => 15000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+      end
+
+      describe "with a single gid integer" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :only_gid => 15_000
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should not purge gids that are not specified" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+
+        it "should purge gids that are specified" do
+          group_hash = {:name => 'special_group', :gid => 15_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+      end
+
+      describe "with a single gid string" do
+        before do
+          @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :only_gid => '15000'
+          @res.catalog = Puppet::Resource::Catalog.new
+        end
+
+        it "should not purge gids that are not specified" do
+          group_hash = {:name => 'special_group', :gid => 25_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_false
+        end
+
+        it "should purge gids that are specified" do
+          group_hash = {:name => 'special_group', :gid => 15_000}
+          group = Puppet::Type.type(:group).new(group_hash)
+          group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+          @res.group_check(group).should be_true
+        end
+      end
+    end
+
+    describe "with unless_gid and only_gid" do
+      before do
+        @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :unless_gid => [15_000, 15_001, 15_002], :only_gid => [15_003]
+        @res.catalog = Puppet::Resource::Catalog.new
+      end
+
+      it "should raise an error" do
+        group_hash = {:name => 'special_group', :gid => 15_000}
+        group = Puppet::Type.type(:group).new(group_hash)
+        group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+        expect { @res.group_check(group) }.to raise_error ArgumentError
+      end
+    end
+
+    describe "with overlapping only_gid and unless_system_group" do
+      before do
+        @res = Puppet::Type.type(:resources).new :name => :group, :purge => true, :only_gid => [125], :unless_system_group => true
+        @res.catalog = Puppet::Resource::Catalog.new
+      end
+
+      it "should raise an error" do
+        group_hash = {:name => 'special_group', :gid => 125}
+        group = Puppet::Type.type(:group).new(group_hash)
+        group.stubs(:retrieve_resource).returns Puppet::Resource.new("group", group_hash[:name], :parameters => group_hash)
+        expect { @res.group_check(group) }.to raise_error ArgumentError
+      end
+    end    
+  end
+
   describe "#generate" do
     before do
       @host1 = Puppet::Type.type(:host).new(:name => 'localhost', :ip => '127.0.0.1')
       @catalog = Puppet::Resource::Catalog.new
     end
 
-      describe "when dealing with non-purging resources" do
-        before do
-          @resources = Puppet::Type.type(:resources).new(:name => 'host')
-        end
+    describe "when dealing with non-purging resources" do
+      before do
+        @resources = Puppet::Type.type(:resources).new(:name => 'host')
+      end
 
-        it "should not generate any resource" do
-          @resources.generate.should be_empty
+      it "should not generate any resource" do
+        @resources.generate.should be_empty
+      end
+    end
+
+    describe "when the catalog contains a purging resource" do
+      before do
+        @resources = Puppet::Type.type(:resources).new(:name => 'host', :purge => true)
+        @purgeable_resource = Puppet::Type.type(:host).new(:name => 'localhost', :ip => '127.0.0.1')
+        @catalog.add_resource @resources
+      end
+
+      it "should not generate a duplicate of that resource" do
+        Puppet::Type.type(:host).stubs(:instances).returns [@host1]
+        @catalog.add_resource @host1
+        @resources.generate.collect { |r| r.ref }.should_not include(@host1.ref)
+      end
+
+      it "should not include the skipped system users" do
+        res = Puppet::Type.type(:resources).new :name => :user, :purge => true
+        res.catalog = Puppet::Resource::Catalog.new
+
+        root = Puppet::Type.type(:user).new(:name => "root")
+        Puppet::Type.type(:user).expects(:instances).returns [ root ]
+
+        list = res.generate
+
+        names = list.collect { |r| r[:name] }
+        names.should_not be_include("root")
+      end
+
+      describe "when generating a purgeable resource" do
+        it "should be included in the generated resources" do
+          Puppet::Type.type(:host).stubs(:instances).returns [@purgeable_resource]
+          @resources.generate.collect { |r| r.ref }.should include(@purgeable_resource.ref)
         end
       end
 
-      describe "when the catalog contains a purging resource" do
-        before do
-          @resources = Puppet::Type.type(:resources).new(:name => 'host', :purge => true)
-          @purgeable_resource = Puppet::Type.type(:host).new(:name => 'localhost', :ip => '127.0.0.1')
-          @catalog.add_resource @resources
-        end
-
-        it "should not generate a duplicate of that resource" do
-          Puppet::Type.type(:host).stubs(:instances).returns [@host1]
-          @catalog.add_resource @host1
-          @resources.generate.collect { |r| r.ref }.should_not include(@host1.ref)
-        end
-
-        it "should not include the skipped system users" do
-          res = Puppet::Type.type(:resources).new :name => :user, :purge => true
-          res.catalog = Puppet::Resource::Catalog.new
-
-          root = Puppet::Type.type(:user).new(:name => "root")
-          Puppet::Type.type(:user).expects(:instances).returns [ root ]
-
-          list = res.generate
-
-          names = list.collect { |r| r[:name] }
-          names.should_not be_include("root")
-        end
-
-        describe "when generating a purgeable resource" do
-          it "should be included in the generated resources" do
-            Puppet::Type.type(:host).stubs(:instances).returns [@purgeable_resource]
-            @resources.generate.collect { |r| r.ref }.should include(@purgeable_resource.ref)
-          end
-        end
-
-        describe "when the instance's do not have an ensure property" do
-          it "should not be included in the generated resources" do
-            @no_ensure_resource = Puppet::Type.type(:exec).new(:name => "#{File.expand_path('/usr/bin/env')} echo")
-            Puppet::Type.type(:host).stubs(:instances).returns [@no_ensure_resource]
-            @resources.generate.collect { |r| r.ref }.should_not include(@no_ensure_resource.ref)
-          end
-        end
-
-        describe "when the instance's ensure property does not accept absent" do
-          it "should not be included in the generated resources" do
-            @no_absent_resource = Puppet::Type.type(:service).new(:name => 'foobar')
-            Puppet::Type.type(:host).stubs(:instances).returns [@no_absent_resource]
-            @resources.generate.collect { |r| r.ref }.should_not include(@no_absent_resource.ref)
-          end
-        end
-
-        describe "when checking the instance fails" do
-          it "should not be included in the generated resources" do
-            @purgeable_resource = Puppet::Type.type(:host).new(:name => 'foobar')
-            Puppet::Type.type(:host).stubs(:instances).returns [@purgeable_resource]
-            @resources.expects(:check).with(@purgeable_resource).returns(false)
-            @resources.generate.collect { |r| r.ref }.should_not include(@purgeable_resource.ref)
-          end
+      describe "when the instance's do not have an ensure property" do
+        it "should not be included in the generated resources" do
+          @no_ensure_resource = Puppet::Type.type(:exec).new(:name => "#{File.expand_path('/usr/bin/env')} echo")
+          Puppet::Type.type(:host).stubs(:instances).returns [@no_ensure_resource]
+          @resources.generate.collect { |r| r.ref }.should_not include(@no_ensure_resource.ref)
         end
       end
+
+      describe "when the instance's ensure property does not accept absent" do
+        it "should not be included in the generated resources" do
+          @no_absent_resource = Puppet::Type.type(:service).new(:name => 'foobar')
+          Puppet::Type.type(:host).stubs(:instances).returns [@no_absent_resource]
+          @resources.generate.collect { |r| r.ref }.should_not include(@no_absent_resource.ref)
+        end
+      end
+
+      describe "when checking the instance fails" do
+        it "should not be included in the generated resources" do
+          @purgeable_resource = Puppet::Type.type(:host).new(:name => 'foobar')
+          Puppet::Type.type(:host).stubs(:instances).returns [@purgeable_resource]
+          @resources.expects(:check).with(@purgeable_resource).returns(false)
+          @resources.generate.collect { |r| r.ref }.should_not include(@purgeable_resource.ref)
+        end
+      end
+    end
   end
 end
