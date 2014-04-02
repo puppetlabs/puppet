aa9ad6fe562f480e1e78ca74827f8ef1e1590386
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-04-02 14:22:17 +0200

    (maint) Improve documentation ofthe assert_type function

--- lib/puppet/functions/assert_type.rb
+++ lib/puppet/functions/assert_type.rb
@@ -1,4 +1,10 @@
-# Returns the give value if it is an instance of the given type, and raises an error otherwise.
+# Returns the given value if it is an instance of the given type, and raises an error otherwise.
+#
+# @example how to assert type
+#   # assert that `$b` is a non empty `String` and assign to `$a`
+#   $a = assert_type(String[1], $b)
+#
+# See the documentation for "The Puppet Type System" for more information about types.
 #
 Puppet::Functions.create_function(:assert_type) do
   dispatch :assert_type do
@@ -6,6 +12,9 @@ Puppet::Functions.create_function(:assert_type) do
     param optional(object()), 'value'
   end
 
+  # @param type [Type] the type the value must be an instance of
+  # @param value [Optional[Object]] the value to assert
+  #
   def assert_type(type, value)
     unless Puppet::Pops::Types::TypeCalculator.instance?(type,value)
       inferred_type = Puppet::Pops::Types::TypeCalculator.infer(value)
