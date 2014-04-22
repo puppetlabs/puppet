7465a9048e937c26df2cca2fc28ae74cbb6d18cf
Adrien Thebo <git@somethingsinistral.net>
2014-04-22 13:54:39 -0700

    (maint) Use 2 space tabs in cron acceptance tests

--- acceptance/tests/resource/cron/should_create_cron.rb
+++ acceptance/tests/resource/cron/should_create_cron.rb
@@ -12,18 +12,18 @@ teardown do
 end
 
 agents.each do |host|
-    step "ensure the user exist via puppet"
-    setup host
+  step "ensure the user exist via puppet"
+  setup host
 
-    step "apply the resource on the host using puppet resource"
-    on(host, puppet_resource("cron", "crontest", "user=tstuser",
-                  "command=/bin/true", "ensure=present")) do
-      assert_match(/created/, stdout, "Did not create crontab for tstuser on #{host}")
-    end
+  step "apply the resource on the host using puppet resource"
+  on(host, puppet_resource("cron", "crontest", "user=tstuser",
+                           "command=/bin/true", "ensure=present")) do
+    assert_match(/created/, stdout, "Did not create crontab for tstuser on #{host}")
+  end
 
-    step "verify that crontab -l contains what you expected"
-    run_cron_on(host, :list, 'tstuser') do
-      assert_match(/\* \* \* \* \* \/bin\/true/, stdout, "Incorrect crontab for tstuser on #{host}")
-    end
+  step "verify that crontab -l contains what you expected"
+  run_cron_on(host, :list, 'tstuser') do
+    assert_match(/\* \* \* \* \* \/bin\/true/, stdout, "Incorrect crontab for tstuser on #{host}")
+  end
 
 end
