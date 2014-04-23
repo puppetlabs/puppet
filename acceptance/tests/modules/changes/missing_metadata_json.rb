69bd60617205746ab9acdad64a2b7c3eabadb14b
Pieter van de Bruggen <pvande@gmail.com>
2014-04-23 14:55:29 -0700

    (maint) Update PMT acceptance tests to match new verbiage
    Prior to this commit, the acceptance tests would fail due to changes
    made to the error output from the changes command.  This change brings
    those tests in line with the expected behavior.

--- acceptance/tests/modules/changes/missing_metadata_json.rb
+++ acceptance/tests/modules/changes/missing_metadata_json.rb
@@ -14,8 +14,5 @@ step 'Run module changes on a module witch is missing metadata.json'
 on( master, puppet("module changes #{testdir}/nginx"),
     :acceptable_exit_codes => [1] ) do
 
-  assert_equal <<-STDERR, stderr
-\e[1;31mError: No metadata.json found.\e[0m
-\e[1;31mError: Try 'puppet help module changes' for usage\e[0m
-  STDERR
+  assert_match /Error: No file containing checksums found/, stderr
 end
