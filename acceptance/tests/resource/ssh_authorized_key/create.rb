30aa04fe594a6d78837c120f0d2f5043834cb58a
John Duarte <john.duarte@puppetlabs.com>
2014-05-01 09:40:44 -0700

    (PUP-2401) Add ssh_authorized_key acceptance tests
    Add basic acceptance tests for ssh_authorized_key type.
    This adds acceptance tests for create, query, modify, and destroy.
    
    The query test is set to be skipped because the type cannot
    currently be queried by username (see PUP-1605).

---
+++ acceptance/tests/resource/ssh_authorized_key/create.rb
@@ -0,0 +1,32 @@
+test_name "should create an entry for an SSH authorized key"
+
+confine :except, :platform => ['windows']
+
+auth_keys = '~/.ssh/authorized_keys'
+name = "pl#{rand(999999).to_i}"
+
+agents.each do |agent|
+  teardown do
+    #(teardown) restore the #{auth_keys} file
+    on(agent, "mv /tmp/auth_keys #{auth_keys}", :acceptable_exit_codes => [0,1])
+  end
+
+  #------- SETUP -------#
+  step "(setup) backup #{auth_keys} file"
+  on(agent, "cp #{auth_keys} /tmp/auth_keys", :acceptable_exit_codes => [0,1])
+
+  #------- TESTS -------#
+  step "create an authorized key entry with puppet (present)"
+  args = ['ensure=present',
+          "user='root'",
+          "type='rsa'",
+          "key='mykey'",
+         ]
+  on(agent, puppet_resource('ssh_authorized_key', "#{name}", args))
+
+  step "verify entry in #{auth_keys}"
+  on(agent, "cat #{auth_keys}")  do |res|
+    fail_test "didn't find the ssh_authorized_key for #{name}" unless stdout.include? "#{name}"
+  end
+
+end
