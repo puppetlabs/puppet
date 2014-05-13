02ba25fbb9410f884f0409e7fd51318cb8ebd363
Pieter van de Bruggen <pvande@gmail.com>
2014-05-13 16:15:23 -0700

    (PUP-2561) Revert to using UNIX pipes in PMT’s tar operations.
    Prior to this change, we’d attempted to handle shell escaping of
    arguments by passing arrays of arguments rather than a single full
    command string.  While this worked as intended, it meant that we needed
    to stream data between processes through Ruby.  As it happens, larger
    tarballs would cause the write pipeline to become backed up, and since
    we would attempt to write all the data before reading any, we would
    effectively deadlock.
    
    Rather than attempting to stream by chunks, we’ve reverted back to using
    the full string commands, and managing shell escapes (where needed) in
    Ruby.

--- lib/puppet/module_tool/tar/gnu.rb
+++ lib/puppet/module_tool/tar/gnu.rb
@@ -1,28 +1,19 @@
+require 'shellwords'
+
 class Puppet::ModuleTool::Tar::Gnu
   def unpack(sourcefile, destdir, owner)
     sourcefile = File.expand_path(sourcefile)
     destdir = File.expand_path(destdir)
 
     Dir.chdir(destdir) do
-      tarball = Puppet::Util::Execution.execute(['gzip', '-dc', sourcefile])
-      Puppet::Util::Execution.execpipe(['tar', 'xof', '-'], true, 'w+') do |pipe|
-        pipe.write(tarball)
-      end
-
-      Puppet::Util::Execution.execute(['find', destdir] + %w[-type d -exec chmod 755 {} +])
-      Puppet::Util::Execution.execute(['find', destdir] + %w[-type f -exec chmod a-wst {} +])
-      Puppet::Util::Execution.execute(['chown', '-R', owner, destdir])
+      Puppet::Util::Execution.execute("gzip -dc #{Shellwords.shellescape(sourcefile)} | tar xof -")
+      Puppet::Util::Execution.execute("find . -type d -exec chmod 755 {} +")
+      Puppet::Util::Execution.execute("find . -type f -exec chmod a-wst {} +")
+      Puppet::Util::Execution.execute("chown -R #{owner} .")
     end
   end
 
   def pack(sourcedir, destfile)
-    tarball = Puppet::Util::Execution.execute(['tar', 'cf', '-', sourcedir])
-    Puppet::Util::Execution.execpipe(['gzip', '-c'], true, 'w+') do |pipe|
-      pipe.write(tarball)
-      pipe.close_write
-      File.open(destfile, 'w+') do |file|
-        file.write(pipe.read)
-      end
-    end
+    Puppet::Util::Execution.execute("tar cf - #{sourcedir} | gzip -c > #{File.basename(destfile)}")
   end
 end
