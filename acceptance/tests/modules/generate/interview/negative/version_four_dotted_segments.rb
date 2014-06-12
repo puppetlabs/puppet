bb9556069b22e02ea5977a52d14c5614bb080da2
John Duarte <john.duarte@puppetlabs.com>
2014-06-12 09:33:01 -0700

    (PUP-2666) Unskip neg tests for PMT generate interview
    Change criteria of negative tests in order to allow them to
    execute.
    
    The scope of the negative tests has been changed to only evaluating
    that the proper error response is given when unsupported input is
    given for a module version.
    
    With this rescoping, these tests no longer need to be skipped.

--- acceptance/tests/modules/generate/interview/negative/version_four_dotted_segments.rb
+++ acceptance/tests/modules/generate/interview/negative/version_four_dotted_segments.rb
@@ -1,9 +1,7 @@
 test_name "puppet module generate interview version invalid form - four dotted segments"
-skip_test "How to terminate module generate after answering version question?"
 require 'puppet/acceptance/module_utils'
 extend Puppet::Acceptance::ModuleUtils
 
-ABORT = "\003"
 module_author = "foo"
 module_name   = "bar"
 module_dependencies = []
@@ -34,16 +32,12 @@ agents.each do |agent|
       answer_a << answers[q]
       break if q == :version
     end
-    answer_s = answer_a.join("\n") << "\n" << ABORT
+    answer_s = answer_a.join("\n") << "\n" << "\n" << "\n" << "\n" << "\n" << "\n" << "\n" << "\n" << "\n" << "\n" << "\n"
     tmpfile = agent.tmpfile('answers')
     create_remote_file(agent, tmpfile, answer_s)
-    on(agent, puppet("module generate #{module_author}-#{module_name} < #{tmpfile}"), { :acceptable_exit_codes => [1] }) do |res|
-      fail_test('entry not rejected') unless res.stdout.match /not.*Semantic Version/
+    on(agent, puppet("module generate #{module_author}-#{module_name} < #{tmpfile}")) do |res|
+      fail_test('entry not rejected') unless res.stderr.match /not.*Semantic Version/
     end
-  end
-
-  step "Validate #{module_author}-#{module_name} not created" do
-    fail_test('not valid json') if File.exist? "#{module_author}-#{module_name}"
   end
 
 end
