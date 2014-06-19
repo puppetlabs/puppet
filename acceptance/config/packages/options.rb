3105b2e392b7980ae5e0c4b9e46135a0ad498056
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-06-19 16:59:11 -0700

    (PUP-417) Make use of QENG-188 to restart puppet in acceptance
    Versions of Beaker prior to the patch in QENG-188 relied on manually
    starting and stopping puppet with a `puppet master *args` commandline
    invocation followed by a `kill $puppet_pid`. To better mirror manual
    smoke testing, we want to be able to start and stop puppet using the
    service scripts provided by the installation packages, using `puppet
    resource service` calls. We also want to be able to test Puppet from
    packages that install with Passenger, relying on an apache restart.
    
    These changes are all in Beaker, in QENG-188, but this commit is needed
    to adjust puppet acceptance's setup steps to differentiate between a git
    installation, a foss package installation, and a foss package
    installation with passenger. Several tests were adjusted as well to be
    agnostic between foss webrick/passenger installations, wherever
    possible.
    
    This commit also pins to Beaker ~> 1.17, which will pick up future
    versions of Beaker < 2.0.  Beaker is smoke testing PRs against Puppet
    acceptance, among other projects, so I think it's safe to stay
    abreast of releases in the 1.y series.

--- acceptance/config/packages/options.rb
+++ acceptance/config/packages/options.rb
@@ -1,8 +1,11 @@
 {
+  :type => 'foss-packages',
   :pre_suite => [
     'setup/packages/pre-suite/010_Install.rb',
+    'setup/packages/pre-suite/015_PackageHostsPresets.rb',
     'setup/common/pre-suite/025_StopFirewall.rb',
     'setup/common/pre-suite/040_ValidateSignCert.rb',
+    'setup/packages/pre-suite/045_EnsureMasterStartedOnPassenger.rb',
     'setup/common/pre-suite/100_SetParser.rb',
   ],
 }
