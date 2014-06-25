de9749d544923d5bc05622e8f5c49f96c036d449
Josh Cooper <josh@puppetlabs.com>
2014-06-24 17:33:51 -0700

    Revert "Merge branch 'pr/2801'"
    This reverts commit ea304a00efd5aa5837ab74979b60a1597d4eb82d, reversing
    changes made to b9a0cb902a8a77635d1c0fba99a5a4ebad8197c1.

--- acceptance/setup/git/pre-suite/000_EnvSetup.rb
+++ acceptance/setup/git/pre-suite/000_EnvSetup.rb
@@ -38,31 +38,12 @@ install_packages_on(hosts, PACKAGES, :check_if_exists => true)
 hosts.each do |host|
   case host['platform']
   when /windows/
-    arch = lookup_in_env('WIN32_RUBY_ARCH', 'puppet-win32-ruby', 'x86')
-    step "#{host} Selected architecture #{arch}"
-
-    step "#{host} Loading git commit-ish to use for Windows Ruby"
-    version_defs_file = File.join(File.dirname(__FILE__), '../../../../ext/windows/versions.yaml')
-    raise "Unable to find yaml config at #{version_defs_file}:" if ! File.exist?(version_defs_file)
-
-    begin
-      require 'yaml'
-      @version_defs ||= YAML.load_file(version_defs_file)
-    rescue Exception => e
-      STDERR.puts "Unable to load yaml from #{version_defs_file}:"
-      STDERR.puts e
-      raise
-    end
-
-    revision = @version_defs['git_dependencies'][arch]['puppet_win32_ruby_sha']
-    raise "Could not find #{arch} architecture git revision for puppet-win32-ruby in #{version_defs_file}" if revision.nil?
-
-    step "#{host} Install ruby from git - revision #{revision}"
+    step "#{host} Install ruby from git"
     # TODO remove this step once we are installing puppet from msi packages
     install_from_git(host, "/opt/puppet-git-repos",
                      :name => 'puppet-win32-ruby',
                      :path => build_giturl('puppet-win32-ruby'),
-                     :rev => revision)
+                     :rev  => '1.9.3')
     on host, 'cd /opt/puppet-git-repos/puppet-win32-ruby; cp -r ruby/* /'
     on host, 'cd /lib; icacls ruby /grant "Everyone:(OI)(CI)(RX)"'
     on host, 'cd /lib; icacls ruby /reset /T'
