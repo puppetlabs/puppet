f623be4731478d22722a0180e1661a6c267f2098
Andrew Parker <andy@puppetlabs.com>
2014-07-01 17:09:21 -0700

    (PUP-500) Increase coverage of collection queries
    The tags and array handling of collection queries is "special". This
    adds some test coverage around that behavior.

--- spec/integration/parser/collector_spec.rb
+++ spec/integration/parser/collector_spec.rb
@@ -15,6 +15,24 @@ describe Puppet::Parser::Collector do
   end
 
   shared_examples_for "virtual resource collection" do
+    it "matches everything when no query given" do
+      expect_the_message_to_be(["the other message", "the message"], <<-MANIFEST)
+        @notify { "testing": message => "the message" }
+        @notify { "other": message => "the other message" }
+
+        Notify <| |>
+      MANIFEST
+    end
+
+    it "matches on tags" do
+      expect_the_message_to_be(["wanted"], <<-MANIFEST)
+        @notify { "testing": tag => ["one"], message => "wanted" }
+        @notify { "other": tag => ["two"], message => "unwanted" }
+
+        Notify <| tag == one |>
+      MANIFEST
+    end
+
     it "matches on title" do
       expect_the_message_to_be(["the message"], <<-MANIFEST)
         @notify { "testing": message => "the message" }
@@ -94,6 +112,24 @@ describe Puppet::Parser::Collector do
         @notify { "the wrong one": message => "bad message" }
 
         Notify <| title != "the wrong one" |>
+      MANIFEST
+    end
+
+    it "does not exclude resources with unequal arrays" do
+      expect_the_message_to_be(["message", ["not this message", "or this one"]], <<-MANIFEST)
+        @notify { "testing": message => "message" }
+        @notify { "the wrong one": message => ["not this message", "or this one"] }
+
+        Notify <| message != "not this message" |>
+      MANIFEST
+    end
+
+    it "does not exclude tags with inequalities" do
+      expect_the_message_to_be(["wanted message", "the way it works"], <<-MANIFEST)
+        @notify { "testing": tag => ["wanted"], message => "wanted message" }
+        @notify { "other": tag => ["why"], message => "the way it works" }
+
+        Notify <| tag != "why" |>
       MANIFEST
     end
 
