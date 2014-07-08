d43ba8f574bc3a3acb90bd34687d21e4c05ff4ca
Rahul Gopinath <rahul@puppetlabs.com>
2014-07-08 13:28:59 -0700

    (PUP-2909) rubocop - Lint/LiteralInInterpolation
    catch unnecessary interpolations

--- lib/puppet/reference/providers.rb
+++ lib/puppet/reference/providers.rb
@@ -91,7 +91,7 @@ providers = Puppet::Util::Reference.newreference :providers, :title => "Provider
 
     ret << markdown_header(type.name.to_s + "_", 2)
 
-    ret << "[#{type.name}](#{"http://docs.puppetlabs.com/references/stable/type.html##{type.name}"})\n\n"
+    ret << "[#{type.name}](http://docs.puppetlabs.com/references/stable/type.html##{type.name})\n\n"
     ret << option("Default provider", default)
     ret << doctable(headers, table_data)
 
