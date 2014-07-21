c5982c812455d5dace79ec3edeaa8aa0fbedbf62
Brandon High <brandon.high@puppetlabs.com>
2014-07-21 14:19:23 -0700

    (PUP-2936) PMT build spec test improvements
    Adds additional checking for checksums.json and checksum field in metadata.json.

--- spec/unit/module_tool/applications/builder_spec.rb
+++ spec/unit/module_tool/applications/builder_spec.rb
@@ -12,6 +12,53 @@ describe Puppet::ModuleTool::Applications::Builder do
   let(:tarball)      { File.join(path, 'pkg', release_name) + ".tar.gz" }
   let(:builder)      { Puppet::ModuleTool::Applications::Builder.new(path) }
 
+  shared_examples "a packagable module" do
+    def target_exists?(file)
+      File.exist?(File.join(path, "pkg", "#{module_name}-#{version}", file))
+    end
+
+    it "packages the module in a tarball named after the module" do
+      tarrer = mock('tarrer')
+      Puppet::ModuleTool::Tar.expects(:instance).returns(tarrer)
+      Dir.expects(:chdir).with(File.join(path, 'pkg')).yields
+      tarrer.expects(:pack).with(release_name, tarball)
+
+      builder.run
+    end
+
+    it "overwrites existing checksums.json" do
+      origchecksumjson = {'metadata.json' => '0123456789abcdef',
+          'Modulefile' => '0123456789abcdef',
+        }.to_json
+
+      File.open(File.join(path, 'checksums.json'), 'w') do |f|
+        f.puts(origchecksumjson)
+      end
+      tarrer = mock('tarrer')
+      Puppet::ModuleTool::Tar.expects(:instance).returns(tarrer)
+      Dir.expects(:chdir).with(File.join(path, 'pkg')).yields
+      tarrer.expects(:pack).with(release_name, tarball)
+
+      builder.run
+
+      buildchecksums = File.read(File.join(path, "pkg/#{release_name}/checksums.json"))
+      buildchecksums.should_not == origchecksumjson
+    end
+
+    it "removes existing checksums field from metadata.json" do
+      tarrer = mock('tarrer')
+      Puppet::ModuleTool::Tar.expects(:instance).returns(tarrer)
+      Dir.expects(:chdir).with(File.join(path, 'pkg')).yields
+      tarrer.expects(:pack).with(release_name, tarball)
+
+      builder.run
+
+      metadatajson = File.read(File.join(path, "pkg/#{release_name}/metadata.json"))
+      JSON.parse(metadatajson).should_not include('checksums')
+      JSON.parse(metadatajson).should include('name')
+    end
+  end
+
   context 'with metadata.json' do
     before :each do
       File.open(File.join(path, 'metadata.json'), 'w') do |f|
@@ -28,14 +75,27 @@ describe Puppet::ModuleTool::Applications::Builder do
       end
     end
 
-    it "packages the module in a tarball named after the module" do
-      tarrer = mock('tarrer')
-      Puppet::ModuleTool::Tar.expects(:instance).returns(tarrer)
-      Dir.expects(:chdir).with(File.join(path, 'pkg')).yields
-      tarrer.expects(:pack).with(release_name, tarball)
+    it_behaves_like "a packagable module"
+  end
 
-      builder.run
+  context 'with metadata.json containing checksums' do
+    before :each do
+      File.open(File.join(path, 'metadata.json'), 'w') do |f|
+        f.puts({
+          "name" => "#{module_name}",
+          "version" => "#{version}",
+          "source" => "http://github.com/testing/#{module_name}",
+          "author" => "testing",
+          "license" => "Apache License Version 2.0",
+          "summary" => "Puppet testing module",
+          "description" => "This module can be used for basic testing",
+          "project_page" => "http://github.com/testing/#{module_name}",
+          "checksums" => {"module/init.pp" => "0123456789abcdef"}
+        }.to_json)
+      end
     end
+
+    it_behaves_like "a packagable module"
   end
 
   context 'with Modulefile' do
@@ -54,13 +114,6 @@ MODULEFILE
       end
     end
 
-    it "packages the module in a tarball named after the module" do
-      tarrer = mock('tarrer')
-      Puppet::ModuleTool::Tar.expects(:instance).returns(tarrer)
-      Dir.expects(:chdir).with(File.join(path, 'pkg')).yields
-      tarrer.expects(:pack).with(release_name, tarball)
-
-      builder.run
-    end
+    it_behaves_like "a packagable module"
   end
 end
