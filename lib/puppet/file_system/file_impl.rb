eb80a4e917f9ba3031f511ae6b290104695b36cf
Maksym Melnychok <maksym.melnychok@gmail.com>
2014-07-31 10:17:59 +0200

    break ruby2 FileImpl into separate class

--- lib/puppet/file_system/file_impl.rb
+++ lib/puppet/file_system/file_impl.rb
@@ -136,25 +136,7 @@ class Puppet::FileSystem::FileImpl
   end
 
   def compare_stream(path, stream)
-    if RUBY_VERSION > "2"
-      open(path, 0, 'rb') do |a|
-        # use FileUtils 1.9 implementation because of problems with encodings
-        bsize = FileUtils.send :fu_stream_blksize, a, stream
-        sa = sb = nil
-        while sa == sb
-          sa = a.read(bsize)
-          sb = stream.read(bsize)
-          unless sa and sb
-            if sa.nil? and sb.nil?
-              return true
-            end
-          end
-        end
-        false
-      end
-    else
-      open(path, 0, 'rb') { |this| FileUtils.compare_stream(this, stream) }
-    end
+    open(path, 0, 'rb') { |this| FileUtils.compare_stream(this, stream) }
   end
 
   def chmod(mode, path)
