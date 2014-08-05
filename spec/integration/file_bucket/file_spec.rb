133c0b8009a48eef77d68c0bd8d5f79c930dab50
Adrien Thebo <git@somethingsinistral.net>
2014-08-05 14:08:58 -0700

    (maint) Use public API when testing binary file #save

--- spec/integration/file_bucket/file_spec.rb
+++ spec/integration/file_bucket/file_spec.rb
@@ -43,26 +43,13 @@ describe Puppet::FileBucket::File do
     end
   end
 
-  describe "#verify_identical_file!", :if => RUBY_VERSION.match(/^2/) do
-    subject { Puppet::FileBucketFile::File.new }
+  describe "saving binary files", :if => RUBY_VERSION.match(/^2/) do
     let(:binary) { "\xD1\xF2\r\n\x81NuSc\x00".force_encoding(Encoding::ASCII_8BIT) }
 
-    let(:contents_file) do
-      tf = Tempfile.new("hello")
-      tf.write(binary)
-      tf.close
-      tf.path
-    end
-
-    let(:bucket_file) do
-      stub(
-        :stream   => StringIO.new(binary),
-        :contents => binary,
-        :checksum => nil)
-    end
-
-    it "must be identical for binary files" do
-      subject.send :verify_identical_file!, contents_file, bucket_file
+    it "does not error when the same contents are saved twice" do
+      bucket_file = Puppet::FileBucket::File.new(binary)
+      Puppet::FileBucket::File.indirection.save(bucket_file, bucket_file.name)
+      Puppet::FileBucket::File.indirection.save(bucket_file, bucket_file.name)
     end
   end
 end
