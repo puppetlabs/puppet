a061b76dcc18af982e4ff34b4a5b270fd918fe0d
Kylo Ginsberg <kylo@puppetlabs.com>
2014-08-08 13:56:06 -0700

    (PUP-2879) Add cryptdisks-udev to the services blacklist
    Also this patch splits out the long line in upstart_spec.rb to be
    one line per service, so more diff-friendly.

--- lib/puppet/provider/service/init.rb
+++ lib/puppet/provider/service/init.rb
@@ -50,6 +50,9 @@ Puppet::Type.type(:service).provide :init, :parent => :base do
     # Prevent puppet failing to get status of these services, which need parameters
     # passed in (see https://bugs.launchpad.net/ubuntu/+source/puppet/+bug/1276766).
     excludes += %w{idmapd-mounting startpar-bridge}
+    # Prevent puppet failing to get status of cryptdisks-udev, another upstart
+    # service with instances
+    excludes += %w{cryptdisks-udev}
   end
 
   # List all services of this type.
