8ec3f162d6ec947779f11c601841d1c11548f676
John Duarte <john.duarte@puppetlabs.com>
2014-08-14 20:05:47 -0700

    (PUP-3013) Accept. tests - 3.7 PMT updates build
    This commit adds additional acceptance test coverage for updates
    the puppet module build command. These tests validate the
    following:
    
        * detection of symlinks in module
        * detection of file changes created by build process

---
+++ acceptance/tests/modules/build/build_should_not_allow_symlinks.rb
@@ -0,0 +1,33 @@
+test_name "puppet module build should verify there are no symlinks in module"
+
+confine :except, :platform => 'windows'
+
+modauthor = 'foo'
+modname = 'bar'
+defaultversion = '0.1.0'
+buildpath = "#{modname}/pkg/#{modname}-#{defaultversion}"
+
+agents.each do |agent|
+  tmpdir = agent.tmpdir('pmtbuildsymlink')
+
+  teardown do
+    on(agent, "rm -rf #{modname}")
+    on(agent, "rm -rf #{tmpdir}")
+  end
+
+  step 'Generate module' do
+    on(agent, puppet("module generate #{modauthor}-#{modname} --skip-interview"))
+  end
+
+  step 'Add symlink to module' do
+    on(agent, "touch #{tmpdir}/hello")
+    on(agent, "ln -s #{tmpdir}/hello #{modname}/tests/symlink")
+  end
+
+  step 'Build module should fail with message about needing symlinks removed' do
+    on(agent, puppet("module build #{modname}"), :acceptable_exit_codes => [1]) do |res|
+      fail_test('Proper failure message not displayed') unless res.stderr.include? 'Symlinks in modules are unsupported'
+    end
+  end
+
+end
