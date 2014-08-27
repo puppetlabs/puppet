d4a9d557efe8d8c33791997317c18e8b59a8c587
John Duarte <john.duarte@puppetlabs.com>
2014-08-27 16:07:41 -0700

    Fix install-with_version.rb

--- acceptance/tests/modules/install/with_version.rb
+++ acceptance/tests/modules/install/with_version.rb
@@ -20,9 +20,7 @@ agents.each do |agent|
 
   step "  install module '#{module_author}-#{module_name}'"
 
-  puppet("module install --version \"#{module_version}\" #{module_author}-#{module_name}")
-
-  on(agent, command) do
+  on(agent, puppet("module install --version \"#{module_version}\" #{module_author}-#{module_name}")) do
     assert_module_installed_ui(stdout, module_author, module_name, module_version)
   end
   assert_module_installed_on_disk(agent, distmoduledir, module_name)
