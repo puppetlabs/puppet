514d1d622aa99c74e333df43b1b96cc5ce7885e3
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-08-27 23:15:24 +0200

    (maint) Make PuppetSpec::Language emit more helpful stack traces
    This change was required to be able to see anything relevant regarding
    where a problem originated. The fix is to patch the stacktrace
    for failed expectencies. While not ideal there is very little that can
    be done with Rspec 2 to fix this except reimplementing the support for
    produces and fails to be matchers. (Rspec 3 has composable matchers that
    may make this simpler).
    
    A ticket has been logged about further improvements to the two support
    functions (produces, and fails).

--- spec/lib/puppet_spec/language.rb
+++ spec/lib/puppet_spec/language.rb
@@ -2,9 +2,13 @@ require 'puppet_spec/compiler'
 require 'matchers/resource'
 
 module PuppetSpec::Language
+  extend RSpec::Matchers::DSL
+
   def produces(expectations)
+    calledFrom = caller
     expectations.each do |manifest, resources|
       it "evaluates #{manifest} to produce #{resources}" do
+        begin
         case resources
         when String
           node = Puppet::Node.new('specification')
@@ -41,16 +45,29 @@ module PuppetSpec::Language
         else
           raise "Unsupported creates specification: #{resources.inspect}"
         end
+      rescue  Puppet::Error, RSpec::Expectations::ExpectationNotMetError => e
+        # provide the backtrace from the caller, or it is close to impossible to find some originators
+        e.set_backtrace(calledFrom)
+        raise
+      end
+
       end
     end
   end
 
   def fails(expectations)
+    calledFrom = caller
     expectations.each do |manifest, pattern|
       it "fails to evaluate #{manifest} with message #{pattern}" do
+        begin
         expect do
           PuppetSpec::Compiler.compile_to_catalog(manifest)
         end.to raise_error(Puppet::Error, pattern)
+        rescue  RSpec::Expectations::ExpectationNotMetError => e
+          # provide the backgrace from the caller, or it is close to impossible to find some originators
+          e.set_backtrace(calledFrom)
+          raise
+        end
       end
     end
   end
