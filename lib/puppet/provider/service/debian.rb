721cf898e51d90f197548fda3bc7d287eac260ac
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-09-01 11:22:34 +0200

    (PUP-3166) Debian provider handles 101 status
    Check for enabled debian service manually on 101 status. 101 is action
    not allowed, meaning we have to manually check the service status
    similar to when we get a status 105 (unknown).

--- lib/puppet/provider/service/debian.rb
+++ lib/puppet/provider/service/debian.rb
@@ -38,7 +38,8 @@ Puppet::Type.type(:service).provide :debian, :parent => :init do
     # See x-man-page://invoke-rc.d
     if [104, 106].include?($CHILD_STATUS.exitstatus)
       return :true
-    elsif [105].include?($CHILD_STATUS.exitstatus)
+    elsif [101, 105].include?($CHILD_STATUS.exitstatus)
+      # 101 is action not allowed, which means we have to do the check manually.
       # 105 is unknown, which generally means the iniscript does not support query
       # The debian policy states that the initscript should support methods of query
       # For those that do not, peform the checks manually
