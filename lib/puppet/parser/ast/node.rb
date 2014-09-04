970eb207ffc73a02ba6ee37f70cc2b3d8cafb896
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-09-04 03:04:15 +0200

    (PUP-987) Remove support for the @ruby_code block in catalog AST types
    This removes the ability to define the logic of node, class and
    resource AST by giving it a block of ruby code. This was only used by
    the Ruby DSL API.

--- lib/puppet/parser/ast/node.rb
+++ lib/puppet/parser/ast/node.rb
@@ -3,7 +3,7 @@ require 'puppet/parser/ast/top_level_construct'
 class Puppet::Parser::AST::Node < Puppet::Parser::AST::TopLevelConstruct
   attr_accessor :names, :context
 
-  def initialize(names, context = {}, &ruby_code)
+  def initialize(names, context = {})
     raise ArgumentError, "names should be an array" unless names.is_a? Array
     if context[:parent]
       msg = "Deprecation notice: Node inheritance is not supported in Puppet >= 4.0.0. See http://links.puppetlabs.com/puppet-node-inheritance-deprecation"
@@ -12,13 +12,11 @@ class Puppet::Parser::AST::Node < Puppet::Parser::AST::TopLevelConstruct
 
     @names = names
     @context = context
-    @ruby_code = ruby_code
   end
 
   def instantiate(modname)
     @names.collect do |name|
       new_node = Puppet::Resource::Type.new(:node, name, @context.merge(:module_name => modname))
-      new_node.ruby_code = @ruby_code if @ruby_code
       new_node
     end
   end
