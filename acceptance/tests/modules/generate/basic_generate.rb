0ff1ecc81eb49158a1ef1c8e2dc3fe87e71bb552
John Duarte <john.duarte@puppetlabs.com>
2014-09-15 08:59:09 -0700

    (PUP-3014) PMT test basic_generate teardown fix
    This commit moves the clean up step in the PMT
    generate/basic_generate test into a teardown step in order to
    ensure that the files are removed before other tests run.

--- acceptance/tests/modules/generate/basic_generate.rb
+++ acceptance/tests/modules/generate/basic_generate.rb
@@ -6,19 +6,16 @@ module_author = "pmtacceptance"
 module_name   = "nginx"
 module_dependencies = []
 
-orig_installed_modules = get_installed_modules_for_hosts hosts
-teardown do
-  rm_installed_modules_from_hosts orig_installed_modules, (get_installed_modules_for_hosts hosts)
-end
-
 agents.each do |agent|
+
+  teardown do
+    on agent,"rm -fr '#{module_author}-#{module_name}'", :acceptable_exit_codes => (0..254)
+  end
 
   step "Generate #{module_author}-#{module_name} module"
   on agent, puppet("module generate #{module_author}-#{module_name} --skip-interview")
 
-  step "Check for #{module_author}-#{module_name} scaffolding"
+  step "Check for #{module_name} scaffolding"
   on agent,"test -f #{module_author}-#{module_name}/manifests/init.pp"
 
-  step "Clean up"
-  on agent,"rm -fr #{module_author}-#{module_name}"
 end
