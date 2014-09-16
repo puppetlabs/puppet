8da4320c2063f26bfc89168f7148ba92e6c501e5
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-09-16 13:28:03 -0700

    (maint) Allow a two node config for a re-run
    And allow setting of host platform.

--- acceptance/bin/ci-rerun-pe-puppet.sh
+++ acceptance/bin/ci-rerun-pe-puppet.sh
@@ -13,6 +13,11 @@ echo "Must provide tests to run in the environment variable 'tests': got '$tests
 exit 1
 fi
 
+if [ -z "$platform" ]; then
+  echo "'platform' not set: assuming 'el-6-x86_64'"
+  platform="el-6-x86_64"
+fi
+
 if [ -z "$1" ]; then
 echo "Must provide the hostname: got '$1'"
 exit 1
@@ -20,7 +25,7 @@ fi
 
 if [ -z "$2" ]; then
 
-cat > hosts-immediate.cfg << EOHOSTS
+  cat > hosts-immediate.cfg << EOHOSTS
 ---
 HOSTS:
   ${1}:
@@ -29,7 +34,7 @@ HOSTS:
     - master
     - dashboard
     - database
-    platform: el-6-x86_64
+    platform: ${platform} 
 CONFIG:
   nfs_server: none
   consoleport: 443
@@ -41,29 +46,22 @@ EOHOSTS
 
 else
 
-if [ -z "$3" ]; then
-echo "Must provide three hostnames: got '$1' '$2' '$3'"
-exit 1
-fi
+  if [ -z "$3" ]; then
 
-cat > hosts-immediate.cfg << EOHOSTS
+    cat > hosts-immediate.cfg << EOHOSTS
 ---
 HOSTS:
   ${1}:
     roles:
     - agent
+    - dashboard
+    - database
     - master
-    platform: el-6-x86_64
+    platform: ${platform}
   ${2}:
     roles:
     - agent
-    - dashboard
-    platform: el-6-x86_64
-  ${3}:
-    roles:
-    - agent
-    - database
-    platform: el-6-x86_64
+    platform: ${platform}
 CONFIG:
   nfs_server: none
   consoleport: 443
@@ -72,6 +70,37 @@ CONFIG:
   resourcepool: delivery/Quality Assurance/Enterprise/Dynamic
   pooling_api: http://vcloud.delivery.puppetlabs.net/
 EOHOSTS
+
+  else
+
+    cat > hosts-immediate.cfg << EOHOSTS
+---
+HOSTS:
+  ${1}:
+    roles:
+    - agent
+    - master
+    platform: ${platform}
+  ${2}:
+    roles:
+    - agent
+    - dashboard
+    platform: ${platform}
+  ${3}:
+    roles:
+    - agent
+    - database
+    platform: ${platform}
+CONFIG:
+  nfs_server: none
+  consoleport: 443
+  datastore: instance0
+  folder: Delivery/Quality Assurance/Enterprise/Dynamic
+  resourcepool: delivery/Quality Assurance/Enterprise/Dynamic
+  pooling_api: http://vcloud.delivery.puppetlabs.net/
+EOHOSTS
+
+  fi
 
 fi
 
