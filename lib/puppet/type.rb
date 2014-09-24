3a7376b70aa1b127fe4373f42ac621b054c9c011
Trevor Vaughan <tvaughan@onyxpoint.com>
2014-09-24 10:19:26 -0700

    (PUP-3331) Updated documentation
    Attempted to update the documentation to make it clear that you can now
    use all four types of relationships.
    
    Updates #PUP-3331

--- lib/puppet/type.rb
+++ lib/puppet/type.rb
@@ -1930,14 +1930,32 @@ class Type
   ###############################
   # All of the relationship code.
 
-  # Adds a block producing a single name (or list of names) of the given resource type name to autorequire.
-  # Resources in the catalog that have the named type and a title that is included in the result will be linked
-  # to the calling resource as a requirement.
+  # Adds a block producing a single name (or list of names) of the given
+  # resource type name to autorelate.
+  #
+  # The four relationship types require, before, notify, and subscribe are all
+  # supported.
+  #
+  # Be *careful* with notify and subscribe as they may have unintended
+  # consequences.
+  #
+  # Resources in the catalog that have the named type and a title that is
+  # included in the result will be linked to the calling resource as a
+  # requirement.
   #
   # @example Autorequire the files File['foo', 'bar']
   #   autorequire( 'file', {|| ['foo', 'bar'] })
   #
-  # @param name [String] the name of a type of which one or several resources should be autorequired e.g. "file"
+  # @example Autobefore the files File['foo', 'bar']
+  #   autobefore( 'file', {|| ['foo', 'bar'] })
+  #
+  # @example Autosubscribe the files File['foo', 'bar']
+  #   autosubscribe( 'file', {|| ['foo', 'bar'] })
+  #
+  # @example Autonotify the files File['foo', 'bar']
+  #   autonotify( 'file', {|| ['foo', 'bar'] })
+  #
+  # @param name [String] the name of a type of which one or several resources should be autorelated e.g. "file"
   # @yield [ ] a block returning list of names of given type to auto require
   # @yieldreturn [String, Array<String>] one or several resource names for the named type
   # @return [void]
@@ -1976,6 +1994,11 @@ class Type
     }
   end
 
+  # Provides iteration over added auto-requirements (see {autobefore}).
+  # @yieldparam type [String] the name of the type to autoriquire an instance of
+  # @yieldparam block [Proc] a block producing one or several dependencies to auto require (see {autobefore}).
+  # @yieldreturn [void]
+  # @return [void]
   def self.eachautobefore
     @autobefores ||= {}
     @autobefores.each { |type,block|
@@ -1983,6 +2006,11 @@ class Type
     }
   end
 
+  # Provides iteration over added auto-requirements (see {autosubscribe}).
+  # @yieldparam type [String] the name of the type to autoriquire an instance of
+  # @yieldparam block [Proc] a block producing one or several dependencies to auto require (see {autosubscribe}).
+  # @yieldreturn [void]
+  # @return [void]
   def self.eachautosubscribe
     @autosubscribes ||= {}
     @autosubscribes.each { |type,block|
@@ -1990,6 +2018,11 @@ class Type
     }
   end
 
+  # Provides iteration over added auto-requirements (see {autonotify}).
+  # @yieldparam type [String] the name of the type to autoriquire an instance of
+  # @yieldparam block [Proc] a block producing one or several dependencies to auto require (see {autonotify}).
+  # @yieldreturn [void]
+  # @return [void]
   def self.eachautonotify
     @autonotifies ||= {}
     @autonotifies.each { |type,block|
@@ -1997,7 +2030,7 @@ class Type
     }
   end
 
-  # Adds dependencies to the catalog from added autorequirements.
+  # Adds dependencies to the catalog from added autorelations.
   # See {autorequire} for how to add an auto-requirement.
   # @todo needs details - see the param rel_catalog, and type of this param
   # @param rel_catalog [Puppet::Resource::Catalog, nil] the catalog to
