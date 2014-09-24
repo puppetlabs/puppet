bdd0dfdda47032a269b42a53c864fe91856eaedd
Rob Braden <bradejr@puppetlabs.com>
2014-09-24 11:41:51 -0700

    (pup-3338) Make systemd the default service provider for SLES 12
    OpenSuSE >= 12 and SLES 12 use systemd as the default service management framework. This
    updates puppet to use the systemd provider by default on OpenSuSE 12 and 13 and
    SLES 12

--- lib/puppet/provider/service/systemd.rb
+++ lib/puppet/provider/service/systemd.rb
@@ -7,6 +7,7 @@ Puppet::Type.type(:service).provide :systemd, :parent => :base do
 
   defaultfor :osfamily => [:archlinux]
   defaultfor :osfamily => :redhat, :operatingsystemmajrelease => "7"
+  defaultfor :osfamily => :suse, :operatingsystemmajrelease => ["12", "13"]
 
   def self.instances
     i = []
