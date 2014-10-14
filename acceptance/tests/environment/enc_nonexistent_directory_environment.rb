5b5add89d387d945abab2930f841865c5d0396ea
Josh Cooper <josh@puppetlabs.com>
2014-10-14 16:08:28 -0700

    (PUP-3244) Fixup test for windows and future parser
    The test failed on windows nodes because the acceptance test wasn't
    using the `puppet` method from beaker. The test failed on future parser
    due to the bare file mode, in 4.0 only string modes will be allowed.
    
    This commit fixes up those issues, and removes the
    `acceptable_exit_codes => [0]` since that is the default behavior.

--- acceptance/tests/environment/enc_nonexistent_directory_environment.rb
+++ acceptance/tests/environment/enc_nonexistent_directory_environment.rb
@@ -4,9 +4,9 @@ testdir = create_tmpdir_for_user master, 'nonexistent_env'
 apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
 File {
   ensure => directory,
-  owner => #{master.puppet['user']},
-  group => #{master.puppet['group']},
-  mode => 0770,
+  owner  => #{master.puppet['user']},
+  group  => #{master.puppet['group']},
+  mode   => '0770',
 }
 
 file {
@@ -38,7 +38,7 @@ master_opts = {
 
 with_puppet_running_on master, master_opts, testdir do
   agents.each do |agent|
-    on agent, "puppet agent --no-daemonize --onetime --server #{master} --verbose", :acceptable_exit_codes => [0] do
+    on(agent, puppet("agent --no-daemonize --onetime --server #{master} --verbose")) do
       assert_match(/Could not find a directory environment named 'doesnotexist'/, stderr, "Errors when nonexistant environment is specified")
       assert_not_match(/In the production environment/, stdout, "Executed manifest from production environment")
     end
