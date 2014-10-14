0a837255d6d772abb5eba761799f4f8cee917b63
Josh Partlow <joshua.partlow@puppetlabs.com>
2014-10-14 11:00:54 -0700

    (PUP-3244) Add a hashbang line to the acceptance test enc
    Ruby's Kernel.exec has different behavior on different platforms when
    handling a script without a hashbang line. On rhel6, for instance:
    
    [root@a3m9lej0pifviyl ~]# cat /root/test
    echo 'foo'
    [root@a3m9lej0pifviyl ~]# /root/test arg
    foo
    [root@a3m9lej0pifviyl ~]# irb
    irb(main):001:0> exec('/root/test', 'arg')
    Errno::ENOEXEC: Exec format error - /root/test
            from (irb):1:in `exec'
            from (irb):1
            from :0
    
    This seems to depend on how the underlying platforms exec functions
    handle this case. Rewriting the enc as a ruby file with a hashbang line
    sidesteps this issue and allows the test to run. Also dropped the
    environment.conf usage since it seemed superfluous.

--- acceptance/tests/environment/enc_nonexistent_directory_environment.rb
+++ acceptance/tests/environment/enc_nonexistent_directory_environment.rb
@@ -1,28 +1,30 @@
 test_name "Master should produce error if enc specifies a nonexistent environment"
 testdir = create_tmpdir_for_user master, 'nonexistent_env'
 
-apply_manifest_on master, <<-MANIFEST
-file {
-  [  "#{testdir}/environments", "#{testdir}/environments/production" ]:
+apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
+File {
   ensure => directory,
+  owner => #{master.puppet['user']},
+  group => #{master.puppet['group']},
+  mode => 0770,
 }
 
-file { "#{testdir}/environments/production/environment.conf":
+file {
+  "#{testdir}":;
+  "#{testdir}/environments":;
+  "#{testdir}/environments/production":;
+  "#{testdir}/environments/production/manifests":;
+  "#{testdir}/environments/production/manifests/site.pp":
     ensure  => file,
-    content => '
-      manifest=./production.pp
-    ',
-}
+    mode => '0644',
+    content => 'notify { "In the production environment": }';
 
-file { "#{testdir}/environments/production/production.pp":
-  ensure  => file,
-  content => 'notify { "In the production environment": }',
-}
-
-file { "#{testdir}/enc.rb":
-  ensure  => file,
-  mode    => '0775',
-  content => 'echo "environment: doesnotexist"',
+  "#{testdir}/enc.rb":
+    ensure  => file,
+    mode    => '0775',
+    content => '#!#{master['puppetbindir']}/ruby
+      puts "environment: doesnotexist"
+    ';
 }
 MANIFEST
 
@@ -38,6 +40,7 @@ with_puppet_running_on master, master_opts, testdir do
   agents.each do |agent|
     on agent, "puppet agent --no-daemonize --onetime --server #{master} --verbose", :acceptable_exit_codes => [0] do
       assert_match(/Could not find a directory environment named 'doesnotexist'/, stderr, "Errors when nonexistant environment is specified")
+      assert_not_match(/In the production environment/, stdout, "Executed manifest from production environment")
     end
   end
 end
