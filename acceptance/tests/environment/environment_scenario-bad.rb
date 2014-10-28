135f9a98fc0146b982ed6c11ac92ebca616705f9
Eric Thompson <eric.thompson@puppetlabs.com>
2014-10-28 12:33:04 -0700

    (PUP-3472) make acceptance/tests/environment/directory.rb atomic
    Split tests/environment/directory.rb into 6 separate tests.
    This makes the tests easier to read, maintain and easier to track
    via testrail. This might allow better metrics on functional test coverage.

---
+++ acceptance/tests/environment/environment_scenario-bad.rb
@@ -0,0 +1,55 @@
+test_name "Test behavior of directory environments when environmentpath is set to a non-existent directory"
+require 'puppet/acceptance/environment_utils'
+extend Puppet::Acceptance::EnvironmentUtils
+require 'puppet/acceptance/classifier_utils'
+extend Puppet::Acceptance::ClassifierUtils
+
+classify_nodes_as_agent_specified_if_classifer_present
+
+step "setup environments"
+
+stub_forge_on(master)
+
+testdir = create_tmpdir_for_user master, "confdir"
+puppet_conf_backup_dir = create_tmpdir_for_user(master, "puppet-conf-backup-dir")
+
+apply_manifest_on(master, environment_manifest(testdir), :catch_failures => true)
+
+step  "Test"
+master_opts = {
+  'main' => {
+    'environmentpath' => '/doesnotexist',
+    'config_version' => '$confdir/static-version.sh',
+  }
+}
+general = [ master_opts, testdir, puppet_conf_backup_dir, { :directory_environments => true } ]
+env = 'doesnotexist'
+path = master['puppetpath']
+
+results = use_an_environment("testing", "bad environmentpath", master_opts, testdir, puppet_conf_backup_dir, :directory_environments => true)
+
+expectations = {
+  :puppet_config => {
+    :exit_code => 1,
+    :matches => [%r{Could not find a directory environment named '#{env}' anywhere in the path.*#{path}}],
+  },
+  :puppet_module_install => {
+    :exit_code => 1,
+    :matches => [%r{Could not find a directory environment named '#{env}' anywhere in the path.*#{path}}],
+  },
+  :puppet_module_uninstall => {
+    :exit_code => 1,
+    :matches => [%r{Could not find a directory environment named '#{env}' anywhere in the path.*#{path}}],
+  },
+  :puppet_apply => {
+    :exit_code => 1,
+    :matches => [%r{Could not find a directory environment named '#{env}' anywhere in the path.*#{path}}],
+  },
+  :puppet_agent => {
+    :exit_code => 1,
+    :matches => [%r{Warning.*404.*Could not find environment '#{env}'},
+                 %r{Could not retrieve catalog; skipping run}],
+  },
+}
+
+review_results(results,expectations)
---
+++ acceptance/tests/environment/environment_scenario-default.rb
@@ -0,0 +1,60 @@
+test_name "Test behavior of default environment"
+require 'puppet/acceptance/environment_utils'
+extend Puppet::Acceptance::EnvironmentUtils
+require 'puppet/acceptance/classifier_utils'
+extend Puppet::Acceptance::ClassifierUtils
+
+classify_nodes_as_agent_specified_if_classifer_present
+
+step "setup environments"
+
+stub_forge_on(master)
+
+testdir = create_tmpdir_for_user master, "confdir"
+puppet_conf_backup_dir = create_tmpdir_for_user(master, "puppet-conf-backup-dir")
+
+apply_manifest_on(master, environment_manifest(testdir), :catch_failures => true)
+
+step  "Test"
+master_opts = {
+  'main' => {
+    'environmentpath' => '$confdir/environments',
+    'config_version' => '$confdir/static-version.sh',
+  }
+}
+general = [ master_opts, testdir, puppet_conf_backup_dir, { :directory_environments => true } ]
+env = nil
+
+results = use_an_environment(env, "default environment", *general)
+
+expectations = {
+  :puppet_config => {
+    :exit_code => 0,
+    :matches => [%r{manifest.*#{master['puppetpath']}/environments/#{env}/manifests$},
+                 %r{modulepath.*#{master['puppetpath']}/environments/#{env}/modules:.+},
+                 %r{config_version = $}]
+  },
+  :puppet_module_install => {
+    :exit_code => 0,
+    :matches => [%r{Preparing to install into #{master['puppetpath']}/environments/#{env}/modules},
+                 %r{pmtacceptance-nginx}],
+  },
+  :puppet_module_uninstall => {
+    :exit_code => 0,
+    :matches => [%r{Removed.*pmtacceptance-nginx.*from #{master['puppetpath']}/environments/#{env}/modules}],
+  },
+  :puppet_apply => {
+    :exit_code => 0,
+    :matches => [%r{include default environment testing_mod}],
+    :notes => "The production directory environment is empty, but the inclusion of basemodulepath in the directory environment modulepath picks up the default testing_mod class in $confdir/modules"
+  },
+  :puppet_agent => {
+    :exit_code => 0,
+    :matches => [ %r{Applying configuration version '\d+'}],
+    :does_not_match => [%r{include.*testing_mod},
+                        %r{Warning.*404}],
+    :notes => "The master automatically creates an empty production env dir."
+  }
+}
+
+review_results(results,expectations)
