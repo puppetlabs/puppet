935a235aa26011988deb3cc0b43a87d21688de89
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2014-11-23 21:40:38 +0100

    (maint) prepare apply integration spec for environments

--- spec/integration/application/apply_spec.rb
+++ spec/integration/application/apply_spec.rb
@@ -66,16 +66,21 @@ describe "apply" do
     expect(@logs.map(&:to_s)).to include('specific manifest applied')
   end
 
-  context "with a module" do
-    let(:modulepath) { tmpdir('modulepath') }
+  context "with a module in an environment" do
+    let(:envdir) { tmpdir('environments') }
+    let(:modulepath) { File.join(envdir, 'spec', 'modules') }
     let(:execute) { 'include amod' }
     let(:args) { ['-e', execute, '--modulepath', modulepath] }
 
     before(:each) do
-      dir_contained_in(modulepath, {
-        "amod" => {
-          "manifests" => {
-            "init.pp" => "class amod{ notice('amod class included') }"
+      dir_contained_in(envdir, {
+        "spec" => {
+          "modules" => {
+            "amod" => {
+              "manifests" => {
+                "init.pp" => "class amod{ notice('amod class included') }"
+              }
+            }
           }
         }
       })
