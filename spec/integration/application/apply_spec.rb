a9da62a52ffce1e894a5e00c04ead029ba2ff722
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2014-11-23 21:45:29 +0100

    (PUP-3341) prepare apply spec for non-modulepath related tests of environments

--- spec/integration/application/apply_spec.rb
+++ spec/integration/application/apply_spec.rb
@@ -70,7 +70,6 @@ describe "apply" do
     let(:envdir) { tmpdir('environments') }
     let(:modulepath) { File.join(envdir, 'spec', 'modules') }
     let(:execute) { 'include amod' }
-    let(:args) { ['-e', execute, '--modulepath', modulepath] }
 
     before(:each) do
       dir_contained_in(envdir, {
@@ -88,37 +87,40 @@ describe "apply" do
       Puppet[:environmentpath] = dir_containing("environments", { Puppet[:environment] => {} })
     end
 
-    def init_cli_args_and_apply_app(args, execute)
-      Puppet.initialize_settings(args)
-      puppet = Puppet::Application.find(:apply).new(stub('command_line', :subcommand_name => :apply, :args => args))
-      puppet.options[:code] = execute
-      return puppet
-    end
+    context "given the --modulepath option" do
+      let(:args) { ['-e', execute, '--modulepath', modulepath] }
+      def init_cli_args_and_apply_app(args, execute)
+        Puppet.initialize_settings(args)
+        puppet = Puppet::Application.find(:apply).new(stub('command_line', :subcommand_name => :apply, :args => args))
+        puppet.options[:code] = execute
+        return puppet
+      end
 
-    it "looks in --modulepath even when the default directory environment exists" do
-      apply = init_cli_args_and_apply_app(args, execute)
+      it "looks in --modulepath even when the default directory environment exists" do
+        apply = init_cli_args_and_apply_app(args, execute)
 
-      expect do
-        expect { apply.run }.to exit_with(0)
-      end.to have_printed('amod class included')
-    end
+        expect do
+          expect { apply.run }.to exit_with(0)
+        end.to have_printed('amod class included')
+      end
 
-    it "looks in --modulepath even when given a specific directory --environment" do
-      args << '--environment' << 'production'
-      apply = init_cli_args_and_apply_app(args, execute)
+      it "looks in --modulepath even when given a specific directory --environment" do
+        args << '--environment' << 'production'
+        apply = init_cli_args_and_apply_app(args, execute)
 
-      expect do
-        expect { apply.run }.to exit_with(0)
-      end.to have_printed('amod class included')
-    end
+        expect do
+          expect { apply.run }.to exit_with(0)
+        end.to have_printed('amod class included')
+      end
 
-    it "looks in --modulepath when given multiple paths in --modulepath" do
-      args = ['-e', execute, '--modulepath', [tmpdir('notmodulepath'), modulepath].join(File::PATH_SEPARATOR)]
-      apply = init_cli_args_and_apply_app(args, execute)
+      it "looks in --modulepath when given multiple paths in --modulepath" do
+        args = ['-e', execute, '--modulepath', [tmpdir('notmodulepath'), modulepath].join(File::PATH_SEPARATOR)]
+        apply = init_cli_args_and_apply_app(args, execute)
 
-      expect do
-        expect { apply.run }.to exit_with(0)
-      end.to have_printed('amod class included')
+        expect do
+          expect { apply.run }.to exit_with(0)
+        end.to have_printed('amod class included')
+      end
     end
   end
 
