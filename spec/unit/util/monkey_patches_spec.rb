15a8a8415c58b50d8d8a0906b37d9c6234b5e3eb
Kylo Ginsberg <kylo@puppetlabs.com>
2014-12-27 23:04:58 -0800

    (PUP-3912) Remove spec test for previously removed code
    These spec tests were added in 3c441212, but the (monkey-patched)
    code they tested was removed in 179ee762 when we dropped support
    for ruby < 1.8.7.
    
    Kill it with fire.

--- spec/unit/util/monkey_patches_spec.rb
+++ spec/unit/util/monkey_patches_spec.rb
@@ -11,33 +11,6 @@ describe Symbol do
   end
 end
 
-
-describe "yaml deserialization" do
-  it "should call yaml_initialize when deserializing objects that have that method defined" do
-    class Puppet::TestYamlInitializeClass
-      attr_reader :foo
-
-      def yaml_initialize(tag, var)
-        var.should == {'foo' => 100}
-        instance_variables.should == []
-        @foo = 200
-      end
-    end
-
-    obj = YAML.load("--- !ruby/object:Puppet::TestYamlInitializeClass\n  foo: 100")
-    expect(obj.foo).to eq(200)
-  end
-
-  it "should not call yaml_initialize if not defined" do
-    class Puppet::TestYamlNonInitializeClass
-      attr_reader :foo
-    end
-
-    obj = YAML.load("--- !ruby/object:Puppet::TestYamlNonInitializeClass\n  foo: 100")
-    expect(obj.foo).to eq(100)
-  end
-end
-
 describe IO do
   include PuppetSpec::Files
 
