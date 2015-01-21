f27343b7cdfd0a12d3d39a3e575ba179eff84912
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2015-01-21 18:18:49 +0000

    Revert "(PUP-3548) Allow 4x function to call 3x function"

--- spec/fixtures/unit/pops/loaders/loaders/mix_4x_and_3x_functions/usee/lib/puppet/parser/functions/callee.rb
+++
@@ -1,8 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:callee, :type => :rvalue, :doc => <<-EOS
-    A function using the 3x API
-  EOS
-  ) do |arguments|
-    "usee::callee() got '#{arguments[0]}'"
-  end
-end
--- spec/fixtures/unit/pops/loaders/loaders/mix_4x_and_3x_functions/user/lib/puppet/functions/user/caller.rb
+++
@@ -1,5 +0,0 @@
-Puppet::Functions.create_function(:'user::caller') do
-  def caller()
-    call_function('callee', 'first') + ' - ' + call_function('callee', 'second')
-  end
-end
