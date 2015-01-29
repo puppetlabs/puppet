c461f98a46043fb8dcae30f777bd2d980b3de2e6
Daniel Sung <Daniel.sung@framestore.com>
2015-01-29 19:04:15 +0000

    (PUP-2916) If reposdir is set, replace the default directories instead of appending
    When a custom reposdir is present in the yum.conf file, yum will only
    read files from that directory/directories (plus yum.conf).
    
    Previously, the yumrepo provider was reading all files from all
    directories in the reposdir array including the default directory, even
    though they would have been ignored by yum. This was an issue if the
    same repo existed in both the default directory and the custom
    directory. This change just replaces the default directories instead of
    appending to them if reposdir is set.

--- lib/puppet/provider/yumrepo/inifile.rb
+++ lib/puppet/provider/yumrepo/inifile.rb
@@ -73,7 +73,9 @@ Puppet::Type.type(:yumrepo).provide(:inifile) do
   # @return [Array<String>] All present directories that may contain yum repo configs.
   def self.reposdir(conf='/etc/yum.conf', dirs=['/etc/yum.repos.d', '/etc/yum/repos.d'])
     reposdir = find_conf_value('reposdir', conf)
-    dirs << reposdir if reposdir
+    # Use directories in reposdir if they are set instead of default
+    dirs = reposdir.split(",").map(&:strip) if reposdir
+    
 
     dirs.select! { |dir| Puppet::FileSystem.exist?(dir) }
     if dirs.empty?
