fd0ed10bd79c0b27bd4c8864412d17df6a7a8b73
Michael Smith <michael.smith@puppetlabs.com>
2015-02-02 14:23:17 -0800

    (PUP-3899) Test agent file and dir with checksums
    Add acceptance tests for the agent creating file resources from
    content and source using non-default checksums.
    
    Adds a test for copying a directory recursive as a file resource with a
    non-default checksum specified.
    
    Also confirms that update occurs if files are touched and using ctime or
    mtime checksums.

--- acceptance/tests/resource/file/source_attribute.rb
+++ acceptance/tests/resource/file/source_attribute.rb
@@ -2,15 +2,24 @@ test_name "The source attribute"
 require 'puppet/acceptance/module_utils'
 extend Puppet::Acceptance::ModuleUtils
 
-target_file_on_windows = 'C:/windows/temp/source_attr_test'
-target_file_on_nix     = '/tmp/source_attr_test'
+@target_file_on_windows = 'C:/windows/temp/source_attr_test'
+@target_file_on_nix     = '/tmp/source_attr_test'
+@target_dir_on_windows  = 'C:/windows/temp/source_attr_test_dir'
+@target_dir_on_nix      = '/tmp/source_attr_test_dir'
+
+checksums = [nil, 'md5', 'md5lite', 'sha256', 'sha256lite', 'ctime', 'mtime']
 
 orig_installed_modules = get_installed_modules_for_hosts hosts
 teardown do
   rm_installed_modules_from_hosts orig_installed_modules, (get_installed_modules_for_hosts hosts)
   hosts.each do |host|
-    file_to_rm = host['platform'] =~ /windows/ ? target_file_on_windows : target_file_on_nix
-    on(host, "rm #{file_to_rm}", :acceptable_exit_codes => [0,1])
+    file_to_rm = host['platform'] =~ /windows/ ? @target_file_on_windows : @target_file_on_nix
+    dir_to_rm = host['platform'] =~ /windows/ ? @target_dir_on_windows : @target_dir_on_nix
+
+    checksums.each do |checksum_type|
+      on(host, "rm #{file_to_rm}#{checksum_type}", :acceptable_exit_codes => [0,1])
+      on(host, "rm -r #{file_to_rm}#{checksum_type}", :acceptable_exit_codes => [0,1])
+    end
   end
 end
 
@@ -27,20 +36,43 @@ test_module_manifests_dir = "#{test_module_dir}/manifests"
 test_module_files_dir = "#{test_module_dir}/files"
 mod_manifest_file = "#{test_module_manifests_dir}/init.pp"
 mod_source_file = "#{test_module_files_dir}/source_file"
+mod_source_dir = "#{test_module_files_dir}/source_dir"
+mod_source_dir_file = "#{mod_source_dir}/source_dir_file"
 
 mod_source = ' the content is present'
 
-mod_manifest = <<EOF
-class source_test_module {
-  $target_file = $::kernel ? {
-    \\'windows\\' => \\'#{target_file_on_windows}\\',
-    default   => \\'#{target_file_on_nix}\\'
+def mod_manifest_entry(checksum_type = nil)
+  checksum = if checksum_type then "checksum => #{checksum_type}," else "" end
+  manifest = <<EOF
+  $target_file#{checksum_type} = $::kernel ? {
+    \\'windows\\' => \\'#{@target_file_on_windows}#{checksum_type}\\',
+    default   => \\'#{@target_file_on_nix}#{checksum_type}\\'
   }
 
-  file { $target_file:
+  file { $target_file#{checksum_type}:
     source => \\'puppet:///modules/source_test_module/source_file\\',
+    #{checksum}
     ensure => present
   }
+
+  $target_dir#{checksum_type} = $::kernel ? {
+    \\'windows\\' => \\'#{@target_dir_on_windows}#{checksum_type}\\',
+    default   => \\'#{@target_dir_on_nix}#{checksum_type}\\'
+  }
+
+  file { $target_dir#{checksum_type}:
+    source => \\'puppet:///modules/source_test_module/source_dir\\',
+    #{checksum}
+    ensure => directory,
+    recurse => true
+  }
+EOF
+  manifest
+end
+
+mod_manifest = <<EOF
+class source_test_module {
+#{checksums.collect { |checksum_type| mod_manifest_entry(checksum_type) }.join("\n")}
 }
 EOF
 
@@ -79,7 +111,19 @@ apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
     mode => '0644',
     content => '#{mod_manifest}',
   }
+
   file { '#{mod_source_file}':
+    ensure => file,
+    mode => '0644',
+    content => '#{mod_source}',
+  }
+
+  file { '#{mod_source_dir}':
+    ensure => directory,
+    mode => '0755'
+  }
+
+  file { '#{mod_source_dir_file}':
     ensure => file,
     mode => '0644',
     content => '#{mod_source}',
@@ -100,15 +144,56 @@ master_opts = {
 }
 with_puppet_running_on(master, master_opts, testdir) do
   agents.each do |agent|
-    # accept an exit code of 2 which is returned if thre are changes
+    # accept an exit code of 2 which is returned if there are changes
+    step "create file the first run"
     on(agent, puppet('agent', "--test --server #{master}"), :acceptable_exit_codes => [0,2]) do
-      file_to_check = agent['platform'] =~ /windows/ ? target_file_on_windows : target_file_on_nix
-      on agent, "cat #{file_to_check}" do
-        assert_match(/the content is present/, stdout, "Result file not created")
+      file_to_check = agent['platform'] =~ /windows/ ? @target_file_on_windows : @target_file_on_nix
+      dir_to_check = agent['platform'] =~ /windows/ ? @target_dir_on_windows : @target_dir_on_nix
+
+      checksums.each do |checksum_type|
+        on agent, "cat #{file_to_check}#{checksum_type}" do
+          assert_match(/the content is present/, stdout, "Result file not created")
+        end
+
+        on agent, "cat #{dir_to_check}#{checksum_type}/source_dir_file" do
+          assert_match(/the content is present/, stdout, "Result file not created")
+        end
+      end
+    end
+
+    step "second run should not update file"
+    on(agent, puppet('agent', "--test --server #{master}")) do
+      assert_no_match(/content changed/, stdout, "Shouldn't have overwrote any files")
+    end
+  end
+
+  step "touch files and verify they're updated with ctime/mtime"
+  sleep(1)
+  on master, "touch #{mod_source_file} #{mod_source_dir_file}"
+  agents.each do |agent|
+    on(agent, puppet('agent', "--test --server #{master}"), :acceptable_exit_codes => [0,2]) do
+      file_to_check = agent['platform'] =~ /windows/ ? @target_file_on_windows : @target_file_on_nix
+      dir_to_check = agent['platform'] =~ /windows/ ? @target_dir_on_windows : @target_dir_on_nix
+      ['ctime', 'mtime'].each do |time_type|
+        assert_match(/File\[#{file_to_check}#{time_type}\]\/content: content changed/, stdout, "Should have updated files")
+        assert_match(/File\[#{dir_to_check}#{time_type}\/source_dir_file\]\/content: content changed/, stdout, "Should have updated files")
       end
     end
   end
 end
+
+new_mod_manifest = <<EOF
+class source_test_module {
+#{mod_manifest_entry}
+}
+EOF
+apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
+  file { '#{mod_manifest_file}':
+    ensure => file,
+    mode => '0644',
+    content => '#{new_mod_manifest}',
+  }
+MANIFEST
 
 # TODO: Add tests for puppet:// URIs with multi-master/agent setups.
 step "when using a puppet://$server/ URI with a master/agent setup"
