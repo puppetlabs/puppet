00a11dfd15e515b4d1f71940f63ef1568edc7cc2
Michael Smith <michael.smith@puppetlabs.com>
2015-02-02 11:31:51 -0800

    (PUP-3899) Test agent writing content with checksum
    Add acceptance test for agent writing a file resource from content with
    a checksum specified, and ensure it's not updated on the 2nd write.

--- acceptance/tests/resource/file/content_attribute.rb
+++ acceptance/tests/resource/file/content_attribute.rb
@@ -8,11 +8,27 @@ agents.each do |agent|
 
   step "Content Attribute: using raw content"
 
+  checksums = ['md5', 'md5lite', 'sha256', 'sha256lite']
   manifest = "file { '#{target}': content => 'This is the test file content', ensure => present }"
-  apply_manifest_on agent, manifest
+  manifest += checksums.collect {|checksum_type|
+    "file { '#{target+checksum_type}': content => 'This is the test file content', ensure => present, checksum => #{checksum_type} }"
+  }.join("\n")
+  apply_manifest_on agent, manifest do
+    checksums.each do |checksum_type|
+      assert_no_match(/content changed/, stdout, "#{agent}: shouldn't have overwrote #{target+checksum_type}")
+    end
+  end
 
   on agent, "cat #{target}" do
     assert_match(/This is the test file content/, stdout, "File content not matched on #{agent}")
+  end
+
+  step "Content Attribute: illegal timesteps"
+  ['mtime', 'ctime'].each do |checksum_type|
+    manifest = "file { '#{target+checksum_type}': content => 'This is the test file content', ensure => present, checksum => #{checksum_type} }"
+    apply_manifest_on agent, manifest, :acceptable_exit_codes => [1] do
+      assert_match(/Error: Validation of File\[#{target+checksum_type}\] failed: You cannot specify content when using checksum '#{checksum_type}'/, stderr, "#{agent}: expected failure")
+    end
   end
 
   step "Ensure the test environment is clean"
