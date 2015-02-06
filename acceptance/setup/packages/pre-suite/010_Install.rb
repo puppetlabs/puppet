00f24fde79ec3d2709c16b10e7d7f78ebbd1fcb2
Josh Cooper <josh@puppetlabs.com>
2015-02-06 12:14:48 -0800

    (QENG-1800) Install MSI during package acceptance
    Previously it was not possible to run Windows acceptance against FOSS
    MSI packages. This commit adds a step to both the package and aio
    rake tasks that can install the puppet-agent MSI.
    It depends on https://github.com/puppetlabs/beaker/pull/674
    
    With this commit you can do:
    
        $ bundle exec rake ci:test:packages \
            CONFIG=config/nodes/win2008r2-rubyx64.yaml \
            SHA=<SHA> VERSION=3.7.4-1303
    
        $ bundle exec rake ci:test:aio \
            CONFIG=config/nodes/win2012r2-rubyx64.yaml \
            SHA=<SHA> VERSION=1.0.0-o-ga354e25 \
            MSI_BASE_URL=http://int-resources.corp.puppetlabs.net/aio
    
    Note the VERSION string must match the filename of the MSI which
    includes the output from `git describe`. The host definition file needs
    to specify which `ruby_arch` it wants to install, either x86 or x64.
    
    Given one off builds may wish to be tested that are not products
    of the build pipeline, the MSI url and filename may be overridden
    completely with two environment variables:
    
    * MSI_BASE_URL may optionally override the URL that Puppet can be
    retrieved from, which normally defaults to:
    
    http://builds.puppetlabs.lan/puppet(-agent)/<SHA>/artifacts/windows
    
    * MSI_FILENAME may optionally override the filename that Puppet pulls
    the MSI from, which normally defaults to:
    
    puppet-agent-<VERSION>-<arch>.msi

--- acceptance/setup/packages/pre-suite/010_Install.rb
+++ acceptance/setup/packages/pre-suite/010_Install.rb
@@ -48,5 +48,15 @@ AGENT_PACKAGES = {
 install_packages_on(master, MASTER_PACKAGES)
 install_packages_on(agents, AGENT_PACKAGES)
 
+agents.each do |agent|
+  if agent['platform'] =~ /windows/
+    arch = agent[:ruby_arch] || 'x86'
+    base_url = ENV['MSI_BASE_URL'] || "http://builds.puppetlabs.lan/puppet/#{ENV['SHA']}/artifacts/windows"
+    filename = ENV['MSI_FILENAME'] || "puppet-agent-#{ENV['VERSION']}-#{arch}.msi"
+
+    install_puppet_from_msi(agent, :url => "#{base_url}/#{filename}")
+  end
+end
+
 configure_gem_mirror(hosts)
 
