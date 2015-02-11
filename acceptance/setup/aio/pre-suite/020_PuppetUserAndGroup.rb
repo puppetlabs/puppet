puppetlabs/puppet

commit e6805564997e64f600b0dc18bdea42267d7e2867

(maint) Create the puppet user and group
Our acceptance tests assume the puppet user and group exist. This commit
adds a setup step for aio to ensure it exists on agent-only hosts. It is
not necessary to create the user/group on the master, because the
puppetserver package does that.

See also PUP-3997

acceptance/setup/aio/pre-suite/020_PuppetUserAndGroup.rb
@@ -1,0 +1,16 @@
+# # The AIO puppet-agent package does not create the puppet user or group, but
+# # puppet-server does. However, some puppet acceptance tests assume the user
+# # is present. This is a temporary setup step to create the puppet user and
+# # group, but only on nodes that are agents and not the master
+# test_name 'Puppet User and Group on agents only' do
+#   agents.each do |agent|
+#     if agent == master
+#       step "Skipping creating puppet user and group on #{agent}"
+#     else
+#       step "Ensure puppet user and group added to #{agent}" do
+#         on agent, puppet("resource user puppet ensure=present")
+#         on agent, puppet("resource group puppet ensure=present")
+#       end
+#     end
+#   end
+# end

commit d2e0e0cb1677cc95d07a4b4a246cd63f14f9a485

(maint) Actually create the puppet user and group
Commit e68055649 accidentally contained a commented out version of the
setup step that I had tested earlier.

acceptance/setup/aio/pre-suite/020_PuppetUserAndGroup.rb
@@ -1,16 +1,16 @@
-# # The AIO puppet-agent package does not create the puppet user or group, but
-# # puppet-server does. However, some puppet acceptance tests assume the user
-# # is present. This is a temporary setup step to create the puppet user and
-# # group, but only on nodes that are agents and not the master
-# test_name 'Puppet User and Group on agents only' do
-#   agents.each do |agent|
-#     if agent == master
-#       step "Skipping creating puppet user and group on #{agent}"
-#     else
-#       step "Ensure puppet user and group added to #{agent}" do
-#         on agent, puppet("resource user puppet ensure=present")
-#         on agent, puppet("resource group puppet ensure=present")
-#       end
-#     end
-#   end
-# end
+# The AIO puppet-agent package does not create the puppet user or group, but
+# puppet-server does. However, some puppet acceptance tests assume the user
+# is present. This is a temporary setup step to create the puppet user and
+# group, but only on nodes that are agents and not the master
+test_name 'Puppet User and Group on agents only' do
+  agents.each do |agent|
+    if agent == master
+      step "Skipping creating puppet user and group on #{agent}"
+    else
+      step "Ensure puppet user and group added to #{agent}" do
+        on agent, puppet("resource user puppet ensure=present")
+        on agent, puppet("resource group puppet ensure=present")
+      end
+    end
+  end
+end