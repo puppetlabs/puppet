02e71c886a35195d440cf835045780d1c1d75615
Josh Cooper <josh@puppetlabs.com>
2015-02-10 23:13:40 -0800

    (maint) Add setup step to AIO options
    Previously aio acceptance wasn't executing the setup step for creating
    the puppet user and group, because it wasn't in the options.rb that
    defines which steps to execute. With this commit, the setup step is
    actually executed:
    
        Begin setup/aio/pre-suite/020_PuppetUserAndGroup.rb
        Puppet User and Group on agents only
        * Ensure puppet user and group added to d7dlw6rntxugxr5.delivery.puppetlabs.net
        d7dlw6rntxugxr5.delivery.puppetlabs.net (agent) 21:18:29$ puppet resource user puppet ensure=present

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -4,6 +4,7 @@
     'setup/common/pre-suite/001_PkgBuildSetup.rb',
     'setup/aio/pre-suite/010_Install.rb',
     'setup/aio/pre-suite/015_PackageHostsPresets.rb',
+    'setup/aio/pre-suite/020_PuppetUserAndGroup.rb',
     'setup/common/pre-suite/025_StopFirewall.rb',
     'setup/common/pre-suite/040_ValidateSignCert.rb',
     'setup/aio/pre-suite/045_EnsureMasterStartedOnPassenger.rb',
