c2211c86d8a587aef38d5693789e27f55a4c2357
Josh Cooper <josh@puppetlabs.com>
2015-02-11 22:09:19 -0800

    (PUP-3809) Ensure puppetserver subdirectories are writable by puppet
    Previously, if puppet apply executed before the puppetserver, then
    puppet running as root could create directories under $vardir owned by
    root, and not writable by the puppet group. For example:
    
        drwxr-x---.  2 root   puppet    6 Feb 11 12:07 bucket
        drwxr-x---.  6 root   puppet 4096 Feb 11 12:09 reports
        drwxr-x---.  2 root   puppet    6 Feb 11 12:07 server_data
        drwxr-x---.  4 puppet puppet   29 Feb 11 12:25 yaml
    
    Later when the puppetserver tried to save a report or a filebucket file,
    acceptance tests would fail, because puppetserver, running as
    puppet, did not have permission to the various subdirectories. In
    particular, the following acceptance tests would fail:
    
        tests/ssl/autosign_command.rb
        tests/resource/file/source_attribute.rb
    
    This commit adds a temporary workaround to the aio setup step to ensure
    the above puppetserver subdirectories have permissions `puppet:puppet`
    and mode 0750.
    
    The issue of puppetserver not being able to write to its directories is
    being tracked as SERVER-357. This commit should be reverted once that
    issue is resolved, and is tracked in PUP-4004.

--- acceptance/setup/aio/pre-suite/020_AIO_Workarounds.rb
+++ acceptance/setup/aio/pre-suite/020_AIO_Workarounds.rb
@@ -4,6 +4,13 @@ step "(PUP-4001) Work around packaging issue"
 on master, "mkdir -p #{master['distmoduledir']}"
 on master, "mkdir -p #{master['sitemoduledir']}"
 
+step "(PUP-4004) Set permissions on puppetserver directories that currently live in the agent cache dir"
+%w[reports server_data yaml bucket].each do |dir|
+  on master, "install --directory /opt/puppetlabs/agent/cache/#{dir}"
+end
+on master, "chown -R puppet:puppet /opt/puppetlabs/agent/cache"
+on master, "chmod -R 750 /opt/puppetlabs/agent/cache"
+
 # The AIO puppet-agent package does not create the puppet user or group, but
 # puppet-server does. However, some puppet acceptance tests assume the user
 # is present. This is a temporary setup step to create the puppet user and
