9d0ece6718a5389de61f8303e7a9bc2c17328f39
Kylo Ginsberg <kylo@puppetlabs.com>
2015-02-24 11:30:17 -0800

    (PUP-3986) Add spec test coverage for new OIDs

--- spec/unit/ssl/oids_spec.rb
+++ spec/unit/ssl/oids_spec.rb
@@ -5,14 +5,27 @@ describe Puppet::SSL::Oids do
   describe "defining application OIDs" do
 
     {
-      'puppetlabs' => '1.3.6.1.4.1.34380',
-      'ppCertExt' => '1.3.6.1.4.1.34380.1',
-      'ppRegCertExt' => '1.3.6.1.4.1.34380.1.1',
-      'pp_uuid' => '1.3.6.1.4.1.34380.1.1.1',
-      'pp_instance_id' => '1.3.6.1.4.1.34380.1.1.2',
-      'pp_image_name' => '1.3.6.1.4.1.34380.1.1.3',
-      'pp_preshared_key' => '1.3.6.1.4.1.34380.1.1.4',
-      'ppPrivCertExt' => '1.3.6.1.4.1.34380.1.2',
+      'puppetlabs'          => '1.3.6.1.4.1.34380',
+      'ppCertExt'           => '1.3.6.1.4.1.34380.1',
+      'ppRegCertExt'        => '1.3.6.1.4.1.34380.1.1',
+      'pp_uuid'             => '1.3.6.1.4.1.34380.1.1.1',
+      'pp_instance_id'      => '1.3.6.1.4.1.34380.1.1.2',
+      'pp_image_name'       => '1.3.6.1.4.1.34380.1.1.3',
+      'pp_preshared_key'    => '1.3.6.1.4.1.34380.1.1.4',
+      'pp_cost_center'      => "1.3.6.1.4.1.34380.1.1.5",
+      'pp_product'          => "1.3.6.1.4.1.34380.1.1.6",
+      'pp_project'          => "1.3.6.1.4.1.34380.1.1.7",
+      'pp_application'      => "1.3.6.1.4.1.34380.1.1.8",
+      'pp_service'          => "1.3.6.1.4.1.34380.1.1.9",
+      'pp_employee'         => "1.3.6.1.4.1.34380.1.1.10",
+      'pp_created_by'       => "1.3.6.1.4.1.34380.1.1.11",
+      'pp_environment'      => "1.3.6.1.4.1.34380.1.1.12",
+      'pp_role'             => "1.3.6.1.4.1.34380.1.1.13",
+      'pp_software_version' => "1.3.6.1.4.1.34380.1.1.14",
+      'pp_department'       => "1.3.6.1.4.1.34380.1.1.15",
+      'pp_cluster'          => "1.3.6.1.4.1.34380.1.1.16",
+      'pp_provisioner'      => "1.3.6.1.4.1.34380.1.1.17",
+      'ppPrivCertExt'       => '1.3.6.1.4.1.34380.1.2',
     }.each_pair do |sn, oid|
       it "defines #{sn} as #{oid}" do
         object_id = OpenSSL::ASN1::ObjectId.new(sn)
