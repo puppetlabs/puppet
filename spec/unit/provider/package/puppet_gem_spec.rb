00989d46446d8029f47a85f5449dfaa42e89cdc4
Kylo Ginsberg <kylo@puppetlabs.com>
2015-03-17 09:40:00 -0700

    (PUP-4237) Add uninstall_options support to puppet_gem

--- spec/unit/provider/package/puppet_gem_spec.rb
+++ spec/unit/provider/package/puppet_gem_spec.rb
@@ -41,4 +41,23 @@ describe provider_class do
       provider.install
     end
   end
+
+  describe "when uninstalling" do
+    it "should use the path to the gem" do
+      provider_class.expects(:which).with(puppet_gem).returns(puppet_gem)
+      provider.expects(:execute).with { |args| args[0] == puppet_gem }.returns ''
+      provider.install
+    end
+
+    it "should not append uninstall_options by default" do
+      provider.expects(:execute).with { |args| args.length == 5 }.returns ''
+      provider.uninstall
+    end
+
+    it "should allow setting an uninstall_options parameter" do
+      resource[:uninstall_options] = [ '--force', {'--bindir' => '/usr/bin' } ]
+      provider.expects(:execute).with { |args| args[5] == '--force' && args[6] == '--bindir=/usr/bin' }.returns ''
+      provider.uninstall
+    end
+  end
 end
