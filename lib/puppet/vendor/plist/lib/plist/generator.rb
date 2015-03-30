a4399c039d48cd884ef180985293e2e3887da739
Michael Smith <michael.smith@puppetlabs.com>
2015-03-30 11:48:01 -0700

    (maint) Re-apply Ruby 1.9 fix from Facter
    The Facter branch applied a fix for Ruby 1.9 to the plist library.
    Re-apply it here against plist 3.0.0.

--- lib/puppet/vendor/plist/lib/plist/generator.rb
+++ lib/puppet/vendor/plist/lib/plist/generator.rb
@@ -155,9 +155,9 @@ module Plist
 
     def self.element_type(item)
       return case item
-        when String, Symbol:          'string'
-        when Fixnum, Bignum, Integer: 'integer'
-        when Float:                   'real'
+        when String, Symbol;          'string'
+        when Fixnum, Bignum, Integer; 'integer'
+        when Float;                   'real'
         else
           raise "Don't know about this data type... something must be wrong!"
       end
