11f810898d7fa65906281848e9833a2cf795ba2d
Branan Riley <branan@puppetlabs.com>
2015-03-31 13:31:09 -0700

    (PUP-4237) Support windows in the puppet_gem provider
    On windows Puppet's ruby is at the start of the path, so we don't need
    to try to figure it out and hard-code it. We simply use the standard
    command lookup.

--- lib/puppet/provider/package/puppet_gem.rb
+++ lib/puppet/provider/package/puppet_gem.rb
@@ -6,5 +6,12 @@ Puppet::Type.type(:package).provide :puppet_gem, :parent => :gem do
 
   has_feature :versionable, :install_options, :uninstall_options
 
-  commands :gemcmd => "/opt/puppetlabs/puppet/bin/gem"
+  if Puppet.features.microsoft_windows?
+    # On windows, we put our ruby ahead of anything that already
+    # existed on the system PATH. This means that we do not need to
+    # sort out the absolute path.
+    commands :gemcmd => "gem"
+  else
+    commands :gemcmd => "/opt/puppetlabs/puppet/bin/gem"
+  end
 end
