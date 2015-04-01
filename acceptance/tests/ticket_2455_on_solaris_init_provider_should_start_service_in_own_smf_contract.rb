3f68b7e796511fda273f61895a83d4481343ef44
Geoff Nichols <geoff.nichols@puppetlabs.com>
2015-04-01 10:39:12 -0700

    Replace vmware-tools with simple ruby daemon as acceptance fixture.

--- acceptance/tests/ticket_2455_on_solaris_init_provider_should_start_service_in_own_smf_contract.rb
+++ acceptance/tests/ticket_2455_on_solaris_init_provider_should_start_service_in_own_smf_contract.rb
@@ -1,5 +1,45 @@
 test_name "(PUP-2455) Service provider should start Solaris init service in its own SMF contract"
 
+sleepy_daemon_script = <<SCRIPT
+#!/usr/bin/env ruby
+while true
+  sleep (2)
+end
+SCRIPT
+
+sleepy_daemon_initscript = <<INITSCRIPT
+#!/bin/sh
+FIXTURESERVICE="/tmp/sleepy_daemon"
+start(){
+    $FIXTURESERVICE &
+}
+
+stop(){
+    FIXTUREPID=`ps -ef | grep "$FIXTURESERVICE" | grep -v grep | awk '{print $2}'`
+    kill -9 $FIXTUREPID
+}
+
+status(){
+    FIXTUREPID=`ps -ef | grep "$FIXTURESERVICE" | grep -v grep | awk '{print $2}'`
+    if [ "x$FIXTUREPID" = "x" ]; then
+      exit 1
+    else
+      exit 0
+    fi
+}
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    status)
+        status
+        ;;
+esac
+INITSCRIPT
+
 step "Setup fixture service manifest on master"
 
   testdir = master.tmpdir('solaris_services_in_own_smf_contract')
@@ -19,11 +59,11 @@ file { '#{testdir}/environments/production/manifests/site.pp':
   ensure  => file,
   content => '
     node default {
-      service { "vmware-tools":
+      service { "sleepy_daemon":
       provider => "init",
       enable   => "true",
       ensure   => "running",
-      status   => "/etc/init.d/vmware-tools status",
+      status   => "/etc/init.d/sleepy_daemon status",
       }
     }
   ',
@@ -52,26 +92,32 @@ step "Start master"
 
     agents.each do |agent|
 
-    fixture_process = 'vmtoolsd'
-    fixture_resource = 'vmware-tools'
-    fixture_service_stop = '/etc/init.d/vmware-tools stop'
+    fixture_service = 'sleepy_daemon'
+    fixture_service_stop = '/etc/init.d/sleepy_daemon stop'
 
       arch = on(agent, facter('architecture')).stdout.chomp
       plat = on(agent, facter('osfamily')).stdout.chomp
 
-      skip_test unless arch == "i86pc" and plat == "Solaris" 
+      skip_test unless plat == "Solaris"
+
+
+      step "Setup fixture service on #{agent}"
+        sleepy_daemon_path = "/tmp/sleepy_daemon"
+        sleepy_daemon_initscript_path = "/etc/init.d/sleepy_daemon"
+        create_remote_file(agent, sleepy_daemon_path, sleepy_daemon_script)
+        create_remote_file(agent, sleepy_daemon_initscript_path, sleepy_daemon_initscript)
+        on(agent, "chmod +x #{sleepy_daemon_path} #{sleepy_daemon_initscript_path}")
 
       step "Start the fixture service on #{agent} "
-        on(agent, "puppet resource service #{fixture_resource} provider=init ensure=stopped")
-        on(agent, "puppet resource service #{fixture_resource} provider=init ensure=running")
+        on(agent, "puppet resource service #{fixture_service} provider=init ensure=stopped")
+        on(agent, "puppet resource service #{fixture_service} provider=init ensure=running")
 
-        assert_match(/ensure changed 'stopped' to 'running'/, stdout, "The fixture service #{fixture_process} is not in a testable state on #{agent}.")
+        assert_match(/ensure changed 'stopped' to 'running'/, stdout, "The fixture service #{fixture_service} is not in a testable state on #{agent}.")
 
       step "Verify whether the fixture process is alone in its SMF contract on #{agent}"
-        service_ctid = on(agent, "sleep 20;ps -eo ctid,args | grep #{fixture_process} | grep -v grep | awk '{print $1}'").stdout.chomp.to_i
+        service_ctid = on(agent, "sleep 20;ps -eo ctid,args | grep #{fixture_service} | grep -v grep | awk '{print $1}'").stdout.chomp.to_i
         number_in_contract = on(agent, "pgrep -c #{service_ctid} | wc -l").stdout.chomp.to_i
-
-        assert(number_in_contract == 1, "The fixture process #{fixture_process} is not alone in its SMF contract on #{agent}.")
+        assert(number_in_contract == 1, "The fixture process #{fixture_service} is not alone in its SMF contract on #{agent}.")
 
       if agent.is_pe? 
 
@@ -84,9 +130,9 @@ step "Start master"
         step "Enable puppet service on #{agent}"
           on(agent, "svcadm enable #{agent_service};sleep 10") do
             puppet_ctid = on(agent, "svcs -Ho CTID #{agent_service} | awk '{print $1}'").stdout.chomp.to_i
-            service_ctid = on(agent, "ps -eo ctid,args | grep #{fixture_process} | grep -v grep | awk '{print $1}'").stdout.chomp.to_i
+            service_ctid = on(agent, "ps -eo ctid,args | grep #{fixture_service} | grep -v grep | awk '{print $1}'").stdout.chomp.to_i
 
-          step "Compare SMF contract ids for puppet and #{fixture_process} on #{agent}"
+          step "Compare SMF contract ids for puppet and #{fixture_service} on #{agent}"
             unless ( puppet_ctid != "0" and service_ctid != "0" ) then
               fail_test("SMF contract ids should not equal zero.")
             end
