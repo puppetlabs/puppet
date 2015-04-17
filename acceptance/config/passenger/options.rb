0edf126db23cfeb174643f13406c454fbeaf1236
Michael Smith <michael.smith@puppetlabs.com>
2015-04-17 12:10:08 -0700

    (PUP-4234) Modify AIO pre-suite to use passenger
    When configured on the master, install puppet-master-passenger instead of
    puppetserver, for use with the new configs from PUP-4233.

---
+++ acceptance/config/passenger/options.rb
@@ -0,0 +1,12 @@
+{
+  :type => 'aio',
+  :passenger => true,
+  :pre_suite => [
+    'setup/aio/pre-suite/010_Install.rb',
+    'setup/aio/pre-suite/015_PackageHostsPresets.rb',
+    'setup/common/pre-suite/025_StopFirewall.rb',
+    'setup/common/pre-suite/040_ValidateSignCert.rb',
+    'setup/aio/pre-suite/045_EnsureMasterStartedOnPassenger.rb',
+    'setup/common/pre-suite/070_InstallCACerts.rb',
+  ],
+}
