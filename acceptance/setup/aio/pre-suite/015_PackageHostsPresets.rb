db5c3daaf6daac5a35a14ebde7b63b145f8154f6
Josh Cooper <josh@puppetlabs.com>
2015-04-22 21:59:53 -0700

    (PUP-4234) Specify apache service names
    Previously, beaker could not start/stop the apache service on redhat 7,
    because beaker defaults the service name to `apache2`, but the service
    is named `httpd` on redhat.
    
    This commit specifies the service names for redhat and debian. While
    it's not necessary for debian, I prefer to be explicit.

--- acceptance/setup/aio/pre-suite/015_PackageHostsPresets.rb
+++ acceptance/setup/aio/pre-suite/015_PackageHostsPresets.rb
@@ -1,5 +1,9 @@
 if master['passenger']
-  master.uses_passenger!
+  if master['platform'] =~ /^el-|^fedora-/
+    master.uses_passenger!('httpd')
+  else
+    master.uses_passenger!('apache2')
+  end
 else
   master['use-service'] = true
 end
