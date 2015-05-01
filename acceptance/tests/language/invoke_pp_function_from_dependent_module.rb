7bf79a1690b00256c2df8a97580ea174c34d67d7
Kurt Wall <kurt.wall@puppetlabs.com>
2015-04-30 17:13:44 -0700

    (PUP-4466) Add acceptance tests that functions can be written/executed in the puppet language

---
+++ acceptance/tests/language/invoke_pp_function_from_dependent_module.rb
@@ -0,0 +1,67 @@
+test_name 'Puppet-language functions can be called from inside an another module' do
+
+  mini_func = 'function jenny::mini($a, $b) {if $a <= $b {$a} else {$b}}'
+  maxi_func = 'function jenny::nested::maxi($a, $b) {if $a >= $b {$a} else {$b}}'
+  
+  step 'Create PP functions on master' do
+    manifest = <<-EOM.gsub(/^ {6}/,'')
+      File {
+        ensure => present,
+        owner => 'puppet',
+        group => 'puppet',
+        mode => '0644',
+      }
+      file {['/etc/puppetlabs',
+          '/etc/puppetlabs/code',
+          '/etc/puppetlabs/code/modules',
+          '/etc/puppetlabs/code/modules/jenny',
+          '/etc/puppetlabs/code/modules/jenny/functions',
+          '/etc/puppetlabs/code/modules/jenny/functions/nested',
+          '/etc/puppetlabs/code/modules/tutone',
+          '/etc/puppetlabs/code/modules/tutone/manifests']:
+        ensure => directory,
+        owner => 'puppet',
+        group => 'puppet',
+        mode => '0755',
+      }  
+      file {'/etc/puppetlabs/code/modules/jenny/functions/mini.pp':
+        content => '#{mini_func}',
+      }
+      file {'/etc/puppetlabs/code/modules/jenny/functions/nested/maxi.pp':
+        content => '#{maxi_func}',
+      }
+      file {'/etc/puppetlabs/code/modules/tutone/manifests/testmini.pp':
+        content => 'notify {"mini": message => jenny::mini(1,2)}'
+      }
+      file {'/etc/puppetlabs/code/modules/tutone/manifests/testmaxi.pp':
+        content => 'notify {"maxi": message => jenny::nested::maxi(1,2)}'
+      }
+    EOM
+    apply_manifest_on(master, manifest)
+  end
+  
+  step 'Call a simple PP function from another module' do
+    rc = on master, puppet('apply /etc/puppetlabs/code/modules/tutone/manifests/testmini.pp')
+    fail_test "Dependent simple PP function failed; returned #{rc.stdout}" \
+      unless rc.stdout.include? "defined 'message' as '1'"
+  end
+
+  step 'Call a nested PP function from another module' do
+    rc = on master, puppet('apply /etc/puppetlabs/code/modules/tutone/manifests/testmaxi.pp')
+    fail_test "Dependent nested PP function failed; returned #{rc.stdout}" \
+      unless rc.stdout.include? "defined 'message' as '2'"
+  end
+
+    step 'Call a simple PP function from outside the scope of any module' do
+    rc = apply_manifest_on(master, 'notify {"mini": message => jenny::mini(1,2)}')
+    fail_test "Simple PP function failed; returned #{rc.stdout}" \
+      unless rc.stdout.include? "defined 'message' as '1'"
+  end
+  
+  step 'Call a nested PP function from outside the scope of any module' do
+    rc = apply_manifest_on(master, 'notify {"maxi": message => jenny::nested::maxi(1,2)}')
+    fail_test "Nested PP function failed: returned #{rc.stdout}" \
+      unless rc.stdout.include? "defined 'message' as '2'"
+  end
+
+end
