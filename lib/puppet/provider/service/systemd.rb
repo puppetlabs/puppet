7c594d8e8f4e113853392e6ab2a69345c199dcd3
Gael Chamoulaud <gchamoul@redhat.com>
2015-06-01 13:06:51 +0200

    (PUP-4663) Ensure Fedora uses the systemd service provider
    - All supported releases of Fedora have systemd and the latest release
      with upstart has been EOLed three years ago. So we could drop the
      version restriction now.
    
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- lib/puppet/provider/service/systemd.rb
+++ lib/puppet/provider/service/systemd.rb
@@ -9,7 +9,7 @@ Puppet::Type.type(:service).provide :systemd, :parent => :base do
 
   defaultfor :osfamily => [:archlinux]
   defaultfor :osfamily => :redhat, :operatingsystemmajrelease => "7"
-  defaultfor :osfamily => :redhat, :operatingsystem => :fedora, :operatingsystemmajrelease => ["17", "18", "19", "20", "21"]
+  defaultfor :osfamily => :redhat, :operatingsystem => :fedora
   defaultfor :osfamily => :suse, :operatingsystemmajrelease => ["12", "13"]
   defaultfor :operatingsystem => :debian, :operatingsystemmajrelease => "8"
   defaultfor :operatingsystem => :ubuntu, :operatingsystemmajrelease => "15.04"
