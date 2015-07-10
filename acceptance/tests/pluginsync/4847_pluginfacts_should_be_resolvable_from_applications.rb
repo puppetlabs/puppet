e909e6303e77e0412f33b027f92714ea2ecd9897
Michael Smith <michael.smith@puppetlabs.com>
2015-07-10 10:55:46 -0700

    (PUP-4847) Add factpath testing to acceptance
    Adds testing that factpath is still honored with the fix for PUP-4847.
    
    [skip ci]

--- acceptance/tests/pluginsync/4847_pluginfacts_should_be_resolvable_from_applications.rb
+++ acceptance/tests/pluginsync/4847_pluginfacts_should_be_resolvable_from_applications.rb
@@ -9,9 +9,10 @@ step "Create a codedir with a test module with external fact"
 codedir = master.tmpdir('4847-codedir')
 
 agents.each do |agent|
+  on agent, "mkdir -p #{codedir}/facts"
   on agent, "mkdir -p #{codedir}/lib/facter"
-
   on agent, "mkdir -p #{codedir}/lib/puppet/{type,provider/test4847}"
+
   on agent, "cat > #{codedir}/lib/puppet/type/test4847.rb", :stdin => <<TYPE
 Puppet::Type.newtype(:test4847) do
   newparam(:name, :namevar => true)
@@ -22,15 +23,23 @@ TYPE
 Puppet::Type.type(:test4847).provide(:only) do
   commands :anything => "#{codedir}/must_exist.exe"
   def self.instances
-    warn "fact foo=\#{Facter.value('foo')}"
+    warn "fact foo=\#{Facter.value('foo')}, snafu=\#{Facter.value('snafu')}"
   end
 end
 PROVIDER
 
-  fact = <<FACT
+  foo_fact = <<FACT
 Facter.add('foo') do
   setcode do
     'bar'
+  end
+end
+FACT
+
+  snafu_fact = <<FACT
+Facter.add('snafu') do
+  setcode do
+    'zifnab'
   end
 end
 FACT
@@ -44,12 +53,19 @@ FACT
 
   file { "#{codedir}/lib/facter/foo.rb":
     ensure  => file,
-    content => "#{fact}",
+    content => "#{foo_fact}",
+  }
+
+  file { "#{codedir}/facts/snafu.rb":
+    ensure  => file,
+    content => "#{snafu_fact}"
   }
 MANIFEST
 
-  on agent, puppet('resource', 'test4847', '--libdir', "#{codedir}/lib"), :acceptable_exit_codes => [1] do
-    assert_match(/fact foo=bar/, stderr)
+  on agent, puppet('resource', 'test4847',
+                   '--libdir', File.join(codedir, 'lib'),
+                   '--factpath', File.join(codedir, 'facts')), :acceptable_exit_codes => [1] do
+    assert_match(/fact foo=bar, snafu=zifnab/, stderr)
   end
 
   teardown do
