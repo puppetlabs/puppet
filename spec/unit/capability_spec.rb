f4564547746b15b6ce8e32bf6cda7a3b25f65c40
David Lutterkort <lutter@watzmann.net>
2015-08-26 14:53:12 -0700

    (PUP-5045) No functional change - group existing tests in a "describe"
    This commit does not change the tests at all, it simply encloses existing
    tests in a 'describe' block so we can later add more tests that test other
    aspects of capability types and keep the source organized/readable.

--- spec/unit/capability_spec.rb
+++ spec/unit/capability_spec.rb
@@ -19,8 +19,9 @@ describe "Capability types" do
     with_app_management(false)
   end
 
-  it "adds a blueprint for a produced resource" do
-    catalog = compile_to_catalog(<<-MANIFEST)
+  describe "annotations" do
+    it "adds a blueprint for a produced resource" do
+      catalog = compile_to_catalog(<<-MANIFEST)
       define test($hostname) {
         notify { "hostname ${hostname}":}
       }
@@ -30,19 +31,19 @@ describe "Capability types" do
       }
     MANIFEST
 
-    krt = catalog.environment_instance.known_resource_types
-    type = krt.definition(:test)
-    expect(type.produces).to be_instance_of(Array)
-    prd = type.produces.first
+      krt = catalog.environment_instance.known_resource_types
+      type = krt.definition(:test)
+      expect(type.produces).to be_instance_of(Array)
+      prd = type.produces.first
 
-    expect(prd).to be_instance_of(Hash)
-    expect(prd[:capability]).to eq("Cap")
-    expect(prd[:mappings]).to be_instance_of(Hash)
-    expect(prd[:mappings]["host"]).to be_instance_of(Puppet::Parser::AST::PopsBridge::Expression)
-  end
+      expect(prd).to be_instance_of(Hash)
+      expect(prd[:capability]).to eq("Cap")
+      expect(prd[:mappings]).to be_instance_of(Hash)
+      expect(prd[:mappings]["host"]).to be_instance_of(Puppet::Parser::AST::PopsBridge::Expression)
+    end
 
-  it "adds a blueprint for a consumed resource" do
-    catalog = compile_to_catalog(<<-MANIFEST)
+    it "adds a blueprint for a consumed resource" do
+      catalog = compile_to_catalog(<<-MANIFEST)
       define test($hostname) {
         notify { "hostname ${hostname}":}
       }
@@ -52,16 +53,16 @@ describe "Capability types" do
       }
     MANIFEST
 
-    krt = catalog.environment_instance.known_resource_types
-    type = krt.definition(:test)
-    expect(type.produces).to be_instance_of(Array)
-    cns = type.consumes.first
+      krt = catalog.environment_instance.known_resource_types
+      type = krt.definition(:test)
+      expect(type.produces).to be_instance_of(Array)
+      cns = type.consumes.first
 
-    expect(cns).to be_instance_of(Hash)
-    expect(cns[:capability]).to eq("Cap")
-    expect(cns[:mappings]).to be_instance_of(Hash)
-    expect(cns[:mappings]["host"]).to be_instance_of(Puppet::Parser::AST::PopsBridge::Expression)
-  end
+      expect(cns).to be_instance_of(Hash)
+      expect(cns[:capability]).to eq("Cap")
+      expect(cns[:mappings]).to be_instance_of(Hash)
+      expect(cns[:mappings]["host"]).to be_instance_of(Puppet::Parser::AST::PopsBridge::Expression)
+    end
 
   it 'can place define and consumes/produces in separate manifests' do
     parse_results = []
@@ -128,18 +129,19 @@ describe "Capability types" do
     expect(cns[:mappings]['host']).to be_instance_of(Puppet::Parser::AST::PopsBridge::Expression)
   end
 
-  ["produces", "consumes"].each do |kw|
-    it "creates an error when #{kw} references nonexistent type" do
-      manifest = <<-MANIFEST
+    ["produces", "consumes"].each do |kw|
+      it "creates an error when #{kw} references nonexistent type" do
+        manifest = <<-MANIFEST
         Test #{kw} Cap {
           host => $hostname
         }
       MANIFEST
 
-      expect {
-        compile_to_catalog(manifest)
-      }.to raise_error(Puppet::Error,
-                       /#{kw} clause references nonexistent type Test/)
+        expect {
+          compile_to_catalog(manifest)
+        }.to raise_error(Puppet::Error,
+                         /#{kw} clause references nonexistent type Test/)
+      end
     end
   end
 end
