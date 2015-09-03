0382263edbc70311d9fdf8357c597910fda5b8aa
Will Hopper <whopper@puppetlabs.com>
2015-09-03 15:15:05 -0700

    (PUP-5024) Add acceptance test for enabling systemd-masked services
    See PUP-4605 for additional context.

---
+++ acceptance/lib/puppet/acceptance/service_utils.rb
@@ -0,0 +1,17 @@
+require 'puppet/acceptance/common_utils'
+
+module Puppet
+  module Acceptance
+    module ServiceUtils
+
+      # Return whether a host supports the systemd provider.
+      # @param host [String] hostname
+      # @return [Boolean] whether the systemd provider is supported.
+      def supports_systemd? (host)
+        ruby = Puppet::Acceptance::CommandUtils.ruby_command(host)
+        suitable = on(host, "#{ruby} -e \"require 'puppet'; puts Puppet::Type.type(:service).provider(:systemd).suitable?\"" ).stdout.chomp
+        suitable == "true" ? true : false
+      end
+    end
+  end
+end
