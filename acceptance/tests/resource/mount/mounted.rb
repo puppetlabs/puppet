fdd6d90e160c47fa037a87e752a04e86bfa079af
Michael Smith <michael.smith@puppetlabs.com>
2015-09-04 11:11:06 -0700

    (PUP-5191) Disable mount tests on Solaris
    Mount tests need some work on Solaris, planned as PUP-5201.
    
    [skip ci]

--- acceptance/tests/resource/mount/mounted.rb
+++ acceptance/tests/resource/mount/mounted.rb
@@ -2,6 +2,7 @@ test_name "mounted should create an entry in filesystem table and mount it"
 
 confine :except, :platform => ['windows']
 confine :except, :platform => /osx/ # See PUP-4823
+confine :except, :platform => /solaris/
 
 fstab = '/etc/fstab'
 name = "pl#{rand(999999).to_i}"
