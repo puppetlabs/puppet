e0c3168e461ff12a4d976ecb4eae14704226ca00
Michael Smith <michael.smith@puppetlabs.com>
2015-09-08 15:07:40 -0700

    (PUP-5024) Fix test on Windows
    On Windows, ruby.exe doesn't know where to `require 'puppet'`, as Puppet
    is not put in Ruby's vendor or site dir in the MSI. Requiring puppet
    fails in the systemd acceptance test.
    
    Explicitly state Windows does not support systemd.
    
    [skip ci]

--- acceptance/lib/puppet/acceptance/service_utils.rb
+++ acceptance/lib/puppet/acceptance/service_utils.rb
@@ -8,6 +8,8 @@ module Puppet
       # @param host [String] hostname
       # @return [Boolean] whether the systemd provider is supported.
       def supports_systemd? (host)
+        # The Windows MSI doesn't put Puppet in the Ruby vendor or site dir, so loading it fails.
+        return false if host.platform.variant == 'windows'
         ruby = Puppet::Acceptance::CommandUtils.ruby_command(host)
         suitable = on(host, "#{ruby} -e \"require 'puppet'; puts Puppet::Type.type(:service).provider(:systemd).suitable?\"" ).stdout.chomp
         suitable == "true" ? true : false
