puppetlabs/puppet

commit c15a894612263efce9649cb44dcd4c6f534711b0

(PUP-3272) Change Status yaml deserialization to support psych
This changes deserialization of a Status instance to use
a custom initialization (based on params hash). The implementation
is in a PsychSupport module to make it more visible and easier to add to
other classes needed the same support.

Since this change is not compatible with 1.8.7, the report_spec test
that depends on this functionality is restricted to not run on Ruby
1.8.7.

lib/puppet/resource/status.rb
@@ -1,5 +1,6 @@
 require 'time'
 require 'puppet/network/format_support'
+require 'puppet/util/psych_support'
 
 module Puppet
   class Resource
@@ -13,6 +14,7 @@ module Puppet
     class Status
       include Puppet::Util::Tagging
       include Puppet::Network::FormatSupport
+      include Puppet::Util::PsychSupport
 
       # @!attribute [rw] file
       #   @return [String] The file where `@real_resource` was defined.
@@ -88,7 +90,6 @@ module Puppet
                            @changed @resource_type @title @skipped @failed
                            @containment_path}.
         map(&:to_sym)
-
 
       def self.from_data_hash(data)
         obj = self.allocate

spec/unit/transaction/report_spec.rb
@@ -407,7 +407,9 @@ describe Puppet::Transaction::Report do
     expect(error_report.render).to validate_against('api/schemas/report.json')
   end
 
-  it "can make a round trip through yaml" do
+  # Test does not run on Ruby 1.8.7 since the functionality requires Psych
+  # Which was introduced in Ruby 1.9 and we do not want to maintain a fix for Ruby 1.8.7
+  it "can make a round trip through yaml", :unless => RUBY_VERSION >= '1.8.7' do
     Puppet[:report_serialization_format] = "yaml"
     report = generate_report
 

lib/puppet/util/psych_support.rb
@@ -1,0 +1,24 @@
+# This module should be included when a class can be serialized to yaml and
+# needs to handle the deserialization from Psych with more control. Psych normall
+# pokes values directly into an instance using `instance_variable_set` which completely
+# bypasses encapsulation.
+#
+# The class that includes this module must implement an instance method `initialize_from_hash`
+# that is given a hash with attribute to value mappings.
+#
+module Puppet::Util::PsychSupport
+  # This method is called from the Psych Yaml deserializer.
+  # The serializer calls this instead of doing the initialization itself using
+  # `instance_variable_set`. The Status class requires this because it serializes its TagSet
+  # as an `Array` in order to optimize the size of the serialized result.
+  # When this array is later deserialized it must be reincarnated as a TagSet. The standard
+  # Psych way of doing this via poking values into instance variables cannot do this.
+  #
+  def init_with(psych_coder)
+    # The PsychCoder has a hashmap of instance variable name (sans the @ symbol) to values
+    # to set, and can thus directly be fed to initialize_from_hash.
+    #
+    initialize_from_hash(psych_coder.map)
+  end
+
+end

commit be80bb53f3fc566b79115f54985f43121b55dfef

(PUP-5192) Revert psych support for Puppet::Resource::Status
When Calling the function "YAML.load_file(path)" with a yaml file where
a resource_statuses: member has events: non-empty, a "undefined
method `[]' for #<Puppet::Transaction::Event:0x0000000341e3f0>" error
pops up.

This commit fixes that bug by reverting psych support introduced in
c15a894612263efce9649cb44dcd4c6f534711b0.

lib/puppet/resource/status.rb
@@ -1,6 +1,5 @@
 require 'time'
 require 'puppet/network/format_support'
-require 'puppet/util/psych_support'
 
 module Puppet
   class Resource
@@ -14,7 +13,6 @@ module Puppet
     class Status
       include Puppet::Util::Tagging
       include Puppet::Network::FormatSupport
-      include Puppet::Util::PsychSupport
 
       # @!attribute [rw] file
       #   @return [String] The file where `@real_resource` was defined.

spec/unit/transaction/report_spec.rb
@@ -404,6 +404,16 @@ describe Puppet::Transaction::Report do
     expect(error_report.render).to validate_against('api/schemas/report.json')
   end
 
+  it "can make a round trip through yaml" do
+    report = generate_report
+
+    yaml_output = report.render(:yaml)
+    tripped = Puppet::Transaction::Report.convert_from(:yaml, yaml_output)
+
+    expect(yaml_output).to match(/^--- /)
+    expect_equivalent_reports(tripped, report)
+  end
+
   def expect_equivalent_reports(tripped, report)
     expect(tripped.host).to eq(report.host)
     expect(tripped.time.to_i).to eq(report.time.to_i)