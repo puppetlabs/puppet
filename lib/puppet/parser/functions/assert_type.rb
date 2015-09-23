99474033c953f3428d04ccbb75e2bc68eb378fe7
Garrett Guillotte <gguillotte@users.noreply.github.com>
2015-09-23 14:49:18 -0700

    (docs) Revise assert_type docs.

--- lib/puppet/parser/functions/assert_type.rb
+++ lib/puppet/parser/functions/assert_type.rb
@@ -2,29 +2,59 @@ Puppet::Parser::Functions::newfunction(
   :assert_type,
   :type => :rvalue,
   :arity => -3,
-  :doc => "Returns the given value if it is an instance of the given type, and raises an error otherwise.
-Optionally, if a block is given (accepting two parameters), it will be called instead of raising
-an error. This to enable giving the user richer feedback, or to supply a default value.
+  :doc => <<DOC
+Returns the given value if it is of the given
+[data type](https://docs.puppetlabs.com/puppet/latest/reference/lang_data.html), or
+otherwise either raises an error or executes an optional two-parameter
+[lambda](https://docs.puppetlabs.com/puppet/latest/reference/lang_lambdas.html).
 
-Example: assert that `$b` is a non empty `String` and assign to `$a`:
+The function takes two mandatory arguments, in this order:
 
-  $a = assert_type(String[1], $b)
+1. The expected data type.
+2. A value to compare against the expected data type.
 
-Example using custom error message:
+**Example**: Using `assert_type`
 
-  $a = assert_type(String[1], $b) |$expected, $actual| {
-    fail('The name cannot be empty')
-  }
+~~~ puppet
+$raw_username = 'Amy Berry'
 
-Example, using a warning and a default:
+# Assert that $raw_username is a non-empty string and assign it to $valid_username.
+$valid_username = assert_type(String[1], $raw_username)
 
-  $a = assert_type(String[1], $b) |$expected, $actual| {
-    warning('Name is empty, using default')
-    'anonymous'
-  }
+# $valid_username contains "Amy Berry".
+# If $raw_username was an empty string or a different data type, the Puppet run would
+# fail with an "Expected type does not match actual" error.
+~~~
 
-See the documentation for 'The Puppet Type System' for more information about types.
+You can use an optional lambda to provide enhanced feedback. The lambda takes two
+mandatory parameters, in this order:
+
+1. The expected data type as described in the function's first argument.
+2. The actual data type of the value.
+
+**Example**: Using `assert_type` with a warning and default value
+
+~~~ puppet
+$raw_username = 'Amy Berry'
+
+# Assert that $raw_username is a non-empty string and assign it to $valid_username.
+# If it isn't, output a warning describing the problem and use a default value.
+$valid_username = assert_type(String[1], $raw_username) |$expected, $actual| {
+  warning( "The username should be \'${expected}\', not \'${actual}\'. Using 'anonymous'." )
+  'anonymous'
+}
+
+# $valid_username contains "Amy Berry".
+# If $raw_username was an empty string, the Puppet run would set $valid_username to
+# "anonymous" and output a warning: "The username should be 'String[1, default]', not
+# 'String[0, 0]'. Using 'anonymous'."
+~~~
+
+For more information about data types, see the
+[documentation](https://docs.puppetlabs.com/puppet/latest/reference/lang_data.html).
+
 - Since 4.0.0
-") do |args|
+DOC
+) do |args|
   function_fail(["assert_type() is only available when parser/evaluator future is in effect"])
 end
