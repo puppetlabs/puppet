puppetlabs/puppet

commit 0412515e2f9d75f00687bc5c01915da5a51c58e3

(maint) Optimize adding tags
Resources add a lot of tags and so streamlining that ends up with a
overall savings for catalog compilation. This removes an extra call to
collect, removes several method calls, and relies on the fact that the
@tags object is a set and so we don't need to check for the tag already
being there.

lib/puppet/util/tagging.rb
@@ -1,20 +1,24 @@
 require 'puppet/util/tag_set'
 
 module Puppet::Util::Tagging
+  ValidTagRegex = /^\w[-\w:.]*$/
+
   # Add a tag to our current list.  These tags will be added to all
   # of the objects contained in this scope.
   def tag(*ary)
     @tags ||= new_tags
 
-    qualified = []
-
-    ary.collect { |tag| tag.to_s.downcase }.each do |tag|
-      fail(Puppet::ParseError, "Invalid tag #{tag.inspect}") unless valid_tag?(tag)
-      qualified << tag if tag.include?("::")
-      @tags << tag unless @tags.include?(tag)
+    ary.each do |tag|
+      name = tag.to_s.downcase
+      if name =~ ValidTagRegex
+        @tags << name
+        name.split("::").each do |section|
+          @tags << section
+        end
+      else
+        fail(Puppet::ParseError, "Invalid tag #{name}")
+      end
     end
-
-    handle_qualified_tags( qualified )
   end
 
   # Are we tagged with the provided tag?
@@ -41,15 +45,6 @@ module Puppet::Util::Tagging
 
   private
 
-  def handle_qualified_tags(qualified)
-    qualified.each do |name|
-      name.split("::").each do |tag|
-        @tags << tag unless @tags.include?(tag)
-      end
-    end
-  end
-
-  ValidTagRegex = /^\w[-\w:.]*$/
   def valid_tag?(tag)
     tag.is_a?(String) and tag =~ ValidTagRegex
   end

commit ce1c0267bbf1067f912bb84ba210ed854dfb1ed4

(PUP-4495) Fix tag filtering regression
Prior to this commit a change made to the way tags are split when
filtering was causing the transaction to create unwanted tags. The
regression was introduced in 0412515e2.

Update the way a tag is recorded when filtering to fix this issue.

lib/puppet/transaction.rb
@@ -350,11 +350,8 @@ class Puppet::Transaction
     resource.appliable_to_host? && resource.appliable_to_device?
   end
 
-  def handle_qualified_tags( qualified )
-    # The default behavior of Puppet::Util::Tagging is
-    # to split qualified tags into parts. That would cause
-    # qualified tags to match too broadly here.
-    return
+  def split_qualified_tags?
+    false
   end
 
   # Is this resource tagged appropriately?
@@ -364,6 +361,7 @@ class Puppet::Transaction
 
     not resource.tagged?(*tags)
   end
+
 end
 
 require 'puppet/transaction/report'

lib/puppet/util/tagging.rb
@@ -14,8 +14,10 @@ module Puppet::Util::Tagging
       name = tag.to_s.downcase
       if name =~ ValidTagRegex
         @tags << name
-        name.split("::").each do |section|
-          @tags << section
+        if split_qualified_tags?
+          name.split("::").each do |section|
+            @tags << section
+          end
         end
       else
         fail(Puppet::ParseError, "Invalid tag '#{name}'")
@@ -67,6 +69,10 @@ module Puppet::Util::Tagging
 
   def valid_tag?(tag)
     tag.is_a?(String) and tag =~ ValidTagRegex
+  end
+
+  def split_qualified_tags?
+    true
   end
 
   def new_tags