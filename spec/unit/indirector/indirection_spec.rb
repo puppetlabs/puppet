489a279e055859379c609233af043183b8f2c6d5
Erik Dalén <dalen@spotify.com>
2015-09-29 12:42:56 +0200

    (PUP-5290) Log exceptions raised by saving to cache
    Failing to cache requests could lead to silent exits. For example if the
    apply application failed to cache to PuppetDB catalog terminus. This
    will log the exception and continue.

--- spec/unit/indirector/indirection_spec.rb
+++ spec/unit/indirector/indirection_spec.rb
@@ -393,6 +393,17 @@ describe Puppet::Indirector::Indirection do
 
           @indirection.find("/my/key")
         end
+
+        it "should fail if saving to the cache fails but log the exception" do
+          @cache.stubs(:find).returns nil
+
+          @terminus.stubs(:find).returns(@instance)
+          @cache.stubs(:save).raises RuntimeError
+
+          Puppet.expects(:log_exception)
+
+          expect { @indirection.find("/my/key") }.to raise_error
+        end
       end
     end
 
