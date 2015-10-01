85345227db3082c8b90dc9a6485e9ae49dc1e316
Will Hopper <whopper@puppetlabs.com>
2015-10-01 10:29:53 -0700

    (PUP-5016) Update linux services tests for Debian systemd behavior
    Previously, services_enable_linux.rb was failing in Debian 8
    and Ubuntu 15.05 due to issues with Systemd and SysVInit. The
    Debian 8 issues are corrected by the actual fix to the systemd
    and init providers, but the test continued to fail in Ubuntu
    due to an upstream systemd-sysv compatibility bug.
    
    This commit updates the test to use a service which has
    a systemd unit file, cron, rather than apache2 which did not. This
    allows the test to run correctly on Ubuntu. Once this upstream bug
    is fixed, we'll probably want to undo the Ubuntu changes for
    consistency.
    
    In addition, the systemd masking test is also updated for strange
    or broken Debian and Ubuntu systemd / Init behavior. In this case,
    Ubuntu suffers from the same bug as above. In Debian, Apache2 does
    not create the expected systemd symlinks as normal systemd services
    do, so we use cron.service instead.
    
    See https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1447807
    for the upstream bug report.

--- acceptance/tests/resource/service/service_enable_linux.rb
+++ acceptance/tests/resource/service/service_enable_linux.rb
@@ -1,7 +1,7 @@
 test_name 'SysV and Systemd Service Provider Validation'
 
 
-confine :to, :platform => /el|centos|fedora|debian|sles|ubuntu-[v-z]/
+confine :to, :platform => /el|centos|fedora|debian|sles|ubuntu-v/
 # osx covered by launchd_provider.rb
 # ubuntu-[a-u] upstart covered by ticket_14297_handle_upstart.rb
 
@@ -10,16 +10,12 @@ package_name = {'el'     => 'httpd',
                 'fedora' => 'httpd',
                 'debian' => 'apache2',
                 'sles'   => 'apache2',
-                'ubuntu' => 'apache2',
+                'ubuntu' => 'cron', # See https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1447807
 }
 
 agents.each do |agent|
   platform = agent.platform.variant
   majrelease = on(agent, facter('operatingsystemmajrelease')).stdout.chomp.to_i
-
-  if ((platform == 'debian' && majrelease == 8) || (platform == 'ubuntu' && majrelease == 15))
-    skip_test 'legit failures on debian8 and ubuntu15; see: PUP-5149'
-  end
 
   init_script_systemd = "/usr/lib/systemd/system/#{package_name[platform]}.service"
   symlink_systemd     = "/etc/systemd/system/multi-user.target.wants/#{package_name[platform]}.service"
@@ -45,27 +41,34 @@ agents.each do |agent|
     kill_symlink      = "K01apache2"
     start_runlevels   = ["3", "5"]
     kill_runlevels    = ["3", "5"]
+  elsif platform == 'ubuntu'
+    # Due to https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1447807, we can't test
+    # a service without a systemd unit file. Instead we use the systemd-managed cron.service.
+    start_symlink     = "S02cron"
+    kill_symlink      = "K01cron"
+    start_runlevels   = ["2", "3", "4", "5"]
+    kill_runlevels    = ["2", "3", "4", "5"]
   else
     start_symlink     = "S85httpd"
     kill_symlink      = "K15httpd"
   end
 
-  manifest_uninstall_httpd = %Q{
+  manifest_uninstall_package = %Q{
     package { '#{package_name[platform]}':
       ensure => absent,
     }
   }
-  manifest_install_httpd = %Q{
+  manifest_install_package = %Q{
     package { '#{package_name[platform]}':
       ensure => present,
     }
   }
-  manifest_httpd_enabled = %Q{
+  manifest_service_enabled = %Q{
     service { '#{package_name[platform]}':
       enable => true,
     }
   }
-  manifest_httpd_disabled = %Q{
+  manifest_service_disabled = %Q{
     service { '#{package_name[platform]}':
       enable => false,
     }
@@ -75,7 +78,7 @@ agents.each do |agent|
     if platform == 'sles'
       on agent, 'zypper remove -y apache2 apache2-prefork apache2-worker libapr1 libapr-util1'
     else
-      apply_manifest_on(agent, manifest_uninstall_httpd)
+      apply_manifest_on(agent, manifest_uninstall_package)
     end
   end
 
@@ -85,15 +88,15 @@ agents.each do |agent|
     fail_test "Provider needs manual update to support Fedora #{majrelease}"
   end
 
-  step "installing httpd/apache"
-  apply_manifest_on(agent, manifest_install_httpd, :catch_failures => true)
+  step "installing #{package_name[platform]}"
+  apply_manifest_on(agent, manifest_install_package, :catch_failures => true)
 
   step "ensure enabling service creates the start & kill symlinks"
   is_sysV = ((platform == 'centos' || platform == 'el') && majrelease < 7) ||
-              platform == 'debian' ||
+              platform == 'debian' || platform == 'ubuntu' ||
              (platform == 'sles'                        && majrelease < 12)
-  apply_manifest_on(agent, manifest_httpd_disabled, :catch_failures => true)
-  apply_manifest_on(agent, manifest_httpd_enabled, :catch_failures => true) do
+  apply_manifest_on(agent, manifest_service_disabled, :catch_failures => true)
+  apply_manifest_on(agent, manifest_service_enabled, :catch_failures => true) do
     if is_sysV
       # debian platforms using sysV put rc runlevels directly in /etc/
       on agent, "ln -s /etc/ /etc/rc.d", :accept_all_exit_codes => true
@@ -102,8 +105,13 @@ agents.each do |agent|
         assert_match("#{runlevel}.d/#{start_symlink}", rc_symlinks, "did not find #{start_symlink} in runlevel #{runlevel}")
         assert_match(/\/etc(\/rc\.d)?\/init\.d\/#{package_name[platform]}/, rc_symlinks, "did not find #{package_name[platform]} init script")
       end
-      kill_runlevels.each do |runlevel|
-        assert_match("#{runlevel}.d/#{kill_symlink}", rc_symlinks, "did not find #{kill_symlink} in runlevel #{runlevel}")
+
+      # Temporary measure until the Ubuntu SysV bugs are fixed. The cron service doesn't keep kill symlinks around while
+      # the service is enabled, unlike Apache2.
+      unless platform == 'ubuntu'
+        kill_runlevels.each do |runlevel|
+          assert_match("#{runlevel}.d/#{kill_symlink}", rc_symlinks, "did not find #{kill_symlink} in runlevel #{runlevel}")
+        end
       end
     else
       rc_symlinks = on(agent, "ls #{symlink_systemd} #{init_script_systemd}", :accept_all_exit_codes => true).stdout
@@ -113,7 +121,7 @@ agents.each do |agent|
   end
 
   step "ensure disabling service removes start symlinks"
-  apply_manifest_on(agent, manifest_httpd_disabled, :catch_failures => true) do
+  apply_manifest_on(agent, manifest_service_disabled, :catch_failures => true) do
     if is_sysV
       rc_symlinks = on(agent, "find /etc/ -name *#{package_name[platform]}", :accept_all_exit_codes => true).stdout
       # sles removes rc.d symlinks
