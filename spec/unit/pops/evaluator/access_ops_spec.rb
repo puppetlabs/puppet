78288e3cdbf2a1f102d352862e9748cf4255acec
Thomas Hallgren <thomas@tada.se>
2015-10-21 10:34:15 +0200

    (PUP-5381) Fix access operator for Undef and Optional types
    Passing a literal String as T to the Undef[T] or Optional[T] types
    was handled badly in the AccessOperator and equally badly in the
    unit test that verified the function (for Undef[T], there was no
    unit test for Optional[T]).
    
    This commit fixes the problem and the tests.

--- spec/unit/pops/evaluator/access_ops_spec.rb
+++ spec/unit/pops/evaluator/access_ops_spec.rb
@@ -412,7 +412,13 @@ describe 'Puppet::Pops::Evaluator::EvaluatorImpl/AccessOperator' do
     it 'produces a NotUndef instance with String type when given a literal String' do
       type_expr = fqr('NotUndef')[literal('hey')]
       tf = Puppet::Pops::Types::TypeFactory
-      expect(evaluate(type_expr)).to eql(tf.not_undef(tf.string('hey')))
+      expect(evaluate(type_expr)).to be_the_type(tf.not_undef(tf.string(nil, 'hey')))
+    end
+
+    it 'Produces Optional instance with String type when using a String argument' do
+      type_expr = fqr('Optional')[literal('hey')]
+      tf = Puppet::Pops::Types::TypeFactory
+      expect(evaluate(type_expr)).to be_the_type(tf.optional(tf.string(nil, 'hey')))
     end
 
     # Type Type
