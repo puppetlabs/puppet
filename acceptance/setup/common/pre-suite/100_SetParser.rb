b1990afa77800597acbdc683500f8b61e0caaa68
Josh Cooper <josh@puppetlabs.com>
2015-11-10 23:48:34 -0800

    (PUP-5308) Ensure future parser setting is in main section
    Previously, future parser acceptance failed because `puppet config set`
    does not work correctly if there is no main section (see PUP-4755),
    which occurs when testing future parser with `git` style acceptance.
    
    During the dynamic environment test, the master contained the following
    puppet.conf:
    
        [root@u8digeb42pv9e5r ~]# cat /etc/puppet/puppet.conf
        [global]
        parser = future
    
        [agent]
        server = u8digeb42pv9e5r.delivery.puppetlabs.net
    
        [main]
        manifest = $confdir/dynamic/$environment/manifests
        modulepath = $confdir/dynamic/$environment/modules
        config_version = $confdir/static-version.sh
    
    And future parser was not enabled:
    
        [root@u8digeb42pv9e5r ~]# puppet master --configprint parser
        current
    
    This confused the dynamic environment test, which expected future parser
    behavior, specifically an error message of the form:
    
        Could not find class ::testing_mod
    
    This commit works around the issue by ensuring we create a main section
    followed by the future parser setting. It uses beaker to introspect the
    location of the puppet.conf, since it's different on windows.

--- acceptance/setup/common/pre-suite/100_SetParser.rb
+++ acceptance/setup/common/pre-suite/100_SetParser.rb
@@ -5,7 +5,13 @@ test_name "add parser=#{ENV['PARSER']} to all puppet.conf (only if $PARSER is se
 
   hosts.each do |host|
     step "adjust #{host} puppet.conf" do
-      on(host, puppet("config set --section main parser #{parser}"))
+      # Future parser tests execute against `git` style
+      # acceptance, which does not create a default puppet.conf.
+      # And due to PUP-4755, we can't use `puppet config set`
+      # otherwise the setting is created outside of any section
+      # which makes it appear future parser is not enabled.
+      puppet_conf = host.puppet['config']
+      on(host, "echo \"[main]\nparser=future\n\" >> '#{puppet_conf}'")
     end
   end
 end
