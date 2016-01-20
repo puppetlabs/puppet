c68e4aa493149924de0564656d52d8f767ea878e
Michael Smith <michael.smith@puppetlabs.com>
2016-01-19 16:04:04 -0800

    Revert "(PUP-5609) Protect shared state with filesystem lock"

--- lib/puppet/application/apply.rb
+++ lib/puppet/application/apply.rb
@@ -6,9 +6,6 @@ class Puppet::Application::Apply < Puppet::Application
   require 'puppet/util/splayer'
   include Puppet::Util::Splayer
 
-  require 'puppet/util/locker'
-  include Puppet::Util::Locker
-
   option("--debug","-d")
   option("--execute EXECUTE","-e") do |arg|
     options[:code] = arg
@@ -167,15 +164,11 @@ Copyright (c) 2011 Puppet Labs, LLC Licensed under the Apache 2.0 License
   end
 
   def run_command
-    lock do
-      if options[:catalog]
-        apply
-      else
-        main
-      end
+    if options[:catalog]
+      apply
+    else
+      main
     end
-  rescue Puppet::LockError
-    Puppet.notice "Run of #{client_class} already in progress; skipping  (#{lockfile_path} exists)"
   end
 
   def apply
--- spec/integration/application/apply_spec.rb
+++ spec/integration/application/apply_spec.rb
@@ -18,7 +18,6 @@ describe "apply" do
       manifest = file_containing("manifest", catalog.to_pson)
 
       puppet = Puppet::Application[:apply]
-      puppet.stubs(:lock).yields
       puppet.options[:catalog] = manifest
 
       puppet.apply
@@ -35,7 +34,6 @@ describe "apply" do
     Puppet.override(:current_environment => special) do
       Puppet[:environment] = 'special'
       puppet = Puppet::Application[:apply]
-      puppet.stubs(:lock).yields
       puppet.stubs(:command_line).returns(stub('command_line', :args => [manifest]))
       expect { puppet.run_command }.to exit_with(0)
     end
@@ -48,7 +46,6 @@ describe "apply" do
     Puppet[:trusted_server_facts] = true
 
     puppet = Puppet::Application[:apply]
-    puppet.stubs(:lock).yields
     puppet.stubs(:command_line).returns(stub('command_line', :args => [manifest]))
 
     expect { puppet.run_command }.to exit_with(0)
@@ -70,7 +67,6 @@ describe "apply" do
       Puppet[:node_terminus] = 'exec'
       Puppet[:external_nodes] = enc
       puppet = Puppet::Application[:apply]
-      puppet.stubs(:lock).yields
       puppet.stubs(:command_line).returns(stub('command_line', :args => [manifest]))
       expect { puppet.run_command }.to exit_with(0)
     end
@@ -82,7 +78,6 @@ describe "apply" do
     it "logs compile errors once" do
       Puppet.initialize_settings([])
       apply = Puppet::Application.find(:apply).new(stub('command_line', :subcommand_name => :apply, :args => []))
-      apply.stubs(:lock).yields
       apply.options[:code] = '08'
 
       msg = 'valid octal'
@@ -98,7 +93,6 @@ describe "apply" do
     it "logs compile post processing errors once" do
       Puppet.initialize_settings([])
       apply = Puppet::Application.find(:apply).new(stub('command_line', :subcommand_name => :apply, :args => []))
-      apply.stubs(:lock).yields
       path = File.expand_path('/tmp/content_file_test.Q634Dlmtime')
       apply.options[:code] = "file { '#{path}':
         content => 'This is the test file content',
@@ -141,7 +135,6 @@ describe "apply" do
     def init_cli_args_and_apply_app(args, execute)
       Puppet.initialize_settings(args)
       puppet = Puppet::Application.find(:apply).new(stub('command_line', :subcommand_name => :apply, :args => args))
-      puppet.stubs(:lock).yields
       puppet.options[:code] = execute
       return puppet
     end
