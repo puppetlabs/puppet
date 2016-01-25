d9cd92bf5c0a0ccc0a1e803c727e1244f16cded2
Melissa Stone <melissa@puppetlabs.com>
2016-01-25 23:54:43 +0000

    (PUP-5718) Update puppet.bat to current standard
    With the switch over to building puppet-agent for windows with Vanagon,
    we need to effectively install puppet with install.rb. This script
    installs puppet.bat, which is a wrapper that lets windows handle ruby
    scripts. This commit updates puppet.bat to use our environment.bat file if
    we install it, and to be more in line with the puppet.bat file installed
    with the puppet-agent package as built by puppet_for_the_win

--- install.rb
+++ install.rb
@@ -430,10 +430,13 @@ def install_binfile(from, op_file, target)
       tmp_file2 = Tempfile.new('puppet-wrapper')
       cwv = <<-EOS
 @echo off
-setlocal
-set RUBY_BIN=%~dp0
-set RUBY_BIN=%RUBY_BIN:\\=/%
-"%RUBY_BIN%ruby.exe" -x "%RUBY_BIN%puppet" %*
+SETLOCAL
+if exist "%~dp0environment.bat" (
+  call "%~dp0environment.bat" %0 %*
+) else (
+  SET PATH=%~dp0;%PATH%
+)
+ruby.exe -S -- puppet %*
 EOS
       File.open(tmp_file2.path, "w") { |cw| cw.puts cwv }
       FileUtils.install(tmp_file2.path, File.join(target, "#{op_file}.bat"), :mode => 0755, :preserve => true, :verbose => true)
