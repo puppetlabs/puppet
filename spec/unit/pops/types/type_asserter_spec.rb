7ce17d87bbad0fde6a0a0761029bc5f8b08f8ac5
Thomas Hallgren <thomas@tada.se>
2016-02-17 15:14:56 +0100

    (PUP-5964) Enhance type asserter with deferred subject formatting
    The TypeAsserter methods takes a `subject` parameter that explains in
    what context the assertion takes place. It is often desirable to create
    this subject string from things in that context (i.e. format a string).
    Formatting strings that may or may not be used is however not a good
    practice since the formatting itself is resource consuming.
    
    This commit makes it possible to defer the formatting of the subject
    string until an error actually has been detected.
    
    This commit enables deferred formatting in one of two ways:
    
    1. As an alternative to passing the subject as a String, it can now also
       be passed as an Array. When an Array is used, the first element is
       treated as a format string and the remaining elements are passed as
       arguments to that format string.
    2. A block can be given provided to the assert call. The result of
       calling that block with the given subject as an argument will become
       the new subject. If the result is an Array, then it will be formatted
       as described in #1.

---
+++ spec/unit/pops/types/type_asserter_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'puppet/pops'
+
+module Puppet::Pops::Types
+describe 'the type asserter' do
+  let!(:asserter) { TypeAsserter }
+
+  context 'when deferring formatting of subject'
+  it 'can use an array' do
+    expect{ asserter.assert_instance_of(['The %s in the %s', 'gizmo', 'gadget'], PIntegerType::DEFAULT, 'lens') }.to(
+    raise_error(TypeAssertionError, 'The gizmo in the gadget value has wrong type, expected Integer, actual String'))
+  end
+
+  it 'can use an array obtained from block' do
+    expect do
+      asserter.assert_instance_of('gizmo', PIntegerType::DEFAULT, 'lens') { |s| ['The %s in the %s', s, 'gadget'] }
+    end.to(raise_error(TypeAssertionError, 'The gizmo in the gadget value has wrong type, expected Integer, actual String'))
+  end
+
+  it 'can use an subject obtained from zero argument block' do
+    expect do
+      asserter.assert_instance_of(nil, PIntegerType::DEFAULT, 'lens') { 'The gizmo in the gadget' }
+    end.to(raise_error(TypeAssertionError, 'The gizmo in the gadget value has wrong type, expected Integer, actual String'))
+  end
+
+  it 'does not produce a string unless the assertion fails' do
+    TypeAsserter.expects(:report_type_mismatch).never
+    asserter.assert_instance_of(nil, PIntegerType::DEFAULT, 1)
+  end
+
+  it 'does not format string unless the assertion fails' do
+    fmt_string = 'The %s in the %s'
+    fmt_string.expects(:'%').never
+    asserter.assert_instance_of([fmt_string, 'gizmo', 'gadget'], PIntegerType::DEFAULT, 1)
+  end
+
+  it 'does not call block unless the assertion fails' do
+    expect do
+      asserter.assert_instance_of(nil, PIntegerType::DEFAULT, 1) { |s| raise Error }
+    end.not_to raise_error
+  end
+end
+end
+
