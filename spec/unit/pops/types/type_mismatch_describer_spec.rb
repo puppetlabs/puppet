24ae7a6a02200b005d67b958a012a5f9c1f4cc41
Thomas Hallgren <thomas@tada.se>
2016-02-24 14:42:24 +0100

    (PUP-5964) Improve type mismatch describers ability to generalize
    Before this commit, the TypeMismatchDescriber would sometimes
    generalize too much because the classes of the expected and actual
    types didn't match (and PStructType != PHashType) or not generalize
    although it was possible (generalized form of actual type still not
    assignable).
    
    This commit improves the logic that decides where or not to generalize
    the types before reporting the mismatch.

--- spec/unit/pops/types/type_mismatch_describer_spec.rb
+++ spec/unit/pops/types/type_mismatch_describer_spec.rb
@@ -5,6 +5,46 @@ require 'puppet_spec/compiler'
 describe 'the type mismatch describer' do
   include PuppetSpec::Compiler
 
+  it 'will report a mismatch between a hash and a struct with details' do
+    code = <<-CODE
+      function f(Hash[String,String] $h) {
+         $h['a']
+      }
+      f({'a' => 'a', 'b' => 23})
+    CODE
+    expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects a Hash\[String, String\] value, got Struct\[\{'a'=>String, 'b'=>Integer\}\]/)
+  end
+
+  it 'will report a mismatch between a array and tuple with details' do
+    code = <<-CODE
+      function f(Array[String] $h) {
+         $h[0]
+      }
+      f(['a', 23])
+    CODE
+    expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects an Array\[String\] value, got Tuple\[String, Integer\]/)
+  end
+
+  it 'will not report a mismatch between a array and struct with details' do
+    code = <<-CODE
+      function f(Array[String] $h) {
+         $h[0]
+      }
+      f({'a' => 'a string', 'b' => 23})
+    CODE
+    expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects an Array value, got Struct/)
+  end
+
+  it 'will not report a mismatch between a hash and tuple with details' do
+    code = <<-CODE
+      function f(Hash[String,String] $h) {
+         $h['a']
+      }
+      f(['a', 23])
+    CODE
+    expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects a Hash value, got Tuple/)
+  end
+
   it 'will report a mismatch against an aliased type correctly' do
     code = <<-CODE
       type UnprivilegedPort = Integer[1024,65537]
