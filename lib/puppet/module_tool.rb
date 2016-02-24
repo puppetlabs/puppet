76df6bedd2af59f72e430598c79e139efc57ab9e
Peter Drake <peter.drake@acquia.com>
2016-02-24 08:14:07 -0500

    (PUP-4742) Switch from vendored Semantic to use the SemanticPuppet gem.
    Without this patch applied, puppet includes vendored code in the Semantic
    namespace.  This namespace collides with the Semantic gem.  This patch
    fixes the problem by removing the vendored Semantic code and using the
    SemanticPuppet gem.

--- lib/puppet/module_tool.rb
+++ lib/puppet/module_tool.rb
@@ -157,12 +157,12 @@ module Puppet
     end
 
     # Handles parsing of module dependency expressions into proper
-    # {Semantic::VersionRange}s, including reasonable error handling.
+    # {SemanticPuppet::VersionRange}s, including reasonable error handling.
     #
     # @param where [String] a description of the thing we're parsing the
     #        dependency expression for
     # @param dep [Hash] the dependency description to parse
-    # @return [Array(String, Semantic::VersionRange, String)] an tuple of the
+    # @return [Array(String, SemanticPuppet::VersionRange, String)] an tuple of the
     #         dependent module's name, the version range dependency, and the
     #         unparsed range expression.
     def self.parse_module_dependency(where, dep)
@@ -170,10 +170,10 @@ module Puppet
       range = dep['version_requirement'] || '>= 0.0.0'
 
       begin
-        parsed_range = Semantic::VersionRange.parse(range)
+        parsed_range = SemanticPuppet::VersionRange.parse(range)
       rescue ArgumentError => e
         Puppet.debug "Error in #{where} parsing dependency #{dep_name} (#{e.message}); using empty range."
-        parsed_range = Semantic::VersionRange::EMPTY_RANGE
+        parsed_range = SemanticPuppet::VersionRange::EMPTY_RANGE
       end
 
       [ dep_name, parsed_range, range ]
