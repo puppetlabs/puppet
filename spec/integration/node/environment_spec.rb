010dd2144eaecd1e72f4ac9eb0e839b568764dbc
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-02-24 17:41:58 -0800

    (PUP-5768) Add tests for broken Windows ENV usage
    This commit adds tests to demonstrate the bad behavior when using
    environment variables with UTF8 characters in the name and values.

--- spec/integration/node/environment_spec.rb
+++ spec/integration/node/environment_spec.rb
@@ -119,4 +119,18 @@ describe Puppet::Node::Environment do
       # be raised if 'b' is evaluated before 'a').
       :strict_variables => true
   end
+
+  describe "#extralibs on Windows", :if => Puppet.features.microsoft_windows? do
+
+    describe "with UTF8 characters in PUPPETLIB" do
+      let(:rune_utf8) { "\u16A0\u16C7\u16BB\u16EB\u16D2\u16E6\u16A6\u16EB\u16A0\u16B1\u16A9\u16A0\u16A2\u16B1\u16EB\u16A0\u16C1\u16B1\u16AA\u16EB\u16B7\u16D6\u16BB\u16B9\u16E6\u16DA\u16B3\u16A2\u16D7" }
+
+      before { Puppet::Util::Windows::Process.set_environment_variable('PUPPETLIB', rune_utf8) }
+      after  { Puppet::Util::Windows::Process.set_environment_variable('PUPPETLIB', nil) }
+
+      it "should use UTF8 characters in PUPPETLIB environment variable" do
+        expect(Puppet::Node::Environment.extralibs()).to eq([rune_utf8])
+      end
+    end
+  end
 end
