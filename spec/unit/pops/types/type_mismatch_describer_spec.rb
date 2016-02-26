e01f30f6670c056257acdb131cf19f643d9220c3
Thomas Hallgren <thomas@tada.se>
2016-02-26 13:31:50 +0100

    (PUP-5964) Handle present and past tense when reporting type mismatch
    This commit adds a 'tense' argument to the TypeMismatchDescriptor. The
    tense can be either :past or :present and controls the wording of the
    created sentences accordingly.
    
    The commit also adjusts the wording of two unit test descriptions for
    the mismatch describer.

--- spec/unit/pops/types/type_mismatch_describer_spec.rb
+++ spec/unit/pops/types/type_mismatch_describer_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 require 'puppet/pops'
 require 'puppet_spec/compiler'
 
+module Puppet::Pops
+module Types
+
 describe 'the type mismatch describer' do
   include PuppetSpec::Compiler
 
@@ -25,7 +28,7 @@ describe 'the type mismatch describer' do
     expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects an Array\[String\] value, got Tuple\[String, Integer\]/)
   end
 
-  it 'will not report a mismatch between a array and struct with details' do
+  it 'will not report details for a mismatch between an array and a struct' do
     code = <<-CODE
       function f(Array[String] $h) {
          $h[0]
@@ -35,7 +38,7 @@ describe 'the type mismatch describer' do
     expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /expects an Array value, got Struct/)
   end
 
-  it 'will not report a mismatch between a hash and tuple with details' do
+  it 'will not report details for a mismatch between a hash and a tuple' do
     code = <<-CODE
       function f(Hash[String,String] $h) {
          $h['a']
@@ -56,4 +59,62 @@ describe 'the type mismatch describer' do
     CODE
     expect { eval_and_collect_notices(code) }.to raise_error(Puppet::Error, /parameter 'port' expects an UnprivilegedPort value, got Integer\[34, 34\]/)
   end
+
+  context 'when using present tense' do
+    let(:parser) { TypeParser.new }
+    let(:subject) { TypeMismatchDescriber.singleton }
+    it 'reports a missing parameter as "has no parameter"' do
+      t = parser.parse('Struct[{a=>String}]')
+      expect { subject.validate_parameters('v', t, {'a'=>'a','b'=>'b'}, false, :present) }.to raise_error(Puppet::Error, "v: has no parameter named 'b'")
+    end
+
+    it 'reports a missing value as "expects a value"' do
+      t = parser.parse('Struct[{a=>String,b=>String}]')
+      expect { subject.validate_parameters('v', t, {'a'=>'a'}, false, :present) }.to raise_error(Puppet::Error, "v: expects a value for parameter 'b'")
+    end
+
+    it 'reports a missing block as "expects a block"' do
+      callable = parser.parse('Callable[String,String,Callable]')
+      args_tuple = parser.parse('Tuple[String,String]')
+      dispatch = Functions::Dispatch.new(callable, 'foo', ['a','b'], 'block', nil, nil, false)
+      expect(subject.describe_signatures('function', [dispatch], args_tuple, :present)).to eq("'function' expects a block")
+    end
+
+    it 'reports an unexpected block as "does not expect a block"' do
+      callable = parser.parse('Callable[String,String]')
+      args_tuple = parser.parse('Tuple[String,String,Callable]')
+      dispatch = Functions::Dispatch.new(callable, 'foo', ['a','b'], nil, nil, nil, false)
+      expect(subject.describe_signatures('function', [dispatch], args_tuple, :present)).to eq("'function' does not expect a block")
+    end
+  end
+
+  context 'when using past tense' do
+    let(:parser) { TypeParser.new }
+    let(:subject) { TypeMismatchDescriber.singleton }
+    it 'reports a missing parameter as "did not have a parameter"' do
+      t = parser.parse('Struct[{a=>String}]')
+      expect { subject.validate_parameters('v', t, {'a'=>'a','b'=>'b'}, false, :past) }.to raise_error(Puppet::Error, "v: did not have a parameter named 'b'")
+    end
+
+    it 'reports a missing value as "expected a value"' do
+      t = parser.parse('Struct[{a=>String,b=>String}]')
+      expect { subject.validate_parameters('v', t, {'a'=>'a'}, false, :past) }.to raise_error(Puppet::Error, "v: expected a value for parameter 'b'")
+    end
+
+    it 'reports a missing block as "expected a block"' do
+      callable = parser.parse('Callable[String,String,Callable]')
+      args_tuple = parser.parse('Tuple[String,String]')
+      dispatch = Functions::Dispatch.new(callable, 'foo', ['a','b'], 'block', nil, nil, false)
+      expect(subject.describe_signatures('function', [dispatch], args_tuple, :past)).to eq("'function' expected a block")
+    end
+
+    it 'reports an unexpected block as "did not expect a block"' do
+      callable = parser.parse('Callable[String,String]')
+      args_tuple = parser.parse('Tuple[String,String,Callable]')
+      dispatch = Functions::Dispatch.new(callable, 'foo', ['a','b'], nil, nil, nil, false)
+      expect(subject.describe_signatures('function', [dispatch], args_tuple, :past)).to eq("'function' did not expect a block")
+    end
+  end
+end
+end
 end
