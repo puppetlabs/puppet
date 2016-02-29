4cd09371c7aa86c9e396a922abc08e88096b5156
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-02-29 15:18:09 +0100

    (PUP-5986) Propagate loader to type parser in 4.x function dispatch
    This ensures that a loader is propagated to the type parser used when
    parsing 4.x function dispatch declarations.
    
    With a loader present, the parser can load aliased types and also
    distinguish between an existing type and a resource.

--- lib/puppet/pops/loader/ruby_function_instantiator.rb
+++ lib/puppet/pops/loader/ruby_function_instantiator.rb
@@ -16,7 +16,10 @@ class Puppet::Pops::Loader::RubyFunctionInstantiator
     unless ruby_code_string.is_a?(String) && ruby_code_string =~ /Puppet\:\:Functions\.create_function/
       raise ArgumentError, "The code loaded from #{source_ref} does not seem to be a Puppet 4x API function - no create_function call."
     end
-    created = eval(ruby_code_string, nil, source_ref, 1)
+    # make the private loader available in a binding to allow it to be passed on
+    loader_for_function = loader.private_loader
+    here = get_binding(loader_for_function)
+    created = eval(ruby_code_string, here, source_ref, 1)
     unless created.is_a?(Class)
       raise ArgumentError, "The code loaded from #{source_ref} did not produce a Function class when evaluated. Got '#{created.class}'"
     end
@@ -27,8 +30,18 @@ class Puppet::Pops::Loader::RubyFunctionInstantiator
     # when calling functions etc.
     # It should be bound to global scope
 
-    # TODO: Cheating wrt. scope - assuming it is found in the context
+    # Cheating wrt. scope - assuming it is found in the context (else an empty hash is used).
     closure_scope = Puppet.lookup(:global_scope) { {} }
-    created.new(closure_scope, loader.private_loader)
+    # If function definition used the loader from the binding to create a new loader, that loader wins
+    created.new(closure_scope, loader_for_function)
+  end
+
+  private
+
+  # Produces a binding where the given loader is bound as a local variable (loader_injected_arg). This variable can be used in loaded
+  # ruby code - e.g. to call Puppet::Function.create_loaded_function(:name, loader,...)
+  #
+  def self.get_binding(loader_injected_arg)
+    binding
   end
 end
