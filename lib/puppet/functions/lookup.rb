1b7ea91be1763ba9310b0735f67f46a2d52d562b
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-03-30 16:41:20 +0200

    (PUP-6074) Remove 'unpack-arrays' options from lookup function and CLI
    The unpack-arrays option is not of any value and is now removed.

--- lib/puppet/functions/lookup.rb
+++ lib/puppet/functions/lookup.rb
@@ -100,9 +100,6 @@
 #     disables this feature.
 #     * `'sort_merged_arrays'` (boolean) --- Whether to sort all arrays that are
 #     merged together. Defaults to `false`.
-#     * `'unpack_arrays'` (string or undef) --- A delimiter string; Puppet will
-#     join merged arrays with this delimiter, then split them again. Defaults to
-#     `undef`, which disables this feature.
 #     * `'merge_hash_arrays'` (boolean) --- Whether to merge hashes within arrays.
 #     Defaults to `false`.
 #
