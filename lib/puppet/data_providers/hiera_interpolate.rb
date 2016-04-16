2570ab9120d0c72e84259a1092d85b9cf34e0b66
Thomas Hallgren <thomas@tada.se>
2016-04-16 08:39:31 +0200

    (PUP-6091) Use strict option and warn only once about deprecated method

--- lib/puppet/data_providers/hiera_interpolate.rb
+++ lib/puppet/data_providers/hiera_interpolate.rb
@@ -5,8 +5,15 @@ module Puppet::DataProviders::HieraInterpolate
   # For backward compatibility
   # @deprecated
   def qualified_lookup(segments, value)
-    Puppet::deprecation_warning(
-      'Puppet::DataProviders::HieraInterpolate#qualified_lookup is deprecated, use Puppet::Pops::Lookup::SubLookup#sub_lookup')
+    if Puppet[:strict] != :off
+      msg = 'Puppet::DataProviders::HieraInterpolate#qualified_lookup is deprecated, use Puppet::Pops::Lookup::SubLookup#sub_lookup'
+      case Puppet[:strict]
+      when :error
+        raise Puppet::DataBinding::LookupError.new(msg)
+      when :warning
+        Puppet.warn_once(:deprecation, 'HieraInterpolate#qualified_lookup', msg)
+      end
+    end
     sub_lookup('<unknown key>', Puppet::Pops::Lookup::Invocation.new(nil), segments, value)
   end
 end
