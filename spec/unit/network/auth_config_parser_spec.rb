5f5343b2effef14dfffb8484f35454ec1a0ee2ab
Iristyle <Iristyle@github>
2016-04-19 19:31:31 -0700

    (PUP-5879) AuthConfigParser reads files as UTF-8
     - Ensure that authorization config files may contain UTF-8.  This is
       unlikely to change behavior within Puppet as Windows cannot host
       routes, but added for UTF-8 unification.

--- spec/unit/network/auth_config_parser_spec.rb
+++ spec/unit/network/auth_config_parser_spec.rb
@@ -4,6 +4,7 @@ require 'puppet/network/auth_config_parser'
 require 'puppet/network/authconfig'
 
 describe Puppet::Network::AuthConfigParser do
+  include PuppetSpec::Files
 
   let(:fake_authconfig) do
     "path ~ ^/catalog/([^/])\nmethod find\nallow *\n"
@@ -96,6 +97,21 @@ describe Puppet::Network::AuthConfigParser do
       Puppet::Network::Rights::Right.any_instance.expects(:restrict_authenticated).with('yes')
 
       described_class.new("path /certificates\nauthenticated yes").parse_rights
+    end
+  end
+
+  describe "when parsing rights from files" do
+    it "can read UTF-8" do
+      rune_path = "/\u16A0\u16C7\u16BB" # ᚠᛇᚻ
+      config = tmpfile('config')
+
+      File.open(config, 'w', :encoding => 'utf-8') do |file|
+        file.puts <<-EOF
+path #{rune_path}
+      EOF
+    end
+
+      expect(described_class.new_from_file(config).parse_rights[rune_path]).to be
     end
   end
 end
