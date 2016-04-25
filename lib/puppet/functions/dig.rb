d99c47018c3c3867ba7e84f67f8fb6653522a5ee
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-04-25 02:22:19 +0200

    (PUP-6046) Add a 'dig' function that allows user to dig into a structure
    The dig function allows "digging into" a structure by specifying a
    sequence of keys/indexes into a structure of Hash/Array values.
    
    An `undef` value is returned if a key does not exists.
    If a key is applied to a a result that is not an Array or Hash (but not
    Undef) an error is raised.

---
+++ lib/puppet/functions/dig.rb
@@ -0,0 +1,35 @@
+# Returns a value for a sequence of given keys/indexes
+# used to "dig into" a complex data structure. The first
+# encountered undef value stops the "dig" and undef is returned.
+# An error is raised if an attempt is made to "dig" into
+# something other than an Array or a Hash. 
+#
+# @example Using `dig`
+#
+# ~~~ puppet
+# $data = {a => { b => [{x => 10, y => 20}, {x => 100, y => 200}]}}
+# notice $data.dig(a, b, 1, x)
+# ~~~
+#
+# Would notice the value 100.
+#
+# @since 4.5.0
+#
+Puppet::Functions.create_function(:dig) do
+  dispatch :dig do
+    param 'Collection', :data
+    repeated_param 'Any', :arg
+  end
+
+  def dig(data, *args)
+    walked_path = []
+    args.reduce(data) do | d, k |
+      return nil if d.nil?
+      if !(d.is_a?(Array) || d.is_a?(Hash))
+        raise ArgumentError, "The given data does not contain a Collection at #{walked_path}, got '#{d.class}'"
+      end
+      walked_path << k
+      d[k]
+    end
+  end
+end
