2f4c0642f8d053ca5c134ae25da258147f2f6270
Richard Clamp <richardc@unixbeard.net>
2016-04-26 11:46:12 +0100

    (PUP-6072) Make --lastrunreport be a path-style argument
    With this change we make the `lastrunreport` option from being a simple
    file-type argument to being a path-type argument.
    
    This is to support a usage case where a user wants to run the puppet with
    a known and distinct run report in addition to the typically configured
    version, which looks something like:
    
       $ puppet apply --lastrunreport=$(puppet config print lastrunreport):/tmp/special_report.yaml -e 'notify {"Hello World":}'
    commit fb252414284a7e31d1290fddcbec155599bc296f

--- lib/puppet/defaults.rb
+++ lib/puppet/defaults.rb
@@ -1517,8 +1517,7 @@ EOT
     },
     :lastrunreport =>  {
       :default  => "$statedir/last_run_report.yaml",
-      :type     => :file,
-      :mode     => "0640",
+      :type     => :path,
       :desc     => "Where puppet agent stores the last run report in yaml format."
     },
     :graph => {
