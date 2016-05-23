3e4098266b707a34b8a516987318bf1c4d3ea794
keymone <keymone@gmail.com>
2016-05-23 14:21:26 +0200

    (PUP-6408) Cache environment config for as long as environment
    Environment config is not cached which generates unnecessary io during
    compilation phase.
    
    This change introduces environment config caching alongside environment
    cache. Whenever environment is evicted, it's config is evicted as well.

--- lib/puppet/environments.rb
+++ lib/puppet/environments.rb
@@ -313,6 +313,7 @@ module Puppet::Environments
       @loader = loader
       @cache_expiration_service = Puppet::Environments::Cached.cache_expiration_service
       @cache = {}
+      @cache_conf = {}
 
       # Holds expiration times in sorted order - next to expire is first
       @expirations = SortedSet.new
@@ -365,6 +366,7 @@ module Puppet::Environments
     # (The intention is that this could be used from a MANUAL cache eviction command (TBD)
     def clear(name)
       @cache.delete(name)
+      @cache_conf.delete(name)
     end
 
     # Clears all cached environments.
@@ -372,6 +374,7 @@ module Puppet::Environments
     def clear_all()
       super
       @cache = {}
+      @cache_conf = {}
       @expirations.clear
       @next_expiration = END_OF_TIME
     end
@@ -403,7 +406,7 @@ module Puppet::Environments
     # @!macro loader_get_conf
     def get_conf(name)
       evict_if_expired(name)
-      @loader.get_conf(name)
+      @cache_conf.fetch(name) { @cache_conf[name] = @loader.get_conf(name) }
     end
 
     # Creates a suitable cache entry given the time to live for one environment
@@ -426,6 +429,7 @@ module Puppet::Environments
       if (result = @cache[name]) && (result.expired? || @cache_expiration_service.expired?(name))
       Puppet.debug {"Evicting cache entry for environment '#{name}'"}
         @cache.delete(name)
+        @cache_conf.delete(name)
         @cache_expiration_service.evicted(name)
 
         Puppet.settings.clear_environment_settings(name)
