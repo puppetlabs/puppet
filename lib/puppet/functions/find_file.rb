6c0df9d40906c980bdd2df77df87406022dec9ee
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-06-01 02:38:55 +0200

    (PUP-5831) Add a find_file function
    When designing (a yet to be committed) binary_file function the ability
    found in the file function to search for a file to read was better
    implemented as a single general purpose function letting the binary file
    function operate on a single file.
    
    The added `find_file` searches for the first existing file among a given
    set of paths; as individual arguments or in an array. The first absolute
    or module relative path to an existing file is returned.
    
    By separating this behavior into a separate file it is also possible for
    the author to write conditional logic instead of just seeing a failure
    from the `binary_file` read if none of the paths existed.

---
+++ lib/puppet/functions/find_file.rb
@@ -0,0 +1,43 @@
+# Finds an existing file from a module and returns its path.
+#
+# The argument to this function should be a String as a `<MODULE NAME>/<FILE>`
+# reference, which will search for `<FILE>` relative to a module's `files`
+# directory. (For example, the reference `mysql/mysqltuner.pl` will search for the
+# file `<MODULES DIRECTORY>/mysql/files/mysqltuner.pl`.)
+#
+# This function can also accept:
+#
+# * An absolute String path, which will check for the existence of a file from anywhere on disk.
+# * Multiple String arguments, which will return the path of the **first** file
+#   found, skipping non existing files.
+# * An array of string paths, which will return the path of the **first** file
+#   found from the given paths in the array, skipping non existing files.
+#
+# @since 4.6.0
+#
+Puppet::Functions.create_function(:find_file, Puppet::Functions::InternalFunction) do
+  dispatch :find_file do
+    scope_param
+    repeated_param 'String', :paths
+  end
+
+  dispatch :find_file_array do
+    scope_param
+    repeated_param 'Array[String]', :paths_array
+  end
+
+  def find_file_array(scope, array)
+    find_file(scope, *array)
+  end
+
+  def find_file(scope, *args)
+    path = nil
+    args.each do |file|
+      found = Puppet::Parser::Files.find_file(file, scope.compiler.environment)
+      if found && Puppet::FileSystem.exist?(found)
+        return found
+      end
+    end
+    nil
+  end
+end
