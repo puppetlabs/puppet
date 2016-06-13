933b7ca57203521d51f876974fc9b452d85fea11
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-06-14 01:36:47 +0200

    (doc) Move documentation for 'type' func from 4.x impl to 3.x stub

--- lib/puppet/functions/type.rb
+++ lib/puppet/functions/type.rb
@@ -1,43 +1,4 @@
-# Returns the data type of a given value with a given degree of generality.
-#
-# @example Using `type()`
-#
-# ~~~ puppet
-# notice type(42) =~ Type[Integer]
-# ~~~
-#
-# Would notice true
-#
-# By default, the best possible inference is made where all details are retained.
-# This is good when the type is used for further type calculations but is overwhelmingly
-# rich in information if it is used in a error message.
-#
-# The optional argument `inference_method` may be given as (from lowest to highest fidelity):
-#
-# * `generalized` - reduces to common type and drops size constraints
-# * `reduced` - reduces to common type in collections
-# * `detailed` - (default) all details about inferred types is retained
-#
-# @example Using `type()` with different qualities:
-#
-# ~~~ puppet
-# notice type([3.14, 42], generalized)
-# notice type([3.14, 42], reduced)
-# notice type([3.14, 42], detailed)
-# notice type([3.14, 42])
-# ~~~
-#
-# Would notice the four values:
-#
-# 1. 'Array[Numeric]'
-# 2. 'Array[Numeric, 2, 2]'
-# 3. 'Tuple[Float[3.14], Integer[42,42]]]'
-# 4. 'Tuple[Float[3.14], Integer[42,42]]]'
-#
-# @param value [Any] - the value for which data type is returned
-# @param inference_type[Enum[generalized, reduced, detailed]] inference_type
-# @returns [Type] - the inferred type
-#
+# (Documentation in 3.x stub)
 # @since 4.4.0
 #
 Puppet::Functions.create_function(:type) do
