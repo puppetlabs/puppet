a74f8830fb686d1b57068cc3b95a7bac77dbf85b
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2016-06-14 16:17:08 +0200

    (doc) Remove UTF-8 multibyte chars in doc for function 'new'
    Before this, loading the 3.x file for documentation production failed on
    Ruby 1.9.3 because it does not load the file using UTF-8 encoding. This
    was however a good thing as it caught back-ticks being multibyte
    characters looking like regular back-ticks. (Have no idea why that
    happened).

--- lib/puppet/parser/functions/new.rb
+++ lib/puppet/parser/functions/new.rb
@@ -261,7 +261,7 @@ the specificity of the mapped type; for example, different formats can be used f
 | ------  | ---------------
 | d       | Decimal, negative values produces leading '-'
 | x X     | Hexadecimal in lower or upper case. Uses ..f/..F for negative values unless + is also used. A `#` adds prefix 0x/0X.
-| o       | Octal. Uses ..0 for negative values unless ´+´ is also used. A `#` adds prefix 0.
+| o       | Octal. Uses ..0 for negative values unless `+` is also used. A `#` adds prefix 0.
 | b B     | Binary with prefix 'b' or 'B'. Uses ..1/..1 for negative values unless `+` is also used
 | c       | numeric value representing a Unicode value, result is a one unicode character string, quoted if alternative flag # is used
 | s       | same as d, or d in quotes if alternative flag # is used
