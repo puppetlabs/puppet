9fa9204f75353ed2b897087452662a179a575edf
Daniel Lu <DLuCJ@users.noreply.github.com>
2016-07-07 15:05:40 -0700

    (maint) Reset Puppet version after version_spec tests
    During work on PUP-6467, it was found that the version_spec
    tests were modifying Puppet.version value globally, which was
    affecting tests in integration/indirector/facts/facter_spec.rb.
    Facter was incorrectly reporting a Puppet version of 1.2.3,
    instead of 4.6.0 during the tests, causing failures.
    
    This commit reverts Puppet.version to its original value after
    each test in version_spec.  It also adds a context block and
    new test to clarify the functionality of the version setter.

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -4,11 +4,16 @@ require 'pathname'
 
 describe "Puppet.version Public API" do
   before :each do
+    @current_ver = Puppet.version
     Puppet.instance_eval do
       if @puppet_version
         @puppet_version = nil
       end
     end
+  end
+
+  after :each do
+    Puppet.version = @current_ver
   end
 
   context "without a VERSION file" do
@@ -35,6 +40,14 @@ describe "Puppet.version Public API" do
       expect(Puppet.version).to eq('3.0.1-260-g9ca4e54')
     end
     it "respects the version= setter" do
+      Puppet.version = '1.2.3'
+      expect(Puppet.version).to eq('1.2.3')
+    end
+  end
+
+  context "Using version setter" do
+    it "does not read VERSION file if using set version" do
+      Puppet.expects(:read_version_file).never
       Puppet.version = '1.2.3'
       expect(Puppet.version).to eq('1.2.3')
     end
