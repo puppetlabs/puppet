puppetlabs/puppet

commit bd3f3a18fa4b5ae1f1f26d8aabf74edd18598746

(PUP-4373) Refactor shared user / group ADSI code
 - Provide a Puppet::Util::Windows::ADSI::Shared module to be leveraged
   by both Puppet::Util::Windows::ADSI::User and
   Puppet::Util::Windows::ADSI::Group since the sid resolution and
   mapping overlaps.

 - Note that there are issues in the implementation of Group that will
   be resolved in subsequent commits.

lib/puppet/util/windows/adsi.rb
@@ -97,18 +97,19 @@ module Puppet::Util::Windows::ADSI
       [:lpwstr, :lpdword], :win32_bool
   end
 
-  class User
-    extend Enumerable
-    extend FFI::Library
+  module Shared
+    def uri(name, host = '.')
+      if sid_uri = Puppet::Util::Windows::ADSI.sid_uri_safe(name) then return sid_uri end
 
-    attr_accessor :native_user
-    attr_reader :name, :sid
-    def initialize(name, native_user = nil)
-      @name = name
-      @native_user = native_user
+      host = '.' if ['NT AUTHORITY', 'BUILTIN', Socket.gethostname].include?(host)
+
+      # group or user
+      account_type = self.name.split('::').last.downcase
+
+      Puppet::Util::Windows::ADSI.uri(name, account_type, host)
     end
 
-    def self.parse_name(name)
+    def parse_name(name)
       if name =~ /\//
         raise Puppet::Error.new( "Value must be in DOMAIN\\user style syntax" )
       end
@@ -120,20 +121,51 @@ module Puppet::Util::Windows::ADSI
       return account, domain
     end
 
+    def get_sids(adsi_child_collection)
+      sids = []
+      adsi_child_collection.each do |m|
+        sids << Puppet::Util::Windows::SID.octet_string_to_sid_object(m.objectSID)
+      end
+
+      sids
+    end
+
+    def name_sid_hash(names)
+      return {} if names.nil? || names.empty?
+
+      sids = names.map do |name|
+        sid = Puppet::Util::Windows::SID.name_to_sid_object(name)
+        raise Puppet::Error.new( "Could not resolve name: #{name}" ) if !sid
+        [sid.to_s, sid]
+      end
+
+      Hash[ sids ]
+    end
+  end
+
+  class User
+    extend Enumerable
+    extend Puppet::Util::Windows::ADSI::Shared
+    extend FFI::Library
+
+    # https://msdn.microsoft.com/en-us/library/aa746340.aspx
+    # IADsUser interface
+
+    require 'puppet/util/windows/sid'
+
+    attr_accessor :native_user
+    attr_reader :name, :sid
+    def initialize(name, native_user = nil)
+      @name = name
+      @native_user = native_user
+    end
+
     def native_user
       @native_user ||= Puppet::Util::Windows::ADSI.connect(self.class.uri(*self.class.parse_name(@name)))
     end
 
     def sid
       @sid ||= Puppet::Util::Windows::SID.octet_string_to_sid_object(native_user.objectSID)
-    end
-
-    def self.uri(name, host = '.')
-      if sid_uri = Puppet::Util::Windows::ADSI.sid_uri_safe(name) then return sid_uri end
-
-      host = '.' if ['NT AUTHORITY', 'BUILTIN', Socket.gethostname].include?(host)
-
-      Puppet::Util::Windows::ADSI.uri(name, 'user', host)
     end
 
     def uri
@@ -301,6 +333,10 @@ module Puppet::Util::Windows::ADSI
 
   class Group
     extend Enumerable
+    extend Puppet::Util::Windows::ADSI::Shared
+
+    # https://msdn.microsoft.com/en-us/library/aa706021.aspx
+    # IADsGroup interface
 
     attr_accessor :native_group
     attr_reader :name, :sid
@@ -311,12 +347,6 @@ module Puppet::Util::Windows::ADSI
 
     def uri
       self.class.uri(name)
-    end
-
-    def self.uri(name, host = '.')
-      if sid_uri = Puppet::Util::Windows::ADSI.sid_uri_safe(name) then return sid_uri end
-
-      Puppet::Util::Windows::ADSI.uri(name, 'group', host)
     end
 
     def native_group
@@ -344,18 +374,6 @@ module Puppet::Util::Windows::ADSI
       self
     end
 
-    def self.name_sid_hash(names)
-      return {} if names.nil? or names.empty?
-
-      sids = names.map do |name|
-        sid = Puppet::Util::Windows::SID.name_to_sid_object(name)
-        raise Puppet::Error.new( "Could not resolve username: #{name}" ) if !sid
-        [sid.to_s, sid]
-      end
-
-      Hash[ sids ]
-    end
-
     def add_member_sids(*sids)
       sids.each do |sid|
         native_group.Add(Puppet::Util::Windows::ADSI.sid_uri(sid))
@@ -376,11 +394,7 @@ module Puppet::Util::Windows::ADSI
     end
 
     def member_sids
-      sids = []
-      native_group.Members.each do |m|
-        sids << Puppet::Util::Windows::SID.octet_string_to_sid_object(m.objectSID)
-      end
-      sids
+      self.class.get_sids(native_group.Members)
     end
 
     def set_members(desired_members, inclusive = true)

spec/unit/util/windows/adsi_spec.rb
@@ -380,7 +380,7 @@ describe Puppet::Util::Windows::ADSI, :if => Puppet.features.microsoft_windows? 
           expect {
             adsi_group.expects(:Members).returns []
             group.set_members(['foobar'])
-          }.to raise_error(Puppet::Error, /Could not resolve username: foobar/)
+          }.to raise_error(Puppet::Error, /Could not resolve name: foobar/)
         end
       end
 

commit 05d1ca22e625e7bb073166c289745a3b4c533a14

(PUP-5938) Fix localized WinNT BUILTIN monikers
 - When building WinNT monikers, BUILTIN and NT AUTHORITY cannot be
   used to refer to users / groups and those names must be replaced
   with "."  For instance, SYSTEM account must use the moniker:

   WinNT://./SYSTEM,user - NOT WinNT://NT AUTHORITY/SYSTEM,user

   455834e7f21f213e9d0085510d5b47c0c3e2a1ab added the initial code for
   User moniker generation to take that into account

   Similarly, a group moniker for Administrators should use:

   WinNT://./Administrators,group - NOT WinNT://BUILTIN/Administrators,group

   bd3f3a18fa4b5ae1f1f26d8aabf74edd18598746 made sure that monikers
   for groups followed this behavior correctly as well.

   NOTE: These monikers are used only for adding / removing users to
   groups, or groups to a user - and aren't always able to be used
   with WIN32OLE.connect - this is an instrinsic behavior of the
   WinNT:// provider.

 - That code remained partially dormant, until other bugs were resolved
   for PUP-5684 in 7e176bfb3adf53d234d893f8481f576e3a2e6a0b and user
   User / Group began to more actively use the WinNT monikers in the
   exists? checks

 - On non-US versions of Windows, the "domains" BUILTIN and
   NT AUTHORITY may be localized.  For instance, on German Windows
   BUILTIN is localized as VORDEFINIERT and NT AUTHORITY is localized
   as NT-AUTORITÄ.  On French Windows, NT AUTHORITY is localized
   as AUTORITE NT, but BUILTIN remains the same.

   The moniker construction code has never taken this into account
   previously, so when trying to add a User to the Administrators
   group in German Windows, this would fail (this issue in particular
   was likely introduced in bd3f3a18fa4b5ae1f1f26d8aabf74edd18598746):

   Given a manifest:

   user { 'bla':
    ensure  => 'present',
    comment => 'bla',
    groups  => ['S-1-5-32-544',],
   }

   Would generate:

lib/puppet/util/windows/adsi.rb
@@ -107,8 +107,18 @@ module Puppet::Util::Windows::ADSI
   end
 
   module Shared
+    def localized_domains
+      @localized_domains ||= [
+        # localized version of BUILTIN
+        # for instance VORDEFINIERT on German Windows
+        Puppet::Util::Windows::SID.sid_to_name('S-1-5-32'),
+        # localized version of NT AUTHORITY (can't use S-1-5)
+        # for instance AUTORITE NT on French Windows
+        Puppet::Util::Windows::SID.name_to_sid_object('SYSTEM').domain
+    ]
+  end
     def uri(name, host = '.')
-      host = '.' if ['NT AUTHORITY', 'BUILTIN', Socket.gethostname].include?(host)
+      host = '.' if (localized_domains << Socket.gethostname).include?(host)
 
       # group or user
       account_type = self.name.split('::').last.downcase

spec/unit/util/windows/adsi_spec.rb
@@ -6,6 +6,10 @@ require 'puppet/util/windows'
 
 describe Puppet::Util::Windows::ADSI, :if => Puppet.features.microsoft_windows? do
   let(:connection) { stub 'connection' }
+  let(:builtin_localized) { Puppet::Util::Windows::SID.sid_to_name('S-1-5-32') }
+  # SYSTEM is special as English can retrieve it via Windows API
+  # but will return localized names
+  let(:ntauthority_localized) { Puppet::Util::Windows::SID::Principal.lookup_account_name('SYSTEM').domain }
 
   before(:each) do
     Puppet::Util::Windows::ADSI.instance_variable_set(:@computer_name, 'testcomputername')
@@ -65,6 +69,14 @@ describe Puppet::Util::Windows::ADSI, :if => Puppet.features.microsoft_windows? 
 
     it "should generate the correct URI for a user with a domain" do
       expect(Puppet::Util::Windows::ADSI::User.uri(username, domain)).to eq("WinNT://#{domain}/#{username},user")
+    end
+
+    it "should generate the correct URI for a BUILTIN user" do
+      expect(Puppet::Util::Windows::ADSI::User.uri(username, builtin_localized)).to eq("WinNT://./#{username},user")
+    end
+
+    it "should generate the correct URI for a NT AUTHORITY user" do
+      expect(Puppet::Util::Windows::ADSI::User.uri(username, ntauthority_localized)).to eq("WinNT://./#{username},user")
     end
 
     it "should be able to parse a username without a domain" do
@@ -422,6 +434,14 @@ describe Puppet::Util::Windows::ADSI, :if => Puppet.features.microsoft_windows? 
 
     it "should generate the correct URI" do
       expect(Puppet::Util::Windows::ADSI::Group.uri("people")).to eq("WinNT://./people,group")
+    end
+
+    it "should generate the correct URI for a BUILTIN group" do
+      expect(Puppet::Util::Windows::ADSI::Group.uri(groupname, builtin_localized)).to eq("WinNT://./#{groupname},group")
+    end
+
+    it "should generate the correct URI for a NT AUTHORITY group" do
+      expect(Puppet::Util::Windows::ADSI::Group.uri(groupname, ntauthority_localized)).to eq("WinNT://./#{groupname},group")
     end
 
     it "should be able to create a group" do