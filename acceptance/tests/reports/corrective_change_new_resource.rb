b643f5f66dc49367a844cd333022bc78e92ba212
Sheena T <sheena@puppet.com>
2016-08-09 15:08:57 +0000

    (PUP-6295) Acceptance test for corrective_change flag
    Thie commit adds new acceptance test for corrective_change flag
    
    [skip ci]

---
+++ acceptance/tests/reports/corrective_change_new_resource.rb
@@ -0,0 +1,100 @@
+require 'yaml'
+
+test_name "C98092: corrective change new resource" do
+require 'puppet/acceptance/environment_utils'
+extend Puppet::Acceptance::EnvironmentUtils
+
+  test_file_name = File.basename(__FILE__, '.*')
+  tmp_environment   = mk_tmp_environment_with_teardown(master, test_file_name)
+  tmp_file = {}
+  
+  agents.each do |agent|
+    tmp_file[agent.hostname] = agent.tmpfile(tmp_environment)
+  end
+
+  teardown do
+    step 'clean out produced resources' do
+      agents.each do |agent|
+       if tmp_file.has_key?(agent.hostname) && tmp_file[agent.hostname] != ''  
+        on(agent, "rm #{tmp_file[agent.hostname]}", :accept_all_exit_codes => true)
+      end 
+     end
+    end
+  end
+
+  step 'create file resource - site.pp to verify corrective change flag' do
+  file_contents     = 'this is a test'   
+manifest = <<MANIFEST
+file { '#{environmentpath}/#{tmp_environment}/manifests/site.pp':
+  ensure => file,
+  content => '
+\$test_path = \$::fqdn ? #{tmp_file}
+file { \$test_path:
+  content => @(UTF8)
+    #{file_contents}
+    | UTF8 
+}
+  ',
+}
+MANIFEST
+    apply_manifest_on(master, manifest, :catch_failures => true)
+  end
+
+  step 'run agent(s)' do
+    with_puppet_running_on(master, {}) do
+      agents.each do |agent|
+        fqdn = agent.hostname
+
+        #Run agent once to create new File resource
+        step 'Run agent once to create new File resource' do
+         on(agent, puppet("agent -t --environment #{tmp_environment} --server #{master.hostname}"),:acceptable_exit_codes => 2)
+        end
+
+        #Verify the file resource is created
+        step 'Verify the file resource is created' do
+         on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_result|
+           assert_equal(file_contents, file_result, 'file contents did not match accepted')
+         end
+        end
+
+        #Delete the file
+        step 'Delete the file' do
+         on(agent, "rm #{tmp_file[fqdn]}", :accept_all_exit_codes => true)
+        end
+
+        #Run agent to correct the file's absence
+        step 'Run agent to correct the files absence' do
+         on(agent, puppet("agent -t --environment #{tmp_environment} --server #{master.hostname}"),:acceptable_exit_codes => 2)
+        end
+ 
+        #Verify the file resource is created
+        step 'Verify the file resource is created' do
+         on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_result|
+           assert_equal(file_contents, file_result, 'file contents did not match accepted')
+         end
+        end
+      end
+    end
+  end
+
+  # Open last_run_report.yaml
+  step 'Check report' do
+     agents.each do |agent|
+       on(agent, puppet('config print statedir')) do |command_result|
+         report_path = command_result.stdout.chomp + '/last_run_report.yaml'
+         on(agent, "cat #{report_path}") do |report_yaml|
+
+           #stripping out ruby class definitions from the report yaml
+           report_yaml = YAML.load(report_yaml.stdout.gsub(/\!ruby[^\n]*/, ''))
+           report_yaml.inspect
+           file_resource_details = report_yaml["resource_statuses"]["File[#{tmp_file[agent.hostname]}]"]
+           assert(file_resource_details.has_key?("corrective_change"),'corrective_change key is missing')
+           corrective_change_value =  file_resource_details["corrective_change"]
+           assert_equal(true, corrective_change_value, 'corrective_change flag should be true') 
+         end
+       end
+     end
+  end
+
+
+end
