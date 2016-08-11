8862d08f2839ce1f20edd32f6e0887ec93635fa8
Moses Mendoza <mendoza.moses@gmail.com>
2016-08-11 15:12:51 -0700

    (PUP-6619) Add test for version file
    This commit adds a new test to ensure the presence of the version file
    as laid down by the AIO in accordance with the puppet agent path specification
    after a recent set of changes to packaging of the AIO impacted the VERSION file
    creation and a regression was discovered in associated logic. This is added as
    a new test (as opposed to an additional step in 'ensure_puppet-agent_paths') to
    take advantage of the benefits of `skip_test` since this test only applies to
    packaged artifacts and not git/gem which don't create a VERSION file.
    
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

---
+++ acceptance/tests/ensure_version_file.rb
@@ -0,0 +1,30 @@
+require 'puppet/acceptance/temp_file_utils'
+extend Puppet::Acceptance::TempFileUtils
+
+# ensure a version file is created according to the puppet-agent path specification:
+# https://github.com/puppetlabs/puppet-specifications/blob/master/file_paths.md
+
+test_name 'PA-466: Ensure version file is created on agent' do
+
+  skip_test 'requires version file which is created by AIO' if [:gem, :git].include?(@options[:type])
+
+  step "test for existence of version file" do
+    agents.each do |agent|
+      platform = agent[:platform]
+      ruby_arch = agent[:ruby_arch] || 'x86' # ruby_arch defaults to x86 if nil
+
+      if platform =~ /windows/
+        version_file = platform =~ /-64$/ && ruby_arch == 'x86' ?
+          "C:/Program Files (x86)/Puppet Labs/Puppet/VERSION" :
+          "C:/Program Files/Puppet Labs/Puppet/VERSION"
+      else
+        version_file = "/opt/puppetlabs/puppet/VERSION"
+      end
+
+      if !file_exists?(agent, version_file)
+        fail_test("Failed to find version file #{version_file} on agent #{agent}")
+      end
+    end
+  end
+end
+
