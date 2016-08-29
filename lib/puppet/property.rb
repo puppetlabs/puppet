puppetlabs/puppet

commit b941773cf0d138a12be68bb5cb718221ed38795c

(maint) Correct usage of log_exception in property.rb
We were using a four argument log_exception instead of a 3. This fixes the problem
and puts in a test for the condition.

Signed-off-by: Ken Barber <ken@bob.sh>

lib/puppet/property.rb
@@ -387,7 +387,7 @@ class Puppet::Property < Puppet::Parameter
     rescue => detail
       # Certain operations may fail, but we don't want to fail the transaction if we can
       # avoid it
-      Puppet.log_exception(detail, "Unknown failure comparing values #{should} and #{is} using insync? on type: #{self.resource.ref} property: #{self.name}", :default, { :level => :info })
+      Puppet.log_exception(detail, "Unknown failure comparing values #{should} and #{is} using insync? on type: #{self.resource.ref} property: #{self.name}", { :level => :info })
 
       # Return nil, ie. unknown
       nil

spec/unit/property_spec.rb
@@ -555,4 +555,11 @@ describe Puppet::Property do
       expect(property.property_matches?(1, "1")).to be_falsey
     end
   end
+
+  describe "#insync_values?" do
+    it "should log an exception when insync? throws one" do
+      property.expects(:insync?).raises ArgumentError
+      expect(property.insync_values?("foo","bar")).to be nil
+    end
+  end
 end

commit c1b162c1749904a948c13b495a9742f8e74cfe99

(PUP-6629) Emit corrective change messages at info
 - The changes made in commits fc9e5b02de9c3876abaafbf879b77423c4d74173
   and b941773cf0d138a12be68bb5cb718221ed38795c introduced exception
   logging around corrective changes.  When comparison for a Puppet
   property by way of insync_values? were performed, the intent was to
   propagate this to an end user.

   However, the code was written against a code path that didn't exist.

 - Instead of refactoring the logging code to allow an info level
   exception, emit an info message instead.

   This has the downside of not carrying stack trace info in the message,
   but has the upside of not changing any existing logging behavior.  A
   stack trace at this point is not as valuable as it might seem given
   there typically won't be any additional info to glean from it, other
   than the failure point.

   To make the message more greppable in the source code, restructure the
   message slightly.

  An example of an emmitted message that shows during a --trace is:

  Info: Unknown failure using insync_values? on type: Acl[c:/temp/REM_FI~2.TXT] / property: permissions to compare values [{}] and [
 { identity => 'NT AUTHORITY\SYSTEM', rights => ["full"], affects => 'self_only', is_inherited => 'true' },
 { identity => 'BUILTIN\Administrators', rights => ["full"], affects => 'self_only', is_inherited => 'true' },
 { identity => 'BUILTIN\Users', rights => ["read", "execute"], affects => 'self_only', is_inherited => 'true' }]

lib/puppet/property.rb
@@ -367,7 +367,8 @@ class Puppet::Property < Puppet::Parameter
     rescue => detail
       # Certain operations may fail, but we don't want to fail the transaction if we can
       # avoid it
-      Puppet.log_exception(detail, "Unknown failure comparing values #{should} and #{is} using insync? on type: #{self.resource.ref} property: #{self.name}", { :level => :info })
+      msg = "Unknown failure using insync_values? on type: #{self.resource.ref} / property: #{self.name} to compare values #{should} and #{is}"
+      Puppet.info(msg)
 
       # Return nil, ie. unknown
       nil