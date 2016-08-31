d52320478b8993c11a1c525646b3a4d15da91ea4
james-stocks <james.stocks@puppetlabs.com>
2016-08-31 10:53:13 +0000

    (maint) Fix formatting of corrective_change tests
    [skip ci]

--- acceptance/tests/reports/corrective_change_new_resource.rb
+++ acceptance/tests/reports/corrective_change_new_resource.rb
@@ -15,16 +15,16 @@ extend Puppet::Acceptance::EnvironmentUtils
   teardown do
     step 'clean out produced resources' do
       agents.each do |agent|
-       if tmp_file.has_key?(agent.hostname) && tmp_file[agent.hostname] != ''  
-        on(agent, "rm #{tmp_file[agent.hostname]}", :accept_all_exit_codes => true)
-      end 
-     end
+        if tmp_file.has_key?(agent.hostname) && tmp_file[agent.hostname] != ''  
+          on(agent, "rm #{tmp_file[agent.hostname]}", :accept_all_exit_codes => true)
+        end 
+      end
     end
   end
 
   step 'create file resource - site.pp to verify corrective change flag' do
-  file_contents     = 'this is a test'   
-manifest = <<MANIFEST
+    file_contents     = 'this is a test'   
+    manifest = <<MANIFEST
 file { '#{environmentpath}/#{tmp_environment}/manifests/site.pp':
   ensure => file,
   content => '
@@ -47,14 +47,14 @@ MANIFEST
 
         #Run agent once to create new File resource
         step 'Run agent once to create new File resource' do
-         on(agent, puppet("agent -t --environment #{tmp_environment} --server #{master.hostname}"),:acceptable_exit_codes => 2)
+          on(agent, puppet("agent -t --environment #{tmp_environment} --server #{master.hostname}"),:acceptable_exit_codes => 2)
         end
 
         #Verify the file resource is created
         step 'Verify the file resource is created' do
-         on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_result|
-           assert_equal(file_contents, file_result, 'file contents did not match accepted')
-         end
+          on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_result|
+            assert_equal(file_contents, file_result, 'file contents did not match accepted')
+          end
         end
       end
     end
@@ -62,30 +62,29 @@ MANIFEST
 
   # Open last_run_report.yaml
   step 'Check report' do
-     agents.each do |agent|
-       on(agent, puppet('config print statedir')) do |command_result|
-         report_path = command_result.stdout.chomp + '/last_run_report.yaml'
-         on(agent, "cat #{report_path}").stdout do |report_contents|
+    agents.each do |agent|
+      on(agent, puppet('config print statedir')) do |command_result|
+        report_path = command_result.stdout.chomp + '/last_run_report.yaml'
+        on(agent, "cat #{report_path}").stdout do |report_contents|
 
-           yaml_data = YAML::parse(report_contents)
-           # Remove any Ruby class tags from the yaml
-           yaml_data.root.each do |o|
-             if o.respond_to?(:tag=) and
-                o.tag != nil and
-                o.tag.start_with?("!ruby")
-               o.tag = nil
-             end
-           end
-           report_yaml = yaml_data.to_ruby
+          yaml_data = YAML::parse(report_contents)
+          # Remove any Ruby class tags from the yaml
+          yaml_data.root.each do |o|
+            if o.respond_to?(:tag=) and
+               o.tag != nil and
+               o.tag.start_with?("!ruby")
+              o.tag = nil
+            end
+          end
+          report_yaml = yaml_data.to_ruby
 
-           file_resource_details = report_yaml["resource_statuses"]["File[#{tmp_file[agent.hostname]}]"]
-           assert(file_resource_details.has_key?("corrective_change"),'corrective_change key is missing')
-           corrective_change_value =  file_resource_details["corrective_change"]
-           assert_equal(false, corrective_change_value, 'corrective_change flag should be false') 
-         end
-       end
-     end
+          file_resource_details = report_yaml["resource_statuses"]["File[#{tmp_file[agent.hostname]}]"]
+          assert(file_resource_details.has_key?("corrective_change"),'corrective_change key is missing')
+          corrective_change_value =  file_resource_details["corrective_change"]
+          assert_equal(false, corrective_change_value, 'corrective_change flag should be false')
+        end
+      end
+    end
   end
-
 
 end
--- acceptance/tests/reports/corrective_change_via_puppet.rb
+++ acceptance/tests/reports/corrective_change_via_puppet.rb
@@ -1,15 +1,15 @@
 require 'yaml'
-
-test_name "C98094 - a resource changed via Puppet manifest will not be reported as a corrective change" do
 require 'puppet/acceptance/environment_utils'
 extend Puppet::Acceptance::EnvironmentUtils
+
+test_name "C98094 - a resource changed via Puppet manifest will not be reported as a corrective change" do
 
   test_file_name = File.basename(__FILE__, '.*')
   tmp_environment = mk_tmp_environment_with_teardown(master, test_file_name)
   tmp_file = {}
   original_test_data = 'this is my original important data'
   modified_test_data = 'this is my modified important data'
-  
+ 
   agents.each do |agent|
     tmp_file[agent.hostname] = agent.tmpfile(tmp_environment)
   end
@@ -17,10 +17,10 @@ extend Puppet::Acceptance::EnvironmentUtils
   teardown do
     step 'clean out produced resources' do
       agents.each do |agent|
-       if tmp_file.has_key?(agent.hostname) && tmp_file[agent.hostname] != ''  
-        on(agent, "rm #{tmp_file[agent.hostname]}", :accept_all_exit_codes => true)
-      end 
-     end
+        if tmp_file.has_key?(agent.hostname) && tmp_file[agent.hostname] != ''
+          on(agent, "rm #{tmp_file[agent.hostname]}", :accept_all_exit_codes => true)
+        end
+      end
     end
   end
 
@@ -69,9 +69,9 @@ MANIFEST
         end
 
         step 'Verify the file resource is created' do
-         on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_contents|
-           assert_equal(modified_test_data, file_contents, 'file contents did not match expected contents')
-         end
+          on(agent, "cat #{tmp_file[fqdn]}").stdout do |file_contents|
+            assert_equal(modified_test_data, file_contents, 'file contents did not match expected contents')
+          end
         end
       end
     end
@@ -79,27 +79,27 @@ MANIFEST
 
   # Open last_run_report.yaml
   step 'Check report' do
-     agents.each do |agent|
-       on(agent, puppet('config print statedir')) do |command_result|
-         report_path = command_result.stdout.chomp + '/last_run_report.yaml'
-         on(agent, "cat #{report_path}").stdout do |report_contents|
+    agents.each do |agent|
+      on(agent, puppet('config print statedir')) do |command_result|
+        report_path = command_result.stdout.chomp + '/last_run_report.yaml'
+        on(agent, "cat #{report_path}").stdout do |report_contents|
 
-           yaml_data = YAML::parse(report_contents)
-           # Remove any Ruby class tags from the yaml
-           yaml_data.root.each do |o|
-             if o.respond_to?(:tag=) and
-                o.tag != nil and
-                o.tag.start_with?("!ruby")
+          yaml_data = YAML::parse(report_contents)
+          # Remove any Ruby class tags from the yaml
+          yaml_data.root.each do |o|
+            if o.respond_to?(:tag=) and
+               o.tag != nil and
+               o.tag.start_with?("!ruby")
                o.tag = nil
-             end
-           end
-           report_yaml = yaml_data.to_ruby
-           file_resource_details = report_yaml["resource_statuses"]["File[#{tmp_file[agent.hostname]}]"]
-           assert(file_resource_details.has_key?("corrective_change"),'corrective_change key is missing')
-           corrective_change_value =  file_resource_details["corrective_change"]
-           assert_equal(false, corrective_change_value, 'corrective_change flag for the changed resource should be false') 
-         end
-       end
-     end
+            end
+          end
+          report_yaml = yaml_data.to_ruby
+          file_resource_details = report_yaml["resource_statuses"]["File[#{tmp_file[agent.hostname]}]"]
+          assert(file_resource_details.has_key?("corrective_change"),'corrective_change key is missing')
+          corrective_change_value =  file_resource_details["corrective_change"]
+          assert_equal(false, corrective_change_value, 'corrective_change flag for the changed resource should be false')
+        end
+      end
+    end
   end
 end
