4ad7717fbf0453466a62af1b49a776afd93a5bb9
Moses Mendoza <moses@puppet.com>
2016-10-27 10:04:00 -0700

    (PUP-7134) Use Puppet::FileSystem in ssl
    This commit updates the various calls to File.read throughout
    Puppet::SSL to instead call their Puppet::FileSystem counterpart, to
    further centralize control of file system access in our library with
    the advantages it affords.
    
    The second aspect of this commit specifes the :encoding attribute
    wherever files are accessed, so that a character set is explicitly
    enforced that is compatible with SSL related files on disk, such as
    .pem, .csr, etc - at relevant call sites it's documented what IETF or
    other specs cover the encoding of a given document.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>
                   Ethan J. Brown <ethan@puppet.com>

--- spec/unit/indirector/ssl_file_spec.rb
+++ spec/unit/indirector/ssl_file_spec.rb
@@ -221,7 +221,7 @@ describe Puppet::Indirector::SslFile do
           @searcher.class.store_in @setting
           fh = mock 'filehandle'
           fh.stubs :print
-          Puppet.settings.setting(@setting).expects(:open_file).with(@certpath, 'w').yields fh
+          Puppet.settings.setting(@setting).expects(:open_file).with(@certpath, 'w:ASCII').yields fh
 
           @searcher.save(@request)
         end
@@ -233,7 +233,7 @@ describe Puppet::Indirector::SslFile do
 
           fh = mock 'filehandle'
           fh.stubs :print
-          Puppet.settings.setting(@setting).expects(:open).with('w').yields fh
+          Puppet.settings.setting(@setting).expects(:open).with('w:ASCII').yields fh
           @searcher.save(@request)
         end
       end
@@ -246,7 +246,7 @@ describe Puppet::Indirector::SslFile do
 
           fh = mock 'filehandle'
           fh.stubs :print
-          Puppet.settings.setting(:cakey).expects(:open).with('w').yields fh
+          Puppet.settings.setting(:cakey).expects(:open).with('w:ASCII').yields fh
           @searcher.stubs(:ca?).returns true
           @searcher.save(@request)
         end
