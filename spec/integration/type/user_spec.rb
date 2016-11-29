dfc90cbe239001f282cbc6e3caf0307c75126d09
Iristyle <Iristyle@github>
2016-11-29 10:19:20 -0800

    (PUP-7134) UTF-8 test user type authorized_keys
     - Add tests demonstrating that SSH key manipulations for user
       authorized_keys files work properly when UTF-8 comments are present

--- spec/integration/type/user_spec.rb
+++ spec/integration/type/user_spec.rb
@@ -8,9 +8,16 @@ describe Puppet::Type.type(:user), '(integration)', :unless => Puppet.features.m
   include PuppetSpec::Compiler
 
   context "when set to purge ssh keys from a file" do
+    # different UTF-8 widths
+    # 1-byte A
+    # 2-byte ۿ - http://www.fileformat.info/info/unicode/char/06ff/index.htm - 0xDB 0xBF / 219 191
+    # 3-byte ᚠ - http://www.fileformat.info/info/unicode/char/16A0/index.htm - 0xE1 0x9A 0xA0 / 225 154 160
+    # 4-byte 𠜎 - http://www.fileformat.info/info/unicode/char/2070E/index.htm - 0xF0 0xA0 0x9C 0x8E / 240 160 156 142
+    let (:mixed_utf8) { "A\u06FF\u16A0\u{2070E}" } # Aۿᚠ𠜎
+
     let(:tempfile) do
       file_containing('user_spec', <<-EOF)
-        # comment
+        # comment #{mixed_utf8}
         ssh-rsa KEY-DATA key-name
         ssh-rsa KEY-DATA key name
         EOF
@@ -22,12 +29,12 @@ describe Puppet::Type.type(:user), '(integration)', :unless => Puppet.features.m
 
     it "should purge authorized ssh keys" do
       apply_compiled_manifest(manifest)
-      expect(File.read(tempfile)).not_to match(/key-name/)
+      expect(File.read(tempfile, :encoding => Encoding::UTF_8)).not_to match(/key-name/)
     end
 
     it "should purge keys with spaces in the comment string" do
       apply_compiled_manifest(manifest)
-      expect(File.read(tempfile)).not_to match(/key name/)
+      expect(File.read(tempfile, :encoding => Encoding::UTF_8)).not_to match(/key name/)
     end
 
     context "with other prefetching resources evaluated first" do
@@ -35,14 +42,14 @@ describe Puppet::Type.type(:user), '(integration)', :unless => Puppet.features.m
 
       it "should purge authorized ssh keys" do
         apply_compiled_manifest(manifest)
-        expect(File.read(tempfile)).not_to match(/key-name/)
+        expect(File.read(tempfile, :encoding => Encoding::UTF_8)).not_to match(/key-name/)
       end
     end
 
     context "with multiple unnamed keys" do
       let(:tempfile) do
         file_containing('user_spec', <<-EOF)
-          # comment
+          # comment #{mixed_utf8}
           ssh-rsa KEY-DATA1
           ssh-rsa KEY-DATA2
           EOF
@@ -50,7 +57,7 @@ describe Puppet::Type.type(:user), '(integration)', :unless => Puppet.features.m
 
       it "should purge authorized ssh keys" do
         apply_compiled_manifest(manifest)
-        expect(File.read(tempfile)).not_to match(/KEY-DATA/)
+        expect(File.read(tempfile, :encoding => Encoding::UTF_8)).not_to match(/KEY-DATA/)
       end
     end
   end
