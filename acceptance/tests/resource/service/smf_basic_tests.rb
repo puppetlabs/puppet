b539fe581f372514e28f4c7e095d81dc8b0f500c
Moses Mendoza <moses@puppet.com>
2016-12-06 16:33:50 -0800

    (maint) (PUP-6767) remove redundant smf acceptance testing
    Prior to this commit, SMF testing was unnecessarily broken out into multiple
    files each with redundant setup and teardown steps, and in many cases actually
    testing the exact same thing.
    
    This commit merges these tests into the smf_basic_tests.rb file, with no impact
    to test value but plenty of time saved.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- acceptance/tests/resource/service/smf_basic_tests.rb
+++ acceptance/tests/resource/service/smf_basic_tests.rb
@@ -11,30 +11,35 @@ teardown do
   end
 end
 
-
 agents.each do |agent|
   clean agent, :service => 'tstapp'
   manifest, method = setup agent, :service => 'tstapp'
-  step "SMF: clean slate"
-  apply_manifest_on(agent, 'service {tstapp : ensure=>stopped}') do
-    assert_match( /.*/, result.stdout, "err: #{agent}")
-  end
 
   step "SMF: ensure it is created with a manifest"
   apply_manifest_on(agent, 'service {tstapp : ensure=>running, manifest=>"%s"}' % manifest) do
     assert_match( /defined 'ensure' as 'running'/, result.stdout, "err: #{agent}")
   end
-  step "SMF: verify it with puppet"
-  on(agent, puppet("resource service tstapp")) do
-    assert_match( /ensure => 'running'/, result.stdout, "err: #{agent}")
-  end
-
+  
   step "SMF: verify with svcs that the service is online"
   on agent, "svcs -l application/tstapp" do
     assert_match( /state\s+online/, result.stdout, "err: #{agent}")
   end
-  step "SMF: stop the service"
+  
+  step "SMF: ensure it is idempotent - ie not created again"
+  apply_manifest_on(agent, 'service {tstapp : ensure=>running, manifest=>"%s"}' % manifest, :catch_changes => true)
+
+  step "SMF: ensure you can query the service with the ral"
+  on(agent, puppet("resource service tstapp")) do
+    assert_match( /ensure => 'running'/, result.stdout, "err: #{agent}")
+  end
+
+  step "SMF: ensure you can stop the service"
   apply_manifest_on(agent, 'service {tstapp : ensure=>stopped}') do
     assert_match( /changed 'running'.* to 'stopped'/, result.stdout, "err: #{agent}")
+  end
+
+  step "SMF: verify with svcs that the service is not online"
+  on agent, "svcs -l application/tstapp", :acceptable_exit_codes => [0,1] do
+    assert_no_match( /state\s+online/, result.stdout, "err: #{agent}")
   end
 end
