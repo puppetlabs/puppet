b304481dca29c4268b851acf97a3fe4979d840bc
Iristyle <Iristyle@github>
2016-12-09 14:50:30 -0800

    (PUP-6993) Fix EventLog test on Japanese Windows
     - Calculate the datetime value as a long form numeric value that
       should not be subject to locale specific formatting concerns.
    
     - Adjust the eventlog string that's being searched for to not include
       Puppet messages (which may later be localized) - but to search for
       error messages propagated from Ruby and its underlying libraries,
       since these are not localized at this time.

--- acceptance/tests/windows/eventlog.rb
+++ acceptance/tests/windows/eventlog.rb
@@ -10,7 +10,9 @@ extend Puppet::Acceptance::WindowsUtils
 
 agents.each do |agent|
   # get remote time
-  now = on(agent, "#{ruby_command(agent)} -e \"puts Time.now.utc.strftime('%m/%d/%Y %H:%M:%S')\"").stdout.chomp
+  # https://msdn.microsoft.com/en-us/library/aa394226(v=vs.85).aspx
+  # set Microsecond and time zone offset both to 0
+  now = on(agent, "#{ruby_command(agent)} -e \"puts Time.now.utc.strftime('%Y%m%d%H%M%S.000000-000')\"").stdout.chomp
 
   # generate an error, no master on windows boxes
   # we use `agent` because it creates an eventlog log destination by default,
@@ -21,6 +23,6 @@ agents.each do |agent|
   # cygwin + ssh + wmic hangs trying to read stdin, so echo '' |
   on agent, "cmd /c echo '' | wmic ntevent where \"LogFile='Application' and SourceName='Puppet' and TimeWritten >= '#{now}'\"  get Message,Type /format:csv" do
     fail_test "Event not found in Application event log" unless
-      stdout =~ /Could not retrieve catalog.*skipping run.*,Error/mi
+      stdout.include?('target machine actively refused it. - connect(2) for "127.0.0.1"')
   end
 end
