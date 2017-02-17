f2a905930ce622b0c04c83792c01a8bcaae115fb
Thomas Hallgren <thomas@tada.se>
2017-02-17 12:50:28 +0100

    (PUP-7224) Add default #_pall_contents and #_pcontents to PuppetObject
    Adds sane default methods to the `PuppetObject` so that these methods
    doesn't need to be generated for types that have no content.

--- lib/puppet/pops/types/puppet_object.rb
+++ lib/puppet/pops/types/puppet_object.rb
@@ -11,6 +11,16 @@ module PuppetObject
   def _ptype
     self.class._ptype
   end
+
+  def _pall_contents(path, &block)
+  end
+
+  def _pcontents
+  end
+
+  def i12n_hash
+    EMPTY_HASH
+  end
 end
 end
 end
