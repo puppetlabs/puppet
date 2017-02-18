b34b720dca27fc72e37d9d9739ebef424010f7be
Thomas Hallgren <thomas@tada.se>
2017-02-18 14:42:48 +0100

    (PUP-7225) Remove the Binder module, classes, and related tests.
    This commit removes the Binder/Injector, corresponding tests, and all
    references.

--- lib/puppet/plugins/configuration.rb
+++ lib/puppet/plugins/configuration.rb
@@ -11,37 +11,6 @@ module Puppet::Plugins
     require 'puppet/syntax_checkers/base64'
     require 'puppet/syntax_checkers/json'
 
-    # Extension-points are registered here:
-    #
-    # - If in a Ruby submodule it is best to create it here
-    # - The class does not have to be required; it will be auto required when the binder
-    #   needs it.
-    # - If the extension is a multibind, it can be registered here; either with a required
-    #   class or a class reference in string form.
-
-    schemes_name = Puppet::Plugins::BindingSchemes::BINDINGS_SCHEMES_KEY
-    schemes_type = Puppet::Plugins::BindingSchemes::BINDINGS_SCHEMES_TYPE
-
-    # Register extension points
-    # -------------------------
-    system_bindings = ::Puppet::Pops::Binder::SystemBindings
-    system_bindings.extensions().multibind(schemes_name).name(schemes_name).hash_of(schemes_type)
-
-    # Register injector boot bindings
-    # -------------------------------
-    boot_bindings = system_bindings.injector_boot_bindings()
-
-    # Register the default bindings scheme handlers
-    { 'module'        => 'ModuleScheme',
-      'confdir'       => 'ConfdirScheme',
-    }.each do |scheme, class_name|
-      boot_bindings \
-        .bind.name(scheme) \
-        .instance_of(schemes_type) \
-        .in_multibind(schemes_name) \
-        .to_instance("Puppet::Pops::Binder::SchemeHandler::#{class_name}")
-    end
-
     def self.load_plugins
       # Register extensions
       # -------------------
