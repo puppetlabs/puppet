b5d50dee0549d5c652ebe5b0450be7776ff1c3fe
Sean Griffin <sean@puppetlabs.com>
2017-02-22 17:34:18 -0800

    (PUP-7243) Don't start master since it's not needed
    This commit removes the unnecessary
      with_puppet_running_on do
      end
    block.

--- acceptance/tests/utf8/utf8-in-function-args.rb
+++ acceptance/tests/utf8/utf8-in-function-args.rb
@@ -18,80 +18,79 @@ test_name 'utf-8 characters in function parameters' do
       'environmentpath' => "#{master_lookup_test_dir}/environments",
     },
   }
-  with_puppet_running_on(master, master_opts, master_lookup_test_dir) do
-    agents.each do |agent|
-      step 'alert' do
-        result = on(
-          agent,
-          puppet("apply", "-e" "'alert(\"alert #{utf8chars}\")'"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(
-          /#{utf8chars}/,
-          result.stderr,
-          "Did not find the utf8 chars."
-        )
-      end
-    
-      step 'assert_type' do
-        on(
-          agent,
-          puppet(
-            "apply", "-e", "'notice(assert_type(String, \"#{utf8chars}\"))'"
-          ),
-          {
-            :environment => {:LANG => "en_US.UTF-8"},
-            :acceptable_exit_codes => [0],
-          }
-        )
-        on(
-          agent,
-          puppet("apply", "-e 'notice(assert_type(Float, \"#{utf8chars}\"))'"),
-          {
-            :environment => {:LANG => "en_US.UTF-8"},
-            :acceptable_exit_codes => [1],
-          }
-        )
-      end
-    
-      step 'filter' do
-        puppet_cmd = "'
-          [$a] = [[\"abc\", \"#{utf8chars}\", \"100\"]];
-          [$f] = [filter($a) |$p| {$p =~ /#{utf8chars}/}];
-          notice(\"f = $f\")
-        '"
-        result = on(
-          agent,
-          puppet("apply", "-e", puppet_cmd),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(/#{utf8chars}/, result.stdout, "filter() failed.")
-      end
-    
-      agent_lookup_test_dir =
-        (agent == master) ?
-        master_lookup_test_dir :
-        agent_lookup_test_dir = agent.tmpdir("lookup_test_dir")
+  agents.each do |agent|
+    step 'alert' do
+      result = on(
+        agent,
+        puppet("apply", "-e" "'alert(\"alert #{utf8chars}\")'"),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(
+        /#{utf8chars}/,
+        result.stderr,
+        "Did not find the utf8 chars."
+      )
+    end
+  
+    step 'assert_type' do
+      on(
+        agent,
+        puppet(
+          "apply", "-e", "'notice(assert_type(String, \"#{utf8chars}\"))'"
+        ),
+        {
+          :environment => {:LANG => "en_US.UTF-8"},
+          :acceptable_exit_codes => [0],
+        }
+      )
+      on(
+        agent,
+        puppet("apply", "-e 'notice(assert_type(Float, \"#{utf8chars}\"))'"),
+        {
+          :environment => {:LANG => "en_US.UTF-8"},
+          :acceptable_exit_codes => [1],
+        }
+      )
+    end
+  
+    step 'filter' do
+      puppet_cmd = "'
+        [$a] = [[\"abc\", \"#{utf8chars}\", \"100\"]];
+        [$f] = [filter($a) |$p| {$p =~ /#{utf8chars}/}];
+        notice(\"f = $f\")
+      '"
+      result = on(
+        agent,
+        puppet("apply", "-e", puppet_cmd),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(/#{utf8chars}/, result.stdout, "filter() failed.")
+    end
+  
+    agent_lookup_test_dir =
+      (agent == master) ?
+      master_lookup_test_dir :
+      agent_lookup_test_dir = agent.tmpdir("lookup_test_dir")
 
-      mod_name = "lookup_module"
-      mod_key = "#{mod_name}::mod_key_#{utf8chars}"
-      mod_val = "mod_val_#{utf8chars}"
-      env_key = "env_key_#{utf8chars}"
-      env_val = "env_val_#{utf8chars}"
-      array_key = "array_key_with_utf8_#{utf8chars}"
-      array_val_2 = "array value 2 with utf8 #{utf8chars}"
-      scalar_key = "scalar_key_with_utf8_#{utf8chars}"
-      scalar_val = "scalar value with utf8 #{utf8chars}"
-      non_key = "non_key_#{utf8chars}"
+    mod_name = "lookup_module"
+    mod_key = "#{mod_name}::mod_key_#{utf8chars}"
+    mod_val = "mod_val_#{utf8chars}"
+    env_key = "env_key_#{utf8chars}"
+    env_val = "env_val_#{utf8chars}"
+    array_key = "array_key_with_utf8_#{utf8chars}"
+    array_val_2 = "array value 2 with utf8 #{utf8chars}"
+    scalar_key = "scalar_key_with_utf8_#{utf8chars}"
+    scalar_val = "scalar value with utf8 #{utf8chars}"
+    non_key = "non_key_#{utf8chars}"
 
-      step 'apply hiera/lookup manifest' do
-        # I want the banner in the output but 
-        # some results: orig_hiera_config,
-        # orig_environmentpath from operations
-        # here are used later, so I don't want
-        # them local to a step block.
-      end
-      lookup_manifest =
+    step 'apply hiera/lookup manifest' do
+      # I want the banner in the output but 
+      # some results: orig_hiera_config,
+      # orig_environmentpath from operations
+      # here are used later, so I don't want
+      # them local to a step block.
+    end
+    lookup_manifest =
 
 <<LOOKUP_MANIFEST
 
@@ -180,159 +179,158 @@ file { "#{agent_lookup_test_dir}/hiera_data/common.yaml" :
 }
 
 LOOKUP_MANIFEST
-      apply_manifest_on(
-        agent, lookup_manifest, :environment => {:LANG => "en_US.UTF-8"}
-      )
+    apply_manifest_on(
+      agent, lookup_manifest, :environment => {:LANG => "en_US.UTF-8"}
+    )
+    result = on(
+      agent,
+      puppet("config", "print hiera_config"),
+      :environment => {:LANG => "en_US.UTF-8"}
+    )
+    orig_hiera_config = result.stdout.chomp
+
+    result = on(
+      agent,
+      puppet("config", "print environmentpath"),
+      :environment => {:LANG => "en_US.UTF-8"}
+    )
+    orig_environmentpath = result.stdout.chomp
+
+    on(
+      agent,
+      puppet(
+        "config",
+        "set hiera_config #{agent_lookup_test_dir}/hiera.yaml"
+      ),
+      :environment => {:LANG => "en_US.UTF-8"}
+    )
+    on(
+      agent,
+      puppet(
+        "config", "set environmentpath #{agent_lookup_test_dir}/environments"
+      ),
+      :environment => {:LANG => "en_US.UTF-8"}
+    )
+
+    step 'hiera' do
       result = on(
         agent,
-        puppet("config", "print hiera_config"),
+        puppet("apply", "-e", "'notice(hiera(\"#{array_key}\"))'"),
         :environment => {:LANG => "en_US.UTF-8"}
       )
-      orig_hiera_config = result.stdout.chomp
+      assert_match(/#{array_val_2}/, result.stdout, "hiera array lookup")
 
       result = on(
         agent,
-        puppet("config", "print environmentpath"),
+        puppet("apply", "-e", "'notice(hiera(\"#{scalar_key}\"))'"),
         :environment => {:LANG => "en_US.UTF-8"}
       )
-      orig_environmentpath = result.stdout.chomp
+      assert_match(/#{scalar_val}/, result.stdout, "hiera scalar lookup")
+
+      result = on(
+        agent,
+        puppet("apply", "-e", "'notice(hiera(\"#{non_key}\"))'"),
+        {
+          :acceptable_exit_codes => (0..255),
+          :environment => {:LANG => "en_US.UTF-8"}
+        }
+      )
+      assert_match(
+        /did not find a value for the name '#{non_key}'/,
+        result.stderr,
+        "hiera non_key lookup"
+      )
+    end
+
+    step 'lookup' do
+      result = on(
+        agent,
+        puppet("apply", "-e", "'notice(lookup(\"#{env_key}\"))'"),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(
+        /#{env_val}/,
+        result.stdout,
+        "env lookup failed for '#{env_key}'"
+      )
+
+      result = on(
+        agent,
+        puppet("apply", "-e", "'notice(lookup(\"#{mod_key}\"))'"),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(
+        /#{mod_val}/,
+        result.stdout,
+        "module lookup failed for '#{mod_key}'"
+      )
 
       on(
         agent,
-        puppet(
-          "config",
-          "set hiera_config #{agent_lookup_test_dir}/hiera.yaml"
-        ),
+        puppet("config", "set hiera_config #{orig_hiera_config}"),
         :environment => {:LANG => "en_US.UTF-8"}
       )
       on(
         agent,
-        puppet(
-          "config", "set environmentpath #{agent_lookup_test_dir}/environments"
-        ),
+        puppet("config", "set environmentpath #{orig_environmentpath}"),
         :environment => {:LANG => "en_US.UTF-8"}
       )
-
-      step 'hiera' do
-        result = on(
-          agent,
-          puppet("apply", "-e", "'notice(hiera(\"#{array_key}\"))'"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(/#{array_val_2}/, result.stdout, "hiera array lookup")
-  
-        result = on(
-          agent,
-          puppet("apply", "-e", "'notice(hiera(\"#{scalar_key}\"))'"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(/#{scalar_val}/, result.stdout, "hiera scalar lookup")
-  
-        result = on(
-          agent,
-          puppet("apply", "-e", "'notice(hiera(\"#{non_key}\"))'"),
-          {
-            :acceptable_exit_codes => (0..255),
-            :environment => {:LANG => "en_US.UTF-8"}
-          }
-        )
-        assert_match(
-          /did not find a value for the name '#{non_key}'/,
-          result.stderr,
-          "hiera non_key lookup"
-        )
-      end
-
-      step 'lookup' do
-        result = on(
-          agent,
-          puppet("apply", "-e", "'notice(lookup(\"#{env_key}\"))'"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(
-          /#{env_val}/,
-          result.stdout,
-          "env lookup failed for '#{env_key}'"
-        )
-  
-        result = on(
-          agent,
-          puppet("apply", "-e", "'notice(lookup(\"#{mod_key}\"))'"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(
-          /#{mod_val}/,
-          result.stdout,
-          "module lookup failed for '#{mod_key}'"
-        )
-  
-        on(
-          agent,
-          puppet("config", "set hiera_config #{orig_hiera_config}"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        on(
-          agent,
-          puppet("config", "set environmentpath #{orig_environmentpath}"),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-      end
-      
-      step 'dig' do
-        hash_string = "{
-          a => {
-            b => [
-              {
-                x => 10,
-                y => 20,
-              },
-              {
-                x => 100,
-                y => \"dig_result = #{utf8chars}\"
-              },
-            ]
-          }
-        }"
-        puppet_cmd = "'
-          [$v] = [#{hash_string}];
-          [$dig_result] = [dig($v, a, b, 1, y)];
-          notice($dig_result)
-        '"
-        result = on(
-          agent,
-          puppet("apply", "-e", puppet_cmd),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(
-          /dig_result = #{utf8chars}/,
-          result.stdout,
-          "dig() test failed."
-        )
-      end
-      
-      step 'match' do
-        strings = [
-          "string1_#{utf8chars}",
-          "string2_#{utf8chars}",
-          "string3_no-utf8",
-          "string4_no-utf8"
-        ]
-        puppet_cmd = "'
-          [$vec] = [#{strings}];
-          [$found] = [match($vec, /#{utf8chars}/)];
-          notice($found)
-        '"
-        result = on(
-          agent,
-          puppet("apply", "-e", puppet_cmd),
-          :environment => {:LANG => "en_US.UTF-8"}
-        )
-        assert_match(
-          /[[#{utf8chars}], [#{utf8chars}], , ]/,
-          result.stdout,
-          "match() result unexpected"
-        )
-      end
+    end
+    
+    step 'dig' do
+      hash_string = "{
+        a => {
+          b => [
+            {
+              x => 10,
+              y => 20,
+            },
+            {
+              x => 100,
+              y => \"dig_result = #{utf8chars}\"
+            },
+          ]
+        }
+      }"
+      puppet_cmd = "'
+        [$v] = [#{hash_string}];
+        [$dig_result] = [dig($v, a, b, 1, y)];
+        notice($dig_result)
+      '"
+      result = on(
+        agent,
+        puppet("apply", "-e", puppet_cmd),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(
+        /dig_result = #{utf8chars}/,
+        result.stdout,
+        "dig() test failed."
+      )
+    end
+    
+    step 'match' do
+      strings = [
+        "string1_#{utf8chars}",
+        "string2_#{utf8chars}",
+        "string3_no-utf8",
+        "string4_no-utf8"
+      ]
+      puppet_cmd = "'
+        [$vec] = [#{strings}];
+        [$found] = [match($vec, /#{utf8chars}/)];
+        notice($found)
+      '"
+      result = on(
+        agent,
+        puppet("apply", "-e", puppet_cmd),
+        :environment => {:LANG => "en_US.UTF-8"}
+      )
+      assert_match(
+        /[[#{utf8chars}], [#{utf8chars}], , ]/,
+        result.stdout,
+        "match() result unexpected"
+      )
     end
   end
 end
