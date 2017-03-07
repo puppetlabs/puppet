a7b9d0e78787bb39d545c48afe32119e20d70dc8
Tom De Vylder <tom@penumbra.be>
2017-03-07 14:15:46 +0100

    (PUP-7312) Confine systemd provider to /lib/systemd/system
    /run/systemd/system does not exist when running Puppet in a chroot environment
    /lib/systemd/system is always available irregardless of the environment

--- spec/integration/provider/service/systemd_spec.rb
+++ spec/integration/provider/service/systemd_spec.rb
@@ -11,6 +11,11 @@ describe Puppet::Type.type(:service).provider(:systemd), '(integration)' do
     expect(described_class).to be_suitable
   end
 
+  it "should be considered suitable if /proc/1/exe is present and contains 'systemd'",
+    :if => File.exist?('/proc/1/exe') && File.open('/proc/1/exe').grep(/systemd/).size > 0
+    expect(provider_class).to be_suitable
+  end
+
   it "should not be cosidered suitable if systemctl is absent",
     :unless => (File.executable?('/bin/systemctl') or File.executable?('/usr/bin/systemctl')) do
     expect(described_class).not_to be_suitable
