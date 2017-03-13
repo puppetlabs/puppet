c6a87954533bf7787152222192d930b375e106e0
Maggie Dreyer <maggie@puppet.com>
2017-03-13 10:44:18 -0700

    (PUP-7310) Fix interpolation in externalized strings in reference

--- lib/puppet/reference/providers.rb
+++ lib/puppet/reference/providers.rb
@@ -56,21 +56,21 @@ providers = Puppet::Util::Reference.newreference :providers, :title => "Provider
         missing.each do |test, values|
           case test
           when :exists
-            details << _("  - Missing files #{values.join(", ")}\n")
+            details << _("  - Missing files %{files}\n") % { files: values.join(", ") }
           when :variable
             values.each do |name, facts|
               if Puppet.settings.valid?(name)
-                details << _("  - Setting #{name} (currently #{Puppet.settings.value(name).inspect}) not in list #{facts.join(", ")}\n")
+                details << _("  - Setting %{name} (currently %{value}) not in list %{facts}\n") % { name: name, value: Puppet.settings.value(name).inspect, facts: facts.join(", ") }
               else
-                details << _("  - Fact #{name} (currently #{Facter.value(name).inspect}) not in list #{facts.join(", ")}\n")
+                details << _("  - Fact %{name} (currently %{value}) not in list %{facts}\n") % { name: name, value: Facter.value(name).inspect, facts: facts.join(", ") }
               end
             end
           when :true
-            details << _("  - Got #{values} true tests that should have been false\n")
+            details << _("  - Got %{values} true tests that should have been false\n") % { values: values }
           when :false
-            details << _("  - Got #{values} false tests that should have been true\n")
+            details << _("  - Got %{values} false tests that should have been true\n") % { values: values }
           when :feature
-            details << _("  - Missing features #{values.collect { |f| f.to_s }.join(",")}\n")
+            details << _("  - Missing features %{values}\n") % { values: values.collect { |f| f.to_s }.join(",") }
           end
         end
         notes << details
