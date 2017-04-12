325d23e9b0d1fb4d4fb31f12684b0392bb234d76
Adrien Thebo <git@somethingsinistral.net>
2017-04-12 09:31:56 -0700

    (maint) separate setup steps for node_name_value test
    Separate auth rules setup and node classification setup steps.

--- acceptance/tests/allow_arbitrary_node_name_for_agent.rb
+++ acceptance/tests/allow_arbitrary_node_name_for_agent.rb
@@ -1,10 +1,11 @@
 test_name "node_name_value should be used as the node name for puppet agent"
 
 success_message = "node_name_value setting was correctly used as the node name"
-in_testdir = master.tmpdir('nodenamevalue')
+testdir = master.tmpdir('nodenamevalue')
 
-authfile = "#{in_testdir}/auth.conf"
-authconf = <<-AUTHCONF
+step "setup auth.conf rules" do
+  authfile = "#{testdir}/auth.conf"
+  authconf = <<-AUTHCONF
 path /puppet/v3/catalog/specified_node_name
 auth yes
 allow *
@@ -16,59 +17,62 @@ allow *
 path /puppet/v3/report/specified_node_name
 auth yes
 allow *
-AUTHCONF
+  AUTHCONF
 
-manifest_file = "#{in_testdir}/environments/production/manifests/manifest.pp"
-manifest = <<-MANIFEST
-  Exec { path => "/usr/bin:/bin" }
-  node default {
-    notify { "false": }
-  }
-  node specified_node_name {
-    notify { "#{success_message}": }
-  }
-MANIFEST
+  apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
+    file { '#{authfile}':
+      ensure => file,
+      mode => '0644',
+      content => '#{authconf}',
+    }
+  MANIFEST
+end
 
-apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
-  File {
-    ensure => directory,
-    mode => '0777',
-  }
+step "Setup site.pp for node name based classification" do
 
-  file {
-    '#{in_testdir}':;
-    '#{in_testdir}/environments':;
-    '#{in_testdir}/environments/production':;
-    '#{in_testdir}/environments/production/manifests':;
-  }
-
-  file { '#{manifest_file}':
-    ensure => file,
-    mode => '0644',
-    content => '#{manifest}',
-  }
-
-  file { '#{authfile}':
-    ensure => file,
-    mode => '0644',
-    content => '#{authconf}',
-  }
-MANIFEST
-
-with_these_opts = {
-  'main' => {
-    'environmentpath' => "#{in_testdir}/environments",
-  },
-  'master' => {
-    'rest_authconfig' => "#{in_testdir}/auth.conf",
-    'node_terminus'   => 'plain',
-  },
+  site_manifest = <<-SITE_MANIFEST
+node default {
+  notify { "false": }
 }
+node specified_node_name {
+  notify { "#{success_message}": }
+}
+  SITE_MANIFEST
 
-with_puppet_running_on master, with_these_opts, in_testdir do
+  apply_manifest_on(master, <<-MANIFEST, :catch_failures => true)
+    $directories = [
+      '#{testdir}',
+      '#{testdir}/environments',
+      '#{testdir}/environments/production',
+      '#{testdir}/environments/production/manifests',
+    ]
 
-  on(agents, puppet('agent', "-t --node_name_value specified_node_name --server #{master}"), :acceptable_exit_codes => [0,2]) do
-    assert_match(/defined 'message'.*#{success_message}/, stdout)
+    file { $directories:
+      ensure => directory,
+      mode => '0755',
+    }
+
+    file { '#{testdir}/environments/production/manifests/manifest.pp':
+      ensure => file,
+      mode => '0644',
+      content => '#{site_manifest}',
+    }
+  MANIFEST
+end
+
+step "Ensure nodes are classified based on the node name fact" do
+  master_opts = {
+    'main' => {
+      'environmentpath' => "#{testdir}/environments",
+    },
+    'master' => {
+      'rest_authconfig' => "#{testdir}/auth.conf",
+      'node_terminus'   => 'plain',
+    },
+  }
+  with_puppet_running_on(master, master_opts, testdir) do
+    on(agents, puppet('agent', "-t --node_name_value specified_node_name --server #{master}"), :acceptable_exit_codes => [0,2]) do
+      assert_match(/defined 'message'.*#{success_message}/, stdout)
+    end
   end
-
 end
