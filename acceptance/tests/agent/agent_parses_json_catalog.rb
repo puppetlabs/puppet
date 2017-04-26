e156dc5cf1488d6546210fc4d589233e68196606
Kurt Wall <kurt.wall@puppet.com>
2017-04-26 00:24:35 -0700

    (PUP-7357) Verfiy agent can parse a JSON catalog

---
+++ acceptance/tests/agent/agent_parses_json_catalog.rb
@@ -0,0 +1,23 @@
+test_name "C99978: Agent parses a JSON catalog"
+tag 'risk:medium'
+
+require 'json'
+
+step "Refresh the catalog" do
+  on(agents, puppet("agent --test --server #{master}"))
+end
+
+step "Agent parses a JSON catalog" do
+  agents.each do |agent|
+    # Is it there and can we read it?
+    json_catalog = File.join(agent.puppet['client_datadir'], 'catalog',
+                             "#{agent.puppet['certname']}.json")
+    on(agent, "[[ -r #{json_catalog} ]]", {:acceptable_exit_codes => [0]})
+
+    # The catalog file should be parseable JSON
+    on(agent, "cat #{json_catalog} | ruby -rjson -e 'JSON.parse(STDIN.read)'")
+
+    # Can the agent parse it as JSON?
+    on(agent, puppet("catalog find --terminus json --server #{master} > /dev/null"))
+  end
+end
