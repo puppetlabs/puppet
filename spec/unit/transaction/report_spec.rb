f2d097b0c98a3f3e120df9f71c4ced9657ca9010
Thomas Hallgren <thomas@tada.se>
2017-05-11 08:15:52 +0200

    (PUP-7381) Fix two faulty tests for report#to_yaml_properties
    Two tests that asserted the removal of properties when serializing
    using YAML where badly written. They both verified that a String was
    not included in the array returned by #to_yaml_properties. That array
    does however not contain Strings. It's a list of Symbols denoting
    attribute names.

--- spec/unit/transaction/report_spec.rb
+++ spec/unit/transaction/report_spec.rb
@@ -484,13 +484,13 @@ describe Puppet::Transaction::Report do
     it "should not include @external_times" do
       report = Puppet::Transaction::Report.new('apply')
       report.add_times('config_retrieval', 1.0)
-      expect(report.to_yaml_properties).not_to include('@external_times')
+      expect(report.to_yaml_properties).not_to include(:@external_times)
     end
 
     it "should not include @resources_failed_to_generate" do
       report = Puppet::Transaction::Report.new("apply")
       report.resources_failed_to_generate = true
-      expect(report.to_yaml_properties).not_to include('@resources_failed_to_generate')
+      expect(report.to_yaml_properties).not_to include(:@resources_failed_to_generate)
     end
   end
 
