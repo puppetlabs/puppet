018a26b2c4f8e822f5ceb9efdaa414629bba1db5
Charlie Sharpsteen <chuck@puppet.com>
2017-05-17 09:41:54 -0700

    (PUP-7558) Remove unmaintained editor extensions
    This commit removes two editor extensions that pre-date the Puppet 4 language:
    
      - ext/emacs/: No meaningful update since 2014
      - ext/vim/: No meaningful update since 2009
    
    Extensions such as this make more sense as separate projects so that updates
    can be shipped independently from the Puppet release cycle.

--- ext/emacs/puppet-mode-init.el
+++
@@ -1,6 +0,0 @@
-;;
-;; Setup puppet-mode for autoloading
-;;
-(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
-
-(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
