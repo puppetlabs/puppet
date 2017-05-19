108bb6f05a64230d254be3f5040248f63bfe4be5
Thomas Hallgren <thomas@tada.se>
2017-05-19 15:56:02 +0200

    Revert "Revert "Merge pull request #5825 from thallgren/issue/pup-7431/upgrade-vendored-semantic_puppet""
    This reverts commit 55c4aaab3647d96d6a4e184d8131dddde8f28059.

--- lib/puppet/vendor/semantic_puppet/lib/semantic_puppet.rb
+++ lib/puppet/vendor/semantic_puppet/lib/semantic_puppet.rb
@@ -1,14 +1,6 @@
-begin
-  require 'gettext-setup'
-rescue LoadError
-  def _(msg)
-    msg
-  end
-end
-
 module SemanticPuppet
-  if defined?(GettextSetup)
-    GettextSetup.initialize(File.absolute_path('semantic_puppet/locales', File.dirname(__FILE__)))
+  if Puppet::GETTEXT_AVAILABLE
+    GettextSetup.initialize(File.absolute_path('../locales', File.dirname(__FILE__)))
   end
 
   autoload :Version, 'semantic_puppet/version'
