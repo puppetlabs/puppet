42845b7b8815d0becb84298032b6fa36ef33bfa5
Iristyle <Iristyle@github>
2017-05-19 16:05:54 -0700

    (PUP-7579) Allow tags to contain Unicode words
      - The regex for tags was originally /^\w[-\w:]*$/ in a Luke commit
        from 10 years ago f98be4a7198326b26f1072c401b3e337f340db40
    
      - Over the years it has received updates to modernize it and has
        shuffled around.
    
      - 0badfce72bafc767bf3099718c63324f51108996 changed it from
    
        /^\w[-\w:.]*$/
    
        to
    
        /^[0-9A-Za-z_][0-9A-Za-z_:.-]*$/
    
        for performance reasons.
    
      - The concat module depends on tags, and as such requires that tags
        can contain valid paths.  The regex here is too restrictive for
        that purpose and should be returned to its original intent - word
        characters.
    
        However, it now needs to be updated to accept word characters in
        any language.
    
      - This now allows for the follow manifest to not cause an error:
    
      puppet apply -e 'notify { "hi": tag => "_メインディレクトリ_醸造所"  }'

--- lib/puppet/util/tagging.rb
+++ lib/puppet/util/tagging.rb
@@ -1,7 +1,7 @@
 require 'puppet/util/tag_set'
 
 module Puppet::Util::Tagging
-  ValidTagRegex = /\A[0-9A-Za-z_][0-9A-Za-z_:.-]*\Z/
+  ValidTagRegex = /\A[[:alnum:]_][[:alnum:]_:.-]*\Z/
 
   # Add a tag to the current tag set.
   # When a tag set is used for a scope, these tags will be added to all of
