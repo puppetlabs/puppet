6291f035a709235dc31e27fdaaaf903224b6bb89
Josh Cooper <josh@puppet.com>
2017-06-13 14:47:44 -0700

    (PUP-7602) Only support binary data in JSON for catalogs
    Prior to commit a7b14ba9, the base JSON terminus supported binary data
    because it used PSON for serialization. Commit a7b14ba9, switched it to
    using JSON instead, which had the side-effect of no longer supporting
    binary data in JSON documents.
    
    The one place where we want to support binary data is in the catalog,
    such as a file resource with a `content` parameter, see PUP-5584.
    
    This commit modifies the base JSON terminus so that it performs file I/O
    in binary mode, so that ruby does not transcode the contents to/from the
    default external encoding. But when converting between the textual
    string and JSON, it will continue to require valid UTF-8.
    
    To support the binary catalog use-case, this commit modifies the
    catalog-specific JSON terminus so that it can fallback to PSON if JSON
    conversion fails. The fallback case will occur if a catalog contains
    binary content and:
    
     * The server attempts to serialize to JSON in the HTTP response
     * The agent attempts to serialize its cached catalog
     * The agent attempts to deserialize its cached catalog
    
    The fallback case won't happen when an agent deserializes the HTTP
    response from the server, because the server will send `Content-Type:
    text/pson`, so the agent will know to expect PSON.
    
    We also use Encoding::BINARY instead of ASCII_8BIT as the former is
    preferred in puppet's codebase.

--- spec/unit/indirector/catalog/json_spec.rb
+++ spec/unit/indirector/catalog/json_spec.rb
@@ -1,12 +1,67 @@
 #! /usr/bin/env ruby
 require 'spec_helper'
+require 'puppet_spec/files'
 require 'puppet/resource/catalog'
 require 'puppet/indirector/catalog/json'
 
 describe Puppet::Resource::Catalog::Json do
-  # This is it for local functionality: we don't *do* anything else.
+  include PuppetSpec::Files
+
+  # This is it for local functionality
   it "should be registered with the catalog store indirection" do
     expect(Puppet::Resource::Catalog.indirection.terminus(:json)).
       to be_an_instance_of described_class
+  end
+
+  describe "when handling requests" do
+    let(:binary) { "\xC0\xFF".force_encoding(Encoding::BINARY) }
+    let(:key)    { 'foo' }
+    let(:file)   { subject.path(key) }
+    let(:catalog) do
+      catalog = Puppet::Resource::Catalog.new(key, Puppet::Node::Environment.create(:testing, []))
+      catalog.add_resource(Puppet::Resource.new(:file, '/tmp/a_file', :parameters => { :content => binary }))
+      catalog
+    end
+
+    before :each do
+      Puppet.run_mode.stubs(:master?).returns(true)
+      Puppet[:server_datadir] = tmpdir('jsondir')
+      FileUtils.mkdir_p(File.join(Puppet[:server_datadir], 'indirector_testing'))
+    end
+
+    def with_content(text)
+      FileUtils.mkdir_p(File.dirname(file))
+      File.binwrite(file, text)
+      yield if block_given?
+    end
+
+    it 'saves a catalog containing binary content' do
+      request = subject.indirection.request(:save, key, catalog)
+
+      subject.save(request)
+    end
+
+    it 'finds a catalog containing binary content' do
+      request = subject.indirection.request(:save, key, catalog)
+      subject.save(request)
+
+      request = subject.indirection.request(:find, key, nil)
+      parsed_catalog = subject.find(request)
+
+      content = parsed_catalog.resource(:file, '/tmp/a_file')[:content]
+      expect(content.bytes).to eq(binary.bytes)
+    end
+
+    it 'searches for catalogs contains binary content' do
+      request = subject.indirection.request(:save, key, catalog)
+      subject.save(request)
+
+      request = subject.indirection.request(:search, '*', nil)
+      parsed_catalogs = subject.search(request)
+
+      expect(parsed_catalogs.size).to eq(1)
+      content = parsed_catalogs.first.resource(:file, '/tmp/a_file')[:content]
+      expect(content.bytes).to eq(binary.bytes)
+    end
   end
 end
