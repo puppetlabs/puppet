5595a82ee0683df4c502bc8e20c5deec1d463141
Thomas Hallgren <thomas@tada.se>
2017-06-19 16:50:54 +0200

    (PUP-7674) Add tests to provoke resource collector load failure
    This commit adds two tests that asserts that generated resource types
    are loaded in favor of their corresponding ruby types during compile.
    One test covers normal resource collections and the other uses a
    collection for exported resources.

--- spec/integration/application/apply_spec.rb
+++ spec/integration/application/apply_spec.rb
@@ -131,6 +131,18 @@ end
         expect(notices).to include('false')
         expect(notices).not_to include('the Puppet::Type says hello')
       end
+
+      it 'does not load the ruby type when when referenced from collector during compile' do
+        pending 'Fix for PUP-7674'
+        notices = eval_and_collect_notices("@applytest { 'applytest was here': }\nApplytest<| title == 'applytest was here' |>", node)
+        expect(notices).not_to include('the Puppet::Type says hello')
+      end
+
+      it 'does not load the ruby type when when referenced from exported collector during compile' do
+        pending 'Fix for PUP-7674'
+        notices = eval_and_collect_notices("@@applytest { 'applytest was here': }\nApplytest<<| |>>", node)
+        expect(notices).not_to include('the Puppet::Type says hello')
+      end
     end
   end
 
