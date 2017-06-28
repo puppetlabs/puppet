3a94db4f8d89d45c07e594e8f46529ba2af99530
LuvCurves <stan@puppet.com>
2017-06-28 18:40:32 +0000

    (PUP-7599) Acceptance: Fix authorized keys tests
    In the past we assumed that this user logged in would be root.
    Since this is not always the case, this has been fixed.

--- acceptance/tests/resource/ssh_authorized_key/destroy.rb
+++ acceptance/tests/resource/ssh_authorized_key/destroy.rb
@@ -17,11 +17,12 @@ agents.each do |agent|
 
   step "(setup) create an authorized key in the #{auth_keys} file"
   on(agent, "echo '' >> #{auth_keys} && echo 'ssh-rsa mykey #{name}' >> #{auth_keys}")
+  on(agent, "chown $LOGNAME #{auth_keys}")
 
   #------- TESTS -------#
   step "delete an authorized key entry with puppet (absent)"
   args = ['ensure=absent',
-          "user='root'",
+          "user=$LOGNAME",
           "type='rsa'",
           "key='mykey'",
          ]
