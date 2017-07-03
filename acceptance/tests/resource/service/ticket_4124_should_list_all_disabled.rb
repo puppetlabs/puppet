d46a541bea507813dfb5e4e00a032bd4d90ab424
Justin Stoller <justin.stoller@gmail.com>
2017-07-03 11:16:35 -0700

    (PUP-7732) Audit Acceptance Tests - Service T&Ps

--- acceptance/tests/resource/service/ticket_4124_should_list_all_disabled.rb
+++ acceptance/tests/resource/service/ticket_4124_should_list_all_disabled.rb
@@ -1,5 +1,8 @@
 test_name "#4124: should list all disabled services on Redhat/CentOS"
 confine :to, :platform => /(el|centos|oracle|redhat|scientific)-5/
+tag 'audit:medium',
+    'audit:refactor',   # Use block style `test_run`
+    'audit:integration' # Doesn't change the system it runs on
 
 step "Validate disabled services agreement ralsh vs. OS service count"
 # This will remotely exec:
