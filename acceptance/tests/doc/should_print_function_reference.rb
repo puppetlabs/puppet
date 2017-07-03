8839708118638dd19ef3f96090836259c1f93968
John Duarte <john.duarte@puppetlabs.com>
2017-07-03 03:19:37 -0700

    (PUP-7732) Audit Acceptance Tests - Doc tests
    This commit adds audit tags to tests in order to facilitate the
    proper application of test tiering and layering in the future.
    The `audit` tags are intended to be removed once the corresponding
    work has been completed.
    
    Also, the `server` tag has been added to any tests that should be
    executed in the context of `puppetserver` and not in the context
    of `puppet` or `puppet-agent` testing.

--- acceptance/tests/doc/should_print_function_reference.rb
+++ acceptance/tests/doc/should_print_function_reference.rb
@@ -1,4 +1,9 @@
 test_name "verify we can print the function reference"
+
+tag 'audit:low',
+    'audit:unit',
+    'audit:delete'
+
 on(agents, puppet_doc("-r", "function")) do
     fail_test "didn't print function reference" unless
         stdout.include? 'Function Reference'
--- acceptance/tests/doc/ticket_4120_cannot_generate_type_reference.rb
+++ acceptance/tests/doc/ticket_4120_cannot_generate_type_reference.rb
@@ -1,4 +1,9 @@
 test_name "verify we can print the function reference"
+
+tag 'audit:low',
+    'audit:unit',
+    'audit:delete'
+
 confine :except, :platform => /^eos-/
 
 on(agents, puppet_doc("-r", "type")) do
