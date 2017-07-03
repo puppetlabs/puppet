5074500fcc9d8f7a3096a864a0d36ae7dcd0f984
John Duarte <john.duarte@puppetlabs.com>
2017-07-03 03:11:57 -0700

    (PUP-7732) Audit Acceptance Tests - Arbitrary Node Name
    This commit adds audit tags to tests in order to facilitate the
    proper application of test tiering and layering in the future.
    The `audit` tags are intended to be removed once the corresponding
    work has been completed.
    
    Also, the `server` tag has been added to any tests that should be
    executed in the context of `puppetserver` and not in the context
    of `puppet` or `puppet-agent` testing.

--- acceptance/tests/allow_arbitrary_node_name_fact_for_apply.rb
+++ acceptance/tests/allow_arbitrary_node_name_fact_for_apply.rb
@@ -1,5 +1,9 @@
 test_name "node_name_fact should be used to determine the node name for puppet apply"
 
+tag 'audit:medium',
+    'audit:integration',  # Ruby level integration tests already exist. This acceptance test can be deleted.
+    'audit:delete'
+
 success_message = "node_name_fact setting was correctly used to determine the node name"
 
 node_names = []
