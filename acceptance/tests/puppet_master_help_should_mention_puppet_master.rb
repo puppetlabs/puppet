83e519bf6b561fd733abea220c1c07fcf89f9560
John Duarte <john.duarte@puppetlabs.com>
2017-07-04 04:36:29 -0700

    (PUP-7732) Audit Acceptance Tests - Puppet Master tests
    This commit adds audit tags to tests in order to facilitate the
    proper application of test tiering and layering in the future.
    The `audit` tags are intended to be removed once the corresponding
    work has been completed.
    
    Also, the `server` tag has been added to any tests that should be
    executed in the context of `puppetserver` and not in the context
    of `puppet` or `puppet-agent` testing.

--- acceptance/tests/puppet_master_help_should_mention_puppet_master.rb
+++ acceptance/tests/puppet_master_help_should_mention_puppet_master.rb
@@ -1,4 +1,8 @@
 test_name "puppet master help should mention puppet master"
+
+tag 'audit:medium',
+    'audit:unit'
+
 on master, puppet_master('--help') do
     fail_test "puppet master wasn't mentioned" unless stdout.include? 'puppet master'
 end
