79ecfcad205b55faa79bdd02d30b2da9df26a41c
Iristyle <Iristyle@github>
2017-08-08 17:02:33 -0700

    (PUP-7821) Ignore invalid Windows env vars
     - When reading environment variables, request that invalid byte
       sequences be replaced with the Unicode replacement character. This
       will prevent Puppet from crashing hard when reading the block of
       memory representing environment variables and it contains bytes
       that are not valid UTF-16LE.
    
     - When iterating over the individual key=value pairs representing
       environment variables in get_environment_strings, check the strings
       for the Unicode replacement character \uFFFD. If found, then emit
       a warning and discard the variable as its not particularly useful
       to Puppet.
    
     - Add test demonstrating bad byte sequences embedded in Windows API
       response are ignored and that Puppet will emit a warning in this
       case.

--- spec/integration/util/windows/process_spec.rb
+++ spec/integration/util/windows/process_spec.rb
@@ -34,6 +34,51 @@ describe "Puppet::Util::Windows::Process", :if => Puppet.features.microsoft_wind
     end
   end
 
+  describe "when reading environment variables" do
+    after :each do
+      # spec\integration\test\test_helper_spec.rb calls set_environment_strings
+      # after :all and thus needs access to the real APIs once again
+      Puppet::Util::Windows::Process.unstub(:GetEnvironmentStringsW)
+      Puppet::Util::Windows::Process.unstub(:FreeEnvironmentStringsW)
+    end
+
+    it "will ignore only keys or values with corrupt byte sequences" do
+      arraydest = []
+      Puppet::Util::Log.newdestination(Puppet::Test::LogCollector.new(arraydest))
+
+      env_vars = {}
+
+      # Create a UTF-16LE version of the below null separated environment string
+      # "a=b\x00c=d\x00e=\xDD\xDD\x00f=g\x00\x00"
+      env_var_block =
+        "a=b\x00".encode(Encoding::UTF_16LE) +
+        "c=d\x00".encode(Encoding::UTF_16LE) +
+        'e='.encode(Encoding::UTF_16LE) + "\xDD\xDD".force_encoding(Encoding::UTF_16LE) + "\x00".encode(Encoding::UTF_16LE) +
+        "f=g\x00\x00".encode(Encoding::UTF_16LE)
+
+      env_var_block_bytes = env_var_block.bytes.to_a
+
+      FFI::MemoryPointer.new(:byte, env_var_block_bytes.count) do |ptr|
+        # uchar here is synonymous with byte
+        ptr.put_array_of_uchar(0, env_var_block_bytes)
+
+        # stub the block of memory that the Win32 API would typically return via pointer
+        Puppet::Util::Windows::Process.expects(:GetEnvironmentStringsW).returns(ptr)
+        # stub out the real API call to free memory, else process crashes
+        Puppet::Util::Windows::Process.expects(:FreeEnvironmentStringsW)
+
+        env_vars = Puppet::Util::Windows::Process.get_environment_strings
+      end
+
+      # based on corrupted memory, the e=\xDD\xDD should have been removed from the set
+      expect(env_vars).to eq({'a' => 'b', 'c' => 'd', 'f' => 'g'})
+
+      # and Puppet should emit a warning about it
+      expect(arraydest.last.level).to eq(:warning)
+      expect(arraydest.last.message).to eq("Discarding environment variable e=\uFFFD which contains invalid bytes")
+    end
+  end
+
   describe "when setting environment variables" do
     it "can properly handle env var values with = in them" do
       begin
