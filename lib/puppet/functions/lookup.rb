3bcbccad40e8fde52b06907f56f207ed4f32c9fa
Jean B <jean@puppet.com>
2017-08-22 16:05:52 -0700

    (docs) fix space error in knockout_prefix

--- lib/puppet/functions/lookup.rb
+++ lib/puppet/functions/lookup.rb
@@ -97,7 +97,7 @@
 # but can adjust the merge with additional options. The available options are:
 #     * `'knockout_prefix'` (string or undef) --- A string prefix to indicate a
 #     value should be _removed_ from the final result. If a value is exactly equal
-#     to the prefix, it will knockout the entire element.Defaults to `undef`, which
+#     to the prefix, it will knockout the entire element. Defaults to `undef`, which
 #     disables this feature.
 #     * `'sort_merged_arrays'` (boolean) --- Whether to sort all arrays that are
 #     merged together. Defaults to `false`.
