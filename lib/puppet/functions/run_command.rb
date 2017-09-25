ade22965505d0d750a1ebf64c5156b287c36ec5f
Thomas Hallgren <thomas@tada.se>
2017-09-25 14:21:21 +0200

    (PUP-7979) Add run_command function
    Adds a `run_command` function that can run a command on a set of nodes
    by calling the `Bolt::Executor#execute`. The function requires that
    --tasks is enabled and that the `bolt` feature is present.

---
+++ lib/puppet/functions/run_command.rb
@@ -0,0 +1,44 @@
+# Runs a command on the given set of nodes and returns the result from each command execution.
+#
+# * This function does nothing if the list of nodes is empty.
+# * It is possible to run on the node 'localhost'
+# * A node is a String with a node's hostname or a URI that also describes how to connect and run the task on that node
+#   including "user" and "password" parts of a URI.
+# * The returned value contains information about the result per node. TODO: needs mapping to a runtime Pcore Object to be useful
+#
+#
+# Since > 5.4.0 TODO: Update when version is known
+#
+Puppet::Functions.create_function(:run_command) do
+  local_types do
+    type 'NodeOrNodes = Variant[String[1], Array[NodeOrNodes]]'
+  end
+
+  dispatch :run_command do
+    param 'String[1]', :command
+    repeated_param 'NodeOrNodes', :nodes
+  end
+
+  def run_command(command, *nodes)
+    unless Puppet[:tasks]
+      raise Puppet::ParseErrorWithIssue.from_issue_and_stack(
+        Puppet::Pops::Issues::TASK_OPERATION_NOT_SUPPORTED_WHEN_COMPILING,
+        {:operation => 'run_command'})
+    end
+
+    unless Puppet.features.bolt?
+      raise Puppet::ParseErrorWithIssue.from_issue_and_stack(Puppet::Pops::Issues::TASK_MISSING_BOLT, :action => _('run a command'))
+    end
+
+    hosts = nodes.flatten
+    if hosts.empty?
+      call_function('debug', "Simulating run_command('#{command}') - no hosts given - no action taken")
+      []
+    else
+      # TODO, rewrite once proper handling of returned values is fully specified
+      Bolt::Executor.from_uris(hosts).execute(command).map do |_, result|
+        result.success? ? result.output_string : result.exit_code
+      end
+    end
+  end
+end
