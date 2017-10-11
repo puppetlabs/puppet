9dde57633fea50f217bd2d206f007e349411087b
Jayant Sane <ghjsane@gmail.com>
2017-10-11 11:31:29 -0700

    (PUP-8044) Enable FIPS mode on agent hosts to test PA in FIPS
    Part of FIPS epic work. Comprised of three sets of tasks:
       > PA-1264: Build PA against system openssl for use in FIPs
       > PUP-7983: Update system openssl package on rhel7 to allow testing PA
         linked against system openssl
       > PUP-8044: Turn on FIPS mode on agent hosts to run acceptance tests in FIPS mode
         and configure using one of the FIPS approved hash algorithms (from default of md5)
    
    This commit currently only enables FIPS mode on rhel7 platforms. More changes
    to come for rhel6 and possibly f24, f25.

--- acceptance/setup/aio/pre-suite/010_Install.rb
+++ acceptance/setup/aio/pre-suite/010_Install.rb
@@ -148,3 +148,76 @@ else
 end
 
 configure_gem_mirror(hosts)
+
+
+step "Enable FIPS on agent hosts..." do
+
+ # There might be a better way to do some things below but till then...
+ # The TODO's need to be followed up
+
+  agents.each do |agent|
+    next if agent == master # Only on agents.
+
+    # Do this only on rhel7, rhel6, f24, f25
+    use_system_openssl = ENV['USE_SYSTEM_OPENSSL']
+    if use_system_openssl
+      # Other platforms to come...
+      next if !agent[:platform].match(/(?:el-7|redhat-7)/)
+
+      # Step 1: Disable prelinking
+      # TODO:: Handle cases where the /etc/sysconfig/prelink might exist
+      on(agent, 'echo "PRELINKING=no" > /etc/sysconfig/prelink')
+      # TODO: prelink is commented till we figure how to attempt executing
+      # it so any failures do not cause this thing to exit
+      # on(agent, "prelink -u -a")
+ 
+      # Step 2: Install dracut-fips, dracut-fips-aesni
+      on(agent, "yum -y install dracut-fips")
+      on(agent, "yum -y install dracut-fips-aesni")
+
+      # TODO:: Backup existing kernel image in case anything goes south..
+      # Step 3: Run dracut to update system for fips. 
+      on(agent, "dracut -v -f")
+
+
+      # TODO:: Steps 4 and 5 need to be adjusted for rhel6, f24, f25
+      # as the method of enabling FIPS are different on those platforms. 
+      # Below works on rhel7
+      
+      # Step 4: Find out the device details (BLOCK ID) of boot partition
+      # append enable fips and boot block id to kernel command line in grub file
+      
+      on(agent, 'boot_blkid=$(blkid `df /boot | grep "/dev" | awk \'BEGIN{ FS=" "}; {print $1}\'` | awk \'BEGIN{ FS=" "}; {print $2}\' | sed \'s/"//g\');\
+                 fips_bootblk="fips=1 boot="$boot_blkid;\
+                 grub_linux_cmdline=`grep -e "^GRUB_CMDLINE_LINUX" /etc/default/grub | sed "s/\"$/ $fips_bootblk\"/"`;\
+                 grep -v GRUB_CMDLINE_LINUX /etc/default/grub > /etc/default/grub.bak;\
+                 /bin/cp -rf /etc/default/grub.bak /etc/default/grub;\
+                 sed -i "/GRUB_DISABLE_RECOVERY/i $grub_linux_cmdline" /etc/default/grub')
+
+
+      # Step 5: Run grub2 mkconfig to make the changes take effect.
+      on(agent, 'grub2-mkconfig -o /boot/grub2/grub.cfg')
+
+      # Reboot is needed for the system to start in FIPS mode
+      agent.reboot
+      timeout = 30
+      begin
+        Timeout.timeout(timeout) do
+          until agent.up?
+            sleep 1
+          end
+        end
+      rescue Timeout::Error => e
+        raise "Agent did not come back up within #{timeout} seconds."
+      end
+      
+      # TODO:
+      # Ensure that agent is actually in fips mode by examining the contents of
+      # /proc/sys/crypto/fips_enabled and that it is 1
+
+      # Switch to using sha256 to work in fips mode.
+      on agent, puppet('config set digest_algorithm sha256')
+      
+    end
+  end
+end
