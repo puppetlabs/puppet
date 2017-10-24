79f9cd3d60374102e99e87f7799575984ff92445
Ethan J. Brown <Iristyle@github>
2017-10-23 17:14:29 -0700

    (PUP-8083) Add Windows mount reparse points
     - Previously, Puppet could only resolve reparse points using the
       DeviceIoControl API when the buffers were representative of
       symbolic links.
    
       It errantly treated mount points (such as those in DFS shares) the
       same as symlinks, causing data corruption when reading their path
       components. Given the wrong struct layout, paths were always
       missing their first 4 bytes (i.e. 2 wide characters) and could
       cause Puppet to crash / raise errors.
    
     - Following the MSDN documentation, peek the reparse data tag, so
       that the appropriate structure is used for resolving the reparse
       points.

--- lib/puppet/util/windows/file.rb
+++ lib/puppet/util/windows/file.rb
@@ -179,9 +179,22 @@ module Puppet::Util::Windows::File
 
   def self.get_reparse_point_data(handle, &block)
     # must be multiple of 1024, min 10240
-    FFI::MemoryPointer.new(SYMLINK_REPARSE_DATA_BUFFER.size) do |reparse_data_buffer_ptr|
+    FFI::MemoryPointer.new(MAXIMUM_REPARSE_DATA_BUFFER_SIZE) do |reparse_data_buffer_ptr|
       device_io_control(handle, FSCTL_GET_REPARSE_POINT, nil, reparse_data_buffer_ptr)
-      yield SYMLINK_REPARSE_DATA_BUFFER.new(reparse_data_buffer_ptr)
+
+      reparse_tag = reparse_data_buffer_ptr.read_win32_ulong
+      buffer_type = case reparse_tag
+      when 0xA000000C
+        SYMLINK_REPARSE_DATA_BUFFER
+      when 0xA0000003
+        MOUNT_POINT_REPARSE_DATA_BUFFER
+      else
+        # includes NFS reparse points of tag 0x80000014
+        raise Puppet::Util::Windows::Error.new("DeviceIoControl(#{handle}, " +
+          "FSCTL_GET_REPARSE_POINT) returned unknown tag 0x#{reparse_tag.to_s(16).upcase}")
+      end
+
+      yield buffer_type.new(reparse_data_buffer_ptr)
     end
 
     # underlying struct MemoryPointer has been cleaned up by this point, nothing to return
@@ -464,6 +477,23 @@ module Puppet::Util::Windows::File
            # max less above fields dword / uint 4 bytes, ushort 2 bytes
            # technically a WCHAR buffer, but we care about size in bytes here
            :PathBuffer, [:byte, MAXIMUM_REPARSE_DATA_BUFFER_SIZE - 20]
+  end
+
+  # MOUNT_POINT_REPARSE_DATA_BUFFER
+  # https://msdn.microsoft.com/en-us/library/cc232007.aspx
+  # https://msdn.microsoft.com/en-us/library/windows/hardware/ff552012(v=vs.85).aspx
+  # struct is always MAXIMUM_REPARSE_DATA_BUFFER_SIZE bytes
+  class MOUNT_POINT_REPARSE_DATA_BUFFER < FFI::Struct
+    layout :ReparseTag, :win32_ulong,
+           :ReparseDataLength, :ushort,
+           :Reserved, :ushort,
+           :SubstituteNameOffset, :ushort,
+           :SubstituteNameLength, :ushort,
+           :PrintNameOffset, :ushort,
+           :PrintNameLength, :ushort,
+           # max less above fields dword / uint 4 bytes, ushort 2 bytes
+           # technically a WCHAR buffer, but we care about size in bytes here
+           :PathBuffer, [:byte, MAXIMUM_REPARSE_DATA_BUFFER_SIZE - 16]
   end
 
   # https://msdn.microsoft.com/en-us/library/windows/desktop/aa364980(v=vs.85).aspx
