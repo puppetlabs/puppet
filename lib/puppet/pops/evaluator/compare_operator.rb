e6eb70fe777d4d3835b927b861083caff9aa2291
Thomas Hallgren <thomas@tada.se>
2017-10-27 12:30:09 +0200

    (PUP-8078) Make it possible to call CompareOperator#match without scope
    This commit removes the requirement to pass a scope to the `#match`
    method of the `CompareOperator` class. The scope is normally used for
    storing the resulting groups of a regular expression match. This step
    is simply omitted when calling `#match` without a scope.

--- lib/puppet/pops/evaluator/compare_operator.rb
+++ lib/puppet/pops/evaluator/compare_operator.rb
@@ -34,7 +34,7 @@ class CompareOperator
   end
 
   # Performs a match of a and b, and returns true if b matches a
-  def match(a, b, scope)
+  def match(a, b, scope = nil)
     @@match_visitor.visit_this_2(self, b, a, scope)
   end
 
@@ -200,7 +200,7 @@ class CompareOperator
   def match_Regexp(regexp, left, scope)
     return false unless left.is_a? String
     matched = regexp.match(left)
-    set_match_data(matched, scope) # creates or clears ephemeral
+    set_match_data(matched, scope) unless scope.nil? # creates or clears ephemeral
     !!matched # convert to boolean
   end
 
