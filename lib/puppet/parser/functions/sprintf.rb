d2169a02953a6cc26f3629e51c78d456393a748f
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2017-11-21 14:21:29 +0100

    (maint) Fold long line of documentation

--- lib/puppet/parser/functions/sprintf.rb
+++ lib/puppet/parser/functions/sprintf.rb
@@ -29,7 +29,8 @@ Puppet::Parser::Functions::newfunction(
   :arity => -2,
   :doc => "Perform printf-style formatting of text.
 
-      The first parameter is format string describing how the rest of the parameters should be formatted.  See the documentation for the `Kernel::sprintf` function in Ruby for all the details."
+  The first parameter is format string describing how the rest of the parameters should be formatted.
+  See the documentation for the `Kernel::sprintf` function in Ruby for all the details."
 ) do |args|
   fmt = args.shift
   return sprintf(fmt, *args)
