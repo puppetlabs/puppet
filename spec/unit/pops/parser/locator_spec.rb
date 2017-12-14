c68a3215abc13e14a87fcc1d903888df06bc06f2
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2017-12-14 11:49:08 +0100

    (PUP-8247) Add tests for Locator cache behavior
    This adds tests that shows that locator's cache of offset to line
    keeps the last computed line.

--- spec/unit/pops/parser/locator_spec.rb
+++ spec/unit/pops/parser/locator_spec.rb
@@ -21,4 +21,24 @@ describe Puppet::Pops::Parser::Locator do
     expect(model.body.locator.extract_tree_text(model.body)).to eq('1 + 2')
   end
 
+  it 'Locator caches last offset / line' do
+    #Puppet::Pops::Parser::Locator::AbstractLocator.expects(:ary_bsearch_i).once
+    parser = Puppet::Pops::Parser::Parser.new()
+    model = parser.parse_string("$a\n = 1\n + 1\n").model
+    model.body.locator.expects(:ary_bsearch_i).with(anything, 2).once.returns(:special_value)
+    expect(model.body.locator.line_for_offset(2)).to eq(:special_value)
+    expect(model.body.locator.line_for_offset(2)).to eq(:special_value)
+  end
+
+  it 'Locator invalidates last offset / line cache if asked for different offset' do
+    #Puppet::Pops::Parser::Locator::AbstractLocator.expects(:ary_bsearch_i).once
+    parser = Puppet::Pops::Parser::Parser.new()
+    model = parser.parse_string("$a\n = 1\n + 1\n").model
+    model.body.locator.expects(:ary_bsearch_i).with(anything, 2).twice.returns(:first_value, :third_value)
+    model.body.locator.expects(:ary_bsearch_i).with(anything, 3).once.returns(:second_value)
+    expect(model.body.locator.line_for_offset(2)).to eq(:first_value)
+    expect(model.body.locator.line_for_offset(3)).to eq(:second_value) # invalidates cache as side effect
+    expect(model.body.locator.line_for_offset(2)).to eq(:third_value)
+  end
+
 end
