36f376cac9148e2e7a220e481214763deb33fb47
Josh Cooper <josh@puppet.com>
2017-12-19 16:05:31 -0800

    (PUP-8267) Default to aio acceptance
    Default to aio acceptance.

--- acceptance/config/gem/options.rb
+++ acceptance/config/gem/options.rb
@@ -1,7 +1,6 @@
 {
+  # Use `git` so that we have a sane ruby environment
   :type => 'git',
-  :install => [
-  ],
   :pre_suite => [
     'setup/common/pre-suite/000-delete-puppet-when-none.rb',
     'setup/git/pre-suite/000_EnvSetup.rb',
