7e00a4060e1b9406aa6aeccf3fb13cc201b04bf3
Melissa Stone <melissa@puppet.com>
2018-02-07 10:19:41 -0800

    Revert "Merge pull request #6550 from joshcooper/ticket/4.10.x/PUP-8267-beaker-subcommands"
    This reverts commit c928ddd4e8af08de2985691ca6dc41678b794486, reversing
    changes made to 24102cf835fbb3381d096c4004db94e33d5c90ca.
    
    Beaker subcommands do not play well with additional arguments passed in
    with ENV['OPTIONS']. This causes acceptance tests to break when they
    pass in `OPTIONS='--test-tag-exclude=server'`.

--- acceptance/setup/aio/pre-suite/010_Install.rb
+++ acceptance/setup/aio/pre-suite/010_Install.rb
@@ -7,20 +7,69 @@ extend BeakerPuppet::Install::Puppet5
 
 test_name "Install Packages"
 
-dev_builds_url  = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
-
 step "Install puppet-agent..." do
+  dev_builds_url  = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
   sha = ENV['SHA']
   install_from_build_data_url('puppet-agent', "#{dev_builds_url}/puppet-agent/#{sha}/artifacts/#{sha}.yaml", hosts)
 end
 
 step "Install puppetserver..." do
-  if ENV['SERVER_VERSION'].nil? || ENV['SERVER_VERSION'] == 'latest'
-    install_puppetlabs_dev_repo(master, 'puppetserver', 'latest', nil, :dev_builds_url => "http://nightlies.puppet.com")
-    master.install_package('puppetserver')
+  if master[:hypervisor] == 'ec2'
+    if master[:platform].match(/(?:el|centos|oracle|redhat|scientific)/)
+      # An EC2 master instance does not have access to puppetlabs.net for getting
+      # dev repos.
+      #
+      # We will install the appropriate repo to satisfy the puppetserver requirement
+      # and then upgrade puppet-agent with the targeted SHA package afterwards.
+      #
+      # Currently, only an `el` master is supported for this operation.
+      if ENV['SERVER_VERSION']
+        variant, version = master['platform'].to_array
+        if ENV['SERVER_VERSION'].to_i < 5
+          logger.info "EC2 master found: Installing nightly build of puppet-agent repo to satisfy puppetserver dependency."
+          on(master, "rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-#{version}.noarch.rpm")
+        else
+          logger.info "EC2 master found: Installing nightly build of puppet-agent repo to satisfy puppetserver dependency."
+          on(master, "rpm -Uvh https://yum.puppetlabs.com/puppet5-release-el-#{version}.noarch.rpm")
+        end
+      else
+        logger.info "EC2 master found: Installing nightly build of puppet-agent repo to satisfy puppetserver dependency."
+        install_repos_on(master, 'puppet-agent', 'nightly', 'repo-configs')
+        install_repos_on(master, 'puppetserver', 'nightly', 'repo-configs')
+      end
+
+      master.install_package('puppetserver')
+
+      logger.info "EC2 master found: Installing #{ENV['SHA']} build of puppet-agent."
+      # Upgrade installed puppet-agent with targeted SHA.
+      opts = {
+        :puppet_collection => 'PC1',
+        :puppet_agent_sha => ENV['SHA'],
+        :puppet_agent_version => ENV['SUITE_VERSION'] || ENV['SHA'] ,
+        :dev_builds_url => "http://builds.delivery.puppetlabs.net"
+      }
+
+      copy_dir_local = File.join('tmp', 'repo_configs', master['platform'])
+      release_path_end, release_file = master.puppet_agent_dev_package_info( opts[:puppet_collection], opts[:puppet_agent_version], opts)
+      release_path = "#{opts[:dev_builds_url]}/puppet-agent/#{opts[:puppet_agent_sha]}/repos/"
+      release_path << release_path_end
+      fetch_http_file(release_path, release_file, copy_dir_local)
+      scp_to master, File.join(copy_dir_local, release_file), master.external_copy_base
+      on master, "rpm -Uvh #{File.join(master.external_copy_base, release_file)} --oldpackage --force"
+    else
+      fail_test("EC2 master found, but it was not an `el` host: The specified `puppet-agent` build (#{ENV['SHA']}) cannot be installed.")
+    end
   else
-    server_version = ENV['SERVER_VERSION']
-    install_from_build_data_url('puppetserver', "#{dev_builds_url}/puppetserver/#{server_version}/artifacts/#{server_version}.yaml", master)
+    if ENV['SERVER_VERSION'].nil? || ENV['SERVER_VERSION'] == 'latest'
+      server_version = 'latest'
+      server_download_url = "http://nightlies.puppet.com"
+      install_puppetlabs_dev_repo(master, 'puppetserver', server_version, nil, :dev_builds_url => server_download_url)
+      master.install_package('puppetserver')
+    else
+      server_version = ENV['SERVER_VERSION']
+      dev_builds_url = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
+      install_from_build_data_url('puppetserver', "#{dev_builds_url}/puppetserver/#{server_version}/artifacts/#{server_version}.yaml", master)
+    end
   end
 end
 
