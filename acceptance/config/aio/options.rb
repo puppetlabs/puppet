f014824972becddec1e9b0dfcf81498504315a4e
Melissa Stone <melissa@puppet.com>
2018-02-15 17:02:34 -0800

    (PUP-8416) Export ci rake tasks and install utils
    This commit moves all ci acceptance rake tasks and install/setup
    utilities out of the puppet repo and into the beaker-puppet repo. This
    will allow us to reuse all of this code across multiple components of
    puppet-agent. The goal is to reduce the number of places we need to
    duplicate code across different code bases. With this change, any future
    updates or overhauls will be consistent across all our projects.

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -1,15 +1,5 @@
 {
   :type                        => 'aio',
-  :pre_suite                   => [
-    'setup/common/pre-suite/000-delete-puppet-when-none.rb',
-    'setup/aio/pre-suite/010_Install.rb',
-    'setup/aio/pre-suite/020_InstallCumulusModules.rb',
-    'setup/aio/pre-suite/021_InstallAristaModule.rb',
-    'setup/common/pre-suite/025_StopFirewall.rb',
-    'setup/common/pre-suite/030_StopSssd.rb',
-    'setup/common/pre-suite/040_ValidateSignCert.rb',
-    'setup/aio/pre-suite/045_EnsureMasterStarted.rb',
-  ],
   :is_puppetserver             => true,
   :'use-service'               => true, # use service scripts to start/stop stuff
   :puppetservice               => 'puppetserver',
