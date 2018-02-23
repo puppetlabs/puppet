930abc157982d7cf010d87da8ec72e315917027c
Garrett Guillotte <gguillotte@users.noreply.github.com>
2018-02-23 11:38:05 -0800

    (docs)(DOC-3205) Update formatting in any.

--- lib/puppet/functions/any.rb
+++ lib/puppet/functions/any.rb
@@ -1,4 +1,4 @@
-# Runs a [lambda](http://docs.puppetlabs.com/puppet/latest/reference/lang_lambdas.html)
+# Runs a [lambda](http://puppet.com/docs/puppet/latest/lang_lambdas.html)
 # repeatedly using each value in a data structure until the lambda returns a "truthy" value which
 # makes the function return `true`, or if the end of the iteration is reached, false is returned.
 #
@@ -18,15 +18,16 @@
 #
 # @example Using the `any` function with an Array and a one-parameter lambda
 #
-# ~~~ puppet
+# ``` puppet
 # # For the array $data, run a lambda that checks if an unknown hash contains those keys
 # $data = ["routers", "servers", "workstations"]
 # $looked_up = lookup('somekey', Hash)
 # notice $data.any |$item| { $looked_up[$item] }
-# ~~~
+# ```
 #
-# Would notice `true` if the looked up hash had a value that is neither `false` nor `undef` for at least
-# one of the keys. That is, it is equivalent to the expression
+# This example produces the notice `true` if the looked-up hash has a value that is
+# neither `false` nor `undef` for at least one of the keys. That is, it is
+# equivalent to the expression
 # `$looked_up[routers] || $looked_up[servers] || $looked_up[workstations]`.
 #
 # When the first argument is a `Hash`, Puppet passes each key and value pair to the lambda
@@ -34,32 +35,31 @@
 #
 # @example Using the `any` function with a `Hash` and a one-parameter lambda
 #
-# ~~~ puppet
+# ``` puppet
 # # For the hash $data, run a lambda using each item as a key-value array.
 # $data = {"rtr" => "Router", "svr" => "Server", "wks" => "Workstation"}
 # $looked_up = lookup('somekey', Hash)
 # notice $data.any |$item| { $looked_up[$item[0]] }
-# ~~~
+# ```
 #
-# Would notice `true` if the looked up hash had a value for one of the wanted key that is
-# neither `false` nor `undef`.
+# This example produces the notice `true` if the looked-up hash has a value for one of
+# the wanted keys that is neither `false` nor `undef`.
 #
 # When the lambda accepts two arguments, the first argument gets the index in an array
-# or the key from a hash, and the second argument the value.
-#
+# or the key from a hash, and the second argument gets the value.
 #
 # @example Using the `any` function with an array and a two-parameter lambda
 #
-# ~~~ puppet
+# ``` puppet
 # # Check if there is an even numbered index that has a non String value
 # $data = [key1, 1, 2, 2]
 # notice $data.any |$index, $value| { $index % 2 == 0 and $value !~ String }
-# ~~~
+# ```
 #
-# Would notice true as the index `2` is even and not a `String`
+# The example produces the notice `true` because the index `2` is even and not a `String`.
 #
-# For an general examples that demonstrates iteration, see the Puppet
-# [iteration](https://docs.puppetlabs.com/puppet/latest/reference/lang_iteration.html)
+# For general examples that demonstrate iteration, see the Puppet
+# [iteration](https://puppet.com/docs/puppet/latest/lang_iteration.html)
 # documentation.
 #
 # @since 5.2.0
