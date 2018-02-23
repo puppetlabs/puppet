e9433f7ac4d7c6a6fb4f91be1e6807480dfcf13c
Garrett Guillotte <gguillotte@users.noreply.github.com>
2018-02-23 12:34:44 -0800

    (docs)(DOC-3205) Copy and edit 3.x-style include  reference to 4.x.

--- lib/puppet/functions/include.rb
+++ lib/puppet/functions/include.rb
@@ -1,5 +1,36 @@
-# Include the specified classes
-# For documentation see the 3.x stub
+# Declares one or more classes, which causes the resources in them to be
+# evaluated and added to the catalog. Accepts a class name, an array of class
+# names, or a comma-separated list of class names.
+#
+# You can use the `include` function multiple times on the same class, and it declares
+# a given class only once. If a class declared with `include` has any parameters, Puppet
+# automatically looks up values for them in Hiera, using `<class name>::<parameter name>`
+# as the lookup key.
+#
+# Contrast this behavior with resource-like class declarations, such as:
+#
+# ``` puppet
+# class {'name': parameter => 'value',}
+# ```
+#
+# These must be used in only one place per class and can directly set parameters.
+#
+# Avoid using both `include` and resource-like declarations with the same class.
+#
+# The `include` function does not cause classes to be contained in the class
+# where they are declared. For that, see the `contain` function.
+#
+# It also does not create a dependency relationship between the declared class and the
+# surrounding class. For that, see the `require` function.
+#
+# You must use a class's full name; relative names are not allowed. In addition to
+# providing class names in string form, you can also directly use Class and Resource Type
+# values produced by evaluating resource and relationship expressions.
+#
+# @since < 3.0.0
+# @since 4.0.0 support for class and resource type values, absolute names
+# @since 4.7.0 returns an Array[Type[Class]] of all included classes
+#
 Puppet::Functions.create_function(:include, Puppet::Functions::InternalFunction) do
   dispatch :include do
     scope_param
