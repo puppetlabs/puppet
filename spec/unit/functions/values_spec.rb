0adde0bc9c792d412e0d02e10e7b0cb0198731f6
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2018-02-27 20:22:34 +0100

    (PUP-8496) Add the keys() and values() functions from stdlib to puppet
    This adds the functions keys() and values() from stdlib to puppet and
    updates them to use the 4.x function API.
    
    The functions are backwards compatible.

--- spec/unit/functions/values_spec.rb
+++ spec/unit/functions/values_spec.rb
@@ -3,29 +3,28 @@ require 'spec_helper'
 require 'puppet_spec/compiler'
 require 'matchers/resource'
 
-describe 'the keys function' do
+describe 'the values function' do
   include PuppetSpec::Compiler
   include Matchers::Resource
 
-  it 'returns the keys in the hash in the order they appear in a hash iteration' do
-    expect(compile_to_catalog(<<-'SRC'.unindent)).to have_resource('Notify[apples & oranges]')
-        $k = {'apples' => 1, 'oranges' => 2}.keys
+  it 'returns the values in the hash in the order they appear in a hash iteration' do
+    expect(compile_to_catalog(<<-'SRC'.unindent)).to have_resource('Notify[1 & 2]')
+        $k = {'apples' => 1, 'oranges' => 2}.values
         notify { "${k[0]} & ${k[1]}": }
       SRC
   end
 
   it 'returns an empty array for an empty hash' do
     expect(compile_to_catalog(<<-'SRC'.unindent)).to have_resource('Notify[0]')
-        $v = {}.keys.reduce(0) |$m, $v| { $m+1 }
+        $v = {}.values.reduce(0) |$m, $v| { $m+1 }
         notify { "${v}": }
       SRC
   end
 
-  it 'includes an undef key if one is present in the hash' do
+  it 'includes an undef value if one is present in the hash' do
     expect(compile_to_catalog(<<-'SRC'.unindent)).to have_resource('Notify[Undef]')
-        $types = {undef => 1}.keys.map |$v| { $v.type }
+        $types = {a => undef}.values.map |$v| { $v.type }
         notify { "${types[0]}": }
-      SRC
+    SRC
   end
-
 end
