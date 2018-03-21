78f99f38a55304743bb3d7fe902ebe1d0f50fdd6
Eric Delaney <eric.delaney@puppet.com>
2018-03-21 15:44:19 -0700

    WIP adding support for running spec tests against jruby in travis

--- spec/unit/context/trusted_information_spec.rb
+++ spec/unit/context/trusted_information_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 require 'puppet/context/trusted_information'
 
-describe Puppet::Context::TrustedInformation do
+describe Puppet::Context::TrustedInformation  do
   let(:key) do
     key = Puppet::SSL::Key.new("myname")
     key.generate
