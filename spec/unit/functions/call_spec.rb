6d45be2f3a9402fbc26111161c19bed0209f04ac
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2018-04-10 10:35:14 +0200

    (PUP-8641) Add tests to call() for deferred resolution of complex args
    This adds tests that show that a `call` given complex structures
    containing `Deferred` instances resolves
    the arguments to a `Deferred` recursively (using DeferredResolver):
    
    * all array values
    * all hash values
    * all Sensitive values

--- spec/unit/functions/call_spec.rb
+++ spec/unit/functions/call_spec.rb
@@ -20,7 +20,9 @@ describe 'the call method' do
         'modules' => {
           'test' => {
             'functions' => {
-              'call_me.pp' => 'function test::call_me() { "called" }'
+              'call_me.pp' => 'function test::call_me() { "called" }',
+              'abc.pp'     => 'function test::abc() { "a-b-c" }',
+              'dash.pp'    => 'function test::dash() { "-" }'
             }
           }
         }
@@ -34,7 +36,7 @@ describe 'the call method' do
     end
 
     it 'call on a built-in 4x Ruby API function' do
-      expect(compile_to_catalog(<<-CODE)).to have_resource('Notify[a]')
+      expect(compile_to_catalog(<<-CODE, node)).to have_resource('Notify[a]')
           $a = call('split', 'a-b-c', '-')
           notify { $a[0]: }
         CODE
@@ -47,7 +49,7 @@ describe 'the call method' do
     end
 
     it 'call a Ruby 4x API built-in with block' do
-      catalog = compile_to_catalog(<<-CODE)
+      catalog = compile_to_catalog(<<-CODE, node)
         $a = 'each'
         $b = [1,2,3]
         call($a, $b) |$index, $v| {
@@ -61,29 +63,64 @@ describe 'the call method' do
     end
 
     it 'call with the calling context' do
-      expect(eval_and_collect_notices(<<-CODE)).to eq(['a'])
+      expect(eval_and_collect_notices(<<-CODE, node)).to eq(['a'])
         class a { call('notice', $title) }
         include a
       CODE
     end
 
     it 'call on a non-existent function name' do
-      expect { compile_to_catalog(<<-CODE) }.to raise_error(Puppet::Error, /Unknown function/)
+      expect { compile_to_catalog(<<-CODE, node) }.to raise_error(Puppet::Error, /Unknown function/)
         $a = call('not_a_function_name')
         notify { $a: }
       CODE
     end
 
     it 'call a deferred value' do
-      expect(compile_to_catalog(<<-CODE)).to have_resource('Notify[a]')
+      expect(compile_to_catalog(<<-CODE, node)).to have_resource('Notify[a]')
           $d = Deferred('split', ['a-b-c', '-'])
           $a = $d.call()
           notify { $a[0]: }
         CODE
     end
 
+    it 'resolves deferred value arguments in an array when calling a deferred' do
+      expect(compile_to_catalog(<<-CODE,node)).to have_resource('Notify[a]')
+          $d = Deferred('split', [Deferred('test::abc'), '-'])
+          $a = $d.call()
+          notify { $a[0]: }
+        CODE
+    end
+
+    it 'resolves deferred value arguments in a Sensitive when calling a deferred' do
+      expect(compile_to_catalog(<<-CODE, node)).to have_resource('Notify[a]')
+          function my_split(Sensitive $sensy, $on) { $sensy.unwrap |$x| { split($x, $on) } }
+          $d = Deferred('my_split', [ Sensitive(Deferred('test::abc')), '-'])
+          $a = $d.call()
+          notify { $a[0]: }
+        CODE
+    end
+
+    it 'resolves deferred value arguments in a Hash when calling a deferred' do
+      expect(compile_to_catalog(<<-CODE, node)).to have_resource('Notify[a]')
+          function my_split(Hash $hashy, $on) { split($hashy['key'], $on)  }
+          $d = Deferred('my_split', [ {'key' => Deferred('test::abc')}, '-'])
+          $a = $d.call()
+          notify { $a[0]: }
+        CODE
+    end
+
+    it 'resolves deferred value arguments in a nested structure when calling a deferred' do
+      expect(compile_to_catalog(<<-CODE,node)).to have_resource('Notify[a]')
+          function my_split(Hash $hashy, Array[Sensitive] $sensy) { split($hashy['key'][0], $sensy[0].unwrap |$x| {$x})  }
+          $d = Deferred('my_split', [ {'key' => [Deferred('test::abc')]}, [Sensitive(Deferred('test::dash'))]])
+          $a = $d.call()
+          notify { $a[0]: }
+        CODE
+    end
+
     it 'call dig into a variable' do
-      expect(compile_to_catalog(<<-CODE)).to have_resource('Notify[value 3]')
+      expect(compile_to_catalog(<<-CODE, node)).to have_resource('Notify[value 3]')
           $x = { 'a' => [1,2,3] }
           $d = Deferred('$x', ['a', 2])
           $a = $d.call()
