2576957d40148162ae4bce4e6085ff0d8fd14722
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2018-05-06 15:58:43 +0200

    (PUP-8604) Add the upcase() function from stdlib to puppet
    This adds the function `upcase()` from stdlib to puppet.
    It is a re-implementation using the 4.x function API.
    
    There is one difference in that the stdlib function would return an
    empty string as the result of `upcase(undef)`, where the new version
    instead errors. The stdlib version would however error if given an Array
    or Hash with `undef` values as only top level arguments have `undef`
    replaced with empty string in the 3.x function API.

---
+++ spec/unit/functions/upcase_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+require 'puppet_spec/compiler'
+require 'matchers/resource'
+
+describe 'the upcase function' do
+  include PuppetSpec::Compiler
+  include Matchers::Resource
+
+  it 'returns upper case version of a string' do
+    expect(compile_to_catalog("notify { 'abc'.upcase: }")).to have_resource('Notify[ABC]')
+  end
+
+  it 'performs upcase of international UTF-8 characters' do
+    expect(compile_to_catalog("notify { 'åäö'.upcase: }")).to have_resource('Notify[ÅÄÖ]')
+  end
+
+  it 'returns upper case version of each entry in an array (recursively)' do
+    expect(compile_to_catalog("notify { String(['a', ['b', ['c']]].upcase == ['A', ['B', ['C']]]): }")).to have_resource('Notify[true]')
+  end
+
+  it 'returns upper case version of keys and values in a hash (recursively)' do
+    expect(compile_to_catalog("notify { String({'a'=>'b','c'=>{'d'=>'e'}}.upcase == {'A'=>'B', 'C'=>{'D'=>'E'}}): }")).to have_resource('Notify[true]')
+  end
+
+  it 'returns upper case version of keys and values in nested hash / array structure' do
+    expect(compile_to_catalog("notify { String({'a'=>['b'],'c'=>[{'d'=>'e'}]}.upcase == {'A'=>['B'],'C'=>[{'D'=>'E'}]}): }")).to have_resource('Notify[true]')
+  end
+
+end
