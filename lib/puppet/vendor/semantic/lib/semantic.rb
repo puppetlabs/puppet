0cb93b8ff770b1c5fcbb6e144f0189eb90cb56ef
Jorie Tappa <jorie@jorietappa.com>
2018-05-14 10:44:13 -0500

    (PUP-7157) remove 'semantic' shim from puppet

--- lib/puppet/vendor/semantic/lib/semantic.rb
+++
@@ -1,5 +0,0 @@
-require 'puppet/vendor/semantic_puppet/lib/semantic_puppet'
-
-$stderr.puts "Warning: Puppet's internal vendored libraries are Private APIs and can change without warning. The 'semantic' library has been replaced with 'semantic_puppet'."
-
-Semantic = SemanticPuppet
