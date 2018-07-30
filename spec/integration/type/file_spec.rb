be91caa1e194020e9d457bda40dfa394bb3510a1
Kris Bosland <kris.bosland@puppet.com>
2018-07-30 15:13:08 -0700

    (PUP-5921) Issue a deprecation warning when users set source_permissions.
    After this change, use of the source_permissions parameter will issue a deprecation warning.
    Pluginsync still uses source_permissions, but this code will not issue a warning
    during pluginsync.

--- spec/integration/type/file_spec.rb
+++ spec/integration/type/file_spec.rb
@@ -602,7 +602,7 @@ describe Puppet::Type.type(:file), :uses_checksums => true do
       end
     end
 
-    it "should not give a deprecation warning when given actual content" do
+    it "should not give a checksum deprecation warning when given actual content" do
       Puppet.expects(:puppet_deprecation_warning).never
       catalog.add_resource described_class.new(:path => path, :content => 'this is content')
       catalog.apply
@@ -613,14 +613,14 @@ describe Puppet::Type.type(:file), :uses_checksums => true do
         let(:filebucket_digest) { method(:digest) }
       end
 
-      it "should give a deprecation warning" do
+      it "should give a checksum deprecation warning" do
         Puppet.expects(:puppet_deprecation_warning).with('Using a checksum in a file\'s "content" property is deprecated. The ability to use a checksum to retrieve content from the filebucket using the "content" property will be removed in a future release. The literal value of the "content" property will be written to the file. The checksum retrieval functionality is being replaced by the use of static catalogs. See https://puppet.com/docs/puppet/latest/static_catalogs.html for more information.', {:file => 'my/file.pp', :line => 5})
         d = digest("this is some content")
         catalog.add_resource described_class.new(:path => path, :content => "{#{digest_algorithm}}#{d}")
         catalog.apply
       end
 
-      it "should not give a deprecation warning when no content is specified while checksum and checksum value are used" do
+      it "should not give a checksum deprecation warning when no content is specified while checksum and checksum value are used" do
         Puppet.expects(:puppet_deprecation_warning).never
         d = digest("this is some content")
         catalog.add_resource described_class.new(:path => path, :checksum => digest_algorithm, :checksum_value => d)
@@ -1281,6 +1281,21 @@ describe Puppet::Type.type(:file), :uses_checksums => true do
   end
 
   describe "when sourcing" do
+    it "should give a deprecation warning when the user sets source_permissions" do
+      Puppet.expects(:puppet_deprecation_warning).with(
+        'The `source_permissions` parameter is deprecated. Explicitly set `owner`, `group`, and `mode`.', 
+        {:file => 'my/file.pp', :line => 5})
+
+      catalog.add_resource described_class.new(:path => path, :content => 'this is content', :source_permissions => :use_when_creating)
+      catalog.apply
+    end
+
+    it "should not give a deprecation warning when the user does not set source_permissions" do
+      Puppet.expects(:puppet_deprecation_warning).never
+      catalog.add_resource described_class.new(:path => path, :content => 'this is content')
+      catalog.apply
+    end
+
     with_checksum_types "source", "default_values" do
       before(:each) do
         set_mode(0770, checksum_file)
