4574b279b6d94940a4d3cddbe3bd4b8a990f09d1
Josh Cooper <josh@puppet.com>
2018-08-03 21:52:30 -0700

    (maint) Expect false not falsey
    YAML.load and YAML.load_file return false if the content is empty.
    Tighten the expectation for Puppet::Util::Yaml.load_file to expect
    false, but not nil:
    
        irb(main):002:0> YAML.load('')
        => false
        irb(main):003:0> FileUtils.touch('empty')
        => ["empty"]
        irb(main):004:0> YAML.load_file('empty')
        => false

--- spec/unit/util/yaml_spec.rb
+++ spec/unit/util/yaml_spec.rb
@@ -37,7 +37,7 @@ describe Puppet::Util::Yaml do
     it "returns false" do
       Puppet::FileSystem.touch(filename)
 
-      expect(Puppet::Util::Yaml.load_file(filename)).to be_falsey
+      expect(Puppet::Util::Yaml.load_file(filename)).to eq(false)
     end
 
     it "allows return value to be overridden" do
