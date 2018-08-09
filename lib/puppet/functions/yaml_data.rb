92cf2625769b2a7fd59c8a7b9f332b3daa70cc48
Josh Cooper <josh@puppet.com>
2018-08-09 15:33:29 -0700

    (PUP-7834) Safely load yaml_data function output
    As described in PUP-7594 and commit 6dd67c3bd7622aae65da22cf093fafadb755bdc4
    hiera hash keys can be symbols and are converted to strings. The
    yaml_data function is used to load the data, and must accept symbols.
    
    Previously, we always provided location information if YAML.load
    produced a class the caller didn't expect. Now we only provide location
    information if the unexpected class is a symbol, string, numeric, etc.
    If an unsafe class would be produced, then we treat it is as a syntax
    error and no location information is included.

--- lib/puppet/functions/yaml_data.rb
+++ lib/puppet/functions/yaml_data.rb
@@ -21,7 +21,7 @@ Puppet::Functions.create_function(:yaml_data) do
     path = options['path']
     context.cached_file_data(path) do |content|
       begin
-        data = YAML.load(content, path)
+        data = Puppet::Util::Yaml.safe_load(content, [Symbol], path)
         if data.is_a?(Hash)
           Puppet::Pops::Lookup::HieraConfig.symkeys_to_string(data)
         else
@@ -30,10 +30,8 @@ Puppet::Functions.create_function(:yaml_data) do
           Puppet.warning(msg)
           {}
         end
-      rescue YAML::SyntaxError => ex
-        # Psych errors includes the absolute path to the file, so no need to add that
-        # to the message
-        raise Puppet::DataBinding::LookupError, "Unable to parse #{ex.message}"
+      rescue Puppet::Util::Yaml::YamlLoadError => ex
+        raise Puppet::DataBinding::LookupError, "Unable to parse (%{path}): %{message}" % { path: path, message: ex.message }
       end
     end
   end
