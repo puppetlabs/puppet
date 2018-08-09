f75ec9b68ccd51e5addcf98264ee861ec1902f59
Josh Cooper <josh@puppet.com>
2018-08-09 16:18:45 -0700

    (maint) Remove dead to_yaml/from_yaml overrides
    The base yaml terminus used to implement to_yaml and from_yaml, see
    2a685f9194c5eb98b67f9759a90a8a193983c467. However, it was removed
    in 3c4669dc042baa6aaf45835b1e94e275fce1668f, so the overriden methods
    were never called. Also since then, we use Puppet::Util::PsychSupport to
    ensure all yaml serialization goes through to_data_hash and
    from_data_hash methods. As a result, we no longer emit objects that
    cause the self-referential issue mentioned in the comment.

--- lib/puppet/indirector/catalog/yaml.rb
+++ lib/puppet/indirector/catalog/yaml.rb
@@ -3,20 +3,4 @@ require 'puppet/indirector/yaml'
 
 class Puppet::Resource::Catalog::Yaml < Puppet::Indirector::Yaml
   desc "Store catalogs as flat files, serialized using YAML."
-
-  private
-
-  # Override these, because yaml doesn't want to convert our self-referential
-  # objects.  This is hackish, but eh.
-  def from_yaml(text)
-    if config = YAML.load(text)
-      return config
-    end
-  end
-
-  def to_yaml(config)
-    # We can't yaml-dump classes.
-    #config.edgelist_class = nil
-    YAML.dump(config)
-  end
 end
