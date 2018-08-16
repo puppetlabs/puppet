86682f75ad456ce06f74aa3b8e56a35dc952a48b
Justin Stoller <justin.stoller@gmail.com>
2018-08-15 21:36:29 -0700

    (maint) Correctly deprecate configprint

--- lib/puppet/defaults.rb
+++ lib/puppet/defaults.rb
@@ -257,8 +257,9 @@ module Puppet
           on the CLI.",
     },
     :configprint => {
-        :default  => "",
-        :desc     => "Prints the value of a specific configuration setting.  If the name of a
+        :default    => "",
+        :deprecated => :completely,
+        :desc       => "Prints the value of a specific configuration setting.  If the name of a
           setting is provided for this, then the value is printed and puppet
           exits.  Comma-separate multiple values.  For a list of all values,
           specify 'all'. This setting is deprecated, the 'puppet config' command replaces this functionality.",
