a99f401792a49fce27bac5d89c7da81643a7a916
Maggie Dreyer <maggie@puppet.com>
2018-08-24 15:39:42 -0700

    Remove CA-related faces

--- lib/puppet/application/certificate_revocation_list.rb
+++
@@ -1,7 +0,0 @@
-require 'puppet/application/indirection_base'
-
-# NOTE: this is using an "old" naming convention (underscores instead of camel-case), for backwards
-#  compatibility with 2.7.x.  When the old naming convention is officially and publicly deprecated,
-#  this should be changed to camel-case.
-class Puppet::Application::Certificate_revocation_list < Puppet::Application::IndirectionBase
-end
