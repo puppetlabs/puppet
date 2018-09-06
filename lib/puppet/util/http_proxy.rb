1fdd75f089ab8c9e3947c7820c85465e4edadf71
Matthias Baur <m.baur@syseleven.de>
2018-09-06 13:02:44 +0200

    (PUP-9109) Add query parameters for HTTP request
    This commit adds the query parameters to the HTTP request done by
    `request_with_redirects`. This is needed for the `source` attribute of
    the `file` resource type if the source URL contains query parameters.
    
    The code itself was extracted from #5002, originally developed by
    @mcasper. The reason for the extraction is:
    * The original PR seems to be stale.
    * The PR fixes another (S3) issue as well

--- lib/puppet/util/http_proxy.rb
+++ lib/puppet/util/http_proxy.rb
@@ -195,7 +195,7 @@ module Puppet::Util::HttpProxy
         headers.merge!({"Accept-Encoding" => Puppet::Network::HTTP::Compression::ACCEPT_ENCODING})
       end
 
-      response = proxy.send(:head, current_uri.path, headers)
+      response = proxy.send(:head, current_uri, headers)
       Puppet.debug("HTTP HEAD request to #{current_uri} returned #{response.code} #{response.message}")
 
       if [301, 302, 307].include?(response.code.to_i)
@@ -206,9 +206,9 @@ module Puppet::Util::HttpProxy
 
       if method != :head
         if block_given?
-          response = proxy.send("request_#{method}".to_sym, current_uri.path, headers, &block)
+          response = proxy.send("request_#{method}".to_sym, current_uri, headers, &block)
         else
-          response = proxy.send(method, current_uri.path, headers)
+          response = proxy.send(method, current_uri, headers)
         end
 
         Puppet.debug("HTTP #{method.to_s.upcase} request to #{current_uri} returned #{response.code} #{response.message}")
