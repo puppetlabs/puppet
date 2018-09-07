8ab28ae485936d7b9d2b471521032a04d918de04
Josh Cooper <josh@puppet.com>
2018-09-07 00:07:34 -0700

    (maint) Refactor Puppet::SSL::Host creation
    Create the host in one place, and pass it through to each action.

--- lib/puppet/application/ssl.rb
+++ lib/puppet/application/ssl.rb
@@ -47,42 +47,42 @@ HELP
     end
 
     Puppet.settings.use(:main, :agent)
+    Puppet::SSL::Host.ca_location = :remote
+    host = Puppet::SSL::Host.new(options[:certname])
 
     action = command_line.args.first
     case action
     when 'submit_request'
-      submit_request(options[:certname])
-      download_cert(options[:certname])
-      exit(0)
+      submit_request(host)
+      download_cert(host)
     when 'download_cert'
-      download_cert(options[:certname])
-      exit(0)
+      download_cert(host)
     else
       puts "Unknown action '#{action}'"
       exit(1)
     end
+
+    exit(0)
   end
 
-  def submit_request(certname = nil)
-    Puppet::SSL::Host.ca_location = :remote
-    ssl = Puppet::SSL::Host.new(certname)
-    ssl.ensure_ca_certificate
-    ssl.submit_request
-    puts "Submitted certificate request for '#{ssl.name}' to https://#{Puppet[:ca_server]}:#{Puppet[:ca_port]}"
+  def submit_request(host)
+    host.ensure_ca_certificate
+
+    host.submit_request
+    puts "Submitted certificate request for '#{host.name}' to https://#{Puppet[:ca_server]}:#{Puppet[:ca_port]}"
   rescue => e
     puts "Failed to submit certificate request: #{e.message}"
     exit(1)
   end
 
-  def download_cert(certname = nil)
-    Puppet::SSL::Host.ca_location = :remote
-    ssl = Puppet::SSL::Host.new(certname)
-    ssl.ensure_ca_certificate
-    puts "Downloading certificate '#{ssl.name}' from https://#{Puppet[:ca_server]}:#{Puppet[:ca_port]}"
-    if cert = ssl.download_host_certificate
-      puts "Downloaded certificate '#{ssl.name}' with fingerprint #{cert.fingerprint}"
+  def download_cert(host)
+    host.ensure_ca_certificate
+
+    puts "Downloading certificate '#{host.name}' from https://#{Puppet[:ca_server]}:#{Puppet[:ca_port]}"
+    if cert = host.download_host_certificate
+      puts "Downloaded certificate '#{host.name}' with fingerprint #{cert.fingerprint}"
     else
-      puts "No certificate for '#{ssl.name}' on CA"
+      puts "No certificate for '#{host.name}' on CA"
     end
   rescue => e
     puts "Failed to download certificate: #{e.message}"
