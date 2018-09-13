ad1bc1704be30031ac77f04ea6ba503384c80382
Maggie Dreyer <maggie@puppet.com>
2018-09-13 09:03:30 -0700

    (PUP-9022) Update help text of `puppet cert` to reference alternatives
    This commit updates the error/help output of `puppet cert` to explain in
    more detail the commands that replace it. Not all of them are currently
    available in `puppetserver ca`, so the user is pointed to use `openssl`
    directly instead.

--- lib/puppet/application/cert.rb
+++ lib/puppet/application/cert.rb
@@ -103,11 +103,66 @@ class Puppet::Application::Cert < Puppet::Application
   end
 
   def help
-    _("This command is no longer functional, please use `puppetserver ca` instead.")
+     <<-HELP
+This command is no longer functional, please use `puppetserver ca` instead.
+
+puppet-cert(8) -- #{summary}
+========
+
+ACTIONS
+-------
+Every action except 'list' and 'generate' requires a hostname to act on,
+unless the '--all' option is set.
+
+* clean:
+  Use `puppetserver ca clean --certname NAME[,NAME...]`
+
+* fingerprint:
+  Use openssl directly:
+  `openssl x509 -noout -fingerprint -<digest> -inform pem -in certificate.crt`
+
+* generate:
+  Use `puppetserver ca generate --certname NAME[,NAME...]`
+
+* list:
+  Use `puppetserver ca list [--all]`
+
+* print:
+  Use openssl directly:
+  `openssl x509 -noout -text -in certificate.pem`
+
+* revoke:
+  Use `puppetserver ca revoke --cerntname NAME[,NAME...]`
+
+* sign:
+  Use `puppetserver ca sign --cerntname NAME[,NAME...]`
+
+* verify:
+  Use `puppet ssl verify [--certname NAME]`
+
+* reinventory:
+  Removed.
+
+OPTIONS
+-------
+There are a couple important notes about previously-supported options.
+
+* --allow-dns-alt-names:
+  In order to sign certificates with subject alternative names using
+  `puppetserver ca sign`, the `allow-subject-alt-names` setting must be
+  set to true in the `certificate-authority` section of Puppet Server's
+  config.
+
+* --allow-authorization-extensions:
+  In order to sign certificates with authorization extensions using
+  `puppetserver ca sign`, the `allow-authorization-extensions` setting must be
+  set to true in the `certificate-authority` section of Puppet Server's
+  config.
+HELP
   end
 
   def main
-    Puppet.error _("This command is no longer functional, please use `puppetserver ca` instead.")
+    help
   end
 
   def setup
