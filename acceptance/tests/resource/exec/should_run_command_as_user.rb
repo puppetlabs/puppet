ba2dae95242934c1d70158fed3c8b26105bb5b2b
Enis Inan <enis.inan@puppet.com>
2018-09-17 23:35:51 -0700

    (PUP-9117) Fix exec/should_run_command_as_user test failure on Arista
    This test failed in the initial agent.user_absent(username) step,
    which is meant to ensure that our stubbed user does not exist on
    our agent before we run the test. The reason it failed there is
    because Beaker::Host's user_absent (and user_present) functions do
    not work correctly on Arista (EOS). This is because `getent passwd <user>`
    always returns an exit code of 0 on Arista, even if the user does
    not exist. We use that command to check for user existence/nonexistence
    before ensuring that they're absent/present, respectively.
    
    This commit fixes the test by creating wrappers around `user_present`
    and `user_absent` until the issue is fixed in Beaker.

--- acceptance/tests/resource/exec/should_run_command_as_user.rb
+++ acceptance/tests/resource/exec/should_run_command_as_user.rb
@@ -8,6 +8,29 @@ test_name "The exec resource should be able to run commands as a different user"
     "pl#{rand(999999).to_i}"
   end
 
+  # TODO: Remove the wrappers to user_present
+  # and user_absent if Beaker::Host's user_present
+  # and user_absent functions are fixed to work with
+  # Arista (EOS).
+
+  def user_present(host, username)
+    case host['platform']
+    when /eos/
+      on(host, "useradd #{username}")
+    else
+      host.user_present(username)
+    end
+  end
+
+  def user_absent(host, username)
+    case host['platform']
+    when /eos/
+      on(host, "userdel #{username}", acceptable_exit_codes: [0, 1])
+    else
+      host.user_absent(username)
+    end
+  end
+
   def exec_resource_manifest(params = {})
     default_params = {
       :logoutput => true,
@@ -33,9 +56,9 @@ MANIFEST
     username = random_username
 
     # Create our user. Ensure that we start with a clean slate.
-    agent.user_absent(username)
-    agent.user_present(username)
-    teardown { agent.user_absent(username) }
+    user_absent(agent, username)
+    user_present(agent, username)
+    teardown { user_absent(agent, username) }
 
     tmpdir = agent.tmpdir("forbidden")
     on(agent, "chmod 700 #{tmpdir}")
@@ -48,7 +71,7 @@ MANIFEST
       on(agent, "cd #{tmpdir} && puppet apply #{tmpfile} --detailed-exitcodes", acceptable_exit_codes: [0, 2])
     end
 
-    step "Runs the command even when the user doesn't have permission to access the cwd" do
+    step "Runs the command even when the user doesn't have permission to access the specified cwd" do
       apply_manifest_on(agent, exec_resource_manifest(user: username, cwd: tmpdir), catch_failures: true)
     end
   end
