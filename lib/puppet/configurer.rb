782ae901e99b31eea987785966814bf23bff9956
Josh Cooper <josh@puppet.com>
2018-09-20 16:06:33 -0700

    (PUP-9698) Request status of the master service
    The 'simple' status includes the status of all trapperkeeper services
    such as the broker and filesync services. Request status for the 'master'
    service specifically.
    
    Backports commit 604212978 from PUP-9159

--- lib/puppet/configurer.rb
+++ lib/puppet/configurer.rb
@@ -387,8 +387,9 @@ class Puppet::Configurer
       port = server[1] || Puppet[:masterport]
       begin
         http = Puppet::Network::HttpPool.http_ssl_instance(host, port)
-        response = http.get('/status/v1/simple')
+        response = http.get('/status/v1/simple/master')
         return [host, port] if response.is_a?(Net::HTTPOK) || response.is_a?(Net::HTTPForbidden)
+
         Puppet.debug(_("Puppet server %{host}:%{port} is unavailable: %{code} %{reason}") %
                      { host: host, port: port, code: response.code, reason: response.message })
       rescue => detail
