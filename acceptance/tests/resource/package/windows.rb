fd04cf8c65414f41100c2a1e5e95232a44029790
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2018-10-01 14:46:10 -0700

    (PUP-6920) Execute windows package installs in source dir
    This commit updates the windows package provider to execute package
    installations in the same directory as the package resource.

---
+++ acceptance/tests/resource/package/windows.rb
@@ -0,0 +1,61 @@
+test_name "Windows Package Provider" do
+  confine :to, :platform => 'windows'
+
+  tag 'audit:medium',
+      'audit:acceptance'
+
+  require 'puppet/acceptance/windows_utils'
+  extend Puppet::Acceptance::WindowsUtils
+
+  def package_manifest(name, params, installer_source)
+    params_str = params.map do |param, value|
+      value_str = value.to_s
+      value_str = "\"#{value_str}\"" if value.is_a?(String)
+
+      "  #{param} => #{value_str}"
+    end.join(",\n")
+
+    <<-MANIFEST
+package { '#{name}':
+  source => '#{installer_source}',
+  #{params_str}
+}
+MANIFEST
+  end
+
+  mock_package = {
+    :name => "MockPackage"
+  }
+
+  agents.each do |agent|
+    tmpdir = agent.tmpdir("mock_installer")
+    installer_location = create_mock_package(agent, tmpdir, mock_package)
+
+    step 'Verify that ensure = present installs the package' do
+      apply_manifest_on(agent, package_manifest(mock_package[:name], {ensure: :present}, installer_location))
+      assert(package_installed?(agent, mock_package[:name]), 'Package succesfully installed')
+    end
+
+    step 'Verify that ensure = absent removes the package' do
+      apply_manifest_on(agent, package_manifest(mock_package[:name], {ensure: :absent}, installer_location))
+      assert_equal(false, package_installed?(agent, mock_package[:name]), 'Package successfully Uninstalled')
+    end
+
+    tmpdir = agent.tmpdir("mock_installer")
+    mock_package[:name] = "MockPackageWithFile"
+    mock_package[:install_commands] = 'System.IO.File.ReadAllLines("install.txt");'
+    installer_location = create_mock_package(agent, tmpdir, mock_package)
+
+    step 'Verify that ensure = present installs a package that requires additional resources' do
+      create_remote_file(agent, "#{tmpdir}/install.txt", 'foobar')
+      apply_manifest_on(agent, package_manifest(mock_package[:name], {ensure: :present}, installer_location))
+      assert(package_installed?(agent, mock_package[:name]), 'Package succesfully installed')
+    end
+
+    step 'Verify that ensure = absent removes the package that required additional resources' do
+      apply_manifest_on(agent, package_manifest(mock_package[:name], {ensure: :absent}, installer_location))
+      assert_equal(false, package_installed?(agent, mock_package[:name]), 'Package successfully Uninstalled')
+    end
+  end
+
+end
