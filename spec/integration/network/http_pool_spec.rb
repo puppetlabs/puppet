ac64ad03298cd77462b60af97a92b1ab6c2511dd
Josh Cooper <josh@puppet.com>
2019-04-16 16:58:54 -0700

    (PUP-9649) Move tests for deprecated method into an example group
    Move tests for `HttpPool.http_instance` into an example group.
    
    Backport commit d53cc016093029ae6cd56c2fba3a0eff83831863

--- spec/integration/network/http_pool_spec.rb
+++ spec/integration/network/http_pool_spec.rb
@@ -3,7 +3,7 @@ require 'puppet_spec/https'
 require 'puppet_spec/files'
 require 'puppet/network/http_pool'
 
-describe Puppet::Network::HttpPool, unless: Puppet::Util::Platform.jruby? do
+describe Puppet::Network::HttpPool do
   include PuppetSpec::Files
 
   before :all do
@@ -14,103 +14,109 @@ describe Puppet::Network::HttpPool, unless: Puppet::Util::Platform.jruby? do
     WebMock.enable!
   end
 
-  let(:hostname) { '127.0.0.1' }
-  let(:wrong_hostname) { 'localhost' }
-  let(:server) { PuppetSpec::HTTPSServer.new }
-  let(:ssl_host) {
-    # use server's cert/key as the client cert/key
-    host = Puppet::SSL::Host.new
-    host.key = Puppet::SSL::Key.from_instance(server.server_key, host.name)
-    host.certificate = Puppet::SSL::Certificate.from_instance(server.server_cert, host.name)
-    host
-  }
-
-  before(:each) do
-    ssldir = tmpdir('http_pool')
-    Puppet[:ssldir] = ssldir
-    Puppet.settings.use(:main, :ssl)
-
-    File.write(Puppet[:localcacert], server.ca_cert.to_pem)
-    File.write(Puppet[:hostcrl], server.ca_crl.to_pem)
-    File.write(Puppet[:hostcert], server.server_cert.to_pem)
-    File.write(Puppet[:hostprivkey], server.server_key.to_pem)
-
+  before :each do
     # make sure we don't take too long
     Puppet[:http_connect_timeout] = '5s'
   end
 
-  # Can't use `around(:each)` because it will cause ssl_host to be
-  # created outside of any rspec example, and $confdir won't be set
-  before(:each) do
-    Puppet.push_context(ssl_host: ssl_host)
-  end
+  let(:hostname) { '127.0.0.1' }
+  let(:wrong_hostname) { 'localhost' }
+  let(:server) { PuppetSpec::HTTPSServer.new }
 
-  after (:each) do
-    Puppet.pop_context
-  end
+  context "when calling deprecated HttpPool methods" do
+    let(:ssl_host) {
+      # use server's cert/key as the client cert/key
+      host = Puppet::SSL::Host.new
+      host.key = Puppet::SSL::Key.from_instance(server.server_key, host.name)
+      host.certificate = Puppet::SSL::Certificate.from_instance(server.server_cert, host.name)
+      host
+    }
 
-  def connection(host, port)
-    Puppet::Network::HttpPool.http_instance(host, port, use_ssl: true)
-  end
+    before(:each) do
+      ssldir = tmpdir('http_pool')
+      Puppet[:ssldir] = ssldir
+      Puppet.settings.use(:main, :ssl)
 
-  shared_examples_for 'HTTPS client' do
-    it "connects over SSL" do
-      server.start_server do |port|
-        http = connection(hostname, port)
-        res = http.get('/')
-        expect(res.code).to eq('200')
+      File.write(Puppet[:localcacert], server.ca_cert.to_pem)
+      File.write(Puppet[:hostcrl], server.ca_crl.to_pem)
+      File.write(Puppet[:hostcert], server.server_cert.to_pem)
+      File.write(Puppet[:hostprivkey], server.server_key.to_pem)
+    end
+
+    # Can't use `around(:each)` because it will cause ssl_host to be
+    # created outside of any rspec example, and $confdir won't be set
+    before(:each) do
+      Puppet.push_context(ssl_host: ssl_host)
+    end
+
+    after (:each) do
+      Puppet.pop_context
+    end
+
+    def connection(host, port)
+      Puppet::Network::HttpPool.http_instance(host, port, use_ssl: true)
+    end
+
+    shared_examples_for 'HTTPS client' do
+      it "connects over SSL" do
+        server.start_server do |port|
+          http = connection(hostname, port)
+          res = http.get('/')
+          expect(res.code).to eq('200')
+        end
+      end
+
+      it "raises if the server's cert doesn't match the hostname we connected to" do
+        server.start_server do |port|
+          http = connection(wrong_hostname, port)
+          expect {
+            http.get('/')
+          }.to raise_error { |err|
+            pending("PUP-8213") if RUBY_VERSION.to_f >= 2.4
+
+            expect(err).to be_instance_of(Puppet::Error)
+            expect(err.message).to match(/\AServer hostname '#{wrong_hostname}' did not match server certificate; expected one of (.+)/)
+
+            md = err.message.match(/expected one of (.+)/)
+            expect(md[1].split(', ')).to contain_exactly('127.0.0.1', 'DNS:127.0.0.1', 'DNS:127.0.0.2')
+          }
+        end
+      end
+
+      it "raises if the server's CA is unknown" do
+        # File must exist and by not empty so DefaultValidator doesn't
+        # downgrade to VERIFY_NONE, so use a different CA that didn't
+        # issue the server's cert
+        capath = tmpfile('empty')
+        File.write(capath, PuppetSpec::HTTPSServer::UNKNOWN_CA)
+        Puppet[:localcacert] = capath
+        Puppet[:certificate_revocation] = false
+
+        server.start_server do |port|
+          http = connection(hostname, port)
+          expect {
+            http.get('/')
+          }.to raise_error(Puppet::Error,
+                           %r{certificate verify failed.* .self signed certificate in certificate chain for /CN=Test CA.})
+        end
       end
     end
 
-    it "raises if the server's cert doesn't match the hostname we connected to" do
-      server.start_server do |port|
-        http = connection(wrong_hostname, port)
-        expect {
-          http.get('/')
-        }.to raise_error { |err|
-          pending("PUP-8213") if RUBY_VERSION.to_f >= 2.4
 
-          expect(err).to be_instance_of(Puppet::Error)
-          expect(err.message).to match(/\AServer hostname '#{wrong_hostname}' did not match server certificate; expected one of (.+)/)
-
-          md = err.message.match(/expected one of (.+)/)
-          expect(md[1].split(', ')).to contain_exactly('127.0.0.1', 'DNS:127.0.0.1', 'DNS:127.0.0.2')
-        }
-      end
+    context "when using single use HTTPS connections" do
+      include_examples 'HTTPS client'
     end
 
-    it "raises if the server's CA is unknown" do
-      # File must exist and by not empty so DefaultValidator doesn't
-      # downgrade to VERIFY_NONE, so use a different CA that didn't
-      # issue the server's cert
-      capath = tmpfile('empty')
-      File.write(capath, PuppetSpec::HTTPSServer::UNKNOWN_CA)
-      Puppet[:localcacert] = capath
-      Puppet[:certificate_revocation] = false
-
-      server.start_server do |port|
-        http = connection(hostname, port)
-        expect {
-          http.get('/')
-        }.to raise_error(Puppet::Error,
-                         %r{certificate verify failed.* .self signed certificate in certificate chain for /CN=Test CA.})
+    context "when using persistent HTTPS connections" do
+      around :each do |example|
+        pool = Puppet::Network::HTTP::Pool.new
+        Puppet.override(:http_pool => pool) do
+          example.run
+        end
+        pool.close
       end
+
+      include_examples 'HTTPS client'
     end
-  end
-
-  context "when using single use HTTPS connections" do
-    include_examples 'HTTPS client'
-  end
-
-  context "when using persistent HTTPS connections" do
-    around :each do |example|
-      pool = Puppet::Network::HTTP::Pool.new
-      Puppet.override(:http_pool => pool) do
-        example.run
-      end
-      pool.close
-    end
-
-    include_examples 'HTTPS client'
   end
 end
