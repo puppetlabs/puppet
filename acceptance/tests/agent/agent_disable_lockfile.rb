f87ba04c21f49b35e33db3cde1f315dee7d64353
John Duarte <john.duarte@puppet.com>
2019-04-30 11:13:09 -0700

    (PUP-9653) Refactor agent_disable_lockfile test
    This commit refactors the agent_disable_lockfile test:
    
    1. Remove the use of `with_puppet_running_on` since this test does not
       test server functionality.
    2. Use the block style for `test_name` in accordance with the
       [beaker style guide](https://github.com/puppetlabs/beaker/blob/master/docs/concepts/style_guide.md)
    
    [skip ci]

--- acceptance/tests/agent/agent_disable_lockfile.rb
+++ acceptance/tests/agent/agent_disable_lockfile.rb
@@ -1,100 +1,89 @@
-test_name "C4553 - agent --disable/--enable functionality should manage the agent lockfile properly"
-confine :except, :platform => 'cisco_nexus' #See BKR-749
+test_name "C4553 - agent --disable/--enable functionality should manage the agent lockfile properly" do
+  confine :except, platform: "cisco_nexus" # See BKR-749
 
-tag 'audit:integration', # lockfile uses the standard `vardir` location to store/query lockfile.
-                         # The validation of the `vardir` at the OS level
-                         # should be accomplished in another test.
-    'audit:medium',
-    'audit:refactor'     # This test should not require a master. Remove the use of `with_puppet_running_on`.
+  require "json"
 
-#
-# This test is intended to ensure that puppet agent --enable/--disable
-#  work properly, both in terms of complying with our public "API" around
-#  lockfile semantics ( http://links.puppet.com/agent_lockfiles ), and
-#  in terms of actually restricting or allowing new agent runs to begin.
-#
+  tag 'audit:integration', # lockfile uses the standard `vardir` location to store/query lockfile.
+                           # The validation of the `vardir` at the OS level
+                           # should be accomplished in another test.
+      'audit:medium'
 
-require 'puppet/acceptance/temp_file_utils'
+  #
+  # This test is intended to ensure that puppet agent --enable/--disable
+  #  work properly, both in terms of complying with our public "API" around
+  #  lockfile semantics ( http://links.puppet.com/agent_lockfiles ), and
+  #  in terms of actually restricting or allowing new agent runs to begin.
+  #
 
-extend Puppet::Acceptance::TempFileUtils
+  require "puppet/acceptance/temp_file_utils"
 
-initialize_temp_dirs()
-@all_tests_passed = false
+  extend Puppet::Acceptance::TempFileUtils
 
+  initialize_temp_dirs
+  @all_tests_passed = false
 
-###############################################################################
-# BEGIN TEST LOGIC
-###############################################################################
+  ###############################################################################
+  # BEGIN TEST LOGIC
+  ###############################################################################
 
-teardown do
-  if @all_tests_passed then
-    remove_temp_dirs()
+  teardown do
+    remove_temp_dirs if @all_tests_passed
+    agents.each do |agent|
+      on(agent, puppet("agent", "--enable"))
+    end
   end
-  agents.each do |agent|
-    on(agent, puppet('agent', "--enable"))
-  end
-end
 
-tuples = [
+  fakemaster = ('a'..'z').to_a.shuffle[0,10].join
+
+  tuples = [
     ["reason not specified", false],
     ["I'm busy; go away.'", true]
-]
+  ]
 
-with_puppet_running_on(master, {}) do
   tuples.each do |expected_message, explicitly_specify_message|
-    step "disable the agent; specify message? '#{explicitly_specify_message}', message: '#{expected_message}'" do
-      agents.each do |agent|
-        if (explicitly_specify_message)
-          on(agent, puppet('agent', "--disable \"#{expected_message}\""))
+    agents.each do |agent|
+      step "disable the agent; specify message? '#{explicitly_specify_message}', message: '#{expected_message}'" do
+        if explicitly_specify_message
+          on(agent, puppet("agent", "--disable \"#{expected_message}\""))
         else
-          on(agent, puppet('agent', "--disable"))
+          on(agent, puppet("agent", "--disable"))
         end
 
         agent_disabled_lockfile = "#{agent.puppet['vardir']}/state/agent_disabled.lock"
-        unless file_exists?(agent, agent_disabled_lockfile) then
-          fail_test("Failed to create disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
-        end
+        assert(agent.file_exist?(agent_disabled_lockfile),
+               "Failed to create disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
+
         lock_file_content = file_contents(agent, agent_disabled_lockfile)
 
-        # This is a hack; we should parse the JSON into a hash, but I don't
-        # think I have a library available from the acceptance test framework
-        # that I can use to do that.  So I'm falling back to <gasp> regex.
-        lock_file_content_regex = /"disabled_message"\s*:\s*"#{expected_message}"/
-        unless lock_file_content =~ lock_file_content_regex
-          fail_test("Disabled lock file contents invalid; expected to match '#{lock_file_content_regex}', got '#{lock_file_content}' on agent '#{agent}'")
-        end
+        file_lock_json = JSON.parse(lock_file_content)
+        assert_equal(expected_message, file_lock_json["disabled_message"])
       end
-    end
 
-    step "attempt to run the agent (message: '#{expected_message}')" do
-      agents.each do |agent|
-        on(agent, puppet('agent', "--test --server #{master}"),
-                     :acceptable_exit_codes => [1]) do
+      step "attempt to run disabled agent (message: '#{expected_message}')" do
+        skip_test "unable to assert match in Japanese" if agent["locale"] == "ja"
+        on(agent, puppet("agent", "--test --server #{fakemaster}"),
+           acceptable_exit_codes: [1]) do |result|
           disabled_regex = /administratively disabled.*'#{expected_message}'/
-          unless result.stdout =~ disabled_regex
-            fail_test("Unexpected output from attempt to run agent disabled; expecting to match '#{disabled_regex}', got '#{result.stdout}' on agent '#{agent}'") unless agent['locale'] == 'ja'
-          end
+          assert_match(disabled_regex, result.stdout)
         end
       end
-    end
 
-    step "enable the agent (message: '#{expected_message}')" do
-      agents.each do |agent|
-
+      step "enable the agent (message: '#{expected_message}')" do
         agent_disabled_lockfile = "#{agent.puppet['vardir']}/state/agent_disabled.lock"
-        on(agent, puppet('agent', "--enable"))
-        if file_exists?(agent, agent_disabled_lockfile) then
-          fail_test("Failed to remove disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
-        end
+        on(agent, puppet("agent", "--enable"))
+        assert_equal(agent.file_exist?(agent_disabled_lockfile), false,
+                     "Failed to remove disabled lock file '#{agent_disabled_lockfile}' on agent '#{agent}'")
       end
-    end
 
-    step "verify that we can run the agent (message: '#{expected_message}')" do
-      agents.each do |agent|
-        on(agent, puppet('agent', "--test --server #{master}"))
+      step "verify that agent is enabled (message: '#{expected_message}')" do
+        # Agent should attempt to run, but fail to connect to fakemaster
+        on(agent, puppet("agent", "--test --server #{fakemaster}"),
+           acceptable_exit_codes: [1]) do |result|
+          assert_match(/Failed to open TCP connection/, result.stdout)
+        end
       end
     end
   end # tuples block
-end # with_puppet_running_on block
 
-@all_tests_passed = true
+  @all_tests_passed = true
+end
