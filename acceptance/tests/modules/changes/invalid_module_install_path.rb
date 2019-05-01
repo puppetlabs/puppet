6c21106a76dbac70a83672e844098c70b434353b
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor modules/changes/invalid_instal
    This commit refactors the `modules/changes/invalid_module_install_path`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/modules/changes/invalid_module_install_path.rb
+++ acceptance/tests/modules/changes/invalid_module_install_path.rb
@@ -1,20 +1,19 @@
-test_name 'puppet module changes (on an invalid module install path)'
+test_name "puppet module changes (on an invalid module install path)" do
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'   # Master is not requiered for this test. Replace with agents.each
-                       # Wrap steps in blocks in accordance with Beaker style guide
+  tag 'audit:medium',
+      'audit:acceptance'
 
-step 'Setup'
-
-stub_forge_on(master)
-testdir = master.tmpdir('module_changes_with_invalid_path')
-
-step 'Run module changes on an invalid module install path'
-on master, puppet("module changes #{testdir}/nginx"), :acceptable_exit_codes => [1] do
-  pattern = Regexp.new([
-%Q{.*Error: Could not find a valid module at "#{testdir}/nginx".*},
-%Q{.*Error: Try 'puppet help module changes' for usage.*},
-  ].join("\n"), Regexp::MULTILINE)
-  assert_match(pattern, result.stderr)
+  agents.each do |agent|
+    testdir = agent.tmpdir("module_changes_with_invalid_path")
+    step "Run module changes on an invalid module install path" do
+      on(agent, puppet("module changes #{testdir}/nginx"),
+         acceptable_exit_codes: [1]) do
+        pattern = Regexp.new([
+          ".*Error: Could not find a valid module at \"#{testdir}/nginx\".*",
+          ".*Error: Try 'puppet help module changes' for usage.*"
+        ].join("\n"), Regexp::MULTILINE)
+        assert_match(pattern, result.stderr)
+      end
+    end
+  end
 end
