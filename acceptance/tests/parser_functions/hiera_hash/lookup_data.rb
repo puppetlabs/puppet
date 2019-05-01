b3bccb2c840f38a58157ec71198742b37bda3b82
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor hiera_hash/lookup_data test
    This commit refactors the `parser_functions/hiera_hash/lookup_data`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/parser_functions/hiera_hash/lookup_data.rb
+++ acceptance/tests/parser_functions/hiera_hash/lookup_data.rb
@@ -1,106 +1,113 @@
-test_name "Lookup data using the hiera_hash parser function"
+require "puppet/acceptance/puppet_type_test_tools.rb"
+test_name "Lookup data using the hiera_hash parser function" do
+  extend Puppet::Acceptance::PuppetTypeTestTools
+  tag 'audit:medium',
+      'audit:acceptance'
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'    # Master is not required for this test. Replace with agents.each
-
-testdir = master.tmpdir('hiera')
-
-step 'Setup'
-
-apply_manifest_on(master, <<-PP, :catch_failures => true)
-File {
-  ensure => directory,
-  mode => "0750",
-  owner => #{master.puppet['user']},
-  group => #{master.puppet['group']},
-}
-file {
-  '#{testdir}':;
-  '#{testdir}/hieradata':;
-  '#{testdir}/environments':;
-  '#{testdir}/environments/production':;
-  '#{testdir}/environments/production/manifests':;
-  '#{testdir}/environments/production/modules':;
-}
-
-file { '#{testdir}/hiera.yaml':
-  ensure  => file,
-  content => '---
-    :backends:
-      - "yaml"
-    :logger: "console"
-    :hierarchy:
-      - "%{fqdn}"
-      - "%{environment}"
-      - "global"
-
-    :yaml:
-      :datadir: "#{testdir}/hieradata"
-  ',
-  mode => "0640";
-}
-
-file { '#{testdir}/hieradata/global.yaml':
-  ensure  => file,
-  content => "---
-    database_user:
-      name: postgres
-      uid: 500
-      gid: 500
-  ",
-  mode => "0640";
-}
-
-file { '#{testdir}/hieradata/production.yaml':
-  ensure  => file,
-  content => "---
-    database_user:
-      shell: '/bin/bash'
-  ",
-  mode => "0640";
-}
-
-file {
-  '#{testdir}/environments/production/modules/ntp/':;
-  '#{testdir}/environments/production/modules/ntp/manifests':;
-}
-
-file { '#{testdir}/environments/production/modules/ntp/manifests/init.pp':
-  ensure => file,
-  content => 'class ntp {
-    $database_user = hiera_hash("database_user")
-
-    notify { "the database user":
-      message => "name: ${database_user["name"]} shell: ${database_user["shell"]}"
-    }
-  }',
-  mode => "0640";
-}
-
-file { '#{testdir}/environments/production/manifests/site.pp':
-  ensure => file,
-  content => "
-    node default {
-      include ntp
-    }",
-  mode => "0640";
-}
-PP
-
-step "Try to lookup hash data"
-
-master_opts = {
-  'main' => {
-    'environmentpath' => "#{testdir}/environments",
-    'hiera_config' => "#{testdir}/hiera.yaml",
-  },
-}
-
-with_puppet_running_on master, master_opts, testdir do
   agents.each do |agent|
-    on(agent, puppet('agent', "-t --server #{master}"), :acceptable_exit_codes => [2])
+    testdir = agent.tmpdir("hiera")
+    confdir = "#{testdir}/puppet"
+    codedir = "#{testdir}/code"
 
-    assert_match("name: postgres shell: /bin/bash", stdout)
+    step "Setup" do
+      apply_manifest_on(agent, <<~PP, catch_failures: true)
+        File {
+          ensure => directory,
+          mode => "0750",
+        }
+
+        file {
+          '#{testdir}/hieradata':;
+          '#{confdir}':;
+          '#{codedir}':;
+          '#{codedir}/environments':;
+          '#{codedir}/environments/production':;
+          '#{codedir}/environments/production/manifests':;
+          '#{codedir}/environments/production/modules':;
+        }
+
+        file { '#{testdir}/puppet/puppet.conf':
+          ensure  => file,
+          content => "
+        [user]
+        environment = production
+        [main]
+        environmentpath = #{codedir}/environments
+        hiera_config = #{testdir}/hiera.yaml
+        ";
+        }
+
+        file { '#{testdir}/hiera.yaml':
+          ensure  => file,
+          content => '---
+            :backends:
+              - "yaml"
+            :logger: "console"
+            :hierarchy:
+              - "%{fqdn}"
+              - "%{environment}"
+              - "global"
+
+            :yaml:
+              :datadir: "#{testdir}/hieradata"
+          ',
+          mode => "0640";
+        }
+
+        file { '#{testdir}/hieradata/global.yaml':
+          ensure  => file,
+          content => "---
+            database_user:
+              name: postgres
+              uid: 500
+              gid: 500
+          ",
+          mode => "0640";
+        }
+
+        file { '#{testdir}/hieradata/production.yaml':
+          ensure  => file,
+          content => "---
+            database_user:
+              shell: '/bin/bash'
+          ",
+          mode => "0640";
+        }
+
+        file {
+          '#{codedir}/environments/production/modules/ntp/':;
+          '#{codedir}/environments/production/modules/ntp/manifests':;
+        }
+
+        file { '#{codedir}/environments/production/modules/ntp/manifests/init.pp':
+          ensure => file,
+          content => 'class ntp {
+            $database_user = hiera_hash("database_user")
+
+            notify { "the database user":
+              message => "name: ${database_user["name"]} shell: ${database_user["shell"]}"
+            }
+          }',
+          mode => "0640";
+        }
+
+        file { '#{codedir}/environments/production/manifests/site.pp':
+          ensure => file,
+          content => "
+            node default {
+              include ntp
+            }",
+          mode => "0640";
+        }
+      PP
+    end
+
+    step "Try to lookup hash data" do
+      on(agent, puppet("apply",
+                       "#{codedir}/environments/production/manifests/site.pp",
+                       confdir: confdir)) do |result|
+        assert_match("name: postgres shell: /bin/bash", result.stdout)
+      end
+    end
   end
 end
