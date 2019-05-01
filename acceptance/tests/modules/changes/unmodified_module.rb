f02c2d68d796061f4a372097b225635a9ba59b2e
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor modules/changes/unmodified_mod
    This commit refactors the `modules/changes/unmodified_module`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/modules/changes/unmodified_module.rb
+++ acceptance/tests/modules/changes/unmodified_module.rb
@@ -1,20 +1,20 @@
-test_name 'puppet module changes (on an unmodified module)'
+test_name "puppet module changes (on an unmodified module)" do
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'   # Master is not required for this test. Replace with agents.each
-                       # Wrap steps in blocks in accordance with Beaker style guide
+  tag 'audit:medium',
+      'audit:acceptance'
 
-step 'Setup'
+  agents.each do |agent|
+    testdir = agent.tmpdir("module_changes_with_unmodified")
+    step "Setup" do
+      stub_forge_on(agent)
+      on agent, puppet("module install pmtacceptance-nginx --modulepath #{testdir}")
+    end
 
-stub_forge_on(master)
-testdir = master.tmpdir('module_changes_with_unmodified')
-
-on master, puppet("module install pmtacceptance-nginx --modulepath #{testdir}")
-
-step 'Run module changes to check an unmodified module'
-on( master, puppet("module changes #{testdir}/nginx"),
-    :acceptable_exit_codes => [0] ) do
-
-  assert_match /No modified files/, stdout
+    step "Run module changes to check an unmodified module" do
+      on(agent, puppet("module changes #{testdir}/nginx"),
+         acceptable_exit_codes: [0]) do
+        assert_match(/No modified files/, stdout)
+      end
+    end
+  end
 end
