003bc337fadc9719a494f103f22c76a575334ce8
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor modules/changes/missing_checks
    This commit refactors the `modules/changes/missing_checksums_json`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/modules/changes/missing_checksums_json.rb
+++ acceptance/tests/modules/changes/missing_checksums_json.rb
@@ -1,42 +1,43 @@
-test_name 'puppet module changes (on a module which is missing checksums.json)'
+test_name "puppet module changes (module missing checksums.json)" do
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'   # Master is not required for this test. Replace with agents.each
-                       # Wrap steps in blocks in accordance with Beaker style guide
+  tag 'audit:medium',
+      'audit:acceptance'
 
-step 'Setup'
+  agents.each do |agent|
+    testdir = agent.tmpdir("module_changes_on_invalid_checksums")
 
-stub_forge_on(master)
-testdir = master.tmpdir('module_changes_on_invalid_checksums')
+    step "Setup" do
+      apply_manifest_on agent, <<~MANIFEST
+        file { '#{testdir}/nginx': ensure => directory;
+               '#{testdir}/nginx/metadata.json': ensure => present,
+                 content => '
+                   {
+                     "name": "puppetlabs-nginx",
+                     "version": "0.0.1",
+                     "author": "Puppet Labs",
+                     "summary": "Nginx Module",
+                     "license": "Apache Version 2.0",
+                     "source": "git://github.com/puppetlabs/puppetlabs-nginx.git",
+                     "project_page": "https://github.com/puppetlabs/puppetlabs-nginx",
+                     "issues_url": "https://github.com/puppetlabs/puppetlabs-nginx",
+                     "dependencies": [
+                       {"name":"puppetlabs-stdlub","version_requirement":">= 1.0.0"}
+                     ]
+                   }'
+        }
+      MANIFEST
+    end
 
-apply_manifest_on master, %Q{
-  file { '#{testdir}/nginx': ensure => directory;
-         '#{testdir}/nginx/metadata.json': ensure => present,
-           content => '
-{
-  "name": "puppetlabs-nginx",
-  "version": "0.0.1",
-  "author": "Puppet Labs",
-  "summary": "Nginx Module",
-  "license": "Apache Version 2.0",
-  "source": "git://github.com/puppetlabs/puppetlabs-nginx.git",
-  "project_page": "https://github.com/puppetlabs/puppetlabs-nginx",
-  "issues_url": "https://github.com/puppetlabs/puppetlabs-nginx",
-  "dependencies": [
-    {"name":"puppetlabs-stdlub","version_requirement":">= 1.0.0"}
-  ]
-}'
-  }
-}
+    step "Run module changes on a module which is missing checksums.json" do
+      on(agent, puppet("module changes #{testdir}/nginx"),
+         acceptable_exit_codes: [1]) do
 
-step 'Run module changes on a module which is missing checksums.json'
-on( master, puppet("module changes #{testdir}/nginx"),
-    :acceptable_exit_codes => [1] ) do
-
-  pattern = Regexp.new([
-%Q{.*Error: No file containing checksums found.*},
-%Q{.*Error: Try 'puppet help module changes' for usage.*},
-  ].join("\n"), Regexp::MULTILINE)
-  assert_match(pattern, result.stderr)
+        pattern = Regexp.new([
+          ".*Error: No file containing checksums found.*",
+          ".*Error: Try 'puppet help module changes' for usage.*"
+        ].join("\n"), Regexp::MULTILINE)
+        assert_match(pattern, result.stderr)
+      end
+    end
+  end
 end
