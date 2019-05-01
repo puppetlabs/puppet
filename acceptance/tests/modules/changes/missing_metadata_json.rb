9a17e5b802f3e474e133a3f7f9325bfd6f69b389
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor modules/changes/missing_metada
    This commit refactors the `modules/changes/missing_metadata_json`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/modules/changes/missing_metadata_json.rb
+++ acceptance/tests/modules/changes/missing_metadata_json.rb
@@ -1,26 +1,26 @@
-test_name 'puppet module changes (on a module which is missing metadata.json)'
+test_name "puppet module changes (module missing metadata.json)" do
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'   # Master is not required for this test. Replace with agents.each
-                       # Wrap steps in blocks in accordance with Beaker style guide
+  tag 'audit:medium',
+      'audit:acceptance'
 
-step 'Setup'
+  agents.each do |agent|
+    testdir = agent.tmpdir("module_changes_on_invalid_metadata")
+    step "Setup" do
+      apply_manifest_on agent, <<~MANIFEST
+        file { '#{testdir}/nginx': ensure => directory }
+      MANIFEST
+    end
 
-stub_forge_on(master)
-testdir = master.tmpdir('module_changes_on_invalid_metadata')
+    step "Run module changes on a module which is missing metadata.json" do
+      on(agent, puppet("module changes #{testdir}/nginx"),
+         acceptable_exit_codes: [1]) do
 
-apply_manifest_on master, %Q{
-  file { '#{testdir}/nginx': ensure => directory }
-}
-
-step 'Run module changes on a module which is missing metadata.json'
-on( master, puppet("module changes #{testdir}/nginx"),
-    :acceptable_exit_codes => [1] ) do
-
-  pattern = Regexp.new([
-%Q{.*Error: Could not find a valid module at.*},
-%Q{.*Error: Try 'puppet help module changes' for usage.*},
-  ].join("\n"), Regexp::MULTILINE)
-  assert_match(pattern, result.stderr)
+        pattern = Regexp.new([
+          ".*Error: Could not find a valid module at.*",
+          ".*Error: Try 'puppet help module changes' for usage.*"
+        ].join("\n"), Regexp::MULTILINE)
+        assert_match(pattern, result.stderr)
+      end
+    end
+  end
 end
