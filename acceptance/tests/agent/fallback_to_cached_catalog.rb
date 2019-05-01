5adc5d0f3e4dc709a63e4485fe1acde95598005f
John Duarte <john.duarte@puppet.com>
2019-05-01 07:08:20 -0700

    (PUP-9653) Refactor fallback_to_cached_catalog tst
    This commit refactors the agent fallback to cached catalog acceptance
    test to use a catalog fixture rather than using a master to see the
    catalog cache.  This removes the need to manage the puppet master, so
    the use of `with_puppet_running_on` has been deleted.
    
    [skip ci]

--- acceptance/tests/agent/fallback_to_cached_catalog.rb
+++ acceptance/tests/agent/fallback_to_cached_catalog.rb
@@ -1,22 +1,32 @@
-test_name "fallback to the cached catalog"
+test_name "fallback to the cached catalog" do
+  tag 'audit:medium',
+      'audit:integration' # This test is not OS sensitive.
 
-tag 'audit:medium',
-    'audit:integration', # This test is not OS sensitive.
-    'audit:refactor'     # A catalog fixture can be used for this test. Remove the usage of `with_puppet_running_on`.
+  fixture_dir = File.expand_path(File.join(File.dirname(__FILE__),
+                                           "../../fixtures"))
 
-step "run agents once to cache the catalog" do
-  with_puppet_running_on master, {} do
-    on(agents, puppet("agent -t --server #{master}"))
-  end
-end
-
-step "run agents again, verify they use cached catalog" do
   agents.each do |agent|
-    # can't use --test, because that will set usecacheonfailure=false
-    # We use a server that the agent can't possibly talk to in order
-    # to guarantee that no communication can take place.
-    on(agent, puppet("agent --onetime --no-daemonize --server puppet.example.com --verbose")) do |result|
-      assert_match(/Using cached catalog/, result.stdout) unless agent['locale'] == 'ja'
+    skip_test("cannot assert regex match for Japanese") if agent["locale"] == "ja"
+    step "seed catalog cache with fixture" do
+      node_name = agent.node_name
+      catalogdir = File.join(agent.puppet["vardir"], "client_yaml/catalog")
+      catalogfile = File.join(catalogdir, "#{node_name}.yaml")
+      template = File.read("#{fixture_dir}/catalog.yaml.erb")
+      renderer = ERB.new(template)
+      catalog = renderer.result(binding)
+
+      agent.mkdir_p(catalogdir)
+      create_remote_file(agent, catalogfile, catalog, mkdirs: true)
+    end
+
+    step "run agents again, verify they use cached catalog" do
+      # can't use --test, because that will set usecacheonfailure=false
+      # We use a server that the agent can't possibly talk to in order
+      # to guarantee that no communication can take place.
+      on(agent, puppet("agent --onetime --no-daemonize --verbose",
+                       "--server puppet.example.com --verbose")) do |result|
+        assert_match(/Using cached catalog/, result.stdout)
+      end
     end
   end
 end
