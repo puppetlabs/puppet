0dc9b75095dfb129c6b761ab7c0c0d5cc264632b
John Duarte <john.duarte@puppet.com>
2019-05-01 15:48:49 -0700

    (PUP-9653) Refactor modules/changes/modified_file
    This commit refactors the `modules/changes/module_with_modified_file`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/modules/changes/module_with_modified_file.rb
+++ acceptance/tests/modules/changes/module_with_modified_file.rb
@@ -1,28 +1,26 @@
-test_name 'puppet module changes (on a module with a modified file)'
+test_name "puppet module changes (on a module with a modified file)" do
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'   # Master is not required for this test. Replace with agents.each
-                       # Wrap steps in blocks in accordance with Beaker style guide
+  tag 'audit:medium',
+      'audit:acceptance'
 
-step 'Setup'
+  agents.each do |agent|
+    testdir = agent.tmpdir("module_changes_with_modified_file")
+    step "Setup" do
+      stub_forge_on(agent)
+      on agent, puppet("module install pmtacceptance-nginx --modulepath #{testdir}")
+      on agent, "echo >> #{testdir}/nginx/README"
+    end
 
-stub_forge_on(master)
-testdir = master.tmpdir('module_changes_with_modified_file')
+    step "Run module changes to check a module with a modified file" do
+      on(agent, puppet("module changes #{testdir}/nginx"),
+         acceptable_exit_codes: [0]) do |result|
 
-on master, puppet("module install pmtacceptance-nginx --modulepath #{testdir}")
-on master, "echo >> #{testdir}/nginx/README"
-
-step 'Run module changes to check a module with a modified file'
-on( master, puppet("module changes #{testdir}/nginx"),
-    :acceptable_exit_codes => [0] ) do
-
-  pattern = Regexp.new([
-%Q{.*Warning: 1 files modified.*},
-  ].join("\n"), Regexp::MULTILINE)
-  assert_match(pattern, result.stderr)
-
-  assert_equal <<-OUTPUT, stdout
-README
-  OUTPUT
+        pattern = Regexp.new([
+          ".*Warning: 1 files modified.*"
+        ].join("\n"), Regexp::MULTILINE)
+        assert_match(pattern, result.stderr)
+        assert_equal("README\n", result.stdout)
+      end
+    end
+  end
 end
