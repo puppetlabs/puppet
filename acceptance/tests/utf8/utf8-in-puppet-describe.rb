f2b54a56195dee638bb3b0e37a080efee91bf715
John Duarte <john.duarte@puppet.com>
2019-05-02 15:45:42 -0700

    (PUP-9653) Refactor utf8-in-puppet-describe test
    This commit refactors the `utf8/utf8-in-puppet-describe.rb`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/utf8/utf8-in-puppet-describe.rb
+++ acceptance/tests/utf8/utf8-in-puppet-describe.rb
@@ -1,95 +1,75 @@
-test_name 'utf-8 characters in module doc string, puppet describe' do
-
+test_name "utf-8 characters in module doc string, puppet describe" do
   tag 'audit:medium',      # utf-8 is high impact in general, puppet describe low risk?
-      'audit:integration', # not package dependent but may want to vary platform by LOCALE/encoding
-      'audit:refactor'     # if keeping, use mk_temp_environment_with_teardown
-                           # remove with_puppet_running_on unless pluginsync is absolutely necessary
-                           # (if it is, add 'server' tag
+      'audit:integration'  # not package dependent but may want to vary platform by LOCALE/encoding
 
-  platforms = hosts.map {|val| val[:platform]}
-  if (platforms.any? { |val| /^eos-/ =~ val})
-    skip_test "Skipping because Puppet describe fails when the Arista module is installed (ARISTA-51)"
-  end 
+  confine :except, :platform => /^eos-/ # Puppet describe fails when the Arista module is installed (ARISTA-51)"
 
   # utf8chars = "€‰ㄘ万竹ÜÖ"
   utf8chars = "\u20ac\u2030\u3118\u4e07\u7af9\u00dc\u00d6"
 
-  master_mod_dir = master.tmpdir("describe_master")
-  on(master, "chmod -R 755 #{master_mod_dir}");
-  teardown do
-    on(master, "rm -rf #{master_mod_dir}")
-  end
-  master_manifest = 
-<<MASTER_MANIFEST
-
-File {
-  ensure => directory,
-  mode => "0755",
-}
-
-file {
-  '#{master_mod_dir}/code':;
-  '#{master_mod_dir}/code/environments':;
-  '#{master_mod_dir}/code/environments/production':;
-  '#{master_mod_dir}/code/environments/production/modules':;
-  '#{master_mod_dir}/code/environments/production/modules/master_mytype_module':;
-  '#{master_mod_dir}/code/environments/production/modules/master_mytype_module/lib':;
-  '#{master_mod_dir}/code/environments/production/modules/master_mytype_module/lib/puppet':;
-  '#{master_mod_dir}/code/environments/production/modules/master_mytype_module/lib/puppet/type':;
-}
-
-file { '#{master_mod_dir}/code/environments/production/modules/master_mytype_module/lib/puppet/type/master_mytype.rb' :
-  ensure => file,
-  mode => '0755',
-  content => '
-Puppet::Type.newtype(:master_mytype) do
-  @doc = "Testing to see if puppet handles describe blocks correctly
-when they contain utf8 characters, such as #{utf8chars}
-"
-  newparam(:name) do
-    isnamevar
-    desc " name parameter for mytype, also with some utf8 chars #{utf8chars}"
-  end
-end
-',
-}
-
-MASTER_MANIFEST
-
-  step "Apply master manifest" do
-    apply_manifest_on(master, master_manifest)
-  end
-  master_opts = {
-    'main' => {
-       'environmentpath' => "#{master_mod_dir}/code/environments",
-    }
-  }
-
-  step "Start puppet server"
-  with_puppet_running_on(master, master_opts, master_mod_dir) do
-    agents.each do |agent|
-      puts "agent name: #{agent.hostname}, platform: #{agent.platform}"
-      step "Run puppet agent for plugin sync" do 
-        on(
-          agent, puppet("agent", "-t", "--server #{master.node_name}"),
-          :acceptable_exit_codes => [0, 2]
-        )
-      end
-
-      step "Puppet describe for master-hosted mytype" do 
-        on(agent, puppet("describe", "master_mytype")) do |result|
-          assert_match(
-            /master_mytype.*such as #{utf8chars}/m,
-            result.stdout,
-  "Main description of master_mytype did not match utf8 chars, '#{utf8chars}'"
-          )
-
-          assert_match(
-            /name parameter.*chars #{utf8chars}/,
-            result.stdout,
-            "Name parameter description of master_mytype did not match utf8 chars, '#{utf8chars}'"
-          )
+  agents.each do |agent|
+    testdir = agent.tmpdir("describe_utf-8_type")
+    confdir = "#{testdir}/puppet"
+    manifest =
+      <<~MANIFEST
+        File {
+          ensure => directory,
+          mode => "0755",
+        }
+         file {
+          '#{confdir}':;
+          '#{testdir}/code':;
+          '#{testdir}/code/environments':;
+          '#{testdir}/code/environments/production':;
+          '#{testdir}/code/environments/production/modules':;
+          '#{testdir}/code/environments/production/modules/mytype_module':;
+          '#{testdir}/code/environments/production/modules/mytype_module/lib':;
+          '#{testdir}/code/environments/production/modules/mytype_module/lib/puppet':;
+          '#{testdir}/code/environments/production/modules/mytype_module/lib/puppet/type':;
+        }
+         file { '#{confdir}/puppet.conf' :
+          ensure => file,
+          mode => '0755',
+          content => '
+        [user]
+        environment = production
+        [main]
+        codedir = #{testdir}/code
+        ',
+        }
+         file { '#{testdir}/code/environments/production/modules/mytype_module/lib/puppet/type/mytype.rb' :
+          ensure => file,
+          mode => '0755',
+          content => '
+        Puppet::Type.newtype(:mytype) do
+          @doc = "Testing to see if puppet handles describe blocks correctly
+        when they contain utf8 characters, such as #{utf8chars}
+        "
+          newparam(:name) do
+            isnamevar
+            desc " name parameter for mytype, also with some utf8 chars #{utf8chars}"
+          end
         end
+        ',
+        }
+      MANIFEST
+
+    apply_manifest_on(agent, manifest)
+
+    puts "agent name: #{agent.hostname}, platform: #{agent.platform}"
+
+    step "Puppet describe for mytype" do
+      on(agent, puppet("describe", "mytype", confdir: confdir)) do |result|
+        assert_match(
+          /mytype.*such as #{utf8chars}/m,
+          result.stdout,
+          "Main description of mytype did not match utf8 chars, '#{utf8chars}'"
+        )
+        assert_match(
+          /name parameter.*chars #{utf8chars}/,
+          result.stdout,
+          "Name parameter description of mytype did not match utf8 chars, '#{utf8chars}'"
+        )
       end
     end
   end
