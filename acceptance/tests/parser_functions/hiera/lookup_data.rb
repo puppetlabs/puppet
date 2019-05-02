5c7ead677122030346aec3352e405807aef73415
John Duarte <john.duarte@puppet.com>
2019-05-02 15:45:42 -0700

    (PUP-9653) Refactor hiera lookup_data test
    This commit refactors the `parser_functions/hiera/lookup_data.rb`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/parser_functions/hiera/lookup_data.rb
+++ acceptance/tests/parser_functions/hiera/lookup_data.rb
@@ -1,96 +1,93 @@
-test_name "Lookup data using the hiera parser function"
+test_name "Lookup data using the hiera parser function" do
+  tag 'audit:medium',
+      'audit:acceptance'
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'    # Master is not required for this test. Replace with agents.each
-
-testdir = master.tmpdir('hiera')
-
-step 'Setup'
-
-apply_manifest_on(master, <<-PP, :catch_failures => true)
-File {
-  ensure => directory,
-  mode => "0750",
-  owner => #{master.puppet['user']},
-  group => #{master.puppet['group']},
-}
-
-file {
-  '#{testdir}':;
-  '#{testdir}/hieradata':;
-  '#{testdir}/environments':;
-  '#{testdir}/environments/production':;
-  '#{testdir}/environments/production/manifests':;
-  '#{testdir}/environments/production/modules':;
-}
-
-file { '#{testdir}/hiera.yaml':
-  ensure  => file,
-  content => '---
-    :backends:
-      - "yaml"
-    :logger: "console"
-    :hierarchy:
-      - "%{fqdn}"
-      - "%{environment}"
-      - "global"
-
-    :yaml:
-      :datadir: "#{testdir}/hieradata"
-  ',
-  mode => "0640",
-}
-
-file { '#{testdir}/hieradata/global.yaml':
-  ensure  => file,
-  content => "---
-    port: 8080
-  ",
-  mode => "0640",
-}
-
-file {
-  '#{testdir}/environments/production/modules/apache':;
-  '#{testdir}/environments/production/modules/apache/manifests':;
-}
-
-file { '#{testdir}/environments/production/modules/apache/manifests/init.pp':
-  ensure => file,
-  content => '
-    class apache {
-      $port = hiera("port")
-
-      notify { "port from hiera":
-        message => "apache server port: ${port}"
-      }
-    }',
-  mode => "0640",
-}
-
-file { '#{testdir}/environments/production/manifests/site.pp':
-  ensure => file,
-  content => "
-    node default {
-      include apache
-    }",
-  mode => "0640",
-}
-PP
-
-step "Try to lookup string data"
-
-master_opts = {
-  'main' => {
-    'environmentpath' => "#{testdir}/environments",
-    'hiera_config' => "#{testdir}/hiera.yaml",
-  },
-}
-
-with_puppet_running_on master, master_opts, testdir do
   agents.each do |agent|
-    on(agent, puppet('agent', "-t --server #{master}"), :acceptable_exit_codes => [2])
+    testdir = agent.tmpdir("hiera")
+    confdir = "#{testdir}/puppet"
+    codedir = "#{testdir}/code"
 
-    assert_match("apache server port: 8080", stdout)
+    step "Setup" do
+      apply_manifest_on(agent, <<~PP, catch_failures: true)
+        File {
+          ensure => directory,
+          mode => "0750",
+        }
+         file {
+          '#{testdir}':;
+          '#{confdir}':;
+          '#{testdir}/hieradata':;
+          '#{codedir}':;
+          '#{codedir}/environments':;
+          '#{codedir}/environments/production':;
+          '#{codedir}/environments/production/manifests':;
+          '#{codedir}/environments/production/modules':;
+        }
+         file { '#{confdir}/puppet.conf':
+          ensure  => file,
+          content => '
+        [user]
+        environment = production
+        [main]
+        codedir = #{codedir}
+        environmentpath = #{codedir}/environments
+        hiera_config = #{testdir}/hiera.yaml
+        ',
+        }
+         file { '#{testdir}/hiera.yaml':
+          ensure  => file,
+          content => '---
+            :backends:
+              - "yaml"
+            :logger: "console"
+            :hierarchy:
+              - "%{fqdn}"
+              - "%{environment}"
+              - "global"
+            :yaml:
+              :datadir: "#{testdir}/hieradata"
+          ',
+          mode => "0640",
+        }
+         file { '#{testdir}/hieradata/global.yaml':
+          ensure  => file,
+          content => "---
+            port: 8080
+          ",
+          mode => "0640",
+        }
+         file {
+          '#{codedir}/environments/production/modules/apache':;
+          '#{codedir}/environments/production/modules/apache/manifests':;
+        }
+         file { '#{codedir}/environments/production/modules/apache/manifests/init.pp':
+          ensure => file,
+          content => '
+            class apache {
+              $port = hiera("port")
+               notify { "port from hiera":
+                message => "apache server port: ${port}"
+              }
+            }',
+          mode => "0640",
+        }
+         file { '#{codedir}/environments/production/manifests/site.pp':
+          ensure => file,
+          content => "
+            node default {
+              include apache
+            }",
+          mode => "0640",
+        }
+      PP
+    end
+
+    step "Try to lookup string data" do
+      on(agent, puppet("apply",
+                       "#{codedir}/environments/production/manifests/site.pp",
+                       confdir: confdir), acceptable_exit_codes: [0, 2])
+
+      assert_match("apache server port: 8080", stdout)
+    end
   end
 end
