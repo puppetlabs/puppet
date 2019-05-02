7adce38f59a513af51d298a408ad6567862f5344
John Duarte <john.duarte@puppet.com>
2019-05-02 15:45:42 -0700

    (PUP-9653) Refactor hiera_array lookup test
    This commit refactors the `parser_functions/hiera_array/lookup_data.rb`
    acceptance test to run on agents rather than using a master.  The
    test has also been updated to comply with the beaker style guide.

--- acceptance/tests/parser_functions/hiera_array/lookup_data.rb
+++ acceptance/tests/parser_functions/hiera_array/lookup_data.rb
@@ -1,109 +1,103 @@
-test_name "Lookup data using the hiera_array parser function"
+test_name "Lookup data using the hiera_array parser function" do
+  tag 'audit:medium',
+      'audit:acceptance'
 
-tag 'audit:medium',
-    'audit:acceptance',
-    'audit:refactor'    # Master is not required for this test. Replace with agents.each
-
-testdir = master.tmpdir('hiera')
-
-step 'Setup'
-
-apply_manifest_on(master, <<-PP, :catch_failures => true)
-File {
-  ensure => directory,
-  mode => "0750",
-  owner => #{master.puppet['user']},
-  group => #{master.puppet['group']},
-}
-
-file {
-  '#{testdir}':;
-  '#{testdir}/hieradata':;
-  '#{testdir}/environments':;
-  '#{testdir}/environments/production':;
-  '#{testdir}/environments/production/manifests':;
-  '#{testdir}/environments/production/modules':;
-}
-
-file { '#{testdir}/hiera.yaml':
-  ensure  => file,
-  content => '---
-    :backends:
-      - "yaml"
-    :logger: "console"
-    :hierarchy:
-      - "%{fqdn}"
-      - "%{environment}"
-      - "global"
-
-    :yaml:
-      :datadir: "#{testdir}/hieradata"
-  ',
-  mode => "0640";
-}
-
-file { '#{testdir}/hieradata/global.yaml':
-  ensure  => file,
-  content => "---
-    port: '8080'
-    ntpservers: ['global.ntp.puppetlabs.com']
-  ",
-  mode => "0640";
-}
-
-file { '#{testdir}/hieradata/production.yaml':
-  ensure  => file,
-  content => "---
-    ntpservers: ['production.ntp.puppetlabs.com']
-  ",
-  mode => "0640";
-}
-
-file {
-  '#{testdir}/environments/production/modules/ntp':;
-  '#{testdir}/environments/production/modules/ntp/manifests':;
-}
-
-file { '#{testdir}/environments/production/modules/ntp/manifests/init.pp':
-  ensure => file,
-  content => '
-    class ntp {
-      $ntpservers = hiera_array("ntpservers")
-
-      define print {
-        $server = $name
-        notify { "ntpserver ${server}": }
-      }
-
-      ntp::print { $ntpservers: }
-    }',
-  mode => "0640";
-}
-
-file { '#{testdir}/environments/production/manifests/site.pp':
-  ensure => file,
-  content => "
-    node default {
-      include ntp
-    }",
-  mode => "0640";
-}
-PP
-
-step "Try to lookup array data"
-
-master_opts = {
-  'main' => {
-    'environmentpath' => "#{testdir}/environments",
-    'hiera_config' => "#{testdir}/hiera.yaml",
-  },
-}
-
-with_puppet_running_on master, master_opts, testdir do
   agents.each do |agent|
-    on(agent, puppet('agent', "-t --server #{master}"), :acceptable_exit_codes => [2])
+    testdir = agent.tmpdir("hiera")
+    confdir = "#{testdir}/puppet"
+    codedir = "#{testdir}/code"
 
-    assert_match("ntpserver global.ntp.puppetlabs.com", stdout)
-    assert_match("ntpserver production.ntp.puppetlabs.com", stdout)
+    step "Setup" do
+      apply_manifest_on(agent, <<~PP, catch_failures: true)
+        File {
+          ensure => directory,
+          mode => "0750",
+        }
+         file {
+          '#{testdir}':;
+          '#{confdir}':;
+          '#{testdir}/hieradata':;
+          '#{codedir}':;
+          '#{codedir}/environments':;
+          '#{codedir}/environments/production':;
+          '#{codedir}/environments/production/manifests':;
+          '#{codedir}/environments/production/modules':;
+        }
+         file { '#{testdir}/puppet/puppet.conf':
+          ensure  => file,
+          content => '
+        [user]
+        environment = production
+        [main]
+        environmentpath = #{codedir}/environments
+        hiera_config = #{testdir}/hiera.yaml
+        ',
+        }
+         file { '#{testdir}/hiera.yaml':
+          ensure  => file,
+          content => '---
+            :backends:
+              - "yaml"
+            :logger: "console"
+            :hierarchy:
+              - "%{fqdn}"
+              - "%{environment}"
+              - "global"
+            :yaml:
+              :datadir: "#{testdir}/hieradata"
+          ',
+          mode => "0640";
+        }
+         file { '#{testdir}/hieradata/global.yaml':
+          ensure  => file,
+          content => "---
+            port: '8080'
+            ntpservers: ['global.ntp.puppetlabs.com']
+          ",
+          mode => "0640";
+        }
+         file { '#{testdir}/hieradata/production.yaml':
+          ensure  => file,
+          content => "---
+            ntpservers: ['production.ntp.puppetlabs.com']
+          ",
+          mode => "0640";
+        }
+         file {
+          '#{codedir}/environments/production/modules/ntp':;
+          '#{codedir}/environments/production/modules/ntp/manifests':;
+        }
+         file { '#{codedir}/environments/production/modules/ntp/manifests/init.pp':
+          ensure => file,
+          content => '
+            class ntp {
+              $ntpservers = hiera_array("ntpservers")
+               define print {
+                $server = $name
+                notify { "ntpserver ${server}": }
+              }
+               ntp::print { $ntpservers: }
+            }',
+          mode => "0640";
+        }
+         file { '#{codedir}/environments/production/manifests/site.pp':
+          ensure => file,
+          content => "
+            node default {
+              include ntp
+            }",
+          mode => "0640";
+        }
+      PP
+    end
+
+    step "Try to lookup array data" do
+      on(agent, puppet("apply",
+                       "#{codedir}/environments/production/manifests/site.pp",
+                       confdir: confdir), acceptable_exit_codes: [0]) do |result|
+        assert_match("ntpserver global.ntp.puppetlabs.com", result.stdout)
+        assert_match("ntpserver production.ntp.puppetlabs.com", result.stdout)
+      end
+    end
   end
 end
