b12a3686b5c44e010f6e6f2b2335b45aa19b6e59
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2019-05-14 19:29:38 +0200

    (PUP-9699) Add recording of all looked up (hiera) keys
    This adds an API for recording all looked up hiera keys (except the
    always implicitly looked up `lookup_options` key built into hiera).

---
+++ lib/puppet/pops/lookup/key_recorder.rb
@@ -0,0 +1,18 @@
+# This class defines the private API of the Lookup Key Recorder support.
+# @api private
+#
+class Puppet::Pops::Lookup::KeyRecorder
+
+  def initialize()
+  end
+
+  def self.singleton
+    @null_recorder ||= self.new
+  end
+
+  # Records a key
+  # (This implementation does nothing)
+  #
+  def record(key)
+  end
+end
