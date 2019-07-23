4b5c52e59650b8735e65cdf5297ad4bc29c9e055
mihaibuzgau <mihai.buzgau@puppet.com>
2019-07-23 13:14:56 +0300

    (PUP-7312) add tests

--- spec/integration/provider/service/systemd_spec.rb
+++ spec/integration/provider/service/systemd_spec.rb
@@ -15,9 +15,19 @@ describe test_title, unless: Puppet::Util::Platform.jruby? do
     expect(provider_class).to be_suitable
   end
 
-  it "should be considered suitable if /proc/1/exe is present and contains 'systemd'",
-    :if => File.exist?('/proc/1/exe') && File.open('/proc/1/exe').grep(/systemd/).size > 0
+  it "should be considered suitable if /proc/1/exe is present and points to 'systemd'",
+    :if => File.exist?('/proc/1/exe') && Puppet::FileSystem.readlink('/proc/1/exe').include?('systemd') do
     expect(provider_class).to be_suitable
+  end
+
+  it "should not be considered suitable if /proc/1/exe is present it does not point to 'systemd'",
+    :if => File.exist?('/proc/1/exe') && !Puppet::FileSystem.readlink('/proc/1/exe').include?('systemd') do
+    expect(provider_class).not_to be_suitable
+  end
+
+  it "should not be considered suitable if /proc/1/exe is absent",
+    :if => !File.exist?('/proc/1/exe') do
+    expect(provider_class).not_to be_suitable
   end
 
   it "should not be cosidered suitable if systemctl is absent",
