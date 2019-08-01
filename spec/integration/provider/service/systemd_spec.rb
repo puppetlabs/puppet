f8cc898822e95c931c7aec59c52fa24fc171b4ec
gimmy <gheorghe.popescu@puppet.com>
2019-08-01 09:27:31 +0300

    (maint) fix travis tests
    - removed the not needed tests from spec/integration/provider/service/systemd_spec.rb
    due to https://github.com/puppetlabs/puppet/pull/7649#issue-302942712

--- spec/integration/provider/service/systemd_spec.rb
+++ spec/integration/provider/service/systemd_spec.rb
@@ -1,37 +1,21 @@
 require 'spec_helper'
 
-test_title = 'Integration Tests for Puppet::Type::Service::Provider::Systemd'
-
-describe test_title, unless: Puppet::Util::Platform.jruby? do
-  let(:provider_class) { Puppet::Type.type(:service).provider(:systemd) }
-
+describe Puppet::Type.type(:service).provider(:systemd), '(integration)' do
   # TODO: Unfortunately there does not seem a way to stub the executable
   #       checks in the systemd provider because they happen at load time.
-  it "should be considered suitable if /bin/systemctl is present", :if => File.executable?('/bin/systemctl') do
-    expect(provider_class).to be_suitable
-  end
-
-  it "should be considered suitable if /usr/bin/systemctl is present", :if => File.executable?('/usr/bin/systemctl')  do
-    expect(provider_class).to be_suitable
-  end
 
   it "should be considered suitable if /proc/1/exe is present and points to 'systemd'",
     :if => File.exist?('/proc/1/exe') && Puppet::FileSystem.readlink('/proc/1/exe').include?('systemd') do
-    expect(provider_class).to be_suitable
+    expect(described_class).to be_suitable
   end
 
   it "should not be considered suitable if /proc/1/exe is present it does not point to 'systemd'",
     :if => File.exist?('/proc/1/exe') && !Puppet::FileSystem.readlink('/proc/1/exe').include?('systemd') do
-    expect(provider_class).not_to be_suitable
+    expect(described_class).not_to be_suitable
   end
 
   it "should not be considered suitable if /proc/1/exe is absent",
     :if => !File.exist?('/proc/1/exe') do
-    expect(provider_class).not_to be_suitable
-  end
-
-  it "should not be cosidered suitable if systemctl is absent",
-    :unless => (File.executable?('/bin/systemctl') or File.executable?('/usr/bin/systemctl')) do
-    expect(provider_class).not_to be_suitable
+    expect(described_class).not_to be_suitable
   end
 end
