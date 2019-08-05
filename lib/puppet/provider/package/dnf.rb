adf2dc5ef259a37db8d6564270cb6eb44e2ec740
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-08-05 12:12:08 +0200

    Limit provider to EL1-3

--- lib/puppet/provider/package/dnf.rb
+++ lib/puppet/provider/package/dnf.rb
@@ -30,8 +30,7 @@ Puppet::Type.type(:package).provide :dnf, :parent => :yum do
 
   defaultfor :operatingsystem => :fedora
   notdefaultfor :operatingsystem => :fedora, :operatingsystemmajrelease => (19..21).to_a
-  defaultfor :osfamily => :redhat
-  notdefaultfor :osfamily => :redhat, :operatingsystemmajrelease => (4..7).to_a
+  defaultfor :osfamily => :redhat, :operatingsystemmajrelease => (1..3).to_a
 
   def self.update_command
     # In DNF, update is deprecated for upgrade
