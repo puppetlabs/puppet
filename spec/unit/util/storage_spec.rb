6f24f3694c56106233c37a438d37d1233af614ca
John O'Connor <john.oconnor@puppet.com>
2019-08-08 15:56:47 +0100

    (PUP-9719) Remove platform/error check
    Minor update to reflect common error handling for EISDIR across Windows
    and posix.

--- spec/unit/util/storage_spec.rb
+++ spec/unit/util/storage_spec.rb
@@ -189,15 +189,8 @@ describe Puppet::Util::Storage do
       Dir.mkdir(Puppet[:statefile])
       Puppet::Util::Storage.cache(:yayness)
 
-      if Puppet.features.microsoft_windows?
-        expect { Puppet::Util::Storage.store }.to raise_error do |error|
-          expect(error).to be_a(Puppet::Util::Windows::Error)
-          expect(error.code).to eq(5) # ERROR_ACCESS_DENIED
-        end
-      else
-        expect { Puppet::Util::Storage.store }.to raise_error(Errno::EISDIR, /Is a directory/)
-      end
-
+      expect { Puppet::Util::Storage.store }.to raise_error(Errno::EISDIR, /Is a directory/)
+      
       Dir.rmdir(Puppet[:statefile])
     end
 
