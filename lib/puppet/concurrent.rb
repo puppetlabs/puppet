f6fc30d20f742ad3f53e981a6042f811bced9a77
Patrick Carlisle <patrick@puppet.com>
2019-09-12 11:55:15 -0700

    (PUP-10024) Add concurrent module
    Add a module to puppet to hold code around concurrency. This starts with
    some basic locking utilities. These will lock on the JVM but not on MRI.

---
+++ lib/puppet/concurrent.rb
@@ -0,0 +1,2 @@
+module Puppet::Concurrent
+end
---
+++ lib/puppet/concurrent/lock.rb
@@ -0,0 +1,16 @@
+require 'puppet/concurrent/synchronized'
+
+module Puppet
+module Concurrent
+
+# A simple lock that at the moment only does any locking on jruby
+class Lock
+  include Puppet::Concurrent::Synchronized
+  def synchronize
+    yield
+  end
+end
+
+end
+end
+
