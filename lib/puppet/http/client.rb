ec35e8911f0a84be39d475db33f34b5dc1992861
Josh Cooper <josh@puppet.com>
2019-09-24 23:24:28 -0700

    (PUP-10033) Add HTTP client

---
+++ lib/puppet/http/client.rb
@@ -0,0 +1,22 @@
+class Puppet::HTTP::Client
+  def initialize(ssl_context:)
+    @pool = Puppet::Network::HTTP::Pool.new
+    @default_headers = {
+      'X-Puppet-Version' => Puppet.version,
+      'User-Agent' => Puppet[:http_user_agent],
+    }.freeze
+    @ssl_context = ssl_context
+  end
+
+  def connect(uri, &block)
+    site = Puppet::Network::HTTP::Site.from_uri(uri)
+    verifier = Puppet::SSL::Verifier.new(uri.host, @ssl_context)
+    @pool.with_connection(site, verifier) do |http|
+      yield http if block_given?
+    end
+  end
+
+  def close
+    @pool.close
+  end
+end
