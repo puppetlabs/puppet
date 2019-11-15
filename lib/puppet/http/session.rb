21f3c17690bc7c51249fd0f072a8c62d83a49a3f
Josh Cooper <josh@puppet.com>
2019-11-15 13:42:34 -0800

    (maint) Rename Route to ServiceType
    The term 'route' was a holdover from an earlier version of the code when
    `route_to` returned a Route object. But that was changed so that `route_to`
    returns a connected `Service` where the service contains the URL representing
    the route to a specific host and port.
    
    As a result, rename `Route` to `ServiceType` as the latter describes the
    information needed to create each type of service.

--- lib/puppet/http/session.rb
+++ lib/puppet/http/session.rb
@@ -1,8 +1,8 @@
 class Puppet::HTTP::Session
-  Route = Struct.new(:service_class, :api, :server_setting, :port_setting)
+  ServiceType = Struct.new(:service_class, :api, :server_setting, :port_setting)
 
-  ROUTES = {
-    ca: Route.new(Puppet::HTTP::Service::Ca, '/puppet-ca/v1', :ca_server, :ca_port),
+  SERVICE_TYPES = {
+    ca: ServiceType.new(Puppet::HTTP::Service::Ca, '/puppet-ca/v1', :ca_server, :ca_port),
   }.freeze
 
   def initialize(client, resolvers)
@@ -12,8 +12,7 @@ class Puppet::HTTP::Session
   end
 
   def route_to(name, ssl_context: nil)
-    route = ROUTES[name]
-    raise ArgumentError, "Unknown service #{name}" unless route
+    raise ArgumentError, "Unknown service #{name}" if SERVICE_TYPES[name].nil?
 
     cached = @resolved_services[name]
     return cached if cached
@@ -41,15 +40,15 @@ class Puppet::HTTP::Session
   end
 
   def create_service(name, server = nil, port = nil)
-    route = ROUTES[name]
-    raise ArgumentError, "Unknown service #{name}" unless route
+    service_type = SERVICE_TYPES[name]
+    raise ArgumentError, "Unknown service #{name}" unless service_type
 
-    server ||= Puppet[route.server_setting]
-    port   ||= Puppet[route.port_setting]
+    server ||= Puppet[service_type.server_setting]
+    port   ||= Puppet[service_type.port_setting]
     url = URI::HTTPS.build(host: server,
                            port: port,
-                           path: route.api
+                           path: service_type.api
                           ).freeze
-    route.service_class.new(@client, url)
+    service_type.service_class.new(@client, url)
   end
 end
