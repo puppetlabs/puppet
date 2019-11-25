43327fec7b71495a1fa77624aa41409cf3349bd0
Ben Ford <ben.ford@puppetlabs.com>
2019-11-25 12:16:04 -0800

    (DOCS) grammar fixes for find_file() function docs
    See #7840 for more context

--- lib/puppet/functions/find_file.rb
+++ lib/puppet/functions/find_file.rb
@@ -1,19 +1,19 @@
 # Finds an existing file from a module and returns its path.
 #
-# The argument to this function should be a String as a `<MODULE NAME>/<FILE>`
-# reference, which will search for `<FILE>` relative to a module's `files`
+# This function accepts an argument that is a String as a `<MODULE NAME>/<FILE>`
+# reference, which searches for `<FILE>` relative to a module's `files`
 # directory. (For example, the reference `mysql/mysqltuner.pl` will search for the
 # file `<MODULES DIRECTORY>/mysql/files/mysqltuner.pl`.)
 #
 # This function can also accept:
-# 
-# * An absolute String path, which will check for the existence of a file from anywhere on disk.
-# * Multiple String arguments, which will return the path of the **first** file
-#   found, skipping non existing files.
-# * An array of string paths, which will return the path of the **first** file
-#   found from the given paths in the array, skipping non existing files.
 #
-# The function returns `undef` if none of the given paths were found
+# * An absolute String path, which checks for the existence of a file from anywhere on disk.
+# * Multiple String arguments, which returns the path of the **first** file
+#   found, skipping nonexistent files.
+# * An array of string paths, which returns the path of the **first** file
+#   found from the given paths in the array, skipping nonexistent files.
+#
+# The function returns `undef` if none of the given paths were found.
 #
 # @since 4.8.0
 #
