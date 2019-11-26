df927b8bb1378e261e3c1cc2a878e8ee0932ba99
Josh Cooper <josh@puppet.com>
2019-11-26 15:44:10 -0800

    SSL debugging

--- lib/puppet/network/http/base_pool.rb
+++ lib/puppet/network/http/base_pool.rb
@@ -8,6 +8,11 @@ class Puppet::Network::HTTP::BasePool
       verifier.setup_connection(http)
       begin
         http.start
+        socket = http.instance_variable_get(:@socket)
+        Puppet.info("Using #{socket.io.ssl_version} with cipher #{socket.io.cipher.first}")
+        # This is useful for debugging, but it leaks sensitive data so is
+        # commented out
+        puts socket.io.session.to_text
       rescue OpenSSL::SSL::SSLError => error
         verifier.handle_connection_error(http, error)
       end
