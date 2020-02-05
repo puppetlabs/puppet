330410d3cd0fa86c256553f6a4082bb7815184e7
Josh Cooper <josh@puppet.com>
2020-02-05 13:16:48 -0800

    (PUP-10260) Use http client for facts rest requests

--- lib/puppet/indirector/facts/rest.rb
+++ lib/puppet/indirector/facts/rest.rb
@@ -7,6 +7,26 @@ class Puppet::Node::Facts::Rest < Puppet::Indirector::REST
   def save(request)
     raise ArgumentError, _("PUT does not accept options") unless request.options.empty?
 
+    return legacy_save(request) unless use_http_client?
+
+    session = Puppet.lookup(:http_session)
+    api = session.route_to(:puppet)
+    api.put_facts(
+      request.key,
+      facts: request.instance,
+      environment: request.environment.to_s
+    )
+
+    # preserve existing behavior
+    nil
+  rescue Puppet::HTTP::ResponseError => e
+    # always raise even if fail_on_404 is false
+    raise convert_to_http_error(e.response.nethttp)
+  end
+
+  private
+
+  def legacy_save(request)
     response = do_request(request) do |req|
       http_put(req, IndirectedRoutes.request_to_uri(req), req.instance.render, headers.merge({ "Content-Type" => req.instance.mime }))
     end
