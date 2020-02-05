d7af168cf9a343de4b4085e054935e43c112ce7f
Josh Cooper <josh@puppet.com>
2020-02-05 15:56:47 -0800

    (PUP-10260) Update REST termini specs to match current behavior
    Consumers of the rest termini don't call the directly, instead calling
    the find, etc method on the indirection, e.g. Puppet::Node.indirection.find.
    So test the rest terminus from that perspective, as there are lots of
    "interesting" bits in between, eg filtering, caching, expiration.
    
    Also update to use webmock and delete "superclass" tests.

--- spec/unit/indirector/node/rest_spec.rb
+++ spec/unit/indirector/node/rest_spec.rb
@@ -3,9 +3,57 @@ require 'spec_helper'
 require 'puppet/indirector/node/rest'
 
 describe Puppet::Node::Rest do
-  before do
-    @searcher = Puppet::Node::Rest.new
+  let(:certname) { 'ziggy' }
+  let(:uri) { %r{/puppet/v3/node/ziggy} }
+  let(:formatter) { Puppet::Network::FormatHandler.format(:json) }
+
+  before :each do
+    Puppet[:server] = 'compiler.example.com'
+    Puppet[:masterport] = 8140
+
+    described_class.indirection.terminus_class = :rest
   end
 
+  def node_response(node)
+    { body: formatter.render(node), headers: {'Content-Type' => formatter.mime } }
+  end
 
+  it 'finds a node' do
+    node = Puppet::Node.new(certname)
+
+    stub_request(:get, uri).to_return(**node_response(node))
+
+    expect(described_class.indirection.find(certname)).to be_a(Puppet::Node)
+  end
+
+  it 'preserves the node environment_name as a symbol' do
+    env = Puppet::Node::Environment.remote('outerspace')
+    node = Puppet::Node.new(certname, environment: env)
+
+    stub_request(:get, uri).to_return(**node_response(node))
+
+    expect(described_class.indirection.find(certname).environment_name).to eq(:outerspace)
+  end
+
+  it 'returns nil if the node does not exist' do
+    stub_request(:get, uri).to_return(status: 404, headers: { 'Content-Type' => 'application/json' }, body: "{}")
+
+    expect(described_class.indirection.find(certname)).to be_nil
+  end
+
+  it 'raises if fail_on_404 is specified' do
+    stub_request(:get, uri).to_return(status: 404, headers: { 'Content-Type' => 'application/json' }, body: "{}")
+
+    expect{
+      described_class.indirection.find(certname, fail_on_404: true)
+    }.to raise_error(Puppet::Error, %r{Find /puppet/v3/node/ziggy\?environment=\*root\*&fail_on_404=true resulted in 404 with the message: {}})
+  end
+
+  it 'raises Net::HTTPError on 500' do
+    stub_request(:get, uri).to_return(status: 500)
+
+    expect{
+      described_class.indirection.find(certname)
+    }.to raise_error(Net::HTTPError, %r{Error 500 on SERVER: })
+  end
 end
--- spec/unit/indirector/report/rest_spec.rb
+++ spec/unit/indirector/report/rest_spec.rb
@@ -22,43 +22,19 @@ describe Puppet::Transaction::Report::Rest do
     expect(Puppet::Transaction::Report::Rest.srv_service).to eq(:report)
   end
 
-  let(:model) { Puppet::Transaction::Report }
-  let(:terminus_class) { Puppet::Transaction::Report::Rest }
-  let(:terminus) { model.indirection.terminus(:rest) }
-  let(:indirection) { model.indirection }
-
   before(:each) do
-    Puppet::Transaction::Report.indirection.terminus_class = :rest
-  end
-
-  def mock_response(code, body, content_type='text/plain', encoding=nil)
-    obj = double('http 200 ok', :code => code.to_s, :body => body)
-    allow(obj).to receive(:[]).with('content-type').and_return(content_type)
-    allow(obj).to receive(:[]).with('content-encoding').and_return(encoding)
-    allow(obj).to receive(:[]).with(Puppet::Network::HTTP::HEADER_PUPPET_VERSION).and_return(Puppet.version)
-    obj
-  end
-
-  def save_request(key, instance, options={})
-    Puppet::Indirector::Request.new(:report, :find, key, instance, options)
+    described_class.indirection.terminus_class = :rest
   end
 
   describe "#save" do
-    let(:response) { mock_response(200, 'body') }
-    let(:connection) { double('mock http connection', :put => response, :verify_callback= => nil) }
-    let(:instance) { model.new('the thing', 'some contents') }
-    let(:request) { save_request(instance.name, instance) }
+    let(:instance) { Puppet::Transaction::Report.new('the thing', 'some contents') }
     let(:body) { ["store", "http"].to_pson }
 
-    before :each do
-      allow(terminus).to receive(:network).and_return(connection)
-    end
-
     it "deserializes the response as an array of report processor names" do
-      response = mock_response('200', body, 'text/pson')
-      expect(connection).to receive(:put).and_return(response)
+      stub_request(:put, %r{/puppet/v3/report})
+        .to_return(status: 200, body: body, headers: { 'Content-Type' => 'text/pson' })
 
-      expect(terminus.save(request)).to eq(["store", "http"])
+      expect(described_class.indirection.save(instance)).to eq(["store", "http"])
     end
 
     describe "when handling the response" do
@@ -66,24 +42,26 @@ describe Puppet::Transaction::Report::Rest do
         it "raises if the save fails and we're not using pson" do
           Puppet[:preferred_serialization_format] = "json"
 
-          response = mock_response('500', '{}', 'text/pson')
-          allow(response).to receive(:[]).with(Puppet::Network::HTTP::HEADER_PUPPET_VERSION).and_return("4.10.1")
-          expect(connection).to receive(:put).and_return(response)
+          stub_request(:put, %r{/puppet/v3/report})
+            .to_return(status: 500,
+                       body: "{}",
+                       headers: { 'Content-Type' => 'text/pson', Puppet::Network::HTTP::HEADER_PUPPET_VERSION => '4.10.1' })
 
           expect {
-            terminus.save(request)
+            described_class.indirection.save(instance)
           }.to raise_error(Puppet::Error, /Server version 4.10.1 does not accept reports in 'json'/)
         end
 
         it "raises with HTTP 500 if the save fails and we're already using pson" do
           Puppet[:preferred_serialization_format] = "pson"
 
-          response = mock_response('500', '{}', 'text/pson')
-          allow(response).to receive(:[]).with(Puppet::Network::HTTP::HEADER_PUPPET_VERSION).and_return("4.10.1")
-          expect(connection).to receive(:put).and_return(response)
+          stub_request(:put, %r{/puppet/v3/report})
+            .to_return(status: 500,
+                       body: "{}",
+                       headers: { 'Content-Type' => 'text/pson', Puppet::Network::HTTP::HEADER_PUPPET_VERSION => '4.10.1' })
 
           expect {
-            terminus.save(request)
+            described_class.indirection.save(instance)
           }.to raise_error(Net::HTTPError, /Error 500 on SERVER/)
         end
       end
