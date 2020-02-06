2310809ed42ada1363e4061df3f6c0c814ecf59e
Josh Cooper <josh@puppet.com>
2020-02-06 10:05:26 -0800

    (PUP-10260) Use http client for status requests

--- lib/puppet/indirector/status/rest.rb
+++ lib/puppet/indirector/status/rest.rb
@@ -6,4 +6,19 @@ class Puppet::Indirector::Status::Rest < Puppet::Indirector::REST
   desc "Get puppet master's status via REST. Useful because it tests the health
     of both the web server and the indirector."
 
+  def find(request)
+    return super unless use_http_client?
+
+    session = Puppet.lookup(:http_session)
+    api = session.route_to(:puppet)
+    api.get_status(request.key)
+  rescue Puppet::HTTP::ResponseError => e
+    if e.response.code == 404
+      _, body = parse_response(e.response.nethttp)
+      msg = _("Find %{uri} resulted in 404 with the message: %{body}") % { uri: elide(e.response.url.path, 100), body: body }
+      raise Puppet::Error, msg
+    else
+      raise convert_to_http_error(e.response.nethttp)
+    end
+  end
 end
