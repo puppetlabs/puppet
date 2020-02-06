599858f11e1e5b9660bd4169689f1aaf3e00ff8b
Josh Cooper <josh@puppet.com>
2020-02-05 16:16:39 -0800

    (PUP-10260) Use http client for node rest requests
    Use the http client to get node requests. It preserves the fail_on_404
    behavior. The only behavior change is we no longer print the query
    parameters used in the request.

--- lib/puppet/indirector/node/rest.rb
+++ lib/puppet/indirector/node/rest.rb
@@ -4,4 +4,27 @@ require 'puppet/indirector/rest'
 class Puppet::Node::Rest < Puppet::Indirector::REST
   desc "Get a node via REST. Puppet agent uses this to allow the puppet master
     to override its environment."
+
+  def find(request)
+    return super unless use_http_client?
+
+    session = Puppet.lookup(:http_session)
+    api = session.route_to(:puppet)
+    api.get_node(
+      request.key,
+      environment: request.environment.to_s,
+      configured_environment: request.options[:configured_environment],
+      transaction_uuid: request.options[:transaction_uuid]
+    )
+  rescue Puppet::HTTP::ResponseError => e
+    if e.response.code == 404
+      return nil unless request.options[:fail_on_404]
+
+      _, body = parse_response(e.response.nethttp)
+      msg = _("Find %{uri} resulted in 404 with the message: %{body}") % { uri: elide(e.response.url.path, 100), body: body }
+      raise Puppet::Error, msg
+    else
+      raise convert_to_http_error(e.response.nethttp)
+    end
+  end
 end
