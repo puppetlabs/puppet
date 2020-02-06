8429cb8d4c820d08c56b376374e80787ac712e4c
Josh Cooper <josh@puppet.com>
2020-02-06 14:31:38 -0800

    (PUP-10260) Use http client for catalog requests
    This preserves the existing behavior of the caller supplying serialized and URL
    encoded facts as indirector request options, which the rest terminus needs to
    decode and deserialize so it can pass a facts object to the http client.
    
    As a result of this change, the agent will always POST for the catalog. In
    theory, it could result in a behavior change, but it would only happen if the
    HTTP request line would have been less 1024. Since facts are sent as query
    parameters and just core facts alone cause the request line to exceed 8k, puppet
    always makes a POST request in practice.
    
    It also preserves the behavior of setting/overwriting the catalog's name based
    on what was requested. Some of the configurer tests rely on that behavior.
    
    [1] https://bugs.ruby-lang.org/issues/3991

--- lib/puppet/indirector/catalog/rest.rb
+++ lib/puppet/indirector/catalog/rest.rb
@@ -3,4 +3,51 @@ require 'puppet/indirector/rest'
 
 class Puppet::Resource::Catalog::Rest < Puppet::Indirector::REST
   desc "Find resource catalogs over HTTP via REST."
+
+  def find(request)
+    return super unless use_http_client?
+
+    # URL encoded facts and facts_format are passed as indirector
+    # request options, so we have to reverse that (unescape, then parse),
+    # and pass a facts object to the http client.
+    format = request.options[:facts_format]
+    if format
+      formatter = Puppet::Network::FormatHandler.format_for(format)
+      facts = formatter.intern(Puppet::Node::Facts, CGI.unescape(request.options[:facts]))
+    else
+      facts = Puppet::Node::Facts.new(request.key, environment: request.environment.to_s)
+    end
+
+    checksum_type = if request.options[:checksum_type]
+                      request.options[:checksum_type].split('.')
+                    else
+                      Puppet[:supported_checksum_types]
+                    end
+
+    session = Puppet.lookup(:http_session)
+    api = session.route_to(:puppet)
+    catalog = api.post_catalog(
+      request.key,
+      facts: facts,
+      environment: request.environment.to_s,
+      configured_environment: request.options[:configured_environment],
+      transaction_uuid: request.options[:transaction_uuid],
+      job_uuid: request.options[:job_id],
+      static_catalog: request.options[:static_catalog],
+      checksum_type: checksum_type
+    )
+    # current tests rely on crazy behavior introduced in 089ac3e37dd
+    catalog.name = request.key
+    catalog
+  rescue Puppet::HTTP::ResponseError => e
+    if e.response.code == 404
+      return nil unless request.options[:fail_on_404]
+
+      _, body = parse_response(e.response.nethttp)
+      msg = _("Find %{uri} resulted in 404 with the message: %{body}") % { uri: elide(e.response.url.path, 100), body: body }
+      raise Puppet::Error, msg
+    else
+      raise convert_to_http_error(e.response.nethttp)
+    end
+  end
 end
