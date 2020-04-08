cc24c697773996738b9e00f9b602c957a2f7d7d7
Melissa Stone <melissa@puppet.com>
2020-04-08 14:37:09 -0700

    (PUP-10385) Migrate filebucket acceptance tests to integration
    With the new framework to handle server requests in the integration
    tests, we should try to migrate what we can from acceptance tests to
    integration. This commit does that for filebucket.

--- spec/integration/application/filebucket_spec.rb
+++ spec/integration/application/filebucket_spec.rb
@@ -76,4 +76,108 @@ describe "puppet filebucket" do
        )).to_stdout
     end
   end
+
+  context 'diff', unless: Puppet::Util::Platform.windows? do
+    context 'using a remote bucket' do
+      it 'outputs a diff between a local and remote file' do
+        File.binwrite(backup_file, "bar\nbaz")
+
+        get_handler = -> (req, res) {
+          res['Content-Type'] = 'application/octet-stream'
+          res.body = 'foo'
+        }
+
+        server.start_server(mounts: {filebucket: get_handler}) do |port|
+          Puppet[:masterport] = port
+          expect {
+            filebucket.command_line.args += ['diff', 'fac251367c9e083c6b1f0f3181', backup_file, '--remote']
+            filebucket.run
+          }.to output(a_string_matching(
+            /[-<] ?foo/
+          ).and matching(
+            /[+>] ?bar/
+          ).and matching(
+            /[+>] ?baz/
+          )).to_stdout
+        end
+      end
+
+      it 'outputs a diff between two remote files' do
+        get_handler = -> (req, res) {
+          res['Content-Type'] = 'application/octet-stream'
+          res.body = "--- /opt/puppetlabs/server/data/puppetserver/bucket/d/3/b/0/7/3/8/4/d3b07384d113edec49eaa6238ad5ff00/contents\t2020-04-06 21:25:24.892367570 +0000\n+++ /opt/puppetlabs/server/data/puppetserver/bucket/9/9/b/9/9/9/2/0/99b999207e287afffc86c053e5693247/contents\t2020-04-06 21:26:13.603398063 +0000\n@@ -1 +1,2 @@\n-foo\n+bar\n+baz\n"
+        }
+
+        server.start_server(mounts: {filebucket: get_handler}) do |port|
+          Puppet[:masterport] = port
+          expect {
+            filebucket.command_line.args += ['diff', 'd3b07384d113edec49eaa6238ad5ff00', "99b999207e287afffc86c053e5693247", '--remote']
+            filebucket.run
+          }.to output(a_string_matching(
+            /[-<] ?foo/
+          ).and matching(
+            /[+>] ?bar/
+          ).and matching(
+            /[+>] ?baz/
+          )).to_stdout
+        end
+      end
+    end
+
+    context 'using a local bucket' do
+      let(:filea) {
+        f = tmpfile('filea')
+        File.binwrite(f, 'foo')
+        f
+      }
+      let(:fileb) {
+        f = tmpfile('fileb')
+        File.binwrite(f, "bar\nbaz")
+        f
+      }
+      let(:checksuma) { Digest::MD5.file(filea).to_s }
+      let(:checksumb) { Digest::MD5.file(fileb).to_s }
+
+      it 'compares to files stored in a local bucket' do
+        expect {
+          filebucket.command_line.args = ['backup', filea, '--local']
+          filebucket.run
+        }.to output(/#{filea}: #{checksuma}/).to_stdout
+
+        expect{
+          filebucket.command_line.args = ['backup', fileb, '--local']
+          filebucket.run
+        }.to output(/#{fileb}: #{checksumb}\n/).to_stdout
+
+        expect {
+          filebucket.command_line.args = ['diff', checksuma, checksumb, '--local']
+          filebucket.run
+        }.to output(a_string_matching(
+          /[-<] ?foo/
+        ).and matching(
+          /[+>] ?bar/
+        ).and matching(
+          /[+>] ?baz/
+        )).to_stdout
+      end
+
+      it 'compares a file on the filesystem and a file stored in a local bucket' do
+        expect {
+          filebucket.command_line.args = ['backup', filea, '--local']
+          filebucket.run
+        }.to output(/#{filea}: #{checksuma}/).to_stdout
+
+        expect {
+          filebucket.command_line.args = ['diff', checksuma, fileb, '--local']
+          filebucket.run
+        }.to output(a_string_matching(
+          /[-<] ?foo/
+        ).and matching(
+          /[+>] ?bar/
+        ).and matching(
+          /[+>] ?baz/
+        )).to_stdout
+      end
+    end
+  end
 end
