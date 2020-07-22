3b6e645876dc922c335454e1db7183bde3b78614
Josh Cooper <joshcooper@users.noreply.github.com>
2020-07-21 18:29:38 -0700

    (PUP-1763) Rewrite plugins integration test
    The previous test used memory-based file metadata and content, which masked the
    source_permissions behavior for the `pluginfacts` mount. Also test the case
    where new plugins are downloaded.

--- spec/integration/faces/plugin_spec.rb
+++ spec/integration/faces/plugin_spec.rb
@@ -1,59 +1,41 @@
 require 'spec_helper'
-require 'puppet/face'
-require 'puppet/file_serving/metadata'
-require 'puppet/file_serving/content'
-require 'puppet/indirector/memory'
+require 'webmock/rspec'
 
-module PuppetFaceIntegrationSpecs
 describe "Puppet plugin face" do
-  INDIRECTORS = [
-    Puppet::Indirector::FileMetadata,
-    Puppet::Indirector::FileContent,
-  ]
+  before :each do
+    metadata = "[{\"path\":\"/etc/puppetlabs/code\",\"relative_path\":\".\",\"links\":\"follow\",\"owner\":0,\"group\":0,\"mode\":420,\"checksum\":{\"type\":\"ctime\",\"value\":\"{ctime}2020-07-10 14:00:00 -0700\"},\"type\":\"directory\",\"destination\":null}]"
+    stub_request(:get, %r{/puppet/v3/file_metadatas/(plugins|locales)}).to_return(status: 200, body: metadata, headers: {'Content-Type' => 'application/json'})
 
-  INDIRECTED_CLASSES = [
-    Puppet::FileServing::Metadata,
-    Puppet::FileServing::Content,
-    Puppet::Node::Facts,
-  ]
-
-  INDIRECTORS.each do |indirector|
-    class indirector::Memory < Puppet::Indirector::Memory
-      def find(request)
-        model.new('/dev/null', { 'type' => 'directory' })
-      end
-    end
+    # response retains owner/group/mode due to source_permissions => use
+    facts_metadata = "[{\"path\":\"/etc/puppetlabs/code\",\"relative_path\":\".\",\"links\":\"follow\",\"owner\":500,\"group\":500,\"mode\":493,\"checksum\":{\"type\":\"ctime\",\"value\":\"{ctime}2020-07-10 14:00:00 -0700\"},\"type\":\"directory\",\"destination\":null}]"
+    stub_request(:get, %r{/puppet/v3/file_metadatas/pluginfacts}).to_return(status: 200, body: facts_metadata, headers: {'Content-Type' => 'application/json'})
   end
 
-  before do
-    FileUtils.mkdir(File.join(Puppet[:vardir], 'lib'))
-    FileUtils.mkdir(File.join(Puppet[:vardir], 'facts.d'))
-    FileUtils.mkdir(File.join(Puppet[:vardir], 'locales'))
-    @termini_classes = {}
-    INDIRECTED_CLASSES.each do |indirected|
-      @termini_classes[indirected] = indirected.indirection.terminus_class
-      indirected.indirection.terminus_class = :memory
-    end
+  it "processes a download request resulting in no changes" do
+    # /opt/puppetlabs/puppet/cache/facts.d will be created based on our umask.
+    # If the mode on disk is not 0755, then the mode from the metadata response
+    # (493 => 0755) will be applied, resulting in "plugins were downloaded"
+    # message. Enforce a umask so the results are consistent.
+    Puppet::FileSystem.mkpath(Puppet[:pluginfactdest])
+    Puppet::FileSystem.chmod(0755, Puppet[:pluginfactdest])
+
+    app = Puppet::Application[:plugin]
+    app.command_line.args << 'download'
+    expect {
+      app.run
+    }.to exit_with(0)
+      .and output(/No plugins downloaded/).to_stdout
   end
 
-  after do
-    INDIRECTED_CLASSES.each do |indirected|
-      indirected.indirection.terminus_class = @termini_classes[indirected]
-      indirected.indirection.termini.clear
-    end
-  end
+  it "updates the facts.d mode", unless: Puppet::Util::Platform.windows? do
+    Puppet::FileSystem.mkpath(Puppet[:pluginfactdest])
+    Puppet::FileSystem.chmod(0775, Puppet[:pluginfactdest])
 
-  def init_cli_args_and_apply_app(args = ["download"])
-    Puppet::Application.find(:plugin).new(double('command_line', :subcommand_name => :plugin, :args => args))
+    app = Puppet::Application[:plugin]
+    app.command_line.args << 'download'
+    expect {
+      app.run
+    }.to exit_with(0)
+     .and output(/Downloaded these plugins: .*facts\.d/).to_stdout
   end
-
-  it "processes a download request" do
-    app = init_cli_args_and_apply_app
-    expect do
-      expect {
-        app.run
-      }.to exit_with(0)
-    end.to have_printed(/No plugins downloaded/)
-  end
-end
 end
