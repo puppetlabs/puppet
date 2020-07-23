505298a2391a1c4e8311c9c5909fa6443ca6e9e5
gimmy <gheorghe.popescu@puppet.com>
2020-07-23 14:41:09 +0300

    (PUP-10588) bump main version to 7.0.0

--- lib/puppet/version.rb
+++ lib/puppet/version.rb
@@ -6,7 +6,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '6.18.0'
+  PUPPETVERSION = '7.0.0'
 
   ##
   # version is a public API method intended to always provide a fast and
