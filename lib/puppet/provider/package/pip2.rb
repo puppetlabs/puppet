1b80f6cb90721f988fd83e31643b13fa15342e45
James Glenn <jglenn9k@gmail.com>
2020-08-28 10:50:54 -0700

    (PUP-7526) Adds pip2 provider

---
+++ lib/puppet/provider/package/pip2.rb
@@ -0,0 +1,17 @@
+# Puppet package provider for Python's `pip2` package management frontend.
+# <http://pip.pypa.io/>
+
+Puppet::Type.type(:package).provide :pip2,
+  :parent => :pip do
+
+  desc "Python packages via `pip2`.
+
+  This provider supports the `install_options` attribute, which allows command-line flags to be passed to pip2.
+  These options should be specified as an array where each element is either a string or a hash."
+
+  has_feature :installable, :uninstallable, :upgradeable, :versionable, :install_options, :targetable
+
+  def self.cmd
+    ["pip2"]
+  end
+end
