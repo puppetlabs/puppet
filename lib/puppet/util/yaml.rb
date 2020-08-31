f3c90046d4cfd8b634b0e5f326cbedc48ba03ca7
Ciprian Badescu <ciprian.badescu@puppet.com>
2020-08-31 12:53:55 +0300

    (PUP-10475) remove deprecated Puppet::Util::Yaml.load_file method

--- lib/puppet/util/yaml.rb
+++ lib/puppet/util/yaml.rb
@@ -42,28 +42,6 @@ module Puppet::Util::Yaml
     safe_load(yaml, allowed_classes, filename)
   end
 
-  # @deprecated Use {#safe_load_file} instead.
-  def self.load_file(filename, default_value = false, strip_classes = false)
-    Puppet.deprecation_warning(_("Puppet::Util::Yaml.load_file is deprecated. Use safe_load_file instead."))
-
-    if(strip_classes) then
-      data = YAML::parse_file(filename)
-      data.root.each do |o|
-        if o.respond_to?(:tag=) and
-           o.tag != nil and
-           o.tag.start_with?("!ruby")
-          o.tag = nil
-        end
-      end
-      data.to_ruby || default_value
-    else
-      yaml = YAML.load_file(filename)
-      yaml || default_value
-    end
-  rescue *YamlLoadExceptions => detail
-    raise YamlLoadError.new(detail.message, detail)
-  end
-
   def self.dump(structure, filename)
     Puppet::FileSystem.replace_file(filename, 0660) do |fh|
       YAML.dump(structure, fh)
