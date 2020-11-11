0d77e853bdffa05b43f1fb3f3fc1cf4e92fed519
Josh Cooper <joshcooper@users.noreply.github.com>
2020-11-10 16:49:56 -0800

    (maint) Cleanup indirector tests
    The tests hid the fact that `save` takes different arguments than other methods,
    and some methods (like `search`) are not supported for the filebucket :file
    terminus.

--- spec/unit/indirector/file_bucket_file/selector_spec.rb
+++ spec/unit/indirector/file_bucket_file/selector_spec.rb
@@ -5,22 +5,40 @@ require 'puppet/indirector/file_bucket_file/file'
 require 'puppet/indirector/file_bucket_file/rest'
 
 describe Puppet::FileBucketFile::Selector do
+  let(:model) { Puppet::FileBucket::File.new('') }
+  let(:indirection) { Puppet::FileBucket::File.indirection }
+  let(:terminus) { indirection.terminus(:selector) }
+
   %w[head find save search destroy].each do |method|
     describe "##{method}" do
       it "should proxy to rest terminus for https requests" do
-        request = double('request', :protocol => 'https')
+        key = "https://example.com/path/to/file"
 
-        expect_any_instance_of(Puppet::FileBucketFile::Rest).to receive(method).with(request)
+        expect(indirection.terminus(:rest)).to receive(method)
 
-        subject.send(method, request)
+        if method == 'save'
+          terminus.send(method, indirection.request(method, key, model))
+        else
+          terminus.send(method, indirection.request(method, key, nil))
+        end
       end
 
       it "should proxy to file terminus for other requests" do
-        request = double('request', :protocol => 'file')
+        key = "file:///path/to/file"
 
-        expect_any_instance_of(Puppet::FileBucketFile::File).to receive(method).with(request)
-
-        subject.send(method, request)
+        case method
+        when 'save'
+          expect(indirection.terminus(:file)).to receive(method)
+          terminus.send(method, indirection.request(method, key, model))
+        when 'find', 'head'
+          expect(indirection.terminus(:file)).to receive(method)
+          terminus.send(method, indirection.request(method, key, nil))
+        else
+          # file terminus doesn't implement search or destroy
+          expect {
+            terminus.send(method, indirection.request(method, key, nil))
+          }.to raise_error(NoMethodError)
+        end
       end
     end
   end
