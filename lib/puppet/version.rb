52db30333d1015744407e92783881168e60ab760
Jenkins CI <ci@puppet.com>
2021-05-21 07:04:03 +0000

    (packaging) Bump to version '7.8.0' [no-promote]

--- lib/puppet/version.rb
+++ lib/puppet/version.rb
@@ -6,7 +6,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '7.7.0'
+  PUPPETVERSION = '7.8.0'
 
   ##
   # version is a public API method intended to always provide a fast and
