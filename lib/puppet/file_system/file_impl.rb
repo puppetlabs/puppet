ee3bbf833ca8a26b988fef6a051469aa521bbdb5
Josh Cooper <joshcooper@users.noreply.github.com>
2021-06-03 23:29:39 -0700

    (PUP-6585) Push binread and compare_stream into FileImpl
    The FileImpl versions of binread and compare_stream are never called because
    Posix overrides them. To reduce complexity, push the binread and compare_stream
    methods from Posix into the FileImpl superclass. JRuby and Windows extend Posix,
    so there's no change in behavior there.

--- lib/puppet/file_system/file_impl.rb
+++ lib/puppet/file_system/file_impl.rb
@@ -1,6 +1,7 @@
 # Abstract implementation of the Puppet::FileSystem
 #
 class Puppet::FileSystem::FileImpl
+  DEFAULT_BLOCK_SIZE = 1024
 
   def pathname(path)
     path.is_a?(Pathname) ? path : Pathname.new(path)
@@ -88,7 +89,7 @@ class Puppet::FileSystem::FileImpl
   end
 
   def binread(path)
-    raise NotImplementedError
+    ::File.binread(path)
   end
 
   def exist?(path)
@@ -147,8 +148,25 @@ class Puppet::FileSystem::FileImpl
     ::File.lstat(path)
   end
 
+  # Provide an encoding agnostic version of compare_stream
+  #
+  # The FileUtils implementation in Ruby 2.0+ was modified in a manner where
+  # it cannot properly compare File and StringIO instances. To sidestep that
+  # issue this method reimplements the faster 2.0 version that will correctly
+  # compare binary File and StringIO streams.
   def compare_stream(path, stream)
-    open(path, 0, 'rb') { |this| FileUtils.compare_stream(this, stream) }
+    open(path, 0, 'rb') do |this|
+      bsize = stream_blksize(this, stream)
+      sa = "".force_encoding('ASCII-8BIT')
+      sb = "".force_encoding('ASCII-8BIT')
+      loop do
+        this.read(bsize, sa)
+        stream.read(bsize, sb)
+        return true if sa.empty? && sb.empty?
+        break if sa != sb
+      end
+      false
+    end
   end
 
   def chmod(mode, path)
@@ -186,5 +204,23 @@ class Puppet::FileSystem::FileImpl
 
   def absolute?(path)
     ::File.absolute_path?(path)
+  end
+
+  private
+
+  def stream_blksize(*streams)
+    streams.each do |s|
+      next unless s.respond_to?(:stat)
+      size = blksize(s.stat)
+      return size if size
+    end
+    DEFAULT_BLOCK_SIZE
+  end
+
+  def blksize(st)
+    s = st.blksize
+    return nil unless s
+    return nil if s == 0
+    s
   end
 end
