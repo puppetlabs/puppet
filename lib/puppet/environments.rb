a6e0a6bed3dd9ccef5d72d4d2c39a873c1cb454a
Luchian Nemes <luchian.nemes@puppet.com>
2021-06-23 16:39:44 +0300

    (PUP-10955) Normalize environment name to symbol before caching
    The `Puppet::Environments::Cached.get` method says it accepts Strings or
    Symbols. This commit normalizes the environment name before caching and
    getting the environment to avoid duplicated environments in cache (as
    String and as Symbol).

--- lib/puppet/environments.rb
+++ lib/puppet/environments.rb
@@ -398,14 +398,15 @@ module Puppet::Environments
       # This strategy favors smaller memory footprint over environment
       # retrieval time.
       clear_all_expired
+      name = name.to_sym
       entry = @cache[name]
       if entry
         Puppet.debug {"Found in cache #{name.inspect} #{entry.label}"}
         # found in cache
         entry.touch
-      elsif (result = @loader.get(name))
+      elsif (env = @loader.get(name))
         # environment loaded, cache it
-        entry = entry(result)
+        entry = entry(env)
         add_entry(name, entry)
       end
       entry
@@ -433,6 +434,7 @@ module Puppet::Environments
     # Clears the cache of the environment with the given name.
     # (The intention is that this could be used from a MANUAL cache eviction command (TBD)
     def clear(name)
+      name = name.to_sym
       entry = @cache[name]
       clear_entry(name, entry) if entry
     end
@@ -482,6 +484,7 @@ module Puppet::Environments
     #
     # @!macro loader_get_conf
     def get_conf(name)
+      name = name.to_sym
       clear_if_expired(name, @cache[name])
       @loader.get_conf(name)
     end
