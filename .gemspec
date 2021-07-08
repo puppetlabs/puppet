8307bf1b765da7166d0f131ec0a39802fc52ea09
Jenkins CI <ci@puppet.com>
2021-07-08 10:43:47 +0000

    (packaging) Bump to version '6.25.0' [no-promote]

--- .gemspec
+++ .gemspec
@@ -13,7 +13,7 @@
 
 Gem::Specification.new do |s|
   s.name = "puppet"
-  version = "6.24.0"
+  version = "6.25.0"
   mdata = version.match(/(\d+\.\d+\.\d+)/)
   s.version = mdata ? mdata[1] : version
 
--- lib/puppet/version.rb
+++ lib/puppet/version.rb
@@ -6,7 +6,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '6.24.0'
+  PUPPETVERSION = '6.25.0'
 
   ##
   # version is a public API method intended to always provide a fast and
