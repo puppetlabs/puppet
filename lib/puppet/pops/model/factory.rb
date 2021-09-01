932d2f630a6b071639a142788f86bd666d6f596b
Luchian Nemes <luchian.nemes@puppet.com>
2021-09-01 15:14:11 +0300

    (PUP-11232) Freeze strings in model factory and type formatter from `pops`
    This commit adds `frozen_string_literal` in
    `lib/puppet/pops/model/factory.rb` and
    `lib/puppet/pops/types/type_formatter.rb` to lower memory consumption of
    `puppet/lib` with `365947` with exception to strings that need to be
    modified during a run.

--- lib/puppet/pops/model/factory.rb
+++ lib/puppet/pops/model/factory.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Factory is a helper class that makes construction of a Pops Model
 # much more convenient. It can be viewed as a small internal DSL for model
 # constructions.
@@ -1156,7 +1157,7 @@ class Factory
   end
 
   def self.concat(*args)
-    result = ''
+    result = ''.dup
     args.each do |e|
       if e.instance_of?(Factory) && e.model_class <= LiteralString
         result << e[KEY_VALUE]
