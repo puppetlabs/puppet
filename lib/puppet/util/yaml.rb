6084fcb53122806877dd4dcc37ad90fa2d340827
Raul Tambre <raul.tambre@cleveron.com>
2022-01-02 17:33:34 +0200

    (PUP-11405) Replace deprecated/changed Psych YAML methods, pin rdoc
    The keyword variant of safe_load() was added and the old variant deprecated in
    Psych 3.1.0. It has been removed in 4.0.0 (https://github.com/ruby/psych/commit/0767227051dbddf1f949eef512c174deabf22891).
    
    The load_file() method was changed to be safe in Psych 4.0.1 (https://github.com/ruby/psych/commit/f8a5e512a1856231bf10c80e8a08714b06fb95c4).
    Use the unsafe_load_file() introduced in 3.3.2 to keep the same behaviour.
    
    The usages are guarded by version checks since Puppet still supports Ruby 2.5,
    which ships Psych 3.0.2.
    
    Also pin rdoc to prevent 6.0.4, which depends on Psych 4.0 causing issues with
    other dependencies that haven't been updated yet to work due to the same
    changes.

--- lib/puppet/util/yaml.rb
+++ lib/puppet/util/yaml.rb
@@ -24,7 +24,11 @@ module Puppet::Util::Yaml
   # @raise [YamlLoadException] If deserialization fails.
   # @return The parsed YAML, which can be Hash, Array or scalar types.
   def self.safe_load(yaml, allowed_classes = [], filename = nil)
-    data = YAML.safe_load(yaml, allowed_classes, [], true, filename)
+    if Gem::Version.new(Psych::VERSION) >= Gem::Version.new('3.1.0')
+      data = YAML.safe_load(yaml, permitted_classes: allowed_classes, aliases: true, filename: filename)
+    else
+      data = YAML.safe_load(yaml, allowed_classes, [], true, filename)
+    end
     data = false if data.nil?
     data
   rescue ::Psych::DisallowedClass => detail
@@ -68,7 +72,11 @@ module Puppet::Util::Yaml
       end
       data.to_ruby || default_value
     else
-      yaml = YAML.load_file(filename)
+      if Gem::Version.new(Psych::VERSION) >= Gem::Version.new('3.3.2')
+        yaml = YAML.unsafe_load_file(filename)
+      else
+        yaml = YAML.load_file(filename)
+      end
       yaml || default_value
     end
   rescue *YamlLoadExceptions => detail
