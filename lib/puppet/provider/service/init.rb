283d1bf93356aa832c74ded2073bcf232eaeec32
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-08-07 13:51:22 +0200

    Simplify confine statement

--- lib/puppet/provider/service/init.rb
+++ lib/puppet/provider/service/init.rb
@@ -20,12 +20,9 @@ Puppet::Type.type(:service).provide :init, :parent => :base do
   end
 
   # Debian and Ubuntu should use the Debian provider.
+  confine :false => ['Debian', 'Ubuntu'].include?(Puppet.runtime[:facter].value('os.name'))
   # RedHat systems should use the RedHat provider.
-  confine :true => begin
-      os = Puppet.runtime[:facter].value('os.name')
-      family = Puppet.runtime[:facter].value('os.family')
-      !(os == 'Debian' || os == 'Ubuntu' || family == 'RedHat')
-  end
+  confine :false => Puppet.runtime[:facter].value('os.family') == 'RedHat'
 
   # We can't confine this here, because the init path can be overridden.
   #confine :exists => defpath
