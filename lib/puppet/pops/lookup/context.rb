8c656a94ce1ad0b4c9d027aa4273ff835a133374
Michael Hashizume <michael.hashizume@puppet.com>
2023-12-21 11:36:11 -0800

    Improve variable assignment readability
    The assignment of the @type instance variable in
    Puppet::Pops::Context#register_ptype resulted in poor readability due to
    the number of arguments in the Pcore::create_object_type method call and
    the number of those arguments that are nested hashes.
    
    This commit reformats this assignment for easier readability and shorter
    line lengths.

--- lib/puppet/pops/lookup/context.rb
+++ lib/puppet/pops/lookup/context.rb
@@ -134,31 +134,38 @@ class Context
   def self.register_ptype(loader, ir)
     tf = Types::TypeFactory
     key_type = tf.optional(tf.scalar)
-    @type = Pcore::create_object_type(loader, ir, self, 'Puppet::LookupContext', 'Any',
-                                      {
-                                        'environment_name' => {
-                                          Types::KEY_TYPE => Types::PStringType::NON_EMPTY,
-                                          Types::KEY_KIND => Types::PObjectType::ATTRIBUTE_KIND_DERIVED
-                                        },
-                                        'module_name' => {
-                                          Types::KEY_TYPE => tf.variant(Types::PStringType::NON_EMPTY, Types::PUndefType::DEFAULT)
-                                        }
-                                      },
-                                      {
-                                        'not_found' => tf.callable([0, 0], tf.undef),
-                                        'explain' => tf.callable([0, 0, tf.callable(0,0)], tf.undef),
-                                        'interpolate' => tf.callable(1, 1),
-                                        'cache' => tf.callable([key_type, tf.any], tf.any),
-                                        'cache_all' => tf.callable([tf.hash_kv(key_type, tf.any)], tf.undef),
-                                        'cache_has_key' => tf.callable([key_type], tf.boolean),
-                                        'cached_value' => tf.callable([key_type], tf.any),
-                                        'cached_entries' => tf.variant(
-                                          tf.callable([0, 0, tf.callable(1,1)], tf.undef),
-                                          tf.callable([0, 0, tf.callable(2,2)], tf.undef),
-                                          tf.callable([0, 0], tf.iterable(tf.tuple([key_type, tf.any])))
-                                        ),
-                                        'cached_file_data' => tf.callable(tf.string, tf.optional(tf.callable([1, 1])))
-                                      }).resolve(loader)
+    @type =
+      Pcore::create_object_type(
+        loader,
+        ir,
+        self,
+        'Puppet::LookupContext',
+        'Any',
+        {
+          'environment_name' => {
+            Types::KEY_TYPE => Types::PStringType::NON_EMPTY,
+            Types::KEY_KIND => Types::PObjectType::ATTRIBUTE_KIND_DERIVED
+          },
+          'module_name' => {
+            Types::KEY_TYPE => tf.variant(Types::PStringType::NON_EMPTY, Types::PUndefType::DEFAULT)
+          }
+        },
+        {
+          'not_found' => tf.callable([0, 0], tf.undef),
+          'explain' => tf.callable([0, 0, tf.callable(0,0)], tf.undef),
+          'interpolate' => tf.callable(1, 1),
+          'cache' => tf.callable([key_type, tf.any], tf.any),
+          'cache_all' => tf.callable([tf.hash_kv(key_type, tf.any)], tf.undef),
+          'cache_has_key' => tf.callable([key_type], tf.boolean),
+          'cached_value' => tf.callable([key_type], tf.any),
+          'cached_entries' => tf.variant(
+            tf.callable([0, 0, tf.callable(1,1)], tf.undef),
+            tf.callable([0, 0, tf.callable(2,2)], tf.undef),
+            tf.callable([0, 0], tf.iterable(tf.tuple([key_type, tf.any])))
+          ),
+          'cached_file_data' => tf.callable(tf.string, tf.optional(tf.callable([1, 1])))
+        }
+      ).resolve(loader)
   end
 
   # Mainly for test purposes. Makes it possible to create a {Context} in Puppet code provided that a current {Invocation} exists.
