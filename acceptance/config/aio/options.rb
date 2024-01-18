99befbd805976e67a77039ea143fedab0bb5a27d
Josh Cooper <joshcooper@users.noreply.github.com>
2024-01-18 15:52:37 -0800

    (PA-4154) Add post-suite step to archive logs
    (cherry picked from commit 48c122c06fe5b60a6d85a342f3fde7952fc829c4)

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -4,5 +4,8 @@
   'use-service'                => true, # use service scripts to start/stop stuff
   'puppetservice'              => 'puppetserver',
   'puppetserver-confdir'       => '/etc/puppetlabs/puppetserver/conf.d',
-  'puppetserver-config'        => '/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf'
+  'puppetserver-config'        => '/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf',
+  :post_suite => [
+    'teardown/common/099_Archive_Logs.rb',
+  ],
 }
