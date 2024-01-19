04b01e2ff5ef3cd48e4b5402c1f4a619464e98e5
Michael Hashizume <michael.hashizume@puppet.com>
2024-01-18 16:14:39 -0800

    Correct Red Hat service providers
    SystemD has been the default init system for Red Hat Enterprise Linux
    and its derivatives since RHEL 7 was released in 2014. In 928cf67, we
    attempted to reflect this in Puppet's service providers.
    
    However, this change did not have the desired effect as the RedHat
    service provider was still specified as the default for systems with the
    os.family fact of "redhat." Tests relating to services started failing
    after 928cf67.
    
    I do not believe that this change will have any undesired effects on Red
    Hat derivatives like Alma or Rocky because those distributions also
    use SystemD as their init system and use the same versioning scheme as
    upstream.

--- lib/puppet/provider/service/redhat.rb
+++ lib/puppet/provider/service/redhat.rb
@@ -10,7 +10,7 @@ Puppet::Type.type(:service).provide :redhat, :parent => :init, :source => :init 
 
   commands :chkconfig => "/sbin/chkconfig", :service => "/sbin/service"
 
-  defaultfor 'os.family' => :redhat
+  defaultfor 'os.name' => :amazon, 'os.release.major' => ["2017", "2018"]
   defaultfor 'os.name' => :redhat, 'os.release.major' => (4..6).to_a
   defaultfor 'os.family' => :suse, 'os.release.major' => ["10", "11"]
 
