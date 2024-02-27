puppetlabs/puppet

commit db7c19628b372699bd4f2f3c64b77c7cd0bbbdfe

(packaging) Bump to version '8.5.0x' [no-promote]

.gemspec
@@ -16,7 +16,7 @@
 
 Gem::Specification.new do |s|
   s.name = "puppet"
-  version = "8.5.0"
+  version = "8.5.0x"
   mdata = version.match(/(\d+\.\d+\.\d+)/)
   s.version = mdata ? mdata[1] : version
 

lib/puppet/version.rb
@@ -8,7 +8,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '8.5.0'
+  PUPPETVERSION = '8.5.0x'
 
   ##
   # version is a public API method intended to always provide a fast and

commit c51c9b0a1846bf271ebd2e9167a37a650a993fe5

(packaging) Bump to version '8.6.0' [no-promote]
db7c196 accidentally bumped Puppet's version to "8.5.0x" instead of
"8.6.0". This commit updates Puppet's version, both in the version file
and gemspec, to 8.6.0.

.gemspec
@@ -16,7 +16,7 @@
 
 Gem::Specification.new do |s|
   s.name = "puppet"
-  version = "8.5.0x"
+  version = "8.6.0"
   mdata = version.match(/(\d+\.\d+\.\d+)/)
   s.version = mdata ? mdata[1] : version
 

lib/puppet/version.rb
@@ -8,7 +8,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '8.5.0x'
+  PUPPETVERSION = '8.6.0'
 
   ##
   # version is a public API method intended to always provide a fast and