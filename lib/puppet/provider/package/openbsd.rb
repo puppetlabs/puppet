839a1c0267055e146f69d50ab2dc05110a4829bc
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2024-03-21 08:07:48 +0100

    get rid of warning

--- lib/puppet/provider/package/openbsd.rb
+++ lib/puppet/provider/package/openbsd.rb
@@ -91,7 +91,7 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
         return match[2]
       end
 
-      vcmp = properties[:ensure].split('.').map { &:to_i } <=> match[2].split('.').map { &:to_i }
+      vcmp = properties[:ensure].split('.').map { |s|s.to_i } <=> match[2].split('.').map { |s|s.to_i }
       if vcmp > 0
         # The locally installed package may actually be newer than what a mirror
         # has. Log it at debug, but ignore it otherwise.
